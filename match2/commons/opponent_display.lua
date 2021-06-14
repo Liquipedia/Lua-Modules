228a8424a029035dd6f206cf467e75b2778194dc
warnull <warnull@gmail.com>
2021-06-14 08:41:06 -0700

    address comments

--- match2/commons/opponent_display.lua
+++ match2/commons/opponent_display.lua
@@ -98,7 +98,7 @@ Displays an opponent as an inline element. Useful for describing opponents in
 prose.
 ]]
 function OpponentDisplay.InlineOpponent(props)
-	DisplayUtil.assertPropTypes(props, OpponentDisplay.propTypes.InlineOpponent)
+	DisplayUtil.assertPropTypes(props, OpponentDisplay.propTypes.InlineOpponent, {maxDepth = 2})
 	local opponent = props.opponent
 
 	if opponent.type == 'team' then
@@ -133,7 +133,7 @@ Displays an opponent as a block element. The width of the component is
 determined by its layout context, and not of the opponent.
 ]]
 function OpponentDisplay.BlockOpponent(props)
-	DisplayUtil.assertPropTypes(props, OpponentDisplay.propTypes.BlockOpponent)
+	DisplayUtil.assertPropTypes(props, OpponentDisplay.propTypes.BlockOpponent, {maxDepth = 2})
 	local opponent = props.opponent
 
 	if opponent.type == 'team' then
