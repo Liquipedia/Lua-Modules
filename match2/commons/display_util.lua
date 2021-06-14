aa5c06caf58454ec74cb0e42822a1474acdb74fb
warnull <warnull@gmail.com>
2021-06-13 20:43:16 -0700

    minor changes

--- match2/commons/display_util.lua
+++ match2/commons/display_util.lua
@@ -2,6 +2,8 @@ local Array = require('Module:Array')
 local FnUtil = require('Module:FnUtil')
 local TypeUtil = require('Module:TypeUtil')
 
+local html = mw.html
+
 local DisplayUtil = {propTypes = {}, types = {}}
 
 --[[
