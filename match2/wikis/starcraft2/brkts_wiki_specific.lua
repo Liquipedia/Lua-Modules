e3686cddc96d2e71659c6cd503c1fbd1fad2a7d4
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2021-06-24 12:31:09 +0200

    Create brkts_wiki_specific.lua

---
+++ match2/wikis/starcraft2/brkts_wiki_specific.lua
@@ -0,0 +1,43 @@
+local FnUtil = require('Module:FnUtil')
+local Lua = require('Module:Lua')
+local Table = require('Module:Table')
+
+local WikiSpecific = Table.copy(require('Module:Brkts/WikiSpecific/Base'))
+
+WikiSpecific.processMatch = FnUtil.lazilyDefineFunction(function()
+	local InputModule = require('Module:DevFlags').matchGroupDev
+		and Lua.requireIfExists('Module:MatchGroup/Input/StarCraft/dev')
+		or require('Module:MatchGroup/Input/StarCraft')
+	return InputModule.processMatch
+end)
+
+-- useless functions that should be present for some default checks
+-- would get called from Module:Match/Subobjects if we wouldn't circumvent that module completly
+function WikiSpecific.processMap(frame, map)
+	return map
+end
+
+function WikiSpecific.processOpponent(frame, opponent)
+	return opponent
+end
+
+WikiSpecific.matchFromRecord = FnUtil.lazilyDefineFunction(function()
+	return require('Module:MatchGroup/Util/Starcraft').matchFromRecord
+end)
+
+function WikiSpecific.getMatchGroupModule(matchGroupType)
+	local DevFlags = require('Module:DevFlags')
+	if matchGroupType == 'matchlist' then
+		return DevFlags.matchGroupDev
+			and Lua.requireIfExists('Module:MatchGroup/Display/Matchlist/Starcraft/dev')
+			or require('Module:MatchGroup/Display/Matchlist/Starcraft')
+	else -- matchGroupType == 'bracket'
+		return DevFlags.matchGroupDev
+			and Lua.requireIfExists('Module:MatchGroup/Display/Bracket/Starcraft/dev')
+			or require('Module:MatchGroup/Display/Bracket/Starcraft')
+	end
+end
+
+WikiSpecific.defaultIcon = 'StarCraft 2 Default logo.png'
+
+return WikiSpecific
