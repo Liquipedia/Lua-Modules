63ee331fccc2032ad7da4247f59a45e0501d37ea
Jasper van Riet <jvanriet@teamliquid.com>
2021-07-01 14:37:37 +0200

    Show agent icon and tweak table

--- match2/wikis/valorant/big_match.lua
+++ match2/wikis/valorant/big_match.lua
@@ -13,6 +13,7 @@ local Links = require('Module:Links')
 local Countdown = require('Module:Countdown')
 local Table = require('Module:Table')
 local Tabs = require('Module:Tabs')
+local Template = require('Module:Template')
 
 local BigMatch = Class.new()
 
@@ -48,7 +49,7 @@ function BigMatch:render(frame, match, tournament)
 
 	overall :node(self:header(match, opponent1, opponent2, tournament))
 			:node(self:overview(match))
-			:node(self:stats(match, playerLookUp))
+			:node(self:stats(frame, match, playerLookUp, opponent1, opponent2))
 			:node(self:economy(match, opponent1, opponent2))
 
 	return overall
@@ -123,7 +124,7 @@ function BigMatch:overview(match)
 								:node(boxRight)
 end
 
-function BigMatch:stats(match, playerLookUp)
+function BigMatch:stats(frame, match, playerLookUp, opponent1, opponent2)
 	local tabs = {
 		This = 1,
 	}
@@ -144,11 +145,10 @@ function BigMatch:stats(match, playerLookUp)
 			DivTable.HeaderRow():cell(mw.html.create('div'):wikitext('Player'))
 				:cell(mw.html.create('div'):wikitext('Agent'))
 				:cell(mw.html.create('div'):wikitext('Kills'))
-				:cell(mw.html.create('div'):wikitext('Assists'))
 				:cell(mw.html.create('div'):wikitext('Deaths'))
+				:cell(mw.html.create('div'):wikitext('Assists'))
 				:cell(mw.html.create('div'):wikitext('ACS'))
 		)
-
 
 		local participants = Json.parse(map.participants or '{}')
 		if not Table.isEmpty(participants) then
@@ -161,17 +161,20 @@ function BigMatch:stats(match, playerLookUp)
 					local row = DivTable.Row()
 
 					row	:cell(mw.html.create('div'):wikitext(playerLookUp[index].name))
-						:cell(mw.html.create('div'):wikitext(player['agent']))
+						:cell(
+							mw.html.create('div')	:addClass('fb-match-page-valorant-stats-agent')
+													:wikitext(Template.safeExpand(frame, 'AgentIcon/' .. player['agent']))
+						)
 						:cell(mw.html.create('div'):wikitext(player['kills']))
-						:cell(mw.html.create('div'):wikitext(player['assists']))
 						:cell(mw.html.create('div'):wikitext(player['deaths']))
+						:cell(mw.html.create('div'):wikitext(player['assists']))
 						:cell(mw.html.create('div'):wikitext(player['acs']))
 					divTable:row(row)
 				end
 			end
 		end
 
-		tabs['content' .. ind] = tostring(divTable:create())
+		tabs['content' .. ind] = tostring(divTable:create():addClass('fb-match-page-valorant-stats'))
 
 		ind = ind + 1
 	end
