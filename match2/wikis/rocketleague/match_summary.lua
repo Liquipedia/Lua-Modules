59ba023a405644488038c0d5fdc62807091417ae
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2021-07-21 10:49:15 +0200

    Add optional gameHeader display (#104)
    * Add optional gameHeader display
    
    Add optional gameHeader display
    
    * Update match_summary.lua
    
    forgot to remove that^^

--- match2/wikis/rocketleague/match_summary.lua
+++ match2/wikis/rocketleague/match_summary.lua
@@ -59,6 +59,15 @@ function p.getByMatchId(args)
 						  "[[File:GreenCheck.png|14x14px|link=]]" or
 						  "[[File:NoCheck.png|link=]]")
 			}
+			local gameHeader = game.header or ''
+			if gameHeader ~= '' then
+				table.insert(gameElements, 1, p._breakNode())
+				table.insert(gameElements, 1, htmlCreate("div")
+					:node(gameHeader)
+					:css("font-weight","bold")
+					:css("font-size","85%")
+					:css("margin","auto"))
+			end
 			if game.comment then
 				table.insert(gameElements, p._breakNode())
 				table.insert(gameElements, htmlCreate("div")
