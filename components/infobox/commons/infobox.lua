54a45091e46986a389c08b0184c7604764642681
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2021-08-13 15:47:44 +0200

    allow twitch2, twitter2, ...
    allow twitch2, twitter2, ...

--- components/infobox/commons/infobox.lua
+++ components/infobox/commons/infobox.lua
@@ -186,13 +186,18 @@ function Infobox:links(links, variant)
                     :addClass('infobox-icons')
 
     for key, value in pairs(links) do
+        local iconString = Infobox.getIconString(key)
         local link = '[' .. Links.makeFullLink(key, value, variant) ..
-            ' <i class="lp-icon lp-' .. key .. '></i>]'
+            ' <i class="lp-icon lp-' .. iconString .. '></i>]'
         infoboxLinks:wikitext(' ' .. link)
     end
 
     self.content:node(mw.html.create('div'):node(infoboxLinks))
     return self
+end
+
+function Infobox.getIconString(key)
+	return string.gsub(key, '%d$', '')
 end
 
 function Infobox:centeredCell(content)
