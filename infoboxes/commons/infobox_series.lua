f9469b247b472f00d8b51705123601ef297633fe
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2021-08-13 08:00:48 +0200

    switch Flags to module instead of template
    switch Flags to module instead of template safeExpand (module has the safeExpand as a fallback)

--- infoboxes/commons/infobox_series.lua
+++ infoboxes/commons/infobox_series.lua
@@ -7,6 +7,7 @@ local Cell = require('Module:Infobox/Cell')
 local Infobox = require('Module:Infobox')
 local Template = require('Module:Template')
 local Table = require('Module:Table')
+local Flags = require('Module:Flags')._Flag
 local Namespace = require('Module:Namespace')
 local Locale = require('Module:Locale')
 local ReferenceCleaner = require('Module:ReferenceCleaner')
@@ -171,8 +172,7 @@ function Series:_createLocation(country, city)
 		return ''
 	end
 
-	return Template.safeExpand(self.frame, 'Flag/' .. string.lower(country)) ..
-				'&nbsp;' .. (city or country)
+	return Flags(country) .. '&nbsp;' .. (city or country)
 end
 
 function Series:_setCountryCategories(country)
