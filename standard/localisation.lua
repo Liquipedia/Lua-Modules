327a2055488d6aa92d666f5c37bfa2218678b963
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2021-08-16 15:17:02 +0200

    Create localisation.lua (#195)
    * Create localisation.lua
    
    * Update localisation.lua
    
    * Update standard/localisation.lua
    
    Co-authored-by: Jasper van Riet <JaspervanRiet@users.noreply.github.com>
    
    * Update standard/localisation.lua
    
    Co-authored-by: Jasper van Riet <JaspervanRiet@users.noreply.github.com>
    
    * Update standard/localisation.lua
    
    Co-authored-by: Jasper van Riet <JaspervanRiet@users.noreply.github.com>
    
    * Update standard/localisation.lua
    
    Co-authored-by: Jasper van Riet <JaspervanRiet@users.noreply.github.com>
    
    * Update localisation.lua
    
    * Update localisation.lua
    
    * Update localisation.lua
    
    * Update localisation.lua
    
    * Update localisation.lua
    
    * Update localisation.lua
    
    Co-authored-by: Jasper van Riet <JaspervanRiet@users.noreply.github.com>

---
+++ standard/localisation.lua
@@ -0,0 +1,68 @@
+local Class = require('Module:Class')
+local Localisation = {}
+local String = require('Module:StringUtils')
+
+function Localisation.getCountryName(country, noentry)
+	noentry = noentry or ''
+	local data = mw.loadData('Module:Localisation/data/country')
+
+	-- clean the entered country value
+	country = Localisation._cleanCountry(country)
+
+	-- First try to look it up
+	local countryname = data[country]
+
+	-- Return message if none is found
+	if countryname == nil then
+		mw.log('No country found in Module:Localisation/data/country: ' .. country)
+		-- set category unless second argument is set
+		if noentry ~= '' then
+			countryname = ''
+		else
+			countryname = mw.getCurrentFrame()
+				:expandTemplate{title = 'Flag/invalidcountry', args = {country}}
+		end
+	end
+
+	return countryname
+end
+
+function Localisation.getLocalisation(country, noentry)
+	noentry = noentry or ''
+	local dataModuleName = 'Module:Localisation/data/localised'
+	local data = mw.loadData(dataModuleName)
+
+	-- clean the entered country value
+	country = Localisation._cleanCountry(country)
+
+	-- First try to look it up
+	local localised = data[country]
+
+	-- Return message if none is found
+	if localised == nil then
+		mw.log('No country found in ' .. dataModuleName .. ': ' .. country)
+		-- set category unless second argument is set
+		if noentry ~= '' then
+			localised = ''
+		else
+			localised = 'Unknown country "[[lpcommons:' .. dataModuleName ..
+				'|' .. country .. ']][[Category:Pages with unknown countries]]'
+		end
+	end
+
+	return localised
+end
+
+function Localisation._cleanCountry(country)
+	if String.isEmpty(country) then
+		country = 'nocountry'
+	end
+	-- Remove whitespace
+	country = mw.text.trim(country)
+	country = mw.text.unstripNoWiki(country)
+	country = string.upper(country)
+
+	return country
+end
+
+return Class.export(Localisation)
