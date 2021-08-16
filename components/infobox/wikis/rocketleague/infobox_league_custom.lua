8a3b078215b0a78399ccd6af716b3dc3fcc361bd
Jasper van Riet <jvanriet@teamliquid.com>
2021-08-16 11:02:01 +0200

    Create maps

--- components/infobox/wikis/rocketleague/infobox_league_custom.lua
+++ components/infobox/wikis/rocketleague/infobox_league_custom.lua
@@ -1,5 +1,6 @@
 local League = require('Module:VoganRL/Infobox/League')
 local Cell = require('Module:Infobox/Cell')
+local String = require('Module:String')
 
 local RLLeague = {}
 
@@ -7,6 +8,7 @@ function RLLeague.run(frame)
     League.addCustomCells = RLLeague.addCustomCells
     League.createTier = RLLeague.createTier
     League.createPrizepool = RLLeague.createPrizepool
+	League.addCustomContent = RLLeague.addCustomContent
     return League:createInfobox(frame)
 end
 
@@ -24,6 +26,41 @@ function RLLeague:createPrizepool(args)
 	return Cell	:new('Prize pool')
 				:options({})
 				:content('0')
+end
+
+function RLLeague:addCustomContent(infobox, args)
+	if String.isEmpty(args.map1) then
+		return infobox
+	end
+
+	infobox:header('Maps', true)
+
+	local maps = {self:_makeInternalLink(args.map1)}
+	local index  = 2
+
+	while not String.isEmpty(args['map' .. index]) do
+		table.insert(maps, '&nbsp;• ' ..
+			self:_createNoWrappingSpan(
+				self:_makeInternalLink(args['map' .. index])
+			)
+		)
+		index = index + 1
+	end
+
+	infobox:centeredCell(unpack(maps))
+
+    return infobox
+end
+
+function RLLeague:_createNoWrappingSpan(content)
+	local span = mw.html.create('span')
+	span:css('white-space', 'nowrap')
+		:node(content)
+	return span
+end
+
+function RLLeague:_makeInternalLink(content)
+	return '[[' .. content .. ']]'
 end
 
 return RLLeague
