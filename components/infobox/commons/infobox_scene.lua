b47b1a6e0ac76a209c7024ca9822d21af18672ce
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2021-08-18 12:31:27 +0200

    Create infobox_scene.lua
    afaik this is only used on sc2

---
+++ components/infobox/commons/infobox_scene.lua
@@ -0,0 +1,97 @@
+local Class = require('Module:Class')
+local Infobox = require('Module:Infobox')
+local Table = require('Module:Table')
+local Variables = require('Module:Variables')
+local Localisation = require('Module:Localisation').getLocalisation
+local Flags = require('Module:Flags')
+local Links = require('Module:Links')
+
+local getArgs = require('Module:Arguments').getArgs
+
+local Scene = Class.new()
+
+function Scene.run(frame)
+    return Scene:createInfobox(frame)
+end
+
+function Scene:createInfobox(frame)
+    local args = getArgs(frame)
+    self.frame = frame
+    self.pagename = mw.title.getCurrentTitle().text
+    self.name = args.country or args.scene or self.pagename
+
+    if args.game == nil then
+        return error('Please provide a game!')
+    end
+
+    local infobox = Infobox:create(frame, args.game)
+    local nameDisplay = Scene:nameDisplay(args)
+
+    infobox :name(nameDisplay)
+            :image(args.image)
+            :centeredCell(args.caption)
+            :header('Scene Information', true)
+            :cell('Region', Scene:createRegion(args))
+            :cell('National team', (args.nationalteam ~= nil) and ('[[' .. args.nationalteam .. ']]') or nil)
+            :cell('Key people', args['key people'])
+            :cell('Events', args.events)
+            :cell('Size', args.size)
+    Scene:addCustomCells(infobox, args)
+
+    local links = Links.transform(args)
+    local achievements = Scene:getAchievements(infobox, args)
+
+    infobox :header('Links', not Table.isEmpty(links))
+            :links(links)
+            :header('Achievements', achievements)
+            :centeredCell(achievements)
+            :centeredCell(args.footnotes)
+    Scene:addCustomContent(infobox, args)
+    infobox:bottom(Scene.createBottomContent(infobox))
+
+    infobox:categories('Scene')
+
+    return infobox:build()
+end
+
+--- Allows for overriding this functionality
+function Scene:nameDisplay(args)
+    local name = args.name
+    local country = Flags._CountryName(args.country or args.scene)
+    if not name then
+        local localised = Localisation(country)
+        local flag = Flags._Flag(country)
+        name = flag .. '&nbsp;' .. localised .. (args.gamenamedisplay or '') .. 'scene'
+    end
+
+    Variables.varDefine('country', country)
+
+    return name
+end
+
+--- Allows for overriding this functionality
+function Scene:addCustomContent(infobox, args)
+    return infobox
+end
+
+--- Allows for overriding this functionality
+function Scene:getAchievements(infobox, args)
+    return args.achievements
+end
+
+--- Allows for overriding this functionality
+function Scene:addCustomCells(infobox, args)
+    return infobox
+end
+
+--- Allows for overriding this functionality
+function Scene:createBottomContent(infobox)
+    return infobox
+end
+
+--- Allows for overriding this functionality
+function Scene:createRegion(args)
+    return args.region
+end
+
+return Scene
