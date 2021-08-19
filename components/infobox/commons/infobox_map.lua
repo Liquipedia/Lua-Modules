9f70f05343140007479001a7d8230c248d523301
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2021-08-19 07:41:06 +0200

    Create infobox_map.lua
    https://liquipedia.net/starcraft2/Module:Hjpalpha/sandbox18
    
    wiki specific (example) still to be added

---
+++ components/infobox/commons/infobox_map.lua
@@ -0,0 +1,52 @@
+local Class = require('Module:Class')
+local Infobox = require('Module:Infobox')
+
+local getArgs = require('Module:Arguments').getArgs
+
+local Map = Class.new()
+
+function Map.run(frame)
+    return Map:createInfobox(frame)
+end
+
+function Map:createInfobox(frame)
+    local args = getArgs(frame)
+    self.frame = frame
+    self.pagename = mw.title.getCurrentTitle().text
+	local name = Map:getName(args)
+    self.name = name or self.pagename
+
+    if args.game == nil then
+        return error('Please provide a game!')
+    end
+
+    local infobox = Infobox:create(frame, args.game)
+
+    infobox :name(name)
+            :image(args.image, args.defaultImage)
+            :centeredCell(args.caption)
+            :header('Map Information', true)
+            :cell('Creator', args.creator or args['vreated-by'])
+    Map:addCustomCells(infobox, args)
+    infobox:bottom(Map.createBottomContent(infobox))
+
+    return infobox:build()
+end
+
+--- Allows for overriding this functionality
+--- for e.g. #external_info:tlpd_map on sc/sc2
+function Map:getName(args)
+    return args.name
+end
+
+--- Allows for overriding this functionality
+function Map:addCustomCells(infobox, args)
+    return infobox
+end
+
+--- Allows for overriding this functionality
+function Map:createBottomContent(infobox)
+    return infobox
+end
+
+return Map
