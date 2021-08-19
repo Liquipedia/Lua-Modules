2ebc99cdbe6bc2983fb23414e77c8fa487212f63
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2021-08-19 17:15:27 +0200

    Update infobox_team.lua

--- components/infobox/commons/infobox_team.lua
+++ components/infobox/commons/infobox_team.lua
@@ -1,6 +1,5 @@
 local Class = require('Module:Class')
 local Cell = require('Module:Infobox/Cell')
-local Infobox = require('Module:Infobox')
 local Template = require('Module:Template')
 local Table = require('Module:Table')
 local Variables = require('Module:Variables')
