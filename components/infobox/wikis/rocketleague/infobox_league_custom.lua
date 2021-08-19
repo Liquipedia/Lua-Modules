a38ca0cd5093e2cec953fec35fc10745a2469c2e
Jasper van Riet <JaspervanRiet@users.noreply.github.com>
2021-08-19 13:24:55 +0200

    Create prizepool calculation for tournaments (#209)
    * Set tournament variables
    
    * Set vars before LPDB
    
    * Build prizepool calculation
    
    * Set var
    
    * Fix typo
    
    * Don't create a new cell

--- components/infobox/wikis/rocketleague/infobox_league_custom.lua
+++ components/infobox/wikis/rocketleague/infobox_league_custom.lua
@@ -1,6 +1,8 @@
 local League = require('Module:Infobox/League')
 local Cell = require('Module:Infobox/Cell')
 local String = require('Module:String')
+local Template = require('Module:Template')
+local Variables = require('Module:Variables')
 
 local RLLeague = {}
 
@@ -23,9 +25,39 @@ function RLLeague:createTier(args)
 end
 
 function RLLeague:createPrizepool(args)
-	return Cell	:new('Prize pool')
-				:options({})
-				:content('0')
+	local cell = Cell:new('Prize pool'):options({})
+	if String.isEmpty(args.prizepool) and
+		String.isEmpty(args.prizepoolusd) then
+			return cell:content()
+	end
+
+	local content
+	local prizepool = args.prizepool
+	local prizepoolInUsd = args.prizepoolusd
+	local localCurrency = args.localcurrency
+
+	if String.isEmpty(prizepool) then
+		content = '$' .. prizepoolInUsd .. ' ' .. Template.safeExpand(mw.getCurrentFrame(), 'Abbr/USD')
+	else
+		if not String.isEmpty(localCurrency) then
+			content = Template.safeExpand(
+				mw.getCurrentFrame(),
+				'Local currency',
+				{localCurrency:lower(), prizepool = prizepool}
+			)
+		else
+			content = prizepool
+		end
+
+		if not String.isEmpty(prizepoolInUsd) then
+			content = content .. '<br>(≃ $' .. prizepoolInUsd .. ' ' ..
+				Template.safeExpand(mw.getCurrentFrame(), 'Abbr/USD') .. ')'
+		end
+	end
+
+	Variables.varDefine('tournament_prizepoolusd', prizepoolInUsd)
+
+	return cell:content(content)
 end
 
 function RLLeague:addCustomContent(infobox, args)
