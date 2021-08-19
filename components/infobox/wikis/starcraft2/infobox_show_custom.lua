421348590eed398b510d6d5d885b9b49e41d07a1
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2021-08-19 10:05:38 +0200

    Create infobox_show_custom.lua

---
+++ components/infobox/wikis/starcraft2/infobox_show_custom.lua
@@ -0,0 +1,26 @@
+local Show = require('Module:Infobox/Show')
+local Namespace = require('Module:Namespace')
+local StarCraft2Show = {}
+
+function StarCraft2Show.run(frame)
+	Show.addCustomCells = StarCraft2Show.addCustomCells
+	return Show:createInfobox(frame)
+end
+
+function StarCraft2Show:addCustomCells(infobox, args)
+	infobox:cell('No. of episodes', args['num_episodes'])
+	infobox:cell('Original Release', StarCraft2Show:_getReleasePeriod(args.sdate, args.edate))
+
+	if Namespace.isMain() and args.edate == nil then
+		infobox:categories('Active Shows')
+	end
+
+	return infobox
+end
+
+function StarCraft2Show:_getReleasePeriod(sdate, edate)
+	if not sdate then return nil end
+	return sdate .. ' - ' .. (edate or '<b>Present</b>')
+end
+
+return StarCraft2Show
