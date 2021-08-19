9e034e67d4d9d856c57ab16c28419740205fc78e
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2021-08-19 13:00:09 +0200

    Update infobox_patch.lua

--- components/infobox/commons/infobox_patch.lua
+++ components/infobox/commons/infobox_patch.lua
@@ -8,82 +8,102 @@ local getArgs = require('Module:Arguments').getArgs
 local Patch = Class.new()
 
 function Patch.run(frame)
-    return Patch:createInfobox(frame)
+	return Patch:createInfobox(frame)
 end
 
 function Patch:createInfobox(frame)
-    local args = getArgs(frame)
-    self.frame = frame
-    self.pagename = mw.title.getCurrentTitle().text
-    self.name = args.name or self.pagename
+	local args = getArgs(frame)
+	self.frame = frame
+	self.pagename = mw.title.getCurrentTitle().text
+	self.name = args.name or self.pagename
 
-    if args.game == nil then
-        return error('Please provide a game!')
-    end
+	if args.game == nil then
+		return error('Please provide a game!')
+	end
 
-    local infobox = Infobox:create(frame, args.game)
+	local infobox = Infobox:create(frame, args.game)
 
-    infobox :name(args.name)
-            :image(args.image, args.defaultImage)
-            :centeredCell(args.caption)
-            :header('Patch Information', true)
-            :cell('Version', args.version)
-            :cell('Release', args.release)
-    Patch:addCustomCells(infobox, args)
+	infobox:name(args.name)
+	infobox:image(args.image, args.defaultImage)
+	infobox:centeredCell(args.caption)
+	infobox:header('Patch Information', true)
+	infobox:cell('Version', args.version)
+	infobox:cell('Release', args.release)
+	Patch:addCustomCells(infobox, args)
 
-    local chronologyData = Patch:getChronologyData(args)
+	local chronologyData = Patch:getChronologyData(args)
 
-    infobox :header('Highlights', args.highlight1)
-            :fcell(Patch:_createHighlightsCell(args))
-            :header('Chronology', not Table.isEmpty(chronologyData))
-            :chronology(chronologyData)
-            :centeredCell(args.footnotes)
-    Patch:addCustomContent(infobox, args)
-    infobox:bottom(Patch.createBottomContent(infobox))
+	infobox:header('Highlights', args.highlight1)
+	infobox:highlights(Patch:_getHighlights(args))
+	infobox:header('Chronology', not Table.isEmpty(chronologyData))
+	infobox:chronology(chronologyData)
+	infobox:centeredCell(args.footnotes)
+	Patch:addCustomContent(infobox, args)
+	infobox:bottom(Patch.createBottomContent(infobox))
 
-    if Namespace.isMain() then
-        infobox:categories('Patches')
-    end
+	if Namespace.isMain() then
+		infobox:categories('Patches')
+	end
 
-    return infobox:build()
+	return infobox:build()
 end
 
 --- Allows for overriding this functionality
 function Patch:addCustomContent(infobox, args)
-    return infobox
+	return infobox
 end
 
 --- Allows for overriding this functionality
 function Patch:addCustomCells(infobox, args)
-    return infobox
+	return infobox
 end
 
 --- Allows for overriding this functionality
 function Patch:createBottomContent(infobox)
-    return infobox
+	return infobox
 end
 
 --- Allows for overriding this functionality
 function Patch:getChronologyData(args)
-    return { previous = args.previous, next = args.next }
+	return { previous = args.previous, next = args.next }
 end
 
-function Patch:_createHighlightsCell(args)
-    local div = mw.html.create('div')
-    local highlights = mw.html.create('ul')
-    if not (args.highlight1 or args.highlight) then
-        return nil
-    else
-        highlights:tag('li'):wikitext(args.highlight1 or args.highlight):done()
-    end
-    for index = 2, 99 do
-        if args['highlight' .. index] then
-            highlights:tag('li'):wikitext(args['highlight' .. index]):done()
-        else
-            break
-        end
-    end
-    return div:node(highlights)
+function Patch:_getHighlights(args)
+	local highlights = {}
+
+	if args.highlight1 or args.highlight then
+		table.insert(highlights, args.highlight1 or args.highlight)
+	end
+
+	for index = 2, 99 do
+		if args['highlight' .. index] then
+			table.insert(highlights, args['highlight' .. index])
+		else
+			break
+		end
+	end
+	return highlights
+end
+
+--extend Infobox class
+function Infobox:highlights(data)
+	if Table.isEmpty(data) then
+        return self
+	end
+
+	local outerDiv = mw.html.create('div')
+	local innerDiv = mw.html.create('div')
+	local highlights = mw.html.create('ul')
+
+	for _, item in ipairs(data) do
+		highlights:tag('li'):wikitext(item):done()
+	end
+
+	outerDiv:node(innerDiv:node(highlights))
+
+	self.content:node(outerDiv)
+
+	return self
 end
 
 return Patch
