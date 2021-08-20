cb4c44b22eae45e0c11238e1aa3ad0c04c950899
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2021-08-20 11:23:15 +0200

    Add infobox show (#216)
    * Create infobox_show.lua
    
    * Create infobox_show_custom.lua
    
    * Update infobox_show.lua
    
    * Update infobox_show.lua
    
    * Update infobox_show.lua
    
    * Update infobox_show_custom.lua
    
    * Update infobox_show.lua
    
    * Update infobox_show.lua
    
    * Update infobox_show.lua
    
    * function for host display (allowing custom links)
    
    * Update infobox_show.lua
    
    * Update infobox_show_custom.lua

---
+++ components/infobox/commons/infobox_show.lua
@@ -0,0 +1,100 @@
+local Class = require('Module:Class')
+local BasicInfobox = require('Module:Infobox/Basic')
+local Namespace = require('Module:Namespace')
+local Cell = require('Module:Infobox/Cell')
+local Links = require('Module:Links')
+local Table = require('Module:Table')
+local Flags = require('Module:Flags')
+
+local Show = Class.new(BasicInfobox)
+
+function Show.run(frame)
+	local show = Show(frame)
+	local args = show.args
+	local infobox = show.infobox
+
+	infobox:name(args.name)
+	infobox:image(args.image, args.defaultImage)
+	infobox:centeredCell(args.caption)
+	infobox:header('Show Information', true)
+	infobox:fcell(Cell:new('Organizer')
+				:options({})
+				:content(
+					unpack(Show:_createHosts(args))
+				)
+				:make()
+			)
+	infobox:cell('Format', args.format)
+	infobox:cell('Airs', args.airs)
+	infobox:fcell(Cell:new('Location'):options({}):content(
+			Show:_createLocation(args.country, args.city),
+			Show:_createLocation(args.country2, args.city2)
+		):make())
+	Show:addCustomCells(infobox, args)
+
+	local links = Links.transform(args)
+	infobox:header('Links', not Table.isEmpty(links))
+	infobox:links(links)
+	infobox:centeredCell(Show:_addSecondaryLinkDisplay(args))
+	Show:addCustomContent(infobox, args)
+	infobox:centeredCell(args.footnotes)
+	infobox:bottom(Show.createBottomContent(infobox))
+
+	if Namespace.isMain() then
+		infobox:categories('Shows')
+	end
+
+	return infobox:build()
+end
+
+function Show:_createHosts(args)
+	local hosts = {}
+	local host1 = args.host or args.host1
+	if host1 then
+		host1 = '[[' ..
+			(args.hostlink or args.host1link or host1)
+			.. '|' .. host1 .. ']]'
+		table.insert(hosts, host1)
+		for index = 2, 99 do
+			if not args['host' .. index] then
+				break
+			else
+				local host = '[[' .. (args['host' .. index .. 'link'] or args['host' .. index])
+				host = host .. '|' .. args['host' .. index] .. ']]'
+				table.insert(hosts, host)
+			end
+		end
+	end
+	return hosts
+end
+
+function Show:_createLocation(country, city)
+	if country == nil or country == '' then
+		return ''
+	end
+
+	local countryDisplay = Flags._CountryName(country)
+
+	return Flags._Flag(country) .. '&nbsp;' ..
+		'[[:Category:' .. countryDisplay .. '|' .. (city or countryDisplay) .. ']]'
+end
+
+function Show:_addSecondaryLinkDisplay(args)
+	local secondaryLinks = {}
+	if args.topicid then
+		secondaryLinks[#secondaryLinks + 1] = '[https://tl.net/forum/viewmessage.php?topic_id='
+			.. args.topicid .. ' TL Thread]'
+	end
+	if args.itunes then
+		secondaryLinks[#secondaryLinks + 1] = '['
+			.. args.itunes .. ' iTunes] [[Category:Podcasts]]'
+	end
+	if args.videoarchive then
+		secondaryLinks[#secondaryLinks + 1] = '['
+			.. args.videoarchive .. ' Video Archive]'
+	end
+
+	return table.concat(secondaryLinks, '&nbsp;•&nbsp;')
+end
+
+return Show
