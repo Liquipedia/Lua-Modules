8fa52d1c60da2a546f536e77403feed7ff09c156
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2021-08-20 11:11:55 +0200

    Add infobox patch implementation (#208)
    * Create infobox_patch.lua
    
    * Update infobox_patch.lua
    
    * Create infobox_patch_custom.lua
    
    * Update infobox_patch.lua
    
    * Update infobox_patch_custom.lua
    
    * Update infobox_patch_custom.lua
    
    * Update infobox_patch.lua
    
    * Update infobox_patch_custom.lua
    
    * Update infobox_patch.lua
    
    * Update infobox_patch.lua
    
    * Update infobox_patch_custom.lua
    
    * Update infobox_patch.lua
    
    * Update components/infobox/wikis/starcraft2/infobox_patch_custom.lua
    
    Co-authored-by: Jasper van Riet <JaspervanRiet@users.noreply.github.com>
    
    Co-authored-by: Jasper van Riet <JaspervanRiet@users.noreply.github.com>

---
+++ components/infobox/commons/infobox_patch.lua
@@ -0,0 +1,85 @@
+local BasicInfobox = require('Module:Infobox/Basic')
+local Class = require('Module:Class')
+local Table = require('Module:Table')
+local Namespace = require('Module:Namespace')
+
+local Patch = Class.new(BasicInfobox)
+
+function Patch.run(frame)
+	local patch = Patch(frame)
+	return patch:createInfobox()
+end
+
+function Patch:createInfobox()
+	local infobox = self.infobox
+	local args = self.args
+	infobox.highlights = Patch._highlights
+
+	infobox:name(args.name)
+	infobox:image(args.image, args.defaultImage)
+	infobox:centeredCell(args.caption)
+	infobox:header('Patch Information', true)
+	infobox:cell('Version', args.version)
+	infobox:cell('Release', args.release)
+	Patch:addCustomCells(infobox, args)
+
+	local chronologyData = Patch:getChronologyData(args)
+
+	infobox:header('Highlights', args.highlight1)
+	infobox:highlights(Patch:_getHighlights(args))
+	infobox:header('Chronology', not Table.isEmpty(chronologyData))
+	infobox:chronology(chronologyData)
+	infobox:centeredCell(args.footnotes)
+	Patch:addCustomContent(infobox, args)
+	infobox:bottom(Patch.createBottomContent(infobox))
+
+	if Namespace.isMain() then
+		infobox:categories('Patches')
+	end
+
+	return infobox:build()
+end
+
+--- Allows for overriding this functionality
+function Patch:getChronologyData(args)
+	return { previous = args.previous, next = args.next }
+end
+
+function Patch:_getHighlights(args)
+	local highlights = {}
+
+	if args.highlight1 or args.highlight then
+		table.insert(highlights, args.highlight1 or args.highlight)
+	end
+
+	for index = 2, 99 do
+		if args['highlight' .. index] then
+			table.insert(highlights, args['highlight' .. index])
+		else
+			break
+		end
+	end
+	return highlights
+end
+
+--extend Infobox class
+function Patch:_highlights(data)
+	if Table.isEmpty(data) then
+		return self
+	end
+
+	local div = mw.html.create('div')
+	local highlights = mw.html.create('ul')
+
+	for _, item in ipairs(data) do
+		highlights:tag('li'):wikitext(item):done()
+	end
+
+	div:node(highlights)
+
+	self.content:node(div)
+
+	return self
+end
+
+return Patch
---
+++ components/infobox/wikis/starcraft2/infobox_patch_custom.lua
@@ -0,0 +1,49 @@
+local Patch = require('Module:Infobox/Patch')
+
+local StarCraft2Patch = {}
+
+function StarCraft2Patch.run(frame)
+	local patch = Patch(frame)
+	Patch.addCustomCells = StarCraft2Patch.addCustomCells
+	Patch.getChronologyData = StarCraft2Patch.getChronologyData
+	return patch:createInfobox()
+end
+
+--- Allows for overriding this functionality
+function StarCraft2Patch:addCustomCells(infobox, args)
+	infobox:cell('SEA Release Date', args.searelease)
+	infobox:cell('NA Release Date', args.narelease)
+	infobox:cell('EU Release Date', args.eurelease)
+	infobox:cell('KR Release Date', args.korrelease)
+	return infobox
+end
+
+--- Allows for overriding this functionality
+function StarCraft2Patch:getChronologyData(args)
+	local data = {}
+	if args.previous == nil and args.next == nil then
+		if args.previoushbu then
+			data.previous = 'Balance Update ' .. args.previoushbu .. '|#' .. args.previoushbu
+		end
+		if args.nexthbu then
+			data.next = 'Balance Update ' .. args.nexthbu .. '|#' .. args.nexthbu
+		end
+	else
+		if args.previous then
+			data.previous = 'Patch ' .. args.previous .. '|' .. args.previous
+		end
+		if args.next then
+			data.next = 'Patch ' .. args.previous .. '|' .. args.next
+		end
+		if args.previoushbu then
+			data.previous2 = 'Balance Update ' .. args.previoushbu .. '|#' .. args.previoushbu
+		end
+		if args.nexthbu then
+			data.next2 = 'Balance Update ' .. args.nexthbu .. '|#' .. args.nexthbu
+		end
+	end
+
+	return data
+end
+
+return StarCraft2Patch
