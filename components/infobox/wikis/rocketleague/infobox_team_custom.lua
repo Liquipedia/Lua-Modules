4a5535116bf4e493a0e14708ea1b3ddb7425e2ae
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2021-08-20 12:46:35 +0200

    switch to use infobox/basic && copy from live

--- components/infobox/wikis/rocketleague/infobox_team_custom.lua
+++ components/infobox/wikis/rocketleague/infobox_team_custom.lua
@@ -1,18 +1,26 @@
 local Team = require('Module:Infobox/Team')
 local Earnings = require('Module:Earnings')
 local Variables = require('Module:Variables')
+local TeamMatches = require('Module:Matches/Team')
+
+local TeamRanking = require('Module:TeamRanking')
 
 local RocketLeagueTeam = {}
 
 function RocketLeagueTeam.run(frame)
+    local team = Team(frame)
     Team.addCustomCells = RocketLeagueTeam.addCustomCells
     Team.calculateEarnings = RocketLeagueTeam.calculateEarnings
-    return Team:createInfobox(frame)
+    return team:createInfobox(frame)
 end
 
 function RocketLeagueTeam.addCustomCells(team, infobox, args)
     Variables.varDefine('rating', args.rating)
+    local teamName = args.rankingname or team.pagename or team.name
+    local rlcsRanking = TeamRanking.get({ranking = 'RLCS_X_Ranking', team = teamName})
+
     infobox :cell('[[Portal:Rating|LPRating]]', args.rating or 'Not enough data')
+            --:cell('[[RankingTableRLCS|RLCS Points]]', rlcsRanking)
     return infobox
 
 end
