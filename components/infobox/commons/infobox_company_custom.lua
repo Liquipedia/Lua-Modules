4476a46ab1d775174c07e61882771487ccd6d926
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2021-08-20 13:51:26 +0200

    Update infobox_company_custom.lua

--- components/infobox/commons/infobox_company_custom.lua
+++ components/infobox/commons/infobox_company_custom.lua
@@ -3,12 +3,9 @@ local Company = require('Module:Infobox/Company')
 local CustomCompany = {}
 
 function CustomCompany.run(frame)
+    local company = Company(frame)
     Company.addCustomCells = CustomCompany.addCustomCells
-    return Company:createInfobox(frame)
-end
-
-function CustomCompany.addCustomCells(company, infobox, args)
-    return infobox
+    return company:createInfobox(frame)
 end
 
 return CustomCompany
