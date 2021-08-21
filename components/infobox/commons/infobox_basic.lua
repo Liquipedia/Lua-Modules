03d33aea086ba965c113076b02ef5632f73ee627
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2021-08-21 19:36:13 +0200

    add 2 functions to `Module:Infobox/Basic`
    add 2 functions to `Module:Infobox/Basic`

--- components/infobox/commons/infobox_basic.lua
+++ components/infobox/commons/infobox_basic.lua
@@ -31,4 +31,49 @@ function BasicInfobox:createBottomContent(infobox)
     return nil
 end
 
+--- Allows for using this for customCells
+function Skill:getMultiArgsForType(args, argType)
+	local typeArgs = {}
+	if String.isEmpty(args[argType]) then
+		return typeArgs
+	end
+
+	local argType1 = (args[argType .. 'link'] or args[argType])
+		.. '|' .. args[argType]
+
+	table.insert(typeArgs, '[[' .. argType1 .. ']]')
+
+	for index = 2, _LARGE_NUMBER do
+		if String.isEmpty(args[argType .. index]) then
+			break
+		else
+			local indexedArgType = (args[argType .. index .. 'link'] or args[argType .. index])
+				.. '|' .. args[argType .. index]
+			table.insert(typeArgs, '[[' .. indexedArgType .. ']]')
+		end
+	end
+
+	return typeArgs
+end
+
+--- Allows for using this for customCells
+function Skill:getMultiArgsForTypeNoLink(args, argType)
+	local typeArgs = {}
+	if String.isEmpty(args[argType]) then
+		return typeArgs
+	end
+
+	table.insert(typeArgs, args[argType])
+
+	for index = 2, _LARGE_NUMBER do
+		if String.isEmpty(args[argType .. index]) then
+			break
+		else
+			table.insert(typeArgs, args[argType .. index])
+		end
+	end
+
+	return typeArgs
+end
+
 return BasicInfobox
