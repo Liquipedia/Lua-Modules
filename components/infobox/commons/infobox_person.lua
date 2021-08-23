836e7725ae4742afebbf2cd9974f07061899482f
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2021-08-23 17:58:24 +0200

    Update infobox_person.lua

--- components/infobox/commons/infobox_person.lua
+++ components/infobox/commons/infobox_person.lua
@@ -1,3 +1,11 @@
+---
+-- @Liquipedia
+-- wiki=commons
+-- page=Module:Infobox/Person
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
 local Class = require('Module:Class')
 local Cell = require('Module:Infobox/Cell')
 local BasicInfobox = require('Module:Infobox/Basic')
