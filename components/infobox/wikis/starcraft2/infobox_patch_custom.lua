35b4cbac1800c689f6ba6851616a045bc5fecd6c
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2021-08-23 17:22:23 +0200

    Update infobox_patch_custom.lua

--- components/infobox/wikis/starcraft2/infobox_patch_custom.lua
+++ components/infobox/wikis/starcraft2/infobox_patch_custom.lua
@@ -3,6 +3,8 @@
 -- wiki=starcraft2
 -- page=Module:Infobox/Patch/Custom
 --
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
 
 local Patch = require('Module:Infobox/Patch')
 
