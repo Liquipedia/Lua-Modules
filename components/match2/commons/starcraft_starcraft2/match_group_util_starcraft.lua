7ad2b98eec1347736aa0431d83346bd66059fca9
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2021-08-23 17:23:11 +0200

    Update match_group_util_starcraft.lua

--- components/match2/commons/starcraft_starcraft2/match_group_util_starcraft.lua
+++ components/match2/commons/starcraft_starcraft2/match_group_util_starcraft.lua
@@ -3,6 +3,8 @@
 -- wiki=commons
 -- page=Module:MatchGroup/Util/Starcraft
 --
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
 
 local Array = require('Module:Array')
 local Logic = require('Module:Logic')
