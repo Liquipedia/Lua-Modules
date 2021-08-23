d53888f3aef0c930308e2d6f23fd0c1a25218814
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2021-08-23 17:01:49 +0200

    Update get_match_group_copy_paste_wiki.lua

--- components/match2/commons/get_match_group_copy_paste_wiki.lua
+++ components/match2/commons/get_match_group_copy_paste_wiki.lua
@@ -1,3 +1,11 @@
+---
+-- @Liquipedia
+-- wiki=starcraft2
+-- page=Module:GetMatchGroupCopyPaste/wiki
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
 local p = require('Module:GetMatchGroupCopyPaste/wiki/Base')
 
 --overwrite stuff you want to overwrite here^^
