e820856f95439ab2bdfb320757d0201b30433950
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2021-08-23 17:44:24 +0200

    Update match_group_legacy_default.lua

--- components/match2/wikis/starcraft2/match_group_legacy_default.lua
+++ components/match2/wikis/starcraft2/match_group_legacy_default.lua
@@ -1,3 +1,11 @@
+---
+-- @Liquipedia
+-- wiki=starcraft2
+-- page=Module:MatchGroup/Legacy/Default
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
 local p = {}
 
 local Lua = require("Module:Lua")
