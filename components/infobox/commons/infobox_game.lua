8f0c938e94d746a3861b14fed8001b08224c9f30
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2021-08-23 17:26:44 +0200

    add `Module:Infobox/Game` (#250)
    * add `Module:Infobox/Game`
    
    * Update infobox_game.lua
    
    * Update infobox_game.lua
    
    * Update infobox_game.lua
    
    * Update infobox_game.lua
    
    * Update infobox_game.lua
    
    * Update infobox_game.lua
    
    * Update infobox_game.lua
    
    * Update infobox_game.lua

---
+++ components/infobox/commons/infobox_game.lua
@@ -0,0 +1,37 @@
+local BasicInfobox = require('Module:Infobox/Basic')
+local Class = require('Module:Class')
+local Cell = require('Module:Infobox/Cell')
+local Namespace = require('Module:Namespace')
+
+local Game = Class.new(BasicInfobox)
+
+function Game.run(frame)
+	local game = Game(frame)
+	return game:createInfobox(frame)
+end
+
+function Game:createInfobox(frame)
+	local infobox = self.infobox
+	local args = self.args
+
+	infobox:name(args.name)
+	infobox:image(args.image, args.defaultImage)
+	infobox:centeredCell(args.caption)
+	infobox:header('Game Information', true)
+	infobox:fcell(Cell:new('Developer'):options({}):content(
+		unpack(self:getAllArgsForBase(args, 'developer'))):make())
+	infobox:fcell(Cell:new('Release Dates'):options({}):content(
+		unpack(self:getAllArgsForBase(args, 'releasedate'))):make())
+	infobox:fcell(Cell:new('Platforms'):options({}):content(
+		unpack(self:getAllArgsForBase(args, 'platform'))):make())
+	self:addCustomCells(infobox, args)
+	infobox:bottom(self:createBottomContent(infobox))
+
+	if Namespace.isMain() then
+		infobox:categories('Games')
+	end
+
+	return infobox:build()
+end
+
+return Game
