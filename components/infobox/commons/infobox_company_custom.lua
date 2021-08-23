9a11e9889a4f6d779fbd3f632e9ada66c342d9a2
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2021-08-23 17:20:57 +0200

    Update infobox_company_custom.lua

--- components/infobox/commons/infobox_company_custom.lua
+++ components/infobox/commons/infobox_company_custom.lua
@@ -3,6 +3,8 @@
 -- wiki=commons
 -- page=Module:Infobox/Company/Custom
 --
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
 
 local Company = require('Module:Infobox/Company')
 
