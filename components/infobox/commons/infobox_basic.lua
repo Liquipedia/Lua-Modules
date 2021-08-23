c4b568c5f26e9677aad68ff8742574ed3d0312ad
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2021-08-23 11:54:54 +0200

    Add function `getAllArgsForBase` to `Module:Infobox/Basic` (#252)
    * add 2 functions to `Module:Infobox/Basic`
    
    add 2 functions to `Module:Infobox/Basic`
    
    * Update infobox_basic.lua
    
    * Update infobox_basic.lua
    
    * Update infobox_basic.lua
    
    * Update infobox_basic.lua
    
    * Update infobox_basic.lua

--- components/infobox/commons/infobox_basic.lua
+++ components/infobox/commons/infobox_basic.lua
@@ -1,5 +1,7 @@
 local Class = require('Module:Class')
 local Infobox = require('Module:Infobox')
+local String = require('Module:String')
+local Logic = require('Module:Logic')
 local getArgs = require('Module:Arguments').getArgs
 
 local BasicInfobox = Class.new(
@@ -29,6 +31,37 @@ end
 --- Allows for overriding this functionality
 function BasicInfobox:createBottomContent(infobox)
     return nil
+end
+
+--- Allows for using this for customCells
+function BasicInfobox:getAllArgsForBase(args, base, options)
+	local foundArgs = {}
+	if String.isEmpty(args[base]) then
+		return foundArgs
+	end
+
+	local makeLink = Logic.readBool(options.makeLink)
+
+	local baseArg = args[base]
+	if makeLink then
+		baseArg = '[[' .. (args[base .. 'link'] or baseArg)
+			.. '|' .. baseArg .. ']]'
+	end
+
+	table.insert(foundArgs, baseArg)
+	local index = 2
+
+	while not String.isEmpty(args[base .. index]) do
+		local indexedbase = args[base .. index]
+		if makeLink then
+			indexedbase = '[[' .. (args[base .. index .. 'link'] or indexedbase)
+				.. '|' .. indexedbase .. ']]'
+		end
+		table.insert(foundArgs, indexedbase)
+		index = index + 1
+	end
+
+	return foundArgs
 end
 
 return BasicInfobox
