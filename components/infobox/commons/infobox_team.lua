8506169fe0d14103917d3c62b0ad494eada77deb
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2021-08-23 16:36:22 +0200

    Update infobox_team.lua

--- components/infobox/commons/infobox_team.lua
+++ components/infobox/commons/infobox_team.lua
@@ -1,3 +1,9 @@
+---
+-- @Liquipedia
+-- wiki=commons
+-- page=Module:Infobox/Team
+--
+
 local Class = require('Module:Class')
 local Cell = require('Module:Infobox/Cell')
 local Template = require('Module:Template')
