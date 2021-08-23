4b2fb70f186677cdd8d31e82061a7ded7fd3d596
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2021-08-23 17:21:07 +0200

    Update infobox_league.lua

--- components/infobox/commons/infobox_league.lua
+++ components/infobox/commons/infobox_league.lua
@@ -3,6 +3,8 @@
 -- wiki=commons
 -- page=Module:Infobox/League
 --
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
 
 local BasicInfobox = require('Module:Infobox/Basic')
 local Cell = require('Module:Infobox/Cell')
