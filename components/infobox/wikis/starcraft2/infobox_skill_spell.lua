da338e535885e6f3633a6ce6ac9f2537c9db6a9d
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2021-08-23 16:25:23 +0200

    Update infobox_skill_spell.lua

--- components/infobox/wikis/starcraft2/infobox_skill_spell.lua
+++ components/infobox/wikis/starcraft2/infobox_skill_spell.lua
@@ -1,3 +1,7 @@
+-- This file is for
+-- wiki=starcraft2
+-- page=Module:Infobox/Skill/Spell
+
 local Spell = require('Module:Infobox/Skill')
 local Hotkeys = require('Module:Hotkey')
 local CleanRace = require('Module:CleanRace2')
