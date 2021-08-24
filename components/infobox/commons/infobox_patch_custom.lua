afce171904d3d470d1837d0123a57a1b4b3d3ada
Jasper van Riet <JaspervanRiet@users.noreply.github.com>
2021-08-24 16:51:16 +0200

    Create Infobox/Patch/Custom (#275)
    * Create Patch/Custom
    
    * Remove unneeded code
    
    * Set to commons

---
+++ components/infobox/commons/infobox_patch_custom.lua
@@ -0,0 +1,18 @@
+---
+-- @Liquipedia
+-- wiki=commons
+-- page=Module:Infobox/Patch/Custom
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Patch = require('Module:Infobox/Patch')
+
+local CustomPatch = {}
+
+function CustomPatch.run(frame)
+	local patch = Patch(frame)
+	return patch:createInfobox()
+end
+
+return CustomPatch
