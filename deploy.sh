d17544b862438fc8e45402d1f910ad6931807551
Alex Winkler <FO-nTTaX@users.noreply.github.com>
2021-08-24 20:38:14 +0200

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+wikiBaseUrl='https://liquipedia.net/'
+luaFiles=$(find . -type f -name '*.lua')
+pat='\-\-\-\
+\-\- @Liquipedia\
+\-\- wiki=([^
+]*)\
+\-\- page=([^
+]*)\
+'
+
+for luaFile in $luaFiles
+do
+  echo "== Checking $luaFile =="
+  fileContents=$(cat $luaFile)
+
+  [[ $fileContents =~ $pat ]]
+
+  if [[ "${BASH_REMATCH[1]}" == "" ]]
+  then
+    echo '...skipping - no magic comment found'
+  else
+    echo '...magic comment found - updating wiki...'
+    wiki="${BASH_REMATCH[1]}"
+    page="${BASH_REMATCH[2]}"
+    echo "...wiki = $wiki"
+    echo "...page = $page"
+    wikiApiUrl="${wikiBaseUrl}${wiki}/api.php"
+
+    # Login
+    loginToken=$(curl -s -b "cookie_${wiki}.ck" -c "cookie_${wiki}.ck" -d "format=json&action=query&meta=tokens&type=login" -H 'User-Agent: GitHub Autodeploy Bot/1.0.0 (fonttax@liquipedia.net)' -H 'Accept-Encoding: gzip' -X POST $wikiApiUrl | gunzip | jq .query.tokens.logintoken -r)
+    curl -s -b "cookie_${wiki}.ck" -c "cookie_${wiki}.ck" --data-urlencode "username=${LP_USER}" --data-urlencode "password=${LP_PASSWORD}" --data-urlencode "logintoken=${loginToken}" --data-urlencode "loginreturnurl=https://liquipedia.net" -H 'User-Agent: GitHub Autodeploy Bot/1.0.0 (fonttax@liquipedia.net)' -H 'Accept-Encoding: gzip' -X POST "${wikiApiUrl}?format=json&action=clientlogin" | gunzip > /dev/null
+
+    # Edit page
+    editToken=$(curl -s -b "cookie_${wiki}.ck" -c "cookie_${wiki}.ck" -d "format=json&action=query&meta=tokens" -H 'User-Agent: GitHub Autodeploy Bot/1.0.0 (fonttax@liquipedia.net)' -H 'Accept-Encoding: gzip' -X POST $wikiApiUrl | gunzip | jq .query.tokens.csrftoken -r)
+    curl -s -b "cookie_${wiki}.ck" -c "cookie_${wiki}.ck" --data-urlencode "title=${page}" --data-urlencode "text=${fileContents}" --data-urlencode "summary=Auto update from git - file \"${luaFile}\"" --data-urlencode "bot=true" --data-urlencode "recreate=true" --data-urlencode "token=${editToken}" -H 'User-Agent: GitHub Autodeploy Bot/1.0.0 (fonttax@liquipedia.net)' -H 'Accept-Encoding: gzip' -X POST "${wikiApiUrl}?format=json&action=edit" | gunzip > /dev/null
+
+    echo '...done'
+    rm cookie_*
+    # Don't get rate limited
+    sleep 5
+  fi
+done
