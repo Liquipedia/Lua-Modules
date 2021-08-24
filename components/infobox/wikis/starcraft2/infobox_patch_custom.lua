a0bc3c41ffa444928d796469446f7eae27227f48
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2021-08-24 17:22:07 +0200

    Add LPDB storage
    Add LPDB storage

--- components/infobox/wikis/starcraft2/infobox_patch_custom.lua
+++ components/infobox/wikis/starcraft2/infobox_patch_custom.lua
@@ -7,6 +7,7 @@
 --
 
 local Patch = require('Module:Infobox/Patch')
+local Namespace = require('Module:Namespace')
 
 local StarCraft2Patch = {}
 
@@ -23,7 +24,21 @@ function StarCraft2Patch:addCustomCells(infobox, args)
 	infobox:cell('NA Release Date', args.narelease)
 	infobox:cell('EU Release Date', args.eurelease)
 	infobox:cell('KR Release Date', args.korrelease)
+	if Namespace.isMain then
+		StarCraft2Patch:_storeToDatapoint(args)
+	end
 	return infobox
+end
+
+function StarCraft2Patch:_storeToDatapoint(args)
+	local date = args.narelease or args.eurelease
+	local monthAndDay = mw.getContentLanguage():formatDate('m-d', date)
+	mw.ext.LiquipediaDB.lpdb_datapoint('patch_' .. (args.name or mw.title.getCurrentTitle().text), {
+		name = args.name,
+		type = 'patch',
+		information = monthAndDay,
+		date = date,
+	})
 end
 
 --- Allows for overriding this functionality
