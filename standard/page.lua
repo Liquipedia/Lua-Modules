0efa2a0980c6bad0d488935e49458031207a68f0
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2021-08-30 11:56:49 +0200

    Add `Module:Page`
    Add `Module:Page` as suggested in #286 
    * function that can check for the existence of a page
    * function that can make internal links
    * function that can make external links
    * function that can make internal links iff the page it wants to link to exists

---
+++ standard/page.lua
@@ -0,0 +1,61 @@
+---
+-- @Liquipedia
+-- wiki=commons
+-- page=Module:Page
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Class = require('Module:Class')
+local String = require('Module:StringUtils')
+
+local page = {}
+
+function page.exists(page)
+	local existingPage = mw.title.new(page)
+
+	-- In some cases we might have gotten an external link,
+	-- which will mean `existingPage` will equal nil
+	if existingPage == nil then
+		return false
+	end
+
+	return existingPage.exists
+end
+
+function page.makeInternalLink(link, display)
+	if String.isEmpty(link) then
+		return nil
+	elseif String.isEmpty(display) then
+		display = link
+	end
+
+	return '[[' .. link .. '|' .. display .. ']]'
+end
+
+function page.makeInternalLinkIfExists(link, display)
+	if String.isEmpty(link) then
+		return nil
+	elseif String.isEmpty(display) then
+		display = link
+	end
+	if not page.exists(link) then
+		return nil
+	end
+
+	return '[[' .. link .. '|' .. display .. ']]'
+end
+
+function page.makeExternalLink(link, display)
+	if String.isEmpty(link) then
+		return nil
+	end
+	local output = '[' .. link
+	if not String.isEmpty(display) then
+		output = output .. ' ' .. display
+	end
+
+	return output .. ']'
+end
+
+return Class.export(page)
