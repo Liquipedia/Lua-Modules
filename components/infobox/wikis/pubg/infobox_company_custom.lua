7d11a5fb9c041b75ae6a4a3c500d4500bdbb2091
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2021-08-31 14:16:54 +0200

    Add `Module:Infobox/Company/Custom`
    Add `Module:Infobox/Company/Custom`

---
+++ components/infobox/wikis/pubg/infobox_company_custom.lua
@@ -0,0 +1,43 @@
+---
+-- @Liquipedia
+-- wiki=rocketleague
+-- page=Module:Infobox/Company/Custom
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Company = require('Module:Infobox/Company')
+local Cell = require('Module:Infobox/Cell')
+
+local CustomCompany = {}
+
+function CustomCompany.run(frame)
+    local company = Company(frame)
+    company.addCustomCells = CustomCompany.addCustomCells
+    return company:createInfobox(frame)
+end
+
+function CustomCompany:addCustomCells(infobox, args)
+    infobox:fcell(Cell:new(CustomCompany._createSisterCompaniesDescription(args))
+					:content(
+						unpack(self:getAllArgsForBase(args, 'sister', {}))
+					)
+					:make()
+				)
+  
+    return infobox
+end
+
+function CustomCompany._createSisterCompanies(infobox, args)
+    infobox:cell('Epic Creator Code', args.creatorcode)
+    return infobox
+end
+
+function CustomCompany._createSisterCompaniesDescription(args)
+    if args.sister2 then
+      return 'Sister Companies'
+    end
+    return 'Sister Company'
+end
+
+return CustomCompany
