c77f4f542a5d8117da443de1fab8cd56992f7fd7
Jasper van Riet <jvanriet@teamliquid.com>
2021-08-31 16:01:07 +0200

    Create Center

---
+++ components/infobox/commons/infobox_widget_center.lua
@@ -0,0 +1,35 @@
+---
+-- @Liquipedia
+-- wiki=commons
+-- page=Module:Infobox/Widget/Center
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Class = require('Module:Class')
+local Widget = require('Module:Infobox/Widget')
+
+local Center = Class.new(
+	Widget,
+	function(self, ...)
+		self.content = {...}
+	end
+)
+
+function Center:make()
+	return {
+		Center:_create(self.content)
+	}
+end
+
+function Center:_create(content)
+	local centered = mw.html.create('div'):addClass('infobox-center')
+
+	for _, item in pairs(content) do
+		centered:wikitext(item)
+	end
+
+	return mw.html.create('div'):node(centered)
+end
+
+return Center
