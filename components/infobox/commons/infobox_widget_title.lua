13b738ab08e0aab263454f9c7318a80a3efce0a4
Jasper van Riet <jvanriet@teamliquid.com>
2021-08-31 15:22:25 +0200

    Create title

---
+++ components/infobox/commons/infobox_widget_title.lua
@@ -0,0 +1,35 @@
+---
+-- @Liquipedia
+-- wiki=commons
+-- page=Module:Infobox/Widget/Title
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Class = require('Module:Class')
+local Widget = require('Module:Infobox/Widget')
+
+local Title = Class.new(
+	Widget,
+	function(self, input)
+		self.content = self:assertExistsAndCopy(input)
+	end
+)
+
+function Title:make()
+	return {
+		Title:_create(self.content)
+	}
+end
+
+function Title:_create(infoDescription)
+    local header = mw.html.create('div')
+    header  :addClass('infobox-header')
+            :addClass('wiki-backgroundcolor-light')
+            :addClass('infobox-header-2')
+            :wikitext(infoDescription)
+    return mw.html.create('div'):node(header)
+end
+
+return Title
+
