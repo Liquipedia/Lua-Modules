5a87110b323f7f5663b061cec2a54f07f383de73
Jasper van Riet <jvanriet@teamliquid.com>
2021-09-01 13:52:37 +0200

    Create Cell widget

---
+++ components/infobox/commons/infobox_widget_cell.lua
@@ -0,0 +1,81 @@
+---
+-- @Liquipedia
+-- wiki=commons
+-- page=Module:Infobox/Widget/Cell
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Class = require('Module:Class')
+local Widget = require('Module:Infobox/Widget')
+
+local Cell = Class.new(Widget,
+	function(self, input)
+		self.name = self:assertExistsAndCopy(input.name)
+		self.content = input.content
+		self.options = input.options
+		self.classes = input.classes
+	end
+)
+
+function Cell:_new(description)
+	self.root = mw.html.create('div')
+	self.description = mw.html.create('div')
+	self.description:addClass('infobox-cell-2')
+					:addClass('infobox-description')
+					:wikitext(description .. ':')
+	self.contentDiv = nil
+	return self
+end
+
+function Cell:_class(...)
+	for i = 1, select('#', ...) do
+		local item = select(i, ...)
+		if item == nil then
+			break
+		end
+
+		self.root:addClass(item)
+	end
+	return self
+end
+
+function Cell:_content(...)
+	self.contentText = ...
+	local firstItem = select(1, ...)
+	if firstItem == nil or firstItem == '' then
+		self.contentDiv = nil
+		return self
+	end
+
+	self.contentDiv = mw.html.create('div')
+	self.contentDiv:addClass('infobox-cell-2')
+	for i = 1, select('#', ...) do
+		if i > 1 then
+			self.contentDiv:wikitext('<br/>')
+		end
+		local item = select(i, ...)
+		if item == nil then
+			break
+		end
+
+		if self.options ~= nil and self.options.makeLink == true then
+			self.contentDiv:wikitext('[[' .. item .. ']]')
+		else
+			self.contentDiv:wikitext(item)
+		end
+	end
+	return self
+end
+
+function Cell:make()
+	self:_new(self.name)
+	self:_class(unpack(self.classes))
+	self:_content(unpack(self.content))
+
+	self.root   :node(self.description)
+				:node(self.contentDiv)
+	return self.root
+end
+
+return Cell
