2b6eb8dd43f5019ab37343c083ebfa578cbecac4
Jasper van Riet <JaspervanRiet@users.noreply.github.com>
2021-09-01 15:15:04 +0200

    Provide all widgets from one export file (#316)

---
+++ components/infobox/commons/infobox_widgets.lua
@@ -0,0 +1,19 @@
+---
+-- @Liquipedia
+-- wiki=commons
+-- page=Module:Infobox/Widget/All
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Widgets = {}
+
+Widgets.Cell = require('Module:Infobox/Widget/Cell')
+Widgets.Header = require('Module:Infobox/Widget/Header')
+Widgets.Center = require('Module:Infobox/Widget/Center')
+Widgets.Title = require('Module:Infobox/Widget/Title')
+Widgets.Customizable = require('Module:Infobox/Widget/Customizable')
+Widgets.Links = require('Module:Infobox/Widget/Links')
+Widgets.Chronology = require('Module:Infobox/Widget/Chronology')
+
+return Widgets
