3de21e3144927c06b538964f448fc7b9cca7d773
Jasper van Riet <JaspervanRiet@users.noreply.github.com>
2021-09-01 12:11:05 +0200

    Create Customizable widget (#308)
    * Create Customizable
    
    * Don't crash on injector being nil
    
    * Allow setting injector in Infobox
    
    * Fix whitespace

---
+++ components/infobox/commons/infobox_widget_customizable.lua
@@ -0,0 +1,35 @@
+---
+-- @Liquipedia
+-- wiki=commons
+-- page=Module:Infobox/Widget/Customizable
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Class = require('Module:Class')
+local Widget = require('Module:Infobox/Widget')
+local Injector = require('Module:Infobox/Widget/Injector')
+
+local Customizable = Class.new(
+	Widget,
+	function(self, id, widgets)
+		self.id = self:assertExistsAndCopy(id)
+		self.widgets = widgets
+	end
+)
+
+function Customizable:setWidgetInjector(injector)
+	if injector:is_a(Injector) == false then
+		return error('Valid Injector from Infobox/Widget/Injector needs to be provided')
+	end
+	self.injector = injector
+end
+
+function Customizable:make()
+	if self.injector == nil then
+		return self.widgets
+	end
+	return self.injector:parse(self.id, self.widgets)
+end
+
+return Customizable
---
+++ components/infobox/commons/infobox_widget_injector.lua
@@ -0,0 +1,18 @@
+---
+-- @Liquipedia
+-- wiki=commons
+-- page=Module:Infobox/Widget/Injector
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Class = require('Module:Class')
+
+local Injector = Class.new()
+
+function Injector:parse(id, widgets)
+	return error('You need to implement the parse function in your widget injector!')
+end
+
+return Injector
+
