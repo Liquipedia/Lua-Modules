979fae94f73bb507df036ccfd10c0085c0812af3
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2021-09-02 14:06:33 +0200

    MIgrate Infobox Skill
    Migrates Infobox/Scene over to table based layout, see #304 . Merges into #321

--- components/infobox/commons/infobox_skill.lua
+++ components/infobox/commons/infobox_skill.lua
@@ -6,67 +6,75 @@
 -- Please see https://github.com/Liquipedia/Lua-Modules to contribute
 --
 
-local BasicInfobox = require('Module:Infobox/Basic')
 local Class = require('Module:Class')
-local Cell = require('Module:Infobox/Cell')
+local InfoboxBasic = require('Module:Infobox/Basic/dev')
+local String = require('Module:String')
 local Namespace = require('Module:Namespace')
-local String = require('Module:StringUtils')
 local Hotkey = require('Module:Hotkey')
+local PageLink = require('Module:Page')
 
-local Skill = Class.new(BasicInfobox)
+local Widgets = require('Module:Infobox/Widget/All')
+local Cell = Widgets.Cell
+local Header = Widgets.Header
+local Title = Widgets.Title
+local Center = Widgets.Center
+local Customizable = Widgets.Customizable
+
+local Skill = Class.new(InfoboxBasic)
 
 function Skill.run(frame)
 	local skill = Skill(frame)
-	return skill:createInfobox(frame)
+	return skill:createInfobox()
 end
 
-function Skill:createInfobox(frame)
+function Skill:createInfobox()
 	local infobox = self.infobox
 	local args = self.args
 
 	if String.isEmpty(args.informationType) then
 		error('You need to specify an informationType, e.g. "Spell", "Ability, ...')
 	end
+	local hotkeyDescription, hotkeyDisplay = self:getHotkeys(args)
+	local durationDescription, durationDisplay = self:getDuration(args)
 
-	local hotkeyDescription, hotkeyDisplay = self:getHotkeys(infobox, args)
-	local durationDescription, durationDisplay = self:getDuration(infobox, args)
-
-	infobox:name(args.name)
-	infobox:image(args.image, args.defaultImage, args.imageSize)
-	infobox:centeredCell(args.caption)
-	infobox:header(args.informationType .. ' Information', true)
-	infobox:fcell(Cell:new('Caster(s)'):options({}):content(
-		unpack(self:getAllArgsForBase(args, 'caster', { makeLink = true }))):make())
-	infobox:cell('Cost', self:getCostDisplay(infobox, args))
-	infobox:cell(hotkeyDescription, hotkeyDisplay)
-	infobox:cell('Range', args.range)
-	infobox:cell('Radius', args.radius)
-	infobox:cell('[[Cooldown]]', args.cooldown)
-	infobox:cell(durationDescription, durationDisplay)
-	self.infobox:centeredCell(args.footnotes)
-	self:addCustomCells(infobox, args)
-	infobox:bottom(self:createBottomContent(infobox))
+	local widgets = ({
+		Header{name = args.name, image = args.image, size = args.imageSize},
+		Center{content = {args.caption}},
+		Title{name = args.informationType .. ' Information'},
+		Cell{name = 'Caster(s)', content = self:getAllArgsForBase(args, 'caster', { makeLink = true })},
+		Cell{name = 'Cost', content = {self:getCostDisplay(args)}},
+		Cell{name = hotkeyDescription, content = {hotkeyDisplay}},
+		Cell{name = 'Range', content = {args.range}},
+		Cell{name = 'Radius', content = {args.radius}},
+		Cell{
+			name = PageLink.makeInternalLink({onlyIfExists = true},'Cooldown') or 'Cooldown',
+			content = {args.cooldown}
+		},
+		Cell{name = durationDescription, content = {durationDisplay}},
+		Customizable{id = 'custom', children = {}},
+	})
+	table.insert(widgets, Center{content = {args.footnotes}})
 
 	if Namespace.isMain() then
-		local categories = self:getCategories(infobox, args)
+		local categories = self:getCategories(args)
 		infobox:categories(unpack(categories))
 	end
 
-	return infobox:build()
+	return infobox:widgetInjector(self:createWidgetInjector()):build(widgets)
 end
 
 --- Allows for overriding this functionality
-function Skill:getCategories(infobox, args)
+function Skill:getCategories(args)
 	return {}
 end
 
 --- Allows for overriding this functionality
-function Skill:getDuration(infobox, args)
+function Skill:getDuration(args)
 	return 'Duration', args.duration
 end
 
 --- Allows for overriding this functionality
-function Skill:getHotkeys(infobox, args)
+function Skill:getHotkeys(args)
 	local description = 'Hotkey'
 	local display
 	if not String.isEmpty(args.hotkey) then
@@ -81,7 +89,7 @@ function Skill:getHotkeys(infobox, args)
 end
 
 --- Allows for overriding this functionality
-function Skill:getCostDisplay(infobox, args)
+function Skill:getCostDisplay(args)
 	return args.cost
 end
 
