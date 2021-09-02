d654530ae97bca21dc806aeb6598d107fee0e801
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2021-09-02 12:36:24 +0200

    Add `Module:Infobox/Company/Custom` (#303)
    * Add `addCustomCells` to `Injector`
    
    * fix rebase issue
    
    * Add `Module:Infobox/Company/Custom`
    
    Add `Module:Infobox/Company/Custom`
    
    * Update infobox_company_custom.lua
    
    * Update to v2.0
    
    Co-authored-by: Jasper van Riet <jvanriet@teamliquid.com>

---
+++ components/infobox/wikis/pubg/infobox_company_custom.lua
@@ -0,0 +1,46 @@
+---
+-- @Liquipedia
+-- wiki=pubg
+-- page=Module:Infobox/Company/Custom
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Class = require('Module:Class')
+local Company = require('Module:Infobox/Company')
+local Injector = require('Module:Infobox/Widget/Injector')
+local Cell = require('Module:Infobox/Widget/Cell')
+
+local CustomCompany = Class.new()
+
+local CustomInjector = Class.new(Injector)
+
+local _args
+
+function CustomInjector:addCustomCells(widgets)
+	table.insert(widgets, Cell({
+		name = CustomCompany._createSisterCompaniesDescription(_args),
+		content = {self:getAllArgsForBase(_args, 'sister', {})}
+	}))
+	return widgets
+end
+
+function CustomCompany.run(frame)
+    local company = Company(frame)
+	company.createWidgetInjector = CustomCompany.createWidgetInjector
+	_args = company.args
+    return company:createInfobox(frame)
+end
+
+function CustomCompany:createWidgetInjector()
+	return CustomInjector()
+end
+
+function CustomCompany._createSisterCompaniesDescription(args)
+    if args.sister2 then
+      return 'Sister Companies'
+    end
+    return 'Sister Company'
+end
+
+return CustomCompany
