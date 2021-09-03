cea02a3a8cd9e7a75af1ed9a059ff03ed7c301c2
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2021-09-03 09:56:59 +0200

    Migrate infobox scene (#322)
    * Migrate infobox scene
    
    Migrates Infobox/Scene over to table based layout, see #304 . Merges into #321
    * has no custom versions on any wikis
    * only used on sc2
    
    * re-add region
    
    * fix typo
    
    * Use named args
    
    * Remove duplicates
    
    * Use Builder
    
    * Update infobox_scene.lua
    
    Co-authored-by: Jasper van Riet <jvanriet@teamliquid.com>

--- components/infobox/commons/infobox_scene.lua
+++ components/infobox/commons/infobox_scene.lua
@@ -7,85 +7,84 @@
 --
 
 local Class = require('Module:Class')
-local Cell = require('Module:Infobox/Cell')
 local Table = require('Module:Table')
 local Variables = require('Module:Variables')
 local Localisation = require('Module:Localisation')
 local Flags = require('Module:Flags')
 local Links = require('Module:Links')
-local BasicInfobox = require('Module:Infobox/Basic')
+local InfoboxBasic = require('Module:Infobox/Basic')
+local String = require('Module:String')
 
-local Scene = Class.new(BasicInfobox)
+local Widgets = require('Module:Infobox/Widget/All')
+local Cell = Widgets.Cell
+local Header = Widgets.Header
+local Title = Widgets.Title
+local Center = Widgets.Center
+local Customizable = Widgets.Customizable
+local Builder = Widgets.Builder
+
+local Scene = Class.new(InfoboxBasic)
 
 function Scene.run(frame)
-    local scene = Scene(frame)
-    return scene:createInfobox()
+	local scene = Scene(frame)
+	return scene:createInfobox()
 end
 
-function Scene:createInfobox(frame)
-    local infobox = self.infobox
-    local args = self.args
+function Scene:createInfobox()
+	local infobox = self.infobox
+	local args = self.args
 
-    local nameDisplay = self:createNameDisplay(args)
+	local widgets = {
+		Header{name = self:createNameDisplay(args), image = args.image},
+		Center{content = {args.caption}},
+		Title{name = 'Scene Information'},
+		Cell{name = 'Region', content = {args.region}},
+		Cell{name = 'National team', content = {args.nationalteam}, options = {makeLink = true}},
+		Cell{name = 'Events', content = self:getAllArgsForBase(args, 'event', {makeLink = true})},
+		Cell{name = 'Size', content = {args.size}},
+		Customizable{id = 'custom', children = {}},
+		Center{content = {args.footnotes}},
+		Builder{
+			builder = function()
+				local links = Links.transform(args)
+				if not Table.isEmpty(links) then
+					return {
+						Title{name = 'Links'},
+						Widgets.Links{content = links}
+					}
+				end
+			end
+		},
+		Builder{
+			builder = function()
+				if not String.isEmpty(args.achievements) then
+					return {
+						Title{name ='Achievements'},
+						Center{content = {args.achievements}}
+					}
+				end
+			end
+		}
+	}
 
-    infobox :name(nameDisplay)
-            :image(args.image)
-            :centeredCell(args.caption)
-            :header('Scene Information', true)
-            :cell('Region', self:createRegion(args))
-            :fcell(Cell:new('National team'):options({makeLink = true}):content(args.nationalteam):make())
-            :fcell(Cell :new('Events')
-                        :options({makeLink = true})
-                        :content(
-                            args.event or args.event1,
-                            args.event2,
-                            args.event3,
-                            args.event4,
-                            args.event5
-                        ):make()
-            )
-            :cell('Size', args.size)
-    self:addCustomCells(infobox, args)
+	infobox:categories('Scene')
 
-    local links = Links.transform(args)
-    local achievements = self:getAchievements(infobox, args)
-
-    infobox :header('Links', not Table.isEmpty(links))
-            :links(links)
-            :header('Achievements', achievements)
-            :centeredCell(achievements)
-            :centeredCell(args.footnotes)
-    self:addCustomContent(infobox, args)
-    infobox:bottom(self:createBottomContent(infobox))
-
-    infobox:categories('Scene')
-
-    return infobox:build()
+	return infobox:widgetInjector(self:createWidgetInjector()):build(widgets)
 end
 
 --- Allows for overriding this functionality
 function Scene:createNameDisplay(args)
-    local name = args.name
-    local country = Flags._CountryName(args.country or args.scene)
-    if not name then
-        local localised = Localisation.getLocalisation(country)
-        local flag = Flags._Flag(country)
-        name = flag .. '&nbsp;' .. localised .. ((' ' .. args.gamenamedisplay) or '') .. ' scene'
-    end
+	local name = args.name
+	local country = Flags._CountryName(args.country or args.scene)
+	if not name then
+		local localised = Localisation.getLocalisation(country)
+		local flag = Flags._Flag(country)
+		name = flag .. '&nbsp;' .. localised .. ((' ' .. args.gamenamedisplay) or '') .. ' scene'
+	end
 
-    Variables.varDefine('country', country)
+	Variables.varDefine('country', country)
 
-    return name
-end
-
---- Allows for overriding this functionality
-function Scene:getAchievements(infobox, args)
-    return args.achievements
-end
-
---- Allows for overriding this functionality
-function Scene:createRegion(args)
-    return args.region
+	return name
 end
 
 return Scene
