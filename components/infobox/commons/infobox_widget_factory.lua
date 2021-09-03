541c4f22230867c301cde905f398aca9761bca4b
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2021-09-03 23:43:54 +0200

    Fix bug 1 of #340
    Fix bug 1 of #340

--- components/infobox/commons/infobox_widget_factory.lua
+++ components/infobox/commons/infobox_widget_factory.lua
@@ -1,15 +1,7 @@
----
--- @Liquipedia
--- wiki=commons
--- page=Module:Infobox/Widget/Factory
---
--- Please see https://github.com/Liquipedia/Lua-Modules to contribute
---
-
 local Class = require('Module:Class')
 local Builder = require('Module:Infobox/Widget/Builder')
 local Customizable = require('Module:Infobox/Widget/Customizable')
-local Widget = require('Module:Infobox/Widget/Widget')
+local Widget = require('Module:Infobox/Widget')
 
 local WidgetFactory = Class.new()
 
@@ -32,8 +24,20 @@ function WidgetFactory.work(widget, injector)
 			if child['is_a'] == nil or child:is_a(Widget) == false then
 				return error('Customizable can only contain Widgets as children')
 			end
-			for _, item in pairs(child:make() or {}) do
-				table.insert(convertedWidgets, item)
+			if child:is_a(Builder) then
+				local subChildren = child:make()
+
+				for _, subChild in pairs(subChildren or {}) do
+					local childOutput = WidgetFactory.work(subChild, injector)
+					-- Our child might contain a list of children, so we need to iterate
+					for _, item in pairs(childOutput) do
+						table.insert(convertedWidgets, item)
+					end
+				end
+			else
+				for _, item in pairs(child:make() or {}) do
+					table.insert(convertedWidgets, item)
+				end
 			end
 		end
 	else
