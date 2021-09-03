243945c109d06760dc7af55729be1e25b8126f39
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2021-09-03 16:09:54 +0200

    Update infobox_show_custom.lua

--- components/infobox/commons/infobox_show_custom.lua
+++ components/infobox/commons/infobox_show_custom.lua
@@ -8,29 +8,21 @@
 
 local Class = require('Module:Class')
 local Show = require('Module:Infobox/Show')
-local Namespace = require('Module:Namespace')
 local Injector = require('Module:Infobox/Widget/Injector')
-local Cell = require('Module:Infobox/Widget/Cell')
 
 local CustomShow = Class.new()
 
-local _show
 
 local CustomInjector = Class.new(Injector)
 
 function CustomShow.run(frame)
     local customShow = Show(frame)
-	_show = customShow
 	customShow.createWidgetInjector = CustomShow.createWidgetInjector
     return customShow:createInfobox(frame)
 end
 
 function CustomShow:createWidgetInjector()
 	return CustomInjector()
-end
-
-function CustomInjector:addCustomCells(widgets)
-	return widgets
 end
 
 return CustomShow
