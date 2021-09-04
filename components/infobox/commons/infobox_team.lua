dd219edaea49394112d74459dbe7336484170b63
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2021-09-04 08:14:32 +0200

    Update infobox_team.lua

--- components/infobox/commons/infobox_team.lua
+++ components/infobox/commons/infobox_team.lua
@@ -35,6 +35,11 @@ function Team:createInfobox()
 	local infobox = self.infobox
 	local args = self.args
 
+	if Namespace.isMain() then
+		infobox:categories('Teams', unpack(self:getWikiCategories(args)))
+		self:_setLpdbData(args, links)
+	end
+
 	local widgets = {
 		Header{name = args.name, image = args.image},
 		Center{content = {args.caption}},
