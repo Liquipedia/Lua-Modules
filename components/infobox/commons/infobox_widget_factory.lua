372cf57662e883cc5c7f78cb4a51f158005dc70d
Jasper van Riet <JaspervanRiet@users.noreply.github.com>
2021-09-06 11:28:55 +0200

    Create widget factory (#333)
    * Create `WidgetFactory` to handle widget processing
    
    * Don't have inner loop
    
    * Add header
    
    * Fix import

---
+++ components/infobox/commons/infobox_widget_factory.lua
@@ -0,0 +1,46 @@
+---
+-- @Liquipedia
+-- wiki=commons
+-- page=Module:Infobox/Widget/Factory
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Class = require('Module:Class')
+local Builder = require('Module:Infobox/Widget/Builder')
+local Customizable = require('Module:Infobox/Widget/Customizable')
+local Widget = require('Module:Infobox/Widget')
+
+local WidgetFactory = Class.new()
+
+function WidgetFactory.work(widget, injector)
+	local convertedWidgets = {}
+
+	if widget:is_a(Builder) then
+		local children = widget:make()
+
+		for _, child in pairs(children or {}) do
+			local childOutput = WidgetFactory.work(child, injector)
+			-- Our child might contain a list of children, so we need to iterate
+			for _, item in pairs(childOutput) do
+				table.insert(convertedWidgets, item)
+			end
+		end
+	elseif widget:is_a(Customizable) then
+		widget:setWidgetInjector(injector)
+		for _, child in pairs(widget:make() or {}) do
+			if child['is_a'] == nil or child:is_a(Widget) == false then
+				return error('Customizable can only contain Widgets as children')
+			end
+			for _, item in pairs(child:make() or {}) do
+				table.insert(convertedWidgets, item)
+			end
+		end
+	else
+		return widget:make()
+	end
+
+	return convertedWidgets
+end
+
+return WidgetFactory
