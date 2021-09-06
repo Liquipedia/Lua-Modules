05e5019fb2c7ca0d8e7937ad5813d9f280846dd5
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2021-09-06 11:03:06 +0200

    Improve `makeFullLinksForTableItems` (#338)
    * improve `makeFullLinksForTableItems`
    
    * allow to also make links for entries that appended a number
    * examples:
    * * esl2, twitch2, bracket2, .....
    
    * Update links.lua
    
    * Update links.lua

--- standard/links.lua
+++ standard/links.lua
@@ -173,9 +173,15 @@ end
 
 function Links.makeFullLinksForTableItems(links, variant)
 	for key, item in pairs(links) do
-		links[key] = Links.makeFullLink(key, item, variant)
+		links[key] = Links.makeFullLink(Links._removeAppendedNumber(key), item, variant)
 	end
 	return links
+end
+
+--remove appended number
+--needed because the link icons require e.g. 'esl' instead of 'esl2'
+function Links._removeAppendedNumber(key)
+    return string.gsub(key, '%d$', '')
 end
 
 return Class.export(Links, {frameOnly = true})
