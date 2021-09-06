a5f9895883f9824f85f9326bd4850bbc118f2e37
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2021-09-06 11:02:39 +0200

    Add Breakdown Widget (#343)
    * Add Breakdown Widget
    
    Needed for sc, sc2, warcraft in infoboxes league and team
    
    * Update infobox_widget_breakdown.lua
    
    * adhere comment

---
+++ components/infobox/commons/infobox_widget_breakdown.lua
@@ -0,0 +1,40 @@
+---
+-- @Liquipedia
+-- wiki=commons
+-- page=Module:Infobox/Widget/Breakdown
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Class = require('Module:Class')
+local Widget = require('Module:Infobox/Widget')
+
+local Breakdown = Class.new(
+	Widget,
+	function(self, input)
+		self.contents = input.content
+	end
+)
+
+function Breakdown:make()
+	return {Breakdown:_breakdown(self.contents)}
+end
+
+function Breakdown:_breakdown(contents)
+	if type(contents) ~= 'table' or contents == {} then
+		return nil
+	end
+
+	local div = mw.html.create('div')
+	local number = #contents
+	for _, content in ipairs(contents) do
+		local infoboxCustomCell = mw.html.create('div'):addClass('infobox-cell-' .. number
+			.. ' infobox-center')
+		infoboxCustomCell:wikitext(content)
+		div:node(infoboxCustomCell)
+	end
+
+	return div
+end
+
+return Breakdown
