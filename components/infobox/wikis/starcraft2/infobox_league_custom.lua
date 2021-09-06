f315dad1c85eae71815a14297ec4f7edfb8a8630
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2021-09-06 09:42:36 +0200

    Update infobox_league_custom.lua

--- components/infobox/wikis/starcraft2/infobox_league_custom.lua
+++ components/infobox/wikis/starcraft2/infobox_league_custom.lua
@@ -66,7 +66,7 @@ end
 
 function CustomInjector:parse(id, widgets)
 	if id == 'server' then
-		return {			
+		return {
 			Cell{name = 'Game version', content = {
 					CustomLeague._getGameVersion()
 				}},
