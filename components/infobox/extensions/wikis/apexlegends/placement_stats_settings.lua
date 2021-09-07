f0c1525c34442ffb3efdc1eb6b1b948b86711f46
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2021-09-07 15:35:52 +0200

    Create placement_stats_settings.lua

---
+++ components/infobox/extensions/wikis/apexlegends/placement_stats_settings.lua
@@ -0,0 +1,12 @@
+---
+-- @Liquipedia
+-- wiki=apexlegends
+-- page=Module:InfoboxPlacementStats/Settings
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+return {
+	tiers = { '1', '2', '3', '4', '6', '7', '9', },
+	exclusionTypes = { },
+}
