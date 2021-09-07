0ba25c8e0fe434b8da2ac7340d0cc1366f77e0e5
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2021-09-07 14:50:35 +0200

    Create placement_stats_settings.lua

---
+++ components/infobox/extensions/commons/placement_stats_settings.lua
@@ -0,0 +1,12 @@
+---
+-- @Liquipedia
+-- wiki=commons
+-- page=Module:InfoboxPlacementStats/Settings
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+return {
+	tiers = { '1', '2', '3', },
+	exclusionTypes = { 'Qualifier', },
+}
