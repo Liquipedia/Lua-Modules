2f99f1ac02741d54650034b07d932664a7fb9b5e
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2021-09-07 08:01:56 +0200

    undo white space clean up for sc2 `infobox_team_custom.lua`
    undo white space clean up for sc2 `infobox_team_custom.lua` due to those code segments getting removed in #357

--- components/infobox/wikis/starcraft2/infobox_team_custom.lua
+++ components/infobox/wikis/starcraft2/infobox_team_custom.lua
@@ -133,20 +133,20 @@ function CustomTeam:createBottomContent()
 end
 
 function CustomTeam.playerBreakDownDisplay(contents)
-	if type(contents) ~= 'table' or contents == {} then
-		return nil
-	end
+    if type(contents) ~= 'table' or contents == {} then
+        return nil
+    end
 
-	local div = mw.html.create('div')
-	local number = #contents
-	for _, content in ipairs(contents) do
-		local infoboxCustomCell = mw.html.create('div'):addClass('infobox-cell-' .. number
+    local div = mw.html.create('div')
+    local number = #contents
+    for _, content in ipairs(contents) do
+        local infoboxCustomCell = mw.html.create('div'):addClass('infobox-cell-' .. number
 			.. ' infobox-center')
-		infoboxCustomCell:wikitext(content)
-		div:node(infoboxCustomCell)
-	end
+        infoboxCustomCell:wikitext(content)
+        div:node(infoboxCustomCell)
+    end
 
-	return div
+    return div
 end
 
 function CustomTeam.storeToLPDB(args)
