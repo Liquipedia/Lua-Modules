acf2d0fd55fee615a28f08b74a7eb7c1755e1c8d
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2021-09-08 11:46:50 +0200

    Update infobox_widget_breakdown.lua

--- components/infobox/commons/infobox_widget_breakdown.lua
+++ components/infobox/commons/infobox_widget_breakdown.lua
@@ -13,23 +13,27 @@ local Breakdown = Class.new(
 	Widget,
 	function(self, input)
 		self.contents = input.content
+		self.options = input.options
 	end
 )
 
 function Breakdown:make()
-	return {Breakdown:_breakdown(self.contents)}
+	return {Breakdown:_breakdown(self.contents, slef.options)}
 end
 
-function Breakdown:_breakdown(contents)
+function Breakdown:_breakdown(contents, options)
 	if type(contents) ~= 'table' or contents == {} then
 		return nil
 	end
+	options = options or {}
 
 	local div = mw.html.create('div')
 	local number = #contents
 	for _, content in ipairs(contents) do
-		local infoboxCustomCell = mw.html.create('div'):addClass('infobox-cell-' .. number
-			.. ' infobox-center')
+		local infoboxCustomCell = mw.html.create('div'):addClass('infobox-cell-' .. number)
+		if not options.isNotCentered then
+			infoboxCustomCell::addClass('infobox-center')
+		end
 		infoboxCustomCell:wikitext(content)
 		div:node(infoboxCustomCell)
 	end
