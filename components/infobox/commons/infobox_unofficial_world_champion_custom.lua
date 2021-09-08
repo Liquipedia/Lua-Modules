e72fdf51aad156e9c763b01edd29001fe96cb3d2
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2021-09-08 16:24:34 +0200

    Update infobox_unofficial_world_champion_custom.lua

--- components/infobox/commons/infobox_unofficial_world_champion_custom.lua
+++ components/infobox/commons/infobox_unofficial_world_champion_custom.lua
@@ -8,24 +8,12 @@
 
 local UnofficialWorldChampion = require('Module:Infobox/UnofficialWorldChampion')
 local Class = require('Module:Class')
-local Injector = require('Module:Infobox/Widget/Injector')
 
 local CustomUnofficialWorldChampion = Class.new()
 
-local CustomInjector = Class.new(Injector)
-
 function CustomUnofficialWorldChampion.run(frame)
 	local unofficialWorldChampion = UnofficialWorldChampion(frame)
-	unofficialWorldChampion.createWidgetInjector = CustomUnofficialWorldChampion.createWidgetInjector
 	return unofficialWorldChampion:createInfobox(frame)
-end
-
-function CustomInjector:addCustomCells(widgets)
-  return widgets
-end
-
-function CustomUnofficialWorldChampion:createWidgetInjector()
-	return CustomInjector()
 end
 
 return CustomUnofficialWorldChampion
