3e9c0a07e31d21471ed3247f0cd26775f903ce9f
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2021-09-08 16:12:42 +0200

    add `Module:Infobox/Campaign`
    rofl

---
+++ components/infobox/commons/infobox_campaign.lua
@@ -0,0 +1,36 @@
+---
+-- @Liquipedia
+-- wiki=commons
+-- page=Module:Infobox/Campaign
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Class = require('Module:Class')
+local InfoboxBasic = require('Module:Infobox/Basic')
+
+local Widgets = require('Module:Infobox/Widget/All')
+local Header = Widgets.Header
+local Center = Widgets.Center
+
+local Campaign = Class.new(InfoboxBasic)
+
+function Campaign.run(frame)
+	local campaign = Campaign(frame)
+	return campaign:createInfobox()
+end
+
+function Campaign:createInfobox()
+	local infobox = self.infobox
+	local args = self.args
+
+	local widgets = {
+		Header{name = self.name, image = args.image},
+		Center{content = {args.caption}},
+	}
+
+	return infobox:widgetInjector(self:createWidgetInjector()):build(widgets)
+end
+
+
+return Campaign
