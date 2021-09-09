a26d618977e67e66f1e8ae586763d756d493b926
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2021-09-09 16:44:23 +0200

    Add `Infobox campaign mission` (#380)
    * Add `Module:Infobox/CampaignMission`
    
    * Create infobox_campaign_mission_custom.lua
    
    * Update infobox_widget_header.lua
    
    * Update infobox_campaign_mission.lua
    
    * Update infobox_campaign_mission_custom.lua
    
    * fix indents
    
    * Update infobox_campaign_mission_custom.lua

---
+++ components/infobox/commons/infobox_campaign_mission.lua
@@ -0,0 +1,55 @@
+---
+-- @Liquipedia
+-- wiki=commons
+-- page=Module:Infobox/CampaignMission
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Class = require('Module:Class')
+local Namespace = require('Module:Namespace')
+local BasicInfobox = require('Module:Infobox/Basic')
+
+local Widgets = require('Module:Infobox/Widget/All')
+local Header = Widgets.Header
+local Title = Widgets.Title
+local Center = Widgets.Center
+local Customizable = Widgets.Customizable
+local Breakdown = Widgets.Breakdown
+
+local Mission = Class.new(BasicInfobox)
+
+function Mission.run(frame)
+	local mission = Mission(frame)
+	return mission:createInfobox()
+end
+
+function Mission:createInfobox()
+	local infobox = self.infobox
+	local args = self.args
+
+	local widgets = {
+		Customizable{id = 'header', children = {
+				Header{name = args.name, image = args.image},
+			}
+		},
+		Center{content = {args.caption}},
+		Title{name = 'Mission Information'},
+		Breakdown{
+			content = {'Mission Objective'},
+			classes = {'infobox-header', 'wiki-backgroundcolor-light', 'infobox-header-3'}
+		},
+		Breakdown{content = { args.objective }},
+		Customizable{id = 'custom', children = {}},
+		Center{content = {args.footnotes}},
+	}
+
+	if Namespace.isMain() then
+		infobox:categories('Missions', 'Campaign')
+		infobox:categories(unpack(self:getWikiCategories(args)))
+	end
+
+	return infobox:widgetInjector(self:createWidgetInjector()):build(widgets)
+end
+
+return Mission
---
+++ components/infobox/wikis/starcraft2/infobox_campaign_mission_custom.lua
@@ -0,0 +1,89 @@
+---
+-- @Liquipedia
+-- wiki=starcraft2
+-- page=Module:Infobox/CampaignMission/Custom
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Mission = require('Module:Infobox/CampaignMission')
+local String = require('Module:StringUtils')
+local Class = require('Module:Class')
+local Injector = require('Module:Infobox/Widget/Injector')
+local Cell = require('Module:Infobox/Widget/Cell')
+local Header = require('Module:Infobox/Widget/Header')
+
+local CustomMission = Class.new()
+
+local CustomInjector = Class.new(Injector)
+
+local _args
+
+local _GAME_SWITCH = {
+	wol = 'Wings of Liberty',
+	hots = 'Heart of the Swarm',
+	lotv = 'Legacy of the Void',
+	nco = 'Nova Covert Ops',
+	coop = 'Co-op Mission',
+}
+local _fullGameName
+
+function CustomMission.run(frame)
+	local mission = Mission(frame)
+	_args = mission.args
+	mission.createWidgetInjector = CustomMission.createWidgetInjector
+	mission.getWikiCategories = CustomMission.getWikiCategories
+	return mission:createInfobox(frame)
+end
+
+function CustomInjector:addCustomCells(widgets)
+	return {
+		Cell{ name = 'Credits Earned', content = {_args.credits}},
+		Cell{ name = 'Research Points', content = {_args.research}},
+		Cell{ name = 'Kerrigan Levels', content = {_args.Kerrigan}},
+		Cell{ name = 'Evolution Unlock', content = {CustomMission._getEvolution()}},
+		Cell{ name = 'New Units', content = {_args.units}},
+		Cell{ name = 'Available Heroes', content = {_args.heroes}},
+	}
+end
+
+function CustomMission._getEvolution()
+	local evolution = _args.Evolution
+	if String.isEmpty(evolution) then
+		return nil
+	elseif evolution == 'None' then
+		return evolution
+	else
+		return '[[evolution (Heart of the Swarm Campaign)|evolution]]'
+	end
+end
+
+function CustomInjector:parse(id, widgets)
+	if id == 'header' then
+		--fallback for non main space
+		_fullGameName = _fullGameName or _GAME_SWITCH[_args.game] or 'Wings of Liberty'
+
+		return {
+			Header{
+				name = _fullGameName,
+				subHeader = _args.name,
+				image = _args.image
+			}
+		}
+	end
+	return widgets
+end
+
+function CustomMission.getWikiCategories()
+	_fullGameName = _GAME_SWITCH[_args.game] or 'Wings of Liberty'
+	return {
+		_fullGameName .. ' Missions',
+		_fullGameName .. ' Campaign',
+	}
+end
+
+function CustomMission:createWidgetInjector()
+	return CustomInjector()
+end
+
+return CustomMission
