a1aa6f622a7826c724f9ceedbcd19cd0e6bb3ad9
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2021-09-09 15:02:19 +0200

    Update infobox_person.lua

--- components/infobox/commons/infobox_person.lua
+++ components/infobox/commons/infobox_person.lua
@@ -48,6 +48,7 @@ end
 function Person:createInfobox()
 	local infobox = self.infobox
 	local args = self.args
+	_should_store_data = Person:shouldStoreData(args)
 
 	if String.isEmpty(args.id) then
 		error('You need to specify an "id"')
