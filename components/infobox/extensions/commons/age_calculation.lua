e75a334f334cd8854ebaa0adc68f0cbba718d9c3
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2021-09-14 17:17:00 +0200

    Update age_calculation.lua

--- components/infobox/extensions/commons/age_calculation.lua
+++ components/infobox/extensions/commons/age_calculation.lua
@@ -203,11 +203,12 @@ end
 function AgeCalculation._assertValidDates(firstDate, secondDate)
 	local isoMin = firstDate.iso or firstDate.isoMin
 	local isoMax = secondDate.iso or secondDate.isoMax
+	local isoMaxMin = secondDate.iso or secondDate.isoMax
 	if isoMin and isoMax and isoMin > isoMax then
 		error('Death date can not be before birth date')
 	elseif isoMin and  (isoMin > _CURRENT_ISO or isoMin < _EPOCH) then
 		error('Birth date can not be in the future')
-	elseif isoMax and isoMax > _CURRENT_ISO then
+	elseif isoMaxMin and isoMaxMin > _CURRENT_ISO then
 		error('Invalid death date (out of allowed date range)')
 	end
 end
