7a298870e43a8f6eaccb53b8e21b63500b6ed38a
Jasper van Riet <JaspervanRiet@users.noreply.github.com>
2021-09-22 13:00:00 +0200

    Create age module (#401)
    * Create basic age calculation
    
    * Improve readability
    
    * Remove unneeded function
    
    * Return display instead
    
    * Avoid age displayed twice
    
    * Don't store wrong dates
    
    * Do not show wrong display
    
    * Add missing properties
    
    * Address comment

---
+++ components/infobox/extensions/commons/age.lua
@@ -0,0 +1,238 @@
+local Class = require('Module:Class')
+local String = require('Module:String')
+local Variables = require('Module:Variables')
+
+local AgeCalculation = {}
+
+local _EPOCH_DATE = { year = 1970, month = 1, day = 1 }
+local _DEFAULT_DAYS_IN_MONTH = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 }
+local _MONTH_DECEMBER = 12
+local _CURRENT_YEAR = tonumber(mw.getContentLanguage():formatDate('Y'))
+
+---
+-- Represents a date
+--
+-- Before accessing the `year`, `month` and `day` values, please verify
+-- whether `isExact` or `isEmpty` are set to false or true respectively.
+--
+-- `isExact`: Whether this is a complete date, consisting of YYYY-MM-DD
+-- `isEmpty`: Whether there is a date here at all.
+-- `time`: Time in seconds
+local Date = Class.new(
+	function(self, dateString, location)
+		if String.isEmpty(dateString) then
+			self.isExact = false
+			self.isEmpty = true
+			return
+		end
+
+		self.location = location
+		self.isEmpty = false
+		self.isExact = true
+		local fields = mw.text.split(dateString, '-')
+
+		self.year = tonumber(fields[1])
+		self.month = tonumber(fields[2])
+		self.day = tonumber(fields[3])
+
+		if self.year == nil or self.month == nil or self.day == nil then
+			self.isExact = false
+		else
+			self.time = os.time({
+				year = self.year,
+				month = self.month,
+				day = self.day
+			})
+		end
+
+	end
+)
+
+function Date:makeDisplay()
+	if self.isEmpty then
+		return ''
+	end
+
+	local formatString = self:_getFormatString()
+	local timestamp = self:getEarliestPossible()
+
+	if formatString then
+		local formatted = os.date(formatString, timestamp)
+		if not String.isEmpty(self.location) then
+			formatted = formatted .. '<br>' .. self.location
+		end
+
+		return formatted
+	end
+
+	return ''
+end
+
+function Date:makeIso()
+	if self.isEmpty or not self.isExact then
+		return ''
+	end
+
+	local year = self.year
+	local month = self.month
+	local day = self.day
+
+	return year .. '-' .. month .. '-' .. day
+end
+
+function Date:_getFormatString()
+	if self.year then
+		if self.month and self.day then
+			return '%B %e, %Y'
+		elseif self.month then
+			return '%B, %Y'
+		else -- Ignore day if we do not know the month
+			return '%Y'
+		end
+	elseif self.month and self.day then
+		return '%B %e'
+	elseif self.month then
+		return '%B'
+	end
+end
+
+function Date:getEarliestPossible()
+	return os.time({
+		year = self.year or _EPOCH_DATE.year,
+		month = self.month or _EPOCH_DATE.month,
+		day = self.day or _EPOCH_DATE.day,
+	})
+end
+
+function Date:getLatestPossible()
+	return os.time({
+		year = self.year or _CURRENT_YEAR,
+		month = self.month or _MONTH_DECEMBER,
+		day = self.day or _DEFAULT_DAYS_IN_MONTH[self.month or _MONTH_DECEMBER],
+	})
+end
+
+local BirthDate = Class.new(Date)
+local DeathDate = Class.new(Date)
+
+local Age = Class.new(
+	function(self, birthDate, deathDate)
+		self.birthDate = birthDate
+		self.deathDate = deathDate
+	end
+)
+
+function Age:calculate()
+	local endDate
+	if self.deathDate.isEmpty then
+		endDate = os.time()
+	else
+		endDate = self.deathDate.time
+	end
+
+	if self.birthDate.isExact and (self.deathDate.exact or self.deathDate.isEmpty) then
+		return self:_secondsToAge(os.difftime(endDate, self.birthDate.time))
+	elseif self.birthDate.year and (self.deathDate.year or self.deathDate.isEmpty) then
+		local minEndDate
+		local maxEndDate
+		if self.deathDate.isEmpty then
+			minEndDate = os.time()
+			maxEndDate = os.time()
+		else
+			minEndDate = self.deathDate:getEarliestPossible()
+			maxEndDate = self.deathDate:getLatestPossible()
+		end
+
+		local minAge = self:_secondsToAge(os.difftime(minEndDate, self.birthDate:getLatestPossible()))
+		local maxAge = self:_secondsToAge(os.difftime(maxEndDate, self.birthDate:getEarliestPossible()))
+
+		-- If our min and max age values are identical, then we can just display that value. Else,
+		-- we have to display a range of ages
+		if minAge == maxAge then
+			return minAge
+		else
+			return minAge .. '-' .. maxAge
+		end
+	end
+
+	return nil
+end
+
+function Age:makeDisplay()
+	local age = self:calculate()
+	local result = {
+		death = nil,
+		birth = nil,
+	}
+
+	if age ~= nil then
+		if not self.deathDate.isEmpty then
+			result.death = self.deathDate:makeDisplay() .. ' (aged ' .. age .. ')'
+			result.birth = self.birthDate:makeDisplay()
+		else
+			result.birth = self.birthDate:makeDisplay() .. ' (age ' .. age .. ')'
+		end
+	else
+		result.death = self.deathDate:makeDisplay()
+		result.birth = self.birthDate:makeDisplay()
+	end
+
+	return result
+end
+
+function Age:_secondsToAge(seconds)
+	return math.floor(seconds / 60 / 60 / 24 / 365.25)
+end
+
+function AgeCalculation.run(args)
+	local shouldStore = args.shouldstore
+	local birthLocation = args.birthlocation
+	local birthDate = BirthDate(args.birthdate, birthLocation)
+	local deathDate = DeathDate(args.deathdate)
+
+	AgeCalculation._assertValidDates(birthDate, deathDate)
+
+	local age = Age(birthDate, deathDate):makeDisplay()
+
+	Variables.varDefine('player_birthdate', birthDate:makeIso())
+	Variables.varDefine('player_deathdate', deathDate:makeIso())
+
+	if shouldStore then
+		if not birthDate.isExact then
+			age.birth = age.birth .. '[[Category:Incomplete birth dates]]'
+		end
+
+		if birthDate.year then
+			age.birth = age.birth .. '[[Category:' .. birthDate.year .. ' births]]'
+		end
+
+		if not deathDate.isExact then
+			age.death = age.death .. '[[Category:Incomplete death dates]]'
+		end
+	end
+
+	return {
+		birth = age.birth,
+		death = age.death
+	}
+end
+
+function AgeCalculation._assertValidDates(birthDate, deathDate)
+	local earliestPossibleBirthDate = birthDate:getEarliestPossible()
+	if deathDate.isExact then
+		if earliestPossibleBirthDate > deathDate:getLatestPossible() then
+			return error('Death date can not be before birth date')
+		end
+
+		if deathDate:getEarliestPossible() > os.time() then
+			return error('Death date out of allowed range')
+		end
+	end
+
+	if earliestPossibleBirthDate > os.time() or
+		earliestPossibleBirthDate < os.time(_EPOCH_DATE) then
+			return error('Birth date out of allowed range')
+	end
+end
+
+return Class.export(AgeCalculation)
