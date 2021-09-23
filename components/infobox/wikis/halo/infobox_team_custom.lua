e416364ec73f5bb14e61474fff44ef0964169ca9
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2021-09-23 10:42:04 +0200

    Add infobox team custom
    Add infobox team custom

---
+++ components/infobox/wikis/halo/infobox_team_custom.lua
@@ -0,0 +1,290 @@
+---
+-- @Liquipedia
+-- wiki=halo
+-- page=Module:Infobox/League/halo
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local League = require('Module:Infobox/League')
+local String = require('Module:String')
+local Template = require('Module:Template')
+local Variables = require('Module:Variables')
+local Tier = require('Module:Tier')
+local Namespace = require('Module:Namespace')
+local Class = require('Module:Class')
+local Injector = require('Module:Infobox/Widget/Injector')
+local Cell = require('Module:Infobox/Widget/Cell')
+local Title = require('Module:Infobox/Widget/Title')
+local Center = require('Module:Infobox/Widget/Center')
+
+local CustomLeague = Class.new()
+local CustomInjector = Class.new(Injector)
+
+local _args
+
+local _ABBR_USD = '<abbr title="United States Dollar">USD</abbr>'
+local _TODAY = os.date('%Y-%m-%d', os.time())
+local _GAME = mw.loadData('Module:GameVersion')
+
+function CustomLeague.run(frame)
+	local league = League(frame)
+	_args = league.args
+
+	league.createWidgetInjector = CustomLeague.createWidgetInjector
+
+	return league:createInfobox(frame)
+end
+
+function CustomLeague:createWidgetInjector()
+	return CustomInjector()
+end
+
+function CustomInjector:parse(id, widgets)
+	if id == 'gamesettings' then
+		return {
+			Cell{name = 'Game version', content = {
+					CustomLeague._getGameVersion()
+				}},
+			}
+	elseif id == 'prizepool' then
+		return {
+			Cell{
+				name = 'Prize pool',
+				content = {CustomLeague:_createPrizepool()},
+			},
+		}
+	elseif id == 'liquipediatier' then
+		return {
+			Cell{
+				name = 'Liquipedia tier',
+				content = {CustomLeague:_createTierDisplay()},
+			},
+		}
+	elseif id == 'customcontent' then
+		--player breakdown
+		if _args.player_number then
+			table.insert(widgets, Title{name = 'Players'})
+			table.insert(widgets, Cell{name = 'Number of players', content = {_args.player_number}})
+		end
+
+		--teams section
+		if _args.team_number or (not String.isEmpty(_args.team1)) then
+			Variables.varDefine('is_team_tournament', 1)
+			table.insert(widgets, Title{name = 'Teams'})
+		end
+		table.insert(widgets, Cell{name = 'Number of teams', content = {_args.team_number}})
+		if not String.isEmpty(_args.team1) then
+			local teams = CustomLeague:_makeBasedListFromArgs('team')
+			table.insert(widgets, Center{content = teams})
+		end
+
+		--maps
+		if not String.isEmpty(_args.map1) then
+			table.insert(widgets, Title{name = 'Maps'})
+			table.insert(widgets, Center{content = CustomLeague:_makeBasedListFromArgs('map')})
+		end
+	end
+	return widgets
+end
+
+function CustomLeague:_createPrizepool()
+	if String.isEmpty(_args.prizepool) and
+		String.isEmpty(_args.prizepoolusd) then
+		return nil
+	end
+
+	local localCurrency = _args.localcurrency
+	local prizePoolUSD = _args.prizepoolusd
+	local prizePool = _args.prizepool
+
+	if localCurrency == 'text' then
+		return prizePool
+	else
+		local display, hasPlus
+		if prizePoolUSD then
+			prizePoolUSD, hasPlus = CustomLeague:_cleanPrizeValue(prizePoolUSD)
+		end
+
+		prizePool, hasPlus = CustomLeague:_cleanPrizeValue(prizePool, localCurrency, hasPlus)
+
+		if localCurrency and not prizePoolUSD then
+			local exchangeDate = Variables.varDefault('tournament_enddate', _TODAY)
+			prizePoolUSD = CustomLeague:_currencyConversion(prizePool, localCurrency:upper(), exchangeDate)
+			if not prizePoolUSD then
+				error('Invalid local currency "' .. localCurrency .. '"')
+			end
+		end
+
+		local plusText = hasPlus and '+' or ''
+		if prizePoolUSD and prizePool then
+			display = Template.safeExpand(
+				mw.getCurrentFrame(),
+				'Local currency',
+				{(localCurrency or 'usd'):lower(), prizepool = CustomLeague:_displayPrizeValue(prizePool, 2) .. plusText}
+			) .. '<br>(≃ $' .. CustomLeague:_displayPrizeValue(prizePoolUSD) .. plusText .. ' ' .. _ABBR_USD .. ')'
+		elseif prizePoolUSD then
+			display = '$' .. CustomLeague:_displayPrizeValue(prizePoolUSD) .. plusText .. ' ' .. _ABBR_USD
+		elseif prizePool then
+			display = Template.safeExpand(
+				mw.getCurrentFrame(),
+				'Local currency',
+				{(localCurrency or 'usd'):lower(), prizepool = CustomLeague:_displayPrizeValue(prizePool, 2) .. plusText}
+			)
+		end
+
+		Variables.varDefine('tournament_prizepoolusd', prizePoolUSD or prizePool)
+
+		return display
+	end
+end
+
+function CustomLeague:_createTierDisplay()
+	local tier = _args.liquipediatier or ''
+	local tierType = _args.liquipediatiertype or _args.tiertype or ''
+	if String.isEmpty(tier) then
+		return nil
+	end
+
+	local tierText = Tier['text'][tier]
+	local hasInvalidTier = tierText == nil
+	tierText = tierText or tier
+
+	local hasInvalidTierType = false
+
+	local output = '[[' .. tierText .. ' Tournaments|' .. tierText .. ']]'
+		.. '[[Category:' .. tierText .. ' Tournaments]]'
+
+	if not String.isEmpty(tierType) then
+		tierType = Tier['types'][string.lower(tierType or '')] or tierType
+		hasInvalidTierType = Tier['types'][string.lower(tierType or '')] == nil
+		tierType = '[[' .. tierType .. ' Tournaments|' .. tierType .. ']]'
+			.. '[[Category:' .. tierType .. ' Tournaments]]'
+		output = tierType .. '&nbsp;(' .. output .. ')'
+	end
+
+	output = output ..
+		(hasInvalidTier and '[[Category:Pages with invalid Tier]]' or '') ..
+		(hasInvalidTierType and '[[Category:Pages with invalid Tiertype]]' or '')
+
+	Variables.varDefine('tournament_tier', tier)
+	Variables.varDefine('tournament_tiertype', tierType)
+	return output
+end
+
+function CustomLeague._getGameVersion()
+	local game = string.lower(_args.game or '')
+	return _GAME[game]
+end
+
+function CustomLeague:_currencyConversion(localPrize, currency, exchangeDate)
+	if exchangeDate and currency and currency ~= 'USD' then
+		localPrize = tonumber(localPrize)
+		if localPrize then
+			local usdPrize = mw.ext.CurrencyExchange.currencyexchange(
+				localPrize,
+				currency,
+				'USD',
+				exchangeDate
+			)
+			if type(usdPrize) == 'number' then
+				return usdPrize
+			end
+		end
+	end
+
+	return nil
+end
+
+function CustomLeague:_displayPrizeValue(value, numDigits)
+	if String.isEmpty(value) or value == 0 or value == '0' then
+		return '-'
+	end
+
+	numDigits = tonumber(numDigits or 0) or 0
+	local factor = 10^numDigits
+	value = math.floor(value * factor + 0.5) / factor
+
+	--split value into
+	--left = first digit
+	--num = all remaining digits before a possible '.'
+	--right = the '.' and all digits after it (unless they are all 0 or do not exist)
+	local left, num, right = string.match(value, '^([^%d]*%d)(%d*)(.-)$')
+	if right:len() > 0 then
+		local decimal = string.sub('0' .. right, 3)
+		right = '.' .. decimal .. string.rep('0', 2 - string.len(decimal))
+	end
+	return left .. (num:reverse():gsub('(%d%d%d)','%1,'):reverse()) .. right
+end
+
+function CustomLeague:_cleanPrizeValue(value, currency, oldHasPlus)
+	if String.isEmpty(value) then
+		return nil, nil, nil
+	end
+
+	--remove currency abbreviations
+	value = value:gsub('<abbr.*abbr>', '')
+	value = value:gsub(',', '')
+
+	--remove currency symbol
+	if currency then
+		Template.safeExpand(mw.getCurrentFrame(), 'Local currency', {currency:lower()})
+		local symbol = Variables.varDefaultMulti('localcurrencysymbol', 'localcurrencysymbolafter') or ''
+		value = value:gsub(symbol, '')
+	else
+		value = value:gsub('%$', '')
+	end
+
+	--check if it has a '+' at the end
+	local hasPlus = string.match(value, '%+$')
+	if hasPlus then
+		value = value:gsub('%+$', '')
+		hasPlus = true
+	end
+
+	return value, hasPlus or oldHasPlus
+end
+
+function CustomLeague:_makeBasedListFromArgs(base)
+	local firstArg = _args[base .. '1']
+	local foundArgs = {PageLink.makeInternalLink({}, firstArg)}
+	local index = 2
+
+	while not String.isEmpty(_args[base .. index]) do
+		local currentArg = _args['map' .. index]
+		table.insert(foundArgs, '&nbsp;• ' ..
+			tostring(CustomLeague:_createNoWrappingSpan(
+				PageLink.makeInternalLink({}, currentArg)
+			))
+		)
+		index = index + 1
+	end
+
+	return foundArgs
+end
+
+function CustomLeague:_concatArgs(base)
+	local firstArg = _args[base] or _args[base .. '1']
+	if String.isEmpty(firstArg) then
+		return nil
+	end
+	local foundArgs = {mw.ext.TeamLiquidIntegration.resolve_redirect(firstArg)}
+	local index = 2
+	while not String.isEmpty(_args[base .. index]) do
+		table.insert(foundArgs,
+			mw.ext.TeamLiquidIntegration.resolve_redirect(_args[base .. index])
+		)
+		index = index + 1
+	end
+
+	return table.concat(foundArgs, ';')
+end
+
+function CustomLeague:_createNoWrappingSpan(content)
+	local span = mw.html.create('span')
+	span:css('white-space', 'nowrap')
+		:node(content)
+	return span
+end
+
+return CustomLeague
