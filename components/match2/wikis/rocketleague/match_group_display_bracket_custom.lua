e9bb2d2529f922b44aea19888ae405bb2f581a2b
warnull <warnull@gmail.com>
2021-09-25 16:38:49 -0700

    Unshadow OpponentDisplay in rocketleague

---
+++ components/match2/wikis/rocketleague/match_group_display_bracket_custom.lua
@@ -0,0 +1,43 @@
+---
+-- @Liquipedia
+-- wiki=rocketleague
+-- page=Module:MatchGroup/Display/Bracket/Custom
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Class = require('Module:Class')
+local Lua = require('Module:Lua')
+local MatchGroupUtil = require('Module:MatchGroup/Util')
+local Table = require('Module:Table')
+
+local BracketDisplay = Lua.import('Module:MatchGroup/Display/Bracket', {requireDevIfEnabled = true})
+local CustomOpponentDisplay = Lua.import('Module:OpponentDisplay/Custom', {requireDevIfEnabled = true})
+
+local CustomBracketDisplay = {propTypes = {}}
+
+function CustomBracketDisplay.luaGet(_, args)
+	return CustomBracketDisplay.BracketContainer({
+		bracketId = args[1],
+		config = BracketDisplay.configFromArgs(args),
+	})
+end
+
+function CustomBracketDisplay.BracketContainer(props)
+	return BracketDisplay.Bracket({
+		bracket = MatchGroupUtil.fetchMatchGroup(props.bracketId),
+		config = Table.merge(props.config, {
+			OpponentEntry = CustomBracketDisplay.OpponentEntry,
+		})
+	})
+end
+
+function CustomBracketDisplay.OpponentEntry(props)
+	local opponentEntry = CustomOpponentDisplay.BracketOpponentEntry(props.opponent)
+	if props.displayType == 'bracket' then
+		opponentEntry:addScores(props.opponent)
+	end
+	return opponentEntry.root
+end
+
+return Class.export(CustomBracketDisplay)
