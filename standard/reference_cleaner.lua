e36b4128301b4efe90e76d5060efa1b30841110d
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2021-09-29 16:20:06 +0200

    Add `Module:ReferenceCleaner` (#483)
    * Add `Module:ReferenceCleaner`
    
    * Copy `Module:ReferenceCleaner` from commons
    * add git action header
    
    * space to tab
    
    * Fit convention
    
    Co-authored-by: Jasper van Riet <jvanriet@teamliquid.com>

---
+++ standard/reference_cleaner.lua
@@ -0,0 +1,41 @@
+---
+-- @Liquipedia
+-- wiki=commons
+-- page=Module:ReferenceCleaner
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Class = require('Module:Class')
+
+local ReferenceCleaner = {}
+
+function ReferenceCleaner.clean(dateWithRef)
+	if dateWithRef == nil then
+		return ''
+	end
+
+	dateWithRef = dateWithRef:gsub('-??', '-01')
+	dateWithRef = dateWithRef:gsub('-XX', '-01')
+	local correctDate = string.match(dateWithRef, '(%d+-%d+-%d+)')
+	if correctDate then
+		return correctDate
+	end
+
+	return ''
+end
+
+function ReferenceCleaner.cleanNumber(numberWithRef)
+	if numberWithRef == nil then
+		return ''
+	end
+
+	local correctNumber = string.match(numberWithRef, '(%d+)')
+	if correctNumber then
+		return correctNumber
+	end
+
+	return ''
+end
+
+return Class.export(ReferenceCleaner, {frameOnly = true})
