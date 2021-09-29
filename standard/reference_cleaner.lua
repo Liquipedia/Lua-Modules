6c44cb75fb26d6c223168116e29a75d4ac64136b
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2021-09-29 08:18:50 +0200

    space to tab

--- standard/reference_cleaner.lua
+++ standard/reference_cleaner.lua
@@ -14,31 +14,31 @@ local Class = require('Module:Class')
 local p = {}
 
 function p.clean(dateWithRef)
-    if dateWithRef == nil then
-        return ''
-    end
+	if dateWithRef == nil then
+		return ''
+	end
 
-    dateWithRef = dateWithRef:gsub('-??', '-01')
-    dateWithRef = dateWithRef:gsub('-XX', '-01')
-    local correctDate = string.match(dateWithRef, '(%d+-%d+-%d+)')
-    if correctDate then
-        return correctDate
-    end
+	dateWithRef = dateWithRef:gsub('-??', '-01')
+	dateWithRef = dateWithRef:gsub('-XX', '-01')
+	local correctDate = string.match(dateWithRef, '(%d+-%d+-%d+)')
+	if correctDate then
+		return correctDate
+	end
 
-    return ''
+	return ''
 end
 
 function p.cleanNumber(numberWithRef)
-    if numberWithRef == nil then
-        return ''
-    end
+	if numberWithRef == nil then
+		return ''
+	end
 
-    local correctNumber = string.match(numberWithRef, '(%d+)')
-    if correctNumber then
-        return correctNumber
-    end
+	local correctNumber = string.match(numberWithRef, '(%d+)')
+	if correctNumber then
+		return correctNumber
+	end
 
-    return ''
+	return ''
 end
 
 return Class.export(p, {frameOnly = true})
