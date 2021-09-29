f4df65f209149c9924ae86666e3910751bb83f2e
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2021-09-29 09:08:18 +0200

    Add `Module:Namespace`
    * Copy `Module:Namespace` from commons
    * add git action header

---
+++ standard/namespace.lua
@@ -0,0 +1,21 @@
+---
+-- @Liquipedia
+-- wiki=commons
+-- page=Module:Namespace
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+---
+-- @author Vogan for Liquipedia
+--
+
+local Class = require('Module:Class')
+
+local Namespace = {}
+
+function Namespace.isMain()
+    return mw.title.getCurrentTitle():inNamespace(0)
+end
+
+return Class.export(Namespace, {frameOnly = true})
