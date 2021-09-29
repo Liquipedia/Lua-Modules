41a75baf719faa9cc4dbf1cc5defc517e0b8d1b1
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2021-09-29 08:06:48 +0200

    Add Module:Template
    * Copy `Module:Template` from commons
    * add git action header

---
+++ standard/template.lua
@@ -0,0 +1,33 @@
+---
+-- @Liquipedia
+-- wiki=commons
+-- page=Module:Template
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+---
+-- @author Vogan for Liquipedia
+--
+
+local Template = {}
+
+function Template.safeExpand(frame, title, args, defaultTemplate)
+	local start = os.clock()
+	local result, value = pcall(frame.expandTemplate, frame, {title = title, args = args})
+	if result then
+		--mw.log('Module:Template title=' .. title)
+		--mw.log('Module:Template:safeExpand_end: ' .. (os.clock() - start))
+		return value
+	else
+		--mw.log('Module:Template title=' .. title)
+		--mw.log('Module:Template:safeExpand_end_failed: ' .. (os.clock() - start))
+		return defaultTemplate or '[[Template:' .. title .. ']]'
+	end
+end
+
+function Template.expandTemplate(frame, title, args)
+	return frame:expandTemplate {title = title, args = args}
+end
+
+return Template
