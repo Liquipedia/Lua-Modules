2d0b68bef7989dcec810eb18167ca38303da9f0e
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2021-09-30 17:12:21 +0200

    add legacy entry points
    add legacy wiki code entry points

--- standard/flags.lua
+++ standard/flags.lua
@@ -16,6 +16,14 @@ local Class = require('Module:Class')
 
 local Flags = {}
 
+--Legacy Entry points .. to be removed after switching them
+function Flags.Flag(flagName)
+	return tostring(Flags.Icon(flagName)) .. '[[Category:Pages using old Flags entry points]]'
+end
+function Flags.FlagNoLink(flagName)
+	return tostring(Flags.Icon({flag = flagName, noLink = true})) .. '[[Category:Pages using old Flags entry points]]'
+end
+
 -- Returns a flag
 --[[
 supported args are:
