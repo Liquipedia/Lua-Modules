be6879c7fdd3f28bfb2a2470c1944adddd3bc05d
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2021-10-03 12:12:28 +0200

    Update region.lua

--- standard/region.lua
+++ standard/region.lua
@@ -24,7 +24,7 @@ function Region.run(args)
 
 	--determine region from country if region is empty
 	if String.isEmpty(region) then
-		local country = Flag.countryName(args.country) or ''
+		local country = Flag.CountryName(args.country) or ''
 		region = countryToRegionData[string.lower(country)]
 		args.region = region
 		if String.isEmpty(region) then
@@ -42,7 +42,7 @@ function Region.run(args)
 	else
 		local display = ''
 		if regionValues.flag then
-			display = Flag.flag(regionValues.flag)
+			display = Flags.Icon({flag = regionValues.flag, shouldLink = true})
 			if display then
 				display = display .. '&nbsp;'
 			else
