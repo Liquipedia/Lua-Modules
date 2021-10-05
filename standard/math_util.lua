72c9e9c232ce90dd2ec285205d7fdb6676f0c33e
warnull <warnull@gmail.com>
2021-10-04 22:01:44 -0700

    Add Math.sum, Math.ilog2

--- standard/math_util.lua
+++ standard/math_util.lua
@@ -9,11 +9,37 @@
 local MathUtil = {}
 
 --[[
+Returns the base 2 log of the input number, rounded down.
+
+Example:
+MathUtil.ilog2(8) -- Returns 3
+MathUtil.ilog2(24) -- Returns 4
+]]
+function MathUtil.ilog2(x)
+	return math.floor(math.log(x) / math.log(2))
+end
+
+--[[
+Returns the sum of an array.
+
+Example:
+MathUtil.sum({3, 5, 4})
+-- Returns 12
+]]
+function MathUtil.sum(xs)
+	local sum = 0
+	for _, x in ipairs(xs) do
+		sum = sum + x
+	end
+	return sum
+end
+
+--[[
 Computes the partial sums of an array. The result is an array one longer than
 the input.
 
 Example:
-math.partialSums({3, 5, 4})
+MathUtil.partialSums({3, 5, 4})
 -- Returns {0, 3, 8, 12}
 ]]
 function MathUtil.partialSums(set)
