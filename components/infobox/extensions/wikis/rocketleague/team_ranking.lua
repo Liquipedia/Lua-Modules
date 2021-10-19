431855ca6b2b11fd7c5687c152481c341a5dba64
Mamdouh Waleed <mamdouh.waleed.96@gmail.com>
2021-10-19 18:48:49 +0200

    Update components/infobox/extensions/wikis/rocketleague/team_ranking.lua

--- components/infobox/extensions/wikis/rocketleague/team_ranking.lua
+++ components/infobox/extensions/wikis/rocketleague/team_ranking.lua
@@ -19,7 +19,7 @@ local Class = require('Module:Class')
 -- @treturn string|nil rankingString the string representation of the team's ranking or nil if the team doesn't have RLCS Points
 function TeamRanking.get(args)
 	if not args['ranking'] then
-		error('Please provide a ranking name!')
+		error('Please provide a ranking name')
 	end
 	if not args['team'] then
 		error('Please provide a team name!')
