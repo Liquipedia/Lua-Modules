50d4ff89d8508ae90017f3f63fbdad0e680f12c1
Jasper van Riet <JaspervanRiet@users.noreply.github.com>
2021-10-22 14:42:02 +0200

    Setup simple test for `Match.splitRecordsByType()` (#619)

---
+++ components/match2/test/match_test.lua
@@ -0,0 +1,19 @@
+---
+-- @Liquipedia
+-- wiki=commons
+-- page=Module:Match/testcases
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Match = require('Module:Match')
+local MatchTestConfig = require('Module:Match/testcases/config')
+local ScribuntoUnit = require('Module:ScribuntoUnit')
+local suite = ScribuntoUnit:new()
+
+function suite:testSplitRecordsByType()
+	local result = Match.splitRecordsByType(MatchTestConfig.EXAMPLE_MATCH)
+	self:assertDeepEquals(MatchTestConfig.EXPECTED_OUTPUT_AFTER_SPLIT, result)
+end
+
+return suite
