a58491945df4c8bbf6aa0f9dc594e6fcb7c05a23
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2021-11-04 08:44:41 +0100

    Update infobox_person_player.lua

--- components/infobox/wikis/starcraft2/infobox_person_player.lua
+++ components/infobox/wikis/starcraft2/infobox_person_player.lua
@@ -166,6 +166,7 @@ function CustomInjector:addCustomCells(widgets)
 
 	local currentYearEarnings = _earningsGlobal[tostring(_CURRENT_YEAR)]
 	if currentYearEarnings then
+		currentYearEarnings = math.floor( (currentYearEarnings) * 100 + 0.5) / 100
 		currentYearEarnings = '$' .. mw.language.new('en'):formatNum(currentYearEarnings)
 	end
 
