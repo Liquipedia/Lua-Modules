0988ada5780780b5ee39e8bab85e81b7d16df58f
Jasper van Riet <jvanriet@teamliquid.com>
2021-11-05 13:42:16 +0100

    Fix line length

--- components/infobox/extensions/test/metadata_generator_test.lua
+++ components/infobox/extensions/test/metadata_generator_test.lua
@@ -13,8 +13,8 @@ local ScribuntoUnit = require('Module:ScribuntoUnit')
 local suite = ScribuntoUnit:new()
 
 local _EXPECTED_RESULT = 'Intel Extreme Masters XVI - Cologne is an offline German tournament organized by ESL.' ..
-	' This [[Template:TierDisplay]] tournament took place from Jul 06 to Jul 18 2021 featuring 24 teams competing over a total prize' ..
-	' pool of $1,000,000 USD.'
+	' This [[Template:TierDisplay]] tournament took place from Jul 06 to Jul 18 2021 featuring 24 teams competing' ..
+	' over a total prize pool of $1,000,000 USD.'
 
 function suite:testGenerator()
 	local args = Arguments.getArgs(mw.getCurrentFrame())
