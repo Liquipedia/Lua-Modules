04e8da7fdde853dcf57f2382f7412f7d614012a8
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2021-11-10 11:09:44 +0100

    Update infobox_league_custom.lua

--- components/infobox/wikis/starcraft2/infobox_league_custom.lua
+++ components/infobox/wikis/starcraft2/infobox_league_custom.lua
@@ -222,6 +222,7 @@ function CustomLeague:_createTierDisplay()
 
 	Variables.varDefine('tournament_tier', tier)
 	Variables.varDefine('tournament_tiertype', tierType)
+	--overwrite wiki var `tournament_liquipediatiertype` to allow `args.tiertype` as alias entry point for tiertype
 	Variables.varDefine('tournament_liquipediatiertype', tierType)
 	return output
 end
