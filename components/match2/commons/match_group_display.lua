132086b438594359cf41549a85828795a8ad9f01
Rikard Blixt <rikardblixt@gmail.com>
2021-11-13 21:19:27 +0100

    If a bracket contain only 1 match, matchId is no longer required but optional

--- components/match2/commons/match_group_display.lua
+++ components/match2/commons/match_group_display.lua
@@ -110,14 +110,18 @@ function MatchGroupDisplay.MatchByMatchId(args)
 	args.id = bracketId
 	args[1] = bracketId
 	assert(bracketId, 'Missing bracket ID')
-	assert(args.matchid, 'Missing match ID')
-	local matchIdLength = string.len(args.matchid)
-	args.matchid = string.rep('0', 4 - matchIdLength) .. args.matchid
 
 	local matches = MatchGroupUtil.fetchMatches(bracketId)
 	assert(#matches ~= 0, 'No data found for bracketId=' .. bracketId)
-	Table.filter(matches, function(match) return MatchGroupUtil.splitMatchId(match.matchId) == args.matchid end)
-	assert(#matches ~= 0, 'Match matchId=' .. args.matchid .. ' not found')
+
+	if (#matches > 1) then
+		assert(args.matchid, 'Missing match ID')
+		local matchIdLength = string.len(args.matchid)
+		args.matchid = string.rep('0', 4 - matchIdLength) .. args.matchid
+
+		matches = Table.filter(matches, function(match) return MatchGroupUtil.splitMatchId(match.matchId) == args.matchid end)
+		assert(#matches ~= 0, 'Match matchId=' .. args.matchid .. ' not found')
+	end
 
 	local SingleMatchDisplay = Lua.import('Module:MatchGroup/Display/SingleMatch', {requireDevIfEnabled = true})
 	local config = SingleMatchDisplay.configFromArgs(args)
