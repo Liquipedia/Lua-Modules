a636d6885f18e5e09bab009b304b87becaaa851e
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2021-11-16 15:59:22 +0100

    Update warning_box.lua

--- standard/warning_box.lua
+++ standard/warning_box.lua
@@ -11,8 +11,8 @@ local WarningBox = {}
 local Class = require('Module:Class')
 
 function WarningBox.display(text)
-	local div = mw.html.create('div'):addClass('show-when-logged-in navigation-not-searchable ambox-wrapper')
-		:addClass('ambox wiki-bordercolor-dark wiki-backgroundcolor-light ambox-red')
+	local div = mw.html.create('div'):addClass('show-when-logged-in navigation-not-searchable ambox-wrapper'
+		.. 'ambox wiki-bordercolor-dark wiki-backgroundcolor-light ambox-red')
 	local tbl = mw.html.create('table')
 	tbl:tag('tr')
 		:tag('td'):addClass('ambox-image'):wikitext('[[File:Emblem-important.svg|40px|link=]]'):done()
