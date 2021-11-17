eff279358851de8abe7bcb9103dabfd3fa21a588
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2021-11-17 10:52:30 +0100

    RL Earnings/Custom

---
+++ components/earnings/wikis/rocketleague/earnings_custom.lua
@@ -0,0 +1,29 @@
+---
+-- @Liquipedia
+-- wiki=rocketleague
+-- page=Module:Earnings/Custom
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Class = require('Module:Class')
+
+local CustomEarnings = require('Module:Earnings')
+
+local _DEFAULT_NUMBER_OF_PLAYERS_IN_TEAM = 3
+
+function CustomEarnings.divisionFactor(mode)
+    if mode == '4v4' then
+        return 4
+    elseif mode == '3v3' or mode == 'teamaward' then
+        return 3
+    elseif mode == '2v2' then
+        return 2
+    elseif mode == '1v1' or mode == 'individual' then
+        return 1
+    end
+
+    return _DEFAULT_NUMBER_OF_PLAYERS_IN_TEAM
+end
+
+return Class.export(CustomEarnings)
