50ccb832438cc14eb324e0ff1a952a51af1b86bc
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2021-11-18 13:25:58 +0100

    halo `/Custom`

--- components/infobox/wikis/halo/infobox_team_custom.lua
+++ components/infobox/wikis/halo/infobox_team_custom.lua
@@ -7,8 +7,6 @@
 --
 
 local Team = require('Module:Infobox/Team')
-local Earnings = require('Module:Earnings')
-local Variables = require('Module:Variables')
 local Flags = require('Module:Flags')
 local Class = require('Module:Class')
 local Injector = require('Module:Infobox/Widget/Injector')
@@ -25,7 +23,6 @@ local _START_YEAR = 2015
 local _region
 
 local CustomInjector = Class.new(Injector)
-local Language = mw.language.new('en')
 
 local _team
 
@@ -38,23 +35,7 @@ function CustomTeam.run(frame)
 end
 
 function CustomInjector:parse(id, widgets)
-	if id == 'earnings' then
-		local earnings = Earnings.calculateForTeam({team = _team.pagename or _team.name})
-		Variables.varDefine('earnings', earnings)
-		if earnings == 0 then
-			earnings = nil
-		else
-			earnings = '$' .. Language:formatNum(earnings)
-		end
-		return {
-			Cell{
-				name = 'Earnings',
-				content = {
-					earnings
-				}
-			}
-		}
-	elseif id == 'region' then
+	if id == 'region' then
 		return {
 			Cell{name = 'Region', content = {CustomTeam:_createRegion(_team.args.region, _team.args.location)}}
 		}
@@ -71,18 +52,10 @@ function CustomTeam:_createRegion(region, location)
 end
 
 function CustomTeam:addToLpdb(lpdbData, args)
-	lpdbData.earnings = Variables.varDefault('earnings', 0)
 	if not String.isEmpty(args.teamcardimage) then
 		lpdbData.logo = 'File:' .. args.teamcardimage
 	elseif not String.isEmpty(args.image) then
 		lpdbData.logo = 'File:' .. args.image
-	end
-
-	lpdbData.extradata = {}
-	for year = _START_YEAR, _CURRENT_YEAR do
-		local id = args.id or _team.pagename
-		local earningsInYear = Template.safeExpand(mw.getCurrentFrame(), 'Total earnings of', {year = year, id})
-		lpdbData.extradata['earningsin' .. year] = (earningsInYear or ''):gsub(',', ''):gsub('$', '')
 	end
 
 	lpdbData.location = CustomTeam:_getStandardLocationValue(_team.args.location)
