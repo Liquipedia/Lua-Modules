c58e7977c75ef6b001ca1d770920bc03ec0405af
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2021-11-18 13:39:26 +0100

    Update earnings.lua

--- components/earnings/wikis/rocketleague/earnings.lua
+++ components/earnings/wikis/rocketleague/earnings.lua
@@ -10,21 +10,7 @@ local Class = require('Module:Class')
 
 local CustomEarnings = require('Module:Earnings/Base')
 
-local _DEFAULT_NUMBER_OF_PLAYERS_IN_TEAM = 3
-
-function CustomEarnings.divisionFactor(mode)
-    if mode == '4v4' then
-        return 4
-    elseif mode == '3v3' or mode == 'teamaward' then
-        return 3
-    elseif mode == '2v2' then
-        return 2
-    elseif mode == '1v1' or mode == 'individual' then
-        return 1
-    end
-
-    return _DEFAULT_NUMBER_OF_PLAYERS_IN_TEAM
-end
+Earnings.defaultNumberOfPlayersInTeam = 3
 
 -- Legacy entry points
 function CustomEarnings.calc_player(input)
