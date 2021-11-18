43662cf6a1e07facee378e0e13db0f144ec354aa
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2021-11-18 13:28:14 +0100

    RL `/Custom`

--- components/infobox/wikis/rocketleague/infobox_team_custom.lua
+++ components/infobox/wikis/rocketleague/infobox_team_custom.lua
@@ -7,7 +7,6 @@
 --
 
 local Team = require('Module:Infobox/Team')
-local Earnings = require('Module:Earnings')
 local Variables = require('Module:Variables')
 local Class = require('Module:Class')
 local Injector = require('Module:Infobox/Widget/Injector')
@@ -22,7 +21,6 @@ local _CURRENT_YEAR = os.date('%Y')
 local _START_YEAR = 2015
 
 local CustomInjector = Class.new(Injector)
-local Language = mw.language.new('en')
 
 local _team
 
@@ -62,44 +60,15 @@ function CustomInjector:wrapErrorMessage(text)
 	return outText
 end
 
-function CustomInjector:parse(id, widgets)
-	if id == 'earnings' then
-		local earnings = Earnings.calculateForTeam({team = _team.pagename or _team.name})
-		Variables.varDefine('earnings', earnings)
-		if earnings == 0 then
-			earnings = nil
-		else
-			earnings = '$' .. Language:formatNum(earnings)
-		end
-		return {
-			Cell{
-				name = 'Earnings',
-				content = {
-					earnings
-				}
-			}
-		}
-	end
-	return widgets
-end
-
 function CustomTeam:addToLpdb(lpdbData, args)
-	lpdbData.earnings = Variables.varDefault('earnings', 0)
 	if not String.isEmpty(args.teamcardimage) then
 		lpdbData.logo = 'File:' .. args.teamcardimage
 	elseif not String.isEmpty(args.image) then
 		lpdbData.logo = 'File:' .. args.image
 	end
 
-	lpdbData.extradata = {
-		rating = Variables.varDefault('rating'),
-		tier = string.lower(args.tier or '')
-	}
-	for year = _START_YEAR, _CURRENT_YEAR do
-		local id = args.id or _team.pagename
-		local earningsInYear = Template.safeExpand(mw.getCurrentFrame(), 'Total earnings of', {year = year, id})
-		lpdbData.extradata['earningsin' .. year] = (earningsInYear or ''):gsub(',', ''):gsub('$', '')
-	end
+	lpdbData.extradata.rating = Variables.varDefault('rating'),
+	lpdbData.extradata.tier = string.lower(args.tier or '')
 
 	return lpdbData
 end
