588515251eb837c27aace26b8a71c0fc6d833623
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2021-11-19 11:31:20 +0100

    Remove custom css
    Remove custom css that is set via css sheet since by now the cache for the old sheet should have refreshed

--- components/match2/commons/starcraft_starcraft2/match_summary_starcraft.lua
+++ components/match2/commons/starcraft_starcraft2/match_summary_starcraft.lua
@@ -101,7 +101,6 @@ function StarcraftMatchSummary.Header(props)
 		if showScore then
 			display:addClass('brkts-popup-header-opponent-' .. side)
 				:addClass('brkts-popup-sc-header-opponent-with-score')
-				:css('width', '50%')--temp needed until css is refreshed in cache
 
 			local scoreDisplay = StarcraftOpponentDisplay.BlockScore{
 				isWinner = opponent.placement == 1 or opponent.advances,
