e45eda61688fa1c93b4fd9e2b71d07c28135c385
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2021-11-25 20:52:11 +0100

    Create info.lua

---
+++ standard/Info/wikis/starcraft2/info.lua
@@ -0,0 +1,14 @@
+---
+-- @Liquipedia
+-- wiki=starcraft2
+-- page=Module:Info
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+return {
+	startYear = 2010,
+	wikiName = 'starcraft2',
+	name = 'StarCraft II',
+	defaultTeamLogo = 'StarCraft_2_Default_logo.png',
+}
