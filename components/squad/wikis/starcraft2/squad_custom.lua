c67c92a2cf93c9e5d2737aad01ea0a611c76e25f
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2021-11-26 11:01:02 +0100

    Update squad_custom.lua

--- components/squad/wikis/starcraft2/squad_custom.lua
+++ components/squad/wikis/starcraft2/squad_custom.lua
@@ -54,7 +54,7 @@ function CustomSquad.run(frame)
 	while args['p' .. index] or args[index] do
 
 		local player = Json.parseIfString(args['p' .. index] or args[index])
-		player.id = player.id or player[1] or player.name
+		player.id = player.id or player[1]
 		player.race = string.lower(player.race)
 		player.race = CleanRace[player.race] or player.race
 		local row = SquadRow(frame, player.role, {useTemplatesForSpecialTeams = true})
