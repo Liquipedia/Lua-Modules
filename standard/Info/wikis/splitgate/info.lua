4d2bc639c6f6679007546c23fbe771200f4f9ee7
Cameron <58013431+FenrirWulf@users.noreply.github.com>
2021-11-26 11:00:36 -0800

    Create info.lua
    Splitgate. Image is the original logo. Switch needs adding

---
+++ standard/Info/wikis/splitgate/info.lua
@@ -0,0 +1,15 @@
+---
+-- @Liquipedia
+-- wiki=splitgate
+-- page=Module:Info
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+return {
+	startYear = 2019,
+	wikiName = 'splitgate',
+	name = 'Splitgate',
+	defaultTeamLogo = 'Splitgate allmode.png',
+	defaultTeamLogoDark = 'Splitgate allmode.png',
+}
