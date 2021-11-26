307c4d22fa242c5f710d8bbac2530f5f064a4924
Cameron <58013431+FenrirWulf@users.noreply.github.com>
2021-11-26 08:54:02 -0800

    Create info.lua
    Rainbow six

---
+++ standard/Info/wikis/rainbowsix/info.lua
@@ -0,0 +1,15 @@
+---
+-- @Liquipedia
+-- wiki=rainbowsix
+-- page=Module:Info
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+return {
+	startYear = 2015,
+	wikiName = 'rainbowsix',
+	name = 'Rainbow Six',
+	defaultTeamLogo = 'Rainbow Six Siege Logo lightmode.png',
+	defaultTeamLogoDark = 'Rainbow Six Siege Logo darkmode.png',
+}
