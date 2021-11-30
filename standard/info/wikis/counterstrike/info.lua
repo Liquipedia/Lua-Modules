52fab7f68009e027219dc50e7cc4c28abd34ee05
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2021-11-30 10:00:54 +0100

    add some more wikis

---
+++ standard/info/wikis/counterstrike/info.lua
@@ -0,0 +1,15 @@
+---
+-- @Liquipedia
+-- wiki=counterstrike
+-- page=Module:Info
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+return {
+	startYear = 2000,
+	wikiName = 'counterstrike',
+	name = 'Counter-Strike',
+	defaultTeamLogo = 'CSGO default lightmode.png',
+	defaultTeamLogoDark = 'CSGO default darkmode.png',
+}
---
+++ standard/info/wikis/fighters/info.lua
@@ -0,0 +1,15 @@
+---
+-- @Liquipedia
+-- wiki=fighters
+-- page=Module:Info
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+return {
+	startYear = 1996,
+	wikiName = 'fighters',
+	name = 'Fighting Games',
+	defaultTeamLogo = 'Fistlogo_std.png',
+	defaultTeamLogoDark = 'Fistlogo_std.png',
+}
