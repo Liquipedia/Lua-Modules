755e5b9525ee99b75927ed16dc9f0064085df7f0
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2021-11-30 20:46:07 +0100

    Add `Module:VodLink`

---
+++ standard/vod_link.lua
@@ -0,0 +1,49 @@
+---
+-- @Liquipedia
+-- wiki=commons
+-- page=Module:VodLink
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Class = require('Module:Class')
+local Logic = require('Module:Logic')
+
+local VodLink = {}
+
+function VodLink.display(args)
+	if Logic.readBool(args.novod) then
+		return mw.html.create('span')
+			:addClass('plainlinks')
+			:attr('title', 'Help Liquipedia find this VOD')
+			:wikitext('[[File:NoVod.png|link=]]')
+	end
+
+	local title
+	local fileName = 'VOD Icon'
+	if Logic.isNumeric(args.gamenum) then
+		title = 'Watch Game ' .. args.gamenum
+		if tonumber(args.gamenum) <= 11 then
+			fileName = fileName .. args.gamenum
+		end
+	else
+		title = args.htext or 'Watch VOD'
+	end
+	fileName = fileName .. '.png'
+
+	local link = args.vod or ''
+	--question if we actually need the tlpd stuff
+	--atm most wikis have it, but it seems very pointless except for sc/sc2
+	if args.source == 'tlpd' or args.source == 'tlpd-kr' then
+		link = 'https://www.tl.net/tlpd/sc2-korean/games/' .. link .. '/vod'
+	elseif args.source == 'tlpd-int' then
+		link = 'https://www.tl.net/tlpd/sc2-international/games/' .. link .. '/vod'
+	end
+
+	return mw.html.create('span')
+		:addClass('plainlinks vodlink')
+		:attr('title', title)
+		:wikitext('[[File:' .. fileName .. '|link=' .. link .. ']]')
+end
+
+return Class.export(VodLink, {frameOnly = true})
