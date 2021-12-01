36b4c2b18ebe794c8a8f08ba5014c2069b920354
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2021-12-01 09:21:22 +0100

    Remove Legacy Date Vars for SC2 (#803)
    * Remove Legacy Date Vars for Infobox League/hidden
    
    * Remove Legacy Date Vars for Infobox League

--- components/infobox/wikis/starcraft2/infobox_league_hidden.lua
+++ components/infobox/wikis/starcraft2/infobox_league_hidden.lua
@@ -200,16 +200,6 @@ function HiddenInfoboxLeague._definePageVariables()
 	local edate = HiddenInfoboxLeague._cleanDate(_args.edate) or HiddenInfoboxLeague._cleanDate(_args.date)
 	Variables.varDefine('tournament_startdate', sdate)
 	Variables.varDefine('tournament_enddate', edate)
-	--Legacy date vars
-	Variables.varDefine('infobox_date', edate)
-	Variables.varDefine('infobox_sdate', sdate)
-	Variables.varDefine('infobox_edate', edate)
-	Variables.varDefine('date', edate)
-	Variables.varDefine('sdate', sdate)
-	Variables.varDefine('edate', edate)
-	Variables.varDefine('tournament_date', edate)
-	Variables.varDefine('formatted_tournament_date', sdate)
-	Variables.varDefine('formatted_tournament_edate', edate)
 
 	--Legacy vars
 	Variables.varDefine('tournament_tier', tier)
