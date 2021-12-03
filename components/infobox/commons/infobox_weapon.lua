2424ef4fc244fd7977291be94cf7e9a9bfd95539
Cameron <58013431+FenrirWulf@users.noreply.github.com>
2021-12-03 06:20:18 -0800

    Update infobox_weapon.lua

--- components/infobox/commons/infobox_weapon.lua
+++ components/infobox/commons/infobox_weapon.lua
@@ -20,86 +20,86 @@ local Customizable = Widgets.Customizable
 local Weapon = Class.new(BasicInfobox)
 
 function Weapon.run(frame)
-    local weapon = Weapon(frame)
-    return weapon:createInfobox()
+	local weapon = Weapon(frame)
+	return weapon:createInfobox()
 end
 
 function Weapon:createInfobox()
-    local infobox = self.infobox
-    local args = self.args
+	local infobox = self.infobox
+	local args = self.args
 
-    local widgets = {
-        Header{
-            name = self:nameDisplay(args),
-            image = args.image,
-            imageDefault = args.default,
-            imageDark = args.imagedark or args.imagedarkmode,
-            imageDefaultDark = args.defaultdark or args.defaultdarkmode,
-        },
-        Center{content = {args.caption}},
-        Title{name = (args.informationType or 'Weapon') .. ' Information'},
-		Cell{
-			name = 'Class',
-			content = self:getAllArgsForBase(args, 'class', {makeLink = true}),
-		},
-		Cell{
-			name = 'Origin',
-			content = {self:_createLocation(args.origin)},
-		},
-		Cell{name = 'Price', content = {args.price}},
-		Cell{name = 'Kill Award', content = {args.killaward}},
-		Cell{name = 'Base Damage', content = {args.damage}},
-		Cell{name = 'Magazine Size', content = {args.magsize}},
-		Cell{name = 'Ammo Capacity', content = {args.ammocap}},
-		Cell{name = 'Reload Speed', content = {args.reloadspeed}},
-		Cell{name = 'Rate of Fire', content = {args.rateoffire}},
-		Cell{name = 'Firing Mode', content = {args.firemode}},
-		Customizable{
-			id = 'side',
-			children = {
-				Cell{name = 'Side', content = {args.side}},
+	local widgets = {
+	Header{
+		name = self:nameDisplay(args),
+		image = args.image,
+		imageDefault = args.default,
+		imageDark = args.imagedark or args.imagedarkmode,
+		imageDefaultDark = args.defaultdark or args.defaultdarkmode,
+	},
+	Center{content = {args.caption}},
+	Title{name = (args.informationType or 'Weapon') .. ' Information'},
+	Cell{
+		name = 'Class',
+		content = self:getAllArgsForBase(args, 'class', {makeLink = true}),
+	},
+	Cell{
+		name = 'Origin',
+		content = {self:_createLocation(args.origin)},
+	},
+	Cell{name = 'Price', content = {args.price}},
+	Cell{name = 'Kill Award', content = {args.killaward}},
+	Cell{name = 'Base Damage', content = {args.damage}},
+	Cell{name = 'Magazine Size', content = {args.magsize}},
+	Cell{name = 'Ammo Capacity', content = {args.ammocap}},
+	Cell{name = 'Reload Speed', content = {args.reloadspeed}},
+	Cell{name = 'Rate of Fire', content = {args.rateoffire}},
+	Cell{name = 'Firing Mode', content = {args.firemode}},
+	Customizable{
+		id = 'side',
+		children = {
+			Cell{name = 'Side', content = {args.side}},
+		}
+	},
+	Customizable{
+		id = 'user',
+		children = {
+			Cell{
+				name = 'User(s)',
+				content = self:getAllArgsForBase(args, 'user', {makeLink = true}),
 			}
-		},
-		Customizable{
-            id = 'user',
-            children = {
-				Cell{
-					name = 'User(s)',
-					content = self:getAllArgsForBase(args, 'user', {makeLink = true}),
-				}
-            }
-        },
-        Customizable{id = 'custom', children = {}},
-        Center{content = {args.footnotes}},
-    }
+		}
+	},
+	Customizable{id = 'custom', children = {}},
+	Center{content = {args.footnotes}},
+	}
 
-    infobox:categories('Weapons')
-    infobox:categories(unpack(self:getWikiCategories(args)))
+	infobox:categories('Weapons')
+	infobox:categories(unpack(self:getWikiCategories(args)))
 
-    local builtInfobox = infobox:widgetInjector(self:createWidgetInjector()):build(widgets)
+	local builtInfobox = infobox:widgetInjector(self:createWidgetInjector()):build(widgets)
 
 	if Namespace.isMain() then
-        self:setLpdbData(args)
-    end
+		self:setLpdbData(args)
+	end
 
-    return builtInfobox
+	return builtInfobox
 end
 
 function Weapon:_createLocation(location)
 	if location == nil then
-		return ''
-	end
+	return ''
+end
 
-	return Flags.Icon({flag = location, shouldLink = true}) .. '&nbsp;' ..
-				'[[:Category:' .. location .. '|' .. location .. ']]'
+return Flags.Icon({flag = location, shouldLink = true}) .. '&nbsp;' ..
+	'[[:Category:' .. location .. '|' .. location .. ']]'
 end
 
 function Weapon:getWikiCategories(args)
-    return {}
+	return {}
 end
 
 function Weapon:nameDisplay(args)
-    return args.name
+	return args.name
 end
 
 function Weapon:setLpdbData(args)
