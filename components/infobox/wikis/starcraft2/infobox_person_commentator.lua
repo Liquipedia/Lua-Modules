02ed191086b36de708b2a4266f89cb69d87b8662
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2021-12-06 09:45:22 +0100

    Update infobox_person_commentator.lua

--- components/infobox/wikis/starcraft2/infobox_person_commentator.lua
+++ components/infobox/wikis/starcraft2/infobox_person_commentator.lua
@@ -337,10 +337,6 @@ function CustomCommentator:adjustLPDB(lpdbData, _, personType)
 		extradata.factionhistorical = true
 	end
 
-	for key, item in pairs(_earningsGlobal or {}) do
-		extradata['earningsin' .. key] = item
-	end
-
 	lpdbData.extradata = extradata
 
 	return lpdbData
@@ -350,7 +346,7 @@ function CustomCommentator:calculateEarnings()
 	local earningsTotal
 	earningsTotal, _earningsGlobal = CustomCommentator._getEarningsMedalsData(self.pagename)
 	earningsTotal = Math.round{earningsTotal, 2}
-	return earningsTotal
+	return earningsTotal, _earningsGlobal
 end
 
 function CustomCommentator._getLPDBrecursive(cond, query, queryType)
