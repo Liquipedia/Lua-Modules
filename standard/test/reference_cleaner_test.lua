edc5b68b5381a4081faded38fa648c13e1bf807e
Jasper van Riet <jvanriet@teamliquid.com>
2021-12-15 10:19:26 +0100

    Add test

---
+++ standard/test/reference_cleaner_test.lua
@@ -0,0 +1,11 @@
+local ReferenceCleaner = require('Module:ReferenceCleaner')
+local ScribuntoUnit = require('Module:ScribuntoUnit')
+
+local suite = ScribuntoUnit:new()
+
+function suite:testClass()
+	self:assertEquals(ReferenceCleaner.clean('2021-07-05'), '2021-07-05')
+	self:assertEquals(ReferenceCleaner.clean('2011-05-??'), '2011-05-01')
+end
+
+return suite
