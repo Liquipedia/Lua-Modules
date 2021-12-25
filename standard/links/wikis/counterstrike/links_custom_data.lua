307b48949640d32024a3d01c5d85dde46ce119d8
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2021-12-25 22:02:55 +0100

    Add cs Links/CustomData

---
+++ standard/links/wikis/counterstrike/links_custom_data.lua
@@ -0,0 +1,17 @@
+---
+-- @Liquipedia
+-- wiki=counterstrike
+-- page=Module:Links/CustomData
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+return {
+	prefixes = {
+		faceit = {
+			'https://www.faceit.com/en/csgo/tournament/',
+			team = 'https://www.faceit.com/teams/',
+			player = 'https://www.faceit.com/players/',
+		}
+	},
+}
