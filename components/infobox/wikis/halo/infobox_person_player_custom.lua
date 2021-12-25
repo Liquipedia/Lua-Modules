e71b57809ef799804016476c9e5aa67b4247bdc3
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2021-12-25 16:30:01 +0100

    Update and rename infobox_person_player.lua to infobox_person_player_custom.lua

--- components/infobox/wikis/halo/infobox_person_player_custom.lua
+++ components/infobox/wikis/halo/infobox_person_player_custom.lua
@@ -1,7 +1,7 @@
 ---
 -- @Liquipedia
 -- wiki=halo
--- page=Module:Infobox/Person/Player
+-- page=Module:Infobox/Person/Player/Custom
 --
 -- Please see https://github.com/Liquipedia/Lua-Modules to contribute
 --
