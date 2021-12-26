c160ffd283a9b8f714f055d6ee02f565e6d43fa1
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2021-12-26 15:05:45 +0100

    Update matches_table.lua

--- components/match2/commons/matches_table.lua
+++ components/match2/commons/matches_table.lua
@@ -129,7 +129,7 @@ function MatchesTable._header()
 end
 
 function MatchesTable._row(match)
-	local matchHeader = match.extradata.matchsection or match.match2bracketdata.header
+	local matchHeader = match.match2bracketdata.header or match.extradata.matchsection
 	if String.isEmpty(matchHeader) then
 		--if we are in the same matchGroup just use the previous _matchHeader
 		if _currentId == match.match2bracketid then
