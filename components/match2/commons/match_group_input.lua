2ec804502ae80901876f0e8868c0d4c4c68514aa
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2021-12-28 07:55:32 +0100

    Update match_group_input.lua

--- components/match2/commons/match_group_input.lua
+++ components/match2/commons/match_group_input.lua
@@ -52,7 +52,7 @@ function MatchGroupInput.readMatchlist(bracketId, args)
 		bracketData.header = args['M' .. matchIndex .. 'header'] or bracketData.header
 		bracketData.bracketindex = Variables.varDefault('match2bracketindex', 0)
 		bracketData.sectionheader = sectionHeader
-		bracketData.dateheader = Logic.readBool(match.dateheader)
+		bracketData.dateheader = Logic.readBool(match.dateheader) or nil
 	end
 
 	return matches
