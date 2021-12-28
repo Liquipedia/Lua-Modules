151a366550a37abb2b70cc1273a3d6636c0e7cbf
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2021-12-28 08:27:30 +0100

    arenaofvalor

--- standard/info/wikis/arenaofvalor/info.lua
+++ standard/info/wikis/arenaofvalor/info.lua
@@ -6,10 +6,20 @@
 -- Please see https://github.com/Liquipedia/Lua-Modules to contribute
 --
 
+-- This wiki has 2 games
+--	Arena of Valor
+--	Honor of Kings
+
 return {
 	startYear = 2015,
 	wikiName = 'arenaofvalor',
 	name = 'Arena of Valor',
-	defaultTeamLogo = 'Arena of ValorLogo.png',
-	defaultTeamLogoDark = 'Arena of ValorLogo.png',
+	defaultTeamLogo = {
+		{logo = 'Arena of ValorLogo.png', game = 'aov'}, --Arena of Valor
+		{logo = 'Honor of Kings 2018-12-24 Logo.png', game = 'hok'}, --Honor of Kings
+	},
+	defaultTeamLogoDark = {
+		{logo = 'Arena of ValorLogo.png', game = 'aov'}, --Arena of Valor
+		{logo = 'Honor of Kings 2018-12-24 Logo.png', game = 'hok'}, --Honor of Kings
+	},
 }
