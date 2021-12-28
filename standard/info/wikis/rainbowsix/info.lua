fcd202198ef78249b30224ed4abb5dafa4790215
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2021-12-28 08:16:46 +0100

    R6 legacy default logo

--- standard/info/wikis/rainbowsix/info.lua
+++ standard/info/wikis/rainbowsix/info.lua
@@ -10,6 +10,18 @@ return {
 	startYear = 2015,
 	wikiName = 'rainbowsix',
 	name = 'Rainbow Six ',
-	defaultTeamLogo = 'Rainbow Six Siege Logo lightmode.png',
-	defaultTeamLogoDark = 'Rainbow Six Siege Logo darkmode.png',
+	defaultTeamLogo = {
+		-- order new to old
+		{logo = 'Rainbow Six Siege Logo lightmode.png', game = 'siege'},
+		{logo = 'Rainbow_Six Vegas Logo lightmode.png', game = 'vegas'},
+	},
+	teamLogoSwitchDates = {
+		-- order new to old
+		'2015-11-26'
+	},
+	defaultTeamLogoDark = {
+		-- order new to old
+		{logo = 'Rainbow Six Siege Logo darkmode.png', game = 'siege'},
+		{logo = 'Rainbow_Six Vegas Logo darkmode.png', game = 'vegas'},
+	},
 }
