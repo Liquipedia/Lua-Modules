a14202c360dff2c406c5998aa5b1567615d4e30a
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2021-12-28 22:24:09 +0100

    Mass-Add `Module:Info` (#795)
    * Create info.lua
    
    * add dark logo
    
    * Rename standard/Info/wikis/starcraft2/info.lua to standard/info/wikis/starcraft2/info.lua
    
    * mass upload auto generated files
    
    * add missing `'` in all files
    
    * Create generate_module_info.ps1
    
    * add some more wikis
    
    * add another one
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload

---
+++ standard/info/wikis/teamfighttactics/info.lua
@@ -0,0 +1,15 @@
+---
+-- @Liquipedia
+-- wiki=teamfighttactics
+-- page=Module:Info
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+return {
+	startYear = 2019,
+	wikiName = 'teamfighttactics',
+	name = 'Teamfight Tactics',
+	defaultTeamLogo = 'Teamfight Tactics Default.png',
+	defaultTeamLogoDark = 'Teamfight Tactics Default.png',
+}
