2ec9c41f2f30a1b8c254d622124836fc83263682
Cameron <58013431+FenrirWulf@users.noreply.github.com>
2021-12-30 15:35:07 -0800

    Create Brkts wiki specific Sideswipe

---
+++ components/match2/wikis/sideswipe/brkts_wiki_specific.lua
@@ -0,0 +1,22 @@
+---
+-- @Liquipedia
+-- wiki=sideswipe
+-- page=Module:Brkts/WikiSpecific
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Lua = require('Module:Lua')
+local Table = require('Module:Table')
+
+local WikiSpecific = Table.copy(Lua.import('Module:Brkts/WikiSpecific/Base', {requireDevIfEnabled = true}))
+
+function WikiSpecific.getMatchGroupContainer(matchGroupType)
+	return matchGroupType == 'matchlist'
+		and Lua.import('Module:MatchGroup/Display/Matchlist', {requireDevIfEnabled = true}).MatchlistContainer
+		or Lua.import('Module:MatchGroup/Display/Bracket/Custom', {requireDevIfEnabled = true}).BracketContainer
+end
+
+WikiSpecific.defaultIcon = 'Sideswipe allmode.png'
+
+return WikiSpecific
