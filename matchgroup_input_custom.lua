4cbf9e95e3dc52c2b5e273f9f113ae23cee75c9b
Hesketh2 <88981446+Hesketh2@users.noreply.github.com>
2022-01-01 01:35:21 +0700

    10th update
    add an underscore the locals
    confiscated input to just the single letter (one format)

--- matchgroup_input_custom.lua
+++ matchgroup_input_custom.lua
@@ -16,11 +16,11 @@ local ChampionNames = mw.loadData('Module:HeroNames')
 
 local MatchGroupInput = Lua.import('Module:MatchGroup/Input', {requireDevIfEnabled = true})
 
-local ALLOWED_STATUSES = { 'W', 'FF', 'DQ', 'L', 'D' }
-local MAX_NUM_OPPONENTS = 2
-local MAX_NUM_PLAYERS = 5
-local DEFAULT_BESTOF = 3
-local NO_SCORE = -99
+local _ALLOWED_STATUSES = { 'W', 'FF', 'DQ', 'L', 'D' }
+local _MAX_NUM_OPPONENTS = 2
+local _MAX_NUM_PLAYERS = 5
+local _DEFAULT_BESTOF = 3
+local _NO_SCORE = -99
 local _DUMMY_MAP = 'default'
 local _NP_STATUSES = {'skip', 'np', 'canceled', 'cancelled'}
 local _EPOCH_TIME = '1970-01-01 00:00:00'
@@ -56,7 +56,7 @@ end
 
 function matchFunctions.adjustMapData(match)
 	local opponents = {}
-	for opponentIndex = 1, MAX_NUM_OPPONENTS do
+	for opponentIndex = 1, _MAX_NUM_OPPONENTS do
 		opponents[opponentIndex] = match['opponent' .. opponentIndex]
 	end
 	local mapIndex = 1
@@ -200,8 +200,8 @@ function CustomMatchGroupInput.setPlacement(opponents, winner, specialType, fini
 end
 
 function CustomMatchGroupInput.placementSortFunction(table, key1, key2)
-	local value1 = tonumber(table[key1].score or NO_SCORE) or NO_SCORE
-	local value2 = tonumber(table[key2].score or NO_SCORE) or NO_SCORE
+	local value1 = tonumber(table[key1].score or _NO_SCORE) or _NO_SCORE
+	local value2 = tonumber(table[key2].score or _NO_SCORE) or _NO_SCORE
 	return value1 > value2
 end
 
@@ -239,7 +239,7 @@ end
 -- match related functions
 --
 function matchFunctions.getBestOf(match)
-	match.bestof = Logic.emptyOr(match.bestof, Variables.varDefault('bestof', DEFAULT_BESTOF))
+	match.bestof = Logic.emptyOr(match.bestof, Variables.varDefault('bestof', _DEFAULT_BESTOF))
 	Variables.varDefine('bestof', match.bestof)
 	return match
 end
@@ -256,13 +256,13 @@ function matchFunctions.getScoreFromMapWinners(match)
 	while match['map'..mapIndex] do
 		local winner = tonumber(match['map'..mapIndex].winner)
 		foundScores = true
-		if winner and winner > 0 and winner <= MAX_NUM_OPPONENTS then
+		if winner and winner > 0 and winner <= _MAX_NUM_OPPONENTS then
 			newScores[winner] = (newScores[winner] or 0) + 1
 		end
 		mapIndex = mapIndex + 1
 	end
 
-	for index = 1, MAX_NUM_OPPONENTS do
+	for index = 1, _MAX_NUM_OPPONENTS do
 		if not match['opponent' .. index].score and foundScores then
 			match['opponent' .. index].score = newScores[index] or 0
 		end
@@ -345,7 +345,7 @@ function matchFunctions.getOpponents(match)
 	-- read opponents and ignore empty ones
 	local opponents = {}
 	local isScoreSet = false
-	for opponentIndex = 1, MAX_NUM_OPPONENTS do
+	for opponentIndex = 1, _MAX_NUM_OPPONENTS do
 		-- read opponent
 		local opponent = match['opponent' .. opponentIndex]
 		if not Logic.isEmpty(opponent) then
@@ -367,7 +367,7 @@ function matchFunctions.getOpponents(match)
 				opponent.score = tonumber(opponent.score)
 				opponent.status = 'S'
 				isScoreSet = true
-			elseif Table.includes(ALLOWED_STATUSES, opponent.score) then
+			elseif Table.includes(_ALLOWED_STATUSES, opponent.score) then
 				opponent.status = opponent.score
 				opponent.score = -1
 			end
@@ -381,7 +381,7 @@ function matchFunctions.getOpponents(match)
 				opponent.match2players = Json.parseIfString(opponent.match2players) or {}
 				opponent.match2players[1].name = opponent.name
 			else
-				error('Unsupported Oppnent Type "' .. (opponent.type or '') .. '"')
+				error('Unsupported Opponent Type "' .. (opponent.type or '') .. '"')
 			end
 
 			opponents[opponentIndex] = opponent
@@ -395,7 +395,7 @@ function matchFunctions.getOpponents(match)
 		opponents = matchFunctions._makeAllOpponentsLoseByWalkover(opponents, 'L')
 		opponents[winnerIndex].status = 'W'
 		match.finished = true
-	elseif Logic.isNumeric(match.winner) and Table.includes(ALLOWED_STATUSES, match.walkover) then
+	elseif Logic.isNumeric(match.winner) and Table.includes(_ALLOWED_STATUSES, match.walkover) then
 		local winnerIndex = tonumber(match.winner)
 		opponents = matchFunctions._makeAllOpponentsLoseByWalkover(opponents, match.walkover)
 		opponents[winnerIndex].status = 'W'
@@ -454,7 +454,7 @@ function matchFunctions.getPlayersOfTeam(match, oppIndex, teamName, playersData)
 	playersData = Json.parseIfString(playersData) or {}
 	local players = {}
 	local count = 1
-	for playerIndex = 1, MAX_NUM_PLAYERS do
+	for playerIndex = 1, _MAX_NUM_PLAYERS do
 		-- parse player
 		local player = Json.parseIfString(match['opponent' .. oppIndex .. '_p' .. playerIndex]) or {}
 		player.name = player.name or playersData['p' .. playerIndex]
@@ -494,21 +494,16 @@ end
 function mapFunctions.getParticipants(map, opponents)
 	local participants = {}
 	local championData = {}
-	for opponentIndex = 1, MAX_NUM_OPPONENTS do
-		for playerIndex = 1, MAX_NUM_PLAYERS do
-			local champ = map['team' .. opponentIndex .. 'champion' .. playerIndex] or
-				map['t' .. opponentIndex .. 'c' .. playerIndex] or
-				map['team' .. opponentIndex .. 'hero' .. playerIndex] or
-				map['t' .. opponentIndex .. 'h' .. playerIndex]
+	for opponentIndex = 1, _MAX_NUM_OPPONENTS do
+		for playerIndex = 1, _MAX_NUM_PLAYERS do
+			local champ = map['t' .. opponentIndex .. 'h' .. playerIndex]
 			championData['team' .. opponentIndex .. 'champion' .. playerIndex] =
 				ChampionNames[champ] or champ
 
-			championData['team' .. opponentIndex .. 'kda' .. playerIndex] =
-				map['team' .. opponentIndex .. 'kda' .. playerIndex] or
+			championData['t' .. opponentIndex .. 'kda' .. playerIndex] =
 				map['t' .. opponentIndex .. 'kda' .. playerIndex]
 
-			local player = map['team' .. opponentIndex .. 'player' .. playerIndex] or
-				map['t' .. opponentIndex .. 'p' .. playerIndex]
+			local player = map['t' .. opponentIndex .. 'p' .. playerIndex]
 			if String.isNotEmpty(player) then
 				participants = mapFunctions.attachToParticipant(
 					player,
@@ -521,13 +516,11 @@ function mapFunctions.getParticipants(map, opponents)
 			end
 		end
 		local banIndex = 1
-		local currentBan = map['team' .. opponentIndex .. 'ban' .. banIndex]
-			or map['t' .. opponentIndex .. 'b' .. banIndex]
+		local currentBan = map['t' .. opponentIndex .. 'b' .. banIndex]
 		while currentBan do
 			championData['team' .. opponentIndex .. 'ban' .. banIndex] = ChampionNames[currentBan] or currentBan
 			banIndex = banIndex + 1
-			currentBan = map['team' .. opponentIndex .. 'ban' .. banIndex]
-				or map['t' .. opponentIndex .. 'b' .. banIndex]
+			currentBan = map['t' .. opponentIndex .. 'b' .. banIndex]
 		end
 	end
 
@@ -555,7 +548,7 @@ end
 function mapFunctions.getScoresAndWinner(map)
 	map.scores = {}
 	local indexedScores = {}
-	for scoreIndex = 1, MAX_NUM_OPPONENTS do
+	for scoreIndex = 1, _MAX_NUM_OPPONENTS do
 		-- read scores
 		local score = map['score' .. scoreIndex] or map['t' .. scoreIndex .. 'score']
 		local obj = {}
@@ -565,7 +558,7 @@ function mapFunctions.getScoresAndWinner(map)
 				score = tonumber(score)
 				map['score' .. scoreIndex] = score
 				obj.score = score
-			elseif Table.includes(ALLOWED_STATUSES, score) then
+			elseif Table.includes(_ALLOWED_STATUSES, score) then
 				obj.status = score
 				obj.score = -1
 			end
