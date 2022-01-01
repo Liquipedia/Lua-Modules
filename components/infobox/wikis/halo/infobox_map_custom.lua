b8001ef4d0e54af4c0829bac0f59664a87d1c4ee
Cameron <58013431+FenrirWulf@users.noreply.github.com>
2022-01-01 10:42:38 -0800

    Updating LPDB

--- components/infobox/wikis/halo/infobox_map_custom.lua
+++ components/infobox/wikis/halo/infobox_map_custom.lua
@@ -96,11 +96,11 @@ function CustomMap:addToLpdb(lpdbData)
 	lpdbData.extradata.type = _args.type
 	lpdbData.extradata.players = _args.players
 	lpdbData.extradata.game = _game
-	lpdbData.extradata.modes = CustomMap:_concatArgs('mode')
+	lpdbData.extradata.modes = table.concat(Map:getAllArgsForBase(_args, 'mode'), ',')
 	return lpdbData
 end
 
-function CustomMap:_concatArgs(base)
+function CustomMap:_concatArgs()
 	return table.concat(Map:getAllArgsForBase(_args, 'mode'), ',')
 end
 
