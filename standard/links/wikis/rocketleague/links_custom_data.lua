9df13c419097f73de862a505e6c784f92bde2943
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2022-01-05 15:28:31 +0100

    Adjust `steam` link for rocketleague (#914)

---
+++ standard/links/wikis/rocketleague/links_custom_data.lua
@@ -0,0 +1,13 @@
+---
+-- @Liquipedia
+-- wiki=rocketleague
+-- page=Module:Links/CustomData
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+return {
+    prefixes = {
+        steam = {'https://steamcommunity.com/'},
+    },
+}
