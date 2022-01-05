241156f5dc30f6c59bd8baf6262d7f242ce8a4fe
Rikard Blixt <rikardblixt@gmail.com>
2022-01-05 14:41:24 +0100

    Improvement to Legacy/Default (#913)
    * Add Team as default brackettype. Add details suffix automatically.
    
    * Change Quotation Marks to use Single Quotes over Double
    
    * Update comment

--- components/match2/wikis/rainbowsix/legacy/match_group_legacy_default.lua
+++ components/match2/wikis/rainbowsix/legacy/match_group_legacy_default.lua
@@ -13,8 +13,8 @@
 
 local MatchGroupLegacyDefault = {}
 
-local String = require("Module:StringUtils")
-local Logic = require("Module:Logic")
+local String = require('Module:StringUtils')
+local Logic = require('Module:Logic')
 
 local MAX_NUM_MAPS = 9
 
@@ -23,7 +23,7 @@ function MatchGroupLegacyDefault.get(templateid, bracketType)
 	local lowerHeader = {}
 	local matches = mw.ext.Brackets.getCommonsBracketTemplate(templateid)
 
-	assert(type(matches) == "table")
+	assert(type(matches) == 'table')
 	local bracketData = {}
 	roundData = roundData or {}
 	local lastRound = 0
@@ -33,33 +33,33 @@ function MatchGroupLegacyDefault.get(templateid, bracketType)
 	end
 
 	-- add reference for map mappings
-	bracketData["$$map"] = {
-		["$notEmpty$"] = "map$1$",
-		map = "map$1$",
-		finished = "map$1$finished",
-		score1 = "map$1$score1",
-		score2 = "map$1$score2",
-		t1ban1 = "map$1$t1ban1",
-		t1ban2 = "map$1$t1ban2",
-		t2ban1 = "map$1$t2ban1",
-		t2ban2 = "map$1$t2ban2",
-		t1firstside = "map$1$t1firstside",
-		t1firstsideot = "map$1$o1t1firstside",
-		t1atk = "map$1$t1atk",
-		t1def = "map$1$t1def",
-		t2atk = "map$1$t2atk",
-		t2def = "map$1$t2def",
-		t1otatk = "map$1$o1t1atk",
-		t1otdef = "map$1$o1t1def",
-		t2otatk = "map$1$o1t2atk",
-		t2otdef = "map$1$o1t2def",
-		vod = "vod$1$",
+	bracketData['$$map'] = {
+		['$notEmpty$'] = 'map$1$',
+		map = 'map$1$',
+		finished = 'map$1$finished',
+		score1 = 'map$1$score1',
+		score2 = 'map$1$score2',
+		t1ban1 = 'map$1$t1ban1',
+		t1ban2 = 'map$1$t1ban2',
+		t2ban1 = 'map$1$t2ban1',
+		t2ban2 = 'map$1$t2ban2',
+		t1firstside = 'map$1$t1firstside',
+		t1firstsideot = 'map$1$o1t1firstside',
+		t1atk = 'map$1$t1atk',
+		t1def = 'map$1$t1def',
+		t2atk = 'map$1$t2atk',
+		t2def = 'map$1$t2def',
+		t1otatk = 'map$1$o1t1atk',
+		t1otdef = 'map$1$o1t1def',
+		t2otatk = 'map$1$o1t2atk',
+		t2otdef = 'map$1$o1t2def',
+		vod = 'vod$1$',
 	}
 
 	for n = 1, lastRound do
-		bracketData["R" .. n .. "M1header"] = "R" .. n
+		bracketData['R' .. n .. 'M1header'] = 'R' .. n
 		if lowerHeader[n] then
-			bracketData["R" .. n .. "M" .. lowerHeader[n] .. "header"] = "L" .. n
+			bracketData['R' .. n .. 'M' .. lowerHeader[n] .. 'header'] = 'L' .. n
 		end
 	end
 
@@ -70,25 +70,25 @@ end
 --it is needed as a basis for the next call
 local _lastRound
 function MatchGroupLegacyDefault._getMatchMapping(match, bracketData, bracketType, lowerHeader)
-	local id = String.split(match.match2id, "_")[2] or match.match2id
+	local id = String.split(match.match2id, '_')[2] or match.match2id
 	--remove 0's and dashes from the match param
 	--e.g. R01-M001 --> R1M1
-	id = id:gsub("0*([1-9])", "%1"):gsub("%-", "")
+	id = id:gsub('0*([1-9])', '%1'):gsub('%-', '')
 	local bd = match.match2bracketdata
 
 	local roundNum
 	local round
 	local reset = false
-	if id == "RxMTP" then
+	if id == 'RxMTP' then
 		round = _lastRound
-	elseif id == "RxMBR" then
+	elseif id == 'RxMBR' then
 		round = _lastRound
 		round.G = round.G - 2
 		round.W = round.W - 2
 		round.D = round.D - 2
 		reset = true
 	else
-		roundNum = id:match("R%d*"):gsub("R", "")
+		roundNum = id:match('R%d*'):gsub('R', '')
 		roundNum = tonumber(roundNum)
 		round = roundData[roundNum] or { R = roundNum, G = 0, D = 1, W = 1 }
 	end
@@ -105,104 +105,104 @@ function MatchGroupLegacyDefault._getMatchMapping(match, bracketData, bracketTyp
 	local finished2
 	if Logic.isEmpty(bd.toupper) and not reset then
 		-- RxDx
-		if bracketType == "team" then
+		if bracketType == 'team' then
 			opponent1 = {
-				["type"] = "type",
-				template = "R" .. round.R .. "D" .. round.D .. "team",
-				score = "R" .. round.R .. "D" .. round.D .. "score",
-				["$notEmpty$"] = "R" .. round.R .. "D" .. round.D .. "team"
+				['type'] = 'type',
+				template = 'R' .. round.R .. 'D' .. round.D .. 'team',
+				score = 'R' .. round.R .. 'D' .. round.D .. 'score',
+				['$notEmpty$'] = 'R' .. round.R .. 'D' .. round.D .. 'team'
 			}
 		else
 			opponent1 = {
-				["type"] = "type",
-				template = "R" .. round.R .. "D" .. round.D .. "team",
-				score = "R" .. round.R .. "D" .. round.D .. "score",
-				["$notEmpty$"] = "R" .. round.R .. "D" .. round.D,
-				name = "R" .. round.R .. "D" .. round.D,
-				displayname = "R" .. round.R .. "D" .. round.D .. "display",
-				flag = "R" .. round.R .. "D" .. round.D .. "flag"
+				['type'] = 'type',
+				template = 'R' .. round.R .. 'D' .. round.D .. 'team',
+				score = 'R' .. round.R .. 'D' .. round.D .. 'score',
+				['$notEmpty$'] = 'R' .. round.R .. 'D' .. round.D,
+				name = 'R' .. round.R .. 'D' .. round.D,
+				displayname = 'R' .. round.R .. 'D' .. round.D .. 'display',
+				flag = 'R' .. round.R .. 'D' .. round.D .. 'flag'
 			}
 		end
-		finished1 = "R" .. round.R .. "D" .. round.D .. "win"
+		finished1 = 'R' .. round.R .. 'D' .. round.D .. 'win'
 		round.D = round.D + 1
 	else
 		-- RxWx
-		if bracketType == "team" then
+		if bracketType == 'team' then
 			opponent1 = {
-				["type"] = "type",
-				template = "R" .. round.R .. "W" .. round.W .. "team",
-				score = "R" .. round.R .. "W" .. round.W .. "score" .. (reset and "2" or ""),
-				["$notEmpty$"] = "R" .. round.R .. "W" .. round.W .. "team"
+				['type'] = 'type',
+				template = 'R' .. round.R .. 'W' .. round.W .. 'team',
+				score = 'R' .. round.R .. 'W' .. round.W .. 'score' .. (reset and '2' or ''),
+				['$notEmpty$'] = 'R' .. round.R .. 'W' .. round.W .. 'team'
 			}
 		else
 			opponent1 = {
-				["type"] = "type",
-				template = "R" .. round.R .. "W" .. round.W .. "team",
-				score = "R" .. round.R .. "W" .. round.W .. "score" .. (reset and "2" or ""),
-				["$notEmpty$"] = "R" .. round.R .. "W" .. round.W,
-				name = "R" .. round.R .. "W" .. round.W,
-				displayname = "R" .. round.R .. "W" .. round.W .. "display",
-				flag = "R" .. round.R .. "W" .. round.W .. "flag"
+				['type'] = 'type',
+				template = 'R' .. round.R .. 'W' .. round.W .. 'team',
+				score = 'R' .. round.R .. 'W' .. round.W .. 'score' .. (reset and '2' or ''),
+				['$notEmpty$'] = 'R' .. round.R .. 'W' .. round.W,
+				name = 'R' .. round.R .. 'W' .. round.W,
+				displayname = 'R' .. round.R .. 'W' .. round.W .. 'display',
+				flag = 'R' .. round.R .. 'W' .. round.W .. 'flag'
 			}
 		end
-		finished1 = "R" .. round.R .. "W" .. round.W .. "win"
+		finished1 = 'R' .. round.R .. 'W' .. round.W .. 'win'
 		round.W = round.W + 1
 	end
 
 	local opponent2
 	if Logic.isEmpty(bd.tolower) and not reset then
 		-- RxDx
-		if bracketType == "team" then
+		if bracketType == 'team' then
 			opponent2 = {
-				["type"] = "type",
-				template = "R" .. round.R .. "D" .. round.D .. "team",
-				score = "R" .. round.R .. "D" .. round.D .. "score",
-				["$notEmpty$"] = "R" .. round.R .. "D" .. round.D .. "team"
+				['type'] = 'type',
+				template = 'R' .. round.R .. 'D' .. round.D .. 'team',
+				score = 'R' .. round.R .. 'D' .. round.D .. 'score',
+				['$notEmpty$'] = 'R' .. round.R .. 'D' .. round.D .. 'team'
 			}
 		else
 			opponent2 = {
-				["type"] = "type",
-				template = "R" .. round.R .. "D" .. round.D .. "team",
-				score = "R" .. round.R .. "D" .. round.D .. "score",
-				["$notEmpty$"] = "R" .. round.R .. "D" .. round.D,
-				name = "R" .. round.R .. "D" .. round.D,
-				displayname = "R" .. round.R .. "D" .. round.D .. "display",
-				flag = "R" .. round.R .. "D" .. round.D .. "flag"
+				['type'] = 'type',
+				template = 'R' .. round.R .. 'D' .. round.D .. 'team',
+				score = 'R' .. round.R .. 'D' .. round.D .. 'score',
+				['$notEmpty$'] = 'R' .. round.R .. 'D' .. round.D,
+				name = 'R' .. round.R .. 'D' .. round.D,
+				displayname = 'R' .. round.R .. 'D' .. round.D .. 'display',
+				flag = 'R' .. round.R .. 'D' .. round.D .. 'flag'
 			}
 		end
-		finished2 = "R" .. round.R .. "D" .. round.D .. "win"
+		finished2 = 'R' .. round.R .. 'D' .. round.D .. 'win'
 		round.D = round.D + 1
 	else
 		-- RxWx
-		if bracketType == "team" then
+		if bracketType == 'team' then
 			opponent2 = {
-				["type"] = "type",
-				template = "R" .. round.R .. "W" .. round.W .. "team",
-				score = "R" .. round.R .. "W" .. round.W .. "score" .. (reset and "2" or ""),
-				["$notEmpty$"] = "R" .. round.R .. "W" .. round.W .. "team"
+				['type'] = 'type',
+				template = 'R' .. round.R .. 'W' .. round.W .. 'team',
+				score = 'R' .. round.R .. 'W' .. round.W .. 'score' .. (reset and '2' or ''),
+				['$notEmpty$'] = 'R' .. round.R .. 'W' .. round.W .. 'team'
 			}
 		else
 			opponent2 = {
-				["type"] = "type",
-				template = "R" .. round.R .. "W" .. round.W .. "team",
-				score = "R" .. round.R .. "W" .. round.W .. "score" .. (reset and "2" or ""),
-				["$notEmpty$"] = "R" .. round.R .. "W" .. round.W,
-				name = "R" .. round.R .. "W" .. round.W,
-				displayname = "R" .. round.R .. "W" .. round.W .. "display",
-				flag = "R" .. round.R .. "W" .. round.W .. "flag"
+				['type'] = 'type',
+				template = 'R' .. round.R .. 'W' .. round.W .. 'team',
+				score = 'R' .. round.R .. 'W' .. round.W .. 'score' .. (reset and '2' or ''),
+				['$notEmpty$'] = 'R' .. round.R .. 'W' .. round.W,
+				name = 'R' .. round.R .. 'W' .. round.W,
+				displayname = 'R' .. round.R .. 'W' .. round.W .. 'display',
+				flag = 'R' .. round.R .. 'W' .. round.W .. 'flag'
 			}
 		end
-		finished2 = "R" .. round.R .. "W" .. round.W .. "win"
+		finished2 = 'R' .. round.R .. 'W' .. round.W .. 'win'
 		round.W = round.W + 1
 	end
 
 	match = {
 		opponent1 = opponent1,
 		opponent2 = opponent2,
-		finished = finished1 .. "|" .. finished2,
-		mapveto = "mapveto",
+		finished = finished1 .. '|' .. finished2,
+		mapveto = 'mapveto',
 		-- reference to variables that shall be flattened
-		["$flatten$"] = { "R" .. round.R .. "G" .. round.G .. "details" }
+		['$flatten$'] = { 'R' .. round.R .. 'G' .. round.G .. 'details' }
 	}
 
 	bracketData[id] = MatchGroupLegacyDefault.addMaps(match)
@@ -221,9 +221,9 @@ parameter format of the old bracket
 --this can be used for custom mappings too
 function MatchGroupLegacyDefault.addMaps(match)
 	for mapIndex = 1, MAX_NUM_MAPS do
-		match["map" .. mapIndex] = {
-			["$ref$"] = "map",
-			["$1$"] = mapIndex,
+		match['map' .. mapIndex] = {
+			['$ref$'] = 'map',
+			['$1$'] = mapIndex,
 		}
 	end
 	return match
@@ -235,38 +235,35 @@ function MatchGroupLegacyDefault.matchMappingFromCustom(data, bracketType)
 	data has the form {
 		opp1, -- e.g. R1D1
 		opp2, -- e.g. R1D20
-		details, -- e.g. R1G5details
+		details, -- e.g. R1G5
 	}
 	]]--
-	local mapping = {
-		["$flatten$"] = { data.details },
-		["finished"] = data.opp1 .. "win|" .. data.opp2 .. "win",
-		["opponent1"] = {
-			["type"] = "type",
-			["$notEmpty$"] = data.opp1 ..
-				(bracketType == "team" and "team" or ""),
-			template = data.opp1 .. "team",
-			score = data.opp1 .. "score",
-			name = bracketType ~= "team" and data.opp1 or nil,
-			displayname = bracketType ~= "team" and (data.opp1 .. "display") or nil,
-			flag = bracketType ~= "team" and data.opp1 or nil,
-			win = data.opp1 .. "win",
-			},
-		["opponent2"] = {
-			["type"] = "type",
-			["$notEmpty$"] = data.opp2 ..
-				(bracketType == "team" and "team" or ""),
-			template = data.opp2 .. "team",
-			score = data.opp2 .. "score",
-			name = bracketType ~= "team" and data.opp2 or nil,
-			displayname = bracketType ~= "team" and (data.opp2 .. "display") or nil,
-			flag = bracketType ~= "team" and data.opp2 or nil,
-			win = data.opp2 .. "win",
-			},
-	}
-	mapping = MatchGroupLegacyDefault.addMaps(mapping)
+	bracketType = bracketType or 'team'
 
-	return mapping
+	return MatchGroupLegacyDefault.addMaps{
+		['$flatten$'] = { data.details .. 'details' },
+		['finished'] = data.opp1 .. 'win|' .. data.opp2 .. 'win',
+		['opponent1'] = {
+			['type'] = 'type',
+			['$notEmpty$'] = data.opp1 .. (bracketType == 'team' and 'team' or ''),
+			template = data.opp1 .. 'team',
+			score = data.opp1 .. 'score',
+			name = bracketType ~= 'team' and data.opp1 or nil,
+			displayname = bracketType ~= 'team' and (data.opp1 .. 'display') or nil,
+			flag = bracketType ~= 'team' and data.opp1 or nil,
+			win = data.opp1 .. 'win',
+		},
+		['opponent2'] = {
+			['type'] = 'type',
+			['$notEmpty$'] = data.opp2 .. (bracketType == 'team' and 'team' or ''),
+			template = data.opp2 .. 'team',
+			score = data.opp2 .. 'score',
+			name = bracketType ~= 'team' and data.opp2 or nil,
+			displayname = bracketType ~= 'team' and (data.opp2 .. 'display') or nil,
+			flag = bracketType ~= 'team' and data.opp2 or nil,
+			win = data.opp2 .. 'win',
+		},
+	}
 end
 
 --this is for custom mappings for Reset finals matches
@@ -274,9 +271,9 @@ end
 --and sets flatten to nil
 function MatchGroupLegacyDefault.matchResetMappingFromCustom(mapping)
 	local mappingReset = mw.clone(mapping)
-	mappingReset.opponent1.score = mapping.opponent1.score .. "2"
-	mappingReset.opponent2.score = mapping.opponent2.score .. "2"
-	mappingReset["$flatten$"] = nil
+	mappingReset.opponent1.score = mapping.opponent1.score .. '2'
+	mappingReset.opponent2.score = mapping.opponent2.score .. '2'
+	mappingReset['$flatten$'] = nil
 	return mappingReset
 end
 
