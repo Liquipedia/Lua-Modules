d0da2d842913fa7bb99c7a80d4f9f6f690f1efbd
Rikard Blixt <rikardblixt@gmail.com>
2022-01-07 13:37:41 +0100

    Match change in #922

--- components/notability/wikis/rainbowsix/notability_checker_config.lua
+++ components/notability/wikis/rainbowsix/notability_checker_config.lua
@@ -22,6 +22,7 @@ Config.TIER_TYPE_SHOW_MATCH = 'show match'
 Config.PLACEMENT_LIMIT = 2000
 
 Config.MAX_NUMBER_OF_PARTICIPANTS = 10
+Config.MAX_NUMBER_OF_COACHES = 5
 
 -- Which LPDB placement parameters do we care about?
 Config.PLACEMENT_QUERY =
