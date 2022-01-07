ab6ea8ebea41e9a27f3d1989a57fb1482412b0bf
Mamdouh Waleed <mamdouh.waleed.96@gmail.com>
2022-01-07 14:01:56 +0200

    Apply suggestions from code review
    Co-authored-by: Jasper van Riet <JaspervanRiet@users.noreply.github.com>

--- components/automatic-points-table/commons/automatic-points-table.lua
+++ components/automatic-points-table/commons/automatic-points-table.lua
@@ -15,7 +15,7 @@ local AutomaticPointsTable = Class.new(
 	function(self, frame)
 		self.frame = frame
 		self.args = Arguments.getArgs(frame)
-		self:parseInput(self.args)
+		self.parsedInput = self:parseInput(self.args)
 
 	end
 )
@@ -24,34 +24,39 @@ function AutomaticPointsTable.run(frame)
 	local pointsTable = AutomaticPointsTable(frame)
 
 
-	mw.logObject(pointsTable.pbg)
-	mw.logObject(pointsTable.tournaments)
+	mw.logObject(pointsTable.parsedInput.pbg)
+	mw.logObject(pointsTable.parsedInput.tournaments)
 
 	return nil
 end
 
 function AutomaticPointsTable:parseInput(args)
-	self:parsePositionBackgroundData(args)
-	self:parseTournaments(args)
+	local pbg = self:parsePositionBackgroundData(args)
+	local tournaments = self:parseTournaments(args)
+	return {
+		pbg = pbg,
+		tournaments = tournaments
+	}
 end
 
 --- parses the pbg arguments, these are the background colors of specific positions
 --- Usually used to indicate where a team in a specific position will end up qualifying to
 function AutomaticPointsTable:parsePositionBackgroundData(args)
-
-	self.pbg = {}
+	local pbg = {}
 	for _, background in Table.iter.pairsByPrefix(args, 'pbg') do
-		table.insert(self.pbg, background)
+		table.insert(pbg, background)
 	end
+	return pbg
 end
 
 function AutomaticPointsTable:parseTournaments(args)
 
-	self.tournaments = {}
+	local tournaments = {}
 	for _, tournament in Table.iter.pairsByPrefix(args, 'tournament') do
-		table.insert(self.tournaments, (Json.parse(tournament)))
+		table.insert(tournaments, (Json.parse(tournament)))
 	end
-	return nil
+	return tournaments
+
 end
 
 return AutomaticPointsTable
