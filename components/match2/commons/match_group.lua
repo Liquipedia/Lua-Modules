9c6e98cd8cda07a33927687166b3ffd3ab706303
Jasper van Riet <JaspervanRiet@users.noreply.github.com>
2022-01-11 15:07:16 +0100

    Remove MatchGroup/Display (#937)
    * Rename MatchGroup/Display to MatchGroup/Core
    
    * Get rid of core altogether and replace it with MatchGroup
    
    * Remove references
    
    * Revert to old MatchGroup implementation

--- components/match2/commons/match_group.lua
+++ components/match2/commons/match_group.lua
@@ -6,32 +6,204 @@
 -- Please see https://github.com/Liquipedia/Lua-Modules to contribute
 --
 
+local Arguments = require('Module:Arguments')
+local Array = require('Module:Array')
+local FeatureFlag = require('Module:FeatureFlag')
 local Lua = require('Module:Lua')
+local Table = require('Module:Table')
+local WarningBox = require('Module:WarningBox')
 
-local MatchGroupDisplay = Lua.import('Module:MatchGroup/Display', {requireDevIfEnabled = true})
+local Match = Lua.import('Module:Match', {requireDevIfEnabled = true})
+local MatchGroupBase = Lua.import('Module:MatchGroup/Base', {requireDevIfEnabled = true})
+local MatchGroupConfig = Lua.loadDataIfExists('Module:MatchGroup/Config')
+local MatchGroupInput = Lua.import('Module:MatchGroup/Input', {requireDevIfEnabled = true})
+local MatchGroupUtil = Lua.import('Module:MatchGroup/Util', {requireDevIfEnabled = true})
+local WikiSpecific = Lua.import('Module:Brkts/WikiSpecific', {requireDevIfEnabled = true})
 
+--[[
+	The core module behind every type of MatchGroup. A MatchGroup is a collection of matches, such as a bracket or
+	a matchlist.
+]]
 local MatchGroup = {}
+
+--[[
+Reads a matchlist input spec, saves it to LPDB, and displays the matchlist.
+]]
+function MatchGroup.MatchlistBySpec(args)
+	local options, optionsWarnings = MatchGroupBase.readOptions(args, 'matchlist')
+	local matches = MatchGroupInput.readMatchlist(options.bracketId, args)
+	Match.storeMatchGroup(matches, options)
+
+	local matchlistNode
+	if options.show then
+		local MatchlistDisplay = Lua.import('Module:MatchGroup/Display/Matchlist', {requireDevIfEnabled = true})
+		local MatchlistContainer = WikiSpecific.getMatchGroupContainer('matchlist')
+		matchlistNode = MatchlistContainer({
+			bracketId = options.bracketId,
+			config = MatchlistDisplay.configFromArgs(args),
+		})
+	end
+
+	local parts = Array.extend(
+		{matchlistNode},
+		Array.map(optionsWarnings, WarningBox.display)
+	)
+	return table.concat(Array.map(parts, tostring))
+end
+
+--[[
+Reads a bracket input spec, saves it to LPDB, and displays the bracket.
+]]
+function MatchGroup.BracketBySpec(args)
+	local options, optionsWarnings = MatchGroupBase.readOptions(args, 'bracket')
+	local matches, bracketWarnings = MatchGroupInput.readBracket(options.bracketId, args, options)
+	Match.storeMatchGroup(matches, options)
+
+	local bracketNode
+	if options.show then
+		local BracketDisplay = Lua.import('Module:MatchGroup/Display/Bracket', {requireDevIfEnabled = true})
+		local BracketContainer = WikiSpecific.getMatchGroupContainer('bracket')
+		bracketNode = BracketContainer({
+			bracketId = options.bracketId,
+			config = BracketDisplay.configFromArgs(args),
+		})
+	end
+
+	local parts = Array.extend(
+		Array.map(optionsWarnings, WarningBox.display),
+		Array.map(bracketWarnings, WarningBox.display),
+		{bracketNode}
+	)
+	return table.concat(Array.map(parts, tostring))
+end
+
+--[[
+Displays a matchlist or bracket specified by ID.
+]]
+function MatchGroup.MatchGroupById(args)
+	local bracketId = args.id or args[1]
+	args.id = bracketId
+	args[1] = bracketId
+	assert(bracketId, 'Missing bracket ID')
+
+	local matches = MatchGroupUtil.fetchMatches(bracketId)
+	assert(#matches ~= 0, 'No data found for bracketId=' .. bracketId)
+	local matchGroupType = matches[1].bracketData.type
+
+	local config
+	if matchGroupType == 'matchlist' then
+		local MatchlistDisplay = Lua.import('Module:MatchGroup/Display/Matchlist', {requireDevIfEnabled = true})
+		config = MatchlistDisplay.configFromArgs(args)
+	else
+		local BracketDisplay = Lua.import('Module:MatchGroup/Display/Bracket', {requireDevIfEnabled = true})
+		config = BracketDisplay.configFromArgs(args)
+	end
+
+	MatchGroupInput.applyOverrideArgs(matches, args)
+
+	local MatchGroupContainer = WikiSpecific.getMatchGroupContainer(matchGroupType)
+	return MatchGroupContainer({
+		bracketId = bracketId,
+		config = config,
+	})
+end
+
+--[[
+Displays a singleMatch specified by a bracket ID and matchID.
+]]
+function MatchGroup.MatchByMatchId(args)
+	local bracketId = args.id
+	local matchId = args.matchid
+	assert(bracketId, 'Missing bracket ID')
+	assert(matchId, 'Missing match ID')
+
+	matchId = MatchGroupUtil.matchIdFromKey(matchId)
+
+	local matchGroup = MatchGroupUtil.fetchMatchGroup(bracketId)
+	local fullMatchId = bracketId .. '_' .. matchId
+	local match = matchGroup.matchesById[fullMatchId]
+
+	assert(match, 'Match bracketId= ' .. bracketId .. ' matchId=' .. matchId .. ' not found')
+
+	local SingleMatchDisplay = Lua.import('Module:MatchGroup/Display/SingleMatch', {requireDevIfEnabled = true})
+	local config = SingleMatchDisplay.configFromArgs(args)
+
+	local MatchGroupContainer = WikiSpecific.getMatchContainer('singleMatch')
+	return MatchGroupContainer({
+		matchId = fullMatchId,
+		config = config,
+	})
+end
+
+
+-- Entry point of Template:Matchlist
+function MatchGroup.TemplateMatchlist(frame)
+	local args = Arguments.getArgs(frame)
+	return MatchGroup.MatchlistBySpec(args)
+end
+
+-- Entry point of Template:Bracket
+function MatchGroup.TemplateBracket(frame)
+	local args = Arguments.getArgs(frame)
+	return MatchGroup.BracketBySpec(args)
+end
+
+-- Entry point of Template:ShowSingleMatch
+function MatchGroup.TemplateShowSingleMatch(frame)
+	local args = Arguments.getArgs(frame)
+	return MatchGroup.MatchByMatchId(args)
+end
+
+-- Entry point of Template:ShowBracket, Template:DisplayMatchGroup
+function MatchGroup.TemplateShowBracket(frame)
+	local args = Arguments.getArgs(frame)
+	return MatchGroup.MatchGroupById(args)
+end
+
+if FeatureFlag.get('perf') then
+	MatchGroup.perfConfig = Table.getByPathOrNil(MatchGroupConfig, {'perf'})
+	require('Module:Performance/Util').setupEntryPoints(MatchGroup)
+end
+
+Lua.autoInvokeEntryPoints(MatchGroup, 'Module:MatchGroup')
+
+
+MatchGroup.deprecatedCategory = '[[Category:Pages using deprecated Match Group functions]]'
 
 -- Entry point used by Template:Bracket
 -- Deprecated
 function MatchGroup.bracket(frame)
-	return MatchGroupDisplay.TemplateBracket(frame) .. MatchGroupDisplay.deprecatedCategory
+	return MatchGroup.TemplateBracket(frame) .. MatchGroup.deprecatedCategory
 end
 
 -- Deprecated
 function MatchGroup.luaBracket(_, args)
-	return MatchGroupDisplay.TemplateBracket(args) .. MatchGroupDisplay.deprecatedCategory
+	return MatchGroup.TemplateBracket(args) .. MatchGroup.deprecatedCategory
 end
 
 -- Entry point used by Template:Matchlist
 -- Deprecated
 function MatchGroup.matchlist(frame)
-	return MatchGroupDisplay.TemplateMatchlist(frame) .. MatchGroupDisplay.deprecatedCategory
+	return MatchGroup.TemplateMatchlist(frame) .. MatchGroup.deprecatedCategory
 end
 
 -- Deprecated
 function MatchGroup.luaMatchlist(_, args)
-	return MatchGroupDisplay.TemplateMatchlist(args) .. MatchGroupDisplay.deprecatedCategory
+	return MatchGroup.TemplateMatchlist(args) .. MatchGroup.deprecatedCategory
+end
+
+-- Entry point from Template:ShowBracket and direct #invoke
+-- Deprecated
+function MatchGroup.Display(frame)
+	return tostring(MatchGroup.TemplateShowBracket(frame)) .. MatchGroup.deprecatedCategory
+end
+
+-- Entry point from direct #invoke
+-- Deprecated
+function MatchGroup.DisplayDev(frame)
+	local args = Arguments.getArgs(frame)
+	args.dev = true
+	return tostring(MatchGroup.TemplateShowBracket(args)) .. MatchGroup.deprecatedCategory
 end
 
 return MatchGroup
