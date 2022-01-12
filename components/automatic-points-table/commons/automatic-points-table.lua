7363e6e245f7d325ab459cd9747ad0c7b941ea27
Mamdouh Waleed <mamdouh.waleed.96@gmail.com>
2022-01-12 17:43:04 +0200

    Added parseTeams (#930)
    * Added parseTeams
    
    * Fix missing local keyword
    
    * Apply suggestions from code review
    
    Co-authored-by: Jasper van Riet <JaspervanRiet@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    argKey -> key
    argVal -> value
    
    * Remove whitespace
    
    * Apply suggestions from code review
    
    Changed the way of iterating over aliases, deductions and points
    
    * Ignoring deduction notes if there are no deducted points
    
    Co-authored-by: Rikard Blixt <rikardblixt@gmail.com>
    
    Co-authored-by: Jasper van Riet <JaspervanRiet@users.noreply.github.com>
    Co-authored-by: Rikard Blixt <rikardblixt@gmail.com>

--- components/automatic-points-table/commons/automatic-points-table.lua
+++ components/automatic-points-table/commons/automatic-points-table.lua
@@ -9,6 +9,7 @@
 local Arguments = require('Module:Arguments')
 local Class = require('Module:Class')
 local Json = require('Module:Json')
+local String = require('Module:StringUtils')
 local Table = require('Module:Table')
 
 local AutomaticPointsTable = Class.new(
@@ -23,6 +24,7 @@ function AutomaticPointsTable.run(frame)
 	local pointsTable = AutomaticPointsTable(frame)
 	mw.logObject(pointsTable.parsedInput.pbg)
 	mw.logObject(pointsTable.parsedInput.tournaments)
+	mw.logObject(pointsTable.parsedInput.teams)
 
 	return nil
 end
@@ -30,9 +32,11 @@ end
 function AutomaticPointsTable:parseInput(args)
 	local pbg = self:parsePositionBackgroundData(args)
 	local tournaments = self:parseTournaments(args)
+	local teams = self:parseTeams(args, #tournaments)
 	return {
 		pbg = pbg,
-		tournaments = tournaments
+		tournaments = tournaments,
+		teams = teams
 	}
 end
 
@@ -52,6 +56,60 @@ function AutomaticPointsTable:parseTournaments(args)
 		table.insert(tournaments, (Json.parse(tournament)))
 	end
 	return tournaments
+end
+
+function AutomaticPointsTable:parseTeams(args, tournamentCount)
+	local teams = {}
+	for _, team in Table.iter.pairsByPrefix(args, 'team') do
+		local parsedTeam = Json.parse(team)
+		parsedTeam.aliases = self:parseAliases(parsedTeam, tournamentCount)
+		parsedTeam.deductions = self:parseDeductions(parsedTeam, tournamentCount)
+		parsedTeam.manualPoints = self:parseManualPoints(parsedTeam, tournamentCount)
+		table.insert(teams, parsedTeam)
+	end
+	return teams
+end
+
+--- Parses the team aliases, used in cases where a team is picked up by an org or changed name in some
+--- of the tournaments, in which case aliases are required to correctly query the team's results & points
+function AutomaticPointsTable:parseAliases(team, tournamentCount)
+	local aliases = {}
+	for index = 1, tournamentCount do
+		if String.isNotEmpty(team['alias' .. index]) then
+			aliases[index] = team['alias' .. index]
+		end
+	end
+	return aliases
+end
+
+--- Parses the teams' deductions, used in cases where a team has disbanded or made a roster change
+--- that causes them to lose a portion or all of their points that they've accumulated up until that change
+function AutomaticPointsTable:parseDeductions(team, tournamentCount)
+	local deductions = {}
+	for index = 1, tournamentCount do
+		if String.isNotEmpty(team['deduction' .. index]) then
+			if not deductions[index] then
+				deductions[index] = {}
+			end
+			deductions[index].amount = tonumber(team['deduction' .. index])
+
+			if String.isNotEmpty(team['deduction' .. index .. 'note']) then
+				deductions[index].note = team['deduction' .. index .. 'note']
+			end
+		end
+	end
+
+	return deductions
+end
+
+function AutomaticPointsTable:parseManualPoints(team, tournamentCount)
+	local manualPoints = {}
+	for index = 1, tournamentCount do
+		if String.isNotEmpty(team['points' .. index]) then
+			manualPoints[index] = tonumber(team['points' .. index])
+		end
+	end
+	return manualPoints
 end
 
 return AutomaticPointsTable
