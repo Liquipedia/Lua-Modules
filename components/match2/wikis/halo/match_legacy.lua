2fd4735070b05a58d880c684fc078c8d260b6df0
Cameron <58013431+FenrirWulf@users.noreply.github.com>
2022-01-18 10:44:06 -0800

    Switching " to '

--- components/match2/wikis/halo/match_legacy.lua
+++ components/match2/wikis/halo/match_legacy.lua
@@ -8,9 +8,9 @@
 
 local p = {}
 
-local json = require("Module:Json")
-local String = require("Module:StringUtils")
-local Table = require("Module:Table")
+local json = require('Module:Json')
+local String = require('Module:StringUtils')
+local Table = require('Module:Table')
 
 function p.storeMatch(match2)
 	local match = p._convertParameters(match2)
@@ -18,13 +18,13 @@ function p.storeMatch(match2)
 	match.games = p.storeGames(match, match2)
 
 	return mw.ext.LiquipediaDB.lpdb_match(
-		"legacymatch_" .. match2.match2id,
+		'legacymatch_' .. match2.match2id,
 		match
 	)
 end
 
 function p.storeGames(match, match2)
-	local games = ""
+	local games = '
 	for gameIndex, game in ipairs(match2.match2games or {}) do
 		game = Table.deepCopy(game)
 		game.opponent1 = match.opponent1
@@ -33,13 +33,13 @@ function p.storeGames(match, match2)
 		game.opponent2flag = match.opponent2flag
 		game.date = match.date
 		local scores = game.scores or {}
-		if type(scores) == "string" then
+		if type(scores) == 'string' then
 			scores = json.parse(scores)
 		end
 		game.opponent1score = scores[1] or 0
 		game.opponent2score = scores[2] or 0
 		local res = mw.ext.LiquipediaDB.lpdb_game(
-			"legacygame_" .. match2.match2id .. gameIndex,
+			'legacygame_' .. match2.match2id .. gameIndex,
 			game
 		)
 		games = games .. res
@@ -50,7 +50,7 @@ end
 function p._convertParameters(match2)
 	local match = Table.deepCopy(match2)
 	for key, _ in pairs(match) do
-		if String.startsWith(key, "match2") then
+		if String.startsWith(key, 'match2') then
 			match[key] = nil
 		end
 	end
@@ -59,18 +59,18 @@ function p._convertParameters(match2)
 
 	local opponent1 = match2.match2opponents[1] or {}
 	local opponent1match2players = opponent1.match2players or {}
-	if opponent1.type == "team" then
+	if opponent1.type == 'team' then
 		match.opponent1 = opponent1.name
 		match.opponent1score = (tonumber(opponent1.score or 0) or 0) >= 0
 			and opponent1.score or 0
 		local opponent1players = {}
 		for i = 1,10 do
 			local player = opponent1match2players[i] or {}
-			opponent1players["p" .. i] = player.name or ""
-			opponent1players["p" .. i .. "flag"] = player.flag or ""
+			opponent1players'"p' .. i] = player.name or ""
+			opponent1players['p' .. i .. 'flag'] = player.flag or ""
 		end
 		match.opponent1players = json.stringify(opponent1players)
-	elseif opponent1.type == "solo" then
+	elseif opponent1.type == 'solo' then
 		local player = opponent1match2players[1] or {}
 		match.opponent1 = player.name
 		match.opponent1score = (tonumber(opponent1.score or 0) or 0) >= 0
@@ -80,18 +80,18 @@ function p._convertParameters(match2)
 
 	local opponent2 = match2.match2opponents[2] or {}
 	local opponent2match2players = opponent2.match2players or {}
-	if opponent2.type == "team" then
+	if opponent2.type == 'team' then
 		match.opponent2 = opponent2.name
 		match.opponent2score = (tonumber(opponent2.score or 0) or 0) >= 0
 			and opponent2.score or 0
 		local opponent2players = {}
 		for i = 1,10 do
 			local player = opponent2match2players[i] or {}
-			opponent2players["p" .. i] = player.name or ""
-			opponent2players["p" .. i .. "flag"] = player.flag or ""
+			opponent2players['p' .. i] = player.name or ""
+			opponent2players['p' .. i .. 'flag'] = player.flag or ""
 		end
 		match.opponent2players = json.stringify(opponent2players)
-	elseif opponent2.type == "solo" then
+	elseif opponent2.type == 'solo' then
 		local player = opponent2match2players[1] or {}
 		match.opponent2 = player.name
 		match.opponent2score = (tonumber(opponent2.score or 0) or 0) >= 0
