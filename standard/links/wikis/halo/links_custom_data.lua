6ad6258541e0f0f3901e16a791d85c8f4817d113
0xWeaseldee <63451355+0xWeaseldee@users.noreply.github.com>
2022-01-22 00:15:46 -0600

    Create links_custom_data.lua
    Create custom links file and add hdh links to halo wiki

---
+++ standard/links/wikis/halo/links_custom_data.lua
@@ -0,0 +1,18 @@
+---
+-- @Liquipedia
+-- wiki=halo
+-- page=Module:Links/CustomData
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+return {
+	prefixes = {
+		halodatahive = {
+			'https://halodatahive.com/Series/Summary/',
+			team = 'https://halodatahive.com/Team/Detail/',
+			player = 'https://halodatahive.com/Player/Detail/',
+      tournament = 'https://halodatahive.com/Tournament/Detail/',
+		}
+	},
+}
