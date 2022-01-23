29520788b4fb204fb38a65ed03d71f9ac709b473
Cameron <58013431+FenrirWulf@users.noreply.github.com>
2022-01-23 09:26:30 -0800

    CustomLink data for Rainbow Six

---
+++ standard/links/wikis/rainbowsix/links_custom_data.lua
@@ -0,0 +1,17 @@
+---
+-- @Liquipedia
+-- wiki=rainbowsix
+-- page=Module:Links/CustomData
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+return {
+	prefixes = {
+		siegegg = {
+			'https://siege.gg/competitions/',
+			team = 'https://siege.gg/teams/',
+			player = 'https://siege.gg/players/',
+		}
+	},
+}
