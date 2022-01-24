721da19e1f8179a75c9f7d497592787496898a01
Jonathan Andres Jimenez Trujillo <jonajitru781@gmail.com>
2022-01-24 11:14:13 -0500

    Add Infobox League for Brawl Stars (#990)
    * Create infobox_league_custom.lua
    
    Add infobox_league for Brawl STars
    
    * Update infobox_league_custom.lua
    
    * Update Infobox league related to Supercell icon
    
    Co-authored-by: hjpalpha <75081997+hjpalpha@users.noreply.github.com>
    
    * Add var of Supercell Sponsored icon
    
    * Minor changes to infobox
    
    * Fix var Supercell Sponsored not being called well
    
    * Clean code supercell icon
    
    * Delete trialing white space
    
    Co-authored-by: hjpalpha <75081997+hjpalpha@users.noreply.github.com>

---
+++ components/infobox/wikis/brawlstars/infobox_league_custom.lua
@@ -0,0 +1,166 @@
+---
+-- @Liquipedia
+-- wiki=brawlstars
+-- page=Module:Infobox/League/Custom
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local League = require('Module:Infobox/League')
+local String = require('Module:StringUtils')
+local Variables = require('Module:Variables')
+local Tier = require('Module:Tier')
+local Class = require('Module:Class')
+local Injector = require('Module:Infobox/Widget/Injector')
+local Cell = require('Module:Infobox/Widget/Cell')
+local PrizePoolCurrency = require('Module:Prize pool currency')
+local SUPERCELL_SPONSORED_ICON = '[[File:Supercell icon.png|x18px|link=Supercell|Tournament sponsored by Supercell.]]'
+
+local _TODAY = os.date('%Y-%m-%d', os.time())
+
+local _args
+local _league
+
+local CustomLeague = Class.new()
+local CustomInjector = Class.new(Injector)
+
+function CustomLeague.run(frame)
+	local league = League(frame)
+	_league = league
+	_args = _league.args
+
+	league.createWidgetInjector = CustomLeague.createWidgetInjector
+	league.defineCustomPageVariables = CustomLeague.defineCustomPageVariables
+	league.addToLpdb = CustomLeague.addToLpdb
+	league.getWikiCategories = CustomLeague.getWikiCategories
+
+	return league:createInfobox(frame)
+end
+
+function CustomLeague:createWidgetInjector()
+	return CustomInjector()
+end
+
+function CustomInjector:addCustomCells(widgets)
+	local args = _args
+	table.insert(widgets, Cell{
+		name = 'Teams',
+		content = {args.team_number}
+	})
+
+	return widgets
+end
+
+function CustomInjector:parse(id, widgets)
+	local args = _args
+	if id == 'prizepool' then
+		return {
+			Cell{
+				name = 'Prize pool',
+				content = {CustomLeague:_createPrizepool()}
+			},
+		}
+	elseif id == 'liquipediatier' then
+		local tierDisplay = CustomLeague:_createLiquipediaTierDisplay()
+		local class
+		if args['supercell-sponsored'] == 'true' then
+			tierDisplay = (tierDisplay or '').. '&nbsp;' .. SUPERCELL_SPONSORED_ICON
+			class = {'valvepremier-highlighted'}
+		end
+		return {
+			Cell{
+				name = 'Liquipedia Tier',
+				content = {tierDisplay},
+				classes = class
+			}
+		}
+	end
+	return widgets
+end
+
+function CustomLeague:addToLpdb(lpdbData, args)
+	lpdbData.publishertier = args['supercell-sponsored']
+	lpdbData.participantsnumber = args.team_number
+
+	return lpdbData
+end
+
+function CustomLeague:_createPrizepool()
+	if String.isEmpty(_args.prizepool) and String.isEmpty(_args.prizepoolusd) then
+		return nil
+	end
+	local date
+	if String.isNotEmpty(_args.currency_rate) then
+		date = _args.currency_date
+	end
+
+	return PrizePoolCurrency._get({
+		prizepool = _args.prizepool,
+		prizepoolusd = _args.prizepoolusd,
+		currency = _args.localcurrency,
+		rate = _args.currency_rate,
+		date = date or Variables.varDefault('tournament_enddate', _TODAY),
+	})
+end
+
+function CustomLeague:_createLiquipediaTierDisplay()
+	local tier = _args.liquipediatier or ''
+	local tierType = _args.liquipediatiertype or ''
+	if String.isEmpty(tier) then
+		return nil
+	end
+
+	local function buildTierString(tierString)
+		local tierText = Tier.text[tierString]
+		if not tierText then
+			table.insert(_league.warnings, tierString .. ' is not a known Liquipedia Tier/Tiertype')
+			return ''
+		else
+			return '[[' .. tierText .. ' Tournaments|' .. tierText .. ']]'
+		end
+	end
+
+	local tierDisplay = buildTierString(tier)
+
+	if String.isNotEmpty(tierType) then
+		tierDisplay = buildTierString(tierType) .. '&nbsp;(' .. tierDisplay .. ')'
+	end
+
+	return tierDisplay
+end
+
+function CustomLeague:defineCustomPageVariables()
+	--Legacy vars
+	Variables.varDefine('tournament_ticker_name', _args.tickername or '')
+	Variables.varDefine('tournament_tier', _args.liquipediatier or '')
+	Variables.varDefine('tournament_prizepool', _args.prizepool or '')
+
+	--Legacy date vars
+	local sdate = Variables.varDefault('tournament_startdate', '')
+	local edate = Variables.varDefault('tournament_enddate', '')
+	Variables.varDefine('tournament_sdate', sdate)
+	Variables.varDefine('tournament_edate', edate)
+	Variables.varDefine('tournament_date', edate)
+	Variables.varDefine('date', edate)
+	Variables.varDefine('sdate', sdate)
+	Variables.varDefine('edate', edate)
+end
+
+function CustomLeague:getWikiCategories(args)
+	local categories = {}
+
+	local tier = args.liquipediatier
+	local tierType = args.liquipediatiertype
+
+	if String.isNotEmpty(tier) and String.isNotEmpty(Tier.text[tier]) then
+		table.insert(categories, Tier.text[tier]  .. ' Tournaments')
+	end
+
+	if String.isNotEmpty(tierType) and String.isNotEmpty(Tier.text[tierType]) then
+		table.insert(categories, Tier.text[tierType] .. ' Tournaments')
+	end
+
+	return categories
+end
+
+return CustomLeague
