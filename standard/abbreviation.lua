7c5c6a965ee7ef399b13346145ee19c8ba6bbb2b
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2022-01-25 10:33:58 +0100

    Create abbreviation module (#1005)
    * Create abbreviation.lua
    
    * mirror the templates input order

---
+++ standard/abbreviation.lua
@@ -0,0 +1,21 @@
+---
+-- @Liquipedia
+-- wiki=commons
+-- page=Module:Abbreviation
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Abbreviation = {}
+
+local Class = require('Module:Class')
+local String = require('Module:StringUtils')
+
+function Abbreviation.make(text, title)
+	if String.isEmpty(title) or String.isEmpty(text) then
+		return nil
+	end
+	return '<abbr title="' .. title .. '>' .. text .. '</abbr>'
+end
+
+return Class.export(Abbreviation)
