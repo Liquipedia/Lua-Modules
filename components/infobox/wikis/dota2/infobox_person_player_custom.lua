47f1d36807dfa1ebd621dd2557baad584268b0e4
Jasper van Riet <jvanriet@teamliquid.com>
2022-01-25 11:05:17 +0100

    Remove unneeded code

--- components/infobox/wikis/dota2/infobox_person_player_custom.lua
+++ components/infobox/wikis/dota2/infobox_person_player_custom.lua
@@ -75,9 +75,7 @@ function CustomPlayer.run(frame)
 	player.args.informationType = player.args.informationType or 'Player'
 
 	player.adjustLPDB = CustomPlayer.adjustLPDB
-	player.defineCustomPageVariables = CustomPlayer.defineCustomPageVariables
 	player.createBottomContent = CustomPlayer.createBottomContent
-	player.getCategories = CustomPlayer.getCategories
 	player.createWidgetInjector = CustomPlayer.createWidgetInjector
 
 	return player:createInfobox(frame)
@@ -183,13 +181,6 @@ function CustomPlayer:adjustLPDB(lpdbData)
 	lpdbData.extradata.playerid = _args.playerid
 
 	return lpdbData
-end
-
-function CustomPlayer:defineCustomPageVariables(args)
-	--retrieve birth month + day for storage in smw
-	local birthMonthAndDay = string.match(args.birth_date or '', '%-%d%d?%-%d%d?$')
-	birthMonthAndDay = string.gsub(birthMonthAndDay or '', '^%-', '')
-	Variables.varDefine('birthday', birthMonthAndDay)
 end
 
 function CustomPlayer:createBottomContent(infobox)
