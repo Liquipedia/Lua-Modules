466317b897cbce066d1f2cf3ad559b934c461b0f
Hesketh2 <88981446+Hesketh2@users.noreply.github.com>
2022-01-26 00:46:42 +0700

    Add Earnings for AOV
    Since AOV and ML will share pretty much the same set of modules/infobox, while Player infobox is still the old version for AOV  I decide to put earnings up first
    
    
    same file as ML

---
+++ components/earnings/wikis/arenaofvalor/earnings.lua
@@ -0,0 +1,29 @@
+---
+-- @Liquipedia
+-- wiki=arenaofvalor
+-- page=Module:Earnings
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Class = require('Module:Class')
+local Lua = require('Module:Lua')
+
+local CustomEarnings = Lua.import('Module:Earnings/Base', {requireDevIfEnabled = true})
+
+CustomEarnings.defaultNumberOfPlayersInTeam = 5
+
+-- Legacy entry points
+function CustomEarnings.calc_player(input)
+    local args = input.args
+
+    return CustomEarnings.calculateForPlayer(args)
+end
+
+function CustomEarnings.calc_team(input)
+    local args = input.args
+
+    return CustomEarnings.calculateForTeam(args)
+end
+
+return Class.export(CustomEarnings)
