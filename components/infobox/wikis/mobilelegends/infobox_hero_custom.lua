9e45177285f08ea5a5e7934cbead72ff4234a9f8
Hesketh2 <88981446+Hesketh2@users.noreply.github.com>
2022-01-28 20:35:21 +0700

    1st Update
    a lot of stuff, but I had no understanding on how to write a new function
    
    so HeroWL function looks super scuffed and this likely dont pass the test

--- components/infobox/wikis/mobilelegends/infobox_hero_custom.lua
+++ components/infobox/wikis/mobilelegends/infobox_hero_custom.lua
@@ -6,13 +6,14 @@
 -- Please see https://github.com/Liquipedia/Lua-Modules to contribute
 --
 
-local Unit = require('Module:Infobox/Unit')
+local Unit = require('Module:Infobox/Unit/dev')
 local String = require('Module:StringUtils')
 local Namespace = require('Module:Namespace')
 local ClassIcon = require('Module:ClassIcon')
 local Math = require('Module:Math')
 local HeroWL = require('Module:HeroWL')
 local Class = require('Module:Class')
+local Table = require('Module:Table')
 
 local Injector = require('Module:Infobox/Widget/Injector')
 local Cell = require('Module:Infobox/Widget/Cell')
@@ -20,7 +21,7 @@ local Title = require('Module:Infobox/Widget/Title')
 local Header = require('Module:Infobox/Widget/Header')
 local Breakdown = require('Module:Infobox/Widget/Breakdown')
 
-local CustomHero = Class.new()
+local CustomHero = {}
 
 local CustomInjector = Class.new(Injector)
 
@@ -28,8 +29,8 @@ local _args
 
 local _pagename = mw.title.getCurrentTitle().text
 
-local _BATTLE_POINTS_ICON = '[[File:Mobile_Legends_BP_icon.png|x16px|Battle Points|link=Battle Points]]'
-local _DIAMONDS_ICON = '[[File:Mobile_Legends_Diamond_icon.png|Diamonds|x16px|link=Diamonds]]'
+local _BATTLE_POINTS_ICON = '[[File:Mobile_Legends_BP_icon.png|x16px|Battle Points|link=Battle Point]]'
+local _DIAMONDS_ICON = '[[File:Mobile_Legends_Diamond_icon.png|Diamonds|x16px|link=Diamond]]'
 
 function CustomHero.run(frame)
 	local unit = Unit(frame)
@@ -45,64 +46,55 @@ end
 
 function CustomInjector:addCustomCells()
 	local widgets = {
-		Cell{name = 'Resource Bar', content = {_args.secondarybar}},
-		Cell{name = 'Secondary Bar', content = {_args.secondarybar1}},
+		Cell{name = 'Specialty', content = {_args.specialty}},
+		Cell{name = 'Attack Type', content = {_args.attacktype}},
+		Cell{name = 'Resource Bar', content = {_args.resourcebar}},
+		Cell{name = 'Secondary Bar', content = {_args.secondarybar}},
 		Cell{name = 'Secondary Attributes', content = {_args.secondaryattributes1}},
 		Cell{name = 'Release Date', content = {_args.releasedate}},
-		Cell{name = 'Species', content = {_args.species}},
-		Cell{name = 'Year of birth', content = {_args.birth}},
-		Cell{name = 'Faction(s)', content = {_args.factions}},
 	}
 
-	if
-		String.isNotEmpty(_args.hp) or
-		String.isNotEmpty(_args.hplvl) or
-		String.isNotEmpty(_args.hpreg) or
-		String.isNotEmpty(_args.hpreglvl)
-	then
+	local statisticsCells = {
+		hp = 'Health',
+		hpreg = 'Health Regen',
+		mana = 'Mana',
+		manareg = 'Mana Regen',
+		cdr = 'Cooldown Reduction',
+		energy = 'Energy',
+		energyreg = 'Energy Regen',
+		attacktype = 'Attack Type',
+		damage = 'Attack Damage',
+		attackspeed = 'Attack Speed',
+		attackrange = 'Attack Range',
+		ap = 'Ability Power',
+		armor = 'Armor',
+		magicresistance = 'Magic Resistance',
+		movespeed = 'Movement Speed',
+	}
+	if Table.any(_args, function(key) return statisticsCells[key] end) then
 		table.insert(widgets, Title{name = 'Base Statistics'})
+		for key, display in pairs(statisticsCells) do
+			table.insert(widgets, Cell{name = display, content = {_args[key]}})
+		end
 	end
-
-	table.insert(widgets, Cell{name = 'Health', content = {_args.hp}})
-	table.insert(widgets, Cell{name = 'Health Regen', content = {_args.hpreg}})
-	table.insert(widgets, Cell{name = 'Mana', content = {_args.mana}})
-	table.insert(widgets, Cell{name = 'Mana Regen', content = {_args.manareg}})
-	table.insert(widgets, Cell{name = 'Cooldown Reduction', content = {_args.cdr}})
-	table.insert(widgets, Cell{name = 'Energy', content = {_args.energy}})
-	table.insert(widgets, Cell{name = 'Energy Regen', content = {_args.energyreg}})
-	table.insert(widgets, Cell{name = 'Attack Type', content = {_args.attacktype}})
-	table.insert(widgets, Cell{name = 'Attack Damage', content = {_args.damage}})
-	table.insert(widgets, Cell{name = 'Attack Speed', content = {_args.attackspeed}})
-	table.insert(widgets, Cell{name = 'Attack Range', content = {_args.attackrange}})
-	table.insert(widgets, Cell{name = 'Ability Power', content = {_args.ap}})
-	table.insert(widgets, Cell{name = 'Armor', content = {_args.armor}})
-	table.insert(widgets, Cell{name = 'Magic Resistance', content = {_args.magicresistance}})
-	table.insert(widgets, Cell{name = 'Movement Speed', content = {_args.movespeed}})
 	table.insert(widgets, Title{name = 'Esports Statistics'})
-
-	local stats = HeroWL.create({hero = _args.heroname or _pagename})
-	stats = mw.text.split(stats, ';')
-	local winPercentage = (tonumber(stats[1]) or 0) / ((tonumber(stats[1]) or 0) + (tonumber(stats[2]) or 1))
-	winPercentage = Math.round({winPercentage, 4}) * 100
-	local statsDisplay = (stats[1] or 0) .. 'W : ' .. (stats[2] or 0) .. 'L (' .. winPercentage .. '%)'
-	table.insert(widgets, Cell{name = 'Win Rate', content = {statsDisplay}})
+	
+function _HeroStatsDisplay() 
+			local stats = HeroWL.create({hero = _args.heroname or _pagename})
+			stats = mw.text.split(stats, ';')
+			local winPercentage = (tonumber(stats[1]) or 0) / ((tonumber(stats[1]) or 0) + (tonumber(stats[2]) or 1))
+			winPercentage = Math.round({winPercentage, 4}) * 100
+			local statsDisplay = (stats[1] or 0) .. 'W : ' .. (stats[2] or 0) .. 'L (' .. winPercentage .. '%)'
+	return stats
+end
+	
+			table.insert(widgets, Cell{name = 'Win Rate', content = {_HeroStatsDisplay}})
 
 	return widgets
 end
 
 function CustomInjector:parse(id, widgets)
-	if id == 'header' then
-		return {
-			Header{
-				name = _args.heroname,
-				subHeader = _args.title,
-				image = _args.image,
-				imageDefault = _args.default,
-				imageDark = _args.imagedark or _args.imagedarkmode,
-				imageDefaultDark = _args.defaultdark or _args.defaultdarkmode,
-			},
-		}
-	elseif id == 'type' then
+	if id == 'type' then
 		local breakDownContents = {}
 		local lane = _args.lane
 		if String.isNotEmpty(lane) then
