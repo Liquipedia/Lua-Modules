166a10c1cdabe436c7bae33fdb8c75860b06b44b
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2022-02-02 08:31:55 +0100

    alternative to #1030

--- standard/stream_links.lua
+++ standard/stream_links.lua
@@ -11,6 +11,10 @@ Module containing utility functions for streaming platforms.
 ]]
 local StreamLinks = {}
 
+local Logic = require('Module:Logic')
+local String = require('Module:StringUtils')
+local Variables = require('Module:Variables')
+
 --[[
 List of streaming platforms supported in Module:Countdown. This is a subset of
 the list in Module:Links
@@ -49,6 +53,47 @@ function StreamLinks.readCountdownStreams(args)
 		stream[platformName] = args[platformName]
 	end
 	return stream
+end
+
+--[[
+Resolves the value of a stream given the platform
+]]
+function StreamLinks.resolve(platformName, streamValue)
+	local streamLink = mw.ext.StreamPage.resolve_stream(platformName, streamValue)
+
+	return string.gsub(streamLink, 'Special:Stream/' .. platformName, '')
+end
+
+--[[
+Extracts the streaming platform args from an argument table or a nested streams table inside the arguments table.
+Uses variable fallbacks and resolves stream redirects.
+]]
+function StreamLinks.processStreams(args)
+	local streams = args.streams or {}
+
+	-- handle args.stream differently due to it having no platform
+	streams.stream = Logic.emptyOr(streams.stream or args.stream, Variables.varDefault('stream'))
+
+	for _, platformName in pairs(StreamLinks.countdownPlatformNames) do
+		local streamValue = Logic.emptyOr(streams[platformName] or args[platformName], Variables.varDefault(platformName))
+
+		-- stream has no platform
+		if platformName == 'stream' then
+			streams.stream = streamValue
+		else
+			-- twitch2 is not a platform but uses the twitch platform instead
+			local lookUpPlatform = platformName
+			if platformName == 'twitch2' then
+				lookUpPlatform = 'twitch'
+			end
+
+			if String.isNotEmpty(streamValue) then
+				streams[platformName] = StreamLinks.resolve(lookUpPlatform, streamValue)
+			end
+		end
+	end
+
+	return streams
 end
 
 return StreamLinks
