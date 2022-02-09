b304b18c44f03f9221ffbca8d1537af18845c906
Cameron <58013431+FenrirWulf@users.noreply.github.com>
2022-02-09 08:30:01 -0800

    CustomLink data for Rainbow Six (#1001)
    * CustomLink data for Rainbow Six
    
    * Update links_custom_data.lua
    
    * Update links_custom_data.lua
    
    * Removing SiegeGG

---
+++ standard/links/wikis/rainbowsix/links_custom_data.lua
@@ -0,0 +1,17 @@
+---
+-- @Liquipedia
+-- wiki=rainbowsix
+-- page=Module:Links/CustomData
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+return {
+	prefixes = {
+		faceit = {
+			'https://www.faceit.com/en/championship/',
+			team = 'https://www.faceit.com/teams/',
+			player = 'https://www.faceit.com/players/',
+		}
+	},
+}
