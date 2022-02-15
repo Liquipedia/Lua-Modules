009eebdf99ec654542ecd6e2c469722150ff543a
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2022-02-15 10:49:11 +0100

    add comment with info and example

--- standard/lpdb.lua
+++ standard/lpdb.lua
@@ -8,6 +8,38 @@
 
 local Lpdb = {}
 
+-- Executes a mass query.
+[==[
+Loops Lpdb queries to e.g.
+- circumvent the maximum limit of 5000
+- use additional filtering (e.g. because lpdb does not support it)
+	and query so long until a certain amount of elements is found
+	or additional limitations are reached
+
+example:
+	local foundMatchIds = {}
+	local getMatchId = function(match)
+		if #foundMatchIds < args.matchLimit then
+			if HeadToHead._fitsAdditionalConditions(args, match) then
+				table.insert(foundMatchIds, match.match2id)
+			end
+		else
+			return false
+		end
+	end
+
+	local queryParameters = {
+		conditions = conditions,
+		order = 'date ' .. args.order,
+		limit = _LPDB_QUERY_LIMIT,
+		query = 'pagename, winner, walkover, finished, date, dateexact, links, '
+			.. 'bestof, vod, tournament, tickername, shortname, icon, icondark, '
+			.. 'extradata, match2opponents, match2games, mode, match2id, match2bracketid',
+	}
+	Lpdb.executeMassQuery('match2', queryParameters, getMatchId)
+
+	return foundMatchIds
+]==]
 function Lpdb.executeMassQuery(lpdbTable, queryParameters, callbackFunction, limit)
 	queryParameters.offset = queryParameters.offset or 0
 	queryParameters.limit = queryParameters.limit or 5000
