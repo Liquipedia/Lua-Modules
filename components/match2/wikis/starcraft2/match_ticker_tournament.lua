fd5296af83dfe26ff0685c54ecb209d36f0434fa
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2022-02-16 13:14:24 +0100

    Update match_ticker_tournament.lua

--- components/match2/wikis/starcraft2/match_ticker_tournament.lua
+++ components/match2/wikis/starcraft2/match_ticker_tournament.lua
@@ -1,6 +1,6 @@
 ---
 -- @Liquipedia
--- wiki=starcraft2
+-- wiki=commons
 -- page=Module:MatchTicker/Tournament
 --
 -- Please see https://github.com/Liquipedia/Lua-Modules to contribute
@@ -9,87 +9,95 @@
 local Class = require('Module:Class')
 local Lua = require('Module:Lua')
 local Logic = require('Module:Logic')
+local String = require('Module:StringUtils')
 local Variables = require('Module:Variables')
 local MatchGroupWorkaround = require('Module:MatchGroup/Workaround')
+
 local MatchTicker = Lua.import('Module:MatchTicker', {requireDevIfEnabled = true})
-MatchTicker.OpponentDisplay = Lua.import('Module:OpponentDisplay/Starcraft', {requireDevIfEnabled = true})
-MatchTicker.Opponent = Lua.import('Module:Opponent/Starcraft', {requireDevIfEnabled = true})
+local Query = MatchTicker.Query
+local Display = MatchTicker.Display
+local HelperFunctions = MatchTicker.HelperFunctions
+
+local _CURRENT_TIME_STAMP = os.date('!%Y-%m-%d %H:%M', os.time(os.date("!*t")))
 
 local TournamentMatchTicker = {}
 
 function TournamentMatchTicker.run(args)
 	args = args or {}
 
+	-- add defaultValue for args.tournament if empty
+	if String.isEmpty(args.tournament) and String.isEmpty(args.tournament1) then
+		args.tournament = mw.title.getCurrentTitle().text
+	end
+
 	-- tournament match ticker always has ongoing and
 	-- upcoming matches but not recent matches
-	args.recent = nil
+	args.recent = false
 	args.upcoming = true
 	args.ongoing = true
 
-	local lpdbConditions = MatchTicker.LpdbConditions()
-	lpdbConditions:addDefaultConditions(args)
+	local lpdbConditions = Query.BaseConditions()
+		:addDefaultConditions(args)
+		:build(args)
+		:toString()
 
-	if Logic.readBool(args.featured) then
-		lpdbConditions:addCondition('[[extradata_featured::true]]')
-	end
-
-	local lpdb = MatchTicker.Lpdb()
-	lpdb:conditions(lpdbConditions:build())
+	local lpdb = Query.Query()
+	lpdb:setConditions(lpdbConditions)
 	if Logic.readBool(args.recent) then
-		lpdb:order('date desc, liquipediatier asc, tournament asc')
+		lpdb:setOrder('date desc, liquipediatier asc, tournament asc')
 	end
 	local limitInput = tonumber(args.limit or 8) or 8
-	lpdb:limit(limitInput)
+	lpdb:setLimit(limitInput)
 
 	local data = lpdb:get()
 	if not data then
 		return ''
 	end
 
-	local wrapper = MatchTicker.Wrapper()
+	local wrapper = Display.Wrapper()
 
 	local game = Variables.varDefault('tournament_game', '')
 	wrapper:addClass('fo-nttax-infobox-wrapper infobox-' .. game)
 	wrapper:addInnerWrapperClass('fo-nttax-infobox wiki-bordercolor-light')
 
-	local header = MatchTicker.Header()
+	local header = Display.Header()
 	header:text('Upcoming matches')
 	wrapper:addElement(header:create())
 
 	local matchIndex = 1
 	local limitCounter = 1
-	while data[matchIndex] and limitCounter <= limitInput do
-		local item = data[matchIndex]
-		-- workaround for a lpdb bug
+	local item = data[matchIndex]
+	while item and limitCounter <= limitInput do
+		-- workaround for a lpdb match2 bug
 		-- remove when it is fixed
 		MatchGroupWorkaround.applyPlayerBugWorkaround(item)
 		if TournamentMatchTicker._isValidMatch(item) then
 			limitCounter = limitCounter + 1
-			wrapper:addElement(TournamentMatchTicker._match(item))
+			wrapper:addElement(TournamentMatchTicker._match(item, args))
 		end
 		matchIndex = matchIndex + 1
+		item = data[matchIndex]
 	end
 
 	return wrapper:create()
 end
 
 function TournamentMatchTicker._isValidMatch(match)
-	return not ( MatchTicker.isByeOpponent(match.match2opponents[1])
-		or MatchTicker.isByeOpponent(match.match2opponents[1])
+	return not ( HelperFunctions.isByeOpponent(match.match2opponents[1])
+		or HelperFunctions.isByeOpponent(match.match2opponents[1])
 	)
 end
 
-local _CURRENT_TIME_STAMP = os.date('!%Y-%m-%d %H:%M', os.time(os.date("!*t")))
-function TournamentMatchTicker._match(matchData)
+function TournamentMatchTicker._match(matchData, args)
 	local winner = tonumber(matchData.winner or 0) or 0
 
-	local upperRow = MatchTicker.UpperRow()
+	local upperRow = Display.UpperRow()
 
-	upperRow:addOpponent(matchData.match2opponents[1], 'left')
-	upperRow:addOpponent(matchData.match2opponents[2], 'right')
+	upperRow:addOpponent(matchData.match2opponents[1], 1)
+	upperRow:addOpponent(matchData.match2opponents[2], 2)
 	upperRow:winner(winner)
 
-	local versus = MatchTicker.Versus()
+	local versus = Display.Versus()
 	versus:bestOf(tonumber(matchData.bestof or ''))
 	-- check if the match is live
 	if matchData.date <= _CURRENT_TIME_STAMP then
@@ -97,17 +105,17 @@ function TournamentMatchTicker._match(matchData)
 	end
 	upperRow:versus(versus:create())
 
-	local lowerRow = MatchTicker.LowerRow()
+	local lowerRow = Display.LowerRow()
 
-	local countDownArgs = {rawcountdown = 'true'}
-	for key, item in pairs(matchData.stream or {}) do
-		countDownArgs[key] = item
-	end
+	local countDownArgs = matchData.stream or {}
+	countDownArgs.rawcountdown = 'true'
 	lowerRow:countDown(matchData, countDownArgs)
 
-	lowerRow:tournament(matchData)
+	if Logic.readBool(args.show_tournament) then
+		lowerRow:tournament(matchData)
+	end
 
-	local match = MatchTicker.Match()
+	local match = Display.Match()
 	match:upperRow(upperRow:create())
 	match:lowerRow(lowerRow:create())
 
