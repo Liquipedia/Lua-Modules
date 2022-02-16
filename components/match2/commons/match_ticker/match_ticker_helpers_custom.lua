517d00353c5e609030b9fb5f800ae590eee04701
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2022-02-16 13:52:33 +0100

    Create match_ticker_helpers_custom.lua

---
+++ components/match2/commons/match_ticker/match_ticker_helpers_custom.lua
@@ -0,0 +1,11 @@
+---
+-- @Liquipedia
+-- wiki=commons
+-- page=Module:MatchTicker/Helpers/Custom
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Lua = require('Module:Lua')
+
+return Lua.import('Module:MatchTicker/Helpers', {requireDevIfEnabled = true})
