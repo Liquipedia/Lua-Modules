a638ee7957bc62b14360d45dc2de8b20f6f52a98
Cameron <58013431+FenrirWulf@users.noreply.github.com>
2022-02-18 12:29:07 -0800

    Adding flag for location display

--- components/infobox/wikis/overwatch/infobox_map_custom.lua
+++ components/infobox/wikis/overwatch/infobox_map_custom.lua
@@ -7,11 +7,12 @@
 --
 
 local Class = require('Module:Class')
-local Map = require('Module:Infobox/Map')
+local Map = require('Module:Infobox/Map/dev')
 local Injector = require('Module:Infobox/Widget/Injector')
 local Cell = require('Module:Infobox/Widget/Cell')
 local MapModes = require('Module:MapModes')
 local String = require('Module:StringUtils')
+local Flags = require('Module:Flags')
 
 local CustomMap = Class.new()
 
@@ -45,6 +46,19 @@ function CustomInjector:addCustomCells(widgets)
 	return widgets
 end
 
+function CustomInjector:parse(id, widgets)
+	mw.logObject(_args)
+	if id == 'location' then
+		return {
+			Cell{
+				name = 'Location',
+				content = {Flags.Icon{flag = _args.location, shouldLink = false} .. '&nbsp;' .. _args.location}
+			},
+		}
+	end
+	return widgets
+end
+
 function CustomMap._getGameMode()
 	if String.isEmpty(_args.mode) and String.isEmpty(_args.mode1) then
 		return {}
@@ -60,7 +74,7 @@ function CustomMap._getGameMode()
 		table.insert(modeDisplayTable, mapModeDisplay)
 	end
 	return modeDisplayTable
-end
+end	
 
 function CustomMap:addToLpdb(lpdbData)
 	lpdbData.extradata.creator = mw.ext.TeamLiquidIntegration.resolve_redirect(_args.creator)
