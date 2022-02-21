fded69b5751eb1e6aed482541a64d592208ba3f6
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2022-02-21 20:17:30 +0100

    Create match_ticker_display.lua

---
+++ components/match2/commons/match_ticker/match_ticker_display.lua
@@ -0,0 +1,51 @@
+---
+-- @Liquipedia
+-- wiki=commons
+-- page=Module:MatchTicker/Display
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+-- Holds DisplayComponents for the MatchTicker modules
+
+local Class = require('Module:Class')
+local Countdown = require('Module:Countdown')
+local Lua = require('Module:Lua')
+local String = require('Module:StringUtils')
+local LeagueIcon = require('Module:LeagueIcon')
+local VodLink = require('Module:VodLink')
+local HelperFunctions = Lua.import('Module:MatchTicker/Helpers', {requireDevIfEnabled = true})
+
+local MatchTickerDisplay = Class.new()
+
+local _LEFT_SIDE = 1
+local _RIGHT_SIDE = 2
+local _TBD = 'TBD'
+local _SIDE_CLASS = {
+	'left',
+	'right',
+}
+local _WINNER_LEFT = 1
+local _WINNER_RIGHT = 2
+local _TOURNAMENT_DEFAULT_ICON = 'InfoboxIcon_Tournament.png'
+local _MATCH_FINISHED = 1
+local _ABBR_UTC = '<abbr data-tz="+0:00" title="Coordinated Universal Time (UTC)">UTC</abbr>'
+
+MatchTickerDisplay.OpponentDisplay = Lua.import('Module:OpponentDisplay', {requireDevIfEnabled = true})
+MatchTickerDisplay.Opponent = Lua.import('Module:Opponent', {requireDevIfEnabled = true})
+
+
+
+--classes here step by step
+
+
+
+MatchTickerDisplay.Header = Header
+MatchTickerDisplay.Match = Match
+MatchTickerDisplay.UpperRow = UpperRow
+MatchTickerDisplay.LowerRow = LowerRow
+MatchTickerDisplay.Versus = Versus
+MatchTickerDisplay.Wrapper = Wrapper
+MatchTickerDisplay.HelperFunctions = HelperFunctions
+
+return MatchTickerDisplay
