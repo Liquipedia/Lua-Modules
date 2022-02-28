a3a3ebaeb6b2295e79eeb095ceb124da865447bd
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2022-02-28 13:14:42 +0100

    Outsource EPT Season value (#1076)
    * Outsource EPT Season value
    
    * Update infobox_person_player_custom.lua
    
    * Update components/infobox/wikis/starcraft2/infobox_person_player_custom.lua
    
    Co-authored-by: Jasper van Riet <JaspervanRiet@users.noreply.github.com>
    
    Co-authored-by: Jasper van Riet <JaspervanRiet@users.noreply.github.com>

--- components/infobox/wikis/starcraft2/infobox_person_player_custom.lua
+++ components/infobox/wikis/starcraft2/infobox_person_player_custom.lua
@@ -18,7 +18,7 @@ local CleanRace = require('Module:CleanRace')
 local Math = require('Module:Math')
 local Matches = require('Module:Upcoming ongoing and recent matches player/new')
 
-local _EPT_SEASON = 2021
+local _EPT_SEASON = mw.loadData('Module:Series/EPT/config').currentSeason
 
 local _PAGENAME = mw.title.getCurrentTitle().prefixedText
 local _DISCARD_PLACEMENT = '99'
