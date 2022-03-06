7489f89b3c67c3315de78e0d58b56ea5496d0eaa
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2022-03-06 12:15:18 +0100

    Update matchgroup_input_custom.lua

--- components/match2/wikis/dota2/matchgroup_input_custom.lua
+++ components/match2/wikis/dota2/matchgroup_input_custom.lua
@@ -650,7 +650,6 @@ function opponentFunctions.getIcon(template)
 		local iconDark = Logic.emptyOr(raw.imagedark, raw.legacyimagedark)
 		return icon, iconDark
 	end
-	return false
 end
 
 return CustomMatchGroupInput
