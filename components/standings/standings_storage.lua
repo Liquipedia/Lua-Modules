4bad116341803ba7bab60403d04f2ad2e17043eb
Rikard Blixt <rikardblixt@gmail.com>
2022-03-10 09:32:12 +0100

    Save parent to standings if set (#1092)
    * (standing) Save parent
    
    * Removed parentname due to lack of usecase

--- components/standings/standings_storage.lua
+++ components/standings/standings_storage.lua
@@ -36,6 +36,7 @@ function StandingsStorage.run(index, data)
 			standingsindex = data.standingsindex,
 			section = Variables.varDefault('last_heading', ''),
 			roundindex = data.roundindex,
+			parent = Variables.varDefault('tournament_parent', ''),
 			extradata = mw.ext.LiquipediaDB.lpdb_create_json({data.extradata or {}})
 		}
 	)
