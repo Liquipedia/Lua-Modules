56ca248707204a32682e7880ef01d3a0c52ca71a
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2022-03-16 11:31:49 +0100

    Update infobox_person_custom_shared.lua

--- components/infobox/wikis/starcraft2/infobox_person_custom_shared.lua
+++ components/infobox/wikis/starcraft2/infobox_person_custom_shared.lua
@@ -109,7 +109,7 @@ function CustomPerson.getRaceData(race, asCategory)
 			mw.text.nowiki('Error: Invalid Race') .. '</strong>'
 	else
 		if asCategory then
-			for raceIndex, raceValue in ipairs(raceTable) do
+			for raceIndex, raceValue in ipairs(raceTable or {}) do
 				raceTable[raceIndex] = ':Category:' .. raceValue .. ' Players|' .. raceValue .. ']]'
 					.. '[[Category:' .. raceValue .. ' Players'
 			end
