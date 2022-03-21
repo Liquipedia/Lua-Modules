392be9c6f541095136bf60d86e187aadea1035d4
leonidkim01 <42477808+leonidkim01@users.noreply.github.com>
2022-03-21 20:18:16 +0600

    Remov indents from copy-paste generator

--- components/match2/wikis/dota2/get_match_group_copy_paste_wiki.lua
+++ components/match2/wikis/dota2/get_match_group_copy_paste_wiki.lua
@@ -19,8 +19,6 @@ local wikiCopyPaste = Table.copy(require('Module:GetMatchGroupCopyPaste/wiki/Bas
 
 --returns the Code for a Match, depending on the input
 function wikiCopyPaste.getMatchCode(bestof, mode, index, opponents, args)
-	local indent = '    '
-
 	if bestof == 0 and args.score ~= 'false' then
 		args.score = 'true'
 	end
@@ -29,57 +27,55 @@ function wikiCopyPaste.getMatchCode(bestof, mode, index, opponents, args)
 
 	local lines = Array.extend(
 		'{{Match2', -- Template:Match is used by match1 for now. Using Template:Match2 until it has been worked away.
-		index == 1 and (indent .. '|bestof=' .. (bestof ~= 0 and bestof or '')) or nil,
-		args.needsWinner == 'true' and indent .. '|winner=' or nil
+		index == 1 and ('|bestof=' .. (bestof ~= 0 and bestof or '')) or nil,
+		args.needsWinner == 'true' and '|winner=' or nil
 	)
 
 	for i = 1, opponents do
-		table.insert(lines, indent .. '|opponent' .. i .. '=' .. wikiCopyPaste._getOpponent(mode, displayScore))
+		table.insert(lines, '|opponent' .. i .. '=' .. wikiCopyPaste._getOpponent(mode, displayScore))
 	end
 
 	if args.hasDate == 'true' then
 		Array.appendWith(lines,
-			indent .. '|date=',
-			indent .. '|finished=',
-			indent .. '|twitch='
+			'|date=',
+			'|finished=',
+			'|twitch='
 		)
 	end
 
 	for i = 1, bestof do
-		Array.appendWith(lines, indent .. '|vodgame'.. i ..'=')
+		Array.appendWith(lines, '|vodgame' .. i .. '=')
 	end
 
 	for i = 1, bestof do
-		Array.appendWith(lines, indent .. '|matchid'.. i ..'=')
+		Array.appendWith(lines, '|matchid' .. i .. '=')
 	end
 
 	for i = 1, bestof do
 		Array.appendWith(lines,
-			indent .. '|map' .. i .. '={{Map',
-			indent .. indent .. '|team1side=',
-			indent .. indent .. '|t1h1=|t1h2=|t1h3=|t1h4=|t1h5='
+			'|map' .. i .. '={{Map',
+			'|team1side=',
+			'|t1h1=|t1h2=|t1h3=|t1h4=|t1h5='
+		)
+
+		if bans then
+			Array.appendWith(lines, '|t1b1=|t1b2=|t1b3=|t1b4=|t1b5=|t1b6=|t1b7=')
+		end
+
+		Array.appendWith(lines,
+			'|team2side=',
+			'|t2h1=|t2h2=|t2h3=|t2h4=|t2h5='
 		)
 
 		if bans then
 			Array.appendWith(lines,
-				indent .. indent .. '|t1b1=|t1b2=|t1b3=|t1b4=|t1b5=|t1b6=|t1b7='
+				'|t2b1=|t2b2=|t2b3=|t2b4=|t2b5=|t2b6=|t2b7='
 			)
 		end
 
 		Array.appendWith(lines,
-			indent .. indent .. '|team2side=',
-			indent .. indent .. '|t2h1=|t2h2=|t2h3=|t2h4=|t2h5='
-		)
-
-		if bans then
-			Array.appendWith(lines,
-				indent .. indent .. '|t2b1=|t2b2=|t2b3=|t2b4=|t2b5=|t2b6=|t2b7='
-			)
-		end
-
-		Array.appendWith(lines,
-			indent .. indent .. '|length=|winner=',
-			indent .. '}}'
+			'|length=|winner=',
+			'}}'
 		)
 	end
 
