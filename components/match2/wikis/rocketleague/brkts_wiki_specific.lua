21504e515318786de5daec5bb543dd91b134e9d8
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2022-03-26 21:44:44 +0100

    Pass along bestof value to MatchSummary in RL

--- components/match2/wikis/rocketleague/brkts_wiki_specific.lua
+++ components/match2/wikis/rocketleague/brkts_wiki_specific.lua
@@ -8,6 +8,7 @@
 
 local Lua = require('Module:Lua')
 local Table = require('Module:Table')
+local FnUtil = require('Module:FnUtil')
 
 local WikiSpecific = Table.copy(Lua.import('Module:Brkts/WikiSpecific/Base', {requireDevIfEnabled = true}))
 
@@ -18,5 +19,17 @@ function WikiSpecific.getMatchGroupContainer(matchGroupType)
 end
 
 WikiSpecific.defaultIcon = 'Rllogo_std.png'
+
+-- Overwrite default matchFromRecord function to pass bestof value along
+WikiSpecific.baseMatchFromRecord = FnUtil.lazilyDefineFunction(function()
+	return Lua.import('Module:MatchGroup/Util', {requireDevIfEnabled = true}).matchFromRecord
+end)
+
+function WikiSpecific.matchFromRecord(record)
+	local match = WikiSpecific.baseMatchFromRecord(record)
+	match.bestof = record.bestof
+
+	return match
+end
 
 return WikiSpecific
