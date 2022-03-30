3a21ab0b108c3de3ad500e3fa40ee175e62dbddb
Rikard Blixt <rikardblixt@gmail.com>
2022-03-30 08:02:23 +0200

    Add SquadCustom to Commons

---
+++ components/squad/squad_custom.lua
@@ -0,0 +1,22 @@
+---
+-- @Liquipedia
+-- wiki=commons
+-- page=Module:Squad/Custom
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+-- local Squad = require('Module:Squad')
+-- local SquadRow = require('Module:Squad/Row')
+
+local CustomSquad = {}
+
+function CustomSquad.run(frame)
+	-- This function needs to be implemented on the local wiki is using manual Squad Tables
+end
+
+function CustomSquad.runAuto(playerList, squadType)
+	-- This function needs to be implemented on the local wiki if using SquadAuto
+end
+
+return CustomSquad
