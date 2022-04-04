6f167ada0aa1218f9d8ad7c2b5f976bcffa6bdc9
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2022-04-04 19:20:39 +0200

    Update brkts_wiki_specific_base.lua

--- components/match2/commons/brkts_wiki_specific_base.lua
+++ components/match2/commons/brkts_wiki_specific_base.lua
@@ -77,6 +77,8 @@ Called from MatchGroup
 function WikiSpecificBase.getMatchGroupContainer(matchGroupType)
 	return matchGroupType == 'matchlist'
 		and Lua.import('Module:MatchGroup/Display/Matchlist', {requireDevIfEnabled = true}).MatchlistContainer
+		or matchGroupType == 'singleMatch'
+			and Lua.import('Module:MatchGroup/Display/SingleMatch', {requireDevIfEnabled = true}).SingleMatchContainer
 		or Lua.import('Module:MatchGroup/Display/Bracket', {requireDevIfEnabled = true}).BracketContainer
 end
 
