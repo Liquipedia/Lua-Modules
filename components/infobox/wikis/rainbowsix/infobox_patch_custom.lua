33d191fc81b2ab7587e77efa874ea0148907aa00
Cameron <58013431+FenrirWulf@users.noreply.github.com>
2022-04-16 10:48:39 -0700

    Create Siege infobox/Patch

---
+++ components/infobox/wikis/rainbowsix/infobox_patch_custom.lua
@@ -0,0 +1,47 @@
+---
+-- @Liquipedia
+-- wiki=rainbowsix
+-- page=Module:Infobox/Patch/Custom
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Patch = require('Module:Infobox/Patch')
+local Class = require('Module:Class')
+local Variables = require('Module:Variables')
+local Logic = require('Module:Logic')
+local Injector = require('Module:Infobox/Widget/Injector')
+local Cell = require('Module:Infobox/Widget/Cell')
+
+local CustomPatch = Class.new()
+
+local _args
+
+function CustomPatch.run(frame)
+	local customPatch = Patch(frame)
+	_args = customPatch.args
+	customPatch.createWidgetInjector = CustomPatch.createWidgetInjector
+	return customPatch:createInfobox(frame)
+end
+
+function CustomPatch:createWidgetInjector()
+	return CustomInjector()
+end
+
+function CustomInjector:parse(id, widgets)
+	if id == 'release' then
+		return {
+			Cell{
+				name = 'PC Release Date',
+				content = {_args.pcrelease}
+			},
+			Cell{
+				name = 'Console Release Date',
+				content = {_args.consolerelease}
+			},
+		}
+	end
+	return widgets
+end
+
+return CustomTeam
