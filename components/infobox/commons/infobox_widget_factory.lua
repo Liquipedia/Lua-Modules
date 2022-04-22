fe67474363fb8301edb215012dc64a35a3061dd1
Rikard Blixt <rikardblixt@gmail.com>
2022-04-22 17:41:28 +0200

    Exception handling in widget creation

--- components/infobox/commons/infobox_widget_factory.lua
+++ components/infobox/commons/infobox_widget_factory.lua
@@ -8,9 +8,15 @@
 
 local Class = require('Module:Class')
 local Customizable = require('Module:Infobox/Widget/Customizable')
+local String = require('Module:StringUtils')
 local Widget = require('Module:Infobox/Widget')
 
 local WidgetFactory = Class.new()
+
+local _ERROR_TEXT = '<span style="color:#ff0000;font-weight:bold" class="show-when-logged-in">' ..
+					'Unexpected Error, report this in #bugs on our [https://discord.gg/liquipedia Discord]. ' ..
+					'${errorMessage}' ..
+					'</span>[[Category:Pages with script errors]]'
 
 function WidgetFactory.work(widget, injector)
 	local convertedWidgets = {}
@@ -20,22 +26,30 @@ function WidgetFactory.work(widget, injector)
 	end
 
 	if widget:is_a(Customizable) then
-		for _, child in ipairs(widget:make() or {}) do
+		for _, child in ipairs(WidgetFactory._makeWidget(widget) or {}) do
 			if child['is_a'] == nil or child:is_a(Widget) == false then
 				return error('Customizable can only contain Widgets as children')
 			end
 
 			child:setContext({injector = injector})
 
-			for _, item in ipairs(child:make() or {}) do
+			for _, item in ipairs(WidgetFactory._makeWidget(child) or {}) do
 				table.insert(convertedWidgets, item)
 			end
 		end
 	else
-		return widget:make()
+		return WidgetFactory._makeWidget(widget)
 	end
 
 	return convertedWidgets
+end
+
+function WidgetFactory._makeWidget(widget)
+	local result, output = pcall(widget.make, widget) -- Equivalent of widget:make()
+	if not result then
+		output = {String.interpolate(_ERROR_TEXT, {errorMessage = output})}
+	end
+	return output
 end
 
 return WidgetFactory
