2b9215f411351d08ea187dd84e24685fed05c052
Murilo <muriloricci@gmail.com>
2022-04-24 10:59:57 +0200

    Add Infobox League for Brawlhalla (#1079)
    * Add Infobox League for Brawlhalla
    
    * Update from live (should also fix most of the issues the check had)
    
    * remove duplicate
    
    * kick valve premier
    
    after checking with insource search that it is not used anywhere
    
    Co-authored-by: hjpalpha <75081997+hjpalpha@users.noreply.github.com>

---
+++ components/infobox/wikis/brawlhalla/infobox_league_custom.lua
@@ -0,0 +1,141 @@
+---
+-- @Liquipedia
+-- wiki=brawlhalla
+-- page=Module:Infobox/League/Custom
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local League = require('Module:Infobox/League')
+local String = require('Module:StringUtils')
+local Variables = require('Module:Variables')
+local Tier = require('Module:Tier')
+local Class = require('Module:Class')
+local Injector = require('Module:Infobox/Widget/Injector')
+local Cell = require('Module:Infobox/Widget/Cell')
+local Title = require('Module:Infobox/Widget/Title')
+local PrizePoolCurrency = require('Module:Prize pool currency')
+
+local CustomLeague = Class.new()
+local CustomInjector = Class.new(Injector)
+
+local _TODAY = os.date('%Y-%m-%d')
+
+local _league
+
+function CustomLeague.run(frame)
+	local league = League(frame)
+	_league = league
+
+	league.createWidgetInjector = CustomLeague.createWidgetInjector
+	league.defineCustomPageVariables = CustomLeague.defineCustomPageVariables
+	league.addToLpdb = CustomLeague.addToLpdb
+
+	return league:createInfobox(frame)
+end
+
+function CustomLeague:createWidgetInjector()
+	return CustomInjector()
+end
+
+function CustomInjector:parse(id, widgets)
+	local args = _league.args
+	if id == 'customcontent' then
+		if not String.isEmpty(args.player_number) or not String.isEmpty(args.doubles_number) then
+			table.insert(widgets, Title{name = 'Player Breakdown'})
+			table.insert(widgets, Cell{
+				name = 'Number of Players',
+				content = {args.player_number}
+			})
+			table.insert(widgets, Cell{
+				name = 'Doubles Players',
+				content = {args.doubles_number}
+			})
+		end
+	elseif id == 'prizepool' then
+		return {
+			Cell{
+				name = 'Prize pool',
+				content = {CustomLeague:_createPrizepool(args)}
+			},
+		}
+	elseif id == 'liquipediatier' then
+		return {
+			Cell{
+				name = 'Liquipedia Tier',
+				content = {CustomLeague:_createLiquipediaTierDisplay(args)}
+			},
+		}
+	end
+	return widgets
+end
+
+function CustomLeague:_createLiquipediaTierDisplay(args)
+	local tier = args.liquipediatier or ''
+	local tierType = args.liquipediatiertype or ''
+
+	if String.isEmpty(tier) then
+		return nil
+	end
+
+	local function buildTierString(tierString)
+		local tierText = Tier.text[tierString]
+		if not tierText then
+			table.insert(_league.warnings, tierString .. ' is not a known Liquipedia Tier/Tiertype')
+			return ''
+		else
+			return '[[' .. tierText .. ' Tournaments|' .. tierText .. ']]'
+		end
+	end
+	local tierDisplay = buildTierString(tier)
+	if String.isNotEmpty(tierType) then
+		tierDisplay = buildTierString(tierType) .. '&nbsp;(' .. tierDisplay .. ')'
+	end
+
+	return tierDisplay
+end
+
+function CustomLeague:_createPrizepool(args)
+	if String.isEmpty(args.prizepool) and String.isEmpty(args.prizepoolusd) then
+		return nil
+	end
+
+	local date
+	if String.isNotEmpty(args.currency_rate) then
+		date = args.currency_date
+	end
+
+	return PrizePoolCurrency._get({
+		prizepool = args.prizepool,
+		prizepoolusd = args.prizepoolusd,
+		currency = args.localcurrency,
+		rate = args.currency_rate,
+		date = date or Variables.varDefault('tournament_enddate', _TODAY),
+	})
+end
+
+function CustomLeague:defineCustomPageVariables(args)
+	-- Legacy vars
+	local sdate = Variables.varDefault('tournament_startdate', _TODAY)
+	local edate = Variables.varDefault('tournament_enddate', _TODAY)
+	Variables.varDefine('tournament_sdate', sdate)
+	Variables.varDefine('tournament_edate', edate)
+	Variables.varDefine('tournament_date', edate)
+
+	Variables.varDefine('tournament_ticker_name', args.tickername)
+	Variables.varDefine('tournament_tier', Variables.varDefault('tournament_liquipediatier'))
+	Variables.varDefine('tournament_link', mw.title.getCurrentTitle().prefixedText)
+end
+
+function CustomLeague:addToLpdb(lpdbData, args)
+	lpdbData['patch'] = args.patch
+	lpdbData['participantsnumber'] = args.team_number or args.player_number
+	lpdbData['extradata'] = {
+		region = args.region,
+		mode = args.mode,
+	}
+
+	return lpdbData
+end
+
+return CustomLeague
