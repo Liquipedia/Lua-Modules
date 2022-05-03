52bcee06208e0c0db467d75f2ea708cfc9954cc3
Rikard Blixt <rikardblixt@gmail.com>
2022-05-03 18:35:43 +0200

    Add template entrypoint to Standings

--- components/standings/standings_storage.lua
+++ components/standings/standings_storage.lua
@@ -6,6 +6,7 @@
 -- Please see https://github.com/Liquipedia/Lua-Modules to contribute
 --
 
+local Arguments = require('Module:Arguments')
 local Flags = require('Module:Flags')
 local Variables = require('Module:Variables')
 
@@ -43,6 +44,30 @@ function StandingsStorage.run(index, data)
 			extradata = mw.ext.LiquipediaDB.lpdb_create_json({data.extradata or {}})
 		}
 	)
+end
+
+function StandingsStorage.fromTemplate(frame)
+	local data = Arguments.getArgs(frame)
+	if not data.standingsindex or not data.roundindex or not data.placement then
+		return
+	end
+
+	 -- finds [[page|display]] and skips images (images have multiple |)
+	data.team = string.match(data.team, '%[%[([^|]-)|[^|]-%]%]')
+	if data.team and mw.ext.TeamTemplate.teamexists(data.team) then
+		local team = mw.ext.TeamTemplate.raw(data.team)
+		data.participant = team.page
+		data.participantdisplay = team.name
+		data.participantflag = ''
+		data.icon = team.image
+		data.icondark = team.imagedark
+	else
+		data.participant = 'tbd'
+		data.participantdisplay = 'TBD'
+	end
+	data.match = {data.win_m or 0, data.tie_m or 0, data.lost_m or 0}
+	data.game = {data.win_g or 0, data.tie_g or 0, data.lost_g or 0}
+	return StandingsStorage.run(data.placement, data)
 end
 
 return StandingsStorage
