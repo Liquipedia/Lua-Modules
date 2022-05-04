6c3e656c578c4a6e88cdac37cac8cb15d3825450
Rikard Blixt <rikardblixt@gmail.com>
2022-05-04 16:27:09 +0200

    Add template entry point to Standings Store (#1206)
    * Add template entrypoint to Standings
    
    * Remove redundant code and fix typo
    
    * Get correct historical TT; Add basic player support
    
    * Fix typo in player handling; allow handle the happy case of just a team in the input
    
    * Renamed variable, update comment for clarity

--- components/standings/standings_storage.lua
+++ components/standings/standings_storage.lua
@@ -6,6 +6,7 @@
 -- Please see https://github.com/Liquipedia/Lua-Modules to contribute
 --
 
+local Arguments = require('Module:Arguments')
 local Flags = require('Module:Flags')
 local Variables = require('Module:Variables')
 
@@ -44,6 +45,50 @@ function StandingsStorage.run(index, data)
 			extradata = mw.ext.LiquipediaDB.lpdb_create_json(data.extradata or {})
 		}
 	)
+end
+
+function StandingsStorage.fromTemplate(frame)
+	local data = Arguments.getArgs(frame)
+
+	if not data.standingsindex or not data.roundindex or not data.placement then
+		return
+	end
+	if not data.team and not data.player then
+		return
+	end
+
+	if data.team then
+		-- attempts to find [[teamPage|teamDisplay]] and skips images (images have multiple |)
+		local teamPage = string.match(data.team, '%[%[([^|]-)|[^|]-%]%]')
+		local date = Variables.varDefaultMulti('tournament_startdate', 'tournament_enddate', nil)
+		local team
+
+		-- Input contains an actual team
+		if data.team and mw.ext.TeamTemplate.teamexists(data.team) then
+			team = mw.ext.TeamTemplate.raw(data.team, date)
+		-- Input is link (possiblity with icon etc), and we managed to parse it
+		elseif teamPage and mw.ext.TeamTemplate.teamexists(teamPage) then
+			team = mw.ext.TeamTemplate.raw(teamPage, date)
+		end
+
+		if team then
+			data.participant = team.page
+			data.participantdisplay = team.name
+			data.icon = team.image
+			data.icondark = team.imagedark
+		else
+			data.participant = 'tbd'
+			data.participantdisplay = 'TBD'
+		end
+	elseif data.player then
+		data.participant, data.participantdisplay = string.match(data.player, '%[%[([^|]-)|([^|]-)%]%]')
+		-- TODO: sanity checks and parse flag
+		-- TODO: handle more input cases
+	end
+
+	data.match = {data.win_m, data.tie_m, data.lose_m}
+	data.game = {data.win_g, data.tie_g, data.lose_g}
+	return StandingsStorage.run(data.placement, data)
 end
 
 return StandingsStorage
