bf29994a23535e00bf57a1a0a5ba352a5f621c65
Rikard Blixt <rikardblixt@gmail.com>
2022-05-17 15:10:10 +0200

    Add dota2 series (#1244)

---
+++ components/infobox/wikis/dota2/infobox_series_custom.lua
@@ -0,0 +1,52 @@
+---
+-- @Liquipedia
+-- wiki=dota2
+-- page=Module:Infobox/Series/Custom
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Series = require('Module:Infobox/Series')
+local Injector = require('Module:Infobox/Widget/Injector')
+local Cell = require('Module:Infobox/Widget/Cell')
+local Class = require('Module:Class')
+local Table = require('Module:Table')
+local Flags = require('Module:Flags')
+
+local CustomSeries = {}
+
+local CustomInjector = Class.new(Injector)
+
+local _args
+
+function CustomSeries.run(frame)
+	local series = Series(frame)
+	_args = series.args
+	series.createWidgetInjector = CustomSeries.createWidgetInjector
+
+	return series:createInfobox(frame)
+end
+
+function CustomSeries:createWidgetInjector()
+	return CustomInjector()
+end
+
+function CustomInjector:parse(id, widgets)
+	if id == 'location' then
+		local locations = {}
+		_args.country1, _args.city1, _args.location1date = _args.country, _args.city, _args.locationdate
+		for _, country, index in Table.iter.pairsByPrefix(_args, 'country') do
+			local city = _args['city'.. index]
+			local locationDate = _args['location'..index..'date']
+			local text = Flags.Icon{flag = country, shouldLink = true} .. '&nbsp;' .. (city or country)
+			if locationDate then
+				text = text .. '&nbsp;<small>' .. locationDate .. '</small>'
+			end
+			table.insert(locations, text)
+		end
+		return { Cell{name = 'Location', content = locations} }
+	end
+	return widgets
+end
+
+return CustomSeries
