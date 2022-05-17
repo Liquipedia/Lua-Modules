5bb131e363b0250f489762925467ac0b8771c000
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2022-05-17 19:54:44 +0200

    Add fortnite Info module

---
+++ standard/info/wikis/fortnite/info.lua
@@ -0,0 +1,15 @@
+---
+-- @Liquipedia
+-- wiki=fortnite
+-- page=Module:Info
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+return {
+	startYear = 2018,
+	wikiName = 'fortnite',
+	name = 'Fortnite',
+	defaultTeamLogo = 'Fortnite logo.png',
+	defaultTeamLogoDark = 'Fortnite logo.png',
+}
