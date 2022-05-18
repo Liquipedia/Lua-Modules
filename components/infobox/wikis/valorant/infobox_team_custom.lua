ae47bd129f0414950796e045ca0027cf4782cadb
Cameron <58013431+FenrirWulf@users.noreply.github.com>
2022-05-18 15:44:45 -0700

    Fixing injector code and add IGL support

--- components/infobox/wikis/valorant/infobox_team_custom.lua
+++ components/infobox/wikis/valorant/infobox_team_custom.lua
@@ -8,18 +8,40 @@
 
 local Team = require('Module:Infobox/Team')
 local Variables = require('Module:Variables')
+local Class = require('Module:Class')
 local String = require('Module:String')
+local Injector = require('Module:Infobox/Widget/Injector')
+local Cell = require('Module:Infobox/Widget/Cell')
 local Template = require('Module:Template')
 
-local CustomTeam = {}
+local CustomTeam = Class.new()
+local CustomInjector = Class.new(Injector)
 
 local _team
 
 function CustomTeam.run(frame)
 	local team = Team(frame)
 	_team = team
+	team.createWidgetInjector = CustomTeam.createWidgetInjector
+	team.createBottomContent = CustomTeam.createBottomContent
 	team.addToLpdb = CustomTeam.addToLpdb
 	return team:createInfobox(frame)
+end
+
+function CustomTeam:createWidgetInjector()
+	return CustomInjector()
+end
+
+function CustomInjector:parse(id, widgets)
+	return widgets
+end
+
+function CustomInjector:addCustomCells(widgets)
+	table.insert(widgets, Cell{
+		name = 'In-Game Leader',
+		content = {_team.args.igl}
+	})
+	return widgets
 end
 
 function CustomTeam:createBottomContent()
