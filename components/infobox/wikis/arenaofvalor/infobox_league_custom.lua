2ce9ae24790de7e40acbb7b785c56f33c7ec8c99
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2022-05-19 11:42:17 +0200

    aov

--- components/infobox/wikis/arenaofvalor/infobox_league_custom.lua
+++ components/infobox/wikis/arenaofvalor/infobox_league_custom.lua
@@ -9,7 +9,6 @@
 local League = require('Module:Infobox/League')
 local String = require('Module:StringUtils')
 local Variables = require('Module:Variables')
-local Tier = require('Module:Tier')
 local Class = require('Module:Class')
 local Injector = require('Module:Infobox/Widget/Injector')
 local Cell = require('Module:Infobox/Widget/Cell')
@@ -30,6 +29,7 @@ function CustomLeague.run(frame)
 	league.addToLpdb = CustomLeague.addToLpdb
 	league.createWidgetInjector = CustomLeague.createWidgetInjector
 	league.defineCustomPageVariables = CustomLeague.defineCustomPageVariables
+	league.liquipediaTierHighlighted = CustomLeague.liquipediaTierHighlighted
 
 	return league:createInfobox(frame)
 end
@@ -45,14 +45,6 @@ function CustomInjector:parse(id, widgets)
 					CustomLeague._getGameVersion()
 				}},
 			}
-	elseif id == 'liquipediatier' then
-		return {
-			Cell{
-				name = 'Liquipedia tier',
-				content = {CustomLeague:_createTierDisplay()},
-				classes = {_args['garena-sponsored'] == 'true' and 'valvepremier-highlighted' or ''},
-			},
-		}
 	elseif id == 'customcontent' then
 		if _args.player_number then
 			table.insert(widgets, Title{name = 'Players'})
@@ -66,6 +58,10 @@ function CustomInjector:parse(id, widgets)
 		end
 	end
 	return widgets
+end
+
+function CustomLeague:liquipediaTierHighlighted()
+	return Logic.readBool(_args['garena-sponsored'])
 end
 
 function CustomLeague:addToLpdb(lpdbData, args)
@@ -89,36 +85,6 @@ function CustomLeague._getGameVersion()
 	local game = string.lower(_args.game or '')
 	_game = _GAME[game]
 	return _game
-end
-
-function CustomLeague:_createTierDisplay()
-	local tier = _args.liquipediatier or ''
-	local tierType = _args.liquipediatiertype or _args.tiertype or ''
-	if String.isEmpty(tier) then
-		return nil
-	end
-
-	local tierText = Tier['text'][tier]
-	local hasInvalidTier = tierText == nil
-	tierText = tierText or tier
-
-	local hasInvalidTierType = false
-
-	local output = '[[' .. tierText .. ' Tournaments|' .. tierText .. ']]'
-		.. '[[Category:' .. tierText .. ' Tournaments]]'
-
-	if not String.isEmpty(tierType) then
-		tierType = Tier['types'][string.lower(tierType or '')] or tierType
-		hasInvalidTierType = Tier['types'][string.lower(tierType or '')] == nil
-		tierType = '[[' .. tierType .. ' Tournaments|' .. tierType .. ']]'
-			.. '[[Category:' .. tierType .. ' Tournaments]]'
-		output = tierType .. '&nbsp;(' .. output .. ')'
-	end
-
-	output = output ..
-		(hasInvalidTier and '[[Category:Pages with invalid Tier]]' or '') ..
-		(hasInvalidTierType and '[[Category:Pages with invalid Tiertype]]' or '')
-	return output
 end
 
 return CustomLeague
