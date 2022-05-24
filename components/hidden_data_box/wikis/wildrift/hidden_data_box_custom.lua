f51cade2b73a0ca8be9f81fdd676b3f77a65603c
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2022-05-24 10:31:42 +0200

    Add wildrift HiddenDataBox/Custom

---
+++ components/hidden_data_box/wikis/wildrift/hidden_data_box_custom.lua
@@ -0,0 +1,26 @@
+---
+-- @Liquipedia
+-- wiki=commons
+-- page=Module:HiddenDataBox/Custom
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Class = require('Module:Class')
+local BasicHiddenDataBox = require('Module:HiddenDataBox')
+local CustomHiddenDataBox = {}
+
+function CustomHiddenDataBox.run(args)
+	BasicHiddenDataBox.addCustomVariables = CustomHiddenDataBox.addCustomVariables
+	return BasicHiddenDataBox.run(args)
+end
+
+function CustomHiddenDataBox:addCustomVariables(args, queryResult)
+	BasicHiddenDataBox:checkAndAssign(
+		'tournament_publishertier',
+		args.riotpremier,
+		queryResult.publishertier
+	)
+end
+
+return Class.export(CustomHiddenDataBox)
