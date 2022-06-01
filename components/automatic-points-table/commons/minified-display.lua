96878c0bbd87e241605cc949c458a855137f8aba
Mamdouh Waleed <mamdouh.waleed.96@gmail.com>
2022-06-01 17:18:13 +0200

    Remove unused variable

--- components/automatic-points-table/commons/minified-display.lua
+++ components/automatic-points-table/commons/minified-display.lua
@@ -6,7 +6,6 @@
 -- Please see https://github.com/Liquipedia/Lua-Modules to contribute
 --
 
-local Array = require('Module:Array')
 local Class = require('Module:Class')
 local Table = require('Module:Table')
 
