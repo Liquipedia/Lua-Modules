adc3fa843265702e25be5ce8294fca6782331cbc
Rikard Blixt <rikardblixt@gmail.com>
2022-06-02 23:06:10 +0200

    Mobile Legends: Store Bestof for match1

--- components/match2/wikis/mobilelegends/match_legacy.lua
+++ components/match2/wikis/mobilelegends/match_legacy.lua
@@ -51,6 +51,7 @@ function MatchLegacy._convertParameters(match2)
 	-- Handle extradata fields
 	match.extradata = {}
 	local extradata = Json.parseIfString(match2.extradata)
+	match.extradata.gamecount = tostring(match2.bestof)
 	match.extradata.matchsection = extradata.matchsection
 	match.extradata.mvpteam = extradata.mvpteam
 	match.extradata.mvp = extradata.mvp
