9d2408dd52eecf3046bf561f62dfb920b345d05b
Rikard Blixt <rikardblixt@gmail.com>
2022-06-02 10:00:20 +0200

    Rework WidgetFactory

--- components/infobox/commons/infobox_widget_factory.lua
+++ components/infobox/commons/infobox_widget_factory.lua
@@ -6,8 +6,8 @@
 -- Please see https://github.com/Liquipedia/Lua-Modules to contribute
 --
 
+local Array = require('Module:Array')
 local Class = require('Module:Class')
-local Customizable = require('Module:Infobox/Widget/Customizable')
 local Widget = require('Module:Infobox/Widget')
 
 local WidgetFactory = Class.new()
@@ -19,20 +19,14 @@ function WidgetFactory.work(widget, injector)
 		return {}
 	end
 
-	if widget:is_a(Customizable) then
-		for _, child in ipairs(widget:tryMake() or {}) do
-			if child['is_a'] == nil or child:is_a(Widget) == false then
-				return error('Customizable can only contain Widgets as children')
-			end
-
-			child:setContext({injector = injector})
-
-			for _, item in ipairs(child:tryMake() or {}) do
-				table.insert(convertedWidgets, item)
-			end
+	local children = widget:tryMake()
+	for _, child in ipairs(children) do
+		if type(child) == 'table' and child['is_a'] and child:is_a(Widget) then
+			child:setContext{injector = injector}
+			Array.extendWith(convertedWidgets, WidgetFactory.work(child, injector))
+		else
+			table.insert(convertedWidgets, child)
 		end
-	else
-		return widget:tryMake()
 	end
 
 	return convertedWidgets
