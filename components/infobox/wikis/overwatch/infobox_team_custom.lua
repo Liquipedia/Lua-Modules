496068136945be98d72ec5cb64182bccc8456fc6
Cameron <58013431+FenrirWulf@users.noreply.github.com>
2022-06-04 20:48:05 -0700

    Adding bottom content and removing Captain stuff

--- components/infobox/wikis/overwatch/infobox_team_custom.lua
+++ components/infobox/wikis/overwatch/infobox_team_custom.lua
@@ -10,36 +10,39 @@ local Team = require('Module:Infobox/Team')
 local Variables = require('Module:Variables')
 local Class = require('Module:Class')
 local String = require('Module:StringUtils')
---[[local Template = require('Module:Template')]]--
+local Template = require('Module:Template')
 
 local CustomTeam = Class.new()
 
---[[local _team]]--
+local _team
 
 function CustomTeam.run(frame)
 	local team = Team(frame)
-	--[[_team = team]]--
-	--[[team.createBottomContent = CustomTeam.createBottomContent]]--
+	_team = team
+	team.createBottomContent = CustomTeam.createBottomContent
 	team.addToLpdb = CustomTeam.addToLpdb
 	team.defineCustomPageVariables = CustomTeam.defineCustomPageVariables
 	return team:createInfobox(frame)
 end
 
---[[ TODO: Impliment after wiki transitions to Match2
+--[[ TODO: Impliment after wiki transitions to Match2 ]]--
 function CustomTeam:createBottomContent()
 	return Template.expandTemplate(
 		mw.getCurrentFrame(),
 		'Upcoming and ongoing matches of',
 		{team = _team.name or _team.pagename}
+	) .. Template.expandTemplate(
+		mw.getCurrentFrame(),
+		'Upcoming and ongoing tournaments of',
+		{team = _team.name or _team.pagename}
 	)
 end
-]]--
 
 function CustomTeam:addToLpdb(lpdbData, args)
 	if not String.isEmpty(args.teamcardimage) then
-		lpdbData.logo = 'File:' .. args.teamcardimage
+		lpdbData.logo = args.teamcardimage
 	elseif not String.isEmpty(args.image) then
-		lpdbData.logo = 'File:' .. args.image
+		lpdbData.logo = args.image
 	end
 
 	lpdbData.region = Variables.varDefault('region', '')
@@ -49,10 +52,6 @@ function CustomTeam:addToLpdb(lpdbData, args)
 	}
 
 	return lpdbData
-end
-
-function CustomTeam:defineCustomPageVariables(args)
-	Variables.varDefine('team_captain', args.captain)
 end
 
 function CustomTeam:getWikiCategories(args)
