d7c670f4d7182df9d9b9e9cef1fd27f45c0499fe
Cameron <58013431+FenrirWulf@users.noreply.github.com>
2022-06-06 13:06:19 -0700

    Overwatch Infobox Team (#1280)
    * Overwatch Infobox Team
    
    * Commenting the remaining unused code
    
    * Adding bottom content and removing Captain stuff
    
    * Fixing leftover code

---
+++ components/infobox/wikis/overwatch/infobox_team_custom.lua
@@ -0,0 +1,66 @@
+---
+-- @Liquipedia
+-- wiki=overwatch
+-- page=Module:Infobox/Team/Custom
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Team = require('Module:Infobox/Team')
+local Variables = require('Module:Variables')
+local Class = require('Module:Class')
+local String = require('Module:StringUtils')
+local Template = require('Module:Template')
+
+local CustomTeam = Class.new()
+
+local _team
+
+function CustomTeam.run(frame)
+	local team = Team(frame)
+	_team = team
+	team.createBottomContent = CustomTeam.createBottomContent
+	team.addToLpdb = CustomTeam.addToLpdb
+	team.getWikiCategories = CustomTeam.getWikiCategories
+	return team:createInfobox(frame)
+end
+
+function CustomTeam:createBottomContent()
+	return Template.expandTemplate(
+		mw.getCurrentFrame(),
+		'Upcoming and ongoing matches of',
+		{team = _team.name}
+	) .. Template.expandTemplate(
+		mw.getCurrentFrame(),
+		'Upcoming and ongoing tournaments of',
+		{team = _team.name}
+	)
+end
+
+function CustomTeam:addToLpdb(lpdbData, args)
+	if not String.isEmpty(args.teamcardimage) then
+		lpdbData.logo = args.teamcardimage
+	elseif not String.isEmpty(args.image) then
+		lpdbData.logo = args.image
+	end
+
+	lpdbData.region = Variables.varDefault('region', '')
+
+	lpdbData.extradata = {
+		owl = String.isNotEmpty(args.owl),
+	}
+
+	return lpdbData
+end
+
+function CustomTeam:getWikiCategories(args)
+	local categories = {}
+
+	if String.isNotEmpty(args.owl) then
+		table.insert(categories, 'Overwatch League Teams')
+	end
+
+	return categories
+end
+
+return CustomTeam
