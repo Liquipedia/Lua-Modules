0fac2ef1c67e91049d0064c04aa5a867bcd21bd6
Rikard Blixt <rikardblixt@gmail.com>
2022-06-11 15:20:10 +0200

    Cleanup

--- components/prize_pool/commons/prize_pool.lua
+++ components/prize_pool/commons/prize_pool.lua
@@ -13,104 +13,117 @@ local Lua = require('Module:Lua')
 local Opponent = require('Module:Opponent')
 local Table = require('Module:Table')
 
+local WidgetInjector = Lua.import('Module:Infobox/Widget/Injector', {requireDevIfEnabled = true})
+-- local LpdbInjector = Lua.import('Module...', {requireDevIfEnabled = true})
+
 local PrizePool = Class.new(function(self, ...) self:init(...) end)
 
 -- TODO: Extract Enum handling to its own Module
 local prizeTypes = Table.map({
-    'USD', 'LOCAL_CURRENCY', 'POINTS', 'QUALIFIES', 'FREETEXT'
+	'USD', 'LOCAL_CURRENCY', 'POINTS', 'QUALIFIES', 'FREETEXT'
 }, function(value, name) return name, value end)
 
--- TODO: Add possiblity to add custom prizeData
 local prizeData = {
-    [prizeTypes.USD] = {row = 'usdprize'},
-    [prizeTypes.LOCAL_CURRENCY] = {header = 'localcurrency', row = 'localprize'},
-    [prizeTypes.POINTS] = {header = 'points', row = 'points'},
-    [prizeTypes.QUALIFIES] = {header = 'qualifies', row = 'qualified'},
-    [prizeTypes.FREETEXT] = {header = 'freetext', row = 'freetext'},
+	[prizeTypes.USD] = {row = 'usdprize'},
+	[prizeTypes.LOCAL_CURRENCY] = {header = 'localcurrency', row = 'localprize'},
+	[prizeTypes.POINTS] = {header = 'points', row = 'points'},
+	[prizeTypes.QUALIFIES] = {header = 'qualifies', row = 'qualified'},
+	[prizeTypes.FREETEXT] = {header = 'freetext', row = 'freetext'},
 }
 
 function PrizePool:init(args)
-    self.args = args
-    self.pagename = mw.title.getCurrentTitle().text
-    self.args.type = Json.parseIfString(self.args.type)
-    self.widgetInjector = nil
-    self.lpdbInjector = nil
+	self.args = args
+	self.pagename = mw.title.getCurrentTitle().text
+	self.args.type = Json.parseIfString(self.args.type)
+	self.widgetInjector = nil
+	self.lpdbInjector = nil
 
-    if not self.args.type then
-        return error('Please provide a type!')
-    elseif type(self.args.type) ~= 'table' or not self.args.type.type then
-        return error('Could not parse type!')
-    elseif not Opponent.isType(self.args.type.type) then
-        return error('Not a valid type!')
-    end
+	if not self.args.type then
+		return error('Please provide a type!')
+	elseif type(self.args.type) ~= 'table' or not self.args.type.type then
+		return error('Could not parse type!')
+	elseif not Opponent.isType(self.args.type.type) then
+		return error('Not a valid type!')
+	end
 
-    self.type = self.args.type.type
+	self.type = self.args.type.type
 
-    self.options = self:_readOptions(args)
-    self.prizes = self:_readPrizes(args)
-    if self.options.showUSD then
-        self:addPrize('USD', 1)
-    end
-    self.placements = {} -- TODO
+	self.options = self:_readOptions(args)
+	self.prizes = self:_readPrizes(args)
+	if self.options.showUSD then
+		self:addPrize('USD', 1)
+	end
+	self.placements = {} -- TODO
 end
 
 function PrizePool:create()
 	-- TODO
+	mw.logObject(self)
 	return ''
 end
 
 function PrizePool:_readOptions(args)
-    return {
+	return {
 		autoUSD = Logic.nilOr(Logic.readBoolOrNil(args.autousd), true),
 		showUSD = Logic.nilOr(Logic.readBoolOrNil(args.showusd), true), -- TODO: Improve
 		prizeSummary = Logic.nilOr(Logic.readBoolOrNil(args.prizesummary), true),
 		exchangeInfo = Logic.nilOr(Logic.readBoolOrNil(args.exchangeinfo), true),
 		storeSmw = Logic.nilOr(Logic.readBoolOrNil(args.storesmw), true),
-        storeLpdb = Logic.nilOr(Logic.readBoolOrNil(args.storelpdb), true),
+		storeLpdb = Logic.nilOr(Logic.readBoolOrNil(args.storelpdb), true),
 	}
 end
 
 function PrizePool:_readPrizes(args)
-    self.prizes = {}
+	self.prizes = {}
 
-    for prizeEnum in pairs(prizeTypes) do
-        local prizeDatum = prizeData[prizeTypes[prizeEnum]]
-        local fieldName = prizeDatum.header
-        if fieldName then
-            args[fieldName .. '1'] = args[fieldName .. '1'] or args[fieldName]
-            for id, prizeValue, index in Table.iter.pairsByPrefix(args, fieldName) do
-                -- TODO: parse the prizeValue into structured data
-                self:addPrize(prizeEnum, index, prizeValue)
-            end
-        end
-    end
+	for prizeEnum in pairs(prizeTypes) do
+		local prizeDatum = prizeData[prizeTypes[prizeEnum]]
+		local fieldName = prizeDatum.header
+		if fieldName then
+			args[fieldName .. '1'] = args[fieldName .. '1'] or args[fieldName]
+			for id, prizeValue, index in Table.iter.pairsByPrefix(args, fieldName) do
+				-- TODO: parse the prizeValue into structured data
+				self:addPrize(prizeEnum, index, prizeValue)
+			end
+		end
+	end
 
-    return self.prizes
+	return self.prizes
 end
 
 function PrizePool:setOption(option, value)
-    self.options[option] = value
-    return self
+	self.options[option] = value
+	return self
+end
+
+-- TODO extract of this to the future Enum class
+local CUSTOM_TYPES_OFFSET = 100
+local CUSTOM_TYPES_USED = 0
+function PrizePool:addPrizeType(enum, data)
+	prizeTypes[enum] = CUSTOM_TYPES_OFFSET + CUSTOM_TYPES_USED
+	CUSTOM_TYPES_USED = CUSTOM_TYPES_USED + 1
+	prizeData[prizeTypes[enum]] = data
+	return self
 end
 
 function PrizePool:addPrize(enum, index, data)
-    assert(prizeTypes[enum], 'addPrize: Not a valid prizeEnum!')
-    assert(Logic.isNumeric(index), 'addPrize: Index is not numeric!')
-    data = data or {}
-    table.insert(self.prizes, {enum = enum, index = index, data = data})
-    return self
+	assert(prizeTypes[enum], 'addPrize: Not a valid prizeEnum!')
+	assert(Logic.isNumeric(index), 'addPrize: Index is not numeric!')
+	data = data or {}
+	table.insert(self.prizes, {enum = enum, index = index, data = data})
+	return self
 end
 
 function PrizePool:setWidgetInjector(widgetInjector)
-    -- TODO: Add type check
-    self.widgetInjector = widgetInjector
-    return self
+	assert(widgetInjector:is_a(WidgetInjector), "Not a WidgetIjector")
+	self.widgetInjector = widgetInjector
+	return self
 end
 
 function PrizePool:setLpdbInjector(lpdbInjector)
-    -- TODO: Add type check
-    self.lpdbInjector = lpdbInjector
-    return self
+	-- TODO: Add type check
+	self.lpdbInjector = lpdbInjector
+	return self
 end
 
 return PrizePool
--- components/prize_pool/wikis/rainbowsix/prize_pool_custom.lua
+++ components/prize_pool/wikis/rainbowsix/prize_pool_custom.lua
@@ -11,28 +11,23 @@ local Class = require('Module:Class')
 local Lua = require('Module:Lua')
 
 local PrizePool = Lua.import('Module:PrizePool', {requireDevIfEnabled = true})
-local WidgetInjector = Lua.import('Module:Infobox/Widget/Injector', {requireDevIfEnabled = true}) -- TODO: Move Widget modules away from Infobox/?
+ -- TODO: Move some Widget modules away from Infobox?
+local WidgetInjector = Lua.import('Module:Infobox/Widget/Injector', {requireDevIfEnabled = true})
 -- local LpdbInjector = Lua.import('Module...', {requireDevIfEnabled = true})
 
-local CustomPrizePool = Class.new(function(self, ...) self:init(...) end)
-
+local CustomPrizePool = {}
 local CustomWidgetInjector = Class.new(WidgetInjector)
 -- local CustomLpdbInjector = Class.new(LpdbInjector)
 
 -- Template entry point
 function CustomPrizePool.run(frame)
-	return CustomPrizePool(frame)
-end
+	local args = Arguments.getArgs(frame)
+	local prizePool = PrizePool(args)
 
-function CustomPrizePool:init(args)
-	self.args = Arguments.getArgs(args)
-
-	local prizePool = PrizePool(self.args)
 	prizePool:setWidgetInjector(CustomWidgetInjector())
 	--prizePool:setLpdbInjector(...)
 
-	mw.logObject(self)
-	mw.logObject(prizePool)
+	return prizePool:create()
 end
 
 function CustomWidgetInjector:parse(id, widgets)
