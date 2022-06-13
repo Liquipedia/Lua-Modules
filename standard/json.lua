2fb6bfcd75e7b07c0ab26c963c81aa476ecf3bbf
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2022-06-13 16:42:50 +0200

    Merge Json and Json/Ext (#1384)
    * Update json.lua
    
    * Delete json_ext.lua
    
    * json --> Json

--- standard/json.lua
+++ standard/json.lua
@@ -6,20 +6,20 @@
 -- Please see https://github.com/Liquipedia/Lua-Modules to contribute
 --
 
-local json = {}
+local Json = {}
 
 local Arguments = require('Module:Arguments')
 
-function json.fromArgs(frame)
+function Json.fromArgs(frame)
 	local args = Arguments.getArgs(frame)
-	return json.stringify(args)
+	return Json.stringify(args)
 end
 
-function json.stringify(obj, pretty)
+function Json.stringify(obj, pretty)
 	return mw.text.jsonEncode(obj, pretty == true and mw.text.JSON_PRETTY or nil)
 end
 
-function json.parse(obj)
+function Json.parse(obj)
 	local parse = function(object) return mw.text.jsonDecode(object, mw.text.JSON_TRY_FIXING) end
 	local status, res = pcall(parse, obj);
 	if status then
@@ -32,12 +32,34 @@ function json.parse(obj)
 	end
 end
 
-function json.parseIfString(obj)
+function Json.parseIfString(obj)
 	if type(obj) == 'string' then
-		return json.parse(obj)
+		return Json.parse(obj)
 	else
 		return obj
 	end
 end
 
-return json
+--[[
+Attempts to parse a JSON encoded table. Returns nil if unsuccessful.
+
+Example:
+
+JsonExt.parseIfTable('{"a" = 3}')
+-- Returns {a = 3}
+
+]]
+function Json.parseIfTable(any)
+	if type(any) == 'string' then
+		local firstChar = any:sub(1, 1)
+		if firstChar == '{' or firstChar == '[' then
+			local result, hasError = Json.parse(any)
+			if not hasError then
+				return result
+			end
+		end
+	end
+	return nil
+end
+
+return Json
