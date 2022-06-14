e5505b2c6fa0b091b284e38d5d3ea03b53eab16f
Rikard Blixt <rikardblixt@gmail.com>
2022-06-14 20:18:42 +0200

    Cleaned down the R6 Custom

--- components/prize_pool/wikis/rainbowsix/prize_pool_custom.lua
+++ components/prize_pool/wikis/rainbowsix/prize_pool_custom.lua
@@ -7,30 +7,18 @@
 --
 
 local Arguments = require('Module:Arguments')
-local Class = require('Module:Class')
 local Lua = require('Module:Lua')
 
 local PrizePool = Lua.import('Module:PrizePool', {requireDevIfEnabled = true})
- -- TODO: Move some Widget modules away from Infobox?
-local WidgetInjector = Lua.import('Module:Infobox/Widget/Injector', {requireDevIfEnabled = true})
 
 local CustomPrizePool = {}
-local CustomWidgetInjector = Class.new(WidgetInjector)
 
 -- Template entry point
 function CustomPrizePool.run(frame)
 	local args = Arguments.getArgs(frame)
-	local prizePool = PrizePool(args)
-	prizePool:create()
-
-	prizePool:setWidgetInjector(CustomWidgetInjector())
+	local prizePool = PrizePool(args):create()
 
 	return prizePool:build()
-end
-
-function CustomWidgetInjector:parse(id, widgets)
-	-- TODO: do something
-	return widgets
 end
 
 return CustomPrizePool
