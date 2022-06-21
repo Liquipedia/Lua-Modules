1b29168802a4581c6914702a3ee0357deef06845
Cameron <58013431+FenrirWulf@users.noreply.github.com>
2022-06-21 13:17:55 -0700

    Sideswipe Info (#1416)
    * Sideswipe Info
    
    * Updating the year

---
+++ standard/info/wikis/sideswipe/info.lua
@@ -0,0 +1,15 @@
+---
+-- @Liquipedia
+-- wiki=sideswipe
+-- page=Module:Info
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+return {
+	startYear = 2021,
+	wikiName = 'sideswipe',
+	name = 'Rocket League Sideswipe',
+	defaultTeamLogo = 'Sideswipe allmode.png',
+	defaultTeamLogoDark = 'Sideswipe allmode.png',
+}
