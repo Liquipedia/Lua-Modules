a65543f1918d99475619469debed765db84caf42
Cameron <58013431+FenrirWulf@users.noreply.github.com>
2022-06-21 12:52:28 -0700

    Naraka Info

---
+++ standard/info/wikis/naraka/info.lua
@@ -0,0 +1,15 @@
+---
+-- @Liquipedia
+-- wiki=naraka
+-- page=Module:Info
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+return {
+	startYear = 2015,
+	wikiName = 'naraka',
+	name = 'Naraka: Bladepoint',
+	defaultTeamLogo = 'NARAKA lightmode.png',
+	defaultTeamLogoDark = 'NARAKA darkmode.png',
+}
