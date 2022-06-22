75a64b1c2d44abc72f24ebcab92c3a1f4d2ae8b4
Rikard Blixt <rikardblixt@gmail.com>
2022-06-22 15:08:32 +0200

    OpponentDisplay.PlayerInlineOpponent doesn't exist. Use PlayerDisplay.InlinePlayer instead.

--- components/match2/commons/opponent_display.lua
+++ components/match2/commons/opponent_display.lua
@@ -127,7 +127,15 @@ function OpponentDisplay.InlineOpponent(props)
 		return opponent.name or ''
 
 	elseif opponent.type == 'solo' then
-		return OpponentDisplay.PlayerInlineOpponent(props)
+		if not opponent.players then
+			return
+		end
+
+		return PlayerDisplay.InlinePlayer{
+			player = opponent.players[1],
+			flip = props.flip,
+			dq = props.dq,
+		}
 
 	else
 		error('Unrecognized opponent.type ' .. opponent.type)
