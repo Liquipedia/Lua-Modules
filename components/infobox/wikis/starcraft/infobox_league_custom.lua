d972ee5210fab9a858bb16a22af03475f3d85097
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2022-06-23 09:26:45 +0200

    Do not resolve redirect on series value in bw infobox league

--- components/infobox/wikis/starcraft/infobox_league_custom.lua
+++ components/infobox/wikis/starcraft/infobox_league_custom.lua
@@ -274,6 +274,10 @@ function CustomLeague:defineCustomPageVariables()
 	end
 
 	Variables.varDefine('tournament_finished', tostring(finished))
+	-- do not reslolve redirect on the series input
+	-- BW wiki has several series that are displayed on the same page
+	-- hence they need to not RR them
+	Variables.varDefine('tournament_series', args.series)
 end
 
 function CustomLeague:addToLpdb(lpdbData)
@@ -288,6 +292,10 @@ function CustomLeague:addToLpdb(lpdbData)
 	lpdbData.participantsnumber = Variables.varDefault('tournament_playerNumber', _args.team_number or 0)
 	lpdbData.next = mw.ext.TeamLiquidIntegration.resolve_redirect(CustomLeague:_getPageNameFromChronology(_next))
 	lpdbData.previous = mw.ext.TeamLiquidIntegration.resolve_redirect(CustomLeague:_getPageNameFromChronology(_previous))
+	-- do not reslolve redirect on the series input
+	-- BW wiki has several series that are displayed on the same page
+	-- hence they need to not RR them
+	lpdbData.series = args.series
 
 	return lpdbData
 end
