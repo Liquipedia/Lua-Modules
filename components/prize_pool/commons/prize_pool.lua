8f7b6b6210a8bb7d47716bc383b518323d515661
Rikard Blixt <rikardblixt@gmail.com>
2022-07-05 22:18:16 +0200

    Update components/prize_pool/commons/prize_pool.lua
    Co-authored-by: hjpalpha <75081997+hjpalpha@users.noreply.github.com>

--- components/prize_pool/commons/prize_pool.lua
+++ components/prize_pool/commons/prize_pool.lua
@@ -545,6 +545,8 @@ function PrizePool:setLpdbInjector(lpdbInjector)
 end
 
 function PrizePool:_storeLpdb()
+ 	local prizePoolIndex = (tonumber(Variables.varDefault('prizepool_index')) or 0) + 1
+ 	Variables.varDefine('prizepool_index', prizePoolIndex)
 	local lpdbTournamentData = {
 		tournament = Variables.varDefault('tournament_name'),
 		parent = Variables.varDefault('tournament_parent'),
