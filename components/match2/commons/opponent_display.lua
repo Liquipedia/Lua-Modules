41eb4ad4c9d70b8a8946e209dc2430ae97692c24
Rikard Blixt <rikardblixt@gmail.com>
2022-07-05 15:39:02 +0200

    Don't call function that doesn't exist (#1420)
    * OpponentDisplay.PlayerInlineOpponent doesn't exist. Use PlayerDisplay.InlinePlayer instead.
    
    * Adjust to feedback

--- components/match2/commons/opponent_display.lua
+++ components/match2/commons/opponent_display.lua
@@ -127,7 +127,11 @@ function OpponentDisplay.InlineOpponent(props)
 		return opponent.name or ''
 
 	elseif opponent.type == 'solo' then
-		return OpponentDisplay.PlayerInlineOpponent(props)
+		return PlayerDisplay.InlinePlayer{
+			player = opponent.players[1],
+			flip = props.flip,
+			dq = props.dq,
+		}
 
 	else
 		error('Unrecognized opponent.type ' .. opponent.type)
