6b01143e3ea3a8a84c8988cd4e7a99d14cb8a0d9
Rikard Blixt <rikardblixt@gmail.com>
2022-07-06 11:51:42 +0200

    Custom Weight calculation

--- components/prize_pool/wikis/rainbowsix/prize_pool_custom.lua
+++ components/prize_pool/wikis/rainbowsix/prize_pool_custom.lua
@@ -7,9 +7,15 @@
 --
 
 local Arguments = require('Module:Arguments')
+local Class = require('Module:Class')
 local Lua = require('Module:Lua')
+local Template = require('Module:Template')
+local Variables = require('Module:Variables')
 
 local PrizePool = Lua.import('Module:PrizePool', {requireDevIfEnabled = true})
+
+local LpdbInjector = Lua.import('Module:Lpdb/Injector', {requireDevIfEnabled = true})
+local CustomLpdbInjector = Class.new(LpdbInjector)
 
 local CustomPrizePool = {}
 
@@ -18,7 +24,22 @@ function CustomPrizePool.run(frame)
 	local args = Arguments.getArgs(frame)
 	local prizePool = PrizePool(args):create()
 
+	prizePool:setLpdbInjector(CustomLpdbInjector())
+
 	return prizePool:build()
+end
+
+function CustomLpdbInjector:parse(lpdbData, placement, opponent)
+	lpdbData.weight = Template.safeExpand(
+		mw.getCurrentFrame(), 'Weight', {
+			math.max(lpdbData.prizemoney, 1),
+			Variables.varDefault('tournament_liquipediatier'),
+			lpdbData.place,
+			Variables.varDefault('tournament_type')
+		}
+	)
+
+	return lpdbData
 end
 
 return CustomPrizePool
