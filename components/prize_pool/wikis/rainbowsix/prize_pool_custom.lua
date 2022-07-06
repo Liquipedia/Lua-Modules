8dd78adf27453332ce6222679e5ab980e180fddb
Rikard Blixt <rikardblixt@gmail.com>
2022-07-06 17:19:03 +0200

    Add Custom LPDB Weight Calculate (#1455)
    * Create LPDB Injector Interface
    
    * Custom Weight calculation
    
    * Rewrote template to lua
    
    * Moved consts to the top
    
    * Fix Math
    
    * Correctly named file
    
    * Cast to tier to number when needed
    
    * Added division at the end for easier reading

--- components/prize_pool/wikis/rainbowsix/prize_pool_custom.lua
+++ components/prize_pool/wikis/rainbowsix/prize_pool_custom.lua
@@ -7,18 +7,50 @@
 --
 
 local Arguments = require('Module:Arguments')
+local Class = require('Module:Class')
 local Lua = require('Module:Lua')
+local String = require('Module:StringUtils')
+local Variables = require('Module:Variables')
 
 local PrizePool = Lua.import('Module:PrizePool', {requireDevIfEnabled = true})
 
+local LpdbInjector = Lua.import('Module:Lpdb/Injector', {requireDevIfEnabled = true})
+local CustomLpdbInjector = Class.new(LpdbInjector)
+
 local CustomPrizePool = {}
+
+local TIER_VALUE = {32, 16, 8, 4, 2}
+local TYPE_MODIFIER = {Online = 0.65}
 
 -- Template entry point
 function CustomPrizePool.run(frame)
 	local args = Arguments.getArgs(frame)
 	local prizePool = PrizePool(args):create()
 
+	prizePool:setLpdbInjector(CustomLpdbInjector())
+
 	return prizePool:build()
+end
+
+function CustomLpdbInjector:adjust(lpdbData, placement, opponent)
+	lpdbData.weight = CustomPrizePool.calculateWeight(
+		lpdbData.prizemoney,
+		Variables.varDefault('tournament_liquipediatier'),
+		placement.placeStart,
+		Variables.varDefault('tournament_type')
+	)
+
+	return lpdbData
+end
+
+function CustomPrizePool.calculateWeight(prizeMoney, tier, place, type)
+	if String.isEmpty(tier) then
+		return 0
+	end
+
+	local tierValue = TIER_VALUE[tier] or TIER_VALUE[tonumber(tier) or ''] or 1
+
+	return tierValue * math.max(prizeMoney, 1) * (TYPE_MODIFIER[type] or 1) / place
 end
 
 return CustomPrizePool
