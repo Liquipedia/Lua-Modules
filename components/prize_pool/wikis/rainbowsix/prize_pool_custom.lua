246f39184f6a10821b02a0b064912e662ce39147
Rikard Blixt <rikardblixt@gmail.com>
2022-07-11 09:33:40 +0200

    Add R6 Custom Points handling

--- components/prize_pool/wikis/rainbowsix/prize_pool_custom.lua
+++ components/prize_pool/wikis/rainbowsix/prize_pool_custom.lua
@@ -7,9 +7,11 @@
 --
 
 local Arguments = require('Module:Arguments')
+local Array = require('Module:Array')
 local Class = require('Module:Class')
 local Lua = require('Module:Lua')
 local String = require('Module:StringUtils')
+local Table = require('Module:Table')
 local Variables = require('Module:Variables')
 
 local PrizePool = Lua.import('Module:PrizePool', {requireDevIfEnabled = true})
@@ -18,6 +20,8 @@ local LpdbInjector = Lua.import('Module:Lpdb/Injector', {requireDevIfEnabled = t
 local CustomLpdbInjector = Class.new(LpdbInjector)
 
 local CustomPrizePool = {}
+
+local PRIZE_TYPE_POINTS = 'POINTS'
 
 local TIER_VALUE = {32, 16, 8, 4, 2}
 local TYPE_MODIFIER = {Online = 0.65}
@@ -40,6 +44,18 @@ function CustomLpdbInjector:adjust(lpdbData, placement, opponent)
 		Variables.varDefault('tournament_type')
 	)
 
+	local sixInvitePoints = Array.filter(placement.parent.prizes, function (prize)
+		if prize.type == PRIZE_TYPE_POINTS and prize.data.title == 'SI' then
+			return true
+		end
+	end)[1]
+
+	if sixInvitePoints then
+		CustomPrizePool.addSiDatapoint(lpdbData, placement:getPrizeRewardForOpponent(opponent, sixInvitePoints.id))
+	end
+
+	Variables.varDefine(lpdbData.participant:lower() .. '_prizepoints', lpdbData.extradata.prizepoints)
+
 	return lpdbData
 end
 
@@ -51,6 +67,19 @@ function CustomPrizePool.calculateWeight(prizeMoney, tier, place, type)
 	local tierValue = TIER_VALUE[tier] or TIER_VALUE[tonumber(tier) or ''] or 1
 
 	return tierValue * math.max(prizeMoney, 1) * (TYPE_MODIFIER[type] or 1) / place
+end
+
+function CustomPrizePool.addSiDatapoint(data, siPoints)
+	local pageName = mw.title.getCurrentTitle().fullText
+	mw.ext.LiquipediaDB.lpdb_datapoint('si_points_' .. pageName .. '_' .. data.placement .. '_' .. data.participant, {
+		type = 'si_points',
+		name = data.participant,
+		date = data.date,
+		information = siPoints,
+		extradata = mw.ext.LiquipediaDB.lpdb_create_json({
+			placement = data.placement,
+		})
+	})
 end
 
 return CustomPrizePool
