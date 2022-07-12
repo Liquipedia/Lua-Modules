223ca23ce5498f04d8a719a5a951c952654de854
Rikard Blixt <rikardblixt@gmail.com>
2022-07-12 15:41:30 +0200

    Initial update to support both new and old lpdb

--- components/standings/standings_storage.lua
+++ components/standings/standings_storage.lua
@@ -7,14 +7,122 @@
 --
 
 local Arguments = require('Module:Arguments')
+local Array = require('Module:Array')
 local Flags = require('Module:Flags')
+local Json = require('Module:Json')
+local Opponent = require('Module:Opponent')
 local Table = require('Module:Table')
 local Variables = require('Module:Variables')
 
 local StandingsStorage = {}
-local SCOREBOARD_FALLBACK = {0, 0, 0}
+local ALLOWED_SCORE_BOARD_KEYS = {'w', 'd', 'l'}
+local SCOREBOARD_FALLBACK = {w = 0, d = 0, l = 0}
+local SCOREBOARD_LEGACY_FALLBACK = {0, 0, 0}
 
-function StandingsStorage.run(index, data)
+function StandingsStorage.run(data)
+	if Table.isEmpty(data) then
+		return
+	end
+
+	if data.opponentLibrary then
+		Opponent = require('Module:'.. data.opponentLibrary)
+	end
+
+	local standingsIndex = tonumber(data.standingsindex) or 0
+
+	data.roundcount = tonumber(data.roundcount) or Array.reduce(
+		Array.map(data.entries, function (entry) return tonumber (entry.roundindex) end),
+		math.max)
+
+	StandingsStorage.table(data)
+
+	Array.forEach(data.entries, function (entry)
+		StandingsStorage.entry(entry, standingsIndex)
+		StandingsStorage.runLegacy(entry.slotIndex, entry) -- Deprecated
+	end)
+end
+
+function StandingsStorage.table(data)
+	local title = data.title or ''
+	local cleanedTitle = title:gsub('<.->.-</.->', '')
+
+	local extradata = {
+		roundcount = data.roundcount,
+		stagename = data.stagename or Variables.varDefault('bracket_header'),
+	}
+
+	mw.ext.LiquipediaDB.lpdb_standingstable('standingsTable_' .. data.standingsindex,
+		{
+			tournament = Variables.varDefault('tournament_name', ''),
+			parent = Variables.varDefault('tournament_parent', ''),
+			standingsindex = tonumber(data.standingsindex),
+			title = mw.text.trim(cleanedTitle),
+			section = Variables.varDefault('last_heading', ''):gsub('<.->', ''),
+			type = data.type,
+			matches = mw.ext.LiquipediaDB.lpdb_create_json(data.matches or {}),
+			extradata = mw.ext.LiquipediaDB.lpdb_create_json(Table.merge(extradata, data.extradata)),
+		}
+	)
+end
+
+function StandingsStorage.entry(entry, standingsIndex)
+	local roundIndex = tonumber(entry.roundindex)
+	local slotIndex = tonumber(entry.slotindex)
+
+	local extradata = {
+		enddate = entry.enddate,
+		roundfinished = entry.finished,
+		placerange = entry.placerange,
+		slotindex = slotIndex,
+	}
+
+	local lpdbEntry = {
+		parent = Variables.varDefault('tournament_parent', ''),
+		standingsindex = standingsIndex,
+		placement = entry.placement or entry.rank,
+		definitestatus = entry.definitestatus or entry.bg,
+		currentstatus = entry.currentstatus or entry.pbg,
+		placementchange = entry.placementchange or entry.change,
+		scoreboard = mw.ext.LiquipediaDB.lpdb_create_json{
+			match = StandingsStorage.toScoreBoardEntry(entry.match),
+			overtime = StandingsStorage.toScoreBoardEntry(entry.overtime),
+			game = StandingsStorage.toScoreBoardEntry(entry.game),
+			points = tonumber(entry.points),
+			diff = tonumber(entry.diff),
+			buchholz = tonumber(entry.buchholz),
+		},
+		roundindex = roundIndex,
+		extradata = mw.ext.LiquipediaDB.lpdb_create_json(Table.merge(extradata, entry.extradata)),
+	}
+
+	mw.ext.LiquipediaDB.lpdb_standingsentry(
+		'standing_' .. standingsIndex .. '_' .. entry.roundindex .. '_' .. slotIndex,
+		Table.merge(lpdbEntry, Opponent.toLpdbStruct(entry.opponent))
+	)
+end
+
+function StandingsStorage.toScoreBoardEntry(data)
+	if Table.isEmpty(data) then
+		return
+	end
+
+	local filterScoreBoard = function (_, key)
+		return ALLOWED_SCORE_BOARD_KEYS[key] and true or false
+	end
+
+	-- Need to use Array.filter here. Because strangely enough Table.filter has no access to keys...
+	local scoreBoard = Table.mapValues(Array.filter(data, filterScoreBoard), tonumber)
+
+	if not scoreBoard.w or not scoreBoard.l then
+		mw.logObject(scoreBoard, 'invalid scoreBoardEntry')
+		return SCOREBOARD_FALLBACK
+	end
+
+	return scoreBoard
+end
+
+--- @deprecated
+function StandingsStorage.runLegacy(index, data)
 	local title = data.title or ''
 	local cleanedTitle = title:gsub('<.->.-</.->', '')
 	mw.ext.LiquipediaDB.lpdb_standing(
@@ -50,15 +158,26 @@ function StandingsStorage.run(index, data)
 	)
 end
 
+---@deprecated
 function StandingsStorage.verifyScoreBoardEntry(entry)
 	-- A valid scoreboard entry must have 3 values in an array
 	if #entry ~= 3 then
-		return SCOREBOARD_FALLBACK
+		return SCOREBOARD_LEGACY_FALLBACK
 	end
 	return entry
 end
 
-function StandingsStorage.fromTemplate(frame)
+function StandingsStorage.fromTemplateHeader(frame)
+	local data = Arguments.getArgs(frame)
+
+	if not data.standingsindex then
+		return
+	end
+
+	return StandingsStorage.table(data)
+end
+
+function StandingsStorage.fromTemplateEntry(frame)
 	local data = Arguments.getArgs(frame)
 
 	if not data.standingsindex or not data.roundindex or not data.placement then
@@ -68,10 +187,14 @@ function StandingsStorage.fromTemplate(frame)
 		return
 	end
 
-	if data.team then
+	local date = Variables.varDefault('tournament_startdate', Variables.varDefault('tournament_enddate'))
+
+	local opponentArgs
+	if data.opponent then
+		opponentArgs = Json.parseIfString(data.opponent)
+	elseif data.team then
 		-- attempts to find [[teamPage|teamDisplay]] and skips images (images have multiple |)
 		local teamPage = string.match(data.team, '%[%[([^|]-)|[^|]-%]%]')
-		local date = Variables.varDefault('tournament_startdate', Variables.varDefault('tournament_enddate'))
 		local team
 
 		-- Input contains an actual team
@@ -82,24 +205,42 @@ function StandingsStorage.fromTemplate(frame)
 			team = mw.ext.TeamTemplate.raw(teamPage, date)
 		end
 
+		opponentArgs = {type = 'team'}
+
 		if team then
+			opponentArgs.team = team.templatename
+
+			-- Legacy
 			data.participant = team.page
 			data.participantdisplay = team.name
 			data.icon = team.image
 			data.icondark = team.imagedark
 		else
+			-- Legacy
 			data.participant = 'tbd'
 			data.participantdisplay = 'TBD'
 		end
 	elseif data.player then
-		data.participant, data.participantdisplay = string.match(data.player, '%[%[([^|]-)|([^|]-)%]%]')
 		-- TODO: sanity checks and parse flag
 		-- TODO: handle more input cases
+		data.participant, data.participantdisplay = string.match(data.player, '%[%[([^|]-)|([^|]-)%]%]')
+		opponentArgs = {link = data.participant, name = data.participantdisplay}
 	end
+
+	data.opponent = Opponent.resolve(Opponent.readOpponentArgs(opponentArgs) or Opponent.tbd(), date)
+
+	data.match = {w = data.win_m, d = data.tie_m, l = data.lose_m}
+	data.game = {w = data.win_g, d = data.tie_g, l = data.lose_g}
+	StandingsStorage.entry(data, data.standingsindex)
 
 	data.match = {data.win_m, data.tie_m, data.lose_m}
 	data.game = {data.win_g, data.tie_g, data.lose_g}
-	return StandingsStorage.run(data.placement, data)
+	StandingsStorage.runLegacy(data.standingsindex, data)
+end
+
+-- Legacy input
+function StandingsStorage.fromTemplate(frame)
+	return StandingsStorage.fromTemplateEntry(frame)
 end
 
 return StandingsStorage
