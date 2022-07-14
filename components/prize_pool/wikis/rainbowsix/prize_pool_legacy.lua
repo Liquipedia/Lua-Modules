cfd4ec4851605c88816c13c7b694d3ce1e879827
Rikard Blixt <rikardblixt@gmail.com>
2022-07-14 11:22:54 +0200

    Legacy Mapping for R6 Prizepool (#1479)
    * Initial version of PPT legacy for R6
    
    * Cleanup
    
    * Pipeline
    
    * Module has been renamed
    
    * Rework opponent
    
    * Code style and minor refactor
    
    * Cleaner
    
    * Proper way
    
    * Rework to use a commons custom with wiki injection
    
    * Improve parsing when templates are involved
    For example `{{LeagueIconSmall/copa_elite six}} [[Copa Elite Six/2022/Stage 1|Copa Elite Six]]`
    would be parsed incorrectly before.
    
    * Made functions public (and overridable)

---
+++ components/prize_pool/wikis/rainbowsix/prize_pool_legacy.lua
@@ -0,0 +1,21 @@
+---
+-- @Liquipedia
+-- wiki=rainbowsix
+-- page=Module:PrizePool/Legacy/Custom
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Lua = require('Module:Lua')
+
+local PrizePoolLegacy = Lua.import('Module:PrizePool/Legacy', {requireDevIfEnabled = true})
+
+local CustomLegacyPrizePool = {}
+
+-- Template entry point
+function CustomLegacyPrizePool.run()
+	return PrizePoolLegacy.run()
+end
+
+
+return CustomLegacyPrizePool
