8bcf34b0e9298d0b291d106c043591bb42958a5a
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2022-07-22 14:03:08 +0200

    Store parentname in matches

--- components/match2/commons/match_group_input.lua
+++ components/match2/commons/match_group_input.lua
@@ -49,6 +49,7 @@ function MatchGroupInput.readMatchlist(bracketId, args)
 			bracketData.matchIndex = matchIndex
 
 			match.parent = context.tournamentParent
+			match.parentname = context.tournamentParentName
 			bracketData.bracketindex = context.bracketIndex
 			bracketData.groupRoundIndex = context.groupRoundIndex
 			bracketData.sectionheader = context.sectionHeader
@@ -116,6 +117,7 @@ function MatchGroupInput.readBracket(bracketId, args, options)
 		bracketData.inheritedheader = MatchGroupInput._inheritedHeader(bracketData.header)
 
 		match.parent = context.tournamentParent
+		match.parentname = context.tournamentParentName
 		bracketData.bracketindex = context.bracketIndex
 		bracketData.groupRoundIndex = context.groupRoundIndex
 		bracketData.sectionheader = context.sectionHeader
@@ -269,6 +271,7 @@ function MatchGroupInput.readContext(matchArgs, matchGroupArgs)
 		matchSection = matchArgs.matchsection or matchGroupArgs.matchsection or globalVars:get('matchsection'),
 		sectionHeader = matchGroupArgs.section or globalVars:get('bracket_header'),
 		tournamentParent = globalVars:get('tournament_parent'),
+		tournamentParentName = globalVars:get('tournament_parent'),
 	}
 end
 
