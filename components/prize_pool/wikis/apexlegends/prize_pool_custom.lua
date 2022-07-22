885c66c9c80547bdaae7bc463177e33da08c101d
Jasper Jacobs <97167868+Kanopedia@users.noreply.github.com>
2022-07-23 01:26:08 +0200

    Add Custom Prizepool handler to Apex (#1545)
    * Create prize_pool_custom.lua
    
    Currently already active and working across the wiki:
    
    https://liquipedia.net/apexlegends/Douyu/2022/Defender_Cup/Season_2
    https://liquipedia.net/apexlegends/Apex_Legends_Global_Series/2022/Championship
    
    * Update prize_pool_custom.lua
    
    * Update prize_pool_custom.lua
    
    * Update components/prize_pool/wikis/apexlegends/prize_pool_custom.lua
    
    Co-authored-by: Rikard Blixt <rikardblixt@gmail.com>
    
    * Update components/prize_pool/wikis/apexlegends/prize_pool_custom.lua
    
    Co-authored-by: hjpalpha <75081997+hjpalpha@users.noreply.github.com>
    
    * Update components/prize_pool/wikis/apexlegends/prize_pool_custom.lua
    
    Co-authored-by: hjpalpha <75081997+hjpalpha@users.noreply.github.com>
    
    * Update components/prize_pool/wikis/apexlegends/prize_pool_custom.lua
    
    Co-authored-by: hjpalpha <75081997+hjpalpha@users.noreply.github.com>
    
    Co-authored-by: Rikard Blixt <rikardblixt@gmail.com>
    Co-authored-by: hjpalpha <75081997+hjpalpha@users.noreply.github.com>

---
+++ components/prize_pool/wikis/apexlegends/prize_pool_custom.lua
@@ -0,0 +1,46 @@
+---
+-- @Liquipedia
+-- wiki=apexlegends
+-- page=Module:PrizePool/Custom
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Arguments = require('Module:Arguments')
+local Class = require('Module:Class')
+local Lua = require('Module:Lua')
+local Variables = require('Module:Variables')
+local Weight = require('Module:Weight')
+
+local PrizePool = Lua.import('Module:PrizePool', {requireDevIfEnabled = true})
+
+local LpdbInjector = Lua.import('Module:Lpdb/Injector', {requireDevIfEnabled = true})
+local CustomLpdbInjector = Class.new(LpdbInjector)
+
+local CustomPrizePool = {}
+
+
+-- Template entry point
+function CustomPrizePool.run(frame)
+	local args = Arguments.getArgs(frame)
+	local prizePool = PrizePool(args):create()
+
+	prizePool:setLpdbInjector(CustomLpdbInjector())
+
+	return prizePool:build()
+end
+
+function CustomLpdbInjector:adjust(lpdbData, placement, opponent)
+	lpdbData.weight = Weight.calc(
+		lpdbData.prizemoney,
+		Variables.varDefault('tournament_liquipediatier'),
+		placement.placeStart,
+		Variables.varDefault('tournament_type'),
+		Variables.varDefault('tournament_liquipediatiertype')
+
+	)
+
+	return lpdbData
+end
+
+return CustomPrizePool
