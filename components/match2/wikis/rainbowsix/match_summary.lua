3cd3f7056a227b4a985f69ed080135c3237c629f
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2022-07-25 18:19:12 +0200

    R6

--- components/match2/wikis/rainbowsix/match_summary.lua
+++ components/match2/wikis/rainbowsix/match_summary.lua
@@ -311,7 +311,7 @@ local CustomMatchSummary = {}
 
 
 function CustomMatchSummary.getByMatchId(args)
-	local match = MatchGroupUtil.fetchMatchForBracketDisplay(args.bracketId, args.matchId)
+	local match = MatchGroupUtil.fetchMatchForBracketDisplay(args.bracketId, args.matchId, _)
 
 	local matchSummary = MatchSummary():init()
 	matchSummary.root:css('flex-wrap', 'unset') -- temporary workaround to fix height, taken from RL
