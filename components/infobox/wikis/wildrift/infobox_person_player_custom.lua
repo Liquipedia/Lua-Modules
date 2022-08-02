7e5150500c74cab01681d9ee5d4962290ed5cd58
Jasper Jacobs <97167868+Kanopedia@users.noreply.github.com>
2022-08-02 15:25:55 +0200

    Update infobox_person_player_custom.lua

--- components/infobox/wikis/wildrift/infobox_person_player_custom.lua
+++ components/infobox/wikis/wildrift/infobox_person_player_custom.lua
@@ -89,6 +89,11 @@ end
 
 function CustomInjector:parse(id, widgets)
 	if id == 'status' then
+		local status = _args.status
+		if String.isNotEmpty(status) then
+			status = mw.getContentLanguage():ucfirst(status)
+		end
+
 		return {
 			Cell{name = 'Status', content = {Page.makeInternalLink({onlyIfExists = true},
 						_args.status) or _args.status}},
