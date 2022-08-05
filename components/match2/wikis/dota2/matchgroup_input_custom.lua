d029a60410d073b41a38d5ba90351f00c62b20b5
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2022-08-05 16:51:56 +0200

    dota2

--- components/match2/wikis/dota2/matchgroup_input_custom.lua
+++ components/match2/wikis/dota2/matchgroup_input_custom.lua
@@ -391,10 +391,6 @@ end
 
 function matchFunctions.getExtraData(match)
 	match.extradata = {
-		timezoneid = match.timezoneId,
-		timezoneoffset = match.timezoneOffset,
-		matchsection = Variables.varDefault('matchsection'),
-		comment = match.comment,
 		featured = Logic.emptyOr(
 			match.featured,
 			matchFunctions.isFeatured(match)
