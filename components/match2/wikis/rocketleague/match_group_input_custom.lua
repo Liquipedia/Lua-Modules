e2dae9dc43b6b7675e3f71eac5bc77939c475551
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2022-08-05 14:15:18 +0200

    rl

--- components/match2/wikis/rocketleague/match_group_input_custom.lua
+++ components/match2/wikis/rocketleague/match_group_input_custom.lua
@@ -199,6 +199,8 @@ function matchFunctions.getExtraData(match)
 	table.sort(casters, function(c1, c2) return c1.displayName:lower() < c2.displayName:lower() end)
 
 	match.extradata = {
+		timezoneid = match.timezoneId,
+		timezoneoffset = match.timezoneOffset,
 		matchsection = Variables.varDefault('matchsection'),
 		team1icon = getIconName(opponent1.template or ''),
 		team2icon = getIconName(opponent2.template or ''),
