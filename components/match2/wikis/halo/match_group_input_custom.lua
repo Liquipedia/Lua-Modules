e11ca04669963fd77817bd073de9e45c809fde89
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2022-08-05 16:52:12 +0200

    halo

--- components/match2/wikis/halo/match_group_input_custom.lua
+++ components/match2/wikis/halo/match_group_input_custom.lua
@@ -325,10 +325,6 @@ end
 
 function matchFunctions.getExtraData(match)
 	match.extradata = {
-		timezoneid = match.timezoneId,
-		timezoneoffset = match.timezoneOffset,
-		matchsection = Variables.varDefault('matchsection'),
-		comment = match.comment,
 		mvp = matchFunctions.getMVP(match),
 		isconverted = 0
 	}
