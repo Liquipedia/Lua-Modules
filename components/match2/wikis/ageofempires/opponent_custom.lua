2e43286273816153b97128da4f719c006937f072
mbergen <mail@mbergen.de>
2022-08-08 21:27:14 +0200

    Use common Opponent:resolve

--- components/match2/wikis/ageofempires/opponent_custom.lua
+++ components/match2/wikis/ageofempires/opponent_custom.lua
@@ -9,28 +9,19 @@
 local Table = require('Module:Table')
 local Opponent = require('Module:Opponent')
 local Abbreviation = require('Module:Abbreviation')
-local TeamTemplate = require('Module:TeamTemplate')
 
 
 local CustomOpponent = Table.deepCopy(Opponent)
 
 function CustomOpponent.resolve(opponent, date)
-	if opponent.type == Opponent.team then
-		opponent.template = TeamTemplate.resolve(opponent.template, date) or 'tbd'
-	elseif Opponent.typeIsParty(opponent.type) then
-		local PlayerExt = require('Module:Player/Ext')
+	Opponent.resolve(opponent, date, {syncPlayer = true})
+	if Opponent.typeIsParty(opponent.type) then
+		local PlayerExt = require('Module:Player/Ext/Custom')
 		for _, player in ipairs(opponent.players) do
 			if Opponent.playerIsTbd(player) then
 				player.displayName = Abbreviation.make('TBD', 'To be determined (or to be decided)')
 			else
-				if player.flag then
-					PlayerExt.populatePageName(player)
-				else
-					PlayerExt.syncPlayer(player)
-				end
-				if player.team then
-					player.team = TeamTemplate.resolve(player.team, date)
-				else
+				if not player.team then
 					player.team = PlayerExt.syncTeam(player.pageName, nil)
 				end
 			end
