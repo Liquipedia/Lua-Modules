e8a9b8367c3b655fb47237c4b0b867478b50a663
mbergen <mail@mbergen.de>
2022-08-09 10:21:57 +0200

    Implement `PrizePool` for Age of Empires (#1641)
    * Add PrizePool/Custom, Opponent/Custom
    
    - Implements AoE-specific weight calculation
    - Sets some extradata
    - Resolves flags and teams for party opponents
    
    * Add Module:Player/Ext/Custom to override syncTeam/fetchTeamHistoryEntry
    
    * Add missing legacy extradata, apply suggestions
    
    * Fix whitespace lines
    
    * Use common Opponent:resolve
    
    * Use custom Module:Opponent, remove unused extradata.monthandday, move setting of patch up a scope
    
    * Remove superflous setting of participantflag
    
    * Use resolve options, use correct prizepool arg
    
    * Lock syncTeam behind options.syncPlayer
    
    Co-authored-by: hjpalpha <75081997+hjpalpha@users.noreply.github.com>
    
    * Move require of PlayerExt up
    
    Co-authored-by: hjpalpha <75081997+hjpalpha@users.noreply.github.com>

---
+++ components/prize_pool/wikis/ageofempires/prize_pool_custom.lua
@@ -0,0 +1,77 @@
+---
+-- @Liquipedia
+-- wiki=ageofempires
+-- page=Module:PrizePool/Custom
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Arguments = require('Module:Arguments')
+local Class = require('Module:Class')
+local Lua = require('Module:Lua')
+local String = require('Module:StringUtils')
+local Variables = require('Module:Variables')
+local Opponent = require('Module:Opponent/Custom')
+
+local PrizePool = Lua.import('Module:PrizePool', {requireDevIfEnabled = true})
+
+local LpdbInjector = Lua.import('Module:Lpdb/Injector', {requireDevIfEnabled = true})
+local CustomLpdbInjector = Class.new(LpdbInjector)
+
+local CustomPrizePool = {}
+
+local TIER_VALUE = {10, 6, 4, 2}
+
+-- Template entry point
+function CustomPrizePool.run(frame)
+	local args = Arguments.getArgs(frame)
+	args.opponentLibrary = 'Opponent/Custom'
+	args.syncPlayers = true
+
+	local prizePool = PrizePool(args)
+		:create()
+
+	prizePool:setLpdbInjector(CustomLpdbInjector())
+
+	return prizePool:build()
+end
+
+function CustomLpdbInjector:adjust(lpdbData, placement, opponent)
+	lpdbData.weight = CustomPrizePool.calculateWeight(
+		lpdbData.prizemoney,
+		Variables.varDefault('tournament_liquipediatier'),
+		placement.placeStart,
+		Variables.varDefault('tournament_type')
+	)
+	if opponent.opponentData.type == Opponent.solo then
+		-- legacy extradata, to be removed once unused
+		lpdbData.extradata.participantname = opponent.opponentData.players[1].displayName
+		lpdbData.extradata.participantteam = opponent.opponentData.players[1].team
+
+		if opponent.additionalData.LASTVS then
+			lpdbData.extradata.lastvsflag = opponent.additionalData.LASTVS.players[1].flag
+			lpdbData.extradata.lastvsname = opponent.additionalData.LASTVS.players[1].displayName
+		end
+	end
+
+	lpdbData.extradata.patch = Variables.varDefault('tournament_patch')
+
+	-- legacy points, to be standardized
+	lpdbData.extradata.points = placement.prizeRewards.POINTS1
+	lpdbData.extradata.points2 = placement.prizeRewards.POINTS2
+
+	return lpdbData
+end
+
+function CustomPrizePool.calculateWeight(prizeMoney, tier, place, type)
+	if String.isEmpty(tier) then
+		return 0
+	end
+
+	local tierValue = TIER_VALUE[tier] or TIER_VALUE[tonumber(tier) or ''] or 1
+	local onlineFactor = type == 'Online' and 0.65 or 1
+
+	return tierValue * math.max(prizeMoney, 0.001) / place * onlineFactor
+end
+
+return CustomPrizePool
