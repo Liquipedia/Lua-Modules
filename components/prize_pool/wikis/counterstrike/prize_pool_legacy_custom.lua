e26c6746fcc1914997800833ce2845bd6aab8e13
Rikard Blixt <rikardblixt@gmail.com>
2022-08-12 09:17:36 +0200

    Implement PPT Custom for Counterstrike (#1669)
    * Initial version of CS PPT Custom
    
    * Add handling for SMW mute, overriding tournament name and result name
    
    * Add comment to the LPDB Tournament setting
    
    * Turn off automations
    
    * Create Legacy Custom for CS, some stuff needed
    
    * Add exception in legacy for local prizes with invalid characters
    
    * Add comments
    
    * Disable storage on disable_SMW_storage
    
    * Comment style
    
    * Add publisher tier storage

---
+++ components/prize_pool/wikis/counterstrike/prize_pool_legacy_custom.lua
@@ -0,0 +1,56 @@
+---
+-- @Liquipedia
+-- wiki=counterstrike
+-- page=Module:PrizePool/Legacy/Custom
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Lua = require('Module:Lua')
+
+local PrizePoolLegacy = Lua.import('Module:PrizePool/Legacy', {requireDevIfEnabled = true})
+
+local CustomLegacyPrizePool = {}
+
+-- Template entry point
+function CustomLegacyPrizePool.run()
+	return PrizePoolLegacy.run(CustomLegacyPrizePool)
+end
+
+function CustomLegacyPrizePool.customSlot(newData, CACHED_DATA, slot)
+	-- 0 used to mean unset, so let's unset it
+	if newData.freetext1 == '0' then
+		newData.freetext1 = nil
+	end
+
+	-- Requested by CS so they can do cleanup of tables with incorrect data
+	if newData.localprize then
+		if newData.localprize:match('[^,%.%d]') then
+			error('Unexpected value in localprize for place=' .. slot.place)
+		end
+	end
+
+	return newData
+end
+
+function CustomLegacyPrizePool.customOpponent(opponentData, CACHED_DATA, slot, opponentIndex)
+	-- CS didn't support multiple points (etc), however they supported points (etc) per opponent
+
+	if slot['usdprize' .. opponentIndex] then
+		opponentData.usdprize = slot['usdprize' .. opponentIndex]
+	end
+
+	if slot['points' .. opponentIndex] then
+		local param = CACHED_DATA.inputToId['points']
+		opponentData[param] = slot['points' .. opponentIndex]
+	end
+
+	if slot['localprize' .. opponentIndex] then
+		local param = CACHED_DATA.inputToId['localprize']
+		opponentData[param] = slot['localprize' .. opponentIndex]
+	end
+
+	return opponentData
+end
+
+return CustomLegacyPrizePool
