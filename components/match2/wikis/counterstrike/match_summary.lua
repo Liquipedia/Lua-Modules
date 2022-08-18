7bd39a9b9d5bb423cc5b9cfdc345991ffc2e716c
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2022-08-18 11:44:11 +0200

    kick unused require

--- components/match2/wikis/counterstrike/match_summary.lua
+++ components/match2/wikis/counterstrike/match_summary.lua
@@ -9,7 +9,6 @@
 local Class = require('Module:Class')
 local Logic = require('Module:Logic')
 local Lua = require('Module:Lua')
-local String = require('Module:StringUtils')
 local Table = require('Module:Table')
 local VodLink = require('Module:VodLink')
 
