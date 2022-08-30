eeb61ddc2fafc90955ebe59306b53219fb1eb0f7
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2022-08-30 19:42:29 +0200

    adjust teamcard storage to also use lpdb prefix if set + change var names in the module

--- components/team_card/team_card_storage.lua
+++ components/team_card/team_card_storage.lua
@@ -26,31 +26,32 @@ function TeamCardStorage.saveToLpdb(args, teamObject, players, playerPrize)
 		end
 	end
 
-	local smwPrefix = args.smw_prefix or Variables.varDefault('smw_prefix') or ''
+	local lpdbPrefix = args.lpdb_prefix or Variables.varDefault('lpdb_prefix')
+		or args.smw_prefix or Variables.varDefault('smw_prefix') or ''
 
 	-- Setup LPDB Data
 	local lpdbData = {}
-	lpdbData = TeamCardStorage._addStandardLpdbFields(lpdbData, team, args, smwPrefix)
+	lpdbData = TeamCardStorage._addStandardLpdbFields(lpdbData, team, args, lpdbPrefix)
 	lpdbData.participanttemplate = teamTemplateName
 	lpdbData.players = players
 	lpdbData.individualprizemoney = playerPrize
 
 	-- If a custom override for LPDB exists, use it
-	lpdbData = Custom.adjustLpdb and Custom.adjustLpdb(lpdbData, team, args, smwPrefix) or lpdbData
+	lpdbData = Custom.adjustLpdb and Custom.adjustLpdb(lpdbData, team, args, lpdbPrefix) or lpdbData
 
 	-- Jsonify the json fields
 	lpdbData.extradata = mw.ext.LiquipediaDB.lpdb_create_json(lpdbData.extradata)
 	lpdbData.players = mw.ext.LiquipediaDB.lpdb_create_json(lpdbData.players)
 
 	-- Name must match prize pool insertion
-	local storageName = Custom.getLpdbObjectName and Custom.getLpdbObjectName(team, smwPrefix)
-						or TeamCardStorage._getLpdbObjectName(team, smwPrefix)
+	local storageName = Custom.getLpdbObjectName and Custom.getLpdbObjectName(team, lpdbPrefix)
+						or TeamCardStorage._getLpdbObjectName(team, lpdbPrefix)
 
 	mw.ext.LiquipediaDB.lpdb_placement(storageName, lpdbData)
 end
 
 -- Adds basic lpdb fields
-function TeamCardStorage._addStandardLpdbFields(lpdbData, team, args, smwPrefix)
+function TeamCardStorage._addStandardLpdbFields(lpdbData, team, args, lpdbPrefix)
 	local title = mw.title.getCurrentTitle().text
 	local tournamentName = Variables.varDefault('tournament name pp') or Variables.varDefault('tournament_name')
 	local date = Variables.varDefault('tournament_date')
@@ -62,7 +63,7 @@ function TeamCardStorage._addStandardLpdbFields(lpdbData, team, args, smwPrefix)
 	lpdbData.series = Variables.varDefault('tournament_series')
 	lpdbData.parent = Variables.varDefault('tournament_parent')
 	lpdbData.startdate = startDate
-	lpdbData.date = args.date or Variables.varDefault('enddate_' .. team .. smwPrefix .. '_date') or endDate
+	lpdbData.date = args.date or Variables.varDefault('enddate_' .. team .. lpdbPrefix .. '_date') or endDate
 	lpdbData.qualifier, lpdbData.qualifierpage, lpdbData.qualifierurl = Qualifier.parseQualifier(args.qualifier)
 
 	if team ~= 'TBD' then
@@ -83,10 +84,10 @@ function TeamCardStorage._addStandardLpdbFields(lpdbData, team, args, smwPrefix)
 end
 
 -- Build the standard LPDB "Object Name", which is used as primary key in the DB record
-function TeamCardStorage._getLpdbObjectName(team, smwPrefix)
+function TeamCardStorage._getLpdbObjectName(team, lpdbPrefix)
 	local storageName = 'ranking'
-	if String.isNotEmpty(smwPrefix) then
-		storageName = storageName .. '_' .. smwPrefix
+	if String.isNotEmpty(lpdbPrefix) then
+		storageName = storageName .. '_' .. lpdbPrefix
 	end
 	storageName = storageName .. '_' ..  mw.ustring.lower(team)
 	if team == 'TBD' then
