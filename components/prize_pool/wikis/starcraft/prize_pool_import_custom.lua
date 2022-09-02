5f2ebfe0b46d68b363a4f98d89e732a73a93b2ed
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2022-09-02 09:02:20 +0200

    Update prize_pool_import_custom.lua

--- components/prize_pool/wikis/starcraft/prize_pool_import_custom.lua
+++ components/prize_pool/wikis/starcraft/prize_pool_import_custom.lua
@@ -1,12 +1,1 @@
----
--- @Liquipedia
--- wiki=starcraft
--- page=Module:PrizePool/Import/Custom
---
--- Please see https://github.com/Liquipedia/Lua-Modules to contribute
---
 
--- default custom that only "redirects" to the Base with dev option
-
-local Lua = require('Module:Lua')
-return Lua.import('Module:PrizePool/Import/Starcraft', {requireDevIfEnabled = true})
