5f5fc2b086701a87cf8566ca9341025008662178
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2022-09-02 10:16:02 +0200

    Update prize_pool_import_custom.lua

--- components/prize_pool/wikis/starcraft2/prize_pool_import_custom.lua
+++ components/prize_pool/wikis/starcraft2/prize_pool_import_custom.lua
@@ -6,7 +6,70 @@
 -- Please see https://github.com/Liquipedia/Lua-Modules to contribute
 --
 
--- default custom that only "redirects" to the Base with dev option
-
+local Logic = require('Module:Logic')
 local Lua = require('Module:Lua')
-return Lua.import('Module:PrizePool/Import/Starcraft', {requireDevIfEnabled = true})
+
+local Import = Lua.import('Module:PrizePool/Import', {requireDevIfEnabled = true})
+local Opponent = Lua.import('Module:Opponent', {requireDevIfEnabled = true})
+local PlayerExt = Lua.import('Module:Player/Ext', {requireDevIfEnabled = true})
+local TournamentUtil = Lua.import('Module:Tournament/Util', {requireDevIfEnabled = true})
+
+local CustomImport = {}
+
+local IMPORT_DEFAULT_ENABLE_START = '2022-01-14'
+
+function CustomImport.run(placements, args)
+	args.importLimit = tonumber(args.importLimit) or CustomImport._computeDefaultImportLimit()
+	args.import = CustomImport._computeImportEnable(args.import)
+
+	-- call the commons import + populate player Teams where necessary
+	return CustomImport._populatePlayerTeams(Import.run(placements, args))
+end
+
+function CustomImport._computeDefaultImportLimit()
+	local tier = tonumber(globalVars:get('tournament_liquipediatier'))
+	if not tier then
+		mw.log('Prize Pool Import: Unset liquipediatier')
+	end
+
+	return tier and tier >= 4 and 8
+		or tier == 3 and 16
+		or nil
+end
+
+function CustomImport._computeImportEnable(importInput)
+	local tournamentEndDate = TournamentUtil.getContextualDate()
+	return Logic.nilOr(
+		Logic.readBoolOrNil(importInput),
+		not tournamentEndDate or tournamentEndDate >= IMPORT_DEFAULT_ENABLE_START
+	)
+end
+
+function CustomImport._populatePlayerTeams(placements)
+	for _, placement in pairs(placements) do
+		for _, opponent in pairs(placement.opponents) do
+			opponent.opponentData = CustomImport._completeOpponentPlayerTeams(
+				opponent.opponentData,
+				opponent.date or placement.date
+			)
+		end
+	end
+
+	return placements
+end
+
+function CustomImport._completeOpponentPlayerTeams(opponent, date)
+	if not Opponent.typeIsParty(opponent.type) then
+		return opponent
+	end
+
+	for _, player in pairs(opponent.players) do
+		if not player.team and player.displayName and player.pageName and not Opponent.playerIsTbd(player) then
+			player.team = PlayerExt.syncTeam(player.pageName, team, {date = date})
+		end
+	end
+
+	return opponent
+end
+
+return CustomImport
