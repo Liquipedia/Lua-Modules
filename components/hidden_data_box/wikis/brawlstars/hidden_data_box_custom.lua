1d55cbd98a9f724fa2ac3810a6fc1604d3f18d93
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2022-09-03 21:31:12 +0200

    Add brawlstars HDB

---
+++ components/hidden_data_box/wikis/brawlstars/hidden_data_box_custom.lua
@@ -0,0 +1,37 @@
+---
+-- @Liquipedia
+-- wiki=brawlstars
+-- page=Module:HiddenDataBox/Custom
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Class = require('Module:Class')
+local Variables = require('Module:Variables')
+
+local BasicHiddenDataBox = require('Module:HiddenDataBox')
+local CustomHiddenDataBox = {}
+
+function CustomHiddenDataBox.run(args)
+	BasicHiddenDataBox.addCustomVariables = CustomHiddenDataBox.addCustomVariables
+	return BasicHiddenDataBox.run(args)
+end
+
+function CustomHiddenDataBox.addCustomVariables(args, queryResult)
+	Variables.varDefine('tournament_tier', Variables.varDefault('tournament_liquipediatier'))
+	Variables.varDefine('tournament_tier_type', Variables.varDefault('tournament_liquipediatiertype'))
+	Variables.varDefine('tournament_ticker_name', Variables.varDefault('tournament_tickername'))
+	Variables.varDefine('tournament_parent_name', queryResult.name or '')
+	Variables.varDefine('tournament_riot_premier', Variables.varDefault('tournament_publishertier', ''))
+
+	local endDate = Variables.varDefault('tournament_enddate')
+	local startDate = Variables.varDefault('tournament_startdate')
+	if startDate == endDate then
+		Variables.varDefine('date', endDate)
+	else
+		Variables.varDefine('edate', endDate)
+		Variables.varDefine('sdate', startDate)
+	end
+end
+
+return Class.export(CustomHiddenDataBox)
