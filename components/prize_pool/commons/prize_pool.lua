Liquipedia/Lua-Modules

commit da5d4c19737208f9a562416903a838cdfe393bed

Allow overwrite of lpdbTournamentData in prizePools (#1815)
* Allow overwrite of lpdbTournamentData in pirzePools

* Update components/prize_pool/commons/prize_pool.lua

Co-authored-by: Rikard Blixt <rikardblixt@gmail.com>

Co-authored-by: Rikard Blixt <rikardblixt@gmail.com>

components/prize_pool/commons/prize_pool.lua
@@ -840,7 +840,7 @@ function PrizePool:_storeData()
 	for _, placement in ipairs(self.placements) do
 		local lpdbEntries = placement:_getLpdbData()
 
-		Array.forEach(lpdbEntries, function(lpdbEntry) Table.mergeInto(lpdbEntry, lpdbTournamentData) end)
+		Array.forEach(lpdbEntries, function(lpdbEntry) Table.merge(lpdbTournamentData, lpdbEntry) end)
 
 		Array.extendWith(lpdbData, lpdbEntries)
 	end

commit 7350be62272fb87c1b23d31951762f7cd4cc38a9

Fix bug from #1815 - entries are not updated with tournament data (#1825)
* Fix bug introduced in #1815

* Save the return

components/prize_pool/commons/prize_pool.lua
@@ -849,7 +849,7 @@ function PrizePool:_storeData()
 	for _, placement in ipairs(self.placements) do
 		local lpdbEntries = placement:_getLpdbData()
 
-		Array.forEach(lpdbEntries, function(lpdbEntry) Table.merge(lpdbTournamentData, lpdbEntry) end)
+		lpdbEntries = Array.map(lpdbEntries, function(lpdbEntry) return Table.merge(lpdbTournamentData, lpdbEntry) end)
 
 		Array.extendWith(lpdbData, lpdbEntries)
 	end