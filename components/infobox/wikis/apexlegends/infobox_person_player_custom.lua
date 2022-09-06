35a340a9e3bdf8ec37fe12577c785f93555b3680
DMeluca <97838082+DMeluca@users.noreply.github.com>
2022-09-06 15:01:17 +0100

    Changed upcoming matches in player infoboxes
    Currently the infobox checks for a team, and then uses that information to display upcoming tournaments.
    However this does not allow for substitutes and has led to several (minor) errors of players being displayed incorrectly. 
    The current template does support players as input, but does not allow for more than one input query (i.e a player with several aliases)
    The module called here replaces the commons module, and is a workaround for apex, as it does not store match2 data yet,

--- components/infobox/wikis/apexlegends/infobox_person_player_custom.lua
+++ components/infobox/wikis/apexlegends/infobox_person_player_custom.lua
@@ -17,6 +17,7 @@ local Table = require('Module:Table')
 local Team = require('Module:Team')
 local Variables = require('Module:Variables')
 local Template = require('Module:Template')
+local UpcomingMatches = require('Module:Matches Player')
 
 local Injector = require('Module:Infobox/Widget/Injector')
 local Cell = require('Module:Infobox/Widget/Cell')
@@ -142,10 +143,8 @@ function CustomPlayer:adjustLPDB(lpdbData)
 end
 
 function CustomPlayer:createBottomContent(infobox)
-	if Player:shouldStoreData(_args) and String.isNotEmpty(_args.team) then
-		local teamPage = Team.page(mw.getCurrentFrame(),_args.team)
-		return
-			Template.safeExpand(mw.getCurrentFrame(), 'Upcoming and ongoing tournaments of', {team = teamPage})
+	if Player:shouldStoreData(_args) then
+		return UpcomingMatches.get(_args)
 	end
 end
 
