dccd6f8c50832f05b080dae34500cc40c310e0e5
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2022-09-07 18:24:10 +0200

    Add option in PrizePool/Placement to not call `readOpponentArgs`

--- components/prize_pool/commons/prize_pool_placement.lua
+++ components/prize_pool/commons/prize_pool_placement.lua
@@ -276,7 +276,12 @@ function Placement:_parseOpponentArgs(input, date)
 	opponentArgs.type = opponentArgs.type or self.parent.opponentType
 	assert(Opponent.isType(opponentArgs.type), 'Invalid type')
 
-	local opponentData = Opponent.readOpponentArgs(opponentArgs)
+	local opponentData
+	if opponentArgs.isAlreadyParsed then
+		opponentData = opponentArgs[1] or opponentArgs
+	else
+		opponentData = Opponent.readOpponentArgs(opponentArgs)
+	end
 
 	if not opponentData or (Opponent.isTbd(opponentData) and opponentData.type ~= Opponent.literal) then
 		opponentData = Opponent.tbd(opponentArgs.type)
