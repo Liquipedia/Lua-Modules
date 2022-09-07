233f0f46136c2acaa5a8910bb2b61f79faf60149
Hesketh2 <88981446+Hesketh2@users.noreply.github.com>
2022-09-07 16:43:29 +0700

    Add PPT (Legacy) for Mobile Legends

---
+++ components/prize_pool/wikis/prize_pool_legacy_custom.lua
@@ -0,0 +1,33 @@
+---
+-- @Liquipedia
+-- wiki=mobilelegends
+-- page=Module:PrizePool/Legacy/Custom
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Lua = require('Module:Lua')
+local Variables = require('Module:Variables')
+
+local PrizePoolLegacy = Lua.import('Module:PrizePool/Legacy', {requireDevIfEnabled = true})
+
+local CustomLegacyPrizePool = {}
+
+-- Template entry point
+function CustomLegacyPrizePool.run()
+	return PrizePoolLegacy.run(CustomLegacyPrizePool)
+end
+
+function CustomLegacyPrizePool.customHeader(newArgs, data, header)
+	newArgs.prizesummary = header.prizenote and true or newArgs.prizesummary
+
+	local localCurrency = Variables.varDefault('currency')
+	if not newArgs.localcurrency and localCurrency then
+		newArgs.localcurrency = localCurrency
+		data.inputToId.localprize = 'localprize'
+	end
+
+	return newArgs
+end
+
+return CustomLegacyPrizePool
