cfa6d7b8047f2adf990f745f6672702a48b925f3
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2022-09-07 10:07:54 +0200

    adjust comment

--- components/match2/commons/player_ext.lua
+++ components/match2/commons/player_ext.lua
@@ -239,7 +239,9 @@ function PlayerExt.syncTeam(pageName, template, options)
 		template = template ~= 'noteam' and template or nil,
 	}
 
-	-- catch some edge cases for pageVarEntry
+	-- catch some edge cases for pageVarEntry where pageVarEntry.team
+	-- (unresolved team template or lowercased underscore replaced pagename of the team)
+	-- is set while pageVarEntry.template is not set
 	if pageVarEntry and not pageVarEntry.template then
 		pageVarEntry.template = pageVarEntry.team
 		pageVarEntry.isResolved = nil
