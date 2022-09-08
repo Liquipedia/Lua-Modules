4eb09030527c5e9da7c5cae393ccc110c8d464bf
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2022-09-08 10:45:57 +0200

    sc2 redirect

---
+++ components/prize_pool/wikis/starcraft2/prize_pool_custom.lua
@@ -0,0 +1,9 @@
+---
+-- @Liquipedia
+-- wiki=starcraft2
+-- page=Module:PrizePool/Custom
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+return require('Module:PrizePool/Starcraft')
