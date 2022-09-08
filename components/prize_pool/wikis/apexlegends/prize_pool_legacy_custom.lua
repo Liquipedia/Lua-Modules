8a486868ab0a36eb99c627bc78cd30ea476ecce2
DMeluca <97838082+DMeluca@users.noreply.github.com>
2022-09-08 16:58:53 +0100

    Update prize_pool_legacy_custom.lua
    Didnt realise that the dependency call would create an object that is permanent. And therefore I can set a true/false in the header call

--- components/prize_pool/wikis/apexlegends/prize_pool_legacy_custom.lua
+++ components/prize_pool/wikis/apexlegends/prize_pool_legacy_custom.lua
@@ -12,7 +12,7 @@ local Lua = require('Module:Lua')
 local PrizePoolLegacy = Lua.import('Module:PrizePool/Legacy', {requireDevIfEnabled = true})
 
 local CustomLegacyPrizePool = {}
-
+local CLEAN_PLACE = false
 -- Template entry point
 function CustomLegacyPrizePool.run()
 	return PrizePoolLegacy.run(CustomLegacyPrizePool)
@@ -22,13 +22,14 @@ function CustomLegacyPrizePool.customHeader(newArgs, data, header)
 	if Logic.readBool(header.seed) then
 		PrizePoolLegacy.assignType(newArgs, 'seed', 'seed')
 	end
-
+	CLEAN_PLACE = Logic.readBool(header.cleanplace)
 	return newArgs
 end
 
 function CustomLegacyPrizePool.customSlot(newData, CACHED_DATA, slot)
-	newData["place"] = nil
+	if Logic.readBool(CLEAN_PLACE) then
+		newData["place"] = nil
+	end
 	return newData
 end
-
 return CustomLegacyPrizePool
