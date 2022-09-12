99ecf95dff34735d93b215d7ed011fe471af8c8a
DMeluca <97838082+DMeluca@users.noreply.github.com>
2022-09-12 15:16:43 +0100

    Allow places to autofill from PrizePool (#1857)
    * Allow places to autofill from PrizePool
    
    Currently a lot of prizepools which call the legacy modules which have many teams in 1 place slot. This allows the PrizePool module to autoassign any slot with the correct sequential order
    
    * Update prize_pool_legacy_custom.lua
    
    Didnt realise that the dependency call would create an object that is permanent. And therefore I can set a true/false in the header call
    
    * Update prize_pool_legacy_custom.lua
    
    * Update prize_pool_legacy_custom.lua

--- components/prize_pool/wikis/apexlegends/prize_pool_legacy_custom.lua
+++ components/prize_pool/wikis/apexlegends/prize_pool_legacy_custom.lua
@@ -12,7 +12,7 @@ local Lua = require('Module:Lua')
 local PrizePoolLegacy = Lua.import('Module:PrizePool/Legacy', {requireDevIfEnabled = true})
 
 local CustomLegacyPrizePool = {}
-
+local REMOVE_PLACE = false
 -- Template entry point
 function CustomLegacyPrizePool.run()
 	return PrizePoolLegacy.run(CustomLegacyPrizePool)
@@ -22,8 +22,14 @@ function CustomLegacyPrizePool.customHeader(newArgs, data, header)
 	if Logic.readBool(header.seed) then
 		PrizePoolLegacy.assignType(newArgs, 'seed', 'seed')
 	end
-
+	REMOVE_PLACE = Logic.readBool(header.removeplace)
 	return newArgs
 end
 
+function CustomLegacyPrizePool.customSlot(newData, CACHED_DATA, slot)
+	if REMOVE_PLACE then
+		newData["place"] = nil
+	end
+	return newData
+end
 return CustomLegacyPrizePool
