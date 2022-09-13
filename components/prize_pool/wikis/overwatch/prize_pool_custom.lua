2f0e52cf5e0625248f54dda2bbf99a6495c83cb0
Cameron <58013431+FenrirWulf@users.noreply.github.com>
2022-09-13 14:40:26 -0700

    OW Custom PrizePool (#1842)
    * OW Custom PrizePool
    
    * Updating variables
    
    * Removing unused code
    
    * Removing code thats unused in the template

---
+++ components/prize_pool/wikis/overwatch/prize_pool_custom.lua
@@ -0,0 +1,56 @@
+---
+-- @Liquipedia
+-- wiki=Overwatch
+-- page=Module:PrizePool/Custom
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Arguments = require('Module:Arguments')
+local Class = require('Module:Class')
+local Lua = require('Module:Lua')
+local String = require('Module:StringUtils')
+local Variables = require('Module:Variables')
+
+local PrizePool = Lua.import('Module:PrizePool', {requireDevIfEnabled = true})
+
+local LpdbInjector = Lua.import('Module:Lpdb/Injector', {requireDevIfEnabled = true})
+local CustomLpdbInjector = Class.new(LpdbInjector)
+
+local CustomPrizePool = {}
+
+local TIER_VALUE = {10000, 5000, 2}
+local TYPE_MODIFIER = {Online = 0.65}
+
+-- Template entry point
+function CustomPrizePool.run(frame)
+	local args = Arguments.getArgs(frame)
+	local prizePool = PrizePool(args):create()
+
+	prizePool:setLpdbInjector(CustomLpdbInjector())
+
+	return prizePool:build()
+end
+
+function CustomLpdbInjector:adjust(lpdbData, placement, opponent)
+	lpdbData.weight = CustomPrizePool.calculateWeight(
+		lpdbData.prizemoney,
+		Variables.varDefault('tournament_liquipediatier'),
+		placement.placeStart,
+		Variables.varDefault('tournament_type')
+	)
+
+	return lpdbData
+end
+
+function CustomPrizePool.calculateWeight(prizeMoney, tier, place, type)
+	if String.isEmpty(tier) then
+		return 0
+	end
+
+	local tierValue = TIER_VALUE[tier] or TIER_VALUE[tonumber(tier) or ''] or 1
+
+	return tierValue * math.max(prizeMoney, 1) * (TYPE_MODIFIER[type] or 1) / place
+end
+
+return CustomPrizePool
