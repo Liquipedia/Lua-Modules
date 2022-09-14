8fcd7a1d3573161b3231d6a676a9f2ce07429ff6
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2022-09-14 11:11:23 +0200

    Create Ordinal.lua

---
+++ standard/Ordinal.lua
@@ -0,0 +1,172 @@
+---
+-- @Liquipedia
+-- wiki=commons
+-- page=Module:Ordinal
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Arguments = require('Module:Arguments')
+local Logic = require('Module:Logic')
+local OrdinalData = mw.loadData('Module:Ordinal/Data')
+local String = require('Module:StringUtils')
+local Table = require('Module:Table')
+
+local Ordinal = {}
+
+local DEFAULT_NEGATIVE_SIGN_TEXT = 'negative'
+
+function Ordinal.written(valueInput, options)
+	local value = valueInput
+	if String.isEmpty(value) then
+		return
+	end
+
+	-- clean value input
+	value = tonumber(string.match(value, '(%d*)%W*$'))
+	if not value then
+		return 
+	end
+
+	options = options or {}
+	local signText = value >= 0 and ''
+		or (options.negativeSignText or DEFAULT_NEGATIVE_SIGN_TEXT) .. ' '
+	local concatText = options.concatWithAnd and ' and ' or ' ' --what do we want as default for this option value?
+
+	value = tostring(math.abs(value))
+	local decimals = value:match('%.(%d+)')
+	if decimals then
+		--do we want support for this???
+		error('Currently only integers are supported in Module:Ordinal.written')
+	end
+
+	-- split the value into digit groups of (max) length 3 each
+	local digitGroups = {}
+	while #value > 3 do
+		local digitGroup = string.sub(value, -3, -1)
+		table.insert(digitGroups, tonumber(digitGroup))
+		value = string.sub(value, 1, -4)
+	end
+	table.insert(digitGroups, tonumber(value))
+
+	if #digitGroups > Table.size(OrdinalData.groups) then
+		error(valueInput .. ' is too large for Module:Ordinal.written')
+	end
+
+	local display = ''
+	local ordinalApplied = false
+	for groupIndex, digitGroup in ipairs(digitGroups) do
+		local groupPostfix = ''
+		if groupIndex ~= 1 and digitGroup ~= 0 then
+			groupPostfix = ' ' .. OrdinalData.groups[groupIndex - 1] .. (applyOrdinal and 'th' or '') .. ' '
+			ordinalApplied = true
+		end
+
+		local text, ordinalApplied = Ordinal._writtenBelowThousand(digitGroup, not ordinalApplied, concatText)
+		display = text .. groupPostfix .. display
+	end
+
+	display = display:gsub(' $', '')
+
+	if String.isEmpty(display) then
+		display = OrdinalData.zeroOrdinal
+	elseif options.plural then -- to my knowledge zeroth has no plural
+		display = display .. 's'
+	end
+
+	if options.capitalize then
+		display = display:gsub('^%l', string.upper)
+	end
+
+	if options.hyphenate then
+		display = display:gsub('%s', '-')
+	end
+
+	return signText .. display
+end
+
+function Ordinal._writtenBelowThousand(value, applyOrdinal, concatText)
+	if value == 0 then
+		return '', not applyOrdinal
+	end
+
+	if value < 100 then
+		return Ordinal._writtenBelowHundred(value, applyOrdinal), true
+	end
+
+	local display = OrdinalData.position[math.floor(value / 100)] .. ' hundred'
+	if value % 100 == 0 then
+		return display .. (applyOrdinal and 'th' or ''), true
+	end
+
+	display = display .. concatText .. Ordinal._writtenBelowHundred(value % 100, applyOrdinal)
+
+	return display, true
+end
+
+function Ordinal._writtenBelowHundred(value, applyOrdinal)
+	local lookUp = {
+		ones = applyOrdinal and OrdinalData.positionOrdinal or OrdinalData.position,
+		tens = applyOrdinal and OrdinalData.positionTensOrdinal or OrdinalData.positionTens,
+	}
+
+	if value < 20 then
+		return lookUp.ones[value]
+	elseif value % 10 == 0 then
+		return lookUp.tens[value / 10]
+	else
+		return lookUp.tens[math.floor(value / 10)] .. '-' .. lookUp.ones[value % 10]
+	end
+end
+
+function Ordinal.suffix(value, options)
+	if String.isEmpty(value) then
+		return 
+	end
+
+	-- clean value input
+	value = tonumber(string.match(value, '(%d*)%W*$'))
+	if not value then
+		return 
+	end
+
+	options = options or {}
+	local residual10 = math.abs(value) % 10
+	local residual100 = math.abs(value) % 100
+
+	local suffix
+	if residual10 == 1 and residual100 ~= 11 then
+		suffix = 'st'
+	elseif residual10 == 1 and residual100 ~= 12 then
+		suffix = options.abbreviateSuffixD and 'd' or 'nd'
+	elseif residual10 == 1 and residual100 ~= 13 then
+		suffix = options.abbreviateSuffixD and 'd' or 'rd'
+	else
+		suffix = 'th'
+	end
+
+	if options.superScript then
+		return '<sup>' .. suffix .. '</sup>'
+	end
+
+	return suffix
+end
+
+--Legacy entry points
+function Ordinal.ordinal(frame)
+	local args = Arguments.getArgs(frame)
+
+	return Ordinal.suffix(args[1], {
+		abbreviateSuffixD = args[2] == 'd',
+		superScript = Logic.readBool(args.sup),
+	}) or ''
+end
+
+function Ordinal._ordinal(value, abbreviateSuffixD, superScript)
+	return Ordinal.suffix(args[1], {
+		abbreviateSuffixD = abbreviateSuffixD,
+		superScript = superScript,
+	}) or ''
+end
+
+return Ordinal
