b4f7a342c1d7801fadfedfa1655020c0dfb2669f
Rikard Blixt <rikardblixt@gmail.com>
2022-09-22 14:10:54 +0200

    League: Cleanup imports, enable dev if dev-flag is on

--- components/infobox/wikis/leagueoflegends/infobox_team_custom.lua
+++ components/infobox/wikis/leagueoflegends/infobox_team_custom.lua
@@ -6,13 +6,17 @@
 -- Please see https://github.com/Liquipedia/Lua-Modules to contribute
 --
 
-local Team = require('Module:Infobox/Team')
-local Variables = require('Module:Variables')
 local Class = require('Module:Class')
+local Lua = require('Module:Lua')
 local String = require('Module:StringUtils')
-local Cell = require('Module:Infobox/Widget/Cell')
-local Injector = require('Module:Infobox/Widget/Injector')
 local Template = require('Module:Template')
+local Variables = require('Module:Variables')
+
+local Injector = Lua.import('Module:Infobox/Widget/Injector', {requireDevIfEnabled = true})
+local Team = Lua.import('Module:Infobox/Team', {requireDevIfEnabled = true})
+
+local Widgets = require('Module:Infobox/Widget/All')
+local Cell = Widgets.Cell
 
 local CustomTeam = Class.new()
 local CustomInjector = Class.new(Injector)
