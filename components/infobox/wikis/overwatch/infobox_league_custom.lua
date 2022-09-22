49ecbdcb454b27d013f6457d10046f12680bae56
Rikard Blixt <rikardblixt@gmail.com>
2022-09-22 14:19:44 +0200

    Overwatch: Cleanup imports, enable  dev if dev-flag is on

--- components/infobox/wikis/overwatch/infobox_league_custom.lua
+++ components/infobox/wikis/overwatch/infobox_league_custom.lua
@@ -6,15 +6,19 @@
 -- Please see https://github.com/Liquipedia/Lua-Modules to contribute
 --
 
-local League = require('Module:Infobox/League')
+local Class = require('Module:Class')
+local Lua = require('Module:Lua')
+local PageLink = require('Module:Page')
 local String = require('Module:StringUtils')
 local Variables = require('Module:Variables')
-local Class = require('Module:Class')
-local Injector = require('Module:Infobox/Widget/Injector')
-local Cell = require('Module:Infobox/Widget/Cell')
-local Title = require('Module:Infobox/Widget/Title')
-local Center = require('Module:Infobox/Widget/Center')
-local PageLink = require('Module:Page')
+
+local Injector = Lua.import('Module:Infobox/Widget/Injector', {requireDevIfEnabled = true})
+local League = Lua.import('Module:Infobox/League', {requireDevIfEnabled = true})
+
+local Widgets = require('Module:Infobox/Widget/All')
+local Cell = Widgets.Cell
+local Title = Widgets.Title
+local Center = Widgets.Center
 
 local _args
 local _league
