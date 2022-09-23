e9ec527c8b1c3b2cb82b8c72934773f24489cfe3
Hesketh2 <88981446+Hesketh2@users.noreply.github.com>
2022-09-23 16:57:38 +0700

    2nd update

--- components/infobox/wikis/wildrift/infobox_team_custom.lua
+++ components/infobox/wikis/wildrift/infobox_team_custom.lua
@@ -29,11 +29,6 @@ function CustomTeam.run(frame)
 	_team = team
 	_args = _team.args
 
-	-- Automatic org people
-	team.args.coach = Template.expandTemplate(frame, 'Coach of')
-	team.args.manager = Template.expandTemplate(frame, 'Manager of')
-	team.args.captain = Template.expandTemplate(frame, 'Captain of')
-
 	team.createWidgetInjector = CustomTeam.createWidgetInjector
 	team.createBottomContent = CustomTeam.createBottomContent
 	team.addToLpdb = CustomTeam.addToLpdb
@@ -53,6 +48,10 @@ function CustomTeam:createBottomContent()
 	) .. Template.expandTemplate(
 		mw.getCurrentFrame(),
 		'Upcoming and ongoing tournaments of',
+		{team = _team.name}
+	).. Template.expandTemplate(
+		mw.getCurrentFrame(),
+		'Placement summary',
 		{team = _team.name}
 	)
 end
@@ -76,23 +75,11 @@ function CustomTeam:addToLpdb(lpdbData, args)
 
 	lpdbData.region = Variables.varDefault('region', '')
 
-	if String.isNotEmpty(args.league) then
-		lpdbData.extradata.competesin = string.upper(args.league)
-	end
-
-	lpdbData.coach = Variables.varDefault('coachid') or args.coach or args.coaches
-	lpdbData.manager = Variables.varDefault('managerid') or args.manager
-
 	return lpdbData
 end
 
 function CustomTeam:getWikiCategories(args)
 	local categories = {}
-
-	if String.isNotEmpty(args.league) then
-		local division = string.upper(args.league)
-		table.insert(categories, division .. ' Teams')
-	end
 
 	return categories
 end
