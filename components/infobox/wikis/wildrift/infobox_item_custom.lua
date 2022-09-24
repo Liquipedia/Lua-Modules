94e680ae78ea577b8a6b93dab0a6af27932f0947
Rikard Blixt <rikardblixt@gmail.com>
2022-09-24 11:22:54 +0200

    WR: Cleanup Imports, enable dev if dev-flag is on (#1936)

--- components/infobox/wikis/wildrift/infobox_item_custom.lua
+++ components/infobox/wikis/wildrift/infobox_item_custom.lua
@@ -6,17 +6,20 @@
 -- Please see https://github.com/Liquipedia/Lua-Modules to contribute
 --
 
-local Item = require('Module:Infobox/Item')
-local String = require('Module:StringUtils')
-local Namespace = require('Module:Namespace')
-local Template = require('Module:Template')
 local Class = require('Module:Class')
+local Lua = require('Module:Lua')
+local Namespace = require('Module:Namespace')
+local String = require('Module:StringUtils')
+local Template = require('Module:Template')
 
-local Injector = require('Module:Infobox/Widget/Injector')
-local Cell = require('Module:Infobox/Widget/Cell')
-local Center = require('Module:Infobox/Widget/Center')
-local Title = require('Module:Infobox/Widget/Title')
-local Breakdown = require('Module:Infobox/Widget/Breakdown')
+local Injector = Lua.import('Module:Infobox/Widget/Injector', {requireDevIfEnabled = true})
+local Item = Lua.import('Module:Infobox/Item', {requireDevIfEnabled = true})
+
+local Widgets = require('Module:Infobox/Widget/All')
+local Breakdown = Widgets.Breakdown
+local Cell = Widgets.Cell
+local Center = Widgets.Center
+local Title = Widgets.Title
 
 local CustomItem = Class.new()
 
