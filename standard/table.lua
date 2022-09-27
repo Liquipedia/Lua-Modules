3a6e8a71099d0ff04f9532c61c3bcf500a9ee234
Rikard Blixt <rikardblixt@gmail.com>
2022-09-27 14:14:02 +0200

    Update standard/table.lua
    Co-authored-by: mbergen <mail@mbergen.de>

--- standard/table.lua
+++ standard/table.lua
@@ -70,7 +70,7 @@ function Table.isEmpty(tbl)
 	if not tbl then
 		return true
 	end
-	return not next(tbl) and true or false
+	return not next(tbl)
 end
 
 ---Return true if table is neither empty nor nil
