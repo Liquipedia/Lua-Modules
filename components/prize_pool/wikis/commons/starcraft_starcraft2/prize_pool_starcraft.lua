b942d0638f37068f5fb30d5cf6022b4a0250b36c
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2022-09-28 10:53:56 +0200

    Update prize_pool_starcraft.lua

--- components/prize_pool/wikis/commons/starcraft_starcraft2/prize_pool_starcraft.lua
+++ components/prize_pool/wikis/commons/starcraft_starcraft2/prize_pool_starcraft.lua
@@ -162,7 +162,9 @@ function CustomLpdbInjector:adjust(lpdbData, placement, opponent)
 
 	lpdbData.extradata = Table.mergeInto(lpdbData.extradata, extradata)
 
+	-- remove the following line once the consumers have been updated
 	lpdbData.mode = CustomPrizePool._getMode(opponent.opponenttype, opponent.opponentData)
+
 	lpdbData.tournament = _tournament_name
 	lpdbData.series = _series
 
