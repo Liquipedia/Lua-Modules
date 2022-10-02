f771995fd480c342cdaec2ad2e46f4bf66e316f7
Hesketh2 <88981446+Hesketh2@users.noreply.github.com>
2022-10-03 02:38:12 +0700

    1st
    Line too long?

--- components/match2/wikis/match_group_input_custom.lua
+++ components/match2/wikis/match_group_input_custom.lua
@@ -586,7 +586,8 @@ function mapFunctions.getParticipants(map, opponents)
 
 		for _, weapon, pickIndex in Table.iter.pairsByPrefix(map, 't' .. opponentIndex .. 'w') do
 			local participantsKey = opponentIndex .. '_' .. pickIndex
-			participants[participantsKey] = Table.merge(participants[participantsKey] or {}, {weapon = mapFunctions._cleanWeaponName(weapon)})
+			participants[participantsKey] = Table.merge(participants[participantsKey] 
+				or {}, {weapon = mapFunctions._cleanWeaponName(weapon)})
 			if maximumPickIndex < pickIndex then
 				maximumPickIndex = pickIndex
 			end
