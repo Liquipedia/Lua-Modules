7e9d2d20728f0779214d524712d0cdfcb96f24bd
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2022-10-04 11:02:17 +0200

    Set `allGroupsUseWdl` as `true` for R6, RL and lol (#2005)
    * Set allGroupsUseWdl as true for R6
    
    * rl
    
    * lol

--- components/prize_pool/wikis/leagueoflegends/prize_pool_custom.lua
+++ components/prize_pool/wikis/leagueoflegends/prize_pool_custom.lua
@@ -24,6 +24,7 @@ local TIER_VALUE = {8, 4, 2}
 -- Template entry point
 function CustomPrizePool.run(frame)
 	local args = Arguments.getArgs(frame)
+	args.allGroupsUseWdl = true
 	local prizePool = PrizePool(args):create()
 
 	prizePool:setLpdbInjector(CustomLpdbInjector())
