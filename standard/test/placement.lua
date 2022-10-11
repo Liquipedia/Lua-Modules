53f83ff6d9a6ae21ec5f385c82fc4dd80b67b980
Rikard Blixt <rikardblixt@gmail.com>
2022-10-11 18:37:56 +0200

    Corrected the testcases

--- standard/test/placement.lua
+++ standard/test/placement.lua
@@ -13,11 +13,13 @@ local Placement = Lua.import('Module:Placement', {requireDevIfEnabled = true})
 
 local suite = ScribuntoUnit:new()
 
+local NON_BREAKING_SPACE = '&nbsp;'
 local ZERO_WIDTH_SPACE = '&#8203;'
 local EN_DASH = '–'
 
 function suite:testRangeLabel()
-	self:assertEquals(('1st' .. ZERO_WIDTH_SPACE .. EN_DASH .. ZERO_WIDTH_SPACE .. '2nd'), Placement.RangeLabel{1, 2})
+	local DASH = ZERO_WIDTH_SPACE .. EN_DASH .. ZERO_WIDTH_SPACE
+	self:assertEquals(('1st' .. DASH .. '2nd'), Placement.RangeLabel{1, 2})
 	self:assertEquals('1st', Placement.RangeLabel{1, 1})
 end
 
@@ -28,16 +30,17 @@ function suite:testGetBgClass()
 end
 
 function suite:testGet()
+	local DASH = NON_BREAKING_SPACE .. '-' .. NON_BREAKING_SPACE
 	self:assertEquals(
-		'class="text-center placement-1" data-sort-value="1"|<b>1</b>',
+		'class="text-center placement-1" data-sort-value="1"|<b class="placement-text">1st</b>',
 		Placement.get('1')
 	)
 	self:assertEquals(
-		'class="text-center placement-draw" data-sort-value="3-4"|<b>3-4</b>',
+		'class="text-center placement-draw" data-sort-value="3-4"|<b class="placement-text">3rd' .. DASH .. '4th</b>',
 		Placement.get('3-4')
 	)
 	self:assertEquals(
-		'class="text-center placement-dnp" data-sort-value="1032"|<b class="placement-text">hi</b>',
+		'class="text-center placement-dnp" data-sort-value="1032"|<b>hi</b>',
 		Placement.get('dnp', 'hi')
 	)
 end
