aa9553b8782d5d44239b0f8a03a32aa724c51bd8
Rikard Blixt <rikardblixt@gmail.com>
2022-10-12 14:33:16 +0200

    Create Uni Tests for Module:Timezone

---
+++ standard/test/timezone_test.lua
@@ -0,0 +1,81 @@
+---
+-- @Liquipedia
+-- wiki=commons
+-- page=Module:Timezone/testcases
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Lua = require('Module:Lua')
+local ScribuntoUnit = require('Module:ScribuntoUnit')
+
+local Timezone = Lua.import('Module:Timezone', {requireDevIfEnabled = true})
+
+local suite = ScribuntoUnit:new()
+
+function suite:testGetString()
+	self:assertEquals(
+		'<abbr data-tz="+05:30" title="Indian Standard Time (UTC+5:30)">IST</abbr>',
+		Timezone.getTimezoneString('IST')
+	)
+	self:assertEquals(
+		'<abbr data-tz="+02:00" title="Central European Summer Time (UTC+2)">CEST</abbr>',
+		Timezone.getTimezoneString('CEST')
+	)
+	self:assertEquals(
+		nil,
+		Timezone.getTimezoneString('DUMMY')
+	)
+end
+
+function suite:testGetOffset()
+	self:assertEquals(
+		5 * 60 * 60 + 30 * 60,
+		Timezone.getOffset('IST')
+	)
+	self:assertEquals(
+		2 * 60 * 60,
+		Timezone.getOffset('CEST')
+	)
+	self:assertEquals(
+		nil,
+		Timezone.getOffset('DUMMY')
+	)
+end
+
+function suite:getTimezoneData()
+	self:assertEquals(
+		{
+			abbr = 'IST',
+			name = 'Indian Standard Time',
+			offset = {5, 30},
+		},
+		Timezone.getOffset('IST')
+	)
+	self:assertEquals(
+		{
+			abbr = 'CEST',
+			name = 'Central European Summer Time',
+			offset = {2, 0},
+		},
+		Timezone.getOffset('CEST')
+	)
+	self:assertEquals(
+		{
+			abbr = 'CEST',
+			name = 'Central European Summer Time',
+			offset = {2, 0},
+		},
+		Timezone.getOffset('cest')
+	)
+	self:assertEquals(
+		nil,
+		Timezone.getOffset('DUMMY')
+	)
+	self:assertEquals(
+		nil,
+		nil
+	)
+end
+
+return suite
