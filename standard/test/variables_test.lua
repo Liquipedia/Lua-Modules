aad1922f9f4716b0241862aa2a18b6b4e04a6c33
Rikard Blixt <rikardblixt@gmail.com>
2022-10-12 20:58:02 +0200

    Create Unit Tests for Module:Variables (#2070)
    * Create Unit Tests for Variables
    
    * Fix typo in expected
    
    * Fix magic comment link

---
+++ standard/test/variables_test.lua
@@ -0,0 +1,43 @@
+---
+-- @Liquipedia
+-- wiki=commons
+-- page=Module:Variables/testcases
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Lua = require('Module:Lua')
+local ScribuntoUnit = require('Module:ScribuntoUnit')
+
+local Variables = Lua.import('Module:Variables', {requireDevIfEnabled = true})
+
+local suite = ScribuntoUnit:new()
+
+function suite:testVarDefine()
+	self:assertEquals('', Variables.varDefine('test', 'foo'))
+	self:assertEquals('foo', Variables.varDefault('test'))
+
+	self:assertEquals('bar', Variables.varDefineEcho('test', 'bar'))
+	self:assertEquals('bar', Variables.varDefault('test'))
+
+	self:assertEquals('', Variables.varDefine('test', 3))
+	self:assertEquals('3', Variables.varDefault('test'))
+
+	self:assertEquals('', Variables.varDefine('test'))
+	self:assertEquals(nil, Variables.varDefault('test'))
+end
+
+function suite:testVarDefault()
+	Variables.varDefine('test', 'foo')
+	self:assertEquals('foo', Variables.varDefault('test'))
+	self:assertEquals(nil, Variables.varDefault('bar'))
+	self:assertEquals('baz', Variables.varDefault('bar', 'baz'))
+end
+
+function suite:testVarDefaultMulti()
+	Variables.varDefine('baz', 'hello world')
+	self:assertEquals('hello world', Variables.varDefaultMulti('foo', 'bar', 'baz'))
+	self:assertEquals('banana', Variables.varDefaultMulti('foo', 'bar', 'banana'))
+end
+
+return suite
