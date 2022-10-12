c3c600100ab03c3e3ffb3f6251c8564e9a973ec1
Rikard Blixt <rikardblixt@gmail.com>
2022-10-12 10:45:47 +0200

    Convert spaces to tabs in Plugin

--- plugins/sumneko_plugin.lua
+++ plugins/sumneko_plugin.lua
@@ -10,47 +10,47 @@ importFunctions.functions = {'require', 'mw%.loadData', 'Lua%.import', 'Lua%.req
 importFunctions.prefixModules = {table = 'standard.', math = 'standard.', string = 'standard.'}
 
 function importFunctions._row(name)
-    local normModuleName =
-        name
-            :gsub('Module:', '') -- Remove starting Module:
-            :gsub('^%u', string.lower) -- Lower case first letter
-            :gsub('%u', '_%0') -- Prefix uppercase letters with an underscore
-            :gsub('/', '_') -- Change slash to underscore
-            :gsub('__', '_') -- Never have two underscores in a row
-            :lower() -- Lowercase everything
+	local normModuleName =
+		name
+			:gsub('Module:', '') -- Remove starting Module:
+			:gsub('^%u', string.lower) -- Lower case first letter
+			:gsub('%u', '_%0') -- Prefix uppercase letters with an underscore
+			:gsub('/', '_') -- Change slash to underscore
+			:gsub('__', '_') -- Never have two underscores in a row
+			:lower() -- Lowercase everything
 
-    if importFunctions.prefixModules[normModuleName] then
-        normModuleName = importFunctions.prefixModules[normModuleName] .. normModuleName
-    end
+	if importFunctions.prefixModules[normModuleName] then
+		normModuleName = importFunctions.prefixModules[normModuleName] .. normModuleName
+	end
 
-    return ' ---@module \'' .. normModuleName ..'\''
+	return ' ---@module \'' .. normModuleName ..'\''
 end
 
 function importFunctions.annotate(text, funcName, diffs)
-    for module, positionEndOfRow in text:gmatch(funcName .. '%s*%(?%s*[\'"](.-)[\'"]%s*%)?.-()\r?\n') do
-        table.insert(diffs,
-            {start = positionEndOfRow, finish = positionEndOfRow - 1, text = importFunctions._row(module)}
-        )
-    end
+	for module, positionEndOfRow in text:gmatch(funcName .. '%s*%(?%s*[\'"](.-)[\'"]%s*%)?.-()\r?\n') do
+		table.insert(diffs,
+			{start = positionEndOfRow, finish = positionEndOfRow - 1, text = importFunctions._row(module)}
+		)
+	end
 end
 
 function liquipedia.annotate(text, diffs)
-    for _, funcName in pairs(importFunctions.functions) do
-        importFunctions.annotate(text, funcName, diffs)
-    end
+	for _, funcName in pairs(importFunctions.functions) do
+		importFunctions.annotate(text, funcName, diffs)
+	end
 end
 
 -- luacheck: push ignore
 -- setting non-standard global variable 'OnSetText' (but it's mandatory)
 function OnSetText(uri, text)
 -- luacheck: pop ignore
-    if text:sub(1, 3) ~= '---' then
-        return nil
-    end
+	if text:sub(1, 3) ~= '---' then
+		return nil
+	end
 
-    local diffs = {}
+	local diffs = {}
 
-    liquipedia.annotate(text, diffs)
+	liquipedia.annotate(text, diffs)
 
-    return diffs
+	return diffs
 end
