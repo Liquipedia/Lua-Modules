9b4b8f835298bc0862ad8f830fe17ff31ace4b97
Rikard Blixt <rikardblixt@gmail.com>
2022-10-12 14:02:23 +0200

    Add annotations to StringUtils

--- standard/string_utils.lua
+++ standard/string_utils.lua
@@ -8,14 +8,23 @@
 
 local String = {}
 
+---@param str string
+---@param start string
+---@return boolean
 function String.startsWith(str, start)
 	return str:sub(1, #start) == start --str:find('^' .. start) ~= nil
 end
 
+---@param str string
+---@param ending string
+---@return boolean
 function String.endsWith(str, ending)
 	return ending == '' or str:sub(-#ending) == ending
 end
 
+---@param inputstr string?
+---@param sep string?
+---@return table
 function String.split(inputstr, sep)
 	if inputstr ~= nil then
 		if sep == nil then
@@ -34,36 +43,54 @@ function String.split(inputstr, sep)
 	end
 end
 
--- need to escape .()[]+-% with % for 'match'
+--- need to escape .()[]+-% with % for 'match'
+---@param str string
+---@param match string
+---@return boolean
 function String.contains(str, match)
 	return string.find(str, match) ~= nil
 end
 
+---@param str string
+---@return string
 function String.trim(str)
 	return (str:gsub('^%s*(.-)%s*$', '%1'))
 end
 
+---@param str string?
+---@return string?
 function String.nilIfEmpty(str)
 	return str ~= '' and str or nil
 end
 
+---@param str string?
+---@return boolean
 function String.isEmpty(str)
 	return str == nil or str == ''
 end
 
+
+---@param str string?
+---@return boolean
 function String.isNotEmpty(str)
 	return str ~= nil and str ~= ''
 end
 
 -- index counts up from 0
+---@deprecated
 function String.explode(str, delimiter, index)
 	return String.split(str, delimiter)[index + 1] or ''
 end
 
---transforms a wiki code list
----->> * text
----->> * text
---into a html list (with ul/li tags)
+---transforms a wiki code list:
+---
+--- * text
+--- * text
+---
+---into a html list (with ul/li tags)
+---@param str string?
+---@param delimiter string?
+---@return string
 function String.convertWikiListToHtmlList(str, delimiter)
 	if String.isEmpty(str) then
 		return ''
@@ -81,12 +108,15 @@ function String.convertWikiListToHtmlList(str, delimiter)
 	return tostring(list)
 end
 
--- Create a string with string interpolation
---
--- String.interpolation('I\'m ${age} years old', {age = 40})
--- Returns `I'm 40 years old`
---
--- Inspiration: http://lua-users.org/wiki/StringInterpolation
+--- Create a string with string interpolation
+---
+--- String.interpolation('I\'m ${age} years old', {age = 40})
+--- Returns `I'm 40 years old`
+---
+--- Inspiration: http://lua-users.org/wiki/StringInterpolation
+---@param s string
+---@param tbl table
+---@return string
 function String.interpolate(s, tbl)
 	return (
 		s:gsub('($%b{})',
