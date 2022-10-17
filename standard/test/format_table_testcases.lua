ab1e49eedbeebdc987a78127b4112cc6a719c6e8
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2022-10-17 17:09:05 +0200

    Create format_table_testcases.lua

---
+++ standard/test/format_table_testcases.lua
@@ -0,0 +1,49 @@
+---
+-- @Liquipedia
+-- wiki=commons
+-- page=Module:Format/Table/testcases
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Lua = require('Module:Lua')
+local ScribuntoUnit = require('Module:ScribuntoUnit')
+
+local TableFormatter = Lua.import('Module:Format/Table', {requireDevIfEnabled = true})
+
+local suite = ScribuntoUnit:new()
+
+function suite:testClass()
+	self:assertEquals(
+		'<pre class="selectall">{}</pre>',
+		tostring(TableFormatter.toLuaCode{})
+	)
+	self:assertEquals(
+		'<pre class="selectall">{'
+			.. '\n\t5,'
+			.. '\n\t10,'
+			.. '\n\t[10] = 100,'
+			.. '\n\t[\'a\'] = \'a\','
+			.. '\n\t[\'a\\\'b\'] = \'te\\\'st\','
+			.. '\n\t[\'b\'] = {'
+			.. '\n\t\t[\'b\'] = \'b\','
+			.. '\n\t},'
+			.. '\n\t[\'c\'] = 1,'
+			.. '\n}</pre>',
+		tostring(TableFormatter.toLuaCode{a = 'a', b = {b = 'b'}, c = 1, 5, 10, [10] = 100, ['a\'b'] = 'te\'st'})
+	)
+	self:assertEquals(
+		'<pre class="selectall">{'
+			.. '\n\t[10] = 100,'
+			.. '\n\t[\'a\'] = \'a\','
+			.. '\n\t[\'a\\\'b\'] = \'te\\\'st\','
+			.. '\n\t[\'b\'] = {'
+			.. '\n\t\t[\'b\'] = \'b\','
+			.. '\n\t},'
+			.. '\n\t[\'c\'] = 1,'
+			.. '\n}</pre>',
+		tostring(TableFormatter.toLuaCode{a = 'a', b = {b = 'b'}, c = 1, [10] = 100, ['a\'b'] = 'te\'st'})
+	)
+end
+
+return suite
