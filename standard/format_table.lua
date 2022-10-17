193f462b4a945a7afa04bd48d07852f1af1fe0d8
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2022-10-17 18:28:28 +0200

    Add `Module:Format/Table` (#2085)
    * Add `Module:Format/Table`
    
    * kick test cases
    
    * kick unused local
    
    * sort by keys (`Table.iter.spairs` instead of `pairs`)
    
    * add comment
    
    * file name ... .lua
    
    * make order function local
    
    * rename var to not shadow
    
    * Update format_table.lua
    
    * Create format_table_testcases.lua
    
    * extend test case
    
    * escape values with nowiki for links
    
    * nowiki in test cases, kick some redundant
    
    * Update format_table_testcases.lua

---
+++ standard/format_table.lua
@@ -0,0 +1,91 @@
+---
+-- @Liquipedia
+-- wiki=commons
+-- page=Module:Format/Table
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Logic = require('Module:Logic')
+local Table = require('Module:Table')
+
+local TableFormatter = {}
+
+function TableFormatter.toLuaCode(inputTable)
+	if type(inputTable) ~= 'table' then
+		error('TableFormatter.toLuaCode needs a table as input')
+	end
+
+	if Table.isEmpty(inputTable) then
+		return mw.html.create('pre')
+			:addClass('selectall')
+			:wikitext('{}')
+	end
+
+	local function escapeSingleQuote(str)
+		return str:gsub('\'', '\\\'')
+	end
+
+	local function displayValue(value)
+		if type(value) == 'number' then
+			return value
+		else
+			return '\'' .. mw.text.nowiki(escapeSingleQuote(value)) .. '\''
+		end
+	end
+
+	local function order(_, key1, key2)
+		-- cases due to possibly having numbers and strings as keys
+		if Logic.isNumeric(key1) and Logic.isNumeric(key2) then
+			return key1 < key2
+		elseif Logic.isNumeric(key1) then
+			return true
+		elseif Logic.isNumeric(key2) then
+			return false
+		else -- 2 strings
+			return key1 < key2
+		end
+	end
+
+	local function toLuaString(tbl, indentNumber)
+		local luaString = '{'
+
+		for index, obj in ipairs(tbl) do
+			luaString = luaString .. '\n' .. string.rep('\t', indentNumber)
+			-- value display
+			if type(obj) == 'table' then
+				luaString = luaString .. toLuaString(obj, indentNumber + 1)
+			else
+				luaString = luaString .. displayValue(obj)
+			end
+			luaString = luaString .. ','
+			tbl[index] = nil
+		end
+
+		for key, obj in Table.iter.spairs(tbl, order) do
+			luaString = luaString .. '\n' .. string.rep('\t', indentNumber)
+			--key display
+			if type(key) == 'number' then
+				luaString = luaString .. '[' .. key .. '] = '
+			else
+				luaString = luaString .. '[\'' .. escapeSingleQuote(key) .. '\'] = '
+			end
+
+			-- value display
+			if type(obj) == 'table' then
+				luaString = luaString .. toLuaString(obj, indentNumber + 1)
+			else
+				luaString = luaString .. displayValue(obj)
+			end
+			luaString = luaString .. ','
+		end
+
+		return luaString .. '\n' .. string.rep('\t', indentNumber - 1) .. '}'
+	end
+
+	return mw.html.create('pre')
+		:addClass('selectall')
+		:wikitext(toLuaString(inputTable, 1))
+end
+
+return TableFormatter
---
+++ standard/test/format_table_testcases.lua
@@ -0,0 +1,54 @@
+---
+-- @Liquipedia
+-- wiki=commons
+-- page=Module:Format/Table/testcases
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Lua = require('Module:Lua')
+local ScribuntoUnit = require('Module:ScribuntoUnit')
+
+local TableFormatter = Lua.import('Module:Format/Table', {requireDevIfEnabled = true})
+
+local suite = ScribuntoUnit:new()
+
+function suite:testClass()
+	self:assertEquals(
+		'<pre class="selectall">{}</pre>',
+		tostring(TableFormatter.toLuaCode{})
+	)
+	self:assertEquals(
+		[[<pre class="selectall">{
+	5,
+	10,
+	[10] = 100,
+	['a'] = 'a',
+	['a\'b'] = ']] .. mw.text.nowiki('te\\\'st') .. [[',
+	['b'] = {
+		['b'] = 'b',
+	},
+	['c'] = 1,
+}</pre>]],
+		tostring(TableFormatter.toLuaCode{a = 'a', b = {b = 'b'}, c = 1, 5, 10, [10] = 100, ['a\'b'] = 'te\'st'})
+	)
+	self:assertEquals(
+		[[<pre class="selectall">{
+	[10] = 100,
+	['a'] = 'a',
+	['b'] = {
+		['b'] = 'b',
+	},
+	['c'] = 1,
+	['x'] = ']] .. mw.text.nowiki('[[Module:Format/Table/testcases|aa]]') .. [[',
+	['z'] = ']] .. mw.text.nowiki('https://discord.com/invite/WaQRYSa') .. [[',
+}</pre>]],
+		tostring(TableFormatter.toLuaCode{
+			a = 'a', b = {b = 'b'}, c = 1, [10] = 100,
+			x = '[[Module:Format/Table/testcases|aa]]',
+			z = 'https://discord.com/invite/WaQRYSa'
+		})
+	)
+end
+
+return suite
