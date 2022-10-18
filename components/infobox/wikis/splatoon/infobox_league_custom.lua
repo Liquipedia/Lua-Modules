39a1697f0a9b78c0962dc0f82cc40e9208443ae6
Hesketh2 <88981446+Hesketh2@users.noreply.github.com>
2022-10-18 18:17:44 +0700

    Infobox League for Splatoon (#2056)
    * Infobox League for Splatoon
    
    * 1st update (INCOMPLETE)
    
    Only includes stuff that has marked resolved
    
    * 2nd update
    
    Module:WeaponIcon/Data calls for lowercase start input, I have batch those with Uppercase start input and add few more entries in https://liquipedia.net/splatoon/index.php?title=Module%3AGameVersion&type=revision&diff=1829&oldid=564, it does now work
    
    * Unused Variable
    
    * 4th update
    
    * Whitespace
    
    * 6th update
    
    * 6th update
    
    * 7th update
    
    * 8th update

---
+++ components/infobox/wikis/splatoon/infobox_league_custom.lua
@@ -0,0 +1,90 @@
+---
+-- @Liquipedia
+-- wiki=splatoon
+-- page=Module:Infobox/League/Custom
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Class = require('Module:Class')
+local Lua = require('Module:Lua')
+local Logic = require('Module:Logic')
+local String = require('Module:StringUtils')
+local Variables = require('Module:Variables')
+
+local Injector = Lua.import('Module:Infobox/Widget/Injector', {requireDevIfEnabled = true})
+local League = Lua.import('Module:Infobox/League', {requireDevIfEnabled = true})
+
+local Widgets = require('Module:Infobox/Widget/All')
+local Cell = Widgets.Cell
+
+local CustomLeague = Class.new()
+local CustomInjector = Class.new(Injector)
+
+local _args
+local _GAME = mw.loadData('Module:GameVersion')
+
+function CustomLeague.run(frame)
+	local league = League(frame)
+	_args = league.args
+
+	league.addToLpdb = CustomLeague.addToLpdb
+	league.createWidgetInjector = CustomLeague.createWidgetInjector
+	league.defineCustomPageVariables = CustomLeague.defineCustomPageVariables
+	league.liquipediaTierHighlighted = CustomLeague.liquipediaTierHighlighted
+
+	return league:createInfobox(frame)
+end
+
+function CustomLeague:createWidgetInjector()
+	return CustomInjector()
+end
+
+function CustomInjector:addCustomCells(widgets)
+	table.insert(widgets, Cell{
+		name = 'Teams',
+		content = {_args.team_number}
+	})
+	table.insert(widgets, Cell{
+		name = 'Players',
+		content = {_args.player_number}
+	})
+
+	return widgets
+end
+
+function CustomInjector:parse(id, widgets)
+	if id == 'gamesettings' then
+		return {
+			Cell{name = 'Game version', content = {CustomLeague._getGameVersion()}},
+		}
+	end
+
+	return widgets
+end
+
+function CustomLeague:liquipediaTierHighlighted()
+	return Logic.readBool(_args.publisherpremier)
+end
+
+function CustomLeague:addToLpdb(lpdbData, args)
+	lpdbData.game = CustomLeague._getGameVersion()
+	lpdbData.participantsnumber = args.player_number or args.team_number
+	lpdbData.publishertier = Logic.readBool(_args.publisherpremier) or nil
+	lpdbData.extradata = {
+		individual = String.isNotEmpty(args.player_number) and 'true' or '',
+	}
+
+	return lpdbData
+end
+
+function CustomLeague:defineCustomPageVariables()
+	Variables.varDefine('tournament_game', CustomLeague._getGameVersion())
+	Variables.varDefine('tournament_publishertier', _args.publisherpremier)
+end
+
+function CustomLeague._getGameVersion()
+	return _GAME[string.lower(_args.game or '')]
+end
+
+return CustomLeague
