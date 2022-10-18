c80ac2f2d6dc0f2247342fa09fa0e3837b5079c2
Hesketh2 <88981446+Hesketh2@users.noreply.github.com>
2022-10-18 18:14:43 +0700

    Infobox Team for AoV (#2089)
    * Infobox Team for AoV
    
    * 1st update
    
    All Option 1 from Hjp's suggested changes were edited into this one
    
    * 2nd update
    
    Co-authored-by: hjpalpha <75081997+hjpalpha@users.noreply.github.com>
    
    * 3rd update
    
    * Update infobox_team_custom.lua
    
    * 5th update
    
    * 6th update
    
    Co-authored-by: hjpalpha <75081997+hjpalpha@users.noreply.github.com>

---
+++ components/infobox/wikis/arenaofvalor/infobox_team_custom.lua
@@ -0,0 +1,61 @@
+---
+-- @Liquipedia
+-- wiki=arenaofvalor
+-- page=Module:Infobox/Team/Custom
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Class = require('Module:Class')
+local Lua = require('Module:Lua')
+local String = require('Module:StringUtils')
+local Template = require('Module:Template')
+local Variables = require('Module:Variables')
+
+local Team = Lua.import('Module:Infobox/Team', {requireDevIfEnabled = true})
+local CustomTeam = Class.new()
+
+local _args
+
+function CustomTeam.run(frame)
+	local team = Team(frame)
+	_args = team.args
+
+	-- Automatic org people
+	team.args.coach = Template.expandTemplate(frame, 'Coach of')
+	team.args.manager = Template.expandTemplate(frame, 'Manager of')
+	team.args.captain = Template.expandTemplate(frame, 'Captain of')
+
+	team.createBottomContent = CustomTeam.createBottomContent
+	team.addToLpdb = CustomTeam.addToLpdb
+	return team:createInfobox(frame)
+end
+
+function CustomTeam:createBottomContent()
+	if not _args.disbanded then
+		return Template.expandTemplate(
+			mw.getCurrentFrame(),
+			'Upcoming and ongoing matches of'
+		) .. Template.expandTemplate(
+			mw.getCurrentFrame(),
+			'Upcoming and ongoing tournaments of'
+		) .. Template.expandTemplate(
+			mw.getCurrentFrame(),
+			'Placement summary'
+		)
+	end
+end
+
+function CustomTeam:addToLpdb(lpdbData, args)
+	if not String.isEmpty(args.teamcardimage) then
+		lpdbData.logo = args.teamcardimage
+	elseif not String.isEmpty(args.image) then
+		lpdbData.logo = args.image
+	end
+
+	lpdbData.region = Variables.varDefault('region', '')
+
+	return lpdbData
+end
+
+return CustomTeam
