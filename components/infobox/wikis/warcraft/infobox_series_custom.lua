beb5dde6505b82a86e69f18d78d8093b42365a1b
Cameron <58013431+FenrirWulf@users.noreply.github.com>
2022-10-18 09:26:37 -0700

    Removing trailing int values

--- components/infobox/wikis/warcraft/infobox_series_custom.lua
+++ components/infobox/wikis/warcraft/infobox_series_custom.lua
@@ -57,7 +57,7 @@ function CustomSeries._getSeriesPrizepools(series)
 	end
 
 	_totalSeriesPrizepool = prizemoney
-	return '$' .. Language:formatNum(Math.round(prizemoney + 0.5))
+	return '$' .. Language:formatNum(Math.round(prizemoney))
 end
 
 function CustomInjector:addCustomCells(widgets)
