ad49a2ca24aad7f673a9b4e6b599d86c0d14ed46
Rikard Blixt <rikardblixt@gmail.com>
2022-10-19 15:59:46 +0200

    Create unit tests for StandingStorage (#2104)
    * Create unit tests for StandingStorage
    
    * Remove unused import
    
    * Add test data for standings
    
    * Add a testcase that tests table + entries
    
    * EOF

---
+++ components/standings/test/standings_storage_test.lua
@@ -0,0 +1,57 @@
+---
+-- @Liquipedia
+-- wiki=commons
+-- page=Module:Standings/Storage/testcases
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Lua = require('Module:Lua')
+local ScribuntoUnit = require('Module:ScribuntoUnit')
+
+local LpdbMock = Lua.import('Module:Mock/Lpdb', {requireDevIfEnabled = true})
+local StandingsStorage = Lua.import('Module:Standings/Storage', {requireDevIfEnabled = true})
+local Table = Lua.import('Module:Table', {requireDevIfEnabled = true})
+local TournamentMock = Lua.import('Module:Infobox/Mock/League', {requireDevIfEnabled = true})
+
+local suite = ScribuntoUnit:new()
+
+function suite:testStorageTable()
+	local tournamentData = mw.loadData('Module:TestAssets/Tournaments').dummy
+	TournamentMock.setUp(tournamentData)
+	LpdbMock.setUp()
+
+	StandingsStorage.run{
+		standingsindex = 0,
+		title = '',
+		type = 'league',
+		entries = {},
+	}
+
+	local fn = function ()
+		StandingsStorage.run{
+			standingsindex = 1,
+			title = '',
+			type = 'ajksd',
+			entries = {},
+		}
+	end
+	self:assertThrows(fn)
+
+	TournamentMock.tearDown()
+	LpdbMock.tearDown()
+end
+
+function suite:testStorageFull()
+	local standingsData = Table.deepCopy(mw.loadData('Module:TestAssets/Standings'))
+	local tournamentData = mw.loadData('Module:TestAssets/Tournaments').dummy
+	TournamentMock.setUp(tournamentData)
+	LpdbMock.setUp()
+
+	StandingsStorage.run(standingsData)
+
+	TournamentMock.tearDown()
+	LpdbMock.tearDown()
+end
+
+return suite
