aeff45fe2dcfc4e9fa21f3800c1f0b0a5670e9df
Hesketh2 <88981446+Hesketh2@users.noreply.github.com>
2022-11-01 01:01:19 +0700

    PPT for PUBG Mobile

---
+++ components/prize_pool/wikis/pubgmobile/prize_pool_custom.lua
@@ -0,0 +1,47 @@
+---
+-- @Liquipedia
+-- wiki=pubgmobile
+-- page=Module:PrizePool/Custom
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Arguments = require('Module:Arguments')
+local Class = require('Module:Class')
+local Lua = require('Module:Lua')
+local Variables = require('Module:Variables')
+local Weight = require('Module:Weight')
+
+local PrizePool = Lua.import('Module:PrizePool', {requireDevIfEnabled = true})
+
+local LpdbInjector = Lua.import('Module:Lpdb/Injector', {requireDevIfEnabled = true})
+local CustomLpdbInjector = Class.new(LpdbInjector)
+
+local CustomPrizePool = {}
+
+
+-- Template entry point
+function CustomPrizePool.run(frame)
+	local args = Arguments.getArgs(frame)
+	args.localcurrency = args.localcurrency or Variables.varDefault('tournament_currency')
+	local prizePool = PrizePool(args):create()
+	prizePool:setLpdbInjector(CustomLpdbInjector())
+
+	return prizePool:build()
+end
+
+function CustomLpdbInjector:adjust(lpdbData, placement, opponent)
+	lpdbData.weight = Weight.calc(
+		lpdbData.prizemoney,
+		Variables.varDefault('tournament_liquipediatier'),
+		placement.placeStart,
+		Variables.varDefault('tournament_type'),
+		Variables.varDefault('tournament_liquipediatiertype')
+
+	)
+
+	local participantLower = mw.ustring.lower(lpdbData.participant)
+	return lpdbData
+end
+
+return CustomPrizePool
