f09d077fed8fb7e0f9a23e1b6ddbb233e5eadc76
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2022-10-31 11:42:21 +0100

    Update match_group_input.lua

--- components/match2/commons/match_group_input.lua
+++ components/match2/commons/match_group_input.lua
@@ -407,18 +407,19 @@ function MatchGroupInput.readMvp(match)
 	local players = mw.text.split(match.mvp, ',')
 
 	-- parse the players to get their information
-	local parsedPlayers = Table.mapValues(players, function(player)
+	local parsedPlayers = Table.map(players, function(playerIndex, player)
 		local link = mw.ext.TeamLiquidIntegration.resolve_redirect(mw.text.split(player, '|')[1]):gsub(' ', '_')
 		for _, opponent in Table.iter.pairsByPrefix(match, 'opponent') do
 			for _, lookUpPlayer in pairs(opponent.match2players) do
 				if link == lookUpPlayer.name then
-					return Table.merge(lookUpPlayer, {team = opponent.name, template = opponent.template})
+					return playerIndex, Table.merge(lookUpPlayer,
+						{team = opponent.name, template = opponent.template, comment = match['mvp' .. playerIndex .. 'comment']})
 				end
 			end
 		end
 	end)
 
-	return {players = parsedPlayers, points = mvppoints, freetext = match.mvpfreetext}
+	return {players = parsedPlayers, points = mvppoints}
 end
 
 return MatchGroupInput
