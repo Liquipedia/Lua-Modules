a7b6425a5cb467576008361b321bae9d0eea95b2
mbergen <mail@mbergen.de>
2022-11-01 12:46:54 +0100

    Fix wrong name in magic comment (#2141)

--- standard/tournaments_summary_table/wikis/trackmania/tournaments_summary_table_custom.lua
+++ standard/tournaments_summary_table/wikis/trackmania/tournaments_summary_table_custom.lua
@@ -1,7 +1,7 @@
 ---
 -- @Liquipedia
 -- wiki=trackmania
--- page=Module:CustomTournamentsSummaryTable/Custom
+-- page=Module:TournamentsSummaryTable/Custom
 --
 -- Please see https://github.com/Liquipedia/Lua-Modules to contribute
 --
