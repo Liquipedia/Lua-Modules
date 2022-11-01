15cf251b6b4a9a037006d2da9954c519cae1e046
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2022-11-01 14:18:47 +0100

    Add `Module:OpponentLibraries`

---
+++ components/opponent/commons/opponent_libraries.lua
@@ -0,0 +1,15 @@
+---
+-- @Liquipedia
+-- wiki=commons
+-- page=Module:OpponentLibraries 
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Info = require('Module:Info')
+local Lua = require('Module:Lua')
+
+local Opponent = Lua.import('Module:'.. Info.opponentLibrary or 'Opponent', {requireDevIfEnabled = true})
+local OpponentDisplay = Lua.import('Module:'.. Info.opponentDisplayLibrary or 'OpponentDisplay', {requireDevIfEnabled = true})
+
+return Opponent, OpponentDisplay
