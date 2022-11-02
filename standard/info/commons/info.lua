c1001214611f50bae3d9b626e316aadfd63a2832
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2022-11-02 09:35:35 +0100

    Add Module:Info on commons

---
+++ standard/info/commons/info.lua
@@ -0,0 +1,16 @@
+---
+-- @Liquipedia
+-- wiki=commons
+-- page=Module:Info
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+return {
+	wikiName = 'commons',
+	name = 'Commons',
+	defaultTeamLogo = 'Liquipedia_logo.png',
+	defaultTeamLogoDark = 'Liquipedia_logo.png',
+	opponentLibrary = 'Opponent',
+	opponentDisplayLibrary = 'OpponentDisplay',
+}
