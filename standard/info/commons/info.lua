13d4d29350a980ba772c49690ca2479a819b41a7
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2022-11-02 09:45:36 +0100

    Add `Module:Info` on commons (#2152)
    * Add Module:Info on commons
    
    * Update info.lua

---
+++ standard/info/commons/info.lua
@@ -0,0 +1,14 @@
+---
+-- @Liquipedia
+-- wiki=commons
+-- page=Module:Info
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+return {
+	wikiName = 'commons',
+	name = 'Commons',
+	defaultTeamLogo = 'Liquipedia_logo.png',
+	defaultTeamLogoDark = 'Liquipedia_logo.png',
+}
