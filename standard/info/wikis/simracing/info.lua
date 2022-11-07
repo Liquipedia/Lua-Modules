4b35eba0e073108ec42be6f8c8ca85e10f6d2e51
Jasper Jacobs <97167868+Kanopedia@users.noreply.github.com>
2022-11-07 10:23:59 +0100

    Set up Sim Racing Info Module

---
+++ standard/info/wikis/simracing/info.lua
@@ -0,0 +1,15 @@
+---
+-- @Liquipedia
+-- wiki=simracing
+-- page=Module:Info
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+return {
+	startYear = 2019,
+	wikiName = 'simracing',
+	name = 'Sim Racing',
+	defaultTeamLogo = 'Sim_Racing_default_lightmode.png',
+	defaultTeamLogoDark = 'Sim_Racing_default_darkmode.png',
+}
