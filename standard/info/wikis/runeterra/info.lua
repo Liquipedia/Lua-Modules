15eaf1608778914d83c68795c8ff5fddcd7a6e1b
Jasper Jacobs <97167868+Kanopedia@users.noreply.github.com>
2022-11-07 16:19:30 +0100

    Set up Runeterra Info Module (#2168)

---
+++ standard/info/wikis/runeterra/info.lua
@@ -0,0 +1,15 @@
+---
+-- @Liquipedia
+-- wiki=runeterra
+-- page=Module:Info
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+return {
+	startYear = 2020,
+	wikiName = 'runeterra',
+	name = 'Legends of Runeterra',
+	defaultTeamLogo = 'Legends_of_Runeterra_logo.png',
+	defaultTeamLogoDark = 'Legends_of_Runeterra_logo.png',
+}
