efd47c2793fcd665a187aa7e5495f1ce33f24603
Jasper Jacobs <97167868+Kanopedia@users.noreply.github.com>
2022-11-07 16:20:07 +0100

    Set up Hearthstone Info Module (#2169)

---
+++ standard/info/wikis/hearthstone/info.lua
@@ -0,0 +1,15 @@
+---
+-- @Liquipedia
+-- wiki=hearthstone
+-- page=Module:Info
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+return {
+	startYear = 2014,
+	wikiName = 'hearthstone',
+	name = 'Hearthstone',
+	defaultTeamLogo = 'Hearthstone_logo.png',
+	defaultTeamLogoDark = 'Hearthstone_logo.png',
+}
