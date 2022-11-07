417f3d6877ffda85dd9ab1f04155e942e3e7cae3
Jasper Jacobs <97167868+Kanopedia@users.noreply.github.com>
2022-11-07 10:10:51 +0100

    Set up Underlords Info Module

---
+++ standard/info/wikis/underlords/info.lua
@@ -0,0 +1,15 @@
+---
+-- @Liquipedia
+-- wiki=underlords
+-- page=Module:Info
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+return {
+	startYear = 2019,
+	wikiName = 'underlords',
+	name = 'Dota Underlords',
+	defaultTeamLogo = 'Dota_Underlords_lightmode.png',
+	defaultTeamLogoDark = 'Dota_Underlords_darkmode.png',
+}
