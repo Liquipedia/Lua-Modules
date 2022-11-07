02ac64cd81dfbe36ac03e71f01323c18df79c59b
Jasper Jacobs <97167868+Kanopedia@users.noreply.github.com>
2022-11-07 16:16:38 +0100

    Set up Battlerite Info Module (#2165)

---
+++ standard/info/wikis/battlerite/info.lua
@@ -0,0 +1,15 @@
+---
+-- @Liquipedia
+-- wiki=battlerite
+-- page=Module:Info
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+return {
+	startYear = 2016,
+	wikiName = 'battlerite',
+	name = 'Battlerite',
+	defaultTeamLogo = 'Battlerite_logo.png',
+	defaultTeamLogoDark = 'Battlerite_logo.png',
+}
