af11beb02303c00bf8d7691ea81c4f48e661a43c
Jasper Jacobs <97167868+Kanopedia@users.noreply.github.com>
2022-11-07 16:17:44 +0100

    Set up Auto Chess Info Module (#2166)

---
+++ standard/info/wikis/autochess/info.lua
@@ -0,0 +1,15 @@
+---
+-- @Liquipedia
+-- wiki=autochess
+-- page=Module:Info
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+return {
+	startYear = 2019,
+	wikiName = 'autochess',
+	name = 'Auto Chess',
+	defaultTeamLogo = 'Auto_Chess_lightmode.png',
+	defaultTeamLogoDark = 'Auto_Chess_darkmode.png',
+}
