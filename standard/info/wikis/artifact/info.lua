713e46c62529a6d09ad297eb557018cc728a254c
Jasper Jacobs <97167868+Kanopedia@users.noreply.github.com>
2022-11-07 16:22:20 +0100

    Set up Artifact Info Module (#2172)

---
+++ standard/info/wikis/artifact/info.lua
@@ -0,0 +1,15 @@
+---
+-- @Liquipedia
+-- wiki=artifact
+-- page=Module:Info
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+return {
+	startYear = 2018,
+	wikiName = 'artifact',
+	name = 'Artifact',
+	defaultTeamLogo = 'Artifact_allmode.png',
+	defaultTeamLogoDark = 'Artifact_allmode.png',
+}
