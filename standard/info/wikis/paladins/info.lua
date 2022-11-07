19c5b857f4704d92f4c57198e78cbb1d87787ac4
Jasper Jacobs <97167868+Kanopedia@users.noreply.github.com>
2022-11-07 09:56:08 +0100

    Set up Paladins Info Module

---
+++ standard/info/wikis/paladins/info.lua
@@ -0,0 +1,15 @@
+---
+-- @Liquipedia
+-- wiki=paladins
+-- page=Module:Info
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+return {
+	startYear = 2016,
+	wikiName = 'paladins',
+	name = 'Paladins',
+	defaultTeamLogo = 'Paladins_allmode.png',
+	defaultTeamLogoDark = 'Paladins_allmode.png',
+}
