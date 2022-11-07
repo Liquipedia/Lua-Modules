7e4e6e4833db25246852fe6b4887cc588ad24317
Jasper Jacobs <97167868+Kanopedia@users.noreply.github.com>
2022-11-07 16:21:28 +0100

    Set up Splatoon Info Module (#2171)

---
+++ standard/info/wikis/splatoon/info.lua
@@ -0,0 +1,15 @@
+---
+-- @Liquipedia
+-- wiki=splatoon
+-- page=Module:Info
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+return {
+	startYear = 2015,
+	wikiName = 'splatoon',
+	name = 'Splatoon',
+	defaultTeamLogo = 'Splatoon default allmode.png',
+	defaultTeamLogoDark = 'Splatoon default allmode.png',
+}
