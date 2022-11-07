d9004cd518ea91f727ccb2398f52aefa152443aa
Jasper Jacobs <97167868+Kanopedia@users.noreply.github.com>
2022-11-07 17:06:25 +0100

    Setup Magic info Module (#2175)
    * Setup Magic info Module
    
    * Fix indent
    
    Co-authored-by: hjpalpha <75081997+hjpalpha@users.noreply.github.com>
    
    Co-authored-by: Rikard Blixt <rikardblixt@gmail.com>
    Co-authored-by: hjpalpha <75081997+hjpalpha@users.noreply.github.com>

---
+++ standard/info/wikis/magic/info.lua
@@ -0,0 +1,15 @@
+---
+-- @Liquipedia
+-- wiki=magic
+-- page=Module:Info
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+return {
+	startYear = 1993,
+	wikiName = 'magic',
+	name = 'Magic: The Gathering',
+	defaultTeamLogo = 'Liquipedia_logo.png',
+	defaultTeamLogoDark = 'Liquipedia_logo.png',
+}
