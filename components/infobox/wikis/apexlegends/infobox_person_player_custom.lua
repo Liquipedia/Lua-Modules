5dba865ed56438bc7a4bac8a0c280f528446ca95
DMeluca <97838082+DMeluca@users.noreply.github.com>
2022-11-14 14:04:58 +0000

    Update components/infobox/wikis/apexlegends/infobox_person_player_custom.lua
    Co-authored-by: Rikard Blixt <rikardblixt@gmail.com>

--- components/infobox/wikis/apexlegends/infobox_person_player_custom.lua
+++ components/infobox/wikis/apexlegends/infobox_person_player_custom.lua
@@ -196,7 +196,7 @@ end
 
 function CustomPlayer:formatInput(args)
 	local lowercaseInput = _args.input and _args.input:lower() or nil
-	local input = _INPUTS[lowercaseInput] or _INPUTS['default']
+	local input = _INPUTS[lowercaseInput] or _INPUTS.default
 	return input
 end
 
