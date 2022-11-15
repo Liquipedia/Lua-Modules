58f935ceff8d6853a6515d2e0b1f8adace3922af
Rikard Blixt <rikardblixt@gmail.com>
2022-11-15 10:26:11 +0100

    Add runAuto to Valorant's Squad (#2190)

--- components/squad/wikis/valorant/squad_custom.lua
+++ components/squad/wikis/valorant/squad_custom.lua
@@ -10,6 +10,8 @@ local Json = require('Module:Json')
 local ReferenceCleaner = require('Module:ReferenceCleaner')
 local Squad = require('Module:Squad')
 local SquadRow = require('Module:Squad/Row')
+local SquadAutoRefs = require('Module:SquadAuto/References')
+local Table = require('Module:Table')
 local Variables = require('Module:Variables')
 
 local CustomSquad = {}
@@ -54,7 +56,7 @@ function CustomSquad.run(frame)
 
 	if squad.type == Squad.TYPE_FORMER then
 		local index = 1
-		while args['p' .. index] ~= nil or args[index] do
+		while args['p' .. index] or args[index] do
 			local player = Json.parseIfString(args['p' .. index] or args[index])
 			if player.inactivedate then
 				squad.type = Squad.TYPE_FORMER_INACTIVE
@@ -69,46 +71,91 @@ function CustomSquad.run(frame)
 	squad:header()
 
 	local index = 1
-	while args['p' .. index] ~= nil or args[index] do
+	while args['p' .. index] or args[index] do
 		local player = Json.parseIfString(args['p' .. index] or args[index])
-		local row = SquadRow(frame, player.role, {useTemplatesForSpecialTeams = true})
-		row	:id{
-				player.id,
-				flag = player.flag,
-				link = player.link,
-				captain = player.captain or player.igl,
-				role = player.role,
-				team = player.team,
-				teamrole = player.teamrole,
-			}
-			:name{name = player.name}
-			:role({role = player.role})
-			:date(player.joindate, 'Join Date:&nbsp;', 'joindate')
 
-		if squad.type == Squad.TYPE_INACTIVE or squad.type == Squad.TYPE_FORMER_INACTIVE then
-			row:date(player.inactivedate, 'Inactive Date:&nbsp;', 'inactivedate')
-		end
-		if squad.type == Squad.TYPE_FORMER or squad.type == Squad.TYPE_FORMER_INACTIVE then
-			row:date(player.leavedate, 'Leave Date:&nbsp;', 'leavedate')
-			row:newteam{
-				newteam = player.newteam,
-				newteamrole = player.newteamrole or player.newrole,
-				newteamdate = player.newteamdate,
-				leavedate = player.leavedate
-			}
-		end
-
-		squad:row(row:create(
-			Variables.varDefault('squad_name',
-			mw.title.getCurrentTitle().prefixedText) ..
-				'_' .. player.id .. '_' ..
-				ReferenceCleaner.clean(player.joindate)
-		))
+		squad:row(CustomSquad._playerRow(player, squad.type))
 
 		index = index + 1
 	end
 
 	return squad:create()
+end
+
+function CustomSquad.runAuto(playerList, squadType)
+	if Table.isEmpty(playerList) then
+		return
+	end
+
+	local squad = Squad()
+	squad:init(mw.getCurrentFrame())
+
+	squad.type = squadType
+
+	squad:title():header()
+
+	for _, player in pairs(playerList) do
+		--Get Reference(s)
+		local joinReference = SquadAutoRefs.useReferences(player.joindateRef, player.joindate)
+		local leaveReference = SquadAutoRefs.useReferences(player.leavedateRef, player.leavedate)
+
+		-- Map between formats
+		player.joindate = (player.joindatedisplay or player.joindate) .. ' ' .. joinReference
+		player.leavedate = (player.leavedatedisplay or player.leavedate) .. ' ' .. leaveReference
+		player.inactivedate = player.leavedate
+
+		player.link = player.page
+		player.role = player.thisTeam.role
+		player.team = player.thisTeam.role == 'Loan' and player.oldTeam.team
+
+		player.newteam = player.newTeam.team
+		player.newteamrole = player.newTeam.role
+		player.newteamdate = player.newTeam.date
+
+		squad:row(CustomSquad._playerRow(player, squad.type))
+	end
+
+	return squad:create()
+end
+
+function CustomSquad._playerRow(player, squadType)
+	local row = SquadRow(mw.getCurrentFrame(), player.role, {useTemplatesForSpecialTeams = true})
+
+	row:id{
+		player.id,
+		flag = player.flag,
+		link = player.link,
+		captain = player.captain or player.igl,
+		role = player.role,
+		team = player.team,
+		teamrole = player.teamrole,
+	}
+	row:name{name = player.name}
+	row:role{role = player.role}
+	row:date(player.joindate, 'Join Date:&nbsp;', 'joindate')
+
+	if squadType == Squad.TYPE_INACTIVE or squadType == Squad.TYPE_FORMER_INACTIVE then
+		row:date(player.inactivedate, 'Inactive Date:&nbsp;', 'inactivedate')
+	end
+
+	if squadType == Squad.TYPE_FORMER or squadType == Squad.TYPE_FORMER_INACTIVE then
+		row:date(player.leavedate, 'Leave Date:&nbsp;', 'leavedate')
+		row:newteam{
+			newteam = player.newteam,
+			newteamrole = player.newteamrole or player.newrole,
+			newteamdate = player.newteamdate,
+			leavedate = player.leavedate
+		}
+	end
+
+	return row:create(
+		Variables.varDefault(
+			'squad_name',
+			mw.title.getCurrentTitle().prefixedText
+		)
+		.. '_' .. player.id .. '_'
+		.. ReferenceCleaner.clean(player.joindate)
+	)
 end
 
 return CustomSquad
