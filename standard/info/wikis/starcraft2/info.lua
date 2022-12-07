887e7b07c7536dd6626e6a8b2b89dbd648b5a8e3
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2022-12-07 09:37:43 +0100

    add data for sc2

--- standard/info/wikis/starcraft2/info.lua
+++ standard/info/wikis/starcraft2/info.lua
@@ -10,8 +10,42 @@ return {
 	startYear = 2010,
 	wikiName = 'starcraft2',
 	name = 'StarCraft II',
-	defaultTeamLogo = 'StarCraft_2_Default_logo.png', ---@deprecated
-	defaultTeamLogoDark = 'StarCraft_2_Default_logo.png', ---@deprecated
 	opponentLibrary = 'Opponent/Starcraft',
 	opponentDisplayLibrary = 'OpponentDisplay/Starcraft',
+	games = {
+		wol = {
+			logo = {
+				darkMode = 'StarCraft 2 Default logo.png',
+				lightMode = 'StarCraft 2 Default logo.png',
+			},
+			defaultTeamLogo = {
+				darkMode = 'StarCraft 2 Default logo.png',
+				lightMode = 'StarCraft 2 Default logo.png',
+			},
+		},
+		hots = {
+			logo = {
+				darkMode = 'StarCraft 2 Default logo.png',
+				lightMode = 'StarCraft 2 Default logo.png',
+			},
+			defaultTeamLogo = {
+				darkMode = 'StarCraft 2 Default logo.png',
+				lightMode = 'StarCraft 2 Default logo.png',
+			},
+		},
+		lotv = {
+			logo = {
+				darkMode = 'StarCraft 2 Default logo.png',
+				lightMode = 'StarCraft 2 Default logo.png',
+			},
+			defaultTeamLogo = {
+				darkMode = 'StarCraft 2 Default logo.png',
+				lightMode = 'StarCraft 2 Default logo.png',
+			},
+		},
+	},
+	defaultGame = 'wol',
+
+	defaultTeamLogo = 'StarCraft 2 Default logo.png', ---@deprecated
+	defaultTeamLogoDark = 'StarCraft 2 Default logo.png', ---@deprecated
 }
