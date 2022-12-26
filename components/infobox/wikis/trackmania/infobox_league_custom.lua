7e9e7a5e1349010945631e0a380a27b0c9e97b0a
mbergen <mail@mbergen.de>
2022-12-26 23:32:56 +0100

    Add infobox league to trackmania

---
+++ components/infobox/wikis/trackmania/infobox_league_custom.lua
@@ -0,0 +1,142 @@
+---
+-- @Liquipedia
+-- wiki=trackmania
+-- page=Module:Infobox/League/Custom
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Array = require('Module:Array')
+local Class = require('Module:Class')
+local Lua = require('Module:Lua')
+local Page = require('Module:Page')
+local String = require('Module:StringUtils')
+local Table = require('Module:Table')
+local Variables = require('Module:Variables')
+
+local Injector = Lua.import('Module:Infobox/Widget/Injector', {requireDevIfEnabled = true})
+local League = Lua.import('Module:Infobox/League', {requireDevIfEnabled = true})
+
+local Widgets = require('Module:Infobox/Widget/All')
+local Cell = Widgets.Cell
+local Title = Widgets.Title
+local Center = Widgets.Center
+
+local Games = mw.loadData('Module:Games')
+
+local CustomLeague = Class.new()
+local CustomInjector = Class.new(Injector)
+local _league
+local _args
+local _categories = {}
+
+function CustomLeague.run(frame)
+	local league = League(frame)
+	_league = league
+	_args = league.args
+
+	_args.player_number = _args.participants_number
+	
+	league.createWidgetInjector = CustomLeague.createWidgetInjector
+	league.liquipediaTierHighlighted = CustomLeague.liquipediaTierHighlighted
+	league.defineCustomPageVariables = CustomLeague.defineCustomPageVariables
+	league.getWikiCategories = CustomLeague.getWikiCategories
+	league.addToLpdb = CustomLeague.addToLpdb
+
+	return league:createInfobox(frame)
+end
+
+function CustomLeague.createWidgetInjector()
+	return CustomInjector()
+end
+
+function CustomInjector:parse(id, widgets)
+	if id == 'sponsors' then
+		local partners = _league:getAllArgsForBase(_args, 'partner')
+		table.insert(widgets, Cell{
+			name = 'Partner' .. (#partners > 1 and 's' or ''),
+			content = Array.map(partners, function(partner) return Page.makeInternalLink(partner) end)
+		})
+	elseif id == 'gamesettings' then
+		local games = _league:getAllArgsForBase(_args, 'game')
+		table.insert(widgets, Cell{
+			name = 'Game' .. (#games > 1 and 's' or ''),
+			content = Array.map(games,
+					function(game)
+						local info = Games[game:lower()]
+						if not info then
+							return 'Unknown game, check Module:Games.'
+						end
+						return Page.makeInternalLink(info.name, info.link)
+					end)
+		})
+	elseif id == 'customcontent' then
+		table.insert(widgets, Title{name = (not String.isEmpty(_args.team_number)) and 'Teams' or 'Players'})
+		table.insert(widgets, Cell{
+			name = 'Number of Teams',
+			content = {_args.team_number}
+		})
+		table.insert(widgets, Cell{
+			name = 'Number of Players',
+			content = {_args.player_number}
+		})
+		
+		local maps = _league:getAllArgsForBase(_args, 'map')
+		if #maps then
+			table.insert(widgets, Title{name = 'Maps'})
+			table.insert(widgets, Center{content = {table.concat(maps, '&nbsp;• ')}})
+		end
+	end
+	
+	return widgets
+end
+
+function CustomLeague:liquipediaTierHighlighted(args)
+	return Array.any(_league:getAllArgsForBase(_args, 'organizer'),
+		function(organizer)
+			return organizer:find('Nadeo', 1, true) or organizer:find('Ubisoft', 1, true)
+		end)
+end
+
+function CustomLeague:defineCustomPageVariables(args)
+	Variables.varDefine('tournament_mode',
+		(not String.isEmpty(args.mode)) and args.mode or
+		(not String.isEmpty(args.team_number)) and 'team' or
+		'solo'
+	)
+	
+	Variables.varDefine('tournament_date', Variables.varDefault('tournament_enddate', ''))
+	
+	-- legacy variables, to be removed
+	Variables.varDefine('tournament_tier', Variables.varDefault('tournament_liquipediatier', ''))
+	Variables.varDefine('tournament_tier_type', Variables.varDefault('tournament_liquipediatiertype', ''))
+	
+	Variables.varDefine('tournament_sdate', Variables.varDefault('tournament_startdate', ''))
+	Variables.varDefine('tournament_edate', Variables.varDefault('tournament_enddate', ''))
+	Variables.varDefine('date', Variables.varDefault('tournament_enddate', ''))
+	Variables.varDefine('sdate', Variables.varDefault('tournament_startdate', ''))
+	Variables.varDefine('edate', Variables.varDefault('tournament_enddate', ''))
+end
+
+function CustomLeague:getWikiCategories(args)
+	return _categories
+end
+
+function CustomLeague:addToLpdb(lpdbData, args)
+	if String.isEmpty(args.tickername) then
+		lpdbData['tickername'] = args.name
+	end
+
+	lpdbData['maps'] = table.concat(_league:getAllArgsForBase(_args, 'map'), ';')
+
+	lpdbData['game'] = (Games[args.game] or {}).link
+	lpdbData['participantsnumber'] = args.team_number or args.player_number
+
+	-- Legacy, can be superseeded by lpdbData.mode
+	lpdbData.extradata.individual = Variables.varDefault('tournament_mode', true)
+
+	return lpdbData
+end
+
+
+return CustomLeague
