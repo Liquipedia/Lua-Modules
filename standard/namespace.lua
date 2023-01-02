d8663d853da03cff4aa1d29cdcdb02891381b57a
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2023-01-02 10:36:43 +0100

    add nil check and tonumber

--- standard/namespace.lua
+++ standard/namespace.lua
@@ -26,7 +26,7 @@ function Namespace.idFromName(name)
 end
 
 function Namespace.nameFromId(id)
-	return mw.site.namespaces[id].name
+	return (mw.site.namespaces[tonumber(id)] OR {}).name
 end
 
 function Namespace.prefixFromId(id)
