9eb7eaa1d208013b207d515cb9d930b4cc46c461
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2023-01-04 09:32:30 +0100

    clashroyale

--- components/match2/wikis/clashroyale/brkts_wiki_specific.lua
+++ components/match2/wikis/clashroyale/brkts_wiki_specific.lua
@@ -12,15 +12,6 @@ local Table = require('Module:Table')
 
 local WikiSpecific = Table.copy(Lua.import('Module:Brkts/WikiSpecific/Base', {requireDevIfEnabled = true}))
 
-function WikiSpecific.matchHasDetails(match)
-	return match.dateIsExact
-		or match.timestamp ~= DateExt.epochZero
-		or match.vod
-		or not Table.isEmpty(match.links)
-		or match.comment
-		or 0 < #match.games
-end
-
 function WikiSpecific.getMatchGroupContainer(matchGroupType)
 	return matchGroupType == 'matchlist'
 		and Lua.import('Module:MatchGroup/Display/Matchlist', {requireDevIfEnabled = true}).MatchlistContainer
