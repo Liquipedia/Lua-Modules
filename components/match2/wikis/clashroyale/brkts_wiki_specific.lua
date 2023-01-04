4e01f50d1bfd440975ce81f03a6b3f7d518ce4eb
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2023-01-04 10:55:30 +0100

    CleanUp `Module:Brkts/WikiSpecific` on several wikis (#2291)
    * clashroyale
    
    * remove splatoon brkts_wiki_specific.lua
    
    * remove r6 brkts_wiki_specific.lua
    
    * remove brawlstars brkts_wiki_specific.lua
    
    * remove cod brkts_wiki_specific.lua
    
    * remove cs brkts_wiki_specific.lua
    
    * remove dota2 brkts_wiki_specific.lua
    
    * remove halo brkts_wiki_specific.lua
    
    * remove ow brkts_wiki_specific.lua
    
    * remove lol brkts_wiki_specific.lua
    
    * remove val brkts_wiki_specific.lua
    
    * remove splitgate brkts_wiki_specific.lua
    
    * cr: kick unused require

--- components/match2/wikis/clashroyale/brkts_wiki_specific.lua
+++ components/match2/wikis/clashroyale/brkts_wiki_specific.lua
@@ -6,20 +6,10 @@
 -- Please see https://github.com/Liquipedia/Lua-Modules to contribute
 --
 
-local DateExt = require('Module:Date/Ext')
 local Lua = require('Module:Lua')
 local Table = require('Module:Table')
 
 local WikiSpecific = Table.copy(Lua.import('Module:Brkts/WikiSpecific/Base', {requireDevIfEnabled = true}))
-
-function WikiSpecific.matchHasDetails(match)
-	return match.dateIsExact
-		or match.timestamp ~= DateExt.epochZero
-		or match.vod
-		or not Table.isEmpty(match.links)
-		or match.comment
-		or 0 < #match.games
-end
 
 function WikiSpecific.getMatchGroupContainer(matchGroupType)
 	return matchGroupType == 'matchlist'
