0a1053dbae92317bea071ffaf67c3fa1e7fbe7fb
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2023-01-06 15:54:51 +0100

    broodwar

--- components/infobox/wikis/starcraft/infobox_series_custom.lua
+++ components/infobox/wikis/starcraft/infobox_series_custom.lua
@@ -35,6 +35,7 @@ function CustomSeries.run(frame)
 	_args = series.args
 
 	_args.liquipediatiertype = _args.liquipediatiertype or _args.tiertype
+	_args.liquipediatier = _args.liquipediatier or args.tier
 
 	series.createWidgetInjector = CustomSeries.createWidgetInjector
 
