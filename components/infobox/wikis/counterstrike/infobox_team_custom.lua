a88b0a1efa6043a76ed2ab84a8d2aab8426e1b2d
Rikard Blixt <rikardblixt@gmail.com>
2023-01-06 12:01:13 +0100

    Leftover

--- components/infobox/wikis/counterstrike/infobox_team_custom.lua
+++ components/infobox/wikis/counterstrike/infobox_team_custom.lua
@@ -19,7 +19,6 @@ local Team = Lua.import('Module:Infobox/Team', {requireDevIfEnabled = true})
 
 local Widgets = require('Module:Infobox/Widget/All')
 local Cell = Widgets.Cell
-local WidgetFactory = require('Module:Infobox/Widget/Factory')
 
 local CustomTeam = Class.new()
 local CustomInjector = Class.new(Injector)
