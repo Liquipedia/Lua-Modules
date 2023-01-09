6a81ab417fab093070ef9ba0e9e8d505ae9b6b26
ThatMoonshot <121267259+ThatMoonshot@users.noreply.github.com>
2023-01-09 16:09:24 -0600

    Update match_group_input_custom.lua

--- components/match2/wikis/overwatch/match_group_input_custom.lua
+++ components/match2/wikis/overwatch/match_group_input_custom.lua
@@ -27,6 +27,7 @@ local _DEFAULT_BESTOF = 3
 local _NO_SCORE = -99
 
 local globalVars = PageVariableNamespace()
+local EPOCH_TIME_EXTENDED = '1970-01-01T00:00:00+00:00'
 
 -- containers for process helper functions
 local matchFunctions = {}
@@ -72,7 +73,19 @@ function CustomMatchGroupInput.processOpponent(record, date)
 		opponent = {type = Opponent.literal, name = 'BYE'}
 	end
 
-	Opponent.resolve(opponent, date)
+	local teamTemplateDate = date
+	-- If date is epoch, resolve using tournament dates instead
+	-- Epoch indicates that the match is missing a date
+	-- In order to get correct child team template, we will use an approximately date and not 1970-01-01
+	if teamTemplateDate == EPOCH_TIME_EXTENDED then
+		teamTemplateDate = Variables.varDefaultMulti(
+			'tournament_enddate',
+			'tournament_startdate',
+			TODAY
+		)
+	end
+
+	Opponent.resolve(opponent, teamTemplateDate)
 	MatchGroupInput.mergeRecordWithOpponent(record, opponent)
 end
 
@@ -264,7 +277,7 @@ function matchFunctions.readDate(matchArgs)
 		return dateProps
 	else
 		return {
-			date = MatchGroupInput.getInexactDate(globalVars:get('tournament_enddate')),
+			date = EPOCH_TIME_EXTENDED,
 			dateexact = false,
 		}
 	end
