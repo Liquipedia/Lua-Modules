904c68ce15525bf838d8a53af254fc9ea8aeae44
Rikard Blixt <rikardblixt@gmail.com>
2023-01-19 11:42:54 +0100

    Add `mw.site`

--- definitions/mw.lua
+++ definitions/mw.lua
@@ -416,4 +416,23 @@ function mw.message:isBlank() end
 ---@return boolean
 function mw.message:isDisabled() end
 
+
+---@alias namespaceInfo {id: number, name: string, canonicalName: string, displayName: string, hasSubpages: boolean, hasGenderDistinction: boolean, isCapitalized: boolean, isContent: boolean, isIncludable: boolean, isMovable:boolean, isSubject: boolean, isTalk: boolean, defaultContentModel: string, aliases: string[], subject: namespaceInfo, talk: namespaceInfo, associated: namespaceInfo}
+---@class Site
+---@field currentVersion string
+---@field scriptPath string
+---@field server string
+---@field siteName string
+---@field namespaces table<number|string, namespaceInfo>
+---@field contentNamespaces table<number|string, namespaceInfo>
+---@field subjectNamespaces table<number|string, namespaceInfo>
+---@field talkNamespaces table<number|string, namespaceInfo>
+---@field stats {pages: number, articles: number, files: number, edits: number, users: number, activeUsers: number, admins: number}
+mw.site = {}
+
+---Returns a table holding data about available interwiki prefixes. If filter is the string "local", then only data for local interwiki prefixes is returned. If filter is the string "!local", then only data for non-local prefixes is returned. If no filter is specified, data for all prefixes is returned. A "local" prefix in this context is one that is for the same project.
+---@param filter nil|'local'|'!local'
+---@return {prefix: string, url: string, isProtocolRelative: boolean, isLocal: boolean, isCurrentWiki: boolean, isTranscludable: boolean, isExtraLanguageLink: boolean, displayText:string, tooltip: string?}
+function mw.site.interwikiMap(filter) end
+
 return mw
