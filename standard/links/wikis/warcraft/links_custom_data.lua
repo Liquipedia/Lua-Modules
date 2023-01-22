c8537efa86cf14a78ca9487dd3436672d17351da
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2023-01-22 08:14:34 +0100

    Add warcraft link overwrite data

---
+++ standard/links/wikis/warcraft/links_custom_data.lua
@@ -0,0 +1,17 @@
+---
+-- @Liquipedia
+-- wiki=warcraft
+-- page=Module:Links/CustomData
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+return {
+	prefixes = {
+		esl = {
+			'https://play.eslgaming.com/warcraft/',
+			team = 'https://play.eslgaming.com/team/',
+			player = 'https://play.eslgaming.com/player/',
+		}
+	},
+}
