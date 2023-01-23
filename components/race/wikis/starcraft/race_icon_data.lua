801f8688748234682d253bc21a98d762d19e840b
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2023-01-23 16:32:43 +0100

    Add BW IconData

---
+++ components/race/wikis/starcraft/race_icon_data.lua
@@ -0,0 +1,37 @@
+---
+-- @Liquipedia
+-- wiki=starcraft
+-- page=Module:Race/IconData
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local byRace = {
+	p = {
+		altIcon = 'File:ProtossIcon.png',
+		icon = 'File:Picon small bw.png',
+	},
+	t = {
+		altIcon = 'File:TerranIcon.png',
+		icon = 'File:Ticon small bw.png',
+	},
+	z = {
+		altIcon = 'File:ZergIcon.png',
+		icon = 'File:Zicon small bw.png',
+	},
+	r = {
+		altIcon = 'File:RaceIcon Random.png',
+		icon = 'File:Ricon small bw.png',
+	},
+	u = {
+		icon = 'File:Space filler race.png',
+	},
+}
+
+local randomIcons = {
+	['r(p)'] = 'File:R(P)icon small bw.png',
+	['r(t)'] = 'File:R(T)icon small bw.png',
+	['r(z)'] = 'File:R(Z)icon small bw.png',
+}
+
+return {byRace = byRace, randomIcons = randomIcons}
