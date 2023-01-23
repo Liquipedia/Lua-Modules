038ca1cbb3620d479c1e900f10b64376b5f935c3
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2023-01-23 16:37:41 +0100

    Add sc2 race data redirect

---
+++ components/race/wikis/starcraft2/race_data.lua
@@ -0,0 +1,9 @@
+---
+-- @Liquipedia
+-- wiki=starcraft2
+-- page=Module:Race/Data
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+return require('Module:Race/Data/Starcraft')
