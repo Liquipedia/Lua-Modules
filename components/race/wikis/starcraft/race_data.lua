34bb476a120481e5382c8208f9fea7c3c7f62cfe
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2023-01-23 16:37:34 +0100

    Add bw race data redirect

---
+++ components/race/wikis/starcraft/race_data.lua
@@ -0,0 +1,9 @@
+---
+-- @Liquipedia
+-- wiki=starcraft
+-- page=Module:Race/Data
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+return require('Module:Race/Data/Starcraft')
