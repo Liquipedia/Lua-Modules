bb517a7c46e2fe883fb759bcaeb96f11d9c2271d
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2023-01-24 14:15:57 +0100

    Update and rename components/race/wikis/warcraft/race_data.lua to components/faction/wikis/warcraft/faction_data.lua

--- components/faction/wikis/warcraft/faction_data.lua
+++ components/faction/wikis/warcraft/faction_data.lua
@@ -1,66 +1,66 @@
 ---
 -- @Liquipedia
 -- wiki=warcraft
--- page=Module:Race/Data
+-- page=Module:Faction/Data
 --
 -- Please see https://github.com/Liquipedia/Lua-Modules to contribute
 --
 
-local raceProps = {
+local factionProps = {
 	h = {
 		bgClass = '',--to be created
 		index = 1,
 		name = 'Human',
 		pageName = 'Human',
-		race = 'h',
+		faction = 'h',
 	},
 	o = {
 		bgClass = '',--to be created
 		index = 2,
 		name = 'Orc',
 		pageName = 'Orc',
-		race = 'o',
+		faction = 'o',
 	},
 	n = {
 		bgClass = '',--to be created
 		index = 3,
 		name = 'Nightelf',
 		pageName = 'Nightelf',
-		race = 'n',
+		faction = 'n',
 	},
 	u = {
 		bgClass = '',--to be created
 		index = 4,
 		name = 'Undead',
 		pageName = 'Undead',
-		race = 'u',
+		faction = 'u',
 	},
 	r = {
 		bgClass = '',--to be created
 		index = 5,
 		name = 'Random',
 		pageName = 'Random',
-		race = 'r',
+		faction = 'r',
 	},
 	m = {
 		bgClass = '',--to be created
 		index = 6,
 		name = 'Multiple',
 		pageName = 'Multiple',
-		race = 'm',
+		faction = 'm',
 	},
 	a = {
 		index = 7,
 		name = 'Anonymous',
-		race = 'a',
+		faction = 'a',
 	},
 }
 
 return {
-	raceProps = raceProps,
-	defaultRace = 'a',
-	races = {'h', 'o', 'n', 'u', 'r', 'a'},
-	knownRaces = {'h', 'o', 'n', 'u', 'r'},
-	coreRaces = {'h', 'o', 'n', 'u'},
+	factionProps = factionProps,
+	defaultFaction = 'a',
+	factions = {'h', 'o', 'n', 'u', 'r', 'a'},
+	knownFactions = {'h', 'o', 'n', 'u', 'r'},
+	coreFactions = {'h', 'o', 'n', 'u'},
 	aliases = {},
 }
