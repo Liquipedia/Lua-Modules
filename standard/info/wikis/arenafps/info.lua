4dd77039c6307cdac1f61e4fc76d45542a0d8fea
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2023-01-25 21:29:09 +0100

    Update info.lua

--- standard/info/wikis/arenafps/info.lua
+++ standard/info/wikis/arenafps/info.lua
@@ -11,17 +11,396 @@ return {
 	wikiName = 'arenafps',
 	name = 'Arena FPS',
 	games = {
-		qc = {
-			logo = {
-				darkMode = 'Quake Champions icon.png',
-				lightMode = 'Quake Champions icon.png',
+		ag = {
+			abbreviation = 'ag',
+			defaultTeamLogo = {
+				darkMode = 'Aim Gods logo.png',
+				lightMode = 'Aim Gods logo.png',
 			},
+			link = 'Aim Gods',
+			logo = {
+				darkMode = 'Aim Gods logo.png',
+				lightMode = 'Aim Gods logo.png',
+			},
+			name = 'Aim Gods',
+		},
+		aq2 = {
+			abbreviation = 'aq2',
+			defaultTeamLogo = {
+				darkMode = 'Action Quake 2.png',
+				lightMode = 'Action Quake 2.png',
+			},
+			link = 'Action Quake 2',
+			logo = {
+				darkMode = 'Action Quake 2.png',
+				lightMode = 'Action Quake 2.png',
+			},
+			name = 'Action Quake 2',
+		},
+		chex = {
+			abbreviation = 'chex',
+			defaultTeamLogo = {
+				darkMode = 'Chex Quest.png',
+				lightMode = 'Chex Quest.png',
+			},
+			link = 'Chex Quest',
+			logo = {
+				darkMode = 'Chex Quest.png',
+				lightMode = 'Chex Quest.png',
+			},
+			name = 'Chex Quest',
+		},
+		cpma = {
+			abbreviation = 'cpma',
+			defaultTeamLogo = {
+				darkMode = 'CPMA logo.png',
+				lightMode = 'CPMA logo.png',
+			},
+			link = 'Quake III Arena: CPMA',
+			logo = {
+				darkMode = 'CPMA logo.png',
+				lightMode = 'CPMA logo.png',
+			},
+			name = 'Quake III Arena: CPMA',
+		},
+		d16 = {
+			abbreviation = 'd16',
+			defaultTeamLogo = {
+				darkMode = 'DOOM 2016 logo.png',
+				lightMode = 'DOOM 2016 logo.png',
+			},
+			link = 'DOOM 2016',
+			logo = {
+				darkMode = 'DOOM 2016 logo.png',
+				lightMode = 'DOOM 2016 logo.png',
+			},
+			name = 'DOOM 2016',
+		},
+		d2 = {
+			abbreviation = 'd2',
+			defaultTeamLogo = {
+				darkMode = 'DOOM II logo.png',
+				lightMode = 'DOOM II logo.png',
+			},
+			link = 'DOOM II',
+			logo = {
+				darkMode = 'DOOM II logo.png',
+				lightMode = 'DOOM II logo.png',
+			},
+			name = 'DOOM II',
+		},
+		d3 = {
+			abbreviation = 'd3',
+			defaultTeamLogo = {
+				darkMode = 'DOOM 3 logo.png',
+				lightMode = 'DOOM 3 logo.png',
+			},
+			link = 'DOOM 3',
+			logo = {
+				darkMode = 'DOOM 3 logo.png',
+				lightMode = 'DOOM 3 logo.png',
+			},
+			name = 'DOOM 3',
+		},
+		dbt = {
+			abbreviation = 'dbt',
+			defaultTeamLogo = {
+				darkMode = 'Diabotical logo.png',
+				lightMode = 'Diabotical logo.png',
+			},
+			link = 'Diabotical',
+			logo = {
+				darkMode = 'Diabotical logo.png',
+				lightMode = 'Diabotical logo.png',
+			},
+			name = 'Diabotical',
+		},
+		dmb = {
+			abbreviation = 'dmb',
+			defaultTeamLogo = {
+				darkMode = 'Doombringer logo.png',
+				lightMode = 'Doombringer logo.png',
+			},
+			link = 'DOOMBRINGER',
+			logo = {
+				darkMode = 'Doombringer logo.png',
+				lightMode = 'Doombringer logo.png',
+			},
+			name = 'DOOMBRINGER',
+		},
+		doom = {
+			abbreviation = 'doom',
+			defaultTeamLogo = {
+				darkMode = 'Doom Logo.png',
+				lightMode = 'Doom Logo.png',
+			},
+			link = 'DOOM',
+			logo = {
+				darkMode = 'Doom Logo.png',
+				lightMode = 'Doom Logo.png',
+			},
+			name = 'DOOM',
+		},
+		ma = {
+			abbreviation = 'ma',
+			defaultTeamLogo = {
+				darkMode = 'Master Arena logo.png',
+				lightMode = 'Master Arena logo.png',
+			},
+			link = 'Master Arena',
+			logo = {
+				darkMode = 'Master Arena logo.png',
+				lightMode = 'Master Arena logo.png',
+			},
+			name = 'Master Arena',
+		},
+		pk = {
+			abbreviation = 'pk',
+			defaultTeamLogo = {
+				darkMode = '',
+				lightMode = '',
+			},
+			link = 'Painkiller',
+			logo = {
+				darkMode = '',
+				lightMode = '',
+			},
+			name = 'Painkiller',
+		},
+		q1 = {
+			abbreviation = 'q1',
 			defaultTeamLogo = {
 				darkMode = 'Quake logo.png',
 				lightMode = 'Quake logo.png',
 			},
+			link = 'Quake',
+			logo = {
+				darkMode = 'Quake logo.png',
+				lightMode = 'Quake logo.png',
+			},
+			name = 'Quake',
 		},
-		-- TODO: https://liquipedia.net/arenafps/Special:PrefixIndex?prefix=Game%2F&namespace=10
+		q2 = {
+			abbreviation = 'q2',
+			defaultTeamLogo = {
+				darkMode = 'Quake 2 logo.png',
+				lightMode = 'Quake 2 logo.png',
+			},
+			link = 'Quake 2',
+			logo = {
+				darkMode = 'Quake 2 logo.png',
+				lightMode = 'Quake 2 logo.png',
+			},
+			name = 'Quake 2',
+		},
+		q3 = {
+			abbreviation = 'q3',
+			defaultTeamLogo = {
+				darkMode = 'Quake 3 logo.png',
+				lightMode = 'Quake 3 logo.png',
+			},
+			link = 'Quake III Arena',
+			logo = {
+				darkMode = 'Quake 3 logo.png',
+				lightMode = 'Quake 3 logo.png',
+			},
+			name = 'Quake III Arena',
+		},
+		q4 = {
+			abbreviation = 'q4',
+			defaultTeamLogo = {
+				darkMode = 'Quake 4 logo.png',
+				lightMode = 'Quake 4 logo.png',
+			},
+			link = 'Quake 4',
+			logo = {
+				darkMode = 'Quake 4 logo.png',
+				lightMode = 'Quake 4 logo.png',
+			},
+			name = 'Quake 4',
+		},
+		qc = {
+			abbreviation = 'qc',
+			defaultTeamLogo = {
+				darkMode = 'Quake logo.png',
+				lightMode = 'Quake logo.png',
+			},
+			link = 'Quake Champions',
+			logo = {
+				darkMode = 'Quake logo.png',
+				lightMode = 'Quake logo.png',
+			},
+			name = 'Quake Champions',
+		},
+		qcde = {
+			abbreviation = 'qcde',
+			defaultTeamLogo = {
+				darkMode = 'QCDoom.png',
+				lightMode = 'QCDoom.png',
+			},
+			link = 'Quake Champions: DOOM Edition',
+			logo = {
+				darkMode = 'QCDoom.png',
+				lightMode = 'QCDoom.png',
+			},
+			name = 'Quake Champions: DOOM Edition',
+		},
+		ql = {
+			abbreviation = 'ql',
+			defaultTeamLogo = {
+				darkMode = 'Quake Live logo.png',
+				lightMode = 'Quake Live logo.png',
+			},
+			link = 'Quake Live',
+			logo = {
+				darkMode = 'Quake Live logo.png',
+				lightMode = 'Quake Live logo.png',
+			},
+			name = 'Quake Live',
+		},
+		qw = {
+			abbreviation = 'qw',
+			defaultTeamLogo = {
+				darkMode = 'QuakeWorld logo.png',
+				lightMode = 'QuakeWorld logo.png',
+			},
+			link = 'Quakeworld',
+			logo = {
+				darkMode = 'QuakeWorld logo.png',
+				lightMode = 'QuakeWorld logo.png',
+			},
+			name = 'Quakeworld',
+		},
+		ra = {
+			abbreviation = 'ra',
+			defaultTeamLogo = {
+				darkMode = 'Rocket Arena logo.png',
+				lightMode = 'Rocket Arena logo.png',
+			},
+			link = 'Rocket Arena',
+			logo = {
+				darkMode = 'Rocket Arena logo.png',
+				lightMode = 'Rocket Arena logo.png',
+			},
+			name = 'Rocket Arena',
+		},
+		reflex = {
+			abbreviation = 'reflex',
+			defaultTeamLogo = {
+				darkMode = '',
+				lightMode = '',
+			},
+			link = 'Reflex',
+			logo = {
+				darkMode = '',
+				lightMode = '',
+			},
+			name = 'Reflex',
+		},
+		sm = {
+			abbreviation = 'sm',
+			defaultTeamLogo = {
+				darkMode = 'ShootMania Storm logo.png',
+				lightMode = 'ShootMania Storm logo.png',
+			},
+			link = 'Shootmania Storm',
+			logo = {
+				darkMode = 'ShootMania Storm logo.png',
+				lightMode = 'ShootMania Storm logo.png',
+			},
+			name = 'Shootmania Storm',
+		},
+		ut2k3 = {
+			abbreviation = 'ut2k3',
+			defaultTeamLogo = {
+				darkMode = 'UT2K3 logo.png',
+				lightMode = 'UT2K3 logo.png',
+			},
+			link = 'Unreal Tournament 2003',
+			logo = {
+				darkMode = 'UT2K3 logo.png',
+				lightMode = 'UT2K3 logo.png',
+			},
+			name = 'Unreal Tournament 2003',
+		},
+		ut2k4 = {
+			abbreviation = 'ut2k4',
+			defaultTeamLogo = {
+				darkMode = 'UT2K4 logo.png',
+				lightMode = 'UT2K4 logo.png',
+			},
+			link = 'Unreal Tournament 2004',
+			logo = {
+				darkMode = 'UT2K4 logo.png',
+				lightMode = 'UT2K4 logo.png',
+			},
+			name = 'Unreal Tournament 2004',
+		},
+		ut3 = {
+			abbreviation = 'ut3',
+			defaultTeamLogo = {
+				darkMode = 'Unreal Tournament 3 logo.png',
+				lightMode = 'Unreal Tournament 3 logo.png',
+			},
+			link = 'Unreal Tournament 3',
+			logo = {
+				darkMode = 'Unreal Tournament 3 logo.png',
+				lightMode = 'Unreal Tournament 3 logo.png',
+			},
+			name = 'Unreal Tournament 3',
+		},
+		ut4 = {
+			abbreviation = 'ut4',
+			defaultTeamLogo = {
+				darkMode = '',
+				lightMode = '',
+			},
+			link = 'Unreal Tournament 4',
+			logo = {
+				darkMode = '',
+				lightMode = '',
+			},
+			name = 'Unreal Tournament 4',
+		},
+		ut99 = {
+			abbreviation = 'ut99',
+			defaultTeamLogo = {
+				darkMode = 'UT99 logo.png',
+				lightMode = 'UT99 logo.png',
+			},
+			link = 'Unreal Tournament 99',
+			logo = {
+				darkMode = 'UT99 logo.png',
+				lightMode = 'UT99 logo.png',
+			},
+			name = 'Unreal Tournament 99',
+		},
+		ws = {
+			abbreviation = 'ws',
+			defaultTeamLogo = {
+				darkMode = 'Warsow logo.png',
+				lightMode = 'Warsow logo.png',
+			},
+			link = 'Warsow',
+			logo = {
+				darkMode = 'Warsow logo.png',
+				lightMode = 'Warsow logo.png',
+			},
+			name = 'Warsow',
+		},
+		xonotic = {
+			abbreviation = 'xonotic',
+			defaultTeamLogo = {
+				darkMode = 'Xonotic logo.png',
+				lightMode = 'Xonotic logo.png',
+			},
+			link = 'Xonotic',
+			logo = {
+				darkMode = 'Xonotic logo.png',
+				lightMode = 'Xonotic logo.png',
+			},
+			name = 'Xonotic',
+		},
 	},
 	defaultGame = 'qc',
 	defaultTeamLogo = 'Quake logo.png', ---@deprecated
