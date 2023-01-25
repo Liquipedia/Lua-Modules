ff3b4e2cfb3e080ba00eeb7b4799754a3f8ccf49
DMeluca <97838082+DMeluca@users.noreply.github.com>
2023-01-25 12:17:45 +0000

    Simplified game mode processing
    Removed excess modes and simplified to either "Battle Royale" or "Arena" with several aliases. Removed perspectives entirely.

--- components/infobox/wikis/pubgmobile/infobox_map_custom.lua
+++ components/infobox/wikis/pubgmobile/infobox_map_custom.lua
@@ -33,26 +33,13 @@ local GAME = {
 }
 
 local MODES = {
-	solo = '[[:Category:Solos Mode Tournaments|Solos]]',
-	duo = '[[:Category:Duos Mode Tournaments|Duos]]',
-	squad = '[[:Category:Squads Mode Tournaments|Squads]]',
-	['2v2'] = '[[:Category:2v2 TDM Tournaments|2v2 TDM]]',
-	['4v4'] = '[[:Category:4v4 TDM Tournaments|4v4 TDM]]',
-	['war mode'] = '[[:Category:War Mode Tournaments|War Mode]]',
-	default = '',
+	['battle royale'] = 'Battle Royale',
+	arena = 'Arena',
 }
-MODES.solos = MODES.solo
-MODES.duos = MODES.duo
-MODES.squads = MODES.squad
-MODES.tdm = MODES['2v2']
-
-local PERSPECTIVES = {
-	fpp = {'FPP'},
-	tpp = {'TPP'},
-	mixed = {'FPP', 'TPP'},
-}
-PERSPECTIVES.first = PERSPECTIVES.fpp
-PERSPECTIVES.third = PERSPECTIVES.tpp
+MODES.arenas = MODES.arena
+MODES.tdm = MODES.arena
+MODES.battleroyale = MODES['battle royale']
+MODES.br = MODES['battle royale']
 
 function CustomMap.run(frame)
 	local customMap = Map(frame)
@@ -95,23 +82,7 @@ function CustomMap._getGameVersion()
 end
 
 function CustomMap:_getGameMode()
-	if String.isEmpty(_args.perspective) and String.isEmpty(_args.mode) then
-		return nil
-	end
-	local getPerspectives = function(perspectiveInput)
-		local perspective = string.lower(perspectiveInput or '')
-		-- Clean unnecessary data from the input
-		perspective = string.gsub(perspective, ' person', '')
-		perspective = string.gsub(perspective, ' perspective', '')
-		return PERSPECTIVES[perspective] or {}
-	end
-	local getPerspectiveDisplay = function(perspective)
-		return Template.safeExpand(mw.getCurrentFrame(), 'Abbr/' .. perspective)
-	end
-	local displayPerspectives = Table.mapValues(getPerspectives(_args.perspective), getPerspectiveDisplay)
-	local mode = MODES[string.lower(_args.mode or '')] or MODES['default']
-
-	return mode .. '&nbsp;' .. table.concat(displayPerspectives, '&nbsp;')
+	return MODES[string.lower(_args.mode or '')]
 end
 
 function CustomMap:addToLpdb(lpdbData)
