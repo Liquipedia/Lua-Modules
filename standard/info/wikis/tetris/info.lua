66720a21e07ae63061ad391610c07008187e2dd7
Cameron <58013431+FenrirWulf@users.noreply.github.com>
2023-01-26 13:02:27 -0800

    Updating to new version

--- standard/info/wikis/tetris/info.lua
+++ standard/info/wikis/tetris/info.lua
@@ -7,9 +7,25 @@
 --
 
 return {
-	startYear = 1984,
 	wikiName = 'tetris',
 	name = 'Tetris',
-	defaultTeamLogo = 'Tetris default allmode.png',
-	defaultTeamLogoDark = 'Tetris default allmode.png',
+	games = {
+		tetris = {
+			abbreviation = 'Tetris',
+			name = 'Tetris',
+			link = 'tetris:Main Page',
+			logo = {
+				darkMode = 'Tetris default allmode.png',
+				lightMode = 'Tetris default allmode.png',
+			},
+			defaultTeamLogo = {
+				darkMode = 'Tetris default allmode.png',
+				lightMode = 'Tetris default allmode.png',
+			},
+		},
+	},
+	defaultGame = 'tetris',
+
+	defaultTeamLogo = 'Tetris default allmode.png', ---@deprecated
+	defaultTeamLogoDark = 'Tetris default allmode.png', ---@deprecated
 }
