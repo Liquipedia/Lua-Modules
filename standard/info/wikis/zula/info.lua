4987429c273807d2027210600e8bc744692b744f
Cameron <58013431+FenrirWulf@users.noreply.github.com>
2023-01-26 12:36:32 -0800

    Create Zula Info

---
+++ standard/info/wikis/zula/info.lua
@@ -0,0 +1,15 @@
+---
+-- @Liquipedia
+-- wiki=zula
+-- page=Module:Info
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+return {
+	startYear = 2015,
+	wikiName = 'zula',
+	name = 'Zula',
+	defaultTeamLogo = 'Zula Global default allmode.png',
+	defaultTeamLogoDark = 'Zula Global default allmode.png',
+}
