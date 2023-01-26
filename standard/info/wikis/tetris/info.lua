8066a2c080f08c2c9bbff59f3ae8b4678cd9beab
Cameron <58013431+FenrirWulf@users.noreply.github.com>
2023-01-26 12:37:47 -0800

    Create Tetris info

---
+++ standard/info/wikis/tetris/info.lua
@@ -0,0 +1,15 @@
+---
+-- @Liquipedia
+-- wiki=tetris
+-- page=Module:Info
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+return {
+	startYear = 1984,
+	wikiName = 'tetris',
+	name = 'Tetris',
+	defaultTeamLogo = 'Tetris default allmode.png',
+	defaultTeamLogoDark = 'Tetris default allmode.png',
+}
