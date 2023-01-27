7ae3eef09e39f1b02064242243a828a40da9972f
DMeluca <97838082+DMeluca@users.noreply.github.com>
2023-01-27 15:06:32 +0000

    pubgmobile: create infobox team custom

---
+++ components/infobox/wikis/pubgmobile/infobox_team_custom.lua
@@ -0,0 +1,49 @@
+---
+-- @Liquipedia
+-- wiki=pubgmobile
+-- page=Module:Infobox/Team/Custom
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Class = require('Module:Class')
+local Lua = require('Module:Lua')
+local Template = require('Module:Template')
+local Variables = require('Module:Variables')
+
+local Team = Lua.import('Module:Infobox/Team', {requireDevIfEnabled = true})
+
+local CustomTeam = Class.new()
+
+local _team
+
+function CustomTeam.run(frame)
+	local team = Team(frame)
+	_team = team
+	team.createBottomContent = CustomTeam.createBottomContent
+	team.addToLpdb = CustomTeam.addToLpdb
+  team.defineCustomPageVariables = CustomTeam.defineCustomPageVariables
+	return team:createInfobox()
+end
+
+function CustomTeam:createBottomContent()
+	return Template.expandTemplate(
+		mw.getCurrentFrame(),
+		'Upcoming and ongoing tournaments of',
+		{team = _team.name or _team.pagename}
+	) ..  Template.expandTemplate(
+		mw.getCurrentFrame(),
+		'Placement summary',
+		{team = _team.name or _team.pagename}
+	)
+end
+
+function CustomTeam:addToLpdb(lpdbData, args)
+	return lpdbData
+end
+
+function CustomTeam:defineCustomPageVariables(args)
+	Variables.varDefine('team_captain', args.captain)
+end
+
+return CustomTeam
