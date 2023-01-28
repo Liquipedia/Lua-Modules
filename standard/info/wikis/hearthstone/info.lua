6ccac18c76f9702b0034cf13f841ed5210bca148
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2023-01-28 16:33:10 +0100

    add hearthstone game data as todo

--- standard/info/wikis/hearthstone/info.lua
+++ standard/info/wikis/hearthstone/info.lua
@@ -10,6 +10,22 @@ return {
 	startYear = 2014,
 	wikiName = 'hearthstone',
 	name = 'Hearthstone',
+	games = {
+		todo = {
+			abbreviation = 'todo',
+			name = 'TODO',
+			link = 'Main Page',
+			logo = {
+				darkMode = 'Logo filler event.png',
+				lightMode = 'Logo filler event.png',
+			},
+			defaultTeamLogo = {
+				darkMode = 'Hearthstone logo.png',
+				lightMode = 'Hearthstone logo.png',
+			},
+		},
+	},
+	defaultGame = 'todo',
 	defaultTeamLogo = 'Hearthstone logo.png', ---@deprecated
 	defaultTeamLogoDark = 'Hearthstone logo.png', ---@deprecated
 }
