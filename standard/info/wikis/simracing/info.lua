e6bb0c808e8bf65630768216b276687d95293352
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2023-01-28 16:33:45 +0100

    add simracing game data as todo

--- standard/info/wikis/simracing/info.lua
+++ standard/info/wikis/simracing/info.lua
@@ -10,6 +10,22 @@ return {
 	startYear = 2019,
 	wikiName = 'simracing',
 	name = 'Sim Racing',
+	games = {
+		todo = {
+			abbreviation = 'todo',
+			name = 'TODO',
+			link = 'Main Page',
+			logo = {
+				darkMode = 'Logo filler event.png',
+				lightMode = 'Logo filler event.png',
+			},
+			defaultTeamLogo = {
+				darkMode = 'Sim Racing default darkmode.png',
+				lightMode = 'Sim Racing default lightmode.png',
+			},
+		},
+	},
+	defaultGame = 'todo',
 	defaultTeamLogo = 'Sim Racing default lightmode.png', ---@deprecated
 	defaultTeamLogoDark = 'Sim Racing default darkmode.png', ---@deprecated
 }
