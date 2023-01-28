74f6260008413440cd21ca39b72856384d112314
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2023-01-28 16:34:50 +0100

    add game data for teamfortress as todo

--- standard/info/wikis/teamfortress/info.lua
+++ standard/info/wikis/teamfortress/info.lua
@@ -10,6 +10,22 @@ return {
 	startYear = 2007,
 	wikiName = 'teamfortress',
 	name = 'Team Fortress',
+	games = {
+		todo = {
+			abbreviation = 'todo',
+			name = 'TODO',
+			link = 'Main Page',
+			logo = {
+				darkMode = 'Logo filler event.png',
+				lightMode = 'Logo filler event.png',
+			},
+			defaultTeamLogo = {
+				darkMode = 'Team Fortress logo.png',
+				lightMode = 'Team Fortress logo.png',
+			},
+		},
+	},
+	defaultGame = 'todo',
 	defaultTeamLogo = 'Team Fortress logo.png', ---@deprecated
 	defaultTeamLogoDark = 'Team Fortress logo.png', ---@deprecated
 }
