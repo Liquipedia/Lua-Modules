e9dd64bf3ebc993772ba8c6e248a5fda437089f9
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2023-02-02 12:14:05 +0100

    Update hidden_data_box_custom.lua

--- components/hidden_data_box/wikis/warcraft/hidden_data_box_custom.lua
+++ components/hidden_data_box/wikis/warcraft/hidden_data_box_custom.lua
@@ -72,39 +72,6 @@ function CustomHiddenDataBox.addCustomVariables(args, queryResult)
 			Variables.varDefine('tournament_timezone', timeZone)
 		end
 	end
-
-	--if specified also store in lpdb (custom for warcraft)
-	if Logic.readBool(args.storage) then
-		local prizepool = CustomHiddenDataBox.cleanPrizePool(args.prizepool) or queryResult.prizepool
-		local lpdbData = {
-			name = Variables.varDefault('tournament_name'),
-			tickername = Variables.varDefault('tournament_ticker_name'),
-			shortname = Variables.varDefault('tournament_shortname', Variables.varDefault('tournament_abbreviation')),
-			icon = Variables.varDefault('tournament_icon'),
-			icondark = Variables.varDefault('tournament_icon_dark'),
-			series = mw.ext.TeamLiquidIntegration.resolve_redirect(Variables.varDefault('tournament_series', '')),
-			game = string.lower(Variables.varDefault('tournament_game', '')),
-			type = Variables.varDefault('tournament_type'),
-			startdate = Variables.varDefault('tournament_startdate', '1970-01-01'),
-			enddate = Variables.varDefault('tournament_enddate', '1970-01-01'),
-			sortdate = Variables.varDefault('tournament_enddate', '1970-01-01'),
-			liquipediatier = Variables.varDefault('tournament_liquipediatier'),
-			liquipediatiertype = Variables.varDefault('tournament_liquipediatiertype'),
-			status = Variables.varDefault('tournament_status'),
-			participantsnumber = Variables.varDefault('participants_number'),
-			location = queryResult.location,
-			prizepool = prizepool,
-		}
-		mw.ext.LiquipediaDB.lpdb_tournament('tournament_' .. Variables.varDefault('tournament_name'), lpdbData)
-	end
-end
-
-function CustomHiddenDataBox.cleanPrizePool(value)
-	value = string.gsub(value or '', ',', '')
-	value = string.gsub(value or '', '$', '')
-	if value ~= '' then
-		return value
-	end
 end
 
 return Class.export(CustomHiddenDataBox)
