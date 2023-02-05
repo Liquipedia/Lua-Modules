6430969bdccbbf57aa6fec15d901ff53d0a32ec2
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2023-02-05 10:17:39 +0100

    Update results_table_custom.lua

--- components/results_table/wikis/fortnite/results_table_custom.lua
+++ components/results_table/wikis/fortnite/results_table_custom.lua
@@ -6,6 +6,7 @@
 -- Please see https://github.com/Liquipedia/Lua-Modules to contribute
 --
 
+local Abbreviation = require('Module:Abbreviation')
 local Class = require('Module:Class')
 local Lua = require('Module:Lua')
 local Page = require('Module:Page')
