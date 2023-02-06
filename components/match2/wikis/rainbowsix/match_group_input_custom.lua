afd7caf7c7f607f1a1efa41417ddb9043eb1654d
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2023-02-06 16:20:29 +0100

    break line

--- components/match2/wikis/rainbowsix/match_group_input_custom.lua
+++ components/match2/wikis/rainbowsix/match_group_input_custom.lua
@@ -315,7 +315,8 @@ function matchFunctions.getVodStuff(match)
 		esl = match.esl and 'https://play.eslgaming.com/match/' .. match.esl or nil,
 		faceit = match.faceit and 'https://www.faceit.com/en/rainbow_6/room/' .. match.faceit or nil,
 		lpl = match.lpl and 'https://letsplay.live/match/' .. match.lpl or nil,
-		r6esports = match.r6esports and 'https://www.ubisoft.com/en-us/esports/rainbow-six/siege/match/' .. match.r6esports or nil,
+		r6esports = match.r6esports
+			and 'https://www.ubisoft.com/en-us/esports/rainbow-six/siege/match/' .. match.r6esports or nil,
 		challengermode = match.challengermode and 'https://www.challengermode.com/games/' .. match.challengermode or nil,
 		ebattle = match.ebattle and 'https://www.ebattle.gg/turnier/match/' .. match.ebattle or nil,
 	}
