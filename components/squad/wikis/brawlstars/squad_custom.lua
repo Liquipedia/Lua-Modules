5e14fd130df4048b099ab3c8955c62755b0c31ae
Hesketh2 <88981446+Hesketh2@users.noreply.github.com>
2023-02-07 01:25:26 +0700

    Rework the whole thing to use VALORANT base
    So that Manual Squad Input still works because only few teams use Autos at this point

--- components/squad/wikis/brawlstars/squad_custom.lua
+++ components/squad/wikis/brawlstars/squad_custom.lua
@@ -6,8 +6,11 @@
 -- Please see https://github.com/Liquipedia/Lua-Modules to contribute
 --
 
+local Json = require('Module:Json')
 local Lua = require('Module:Lua')
+local ReferenceCleaner = require('Module:ReferenceCleaner')
 local Table = require('Module:Table')
+local Variables = require('Module:Variables')
 
 local Squad = Lua.import('Module:Squad', {requireDevIfEnabled = true})
 local SquadRow = Lua.import('Module:Squad/Row', {requireDevIfEnabled = true})
@@ -15,8 +18,70 @@ local SquadAutoRefs = Lua.import('Module:SquadAuto/References', {requireDevIfEna
 
 local CustomSquad = {}
 
+function CustomSquad.header(self)
+	local makeHeader = function(wikiText)
+		local headerCell = mw.html.create('th')
+
+		if wikiText == nil then
+			return headerCell
+		end
+
+		return headerCell:wikitext(wikiText):addClass('divCell')
+	end
+
+	local headerRow = mw.html.create('tr'):addClass('HeaderRow')
+
+	headerRow:node(makeHeader('ID'))
+			:node(makeHeader())
+			:node(makeHeader('Name'))
+			:node(makeHeader()) -- "Role"
+			:node(makeHeader('Join Date'))
+	if self.type == Squad.TYPE_INACTIVE or self.type == Squad.TYPE_FORMER_INACTIVE then
+		headerRow:node(makeHeader('Inactive Date'))
+	end
+	if self.type == Squad.TYPE_FORMER or self.type == Squad.TYPE_FORMER_INACTIVE then
+		headerRow:node(makeHeader('Leave Date'))
+				:node(makeHeader('New Team'))
+	end
+
+	self.content:node(headerRow)
+
+	return self
+end
+
 function CustomSquad.run(frame)
-	error('Brawl Stars wiki doesn\'t support manual Squad Tables')
+	local squad = Squad()
+
+	squad:init(frame):title()
+
+	local args = squad.args
+
+	if squad.type == Squad.TYPE_FORMER then
+		local index = 1
+		while args['p' .. index] or args[index] do
+			local player = Json.parseIfString(args['p' .. index] or args[index])
+			if player.inactivedate then
+				squad.type = Squad.TYPE_FORMER_INACTIVE
+				break
+			end
+
+			index = index + 1
+		end
+	end
+
+	squad.header = CustomSquad.header
+	squad:header()
+
+	local index = 1
+	while args['p' .. index] or args[index] do
+		local player = Json.parseIfString(args['p' .. index] or args[index])
+
+		squad:row(CustomSquad._playerRow(player, squad.type))
+
+		index = index + 1
+	end
+
+	return squad:create()
 end
 
 function CustomSquad.runAuto(playerList, squadType)
@@ -32,6 +97,23 @@ function CustomSquad.runAuto(playerList, squadType)
 	squad:title():header()
 
 	for _, player in pairs(playerList) do
+		--Get Reference(s)
+		local joinReference = SquadAutoRefs.useReferences(player.joindateRef, player.joindate)
+		local leaveReference = SquadAutoRefs.useReferences(player.leavedateRef, player.leavedate)
+
+		-- Map between formats
+		player.joindate = (player.joindatedisplay or player.joindate) .. ' ' .. joinReference
+		player.leavedate = (player.leavedatedisplay or player.leavedate) .. ' ' .. leaveReference
+		player.inactivedate = player.leavedate
+
+		player.link = player.page
+		player.role = player.thisTeam.role
+		player.team = player.thisTeam.role == 'Loan' and player.oldTeam.team
+
+		player.newteam = player.newTeam.team
+		player.newteamrole = player.newTeam.role
+		player.newteamdate = player.newTeam.date
+
 		squad:row(CustomSquad._playerRow(player, squad.type))
 	end
 
@@ -39,41 +121,43 @@ function CustomSquad.runAuto(playerList, squadType)
 end
 
 function CustomSquad._playerRow(player, squadType)
-	--Get Reference(s)
-	local joinReference = SquadAutoRefs.useReferences(player.joindateRef, player.joindate)
-	local leaveReference = SquadAutoRefs.useReferences(player.leavedateRef, player.leavedate)
+	local row = SquadRow{useTemplatesForSpecialTeams = true}
 
-	local joinText = (player.joindatedisplay or player.joindate) .. ' ' .. joinReference
-	local leaveText = (player.leavedatedisplay or player.leavedate) .. ' ' .. leaveReference
-
-	local row = SquadRow()
-	row:id({
-		(player.idleavedate or player.id),
+	row:id{
+		player.id,
 		flag = player.flag,
-		link = player.page,
-		captain = player.captain,
-		role = player.thisTeam.role,
-		team = player.thisTeam.role == 'Loan' and player.oldTeam.team,
-	})
-	row:name({name = player.name})
-	row:role({role = player.thisTeam.role})
-	row:date(joinText, 'Join Date:&nbsp;', 'joindate')
+		link = player.link,
+		captain = player.captain or player.igl,
+		role = player.role,
+		team = player.team,
+		teamrole = player.teamrole,
+	}
+	row:name{name = player.name}
+	row:role{role = player.role}
+	row:date(player.joindate, 'Join Date:&nbsp;', 'joindate')
 
-	if squadType == Squad.TYPE_FORMER then
-		row:date(leaveText, 'Leave Date:&nbsp;', 'leavedate')
-		row:newteam({
-			newteam = player.newTeam.team,
-			newteamrole = player.newTeam.role,
-			newteamdate = player.newTeam.date,
-			leavedate = player.newTeam.date
-		})
-	elseif squadType == Squad.TYPE_INACTIVE then
-		row:date(leaveText, 'Inactive Date:&nbsp;', 'inactivedate')
+	if squadType == Squad.TYPE_INACTIVE or squadType == Squad.TYPE_FORMER_INACTIVE then
+		row:date(player.inactivedate, 'Inactive Date:&nbsp;', 'inactivedate')
+	end
+
+	if squadType == Squad.TYPE_FORMER or squadType == Squad.TYPE_FORMER_INACTIVE then
+		row:date(player.leavedate, 'Leave Date:&nbsp;', 'leavedate')
+		row:newteam{
+			newteam = player.newteam,
+			newteamrole = player.newteamrole or player.newrole,
+			newteamdate = player.newteamdate,
+			leavedate = player.leavedate
+		}
 	end
 
 	return row:create(
-		mw.title.getCurrentTitle().prefixedText .. '_' .. player.id .. '_'
-		.. player.joindate .. (player.role and '_' .. player.role or '')
+		Variables.varDefault(
+			'squad_name',
+			mw.title.getCurrentTitle().prefixedText
+		)
+		.. '_' .. player.id .. '_'
+		.. ReferenceCleaner.clean(player.joindate)
+		.. (player.role and '_' .. player.role or '')
 	)
 end
 
