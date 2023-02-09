1786d2020a866d4822bd766a66aa50b89a438b05
mbergen <mail@mbergen.de>
2023-02-09 10:57:08 +0100

    Fix condition when deciding notability

--- components/notability/commons/notability_checker.lua
+++ components/notability/commons/notability_checker.lua
@@ -41,16 +41,14 @@ function NotabilityChecker.run(args)
 
 	output = output .. '===Summary===\n'
 	output = output .. '<b>Final weight:</b> ' .. tostring(weight) .. '\n\n'
+	output = output .. 'This means this ' .. (isTeamResult and 'team' or 'person')
 
-	if weight < Config.NOTABILITY_THRESHOLD_NOTABLE and weight > Config.NOTABILITY_THRESHOLD_MIN then
-		output = output .. 'This means this ' .. (isTeamResult and 'team' or 'person') ..
-		' is <b>OPEN FOR DISCUSSION</b>\n'
-	elseif weight < Config.NOTABILITY_THRESHOLD_MIN then
-		output = output .. 'This means this ' .. (isTeamResult and 'team' or 'person') ..
-		' is <b>NOT NOTABLE</b>\n'
+	if weight >= Config.NOTABILITY_THRESHOLD_NOTABLE then
+		output = output .. ' is <b>NOTABLE</b>\n'
+	elseif weight >= Config.NOTABILITY_THRESHOLD_MIN then
+		output = output .. ' is <b>OPEN FOR DISCUSSION</b>\n'
 	else
-		output = output .. 'This means this ' .. (isTeamResult and 'team' or 'person') ..
-		' is <b>NOTABLE</b>\n'
+		output = output .. ' is <b>NOT NOTABLE</b>\n'
 	end
 
 	return output
