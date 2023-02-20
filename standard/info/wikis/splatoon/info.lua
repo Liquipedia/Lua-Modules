e6b0018541919751468c8911f542ccbad4416433
Hesketh2 <88981446+Hesketh2@users.noreply.github.com>
2023-02-20 18:41:27 +0700

    Add missing games for Splatoon Info

--- standard/info/wikis/splatoon/info.lua
+++ standard/info/wikis/splatoon/info.lua
@@ -12,7 +12,7 @@ return {
 	name = 'Splatoon',
 	games = {
 		splatoon = {
-			abbreviation = 'Splatoon',
+			abbreviation = 'SP1',
 			name = 'Splatoon',
 			link = 'Splatoon',
 			logo = {
@@ -24,8 +24,34 @@ return {
 				lightMode = 'Splatoon default lightmode.png',
 			},
 		},
+		['2'] = {
+			abbreviation = 'SP2',
+			name = 'Splatoon 2',
+			link = 'Splatoon 2',
+			logo = {
+				darkMode = 'Splatoon 2 default allmode.png',
+				lightMode = 'Splatoon 2 default allmode.png',
+			},
+			defaultTeamLogo = {
+				darkMode = 'Splatoon 2 default allmode.png',
+				lightMode = 'Splatoon 2 default allmode.png',
+			},
+		},
+		['3'] = {
+			abbreviation = 'SP3',
+			name = 'Splatoon 3',
+			link = 'Splatoon 3',
+			logo = {
+				darkMode = 'Splatoon 3 default allmode.png',
+				lightMode = 'Splatoon 3 default allmode.png',
+			},
+			defaultTeamLogo = {
+				darkMode = 'Splatoon 3 default allmode.png',
+				lightMode = 'Splatoon 3 default allmode.png',
+			},
+		},
 	},
-	defaultGame = 'todo',
+	defaultGame = 'splatoon',
 	defaultTeamLogo = 'Splatoon default lightmode.png', ---@deprecated
 	defaultTeamLogoDark = 'Splatoon default darkmode.png', ---@deprecated
 }
