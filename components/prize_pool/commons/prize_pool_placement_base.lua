a1e8c9cefd7c9bd5337f5ddbb04ae61924c1c746
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2023-02-20 13:10:16 +0100

    Add PrizePool/Placement base version

---
+++ components/prize_pool/commons/prize_pool_placement_base.lua
@@ -0,0 +1,181 @@
+---
+-- @Liquipedia
+-- wiki=commons
+-- page=Module:PrizePool/Placement/Base
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Array = require('Module:Array')
+local Class = require('Module:Class')
+local Json = require('Module:Json')
+local String = require('Module:StringUtils')
+local Table = require('Module:Table')
+
+local Opponent = require('Module:OpponentLibraries').Opponent
+
+local PRIZE_TYPE_BASE_CURRENCY = 'BASE_CURRENCY'
+
+--- @class Placement
+--- A Placement is a set of opponents who all share the same final place/award in the tournament.
+--- Its input is generally a table created by `Template:Slot`.
+--- It has a range from placeStart to placeEnd (e.g. 5 to 8) or a slotSize (count) or an award.
+local Placement = Class.new(function(self, ...) self:init(...) end)
+
+--- @class Placement
+--- @param args table Input information
+--- @param parent PrizePool The PrizePool this Placement is part of
+function Placement:init(args, parent)
+	self.args = Table.deepCopy(args)
+	self.parent = parent
+	self.prizeTypes = parent.prizeTypes
+	self.date = self.args.date or parent.date
+	self.hasBaseCurrency = false
+
+	self.prizeRewards = self:_readPrizeRewards(self.args)
+
+	return self
+end
+
+function Placement:parseArgs(args)
+	error('This needs to be set via the module that requires "Module:PrizePool/Placement/Base"')
+end
+
+--- Parse the input for available rewards of prizes, for instance how much money a team would win.
+--- This also checks if the Placement instance has a dollar reward and assigns a variable if so.
+function Placement:_readPrizeRewards(args)
+	local rewards = {}
+
+	-- Loop through all prizes that have been defined in the header
+	Array.forEach(self.parent.prizes, function (prize)
+		local prizeData = self.prizeTypes[prize.type]
+		local fieldName = prizeData.row
+		if not fieldName then
+			return
+		end
+
+		local prizeIndex = prize.index
+		local reward = args[fieldName .. prizeIndex]
+		if prizeIndex == 1 then
+			reward = reward or args[fieldName]
+		end
+		if not reward then
+			return
+		end
+
+		rewards[prize.id] = prizeData.rowParse(self, reward, args, prizeIndex)
+	end)
+
+	-- Special case for Base Currency, as it's not defined in the header.
+	local baseType = self.prizeTypes[PRIZE_TYPE_BASE_CURRENCY]
+	if baseType.row and args[baseType.row] then
+		self.hasBaseCurrency = true
+		rewards[PRIZE_TYPE_BASE_CURRENCY .. 1] = baseType.rowParse(self, args[baseType.row], args, 1)
+	end
+
+	return rewards
+end
+
+function Placement:parseOpponents(args)
+	return Array.mapIndexes(function(opponentIndex)
+		local opponentInput = Json.parseIfString(args[opponentIndex])
+		local opponent = {opponentData = {}, prizeRewards = {}, additionalData = {}}
+		if not opponentInput then
+			if self:shouldAddTbdOpponent(opponentIndex) then
+				opponent.opponentData = Opponent.tbd(self.parent.opponentType)
+			else
+				return
+			end
+		else
+			-- Set the date
+			if not Placement._isValidDateFormat(opponentInput.date) then
+				opponentInput.date = self.date
+			end
+
+			-- Parse Opponent Data
+			if opponentInput.type then
+				self.parent:assertOpponentStructType(opponentInput)
+			else
+				opponentInput.type = self.parent.opponentType
+			end
+			opponent.opponentData = self:parseOpponentArgs(opponentInput, opponentInput.date)
+
+			opponent.prizeRewards = self:_readPrizeRewards(opponentInput)
+			opponent.additionalData = self:readAdditionalData(opponentInput)
+
+			-- Set date
+			opponent.date = opponentInput.date
+		end
+		return opponent
+	end)
+end
+
+function Placement:shouldAddTbdOpponent(args)
+	error('Function shouldAddTbdOpponent needs to be set via the module that requires "Module:PrizePool/Placement/Base"')
+end
+
+function Placement:readAdditionalData(args)
+	error('Function readAdditionalData needs to be set via the module that requires "Module:PrizePool/Placement/Base"')
+end
+
+function Placement:parseOpponentArgs(input, date)
+	-- Allow for lua-table, json-table and just raw string input
+	local opponentArgs = Json.parseIfTable(input) or (type(input) == 'table' and input or {input})
+	opponentArgs.type = opponentArgs.type or self.parent.opponentType
+	assert(Opponent.isType(opponentArgs.type), 'Invalid type')
+
+	local opponentData
+	if type(opponentArgs[1]) == 'table' and opponentArgs[1].isAlreadyParsed then
+		opponentData = opponentArgs[1]
+	elseif type(opponentArgs[1]) ~= 'table' then
+		opponentData = Opponent.readOpponentArgs(opponentArgs)
+	end
+
+	if not opponentData or (Opponent.isTbd(opponentData) and opponentData.type ~= Opponent.literal) then
+		opponentData = Table.deepMergeInto(Opponent.tbd(opponentArgs.type), opponentData or {})
+	end
+
+	return Opponent.resolve(opponentData, date, {syncPlayer = self.parent.options.syncPlayers})
+end
+
+
+function Placement:getPrizeRewardForOpponent(opponent, prize)
+	return opponent.prizeRewards[prize] or self.prizeRewards[prize]
+end
+
+function Placement:_setBaseFromRewards(prizesToUse, prizeTypes)
+	Array.forEach(self.opponents, function(opponent)
+		if opponent.prizeRewards[PRIZE_TYPE_BASE_CURRENCY .. 1] or self.prizeRewards[PRIZE_TYPE_BASE_CURRENCY .. 1] then
+			return
+		end
+
+		local baseReward = 0
+		Array.forEach(prizesToUse, function(prize)
+			local localMoney = opponent.prizeRewards[prize.id] or self.prizeRewards[prize.id]
+
+			if not localMoney or localMoney <= 0 then
+				return
+			end
+
+			baseReward = baseReward + prizeTypes[prize.type].convertToBaseCurrency(
+				prize.data,
+				localMoney,
+				opponent.date,
+				self.parent.options.currencyRatePerOpponent
+			)
+			self.parent.usedAutoConvertedCurrency = true
+		end)
+
+		opponent.prizeRewards[PRIZE_TYPE_BASE_CURRENCY .. 1] = baseReward
+	end)
+end
+
+--- Returns true if the input matches the format of a date
+function Placement._isValidDateFormat(date)
+	if type(date) ~= 'string' or String.isEmpty(date) then
+		return false
+	end
+	return date:match('%d%d%d%d%-%d%d%-%d%d') and true or false
+end
+
+return Placement
