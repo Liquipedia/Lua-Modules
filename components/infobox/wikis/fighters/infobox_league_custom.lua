b52bbbd5f5ef5224694a420bdbacaaf1c7d4e81e
Rikard Blixt <rikardblixt@gmail.com>
2023-02-21 16:42:43 +0100

    Unused import

--- components/infobox/wikis/fighters/infobox_league_custom.lua
+++ components/infobox/wikis/fighters/infobox_league_custom.lua
@@ -11,7 +11,6 @@ local Class = require('Module:Class')
 local Game = require('Module:Game')
 local Logic = require('Module:Logic')
 local Lua = require('Module:Lua')
-local String = require('Module:StringUtils')
 local Variables = require('Module:Variables')
 
 local Injector = Lua.import('Module:Infobox/Widget/Injector', {requireDevIfEnabled = true})
