23d95f44f11995058234417f313f0cabd929f96a
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2023-03-06 12:10:58 +0100

    Create tier_custom.lua

---
+++ standard/tier/wikis/counterstrike/tier_custom.lua
@@ -0,0 +1,30 @@
+---
+-- @Liquipedia
+-- wiki=counterstrike
+-- page=Module:Tier/Custom
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Lua = require('Module:Lua')
+
+local Tier = Lua.import('Module:Tier/Utils', {requireDevIfEnabled = true})
+
+local NON_BREAKING_SPACE = '&nbsp;'
+
+local TierCustom = Table.copy(Tier)
+
+--- Converts input to standardized identifier format
+---@param input string|integer|nil
+---@return string|integer|nil
+function Tier.toIdentifier(input)
+	if String.isEmpty(input) then
+		return
+	end
+
+	return tonumber(input)
+		or Tier.legacyNumbers[string.lower(input):gsub(' ', '')]
+		or string.lower(input):gsub(' ', '')
+end
+
+return TierCustom
