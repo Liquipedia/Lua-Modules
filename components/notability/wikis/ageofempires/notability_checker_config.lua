4aa9efbad073cdbed846d8873c1b95621298ea3a
mbergen <mail@mbergen.de>
2023-03-06 14:36:03 +0100

    Fix nil issue caused by misspelled tiertype

--- components/notability/wikis/ageofempires/notability_checker_config.lua
+++ components/notability/wikis/ageofempires/notability_checker_config.lua
@@ -16,7 +16,7 @@ Config.TIER_TYPE_GENERAL = 'general'
 Config.TIER_TYPE_QUALIFIER = 'qualifier'
 Config.TIER_TYPE_WEEKLY = 'weekly'
 Config.TIER_TYPE_MONTHLY = 'monthly'
-Config.TIER_TYPE_SHOW_MATCH = 'show match'
+Config.TIER_TYPE_SHOW_MATCH = 'showmatch'
 Config.TIER_TYPE_MISC = 'misc'
 
 -- How many placements should we retrieve from LPDB for a team/player?
