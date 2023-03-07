2dc04ec6f728335fc8ac287ac80504209fe20128
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2023-03-07 06:14:57 +0100

    Update hidden_data_box_custom.lua

--- components/hidden_data_box/wikis/ageofempires/hidden_data_box_custom.lua
+++ components/hidden_data_box/wikis/ageofempires/hidden_data_box_custom.lua
@@ -8,6 +8,7 @@
 local Class = require('Module:Class')
 local Json = require('Module:Json')
 local Lua = require('Module:Lua')
+local Tier = require('Module:Tier/Custom')
 local Variables = require('Module:Variables')
 
 local BasicHiddenDataBox = Lua.import('Module:HiddenDataBox', {requireDevIfEnabled = true})
@@ -15,6 +16,8 @@ local BasicHiddenDataBox = Lua.import('Module:HiddenDataBox', {requireDevIfEnabl
 local CustomHiddenDataBox = {}
 
 function CustomHiddenDataBox.run(args)
+	args.liquipediatier = args.liquipediatier and Tier.toNumber(args.liquipediatier) or args.liquipediatier
+
 	BasicHiddenDataBox.addCustomVariables = CustomHiddenDataBox.addCustomVariables
 	return BasicHiddenDataBox.run(args)
 end
