247e7552b293d41a483d6bcbab78b383c326b621
spazer <1197637+spazer@users.noreply.github.com>
2023-03-14 08:44:43 -0600

    Prevent rounding of prize pools in Infobox League for fighters (#2699)
    * Prevent rounding of prize pools in Infobox League for fighters
    
    * Update components/infobox/wikis/fighters/infobox_league_custom.lua
    
    Co-authored-by: hjpalpha <75081997+hjpalpha@users.noreply.github.com>
    
    * Cleanup
    
    Remove text handling for prizepools
    Remove unused var
    
    * Swap currency formatting method
    
    ---------
    
    Co-authored-by: hjpalpha <75081997+hjpalpha@users.noreply.github.com>

--- components/infobox/wikis/fighters/infobox_league_custom.lua
+++ components/infobox/wikis/fighters/infobox_league_custom.lua
@@ -8,9 +8,11 @@
 
 local Abbreviation = require('Module:Abbreviation')
 local Class = require('Module:Class')
+local Currency = require('Module:Currency')
 local Game = require('Module:Game')
 local Logic = require('Module:Logic')
 local Lua = require('Module:Lua')
+local String = require('Module:StringUtils')
 local Variables = require('Module:Variables')
 
 local Injector = Lua.import('Module:Infobox/Widget/Injector', {requireDevIfEnabled = true})
@@ -24,8 +26,9 @@ local Chronology = Widgets.Chronology
 local _args
 local _league
 
-local BASE_CURRENCY = 'USD'
+local ABBR_USD = '<abbr title="United States Dollar">USD</abbr>'
 local DEFAULT_TYPE = 'offline'
+local TODAY = os.date('%Y-%m-%d', os.time())
 
 local CustomLeague = Class.new()
 local CustomInjector = Class.new(Injector)
@@ -63,12 +66,6 @@ function CustomLeague.run(frame)
 		end
 	end
 
-	-- Swap prizepool to prizepoolusd when no currency
-	if not _args.localcurrency or _args.localcurrency:upper() == BASE_CURRENCY then
-		_args.prizepoolusd = _args.prizepoolusd or _args.prizepool
-		_args.prizepool = nil
-	end
-
 	league.createWidgetInjector = CustomLeague.createWidgetInjector
 	league.defineCustomPageVariables = CustomLeague.defineCustomPageVariables
 	league.addToLpdb = CustomLeague.addToLpdb
@@ -101,12 +98,9 @@ function CustomInjector:parse(id, widgets)
 		end
 
 	elseif id == 'prizepool' then
-		if _args.prizepoolassumed then
-			widgets[1].content[1] = Abbreviation.make(
-				widgets[1].content[1],
-				'This prize is assumed, and has not been confirmed'
-			)
-		end
+		return {
+			Cell{name = 'Prize pool', content = {CustomLeague:_createPrizepool()}}
+		}
 
 	elseif id == 'gamesettings' then
 		return {
@@ -198,7 +192,82 @@ function CustomLeague.getIconFromSeries(page)
 	if not series then
 		return
 	end
+
 	return series.icon, series.icondark
+end
+
+function CustomLeague:_createPrizepool()
+	if String.isEmpty(_args.prizepool) and
+		String.isEmpty(_args.prizepoolusd) then
+		return nil
+	end
+
+	local localCurrency = _args.localcurrency
+	local prizePoolUSD = _args.prizepoolusd
+	local prizePool = _args.prizepool
+
+	local display
+	if prizePoolUSD then
+		prizePoolUSD = CustomLeague:_cleanPrizeValue(prizePoolUSD)
+	end
+
+	prizePool = CustomLeague:_cleanPrizeValue(prizePool, localCurrency)
+
+	if not prizePoolUSD and localCurrency then
+		local exchangeDate = Variables.varDefault('tournament_enddate', TODAY)
+		prizePoolUSD = CustomLeague:_currencyConversion(prizePool, localCurrency:upper(), exchangeDate)
+		if not prizePoolUSD then
+			error('Invalid local currency "' .. localCurrency .. '"')
+		end
+	end
+
+	if prizePoolUSD and prizePool then
+		display = Currency.display((localCurrency or ''):lower(), Currency.formatMoney(prizePool, 2))
+		.. '<br>(≃ $' .. Currency.formatMoney(prizePoolUSD, 2) .. ' ' .. ABBR_USD .. ')'
+	elseif prizePool or prizePoolUSD then
+		display = '$' .. Currency.formatMoney(prizePool or prizePoolUSD, 2) .. ' ' .. ABBR_USD
+	end
+
+	Variables.varDefine('usd prize', prizePoolUSD or prizePool)
+	Variables.varDefine('tournament_prizepoolusd', prizePoolUSD or prizePool)
+	Variables.varDefine('local prize', prizePool)
+
+	if _args.prizepoolassumed then
+		display = Abbreviation.make(
+			display,
+			'This prize is assumed, and has not been confirmed'
+		)
+	end
+
+	return display
+end
+
+function CustomLeague:_currencyConversion(localPrize, currency, exchangeDate)
+	local usdPrize
+	local currencyRate = Currency.getExchangeRate{
+		currency = currency,
+		date = exchangeDate,
+		setVariables = true,
+	}
+	if currencyRate then
+		usdPrize = currencyRate * localPrize
+	end
+
+	return usdPrize
+end
+
+function CustomLeague:_cleanPrizeValue(value, currency)
+	if String.isEmpty(value) then
+		return nil
+	end
+
+	--remove white spaces, '&nbsp;' and ','
+	value = string.gsub(value, '%s', '')
+	value = string.gsub(value, '&nbsp;', '')
+	value = string.gsub(value, ',', '')
+	value = string.gsub(value, '%$', '')
+
+	return value
 end
 
 return CustomLeague
