a6e9da9bbf1c9b4fc41e6df1803ab7719813f7c9
Martin B <theimarbot@gmail.com>
2023-03-14 23:48:45 +0000

    Update infobox_league_custom.lua

--- components/infobox/wikis/counterstrike/infobox_league_custom.lua
+++ components/infobox/wikis/counterstrike/infobox_league_custom.lua
@@ -5,6 +5,7 @@
 --
 -- Please see https://github.com/Liquipedia/Lua-Modules to contribute
 --
+
 local Array = require('Module:Array')
 local Class = require('Module:Class')
 local Logic = require('Module:Logic')
