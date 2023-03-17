94d67778c9a5b1f6eecb9f576d07a56cdc110efc
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2023-03-17 16:29:01 +0100

    commons

---
+++ standard/circuit_tier_highlight/commons/circuit_tier_highlight.lua
@@ -0,0 +1,24 @@
+---
+-- @Liquipedia
+-- wiki=commons
+-- page=Module:CircuitTierHighlight
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Class = require('Module:Class')
+
+local CircuitTierHighlight = {}
+
+--- Check arguments or queryData if the tournament should be highlighted
+---@param args table
+---@return boolean
+function CircuitTierHighlight.tournament(args)
+	return Logic.readBool(args.publishertier)
+end
+
+-- if a wiki needs a different function for matches they can set it up
+-- while commons uses the same function as for tournaments
+CircuitTierHighlight.match = CircuitTierHighlight.tournament
+
+return Class.export(CircuitTierHighlight)
