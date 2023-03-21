73c8746b6593f431c2452c009ae40520b3a5abcf
Hesketh2 <88981446+Hesketh2@users.noreply.github.com>
2023-03-22 00:52:50 +0700

    Removed more stuff

--- components/infobox/wikis/fifa/infobox_team_custom.lua
+++ components/infobox/wikis/fifa/infobox_team_custom.lua
@@ -11,13 +11,9 @@ local Lua = require('Module:Lua')
 local MatchTicker = require('Module:MatchTicker/Participant')
 local Variables = require('Module:Variables')
 
-local Injector = Lua.import('Module:Infobox/Widget/Injector', {requireDevIfEnabled = true})
 local Team = Lua.import('Module:Infobox/Team', {requireDevIfEnabled = true})
 
-local Widgets = require('Module:Infobox/Widget/All')
-
 local CustomTeam = Class.new()
-local CustomInjector = Class.new(Injector)
 
 local _team
 local _doStore = true
@@ -25,19 +21,14 @@ local _doStore = true
 function CustomTeam.run(frame)
 	local team = Team(frame)
 	_team = team
-	team.createWidgetInjector = CustomTeam.createWidgetInjector
 	team.createBottomContent = CustomTeam.createBottomContent
 	team.addToLpdb = CustomTeam.addToLpdb
 	return team:createInfobox()
 end
 
-function CustomTeam:createWidgetInjector()
-	return CustomInjector()
-end
-
 function CustomTeam:createBottomContent()
 	if _doStore then
-		return MatchTicker.run({team = _team.pagename})
+		return MatchTicker.run{team = _team.pagename}
 	end
 end
 
