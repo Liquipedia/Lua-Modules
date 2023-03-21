4ffc12e7ea22fb68404f6e477e561af50c41d89a
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2023-03-21 08:06:04 +0100

    add missing require

--- components/infobox/commons/infobox_series.lua
+++ components/infobox/commons/infobox_series.lua
@@ -6,6 +6,7 @@
 -- Please see https://github.com/Liquipedia/Lua-Modules to contribute
 --
 
+local Array = require('Module:Array')
 local Class = require('Module:Class')
 local Lua = require('Module:Lua')
 local Namespace = require('Module:Namespace')
