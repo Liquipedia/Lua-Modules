74d5bb98cbb40aab85a691648c828e6b2dae6096
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2023-03-22 13:02:13 +0100

    Create earnings.lua

---
+++ components/earnings/wikis/brawlhalla/earnings.lua
@@ -0,0 +1,24 @@
+---
+-- @Liquipedia
+-- wiki=brawlhalla
+-- page=Module:Earnings
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Class = require('Module:Class')
+local Lua = require('Module:Lua')
+
+local CustomEarnings = Lua.import('Module:Earnings/Base', {requireDevIfEnabled = true})
+
+---@diagnostic disable-next-line: duplicate-set-field
+CustomEarnings.divisionFactorPlayer = function(mode)
+	return 1
+	-- Currently 1 due to brawlhalla storing indiv earnings in prizemoney field
+	-- for duo opponents and no usage of teamCard
+end
+
+-- legacy mode since data on this wiki is legacy
+CustomEarnings.legacyMode = true
+
+return Class.export(CustomEarnings)
