9e9a11789c26abf3e7fb3150c28c51ca83e30663
Hesketh2 <88981446+Hesketh2@users.noreply.github.com>
2023-03-22 19:24:06 +0700

    Update infobox_team_custom.lua

--- components/infobox/wikis/fifa/infobox_team_custom.lua
+++ components/infobox/wikis/fifa/infobox_team_custom.lua
@@ -9,7 +9,6 @@
 local Class = require('Module:Class')
 local Lua = require('Module:Lua')
 local MatchTicker = require('Module:MatchTicker/Participant')
-local Variables = require('Module:Variables')
 
 local Team = Lua.import('Module:Infobox/Team', {requireDevIfEnabled = true})
 
@@ -21,7 +20,6 @@ function CustomTeam.run(frame)
 	local team = Team(frame)
 	_team = team
 	team.createBottomContent = CustomTeam.createBottomContent
-	team.addToLpdb = CustomTeam.addToLpdb
 	return team:createInfobox()
 end
 
