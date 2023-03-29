f7c9cb1acba2606959fa43715ddc3f96e1e5cded
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2023-03-29 07:49:55 +0200

    Create highlight_conditions.lua

---
+++ standard/circuit_tier_highlight/wikis/valorant/highlight_conditions.lua
@@ -0,0 +1,21 @@
+---
+-- @Liquipedia
+-- wiki=valorant
+-- page=Module:HighlightConditions
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Class = require('Module:Class')
+local String = require('Module:StringUtils')
+
+local CircuitTierHighlight = {}
+
+--- Check arguments or queryData if the tournament should be highlighted
+---@param args table
+---@return boolean
+function CircuitTierHighlight.tournament(args)
+	return String.isNotEmpty(args.publishertier)
+end
+
+return Class.export(CircuitTierHighlight)
