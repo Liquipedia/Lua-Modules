e85802726c396e444e87c55413c3fe5d8d99b98e
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2023-04-05 18:53:53 +0200

    Update match_group_shorten_bracket.lua

--- components/match2/commons/match_group_shorten_bracket.lua
+++ components/match2/commons/match_group_shorten_bracket.lua
@@ -11,37 +11,63 @@ local Lua = require('Module:Lua')
 local Json = require('Module:Json')
 local Table = require('Module:Table')
 
+local MatchGroupInput = Lua.import('Module:MatchGroup/Input', {requireDevIfEnabled = true})
 local MatchGroupUtil = Lua.import('Module:MatchGroup/Util', {requireDevIfEnabled = true})
 
 local ShortenBracket = {}
 
+local FIRST_MATCH_EXTENDED_KEY = 'R01-M001'
+local FIRST_MATCH_KEY = 'R1M1'
+
 --- Builds the arguments to be passed to Bracket input processing
 --- when shortening a bracket display
----@param args {bracketId: string, matchGroupId: string, skipRounds: integer, sourceId: string}
+---@param props {bracketId: string, shortTemplate: string, args: table}
 ---@return table
-function ShortenBracket.run(args)
-	local newArgs = {
-		'Bracket/' .. args.bracketId,
-		id = args.matchGroupId,
+function ShortenBracket.buildArgs(props)
+	local shortTemplate = 'Bracket/' .. string.gsub(props.shortTemplate, '^[bB]racket/', '')
+	local bracketId = props.bracketId
+
+	local newArgs = Table.merge(props.args, {
+		shortTemplate,
+		id = bracketId:sub(-10),
 
 		noDuplicateCheck = true,
 		store = false,
 		matchesAreProcessed = true,
-	}
+	})
 
-	local sourceIdLength = string.len(args.sourceId)
-	local matches = MatchGroupUtil.fetchMatchRecords(args.sourceId)
-	assert(Table.isNotEmpty(matches), 'No data found for "|matchGroupId=' .. args.sourceId .. '"')
+	local matches = MatchGroupUtil.fetchMatchRecords(bracketId)
 
-	-- brackets always have the `R01-M001` match, hence checking for its existence ensures it is a bracket
-	local firstMatchId = args.sourceId .. '_R01-M001'
-	assert(Array.any(matches, function(match) return match.match2id == firstMatchId end),
-		'The data found for "|matchGroupId=' .. args.sourceId .. '" seems to be no bracket')
+	assert(Table.isNotEmpty(matches), 'No data found for id "' .. bracketId .. '"')
+	assert(matches[1].match2bracketdata.type == 'bracket',
+		'The data found for id "' .. bracketId .. '" seems to be no bracket')
 
-	local hasFoundMatches
+	ShortenBracket._processMatchesToArgs(
+		newArgs,
+		matches,
+		string.len(bracketId),
+		ShortenBracket._getSkipRoundValue(shortTemplate, bracketId, matches[1])
+	)
+
+	assert(newArgs[FIRST_MATCH_KEY], 'The provided id and shortTemplate values leave an empty bracket')
+
+	return newArgs
+end
+
+function ShortenBracket._getSkipRoundValue(shortTemplate, bracketId, firstMatch)
+	local bracketDatasById = MatchGroupInput._fetchBracketDatas(shortTemplate, bracketId)
+	local newRoundCount = bracketDatasById[FIRST_MATCH_EXTENDED_KEY].coordinates.roundCount
+	local oldRoundCount = firstMatch.match2bracketdata.coordinates.roundCount
+
+	return oldRoundCount - newRoundCount
+end
+
+function ShortenBracket._processMatchesToArgs(newArgs, matches, idLength, skipRounds)
 	for _, match in pairs(matches) do
-		local matchId = string.sub(match.match2id, sourceIdLength + 2)
+		local matchId = string.sub(match.match2id, idLength + 2)
 		local round = tonumber(string.sub(matchId, 2, 3))
+
+		match.bracketdata = {header = match.match2bracketdata.header}
 
 		-- remove data that needs to be adjusted from input processing
 		match.match2bracketdata = nil
@@ -53,18 +79,12 @@ function ShortenBracket.run(args)
 			newArgs[matchId] = Json.stringify(match)
 
 		-- valid match we want to keep
-		elseif round > args.skipRounds then
+		elseif round > skipRounds then
 			hasFoundMatches = true
-			local newMatchId = 'R' .. (round - args.skipRounds) .. 'M' .. tonumber(string.sub(matchId, -3))
+			local newMatchId = 'R' .. (round - skipRounds) .. 'M' .. tonumber(string.sub(matchId, -3))
 			newArgs[newMatchId] = Json.stringify(match)
 		end
 	end
-
-	if not hasFoundMatches then
-		error('The provided matchGroupId and skipRounds values leave an empty bracket')
-	end
-
-	return newArgs
 end
 
 return ShortenBracket
