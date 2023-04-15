ed282d73733b1f2ed1ee6fca4af215734c745594
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2023-04-15 22:30:21 +0200

    Update results_table_award.lua

--- components/results_table/commons/results_table_award.lua
+++ components/results_table/commons/results_table_award.lua
@@ -70,9 +70,9 @@ function AwardsTable:buildRow(placement)
 		))
 	end
 
-	row:tag('td'):wikitext('$' .. Currency.formatMoney(
-			self.config.opponentType ~= Opponent.team and placement.individualprizemoney
-			or placement.prizemoney
+	row:tag('td'):wikitext(Currency.display('USD',
+			self.config.queryType ~= Opponent.team and placement.individualprizemoney or placement.prizemoney,
+			{setVariables = false, abbreviation = false}
 		))
 
 	return row
