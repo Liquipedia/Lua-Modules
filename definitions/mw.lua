6785a08803e548e887ba6a8dbc092e5f98d9dd45
hjpalpha <andreasbuesch1@gmail.com>
2023-04-28 10:11:34 +0200

    Fix annotation for :node() numbers are allowed too

--- definitions/mw.lua
+++ definitions/mw.lua
@@ -141,7 +141,7 @@ mw.html = {}
 function mw.html.create(tagName, args) end
 
 ---Appends a child mw.html (builder) node to the current mw.html instance. If a nil parameter is passed, this is a no-op. A (builder) node is a string representation of an html element.
----@param builder? Html|string
+---@param builder? Html|string|number
 ---@return self
 function mw.html:node(builder) end
 
