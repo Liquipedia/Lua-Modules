4c5a89ff41dd32293af0a998b13f1e04251bfbca
hjpalpha <andreasbuesch1@gmail.com>
2023-04-28 10:13:46 +0200

    Fix annotation for :wikitext() numbers are allowed

--- definitions/mw.lua
+++ definitions/mw.lua
@@ -146,7 +146,7 @@ function mw.html.create(tagName, args) end
 function mw.html:node(builder) end
 
 ---Appends an undetermined number of wikitext strings to the mw.html object. Note that this stops at the first nil item.
----@param ... string?
+---@param ... string|number|nil
 ---@return self
 function mw.html:wikitext(...) end
 
