38e0fff9729a29f128dc42ba404ab6c6231f13b9
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2023-05-04 08:24:29 +0200

    Add TournamentsListing component  (#2852)
    * Create tournaments_card_base.lua
    
    * commons "custom"
    
    * sc2 custom
    
    * Update tournaments_card_base.lua
    
    * sc2: remove unused
    
    * Update tournaments_card_base.lua
    
    * Update tournaments_card_base.lua
    
    * Update tournaments_card_base.lua
    
    * Update tournaments_card_base.lua
    
    * Update tournaments_card_base.lua
    
    * rename
    
    * Fix annotation for :node()
    numbers are allowed too
    
    * Fix annotation for :wikitext()
    numbers are allowed
    
    * Split conditions stuff out into sep module
    
    * adjust location display
    
    * Update components/tournaments_listing/commons/tournaments_listing_conditions.lua
    
    Co-authored-by: Martin B <theimarbot@gmail.com>
    
    * Update components/tournaments_listing/commons/tournaments_listing_base.lua
    
    Co-authored-by: Martin B <theimarbot@gmail.com>
    
    * rename as per martins request
    
    * rename in require
    
    * another one
    
    * Update components/tournaments_listing/commons/tournaments_listing_card_list.lua
    
    Co-authored-by: Rikard Blixt <rikardblixt@gmail.com>
    
    * Update components/tournaments_listing/commons/tournaments_listing_card_list.lua
    
    Co-authored-by: Rikard Blixt <rikardblixt@gmail.com>
    
    ---------
    
    Co-authored-by: Martin B <theimarbot@gmail.com>
    Co-authored-by: Rikard Blixt <rikardblixt@gmail.com>

--- definitions/mw.lua
+++ definitions/mw.lua
@@ -141,12 +141,12 @@ mw.html = {}
 function mw.html.create(tagName, args) end
 
 ---Appends a child mw.html (builder) node to the current mw.html instance. If a nil parameter is passed, this is a no-op. A (builder) node is a string representation of an html element.
----@param builder? Html|string
+---@param builder? Html|string|number
 ---@return self
 function mw.html:node(builder) end
 
 ---Appends an undetermined number of wikitext strings to the mw.html object. Note that this stops at the first nil item.
----@param ... string?
+---@param ... string|number|nil
 ---@return self
 function mw.html:wikitext(...) end
 
