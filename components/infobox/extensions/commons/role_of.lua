8bc2fd33064bbf41c23ffe72b3b2218d77d74ae9
Rikard Blixt <rikardblixt@gmail.com>
2023-05-09 10:21:47 +0200

    Create Role Of Infobox Extension

---
+++ components/infobox/extensions/commons/role_of.lua
@@ -0,0 +1,45 @@
+---
+-- @Liquipedia
+-- wiki=commons
+-- page=Module:RoleOf
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Class = require('Module:Class')
+local Opponent = require('Module:Opponent')
+local OpponentDisplay = require('Module:OpponentDisplay')
+local Variables = require('Module:Variables')
+
+local RoleOf = {}
+
+---Fetches a team's current person in a specific role.
+---Defaults to current page if team is not provided.
+---Data is fetched from LPDB.
+---Returns a formatted string to be used in an Infobox. `nil` is return if no person has the provided role.
+---@param args {team: string?, role: string}
+---@return string?
+function RoleOf.get(args)
+	assert(args.role, 'RoleOf.get() requires a role input')
+
+	local teamPage = (args.team or mw.title.getCurrentTitle().text):gsub(' ', '_')
+	local teamData = mw.ext.LiquipediaDB.lpdb('squadplayer', {
+		conditions = '[[pagename::' .. teamPage .. ']] AND [[position::' .. args.role .. ']] AND [[leavedate::1970-01-01]]',
+		query = 'id, link, nationality',
+		limit = 1
+	})[1]
+
+	if not teamData then
+		return
+	end
+
+	Variables.varDefine(args.role .. 'id', teamData.link)
+	return tostring(OpponentDisplay.InlineOpponent{opponent = Opponent.readOpponentArgs{
+		type = Opponent.solo,
+		name = teamData.id,
+		link = teamData.link,
+		flag = teamData.nationality,
+	}})
+end
+
+return Class.export(RoleOf)
