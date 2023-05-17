4b956b82d154f49a8eb2ed4fc253f0e65c4ea31b
hjpalpha <andreasbuesch1@gmail.com>
2023-05-17 20:26:06 +0200

    Add annotations to links modules

--- standard/links/commons/links.lua
+++ standard/links/commons/links.lua
@@ -233,6 +233,8 @@ local ALIASES = {
 	zhanqitv = {'zhanqi'},
 }
 
+---@param links {[string]: string}
+---@return {[string]: string}
 function Links.transform(links)
 	local function iterateLinks(tbl, aliases)
 		local index = 1
@@ -275,6 +277,10 @@ function Links.transform(links)
 	return transformedLinks
 end
 
+---@param platform string
+---@param id string?
+---@param variant string?
+---@return string
 function Links.makeFullLink(platform, id, variant)
 	if id == nil or id == '' then
 		return ''
@@ -294,6 +300,9 @@ function Links.makeFullLink(platform, id, variant)
 	return prefix .. id .. suffix
 end
 
+---@param links {[string]: string}
+---@param variant string?
+---@return {[string]: string}
 function Links.makeFullLinksForTableItems(links, variant)
 	for key, item in pairs(links) do
 		links[key] = Links.makeFullLink(Links._removeAppendedNumber(key), item, variant)
@@ -303,8 +312,10 @@ end
 
 --remove appended number
 --needed because the link icons require e.g. 'esl' instead of 'esl2'
+---@param key string
+---@return string
 function Links._removeAppendedNumber(key)
-	return string.gsub(key, '%d$', '')
+	return (string.gsub(key, '%d$', ''))
 end
 
 return Class.export(Links, {frameOnly = true})
