ec339b9ed5243b6d833a06f8e8b4657b81d300bc
hjpalpha <andreasbuesch1@gmail.com>
2023-05-18 12:52:21 +0200

    add annos to page.lua and namespace.lua

--- standard/page.lua
+++ standard/page.lua
@@ -11,6 +11,9 @@ local String = require('Module:StringUtils')
 
 local page = {}
 
+---Checks if a page exists for a given (internal) link
+---@param link string
+---@return boolean
 function page.exists(link)
 	local existingPage = mw.title.new(link)
 
@@ -23,6 +26,11 @@ function page.exists(link)
 	return existingPage.exists
 end
 
+---@param options {onlyIfExists: boolean?}
+---@param display string?
+---@param customLink string?
+---@return string?
+---@overload fun(options: string, display: string): string?
 function page.makeInternalLink(options, display, customLink)
 	-- if no options are passed along (e.g. if the module is invoked from wiki code)
 	-- we need to shift the vars around to account for that
@@ -35,6 +43,7 @@ function page.makeInternalLink(options, display, customLink)
 	elseif String.isEmpty(customLink) then
 		customLink = display
 	end
+	---@cast customLink -nil
 
 	if (options or {}).onlyIfExists == true and (not page.exists(customLink)) then
 		return nil
@@ -43,6 +52,9 @@ function page.makeInternalLink(options, display, customLink)
 	return '[[' .. customLink .. '|' .. display .. ']]'
 end
 
+---@param display string?
+---@param link string?
+---@return string?
 function page.makeExternalLink(display, link)
 	if String.isEmpty(display) or String.isEmpty(link) then
 		return nil
