18fe5bd6635c3425e9942b7678dbf61ac08d920b
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2023-05-21 13:25:15 +0200

    commons custom to class stuff

--- components/portal_players/commons/portal_players_custom.lua
+++ components/portal_players/commons/portal_players_custom.lua
@@ -6,6 +6,20 @@
 -- Please see https://github.com/Liquipedia/Lua-Modules to contribute
 --
 
+local Arguments = require('Module:Arguments')
 local Lua = require('Module:Lua')
 
-return Lua.import('Module:PortalPlayers', {requireDevIfEnabled = true})
+local PortalPlayers = Lua.import('Module:PortalPlayers', {requireDevIfEnabled = true})
+
+local CustomPortalPlayers = {}
+
+---Entry Point. Builds the player portal
+---@param frame Frame
+---@return Html
+function CustomPortalPlayers.run(frame)
+	local args = Arguments.getArgs(frame)
+
+	return PortalPlayers(args):create()
+end
+
+return CustomPortalPlayers
