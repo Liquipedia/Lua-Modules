10eb9e594eddfaa6676f39624729a644e182d0a5
DMeluca <97838082+DMeluca@users.noreply.github.com>
2023-05-29 23:02:43 +0100

    Update results_table.lua

--- components/results_table/commons/results_table.lua
+++ components/results_table/commons/results_table.lua
@@ -87,7 +87,8 @@ function ResultsTable:buildRow(placement)
 		self.config.queryType ~= Opponent.team or
 		Table.isNotEmpty(self.config.aliases) then
 
-		row:tag('td'):css('text-align', self.config.hideResult and 'left' or 'right'):attr('data-sort-value', placement.opponentname)
+		row:tag('td'):css('text-align', self.config.hideResult and 'left' or 'right')
+			:attr('data-sort-value', placement.opponentname)
 			:node(self:opponentDisplay(placement,
 			{teamForSolo = not self.config.playerResultsOfTeam, flip = not self.config.hideResult}
 		))
