74f7266d667bff13d6b94afc6a51817c1f915de2
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2023-05-30 13:47:45 +0200

    Create `upperCaseFirst` in StringUtils (#3030)
    * Add `upperCaseFirst` function to StringUtils
    
    * testcases
    
    * Update standard/string_utils.lua
    
    Co-authored-by: Rikard Blixt <rikardblixt@gmail.com>
    
    * Update standard/test/string_utils_test.lua
    
    Co-authored-by: Rikard Blixt <rikardblixt@gmail.com>
    
    ---------
    
    Co-authored-by: Rikard Blixt <rikardblixt@gmail.com>

--- standard/string_utils.lua
+++ standard/string_utils.lua
@@ -126,4 +126,11 @@ function String.interpolate(s, tbl)
 	)
 end
 
+---Uppercase the first letter of a string
+---@param str string
+---@return string
+function String.upperCaseFirst(str)
+	return mw.getContentLanguage():ucfirst(str)
+end
+
 return String
