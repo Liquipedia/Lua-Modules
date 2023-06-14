c6633e229b2f2c2b9242c41c3cbb55bc213478e1
Hesketh2 <88981446+Hesketh2@users.noreply.github.com>
2023-06-14 23:07:42 +0700

    1st revision
    - Remove Organizer Icon LOCAL and those Organizer function entirely, I don't think it really needs for organizer icon (not to confuse with the highlighted tier icons)
    
    - Removed the vars
    
    - For the lpdbData.publishertier I'm not sure if I understand correctly for that paste if its a parse

--- components/infobox/wikis/clashofclans/infobox_league_custom.lua
+++ components/infobox/wikis/clashofclans/infobox_league_custom.lua
@@ -23,15 +23,6 @@ local SUPERCELL_SPONSORED_ICON = '[[File:Supercell lightmode.png|x18px|link=Supe
 	.. '|Tournament sponsored by Supercell.|class=show-when-light-mode]][[File:Supercell darkmode.png'
 	.. '|x18px|link=Supercell|Tournament sponsored by Supercell.|class=show-when-dark-mode]]'
 
-local ORGANIZER_ICONS = {
-	supercell = '[[File:Supercell lightmode.png|x18px|link=Supercell|Supercell|class=show-when-light-mode]]'
-		.. '[[File:Supercell darkmode.png|x18px|link=Supercell|Supercell|class=show-when-dark-mode]] ',
-	['esports engine'] = '[[File:Esports Engine icon allmode.png|x18px|link=Esports Engine|Esports Engine]] ',
-	['esl'] = '[[File:ESL 2019 icon lightmode.png|x18px|link=ESL|ESL|class=show-when-light-mode]]'
-		.. '[[File:ESL 2019 icon darkmode.png|x18px|link=ESL|ESL|class=show-when-dark-mode]] ',
-	['faceit'] = '[[File:FACEIT icon allmode.png|x18px|link=Esports Engine|Esports Engine]] ',
-}
-
 local _args
 local _league
 
@@ -56,27 +47,10 @@ function CustomLeague:createWidgetInjector()
 	return CustomInjector()
 end
 
-function CustomInjector:parse(id, widgets)
-	if id == 'organizers' then
-		local organizers = CustomLeague._createOrganizers()
-		local title = Table.size(organizers) == 1 and 'Organizer' or 'Organizers'
-
-		return {
-			Cell{
-				name = title,
-				content = organizers
-			}
-		}
-	end
-
-	return widgets
-end
-
 function CustomInjector:addCustomCells(widgets)
-	local args = _args
 	table.insert(widgets, Cell{
 		name = 'Teams',
-		content = {args.team_number}
+		content = {_args.team_number}
 	})
 
 	return widgets
@@ -91,59 +65,10 @@ function CustomLeague:appendLiquipediatierDisplay()
 end
 
 function CustomLeague:addToLpdb(lpdbData, args)
-	lpdbData.publishertier = args['supercell-sponsored']
+	lpdbData.publishertier = _args['supercell-sponsored'] == 'true' and 'true' or nil
 	lpdbData.participantsnumber = args.team_number
 
 	return lpdbData
-end
-
-function CustomLeague:defineCustomPageVariables()
-	--Legacy vars
-	Variables.varDefine('tournament_ticker_name', _args.tickername or '')
-	Variables.varDefine('tournament_tier', _args.liquipediatier or '')
-	Variables.varDefine('tournament_prizepool', _args.prizepool or '')
-
-	--Legacy date vars
-	local sdate = Variables.varDefault('tournament_startdate', '')
-	local edate = Variables.varDefault('tournament_enddate', '')
-	Variables.varDefine('tournament_sdate', sdate)
-	Variables.varDefine('tournament_edate', edate)
-	Variables.varDefine('tournament_date', edate)
-	Variables.varDefine('date', edate)
-	Variables.varDefine('sdate', sdate)
-	Variables.varDefine('edate', edate)
-end
-
-function CustomLeague._organizerIcon(organizer)
-	if not organizer then return '' end
-
-	return ORGANIZER_ICONS[organizer:lower()] or ''
-end
-
-function CustomLeague._createOrganizers()
-	if not _args.organizer then
-		return {}
-	end
-
-	local organizers = {
-		CustomLeague._organizerIcon(_args.organizer) .. _league:createLink(
-			_args.organizer, _args['organizer-name'], _args['organizer-link'], _args.organizerref),
-	}
-
-	local index = 2
-	while not String.isEmpty(_args['organizer' .. index]) do
-		table.insert(
-			organizers,
-			CustomLeague._organizerIcon(_args['organizer' .. index]) .. _league:createLink(
-				_args['organizer' .. index],
-				_args['organizer' .. index .. '-name'],
-				_args['organizer' .. index .. '-link'],
-				_args['organizerref' .. index])
-		)
-		index = index + 1
-	end
-
-	return organizers
 end
 
 return CustomLeague
