4d2fcf0f9b4973321dd519d3eef92b6bed0d178b
DMeluca <97838082+DMeluca@users.noreply.github.com>
2023-06-15 10:58:28 +0100

    Removed 4-5

--- components/infobox/wikis/tft/infobox_person_player_custom.lua
+++ components/infobox/wikis/tft/infobox_person_player_custom.lua
@@ -75,8 +75,6 @@ function CustomInjector:parse(id, widgets)
 				CustomPlayer._getRoleData('role').value,
 				CustomPlayer._getRoleData('role2').value,
 				CustomPlayer._getRoleData('role3').value,
-				CustomPlayer._getRoleData('role4').value,
-				CustomPlayer._getRoleData('role5').value,
 			}},
 		}
 	elseif id == 'history' then
@@ -92,8 +90,6 @@ function CustomPlayer:adjustLPDB(lpdbData)
 	lpdbData.extradata.role = Variables.varDefault('role')
 	lpdbData.extradata.role2 = Variables.varDefault('role2')
 	lpdbData.extradata.role3 = Variables.varDefault('role3')
-	lpdbData.extradata.role4 = Variables.varDefault('role4')
-	lpdbData.extradata.role5 = Variables.varDefault('role5')
 
 	return lpdbData
 end
@@ -111,8 +107,6 @@ function CustomPlayer:defineCustomPageVariables(args)
 	Variables.varDefine('role', CustomPlayer._getRoleData('role').value)
 	Variables.varDefine('role2', CustomPlayer._getRoleData('role2').value)
 	Variables.varDefine('role3', CustomPlayer._getRoleData('role3').value)
-	Variables.varDefine('role4', CustomPlayer._getRoleData('role4').value)
-	Variables.varDefine('role5', CustomPlayer._getRoleData('role5').value)
 end
 
 function CustomPlayer:getPersonType(args)
