3adccfba70a48cf9b23008a6815260201e895b91
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2023-06-25 04:55:13 +0200

    Add nationalteams support to pubg infobox player

--- components/infobox/wikis/pubg/infobox_person_player_custom.lua
+++ components/infobox/wikis/pubg/infobox_person_player_custom.lua
@@ -17,6 +17,8 @@ local Player = Lua.import('Module:Infobox/Person', {requireDevIfEnabled = true})
 
 local Widgets = require('Module:Infobox/Widget/All')
 local Cell = Widgets.Cell
+local Center = Widgets.Center
+local Title = Widgets.Title
 
 local _ROLES = {
 	-- Playes
@@ -73,6 +75,9 @@ function CustomInjector:parse(id, widgets)
 				CustomPlayer._createRoleDisplay('role2', _args.role2)
 			}},
 		}
+	elseif id == 'history' and _args.nationalteams then
+		table.insert(widgets, 1, Center{content = {_args.nationalteams}})
+		table.insert(widgets, 1, Title{name = 'National Teams'})
 	end
 	return widgets
 end
