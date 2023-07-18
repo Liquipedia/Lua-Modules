1e253bc7b1e98434824cb1a80dc4d0b8b5255a8e
mbergen <mail@mbergen.de>
2023-07-18 14:43:25 +0200

    AoE: Adjust notability config to new tier/tiertypes (#2679)
    * Fix nil issue caused by misspelled tiertype
    
    * Adjust config to new misc tier
    
    * Update notability_checker_config.lua

--- components/notability/wikis/ageofempires/notability_checker_config.lua
+++ components/notability/wikis/ageofempires/notability_checker_config.lua
@@ -16,8 +16,9 @@ Config.TIER_TYPE_GENERAL = 'general'
 Config.TIER_TYPE_QUALIFIER = 'qualifier'
 Config.TIER_TYPE_WEEKLY = 'weekly'
 Config.TIER_TYPE_MONTHLY = 'monthly'
-Config.TIER_TYPE_SHOW_MATCH = 'show match'
-Config.TIER_TYPE_MISC = 'misc'
+Config.TIER_TYPE_SHOWMATCH = 'showmatch'
+Config.TIER_TYPE_FFA = 'ffa'
+Config.TIER_TYPE_CHARITY = 'charity'
 
 -- How many placements should we retrieve from LPDB for a team/player?
 Config.PLACEMENT_LIMIT = 2000
@@ -43,8 +44,7 @@ Config.NOTABILITY_THRESHOLD_NOTABLE = 1600
 -- quickly decrease the point rewards as the placement gets lower
 Config.EXTRA_DROP_OFF_TYPES = {
 	Config.TIER_TYPE_QUALIFIER,
-	Config.TIER_TYPE_SHOW_MATCH,
-	Config.TIER_TYPE_MISC,
+	Config.TIER_TYPE_SHOWMATCH,
 }
 
 -- Weights used for tournaments
@@ -64,12 +64,8 @@ Config.weights = {
 				points = 1000,
 			},
 			{
-				name = Config.TIER_TYPE_SHOW_MATCH,
-				points = 500,
-			},
-			{
-				name = Config.TIER_TYPE_MISC,
-				points = 500,
+				name = Config.TIER_TYPE_SHOWMATCH,
+				points = 50,
 			},
 			{
 				name = Config.TIER_TYPE_MONTHLY,
@@ -78,6 +74,10 @@ Config.weights = {
 			{
 				name = Config.TIER_TYPE_WEEKLY,
 				points = 2000,
+			},
+			{
+				name = Config.TIER_TYPE_FFA,
+				points = 1500,
 			},
 		},
 	},
@@ -96,12 +96,8 @@ Config.weights = {
 				points = 400,
 			},
 			{
-				name = Config.TIER_TYPE_SHOW_MATCH,
-				points = 200,
-			},
-			{
-				name = Config.TIER_TYPE_MISC,
-				points = 200,
+				name = Config.TIER_TYPE_SHOWMATCH,
+				points = 50,
 			},
 			{
 				name = Config.TIER_TYPE_MONTHLY,
@@ -110,6 +106,10 @@ Config.weights = {
 			{
 				name = Config.TIER_TYPE_WEEKLY,
 				points = 1500,
+			},
+			{
+				name = Config.TIER_TYPE_FFA,
+				points = 1000,
 			},
 		},
 	},
@@ -128,12 +128,8 @@ Config.weights = {
 				points = 200,
 			},
 			{
-				name = Config.TIER_TYPE_SHOW_MATCH,
-				points = 100,
-			},
-			{
-				name = Config.TIER_TYPE_MISC,
-				points = 100,
+				name = Config.TIER_TYPE_SHOWMATCH,
+				points = 50,
 			},
 			{
 				name = Config.TIER_TYPE_MONTHLY,
@@ -142,6 +138,10 @@ Config.weights = {
 			{
 				name = Config.TIER_TYPE_WEEKLY,
 				points = 800,
+			},
+			{
+				name = Config.TIER_TYPE_FFA,
+				points = 500,
 			},
 		},
 	},
@@ -160,11 +160,7 @@ Config.weights = {
 				points = 100,
 			},
 			{
-				name = Config.TIER_TYPE_SHOW_MATCH,
-				points = 50,
-			},
-			{
-				name = Config.TIER_TYPE_MISC,
+				name = Config.TIER_TYPE_SHOWMATCH,
 				points = 50,
 			},
 			{
@@ -175,21 +171,45 @@ Config.weights = {
 				name = Config.TIER_TYPE_WEEKLY,
 				points = 400,
 			},
+			{
+				name = Config.TIER_TYPE_FFA,
+				points = 200,
+			},
 		},
 	},
 	{
-		tier = 9,
+		tier = -1,
 		options = {
 			dateLossIgnored = true,
 		},
 		tiertype = {
 			{
 				name = Config.TIER_TYPE_GENERAL,
-				points = 100,
+				points = 0,
 			},
 			{
-				name = Config.TIER_TYPE_SHOW_MATCH,
-				points = 100,
+				name = Config.TIER_TYPE_QUALIFIER,
+				points = 0,
+			},
+			{
+				name = Config.TIER_TYPE_SHOWMATCH,
+				points = 50,
+			},
+			{
+				name = Config.TIER_TYPE_MONTHLY,
+				points = 0,
+			},
+			{
+				name = Config.TIER_TYPE_WEEKLY,
+				points = 0,
+			},
+			{
+				name = Config.TIER_TYPE_FFA,
+				points = 0,
+			},
+			{
+				name = Config.TIER_TYPE_CHARITY,
+				points = 0,
 			},
 		},
 	},
