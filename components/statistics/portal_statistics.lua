300d3693d6eb6d5c90e53c531733e9aafc97a721
DMeluca <97838082+DMeluca@users.noreply.github.com>
2023-07-25 14:31:23 +0100

    Update components/statistics/portal_statistics.lua
    Co-authored-by: Rikard Blixt <rikardblixt@gmail.com>

--- components/statistics/portal_statistics.lua
+++ components/statistics/portal_statistics.lua
@@ -1073,7 +1073,7 @@ function StatisticsPortal._earningsTableRow(args, placements, earnings, opponent
 		:tag('td'):wikitext(placements['3'] or '0'):done()
 
 	if Logic.readBool(args.displayShowMatches) then
-		row:tag('td'):wikitext(placements.showWins or '0')
+		row:tag('td'):wikitext(placements.showWins or 0)
 	end
 
 	row:tag('td')
