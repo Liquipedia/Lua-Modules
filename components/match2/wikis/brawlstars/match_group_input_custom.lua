487a8dd3841614b94259ee4fcee3246e75994b18
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2023-07-27 17:05:33 +0200

    Add missing displayname reading for brawlstars

--- components/match2/wikis/brawlstars/match_group_input_custom.lua
+++ components/match2/wikis/brawlstars/match_group_input_custom.lua
@@ -269,6 +269,7 @@ function matchFunctions.getPlayers(match, opponentIndex, teamName)
 		local player = Json.parseIfString(match['opponent' .. opponentIndex .. '_p' .. playerIndex]) or {}
 		player.name = player.name or Variables.varDefault(teamName .. '_p' .. playerIndex)
 		player.flag = player.flag or Variables.varDefault(teamName .. '_p' .. playerIndex .. 'flag')
+		player.displayname = player.displayname or Variables.varDefault(teamName .. '_p' .. playerIndex .. 'dn')
 		if not Table.isEmpty(player) then
 			table.insert(match['opponent' .. opponentIndex].match2players, player)
 		end
