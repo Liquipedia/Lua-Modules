ceda1d6b479fc1813235b76a9ec880e29c137ad1
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2023-07-27 08:57:24 +0200

    Adjust parsing of team players in matches on some wikis (#3141)
    * escape nil
    
    * directly set players into `opponent.match2players`
    instead of pushing them into `match['opponent' .. opponentIndex .. '_p' .. playerIndex]`

--- components/match2/wikis/valorant/match_group_input_custom.lua
+++ components/match2/wikis/valorant/match_group_input_custom.lua
@@ -423,6 +423,7 @@ function matchFunctions.getOpponents(match)
 end
 
 function matchFunctions.getPlayers(match, opponentIndex, teamName)
+	match['opponent' .. opponentIndex].match2players = {}
 	for playerIndex = 1, MAX_NUM_PLAYERS do
 		-- parse player
 		local player = Json.parseIfString(match['opponent' .. opponentIndex .. '_p' .. playerIndex]) or {}
@@ -430,9 +431,10 @@ function matchFunctions.getPlayers(match, opponentIndex, teamName)
 		player.flag = player.flag or Variables.varDefault(teamName .. '_p' .. playerIndex .. 'flag')
 		player.displayname = player.displayname or Variables.varDefault(teamName .. '_p' .. playerIndex .. 'dn')
 		if not Table.isEmpty(player) then
-			match['opponent' .. opponentIndex .. '_p' .. playerIndex] = player
+			table.insert(match['opponent' .. opponentIndex].match2players, player)
 		end
 	end
+
 	return match
 end
 
