f4fe2cbd66efc65e34b533f3005ebe8652bd0b2d
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2023-08-08 09:01:41 +0200

    Add `.identity` and `.compareByKey` functions to `Operator.lua`

--- standard/operator.lua
+++ standard/operator.lua
@@ -85,4 +85,24 @@ function Operator.method(funcName, ...)
 	end
 end
 
+---@generic T
+---@param a T
+---@return T
+function Operator.identity(a)
+	return a
+end
+
+---@param item string
+---@param invert boolean?
+---@return fun(a: table, b: table): boolean
+function Operator.compareByKey(key, invert)
+	return function(a, b)
+		if invert then
+			return a[key] < b[key]
+		end
+
+		return a[key] > b[key]
+	end
+end
+
 return Operator
