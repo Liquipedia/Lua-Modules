affd7bbf854b7b1007ad0a8666ecb616acc5bd2c
hjpalpha <andreasbuesch1@gmail.com>
2023-08-31 12:48:06 +0200

    Add annos to infobox company

--- components/infobox/wikis/pubg/infobox_company_custom.lua
+++ components/infobox/wikis/pubg/infobox_company_custom.lua
@@ -21,6 +21,8 @@ local CustomInjector = Class.new(Injector)
 
 local _args
 
+---@param widgets Widget[]
+---@return Widget[]
 function CustomInjector:addCustomCells(widgets)
 	table.insert(widgets, Cell({
 		name = CustomCompany._createSisterCompaniesDescription(_args),
@@ -29,6 +31,8 @@ function CustomInjector:addCustomCells(widgets)
 	return widgets
 end
 
+---@param frame Frame
+---@return Html
 function CustomCompany.run(frame)
 	local company = Company(frame)
 	company.createWidgetInjector = CustomCompany.createWidgetInjector
@@ -36,10 +40,13 @@ function CustomCompany.run(frame)
 	return company:createInfobox()
 end
 
+---@return WidgetInjector
 function CustomCompany:createWidgetInjector()
 	return CustomInjector()
 end
 
+---@param args table
+---@return string
 function CustomCompany._createSisterCompaniesDescription(args)
 	if args.sister2 then
 		return 'Sister Companies'
