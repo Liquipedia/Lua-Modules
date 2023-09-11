b86c2099a8001c995c31f4d4250a962a4e875b73
Rikard Blixt <rikardblixt@gmail.com>
2023-09-11 11:04:51 +0200

    Game Start box

--- components/match2/wikis/apexlegends/match_summary.lua
+++ components/match2/wikis/apexlegends/match_summary.lua
@@ -25,6 +25,9 @@ function CustomMatchSummary.getByMatchId(args)
 
 	matchSummary:node(CustomMatchSummary._createHeader(match))
 	matchSummary:node(CustomMatchSummary._createOverallPage(match))
+	for idx in ipairs(match.games) do
+		matchSummary:node(CustomMatchSummary._createGameTab(match, idx))
+	end
 
 	return tostring(matchSummary)
 end
@@ -47,19 +50,10 @@ function CustomMatchSummary._createHeader(match)
 	return mw.html.create('div'):addClass('panel-tabs'):attr('role', 'tabpanel'):node(header)
 end
 
-function CustomMatchSummary._createOverallPage(match)
-	local wrapper = mw.html.create('div'):addClass('panel-content'):attr('id', 'panel1')
-	-- Schedule
-	local schedule = wrapper:tag('h5'):addClass('panel-content__button'):attr('tabindex', 0):wikitext('Schedule')
-	local timeList = wrapper:tag('div')
-	timeList:addClass('panel-content__container'):attr('id', 'panelContent1'):attr('role', 'tabpanel')
-	for idx, game in ipairs(match.games) do
-		timeList:tag('div'):wikitext('Game ', idx, ': '):node(CustomMatchSummary._gameCountdown(game))
-	end
-
-	-- Help Text
+function CustomMatchSummary._createPointsDistributionTable(match)
+	local wrapper = mw.html.create(nil)
 	local scoring = match.extradata.scoring
-	local helpText = wrapper:tag('h5'):addClass('panel-content__button'):addClass('is--collapsed'):attr('tabindex', 0):wikitext('Points Distribution')
+	wrapper:tag('h5'):addClass('panel-content__button'):addClass('is--collapsed'):attr('tabindex', 0):wikitext('Points Distribution')
 
 	local pointsList = wrapper:tag('div')
 	pointsList:addClass('panel-content__container'):addClass('is--hidden'):attr('id', 'panelContent1'):attr('role', 'tabpanel'):attr('hidden')
@@ -67,7 +61,7 @@ function CustomMatchSummary._createOverallPage(match)
 	local points = Table.groupBy(scoring, function (_, value)
 		return value
 	end)
-	for point, placements in Table.iter.spairs(points, function (tbl, a, b)
+	for point, placements in Table.iter.spairs(points, function (_, a, b)
 		return a > b
 	end) do
 		if Table.size(placements) == 1 then
@@ -77,12 +71,161 @@ function CustomMatchSummary._createOverallPage(match)
 			pointsList:tag('div'):wikitext(placementRange[1], ' - ', placementRange[#placementRange], ' ', point, ' placement points')
 		end
 	end
+	return wrapper
+end
 
+function CustomMatchSummary._createOverallPage(match)
+	local infoArea = mw.html.create('div'):addClass('panel-content'):attr('id', 'panel1')
+	-- Schedule
+	infoArea:tag('h5'):addClass('panel-content__button'):attr('tabindex', 0):wikitext('Schedule')
+	local schedule = infoArea:tag('div')
+	schedule:addClass('panel-content__container'):attr('id', 'panelContent1'):attr('role', 'tabpanel')
+	for idx, game in ipairs(match.games) do
+		schedule:tag('div'):wikitext('Game ', idx, ': '):node(CustomMatchSummary._gameCountdown(game))
+	end
 
+	infoArea:node(CustomMatchSummary._createPointsDistributionTable(match))
+
+	return tostring(infoArea) -- .. CustomMatchSummary._createOverallStandings(match)
+end
+
+function CustomMatchSummary._createGameTab(match, idx)
+	local game = match.games[idx]
+	local infoArea = mw.html.create('div'):addClass('panel-content'):attr('id', 'panel1')
+	-- Schedule
+	infoArea:tag('h5'):addClass('panel-content__button'):attr('tabindex', 0):wikitext('Game Details')
+	local gameDetails = infoArea:tag('div')
+	gameDetails:addClass('panel-content__container'):attr('id', 'panelContent1'):attr('role', 'tabpanel')
+	gameDetails:tag('div'):wikitext('Game ', idx, ': '):node(CustomMatchSummary._gameCountdown(game))
+	gameDetails:tag('div'):node(CustomMatchSummary._gameCountdown(game))
+	gameDetails:tag('div'):wikitext(game.map)
+
+	-- Help Text
+	infoArea:node(CustomMatchSummary._createPointsDistributionTable(match))
+
+	return tostring(infoArea) .. tostring(CustomMatchSummary._createGameStandings(game))
+end
+
+local gamestuff = {
+	{
+		header = {
+			class = nil,
+			value = function ()
+				return ''
+			end,
+		},
+		row = {
+			class = nil,
+			value = function (opponent)
+				return '\\/'
+			end,
+		},
+		show = function ()
+			return false
+		end,
+	},
+	{
+		header = {
+			class = nil,
+			value = function ()
+				return 'Rank'
+			end,
+		},
+		row = {
+			class = nil,
+			value = function (opponent)
+				return 'TODO'
+			end,
+		},
+		show = function ()
+			return true
+		end,
+	},
+	{
+		header = {
+			class = nil,
+			value = function ()
+				return 'Team'
+			end,
+		},
+		row = {
+			class = nil,
+			value = function (opponent)
+				return opponent.name
+			end,
+		},
+		show = function ()
+			return true
+		end,
+	},
+	{
+		header = {
+			class = nil,
+			value = function ()
+				return 'Total Points'
+			end,
+		},
+		row = {
+			class = nil,
+			value = function (opponent)
+				return opponent.score
+			end,
+		},
+		show = function ()
+			return true
+		end,
+	},
+	{
+		header = {
+			class = nil,
+			value = function ()
+				return 'Placement Points'
+			end,
+		},
+		row = {
+			class = nil,
+			value = function (opponent)
+				return 'TODO'
+			end,
+		},
+		show = function ()
+			return true
+		end,
+	},
+	{
+		header = {
+			class = nil,
+			value = function ()
+				return 'Kill Points'
+			end,
+		},
+		row = {
+			class = nil,
+			value = function (opponent)
+				return 'TODO'
+			end,
+		},
+		show = function ()
+			return true
+		end,
+	},
+}
+
+function CustomMatchSummary._createGameStandings(game)
+	local wrapper = mw.html.create('div')
+	for _, column in ipairs(gamestuff) do
+		wrapper:node(mw.html.create('div'):wikitext(column.header.value()))
+	end
+	for _, opponent in ipairs({}) do
+		for _, column in ipairs(gamestuff) do
+			wrapper:node(mw.html.create('div'):wikitext(column.row.value(opponent)))
+		end
+	end
 	return wrapper
 end
 
 function CustomMatchSummary._gameCountdown(game)
+	--- TODO Add VOD for completed games
 	local timestamp = Date.readTimestamp(game.date)
 	if not timestamp then
 		return
