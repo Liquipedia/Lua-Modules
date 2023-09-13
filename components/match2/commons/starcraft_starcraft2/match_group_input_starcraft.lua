cc9e6be86f6618426032260b4e41def8292cde0a
hjpalpha <andreasbuesch1@gmail.com>
2023-09-13 13:24:54 +0200

    shut up anno warning

--- components/match2/commons/starcraft_starcraft2/match_group_input_starcraft.lua
+++ components/match2/commons/starcraft_starcraft2/match_group_input_starcraft.lua
@@ -110,7 +110,7 @@ function StarcraftMatchGroupInput._checkFinished(match)
 	-- Match is automatically marked finished upon page edit after a
 	-- certain amount of time (depending on whether the date is exact)
 	if match.finished ~= true then
-		local currentUnixTime = os.time(os.date('!*t') --[[@as osdate]])
+		local currentUnixTime = os.time(os.date('!*t') --[[@as osdateparam]])
 		local matchUnixTime = tonumber(mw.getContentLanguage():formatDate('U', match.date))
 		local threshold = match.dateexact and 30800 or 86400
 		if matchUnixTime + threshold < currentUnixTime then
