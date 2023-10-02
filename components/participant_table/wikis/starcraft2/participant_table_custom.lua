b7b613b6350ef779645ad48e953c7c913753a0b0
hjpalpha <andreasbuesch1@gmail.com>
2023-10-02 08:08:48 +0200

    low hanging fruits

---
+++ components/participant_table/wikis/starcraft2/participant_table_custom.lua
@@ -0,0 +1,11 @@
+---
+-- @Liquipedia
+-- wiki=starcraft2
+-- page=Module:ParticipantTable/Custom
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Lua = require('Module:Lua')
+
+return Lua.import('Module:ParticipantTable/Starcraft', {requireDevIfEnabled = true})
