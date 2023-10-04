7e932a0d87953df65891eb03119991ec96d667a2
Martin B <theimarbot@gmail.com>
2023-10-04 17:23:10 +0100

    Update match_links.lua

--- components/match2/wikis/leagueoflegends/match_links.lua
+++ components/match2/wikis/leagueoflegends/match_links.lua
@@ -7,7 +7,16 @@
 --
 
 return {
-	reddit = {icon = 'File:Reddit-icon.png', text = 'Reddit Thread'},
-	gol = {icon = 'File:Gol.gg allmode.png', text = 'GolGG Match Report'},
-	factor = {icon = 'File:Factor.gg lightmode.png', iconDark = 'File:Factor.gg darkmode.png', text = 'FactorGG Match Report'},
+	reddit = {
+		icon = 'File:Reddit-icon.png',
+		text = 'Reddit Thread',
+	},
+	gol = {
+		icon = 'File:Gol.gg allmode.png',
+		text = 'GolGG Match Report',
+	},
+	factor = {icon = 'File:Factor.gg lightmode.png',
+		iconDark = 'File:Factor.gg darkmode.png',
+		text = 'FactorGG Match Report',
+	},
 }
