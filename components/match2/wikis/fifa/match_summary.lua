717efc7ba14a5d3ea6f91d5d4505a7a62359c847
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2023-10-05 08:37:57 +0200

    Update match_summary.lua

--- components/match2/wikis/fifa/match_summary.lua
+++ components/match2/wikis/fifa/match_summary.lua
@@ -13,7 +13,7 @@ local Lua = require('Module:Lua')
 local Table = require('Module:Table')
 
 local DisplayHelper = Lua.import('Module:MatchGroup/Display/Helper', {requireDevIfEnabled = true})
-local MatchSummary = Lua.import('Module:MatchSummary/Base/temp', {requireDevIfEnabled = true})
+local MatchSummary = Lua.import('Module:MatchSummary/Base', {requireDevIfEnabled = true})
 
 local OpponentDisplay = require('Module:OpponentLibraries').OpponentDisplay
 
