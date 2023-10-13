5bb82678ca081222866714eb81ff277bdd520c3d
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2023-10-13 10:42:26 +0200

    Update components/match2/wikis/warcraft/match_summary.lua

--- components/match2/wikis/warcraft/match_summary.lua
+++ components/match2/wikis/warcraft/match_summary.lua
@@ -27,7 +27,7 @@ local OpponentDisplay = OpponentLibraries.OpponentDisplay
 local ICONS = {
 	greenCheck = '<i class="fa fa-check forest-green-text" style="width: 14px; text-align: center" ></i>',
 	yellowLine = '<i class="fas fa-minus bright-sun-text" style="width: 14px; text-align: center" ></i>',
-	yellowQuestionMark = '[[File:YellowQuestionMark.png|14x14px|link=]]',
+	yellowQuestionMark = '<i class="fas fa-question bright-sun-text" style="width: 14px; text-align: center" ></i>',
 	redCross = '<i class="fas fa-times cinnabar-text" style="width: 14px; text-align: center" ></i>',
 	noCheck = '[[File:NoCheck.png|link=]]',
 }
