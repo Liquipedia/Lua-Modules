d7789e5dda6ac5a4fab08a259238262cf1fecfc3
Rikard Blixt <rikardblixt@gmail.com>
2023-10-16 12:01:40 +0200

    feat: match2 for freefire

---
+++ components/match2/wikis/freefire/match_legacy.lua
@@ -0,0 +1,111 @@
+---
+-- @Liquipedia
+-- wiki=freefire
+-- page=Module:Match/Legacy
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local p = {}
+
+local json = require("Module:Json")
+local Logic = require("Module:Logic")
+local String = require("Module:StringUtils")
+local Table = require("Module:Table")
+local Variables = require("Module:Variables")
+
+function p.storeMatch(match2)
+	local match = p._convertParameters(match2)
+
+	match.games = p.storeGames(match, match2)
+
+	return mw.ext.LiquipediaDB.lpdb_match(
+		"legacymatch_" .. match2.match2id,
+		match
+	)
+end
+
+function p.storeGames(match, match2)
+	local games = ""
+	for gameIndex, game in ipairs(match2.match2games or {}) do
+		game = Table.deepCopy(game)
+		game.opponent1 = match.opponent1
+		game.opponent2 = match.opponent2
+		game.opponent1flag = match.opponent1flag
+		game.opponent2flag = match.opponent2flag
+		game.date = match.date
+		local scores = game.scores or {}
+		if type(scores) == "string" then
+			scores = json.parse(scores)
+		end
+		game.opponent1score = scores[1] or 0
+		game.opponent2score = scores[2] or 0
+		local res = mw.ext.LiquipediaDB.lpdb_game(
+			"legacygame_" .. match2.match2id .. gameIndex,
+			game
+		)
+		games = games .. res
+	end
+	return games
+end
+
+function p._convertParameters(match2)
+	local match = Table.deepCopy(match2)
+	for key, _ in pairs(match) do
+		if String.startsWith(key, "match2") then
+			match[key] = nil
+		end
+	end
+
+	match.staticid = match2.match2id
+
+	local opponent1 = match2.match2opponents[1] or {}
+	local opponent1match2players = opponent1.match2players or {}
+	if opponent1.type == "team" then
+		match.opponent1 = opponent1.name
+		match.opponent1score = (tonumber(opponent1.score or 0) or 0) >= 0
+			and opponent1.score or 0
+		local opponent1players = {}
+		for i = 1,10 do
+			local player = opponent1match2players[i] or {}
+			opponent1players["p" .. i] = player.name or ""
+			opponent1players["p" .. i .. "flag"] = player.flag or ""
+		end
+		match.opponent1players = json.stringify(opponent1players)
+	elseif opponent1.type == "solo" then
+		local player = opponent1match2players[1] or {}
+		match.opponent1 = player.name
+		match.opponent1score = (tonumber(opponent1.score or 0) or 0) >= 0
+			and opponent1.score or 0
+		match.opponent1flag = player.flag
+	end
+
+	local opponent2 = match2.match2opponents[2] or {}
+	local opponent2match2players = opponent2.match2players or {}
+	if opponent2.type == "team" then
+		match.opponent2 = opponent2.name
+		match.opponent2score = (tonumber(opponent2.score or 0) or 0) >= 0
+			and opponent2.score or 0
+		local opponent2players = {}
+		for i = 1,10 do
+			local player = opponent2match2players[i] or {}
+			opponent2players["p" .. i] = player.name or ""
+			opponent2players["p" .. i .. "flag"] = player.flag or ""
+		end
+		match.opponent2players = json.stringify(opponent2players)
+	elseif opponent2.type == "solo" then
+		local player = opponent2match2players[1] or {}
+		match.opponent2 = player.name
+		match.opponent2score = (tonumber(opponent2.score or 0) or 0) >= 0
+			and opponent2.score or 0
+		match.opponent2flag = player.flag
+	end
+	if match2.walkover then
+		match.resulttype = match2.walkover
+		match.walkover = nil
+	end
+
+	return match
+end
+
+return p
---
+++ components/match2/wikis/freefire/match_summary.lua
@@ -0,0 +1,179 @@
+---
+-- @Liquipedia
+-- wiki=freefire
+-- page=Module:MatchSummary
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Logic = require('Module:Logic')
+local Lua = require('Module:Lua')
+local Table = require('Module:Table')
+local VodLink = require('Module:VodLink')
+local String = require('Module:StringUtils')
+local MapModes = require('Module:MapModes')
+
+local DisplayHelper = Lua.import('Module:MatchGroup/Display/Helper', {requireDevIfEnabled = true})
+local MatchSummary = Lua.import('Module:MatchSummary/Base', {requireDevIfEnabled = true})
+
+local _EPOCH_TIME = '1970-01-01 00:00:00'
+local _EPOCH_TIME_EXTENDED = '1970-01-01T00:00:00+00:00'
+
+local htmlCreate = mw.html.create
+
+local _GREEN_CHECK = '<i class="fa fa-check forest-green-text" style="width: 14px; text-align: center" ></i>'
+local _NO_CHECK = '[[File:NoCheck.png|link=]]'
+local _ICONS = {
+	check = _GREEN_CHECK,
+}
+
+local LINK_DATA = {
+	preview = {icon = 'File:Preview Icon32.png', text = 'Preview'},
+	lrthread = {icon = 'File:LiveReport32.png', text = 'LiveReport.png'},
+	esl = {
+		icon = 'File:ESL_2019_icon_lightmode.png',
+		iconDark = 'File:ESL_2019_icon_darkmode.png',
+		text = 'Match page on ESL'
+	},
+	owl = {icon = 'File:Overwatch League allmode.png', text = 'Overwatch League matchpage'},
+	owc = {icon = 'File:OWC-BMS icon.png', text = 'OW Contenders matchpage'},
+	jcg = {icon = 'File:JCG-BMS icon.png', text = 'JCG matchpage'},
+	oceow = {icon = 'File:OCEOW-BMS icon.png', text = 'OCEOverwatch matchpage'},
+	tespa = {icon = 'File:Tespa icon.png', text = 'Tespa matchpage'},
+	overgg = {icon = 'File:overgg icon.png', text = 'over.gg matchpage'},
+	pf = {icon = 'File:Plus Forward icon.png', text = 'Plus Forward matchpage'},
+	wl = {icon = 'File:Winstons Lab-icon.png', text = 'Winstons Lab matchpage'},
+}
+
+local CustomMatchSummary = {}
+
+---@param args table
+---@return Html
+function CustomMatchSummary.getByMatchId(args)
+	return MatchSummary.defaultGetByMatchId(CustomMatchSummary, args, {width = '400px'})
+end
+
+---@param match MatchGroupUtilMatch
+---@param footer MatchSummaryFooter
+---@return MatchSummaryFooter
+function CustomMatchSummary.addToFooter(match, footer)
+	footer = MatchSummary.addVodsToFooter(match, footer)
+
+	return footer:addLinks(LINK_DATA, match.links)
+end
+
+function CustomMatchSummary.createBody(match)
+	local body = MatchSummary.Body()
+
+	if match.dateIsExact or (match.date ~= _EPOCH_TIME_EXTENDED and match.date ~= _EPOCH_TIME) then
+		-- dateIsExact means we have both date and time. Show countdown
+		-- if match is not epoch=0, we have a date, so display the date
+		body:addRow(MatchSummary.Row():addElement(
+			DisplayHelper.MatchCountdownBlock(match)
+		))
+	end
+
+	-- Iterate each map
+	for _, game in ipairs(match.games) do
+		if game.map then
+			body:addRow(CustomMatchSummary._createMapRow(game))
+		end
+	end
+
+	-- Add Match MVP(s)
+	if (match.extradata or {}).mvp then
+		local mvpData = match.extradata.mvp
+		if not Table.isEmpty(mvpData) and mvpData.players then
+			local mvp = MatchSummary.Mvp()
+			for _, player in ipairs(mvpData.players) do
+				mvp:addPlayer(player)
+			end
+			mvp:setPoints(mvpData.points)
+
+			body:addRow(mvp)
+		end
+
+	end
+
+	return body
+end
+
+function CustomMatchSummary._gameScore(game, opponentIndex)
+	local score = game.scores[opponentIndex] or ''
+	return htmlCreate('div'):wikitext(score)
+end
+
+function CustomMatchSummary._createMapRow(game)
+	local row = MatchSummary.Row()
+
+	-- Add Header
+	if Logic.isNotEmpty(game.header) then
+		local mapHeader = htmlCreate('div')
+			:wikitext(game.header)
+			:css('font-weight','bold')
+			:css('font-size','85%')
+			:css('margin','auto')
+		row:addElement(mapHeader)
+		row:addElement(MatchSummary.Break():create())
+	end
+
+	local centerNode = htmlCreate('div')
+		:addClass('brkts-popup-spaced')
+		:wikitext(CustomMatchSummary._getMapDisplay(game))
+		:css('text-align', 'center')
+
+	if game.resultType == 'np' then
+		centerNode:addClass('brkts-popup-spaced-map-skip')
+	end
+
+	local leftNode = htmlCreate('div')
+		:addClass('brkts-popup-spaced')
+		:node(CustomMatchSummary._createCheckMarkOrCross(game.winner == 1, 'check'))
+		:node(CustomMatchSummary._gameScore(game, 1))
+
+	local rightNode = htmlCreate('div')
+		:addClass('brkts-popup-spaced')
+		:node(CustomMatchSummary._gameScore(game, 2))
+		:node(CustomMatchSummary._createCheckMarkOrCross(game.winner == 2, 'check'))
+
+	row:addElement(leftNode)
+		:addElement(centerNode)
+		:addElement(rightNode)
+
+	row:addClass('brkts-popup-body-game')
+		:css('overflow', 'hidden')
+
+	-- Add Comment
+	if Logic.isNotEmpty(game.comment) then
+		row:addElement(MatchSummary.Break():create())
+		local comment = htmlCreate('div')
+			:wikitext(game.comment)
+			:css('margin', 'auto')
+		row:addElement(comment)
+	end
+
+	return row
+end
+
+function CustomMatchSummary._getMapDisplay(game)
+	local mapDisplay = '[[' .. game.map .. ']]'
+	if String.isNotEmpty(game.mode) then
+		mapDisplay = MapModes.get{mode = game.mode} .. mapDisplay
+	end
+	return mapDisplay
+end
+
+function CustomMatchSummary._createCheckMarkOrCross(showIcon, iconType)
+	local container = htmlCreate('div')
+	container:addClass('brkts-popup-spaced'):css('line-height', '27px')
+
+	if showIcon then
+		container:node(_ICONS[iconType])
+	else
+		container:node(_NO_CHECK)
+	end
+
+	return container
+end
+
+return CustomMatchSummary
