d9503ae2726e2d2e5a526756d0918681a61f132d
Hesketh2 <88981446+Hesketh2@users.noreply.github.com>
2023-10-18 02:12:03 +0700

    Cleanup
    (what was _team originally supposed to do then?)

--- components/infobox/wikis/callofduty/infobox_team_custom.lua
+++ components/infobox/wikis/callofduty/infobox_team_custom.lua
@@ -10,19 +10,14 @@ local Class = require('Module:Class')
 local Lua = require('Module:Lua')
 local MatchTicker = require('Module:MatchTicker/Custom')
 local String = require('Module:StringUtils')
-local Template = require('Module:Template')
-local Variables = require('Module:Variables')
 
 local Team = Lua.import('Module:Infobox/Team', {requireDevIfEnabled = true})
 
 local CustomTeam = Class.new()
-
-local _team
 local _doStore = true
 
 function CustomTeam.run(frame)
 	local team = Team(frame)
-	_team = team
 	team.createBottomContent = CustomTeam.createBottomContent
 	team.addToLpdb = CustomTeam.addToLpdb
 	team.getWikiCategories = CustomTeam.getWikiCategories
