992a4a051661cb69a4ddf16eb5f89d22daa3e928
hjpalpha <andreasbuesch1@gmail.com>
2023-10-24 08:25:52 +0200

    customs

--- components/match2/wikis/arenaofvalor/match_summary.lua
+++ components/match2/wikis/arenaofvalor/match_summary.lua
@@ -81,7 +81,7 @@ end
 ---@param args table
 ---@return Html
 function CustomMatchSummary.getByMatchId(args)
-	return MatchSummary.defaultGetByMatchId(CustomMatchSummary, args, {width = '420px'})
+	return MatchSummary.defaultGetByMatchId(CustomMatchSummary, args, {width = '420px', teamStyle = 'bracket'})
 end
 
 ---@param match MatchGroupUtilMatch
