abcb57d794f878a09106f64d88eaa9701668b48b
Martin B <theimarbot@gmail.com>
2023-10-25 22:02:16 +0100

    Create infobox_team_custom.lua

---
+++ components/infobox/wikis/worldofwarcraft/infobox_team_custom.lua
@@ -0,0 +1,60 @@
+---
+-- @Liquipedia
+-- wiki=worldofwarcraft
+-- page=Module:Infobox/Team/Custom
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Class = require('Module:Class')
+local Lua = require('Module:Lua')
+local String = require('Module:String')
+local Table = require('Module:Table')
+local Variables = require('Module:Variables')
+
+local Team = Lua.import('Module:Infobox/Team', {requireDevIfEnabled = true})
+local Region = Lua.import('Module:Region', {requireDevIfEnabled = true})
+
+local REGION_REMAPPINGS = {
+	['cis'] = 'europe',
+
+	['south america'] = 'americas',
+	['latin america'] = 'americas',
+	['north america'] = 'americas',
+
+	['asia'] = 'southeast asia',
+	['oceania'] = 'southeast asia',
+	['asia-pacific'] = 'southeast asia',
+}
+
+local CustomTeam = Class.new()
+
+function CustomTeam.run(frame)
+	local team = Team(frame)
+
+	team._createRegion = CustomTeam._createRegion
+
+	return team:createInfobox()
+end
+
+function CustomTeam:_createRegion(region)
+	if String.isEmpty(region) then
+		return
+	end
+
+	local regionData = Region.run({region = region})
+	if Table.isEmpty(regionData) then
+		return
+	end
+
+	local remappedRegion = REGION_REMAPPINGS[regionData.region:lower()]
+	if remappedRegion then
+		return CustomTeam:_createRegion(remappedRegion)
+	end
+
+	Variables.varDefine('region', regionData.region)
+
+	return regionData.display
+end
+
+return CustomTeam
