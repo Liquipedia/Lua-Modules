759804d6f16915ed349cf9c126530e5193e532d0
Hesketh2 <88981446+Hesketh2@users.noreply.github.com>
2023-11-06 23:37:57 +0700

    Change THRESHOLD
    Time expired still persist

--- components/match2/wikis/osu/match_group_input_custom.lua
+++ components/match2/wikis/osu/match_group_input_custom.lua
@@ -31,7 +31,7 @@ local EPOCH_TIME_EXTENDED = '1970-01-01T00:00:00+00:00'
 
 local NOW = os.time(os.date('!*t'))
 local MATCHTIME = nil
-local THRESHOLD = Match.dateexact and 30800 or 86400
+local THRESHOLD = 86400
 
 -- containers for process helper functions
 local matchFunctions = {}
