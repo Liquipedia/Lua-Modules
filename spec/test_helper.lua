b322d92cd1fbe01cbeb1d3b5552468cfff7d66e4
Rikard Blixt <rikardblixt@gmail.com>
2023-11-08 10:49:39 +0100

    try to have some handling for files not on git

--- spec/test_helper.lua
+++ spec/test_helper.lua
@@ -1,4 +1,25 @@
- -- luacheck: ignore
+-- luacheck: ignore
+
+-- Copy from Standard/lua.lua
+local function fileExists(name)
+	if package.loaded[name] then
+		return true
+	else
+		-- Package.Searchers was renamed from Loaders in lua5.2, have support for both
+		---@diagnostic disable-next-line: deprecated
+		for _, searcher in ipairs(package.searchers or package.loaders) do
+			local loader = searcher(name)
+			if type(loader) == 'function' then
+				-- luacheck: ignore
+				-- luacheck complains about package.preload being read-only
+				package.preload[name] = loader
+				return true
+			end
+		end
+		return false
+	end
+end
+
 local function mockRequire()
 	package.path = '?.lua;' ..
 		-- Load plugin for module name parsing
@@ -12,15 +33,27 @@ local function mockRequire()
 		package.path
 
 	local require_original = require
-	local plugin = require_original('sumneko_plugin')
+	local Plugin = require_original('sumneko_plugin')
 
 	function require(module)
 		local newName = module
 		if (string.find(module, 'Module:')) then
-			newName = plugin.luaifyModuleName(module)
+			newName = Plugin.luaifyModuleName(module)
 		end
 
-		return require_original(newName)
+		if fileExists(newName) then
+			return require_original(newName)
+		end
+
+		-- Just apply a fake function that returns the first input, as something
+		local mocked_import = {}
+		setmetatable(mocked_import, {
+			__index = function (t, k)
+				return function(v) return v end
+			end
+		})
+
+		return mocked_import
 	end
  end
 
