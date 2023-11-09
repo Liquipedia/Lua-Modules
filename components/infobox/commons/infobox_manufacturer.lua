01a6017a567e176ab1123ed345ddfe83d29d6e9a
kitbinary <110872385+kitbinary@users.noreply.github.com>
2023-11-09 17:30:19 +0000

    Update components/infobox/commons/infobox_manufacturer.lua
    Co-authored-by: hjpalpha <75081997+hjpalpha@users.noreply.github.com>

--- components/infobox/commons/infobox_manufacturer.lua
+++ components/infobox/commons/infobox_manufacturer.lua
@@ -126,9 +126,7 @@ function Manufacturer:setLpdbData(args)
 	lpdbData = self:addToLpdb(lpdbData, args)
 
 	lpdbData.extradata = mw.ext.LiquipediaDB.lpdb_create_json(lpdbData.extradata or {})
-	mw.ext.LiquipediaDB.lpdb_datapoint('manufacturer_' .. self.name, Table.merge(lpdbData, {
-		extradata = mw.ext.LiquipediaDB.lpdb_create_json(lpdbData.extradata)
-	}))
+	mw.ext.LiquipediaDB.lpdb_datapoint('manufacturer_' .. self.name, lpdbData.extradata)
 end
 
 function Manufacturer:addToLpdb(lpdbData, args)
