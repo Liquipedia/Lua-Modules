01124d715baed1a28b6a25d1e8871ffd49101c20
Rikard Blixt <rikardblixt@gmail.com>
2023-11-10 15:48:55 +0100

    fix: annotations in widget and widget factory

--- components/infobox/commons/infobox_widget_factory.lua
+++ components/infobox/commons/infobox_widget_factory.lua
@@ -17,19 +17,21 @@ local WidgetFactory = Class.new()
 
 ---@param widget Widget
 ---@param injector WidgetInjector
----@return Widget[]
+---@return Html[]
 function WidgetFactory.work(widget, injector)
-	local convertedWidgets = {}
+	local convertedWidgets = {} ---@type Html[]
 
 	if widget == nil then
 		return {}
 	end
 
 	for _, child in ipairs(widget:tryMake() or {}) do
-		if type(child) == 'table' and child['is_a'] and child:is_a(Widget) then
+		if type(child) == 'table' and type(child['is_a']) == 'function' and child:is_a(Widget) then
+			---@cast child Widget
 			child:setContext{injector = injector}
 			Array.extendWith(convertedWidgets, WidgetFactory.work(child, injector))
 		else
+			---@cast child Html
 			table.insert(convertedWidgets, child)
 		end
 	end
