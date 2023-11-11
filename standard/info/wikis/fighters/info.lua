39591a8afc15abb154f183b429d3e637f66dfa2e
spazer <1197637+spazer@users.noreply.github.com>
2023-11-11 08:37:46 -0700

    Update MK1 icons for Fighters (#3475)
    * Update info.lua
    
    Correct mk1 icons
    
    * Update info.lua
    
    Update game logo with default naming

--- standard/info/wikis/fighters/info.lua
+++ standard/info/wikis/fighters/info.lua
@@ -887,12 +887,12 @@ return {
 			name = 'Mortal Kombat 1',
 			link = 'Mortal Kombat 1',
 			logo = {
-				darkMode = 'Mortal Kombat 1 default darkmode.png',
-				lightMode = 'Mortal Kombat 1 default lightmode.png',
+				darkMode = 'Mortal Kombat 1 default allmode.png',
+				lightMode = 'Mortal Kombat 1 default allmode.png',
 			},
 			defaultTeamLogo = {
-				darkMode = 'Mortal Kombat 1 default darkmode.png',
-				lightMode = 'Mortal Kombat 1 default lightmode.png',
+				darkMode = 'Mortal Kombat 1 default allmode.png',
+				lightMode = 'Mortal Kombat 1 default allmode.png',
 			},
 		},
 		mvc2 = {
