54bd65aa0381be7fc53a53a9173d1bc2530c7642
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2023-11-11 15:19:02 +0100

    Update components/prize_pool/commons/prize_pool_base.lua
    Co-authored-by: Rikard Blixt <rikardblixt@gmail.com>

--- components/prize_pool/commons/prize_pool_base.lua
+++ components/prize_pool/commons/prize_pool_base.lua
@@ -567,7 +567,7 @@ function BasePrizePool:_buildTable(isAward)
 	tbl:setContext{self._widgetInjector}
 	local tableNode = mw.html.create('div'):css('overflow-x', 'auto')
 	for _, node in ipairs(WidgetFactory.work(tbl, self._widgetInjector)) do
-		tableNode:node(node --[[@as Html]])
+		tableNode:node(node)
 	end
 
 	return tableNode
