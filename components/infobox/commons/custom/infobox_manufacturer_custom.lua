39f201c5d105f1f53aa1c92140f1a8bc33bf81e2
kitbinary <110872385+kitbinary@users.noreply.github.com>
2023-11-12 18:18:57 +0000

    Update infobox_manufacturer_custom.lua

--- components/infobox/commons/custom/infobox_manufacturer_custom.lua
+++ components/infobox/commons/custom/infobox_manufacturer_custom.lua
@@ -8,6 +8,7 @@
 
 local Class = require('Module:Class')
 local Lua = require('Module:Lua')
+local Game = Lua.import('Module:Infobox/Manufacturer', {requireDevIfEnabled = true})
 
 local CustomManufacturer = Class.new()
 
