3669fe1e548c362fde8178d6e23e3f27cbc64b2c
kitbinary <110872385+kitbinary@users.noreply.github.com>
2023-11-13 18:30:31 +0000

    Create infobox_team_custom.lua

---
+++ components/infobox/wikis/formula1/infobox_team_custom.lua
@@ -0,0 +1,105 @@
+---
+-- @Liquipedia
+-- wiki=formula1
+-- page=Module:Infobox/Team/Custom
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Array = require('Module:Array')
+local Class = require('Module:Class')
+local Lua = require('Module:Lua')
+local Template = require('Module:Template')
+local Variables = require('Module:Variables')
+local Table = require('Module:Table')
+
+local Injector = Lua.import('Module:Infobox/Widget/Injector', {requireDevIfEnabled = true})
+local Team = Lua.import('Module:Infobox/Team', {requireDevIfEnabled = true})
+
+local Widgets = require('Module:Infobox/Widget/All')
+local Cell = Widgets.Cell
+local Header = Widgets.Header
+local Title = Widgets.Title
+local Center = Widgets.Center
+local Customizable = Widgets.Customizable
+local Builder = Widgets.Builder
+local Chronology = Widgets.Chronology
+
+local CustomTeam = Class.new()
+local CustomInjector = Class.new(Injector)
+local Chronology = Widgets.Chronology
+
+local _args 
+local _team
+
+function CustomTeam.run(frame)
+	local team = Team(frame)
+	_team = team
+	_args = _team.args
+
+	team.createWidgetInjector = CustomTeam.createWidgetInjector
+	team.createBottomContent = CustomTeam.createBottomContent
+	team.addToLpdb = CustomTeam.addToLpdb
+	team.getWikiCategories = CustomTeam.getWikiCategories
+	return team:createInfobox()
+end
+
+function CustomTeam:createWidgetInjector()
+	return CustomInjector()
+end
+
+function CustomInjector:addCustomCells()
+	local widgets = {}
+	local statisticsCells = {
+		races = {order = 1, name = 'Races'},
+		wins = {order = 2, name = 'Wins'},
+		podiums = {order = 3, name = 'Podiums'},
+		poles = {order = 4, name = 'Pole positions'},
+		fastestlaps = {order = 5, name = 'Fastest Laps'},
+		points = {order = 6, name = 'Career Points'},
+		firstentry = {order = 7, name = 'First entry'},
+		firstwin = {order = 8, name = 'First win'},
+		lastentry = {order = 9, name = 'Last entry'},
+	}
+	if Table.any(_args, function(key) return statisticsCells[key] end) then
+		table.insert(widgets, Title{name = 'Team Statistics'})
+		local statisticsCellsOrder = function(tbl, a, b) return tbl[a].order < tbl[b].order end
+		for key, item in Table.iter.spairs(statisticsCells, statisticsCellsOrder) do
+			table.insert(widgets, Cell{name = item.name, content = {_args[key]}})
+		end
+	end
+	
+	if _args.academy then
+		table.insert(widgets, Title{name = 'Academy Team(s)'})
+		table.insert(widgets, Center{content = {_args.academy}})
+		table.insert(widgets, Center{content = {_args.academy2}})
+	end
+
+	if _args.previous or _args.next then
+		Array.appendWith(
+			widgets,
+			Title{name = 'Chronology'},
+			Chronology{content = {
+				previous = _args.previous,
+				previous2 = _args.previous2,
+				next = _args.next,
+				next2 = _args.next2,
+			}}
+		)
+	end
+
+	return widgets
+end
+
+function CustomTeam:addToLpdb(lpdbData, args)
+	lpdbData.region = Variables.varDefault('region', '')
+	lpdbData.extradata = lpdbData.extradata or {}
+	lpdbData.extradata.previous = args.previous
+	lpdbData.extradata.previous2 = args.previous2
+	lpdbData.extradata.next = args.next
+	lpdbData.extradata.next2 = args.next2
+	
+	return lpdbData
+end
+
+return CustomTeam
