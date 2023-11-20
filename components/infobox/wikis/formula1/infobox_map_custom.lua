beadd1c1ae2814a15b6be7aa9ee73636ca49d78d
Hesketh2 <88981446+Hesketh2@users.noreply.github.com>
2023-11-20 16:26:53 +0700

    1st update

--- components/infobox/wikis/formula1/infobox_map_custom.lua
+++ components/infobox/wikis/formula1/infobox_map_custom.lua
@@ -6,6 +6,7 @@
 -- Please see https://github.com/Liquipedia/Lua-Modules to contribute
 --
 
+local Array = require('Module:Array')
 local Class = require('Module:Class')
 local Lua = require('Module:Lua')
 
@@ -21,78 +22,39 @@ local CustomInjector = Class.new(Injector)
 
 local _args
 
+---@param frame Frame
+---@return Html
 function CustomMap.run(frame)
 	local customMap = Map(frame)
 	customMap.createWidgetInjector = CustomMap.createWidgetInjector
-	customMap.addToLpdb = CustomMap.addToLpdb
 	_args = customMap.args
 	return customMap:createInfobox()
 end
 
+---@return WidgetInjector
 function CustomMap:createWidgetInjector()
 	return CustomInjector()
 end
 
+---@param widgets Widget[]
+---@return Widget[]
 function CustomInjector:addCustomCells(widgets)
-	table.insert(widgets, Cell{
-		name = 'Architect',
-		content = {_args.architect}
-	})
-	table.insert(widgets, Cell{
-		name = 'Capacity',
-		content = {_args.capacity}
-	})
-	table.insert(widgets, Cell{
-		name = 'Opened',
-		content = {_args.opened}
-	})
-	table.insert(widgets, Cell{
-		name = 'Closed',
-		content = {_args.closed}
-	})
-	table.insert(widgets, Cell{
-		name = 'Turns',
-		content = {_args.turns}
-	})
-	table.insert(widgets, Cell{
-		name = 'Laps',
-		content = {_args.laps}
-	})
-	table.insert(widgets, Cell{
-		name = 'Direction',
-		content = {_args.direction}
-	})
-	table.insert(widgets, Cell{
-		name = 'Length',
-		content = {_args.length}
-	})
-	table.insert(widgets, Cell{
-		name = 'First Race',
-		content = {_args.debut}
-	})
-	table.insert(widgets, Cell{
-		name = 'Last Race',
-		content = {_args.lastrace}
-	})
-	table.insert(widgets, Cell{
-		name = 'Most wins (drivers)',
-		content = {_args.driverwin}
-	})
-	table.insert(widgets, Cell{
-		name = 'Most wins (teams)',
-		content = {_args.teamwin}
-	})
-	table.insert(widgets, Cell{
-		name = 'Lap Record',
-		content = {_args.laprecord}
-	})
-	table.insert(widgets, Cell{
-		name = 'Span',
-		content = {_args.span}
-	})
-	return widgets
+	return Array.appendWith(widgets,
+		Cell{name = 'Architect', content = {_args.architect}},
+		Cell{name = 'Capacity', content = {_args.capacity}},
+		Cell{name = 'Location', content = {_args.circuitlocation}},
+		Cell{name = 'Opened', content = {_args.opened}},
+		Cell{name = 'Turns', content = {_args.turns}},
+		Cell{name = 'Laps', content = {_args.laps}},
+		Cell{name = 'Direction', content = {_args.direction}},
+		Cell{name = 'Length', content = {_args.length}},
+		Cell{name = 'Debut', content = {_args.debut}},
+		Cell{name = 'Last Race', content = {_args.lastrace}},
+		Cell{name = 'Most wins (drivers)', content = {_args.driverwin}},
+		Cell{name = 'Most wins (teams)', content = {_args.teamwin}},
+		Cell{name = 'Lap Record', content = {_args.laprecord}},
+		Cell{name = 'Span', content = {_args.span}}
+	)
 end
-
-
 
 return CustomMap
