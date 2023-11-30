e10a22532af9595da510da088ab48178a258ba11
Verus <145709809+Verus-Vales@users.noreply.github.com>
2023-11-30 21:38:37 +0000

    Cleanup Infobox infobox_company_custom

--- components/infobox/wikis/formula1/infobox_company_custom.lua
+++ components/infobox/wikis/formula1/infobox_company_custom.lua
@@ -9,55 +9,23 @@
 local Array = require('Module:Array')
 local Class = require('Module:Class')
 local Lua = require('Module:Lua')
-local Template = require('Module:Template')
-local Variables = require('Module:Variables')
-local Table = require('Module:Table')
-local Company = require('Module:Infobox/Company')
-local Injector = require('Module:Infobox/Widget/Injector')
-local Cell = require('Module:Infobox/Widget/Cell')
+
+local Injector = Lua.import('Module:Infobox/Widget/Injector', {requireDevIfEnabled = true})
+local Company = Lua.import('Module:Infobox/Company', {requireDevIfEnabled = true})
 
 local CustomCompany = Class.new()
 
 local Widgets = require('Module:Infobox/Widget/All')
 local Cell = Widgets.Cell
-local Header = Widgets.Header
 local Title = Widgets.Title
-local Center = Widgets.Center
-local Customizable = Widgets.Customizable
-local Builder = Widgets.Builder
-local Chronology = Widgets.Chronology
 
 local CustomTeam = Class.new()
 local CustomInjector = Class.new(Injector)
-local Chronology = Widgets.Chronology
 
-local _args 
-local _team
+local _args
 
-
-function CustomInjector:addCustomCells()
-	local widgets = {}
-	local statisticsCells = {
-		president = {order = 1, name = 'President'},
-		deputypresidentmobility = {order = 2, name = 'Deputy President of Mobility'},
-		deputypresidentsport = {order = 3, name = 'Deputy President of Sport'},
-		senatepresident = {order = 4, name = 'Senate President'},
-		chiefexecutiveofficer = {order = 5, name = 'Chief Executive Officer'},
-		singleseaterdirector = {order = 6, name = 'Single-Seater Director'},
-		circuitsportdirector = {order = 7, name = 'Circuit Sport Director'},
-		roadsportdirector = {order = 8, name = 'Road Sport Director'},
-	}
-	if Table.any(_args, function(key) return statisticsCells[key] end) then
-		table.insert(widgets, Title{name = 'Staff Information'})
-		local statisticsCellsOrder = function(tbl, a, b) return tbl[a].order < tbl[b].order end
-		for key, item in Table.iter.spairs(statisticsCells, statisticsCellsOrder) do
-			table.insert(widgets, Cell{name = item.name, content = {_args[key]}})
-		end
-	end
-	
-	return widgets
-end
-
+---@param frame Frame
+---@return Html
 function CustomCompany.run(frame)
 	local company = Company(frame)
 	company.createWidgetInjector = CustomCompany.createWidgetInjector
@@ -65,8 +33,34 @@ function CustomCompany.run(frame)
 	return company:createInfobox(frame)
 end
 
+---@return WidgetInjector
 function CustomCompany:createWidgetInjector()
 	return CustomInjector()
+end
+
+---@param widgets Widget[]
+---@return Widget[]
+function CustomInjector:addCustomCells(widgets)
+	local staffInfoCells = {
+		{key = 'president', name = 'President'},
+		{key = 'deputypresidentmobility', name = 'Deputy President of Mobility'},
+		{key = 'deputypresidentsport', name = 'Deputy President of Sport'},
+		{key = 'senatepresident', name = 'Senate President'},
+		{key = 'chiefexecutiveofficer', name = 'Chief Executive Officer'},
+		{key = 'singleseaterdirector', name = 'Single-Seater Director'},
+		{key = 'circuitsportdirector', name = 'Circuit Sport Director'},
+		{key = 'roadsportdirector', name = 'Road Sport Director'},
+	}
+	if Array.all(staffInfoCells, function(cellData) return not _args[cellData.key] end) then
+		return widgets
+	end
+
+	return Array.extendWith(widgets,
+		{Title{name = 'Staff Information'}},
+		Array.map(staffInfoCells, function(cellData)
+			return Cell{name = cellData.name, content = {_args[cellData.key]}}
+		end)
+	)
 end
 
 return CustomCompany
