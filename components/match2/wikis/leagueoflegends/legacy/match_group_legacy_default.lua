752ff96bb379bef0b52a73766c1955b68973a986
LuckeLucky <officialluckelucky@gmail.com>
2023-12-06 15:15:14 +0000

    Remove nested match json

--- components/match2/wikis/leagueoflegends/legacy/match_group_legacy_default.lua
+++ components/match2/wikis/leagueoflegends/legacy/match_group_legacy_default.lua
@@ -34,9 +34,34 @@ function MatchGroupLegacyDefault.get(templateid, bracketType)
 		end
 	end
 
-	bracketData['$$match'] = {
-        ['$notEmpty$'] = 'match$1$',
-		data = 'match$1$'
+	-- add reference for map mappings
+	bracketData['$$map'] = {
+		['$notEmpty$'] = 'map$1$',
+		map = 'map$1$',
+		team1side = 'map$1$team1side',
+		t1c1 = 'map$1$t1c1',
+		t1c2 = 'map$1$t1c2',
+		t1c3 = 'map$1$t1c3',
+		t1c4 = 'map$1$t1c4',
+		t1c5 = 'map$1$t1c5',
+		t1b1 = 'map$1$t1b1',
+		t1b2 = 'map$1$t1b2',
+		t1b3 = 'map$1$t1b3',
+		t1b4 = 'map$1$t1b4',
+		t1b5 = 'map$1$t1b5',
+		team2side = 'map$1$team2side',
+		t2c1 = 'map$1$t2c1',
+		t2c2 = 'map$1$t2c2',
+		t2c3 = 'map$1$t2c3',
+		t2c4 = 'map$1$t2c4',
+		t2c5 = 'map$1$t2c5',
+		t2b1 = 'map$1$t2b1',
+		t2b2 = 'map$1$t2b2',
+		t2b3 = 'map$1$t2b3',
+		t2b4 = 'map$1$t2b4',
+		t2b5 = 'map$1$t2b5',
+		length = 'map$1$length',
+		winner = 'map$1$winner'
 	}
 
 	return bracketData
@@ -197,7 +222,7 @@ parameter format of the old bracket
 function MatchGroupLegacyDefault.addMaps(match)
 	for mapIndex = 1, MAX_NUM_MAPS do
 		match['map' .. mapIndex] = {
-			['$ref$'] = 'match',
+			['$ref$'] = 'map',
 			['$1$'] = mapIndex
 		}
 	end
