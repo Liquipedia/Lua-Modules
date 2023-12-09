6ca2b735a31110d0a01796ff9f3ab12b41362027
Martin B <theimarbot@gmail.com>
2023-12-09 04:10:25 +0000

    fix for inverted logic in other PR

--- components/match2/commons/match_group_input.lua
+++ components/match2/commons/match_group_input.lua
@@ -490,7 +490,7 @@ function MatchGroupInput.readPlayersOfTeam(match, opponentIndex, teamName, optio
 			local joinDate = DateExt.readTimestamp(Variables.varDefault(varPrefix .. 'joindate', ''))
 			local leaveDate = DateExt.readTimestamp(Variables.varDefault(varPrefix .. 'leavedate', ''))
 			if joinDate or leaveDate then
-				local timestampLocal = match.timestamp - DateExt.getOffsetSeconds(match.timezoneOffset or '')
+				local timestampLocal = match.timestamp + DateExt.getOffsetSeconds(match.timezoneOffset or '')
 				wasPresentInMatch = (not joinDate or (joinDate <= timestampLocal)) and
 					(not leaveDate or (leaveDate > timestampLocal))
 			end
