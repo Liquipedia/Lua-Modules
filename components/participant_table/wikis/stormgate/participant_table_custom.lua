50155e52d728ecd77c9ac52e07377c2cd40a9f39
Hesketh2 <88981446+Hesketh2@users.noreply.github.com>
2023-12-11 15:23:15 +0700

    Add Participant Table for Stormgate (#3604)

---
+++ components/participant_table/wikis/stormgate/participant_table_custom.lua
@@ -0,0 +1,11 @@
+---
+-- @Liquipedia
+-- wiki=stormgate
+-- page=Module:ParticipantTable/Custom
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Lua = require('Module:Lua')
+
+return Lua.import('Module:ParticipantTable/Starcraft', {requireDevIfEnabled = true})
