39d289a74a05ff1d0f307a5f611147191e262b97
Martin B <theimarbot@gmail.com>
2023-12-11 13:34:23 +0000

    fix: Replace broken dota2 achievements template with Lua

--- components/infobox/wikis/dota2/infobox_team_custom.lua
+++ components/infobox/wikis/dota2/infobox_team_custom.lua
@@ -6,15 +6,29 @@
 -- Please see https://github.com/Liquipedia/Lua-Modules to contribute
 --
 
+local Array = require('Module:Array')
 local Class = require('Module:Class')
+local LeagueIcon = require('Module:LeagueIcon')
 local Lua = require('Module:Lua')
 local RoleOf = require('Module:RoleOf')
+local String = require('Module:StringUtils')
+local Table = require('Module:Table')
+local TeamTemplates = require('Module:Team')
 local Template = require('Module:Template')
 local Variables = require('Module:Variables')
 
 local Team = Lua.import('Module:Infobox/Team', {requireDevIfEnabled = true})
+local Injector = Lua.import('Module:Infobox/Widget/Injector', {requireDevIfEnabled = true})
 
 local CustomTeam = Class.new()
+local CustomInjector = Class.new(Injector)
+
+local Widgets = require('Module:Infobox/Widget/All')
+local Title = Widgets.Title
+local Center = Widgets.Center
+local Builder = Widgets.Builder
+
+local _team
 
 function CustomTeam.run(frame)
 	local team = Team(frame)
@@ -24,17 +38,17 @@ function CustomTeam.run(frame)
 	team.args.dotabuff = team.args.teamid
 	team.args.stratz = team.args.teamid
 
-	-- Automatic achievements
-	team.args.achievements = Template.expandTemplate(frame, 'Team achievements', {team.args.name})
-
 	-- Automatic org people
 	team.args.coach = RoleOf.get{role = 'Coach'}
 	team.args.director = RoleOf.get{role = 'Director'}
 	team.args.manager = RoleOf.get{role = 'Manager'}
 	team.args.captain = RoleOf.get{role = 'Captain'}
 
+	team.createWidgetInjector = CustomTeam.createWidgetInjector
 	team.createBottomContent = CustomTeam.createBottomContent
 	team.addToLpdb = CustomTeam.addToLpdb
+
+	_team = team
 
 	return team:createInfobox()
 end
@@ -57,6 +71,51 @@ function CustomTeam:createBottomContent()
 		)
 	end
 --]]
+end
+
+function CustomTeam:createWidgetInjector()
+	return CustomInjector()
+end
+
+function CustomInjector:parse(id, widgets)
+	if id == 'achievements' then
+		return {
+			Builder{
+				builder = function()
+					if String.isNotEmpty(_team.args.achievements) then
+						return {
+							Title{name = 'Achievements'},
+							Center{content = {_team.args.achievements}}
+						}
+					else
+						local achievements = mw.ext.LiquipediaDB.lpdb('placement', {
+							conditions = '[[placement::1]] AND [[liquipediatier::1]] AND ' ..
+								'[[liquipediatiertype::]] AND [[mode::team]] AND ' ..
+								'([[opponentname::' .. table.concat(
+									TeamTemplates.queryHistoricalNames(_team.teamTemplate.historicaltemplate),
+									']] OR [[opponentname::'
+								) .. ']])',
+							query = 'parent, tournament, date, icon, iconDark, series',
+							order = 'date asc',
+							limit = 500,
+						})
+						if Table.isNotEmpty(achievements) then
+							return {
+								Title{name = 'Achievements'},
+								Center{content = Array.flatMap(achievements, function(placement, index)
+										placement.link = placement.parent
+										placement.name = placement.tournament
+										placement.iconDark = placement.icondark
+										return {LeagueIcon.display(placement), (index ~= #achievements) and '&nbsp;' or nil}
+									end)}
+							}
+						end
+					end
+				end
+			}
+		}
+	end
+	return widgets
 end
 
 function CustomTeam:addToLpdb(lpdbData, args)
