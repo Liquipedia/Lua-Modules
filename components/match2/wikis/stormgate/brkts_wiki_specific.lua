b361a7b85e055ae56ad7d10d57b5a7ce794da30e
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2023-12-13 11:10:03 +0100

    Add stormgate match support via using sc/sc2 via commons (#3603)
    * add stormgate match support via using sc/sc2 via commons
    
    * update as per #3624

---
+++ components/match2/wikis/stormgate/brkts_wiki_specific.lua
@@ -0,0 +1,72 @@
+---
+-- @Liquipedia
+-- wiki=stormgate
+-- page=Module:Brkts/WikiSpecific
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local FnUtil = require('Module:FnUtil')
+local Lua = require('Module:Lua')
+local Table = require('Module:Table')
+
+local BaseWikiSpecific = Lua.import('Module:Brkts/WikiSpecific/Base', {requireDevIfEnabled = true})
+
+---@class StormgateBrktsWikiSpecific: BrktsWikiSpecific
+local WikiSpecific = Table.copy(BaseWikiSpecific)
+
+WikiSpecific.matchFromRecord = FnUtil.lazilyDefineFunction(function()
+	local StarcraftMatchGroupUtil = Lua.import('Module:MatchGroup/Util/Starcraft', {requireDevIfEnabled = true})
+	return StarcraftMatchGroupUtil.matchFromRecord
+end)
+
+WikiSpecific.processMatch = FnUtil.lazilyDefineFunction(function()
+	local InputModule = Lua.import('Module:MatchGroup/Input/Starcraft', {requireDevIfEnabled = true})
+	return InputModule.processMatch
+end)
+
+---@param matchGroupType string
+---@return function
+function WikiSpecific.getMatchGroupContainer(matchGroupType)
+	if matchGroupType == 'matchlist' then
+		local MatchList = Lua.import('Module:MatchGroup/Display/Matchlist', {requireDevIfEnabled = true})
+		return WikiSpecific.adjustMatchGroupContainerConfig(MatchList.MatchlistContainer)
+	end
+
+	local Bracket = Lua.import('Module:MatchGroup/Display/Bracket', {requireDevIfEnabled = true})
+	return WikiSpecific.adjustMatchGroupContainerConfig(Bracket.BracketContainer)
+end
+
+---@param displayContainer function
+---@return function
+function WikiSpecific.adjustMatchGroupContainerConfig(displayContainer)
+	local StarcraftMatchSummary = Lua.import('Module:MatchSummary/Starcraft', {requireDevIfEnabled = true})
+	return function(props, matches)
+		local config = Table.merge(props.config, {MatchSummaryContainer = StarcraftMatchSummary.MatchSummaryContainer})
+		return displayContainer(Table.merge(props, {config = config}), matches)
+	end
+end
+
+---@param displayMode string
+---@return function?
+function WikiSpecific.getMatchContainer(displayMode)
+	if displayMode == 'singleMatch' then
+		-- Single match, displayed flat on a page (no popup)
+		local SingleMatch = Lua.import(
+			'Module:MatchGroup/Display/SingleMatch/Starcraft',
+			{requireDevIfEnabled = true}
+		)
+		return SingleMatch.SingleMatchContainer
+	end
+end
+
+WikiSpecific.matchHasDetails = FnUtil.lazilyDefineFunction(function()
+	local StarcraftMatchGroupUtil = Lua.import('Module:MatchGroup/Util/Starcraft', {requireDevIfEnabled = true})
+	return StarcraftMatchGroupUtil.matchHasDetails
+end)
+
+-- useless functions that should be present for some default checks
+-- would get called from Module:Match/Subobjects if we wouldn't circumvent that module completly
+WikiSpecific.processMap = FnUtil.identity
+
+return WikiSpecific
