aca19f0ad2b1257cb1c69a69c1d17eab0fb65912
Martin B <theimarbot@gmail.com>
2023-12-19 00:37:02 +0000

    Use new json function

--- components/match2/commons/match_group_input.lua
+++ components/match2/commons/match_group_input.lua
@@ -511,12 +511,12 @@ function MatchGroupInput.readPlayersOfTeam(match, opponentIndex, teamName, optio
 	end
 
 	---@return standardPlayer?
-	local getStandardPlayer = function(standardPlayerInput)
-		local playerJson = Json.parseIfTable(standardPlayerInput) or {}
+	local getStandardPlayer = function(playerData)
+		if not playerData then return end
 		local player = {
-			displayName = Logic.emptyOr(playerJson.displayName, playerJson.displayName, playerJson[1] or playerJson.name),
-			pageName = Logic.emptyOr(playerJson.pageName or playerJson.pageName or playerJson.link),
-			flag = playerJson.flag,
+			displayName = Logic.emptyOr(playerData.displayName, playerData.displayName, playerData[1] or playerData.name),
+			pageName = Logic.emptyOr(playerData.pageName or playerData.pageName or playerData.link),
+			flag = playerData.flag,
 		}
 		if Logic.isEmpty(player.displayName) then return end
 		player = PlayerExt.populatePlayer(player)
@@ -524,16 +524,18 @@ function MatchGroupInput.readPlayersOfTeam(match, opponentIndex, teamName, optio
 		return player
 	end
 
-	local substitutes = Json.parseIfTable(opponent.substitutes) or {}
+	local substitutions, parseFailure = Json.parseStringified(opponent.substitutes)
+	if parseFailure then
+		substitutions = {}
+	end
 
-	local readSubstitute = function(prefix, substituteInput)
-		local index = prefix:sub(4)
-		local substitute = getStandardPlayer(substituteInput)
-		if not substitute then return end
+	local handleSubstitution = function(substitution)
+		if type(substitution) ~= 'table' or not substitution['in'] then return end
+		local substitute = getStandardPlayer(substitution['in'])
 
-		local subbedGames = substitutes['games' .. index]
+		local subbedGames = substitution['games']
 
-		local player = getStandardPlayer(substitutes['player' .. index])
+		local player = getStandardPlayer(substitution['out'])
 		if player then
 			players[player.pageName] = subbedGames and players[player.pageName] or nil
 		end
@@ -543,16 +545,14 @@ function MatchGroupInput.readPlayersOfTeam(match, opponentIndex, teamName, optio
 			substitute = substitute,
 			player = player,
 			games = subbedGames and Array.map(mw.text.split(subbedGames, ';'), String.trim) or nil,
-			reason = substitutes['reason' .. index],
+			reason = substitution['reason'],
 		})
 
 		insertIntoPlayers(substitute)
 	end
 
-	--handle substitutes input for opponenets
-	for prefix, substituteOpponent in Table.iter.pairsByPrefix(substitutes, 'sub', {requireIndex = false}) do
-		readSubstitute(prefix, substituteOpponent)
-	end
+	--handle `substitutes` input for opponenets
+	Array.forEach(substitutions, handleSubstitution)
 
 	opponent.match2players = Array.extractValues(players)
 	Array.sortInPlaceBy(opponent.match2players, function (player)
