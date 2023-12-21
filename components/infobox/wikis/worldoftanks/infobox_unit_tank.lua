d1072dc41cd13c1be0fa2ff34f2ee8f316465e91
Cameron <58013431+FenrirWulf@users.noreply.github.com>
2023-12-21 10:08:21 -0800

    Update infobox_unit_tank.lua

--- components/infobox/wikis/worldoftanks/infobox_unit_tank.lua
+++ components/infobox/wikis/worldoftanks/infobox_unit_tank.lua
@@ -15,7 +15,6 @@ local TankTypes = require('Module:TankTypes')
 
 local Injector = Lua.import('Module:Infobox/Widget/Injector', {requireDevIfEnabled = true})
 local Unit = Lua.import('Module:Infobox/Unit', {requireDevIfEnabled = true})
-
 local Nation = Lua.import('Module:Infobox/Extension/Nation', {requireDevIfEnabled = true})
 
 local Widgets = require('Module:Infobox/Widget/All')
@@ -34,7 +33,6 @@ local CustomInjector = Class.new(Injector)
 function CustomUnit.run(frame)
 	local unit = Unit(frame)
 	_args = unit.args
-	unit.args.informationType = 'Tank'
 	unit.setLpdbData = CustomUnit.setLpdbData
 	unit.getWikiCategories = CustomUnit.getWikiCategories
 	unit.createWidgetInjector = CustomUnit.createWidgetInjector
@@ -47,41 +45,27 @@ function CustomInjector:addCustomCells(widgets)
 	return Array.append(widgets,
 		Cell{name = 'Released', content = {_args.released}},
 		Cell{name = 'Technical Name', content = {_args.techname}},
+		Cell{name = 'Tank Type', content = {CustomUnit._getTankType()}},
 		Cell{name = 'Tank Tier', content = {_args.tier}},
 		Cell{name = 'Nation', content = {Nation.run(_args.nation)}},
 		Cell{name = 'Role', content = {_args.role}}
 	)
 end
 
----@param id string
----@param widgets Widget[]
----@return Widget[]
-function CustomInjector:parse(id, widgets)
-	if id == 'type' then
-		return {
-			Cell{
-				name = 'Type',
-				content = {CustomUnit._getTankType()}
-			},
-		}
+---@return string[]
+function CustomUnit._getTankType()
+	if String.isEmpty(_args.tanktype) then
+		return {}
 	end
-	return widgets
+
+	local releasedate = _args.releasedate
+	local typeIcon = TankTypes.get{type = _args.tanktype, date = releasedate, size = 15}
+	return typeIcon .. ' [[' .. _args.tanktype .. ']]'
 end
 
 ---@return WidgetInjector
 function CustomUnit:createWidgetInjector()
 	return CustomInjector()
-end
-
----@return string[]
-function CustomUnit._getTankType()
-	if String.isEmpty(_args.type) then
-		return {}
-	end
-
-	local releasedate = _args.releasedate
-	local typeIcon = TankTypes.get{type = _args.type, date = releasedate, size = 15}
-	return typeIcon .. ' [[' .. _args.type .. ']]'
 end
 
 ---@param args table
@@ -99,15 +83,15 @@ end
 function CustomUnit:setLpdbData(args)
 	local lpdbData = {
 		name = args.name or _pagename,
-		type = args.type,
+		type = 'tank',
 		image = args.image,
 		date = args.released,
-		information = 'tank',
 		extradata = mw.ext.LiquipediaDB.lpdb_create_json{
 			technicalname = args.techname,
 			tanktier = args.tier,
 			nation = args.nation,
 			tankrole = args.role,
+			tanktype = args.tanktype
 		}
 	}
 
