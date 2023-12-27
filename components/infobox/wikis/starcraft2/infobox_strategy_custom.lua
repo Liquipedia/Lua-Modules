f473bed2a6432d99281269162ce0b145429683bb
hjpalpha <andreasbuesch1@gmail.com>
2023-12-27 16:08:10 +0100

    Strategy

--- components/infobox/wikis/starcraft2/infobox_strategy_custom.lua
+++ components/infobox/wikis/starcraft2/infobox_strategy_custom.lua
@@ -20,12 +20,9 @@ local Widgets = require('Module:Infobox/Widget/All')
 local Cell = Widgets.Cell
 local Header = Widgets.Header
 
-local CustomStrategy = Class.new()
+local CustomStrategy = Class.new(Strategy)
 
-local _strategy
-local _args
-
-local _RACE_MATCHUPS = {
+local RACE_MATCHUPS = {
 	'ZvZ', 'ZvP', 'ZvT',
 	'TvZ', 'TvP', 'TvT',
 	'PvZ', 'PvP', 'PvT',
@@ -36,54 +33,38 @@ local CustomInjector = Class.new(Injector)
 ---@param frame Frame
 ---@return Html
 function CustomStrategy.run(frame)
-	local customStrategy = Strategy(frame)
-	_strategy = customStrategy
-	_args = customStrategy.args
-	customStrategy.createWidgetInjector = CustomStrategy.createWidgetInjector
+	local customStrategy = CustomStrategy(frame)
+	customStrategy
+		:setWidgetInjector(CustomInjector(customStrategy))
+		:setWikiCategories()
+
 	return customStrategy:createInfobox()
-end
-
----@return WidgetInjector
-function CustomStrategy:createWidgetInjector()
-	return CustomInjector()
-end
-
----@param widgets Widget[]
----@return Widget[]
-function CustomInjector:addCustomCells(widgets)
-	Array.appendWith(
-		widgets,
-		Cell{name = 'Matchups', content = {_args.matchups or 'All'}},
-		Cell{name = 'Type', content = {_args.type or 'Opening'}},
-		Cell{name = 'Popularized by', content = {_args.popularized}, options = {makeLink = true}},
-		Cell{name = 'Converted Form', content = {_args.convert}},
-		Cell{name = 'TL-Article', content = {CustomStrategy:_getTLarticle(_args.tlarticle)}}
-	)
-
-	if Namespace.isMain() then
-		local categories = CustomStrategy:_getCategories(_args.race, _args.matchups)
-		_strategy.infobox:categories(unpack(categories))
-	end
-
-	return widgets
 end
 
 ---@param id string
 ---@param widgets Widget[]
 ---@return Widget[]
 function CustomInjector:parse(id, widgets)
+	local args = self.caller.args
 	if id == 'header' then
-		return {
-			Header{name = CustomStrategy:_getNameDisplay(), image = _args.image},
-		}
+		return {Header{name = self.caller:_getNameDisplay(), image = args.image}}
+	elseif id == 'custom' then
+		Array.appendWith(
+			widgets,
+			Cell{name = 'Matchups', content = {args.matchups or 'All'}},
+			Cell{name = 'Type', content = {args.type or 'Opening'}},
+			Cell{name = 'Popularized by', content = {args.popularized}, options = {makeLink = true}},
+			Cell{name = 'Converted Form', content = {args.convert}},
+			Cell{name = 'TL-Article', content = {self:_getTLarticle(args.tlarticle)}}
+		)
 	end
 	return widgets
 end
 
 ---@return string
 function CustomStrategy:_getNameDisplay()
-	local race = Faction.Icon{size = 'large', faction = _args.race} or ''
-	return race .. (_args.name or mw.title.getCurrentTitle().text)
+	local race = Faction.Icon{size = 'large', faction = self.args.race} or ''
+	return race .. (self.args.name or mw.title.getCurrentTitle().text)
 end
 
 ---@param tlarticle string?
@@ -96,34 +77,39 @@ function CustomStrategy:_getTLarticle(tlarticle)
 	end
 end
 
----@param race string?
----@param matchups string?
----@return table
-function CustomStrategy:_getCategories(race, matchups)
-	local categories = {}
+---@return self
+function CustomStrategy:setWikiCategories()
+	if not Namespace.isMain() then
+		return self
+	end
 
-	if String.isEmpty(matchups) then
+	self.infobox:categories(unpack(self:_getCategories()))
+
+	return self
+end
+
+---@return string[]
+function CustomStrategy:_getCategories()
+	local race = Faction.toName(Faction.read(self.args.race))
+
+	if not race then return {} end
+
+	local categoryType = self.args.informationType
+	if categoryType == 'Strategy' then
+		categoryType = 'Build Order'
+	end
+
+	local categories = {race .. ' ' .. categoryType .. 's'}
+
+	local matchups = self.args.matchups
+	if String.isEmpty(matchups) or categoryType ~= 'Build Order' then
 		return categories
 	end
 	---@cast matchups -nil
 
-	race = Faction.toName(Faction.read(race))
-	if not race then
-		return categories
-	end
-
-	local informationType = _args.informationType
-	if informationType == 'Strategy' then
-		informationType = 'Build Order'
-	end
-
-	table.insert(categories, race .. ' ' .. informationType .. 's')
-
-	if informationType == 'Build Order' then
-		for _, raceMatchupItem in ipairs(_RACE_MATCHUPS) do
-			if String.contains(matchups, raceMatchupItem) then
-				table.insert(categories, raceMatchupItem .. '_Builds')
-			end
+	for _, raceMatchupItem in ipairs(RACE_MATCHUPS) do
+		if String.contains(matchups, raceMatchupItem) then
+			table.insert(categories, raceMatchupItem .. ' Builds')
 		end
 	end
 
