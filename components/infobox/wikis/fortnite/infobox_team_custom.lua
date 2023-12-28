003c067cb4771476c2234668d4fa7572595e2954
hjpalpha <andreasbuesch1@gmail.com>
2023-12-28 13:53:52 +0100

    annos + 3669 + 3663

--- components/infobox/wikis/fortnite/infobox_team_custom.lua
+++ components/infobox/wikis/fortnite/infobox_team_custom.lua
@@ -27,70 +27,70 @@ local Comparator = Condition.Comparator
 local BooleanOperator = Condition.BooleanOperator
 local ColumnName = Condition.ColumnName
 
-local CustomTeam = Class.new()
+---@class FortniteInfoboxTeam: InfoboxTeam
+local CustomTeam = Class.new(Team)
 
-local _PLAYER_EARNINGS_ABBREVIATION = '<abbr title="Earnings of players while on the team">Player earnings</abbr>'
-local _MAXIMUM_NUMBER_OF_PLAYERS_IN_PLACEMENTS = 10
-local _EARNINGS_MODES = {team = 'team'}
-local _LANGUAGE = mw.language.new('en')
-
-local _team, _args, _earnings
+local PLAYER_EARNINGS_ABBREVIATION = '<abbr title="Earnings of players while on the team">Player earnings</abbr>'
+local MAXIMUM_NUMBER_OF_PLAYERS_IN_PLACEMENTS = 10
+local EARNINGS_MODES = {team = 'team'}
 
 local CustomInjector = Class.new(Injector)
 
+---@param frame Frame
+---@return Html
 function CustomTeam.run(frame)
 	local team = Team(frame)
-	_team = team
-	_args = team.args
-
-	team.addToLpdb = CustomTeam.addToLpdb
-	team.createWidgetInjector = CustomTeam.createWidgetInjector
+	team:setWidgetInjector(CustomInjector(team))
 
 	return team:createInfobox()
 end
 
-function CustomTeam:addToLpdb(lpdbData)
-	lpdbData.earnings = _earnings
+---@param lpdbData table
+---@param args table
+---@return table
+function CustomTeam:addToLpdb(lpdbData, args)
+	lpdbData.earnings = self.earnings
 
 	return lpdbData
 end
 
-function CustomTeam:createWidgetInjector()
-	return CustomInjector()
-end
-
+---@param id string
+---@param widgets Widget[]
+---@return Widget[]
 function CustomInjector:parse(id, widgets)
 	if id == 'earnings' then
 		local earningsWhileOnTeam
-		_earnings, earningsWhileOnTeam = CustomTeam.calculateEarnings(_args)
+		self.caller.earnings, earningsWhileOnTeam = self:calculateEarnings()
 		local earningsDisplay
-		if _earnings == 0 then
+		if self.caller.earnings == 0 then
 			earningsDisplay = nil
 		else
-			earningsDisplay = '$' .. _LANGUAGE:formatNum(_earnings)
+			earningsDisplay = '$' .. mw.language.new('en'):formatNum(self.caller.earnings)
 		end
 		local earningsFromPlayersDisplay
 		if earningsWhileOnTeam > 0 then
-			earningsFromPlayersDisplay = '$' .. _LANGUAGE:formatNum(earningsWhileOnTeam)
+			earningsFromPlayersDisplay = '$' .. mw.language.new('en'):formatNum(earningsWhileOnTeam)
 		end
 		return {
 			Cell{name = 'Approx. Total Winnings', content = {earningsDisplay}},
-			Cell{name = _PLAYER_EARNINGS_ABBREVIATION, content = {earningsFromPlayersDisplay}},
+			Cell{name = PLAYER_EARNINGS_ABBREVIATION, content = {earningsFromPlayersDisplay}},
 		}
 	end
 	return widgets
 end
 
-function CustomTeam.calculateEarnings(args)
+---@return number
+---@return number
+function CustomTeam:calculateEarnings()
 	if not Namespace.isMain() then
 		return 0, 0
 	end
 
-	local team = _team.pagename
+	local team = self.pagename
 	local query = 'individualprizemoney, prizemoney, players, date, mode'
 
 	local playerTeamConditions = ConditionTree(BooleanOperator.any)
-	for playerIndex = 1, _MAXIMUM_NUMBER_OF_PLAYERS_IN_PLACEMENTS do
+	for playerIndex = 1, MAXIMUM_NUMBER_OF_PLAYERS_IN_PLACEMENTS do
 		playerTeamConditions:add{
 			ConditionNode(ColumnName('players_p' .. playerIndex .. 'team'), Comparator.eq, team),
 		}
@@ -117,7 +117,7 @@ function CustomTeam.calculateEarnings(args)
 	local playerEarnings = 0
 
 	local processPlacement = function(placement)
-		earnings, playerEarnings = CustomTeam._addPlacementToEarnings(earnings, playerEarnings, placement)
+		earnings, playerEarnings = self:_addPlacementToEarnings(earnings, playerEarnings, placement)
 	end
 
 	Lpdb.executeMassQuery('placement', queryParameters, processPlacement)
@@ -129,7 +129,7 @@ function CustomTeam.calculateEarnings(args)
 	if Namespace.isMain() then
 		mw.ext.LiquipediaDB.lpdb_datapoint('total_earnings_players_while_on_team_' .. team, {
 				type = 'total_earnings_players_while_on_team',
-				name = _team.pagename,
+				name = self.pagename,
 				information = playerEarnings,
 		})
 	end
@@ -139,13 +139,18 @@ function CustomTeam.calculateEarnings(args)
 	return Math.round(earnings.team.total or 0), Math.round(playerEarnings or 0)
 end
 
-function CustomTeam._addPlacementToEarnings(earnings, playerEarnings, data)
+---@param earnings table
+---@param playerEarnings number
+---@param data placement
+---@return table
+---@return number
+function CustomTeam:_addPlacementToEarnings(earnings, playerEarnings, data)
 	local prizeMoney = data.prizemoney
 	data.players = data.players or {}
 	local mode = data.players.type or data.mode or ''
-	mode = _EARNINGS_MODES[mode]
+	mode = EARNINGS_MODES[mode]
 	if not mode then
-		prizeMoney = data.individualprizemoney * CustomTeam._amountOfTeamPlayersInPlacement(data.players)
+		prizeMoney = data.individualprizemoney * self:_amountOfTeamPlayersInPlacement(data.players)
 		playerEarnings = playerEarnings + prizeMoney
 		mode = 'other'
 	end
@@ -160,10 +165,12 @@ function CustomTeam._addPlacementToEarnings(earnings, playerEarnings, data)
 	return earnings, playerEarnings
 end
 
-function CustomTeam._amountOfTeamPlayersInPlacement(players)
+---@param players table
+---@return integer
+function CustomTeam:_amountOfTeamPlayersInPlacement(players)
 	local amount = 0
 	for playerKey in Table.iter.pairsByPrefix(players, 'p') do
-		if players[playerKey .. 'team'] == _team.pagename then
+		if players[playerKey .. 'team'] == self.pagename then
 			amount = amount + 1
 		end
 	end
