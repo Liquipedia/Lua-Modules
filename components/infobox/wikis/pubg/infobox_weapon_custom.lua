5e9922aedefea5696d9437db7a6e84a7876b4e58
Cameron <58013431+FenrirWulf@users.noreply.github.com>
2024-01-02 10:39:42 -0800

    PUBG
    WiP - Map display

--- components/infobox/wikis/pubg/infobox_weapon_custom.lua
+++ components/infobox/wikis/pubg/infobox_weapon_custom.lua
@@ -6,6 +6,7 @@
 -- Please see https://github.com/Liquipedia/Lua-Modules to contribute
 --
 
+local Array = require('Module:Array')
 local Class = require('Module:Class')
 local Lua = require('Module:Lua')
 local PageLink = require('Module:Page')
@@ -19,65 +20,33 @@ local Cell = Widgets.Cell
 local Title = Widgets.Title
 local Center = Widgets.Center
 
-local CustomWeapon = Class.new()
+local CustomWeapon = Class.new(Weapon)
 local CustomInjector = Class.new(Injector)
-
-local _weapon
-local _args
 
 ---@param frame Frame
 ---@return Html
 function CustomWeapon.run(frame)
-	local weapon = Weapon(frame)
-	_weapon = weapon
-	_args = _weapon.args
-	weapon.createWidgetInjector = CustomWeapon.createWidgetInjector
+	local weapon = CustomWeapon(frame)
+	weapon:setWidgetInjector(CustomInjector(weapon))
+
 	return weapon:createInfobox()
 end
 
----@return WidgetInjector
-function CustomWeapon:createWidgetInjector()
-	return CustomInjector()
-end
-
+---@param id string
 ---@param widgets Widget[]
 ---@return Widget[]
-function CustomInjector:addCustomCells(widgets)
-	local args = _args
-	table.insert(widgets, Cell{
-		name = 'Ammo Type',
-		content = {args.ammotype}
-	})
-	table.insert(widgets, Cell{
-		name = 'Throw Speed',
-		content = {args.throwspeed}
-	})
-	table.insert(widgets, Cell{
-		name = 'Throw Cooldown',
-		content = {args.throwcooldown}
-	})
-	if String.isNotEmpty(args.map1) then
-		local maps = {}
-
-		for _, map in ipairs(_weapon:getAllArgsForBase(args, 'map')) do
-			table.insert(maps, tostring(CustomWeapon:_createNoWrappingSpan(
-						PageLink.makeInternalLink({}, map)
-					)))
-
-		end
-		table.insert(widgets, Title{name = 'Maps'})
-		table.insert(widgets, Center{content = {table.concat(maps, '&nbsp;• ')}})
+function CustomInjector:parse(id, widgets)
+	local args = self.caller.args
+	if id == 'custom' then
+		Array.appendWith(
+			widgets,
+			Cell{name = 'Ammo Type', content = {args.ammotype}},
+			Cell{name = 'Throw Speed', content = {args.throwspeed}},
+			Cell{name = 'Throw Cooldown', content = {args.throwcooldown}},
+			Cell{name = 'Maps', content = {args.maps}}
+		)
 	end
 	return widgets
-end
-
----@param content string|number|Html|nil
----@return Html
-function CustomWeapon:_createNoWrappingSpan(content)
-	local span = mw.html.create('span')
-		:css('white-space', 'nowrap')
-		:node(content)
-	return span
 end
 
 return CustomWeapon
