68fb5f037960f0dac55d11fd8c89002053013287
Cameron <58013431+FenrirWulf@users.noreply.github.com>
2024-01-02 11:26:27 -0800

    Apex

--- components/infobox/wikis/apexlegends/infobox_weapon_custom.lua
+++ components/infobox/wikis/apexlegends/infobox_weapon_custom.lua
@@ -6,23 +6,20 @@
 -- Please see https://github.com/Liquipedia/Lua-Modules to contribute
 --
 
+local Array = require('Module:Array')
+local Weapon = require('Module:Infobox/Weapon')
 local Class = require('Module:Class')
-local Lua = require('Module:Lua')
 local String = require('Module:StringUtils')
+local Lua = require('Module:Lua')
+local Cell = require('Module:Infobox/Widget/Cell')
+local Title = require('Module:Infobox/Widget/Title')
+local Center = require('Module:Infobox/Widget/Center')
 
 local Injector = Lua.import('Module:Infobox/Widget/Injector', {requireDevIfEnabled = true})
 local Weapon = Lua.import('Module:Infobox/Weapon', {requireDevIfEnabled = true})
 
-local Widgets = require('Module:Infobox/Widget/All')
-local Cell = Widgets.Cell
-local Title = Widgets.Title
-local Center = Widgets.Center
-
-local CustomWeapon = Class.new()
+local CustomWeapon = Class.new(Weapon)
 local CustomInjector = Class.new(Injector)
-
-local _weapon
-local _args
 
 local MAGAZINE_INFO = {
 	{},
@@ -46,151 +43,118 @@ local DAMAGE_INFO = {
 }
 local NON_BREAKING_SPACE = '&nbsp;'
 
----@param frame Frame
----@return Html
 function CustomWeapon.run(frame)
-	local weapon = Weapon(frame)
-	_weapon = weapon
-	_args = _weapon.args
-	weapon.addToLpdb = CustomWeapon.addToLpdb
-	weapon.createWidgetInjector = CustomWeapon.createWidgetInjector
-	return weapon:createInfobox()
+	local weapon = CustomWeapon(frame)
+	weapon:setWidgetInjector(CustomInjector(weapon))
+
+	return weapon:createInfobox(frame)
 end
 
----@return WidgetInjector
-function CustomWeapon:createWidgetInjector()
-	return CustomInjector()
-end
-
+---@param id string
 ---@param widgets Widget[]
 ---@return Widget[]
-function CustomInjector:addCustomCells(widgets)
-	local args = _args
+function CustomInjector:parse(id, widgets)
+	local args = self.caller.args
 	if String.isNotEmpty(args.basedamage) and String.isEmpty(args.damage) then
 		local baseDamages = {}
-		for index, baseDamage in ipairs(_weapon:getAllArgsForBase(args, 'basedamage')) do
+		for index, baseDamage in ipairs(self:getAllArgsForBase(args, 'basedamage')) do
 			table.insert(baseDamages, CustomWeapon:_createContextualNoWrappingSpan(baseDamage, index, DAMAGE_INFO))
 		end
 		if String.isNotEmpty(args.basedamagenote) then
 			table.insert(baseDamages, CustomWeapon:_createContextualNote(args.basedamagenote))
 		end
-		table.insert(widgets, Cell{
-			name = 'Damage',
-			content = baseDamages
-		})
+		return {
+			Cell{name = 'Damage', content = baseDamages}
+		}
 	end
-
-	table.insert(widgets, Cell{
-		name = 'Rate of fire (Single)',
-		content = {args.rateoffiresingle}
-	})
-
-	table.insert(widgets, Cell{
-		name = 'Rate of fire (Auto)',
-		content = {args.rateoffireauto}
-	})
-
-	table.insert(widgets, Cell{
-		name = 'Rate of fire (Burst)',
-		content = {args.rateoffireburst}
-	})
 
 	if String.isNotEmpty(args.ratesoffire) and String.isEmpty(args.rateoffireauto) then
 		local rofTimes = {}
-		for index, rofTime in ipairs(_weapon:getAllArgsForBase(args, 'ratesoffire')) do
+		for index, rofTime in ipairs(self:getAllArgsForBase(args, 'ratesoffire')) do
 			table.insert(rofTimes, CustomWeapon:_createContextualNoWrappingSpan(rofTime, index, BOLT_INFO))
 		end
 		if String.isNotEmpty(args.ratesoffirenote) then
 			table.insert(rofTimes, CustomWeapon:_createContextualNote(args.ratesoffirenote))
 		end
-		table.insert(widgets, Cell{
-			name = 'Rates of Fire',
-			content = rofTimes
-		})
+		return {
+			Cell{name = 'Rates of Fire', content = rofTimes}
+		}
 	end
-
-	table.insert(widgets, Cell{
-		name = 'Projectile Speed',
-		content = {args.projectilespeed}
-	})
 
 	if String.isNotEmpty(args.ammocapacity) and String.isEmpty(args.ammocap) then
 		local ammoSizes = {}
-		for index, ammoSize in ipairs(_weapon:getAllArgsForBase(args, 'ammocapacity')) do
+		for index, ammoSize in ipairs(self:getAllArgsForBase(args, 'ammocapacity')) do
 			table.insert(ammoSizes, CustomWeapon:_createContextualNoWrappingSpan(ammoSize, index, MAGAZINE_INFO))
 		end
 		if String.isNotEmpty(args.ammocapacitynote) then
 			table.insert(ammoSizes, CustomWeapon:_createContextualNote(args.ammocapacitynote))
 		end
-		table.insert(widgets, Cell{
-			name = 'Ammo Capacity',
-			content = ammoSizes
-		})
+		return { 
+			Cell{name = 'Ammo Capacity', content = ammoSizes}
+		}
 	end
 
 	if String.isNotEmpty(args.reloadtime) and String.isEmpty(args.reloadspeed) then
 		local reloadTimes = {}
-		for index, reloadTime in ipairs(_weapon:getAllArgsForBase(args, 'reloadtime')) do
+		for index, reloadTime in ipairs(self:getAllArgsForBase(args, 'reloadtime')) do
 			table.insert(reloadTimes, CustomWeapon:_createContextualNoWrappingSpan(reloadTime, index, MAGAZINE_INFO))
 		end
 		if String.isNotEmpty(args.reloadtimenote) then
 			table.insert(reloadTimes, CustomWeapon:_createContextualNote(args.reloadtimenote))
 		end
-		table.insert(widgets, Cell{
-			name = 'Reload Speed',
-			content = reloadTimes
-		})
+		return {
+			Cell{name = 'Reload Speed', content = reloadTimes}
+		}
 	end
 
 	if String.isNotEmpty(args.ammotype) and String.isNotEmpty(args.ammotypeicon) then
-		table.insert(widgets, Cell{
-			name = 'Ammo Type',
-			content = {args.ammotypeicon .. ' ' .. args.ammotype}
-		})
+		return {
+			Cell{name = 'Ammo Type', content = {args.ammotypeicon .. ' ' .. args.ammotype}}
+		}
 	end
-
-	table.insert(widgets, Cell{
-		name = 'Range',
-		content = {args.range}
-	})
-
-	table.insert(widgets, Cell{
-		name = 'Ignition Time',
-		content = {args.ignitiontime}
-	})
-
-	table.insert(widgets, Cell{
-		name = 'Release Date',
-		content = {args.release}
-	})
 
 	if String.isNotEmpty(args.attachment) then
 		local attachments = {}
-		for index, attachment in ipairs(_weapon:getAllArgsForBase(args, 'attachment')) do
+		for index, attachment in ipairs(self:getAllArgsForBase(args, 'attachment')) do
 			table.insert(attachments, CustomWeapon:_createContextualNoWrappingSpan(attachment, index))
 		end
-		table.insert(widgets, Title{name = 'Attachment Slots'})
-		table.insert(widgets, Center{content = {table.concat(attachments, '&nbsp;&nbsp;')}})
+		return {
+			Title{name = 'Attachment Slots'},
+			Center{content = {table.concat(attachments, '&nbsp;&nbsp;')}}
+		}
 	end
 
 	if String.isNotEmpty(args.hopup) then
 		local hopups = {}
 		args.hopupdesc1 = args.hopupdesc1 or args.hopupdesc
-		for index, hopup in ipairs(_weapon:getAllArgsForBase(args, 'hopup')) do
+		for index, hopup in ipairs(self:getAllArgsForBase(args, 'hopup')) do
 			table.insert(hopups, CustomWeapon:_createContextualNoWrappingSpan(hopup, index))
 			table.insert(hopups, args['hopupdesc' .. index])
 		end
-		table.insert(widgets, Title{name = 'Hop-Ups'})
-		table.insert(widgets, Center{content = {table.concat(hopups, '<br>')}})
+		return { 
+			Title{name = 'Hop-Ups'},
+			Center{content = {table.concat(hopups, '<br>')}}
+		}
 	end
+
+	if id == 'custom' then
+		Array.appendWith(
+			widgets,
+			Cell{name = 'Rate of fire (Single)', content = {args.rateoffiresingle}},
+			Cell{name = 'Rate of fire (Auto)', content = {args.rateoffireauto}},
+			Cell{name = 'Rate of fire (Burst)', content = {args.rateoffireburst}},
+			Cell{name = 'Projectile Speed', content = {args.projectilespeed}},
+			Cell{name = 'Range', content = {args.range}},
+			Cell{name = 'Ignition Time', content = {args.ignitiontime}},
+			Cell{name = 'Release Date', content = {args.release}}
+		)
+	end
+
 	return widgets
 end
 
----@param content string
----@param index integer?
----@param lookUpTable {text: string, bgClass: string, textBgClass: string?}[]?
----@return string?
 function CustomWeapon:_createContextualNoWrappingSpan(content, index, lookUpTable)
+
 	if not lookUpTable then
 		local page = 'link=Portal:Attachments'
 		local icon = '[[File:Apex ATTM_' .. content .. '_lightmode.png|60px|'.. page ..'|class=show-when-light-mode]]'
@@ -239,4 +203,5 @@ function CustomWeapon:addToLpdb(lpdbData, args)
 	lpdbData.extradata.ammotype = args.ammotype
 	return lpdbData
 end
+
 return CustomWeapon
