892a77ff0b3cf4e13598cbe24699302c428efb97
hjpalpha <andreasbuesch1@gmail.com>
2024-01-03 13:00:03 +0100

    leagueoflegends

--- components/infobox/wikis/leagueoflegends/infobox_league_custom.lua
+++ components/infobox/wikis/leagueoflegends/infobox_league_custom.lua
@@ -6,6 +6,7 @@
 -- Please see https://github.com/Liquipedia/Lua-Modules to contribute
 --
 
+local Array = require('Module:Array')
 local Class = require('Module:Class')
 local Logic = require('Module:Logic')
 local Lua = require('Module:Lua')
@@ -13,55 +14,47 @@ local String = require('Module:StringUtils')
 local Variables = require('Module:Variables')
 
 local Injector = Lua.import('Module:Infobox/Widget/Injector', {requireDevIfEnabled = true})
-local League = Lua.import('Module:Infobox/League', {requireDevIfEnabled = true})
+local League = Lua.import('Module:Infobox/League/temp', {requireDevIfEnabled = true})
 
 local Widgets = require('Module:Infobox/Widget/All')
 local Cell = Widgets.Cell
 
-local _args
-
 local RIOT_ICON = '[[File:Riot Games Tier Icon.png|x12px|link=Riot Games|Premier Tournament held by Riot Games]]'
 
-local CustomLeague = Class.new()
+---@class LeagueoflegendsLeagueInfobox: InfoboxLeagueTemp
+local CustomLeague = Class.new(League)
 local CustomInjector = Class.new(Injector)
 
+---@param frame Frame
+---@return Html
 function CustomLeague.run(frame)
-	local league = League(frame)
-	_args = league.args
+	local league = CustomLeague(frame)
+	league:setWidgetInjector(CustomInjector(league))
 
-	_args.tickername = _args.tickername or _args.shortname
-
-
-	league.createWidgetInjector = CustomLeague.createWidgetInjector
-	league.defineCustomPageVariables = CustomLeague.defineCustomPageVariables
-	league.addToLpdb = CustomLeague.addToLpdb
-	league.liquipediaTierHighlighted = CustomLeague.liquipediaTierHighlighted
-	league.appendLiquipediatierDisplay = CustomLeague.appendLiquipediatierDisplay
+	league.args.tickername = league.args.tickername or league.args.shortname
 
 	return league:createInfobox()
 end
 
-function CustomLeague:createWidgetInjector()
-	return CustomInjector()
-end
+---@param id string
+---@param widgets Widget[]
+---@return Widget[]
+function CustomInjector:parse(id, widgets)
+	local args = self.caller.args
 
-function CustomInjector:addCustomCells(widgets)
-	local args = _args
-	table.insert(widgets, Cell{
-		name = 'Teams',
-		content = {args.team_number}
-	})
-	table.insert(widgets, Cell{
-		name = 'Players',
-		content = {args.participants_number}
-	})
-	table.insert(widgets, Cell{
-		name = 'Version',
-		content = {CustomLeague:_createPatchCell(args)}
-	})
+	if id == 'custom' then
+		Array.appendWith(widgets,
+			Cell{name = 'Teams', content = {args.team_number}},
+			Cell{name = 'Players', content = {args.participants_number}},
+			Cell{name = 'Version', content = {self.caller:_createPatchCell(args)}}
+		)
+	end
+
 	return widgets
 end
 
+---@param args table
+---@return string
 function CustomLeague:appendLiquipediatierDisplay(args)
 	if Logic.readBool(args.riotpremier) then
 		return ' ' .. RIOT_ICON
@@ -69,10 +62,15 @@ function CustomLeague:appendLiquipediatierDisplay(args)
 	return ''
 end
 
+---@param args table
+---@return boolean
 function CustomLeague:liquipediaTierHighlighted(args)
 	return Logic.readBool(args.riotpremier)
 end
 
+---@param lpdbData table
+---@param args table
+---@return table
 function CustomLeague:addToLpdb(lpdbData, args)
 	lpdbData.extradata.individual = String.isNotEmpty(args.participants_number) or
 			String.isNotEmpty(args.individual) and 'true' or ''
@@ -82,29 +80,33 @@ function CustomLeague:addToLpdb(lpdbData, args)
 	return lpdbData
 end
 
+---@param args table
+function CustomLeague:customParseArguments(args)
+	self.data.publishertier = Logic.readBool(args.riotpremier) and '1' or nil
+end
+
+---@param args table
 function CustomLeague:defineCustomPageVariables(args)
 	-- Custom Vars
 	Variables.varDefine('tournament_riot_premier', args.riotpremier)
 	Variables.varDefine('tournament_publisher_major', args.riotpremier)
-	Variables.varDefine('tournament_publishertier', Logic.readBool(args.riotpremier) and '1' or nil)
 
 	--Legacy vars
 	Variables.varDefine('tournament_ticker_name', args.tickername or '')
 	Variables.varDefine('tournament_tier', args.liquipediatier or '')
 	Variables.varDefine('tournament_tier_type', Variables.varDefault('tournament_liquipediatiertype'))
-	Variables.varDefine('tournament_mode', args.mode or '')
 
 	--Legacy date vars
-	local sdate = Variables.varDefault('tournament_startdate', '')
-	local edate = Variables.varDefault('tournament_enddate', '')
-	Variables.varDefine('tournament_sdate', sdate)
-	Variables.varDefine('tournament_edate', edate)
-	Variables.varDefine('tournament_date', edate)
-	Variables.varDefine('date', edate)
-	Variables.varDefine('sdate', sdate)
-	Variables.varDefine('edate', edate)
+	Variables.varDefine('tournament_sdate', self.data.startDate)
+	Variables.varDefine('tournament_edate', self.data.endDate)
+	Variables.varDefine('tournament_date', self.data.endDate)
+	Variables.varDefine('date', self.data.endDate)
+	Variables.varDefine('sdate', self.data.startDate)
+	Variables.varDefine('edate', self.data.endDate)
 end
 
+---@param args table
+---@return string?
 function CustomLeague:_createPatchCell(args)
 	if String.isEmpty(args.patch) then
 		return nil
