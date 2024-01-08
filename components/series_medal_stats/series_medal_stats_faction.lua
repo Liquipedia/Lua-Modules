bff9aab7fa5a6f229741cbfa53c272e8ed708ee2
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2024-01-08 09:40:04 +0100

    Add medal stats modules (#3659)
    * add medal stats modules
    
    * rename folder
    
    * add git deploy headers
    
    * as per review
    
    * add mergeIntoSemifinalists as per suggestion from review
    
    * adjust nonumber stuff
    
    * :P
    
    * Update components/series_medal_stats/series_medal_stats.lua
    
    Co-authored-by: Martin B <theimarbot@gmail.com>
    
    ---------
    
    Co-authored-by: Martin B <theimarbot@gmail.com>

---
+++ components/series_medal_stats/series_medal_stats_faction.lua
@@ -0,0 +1,64 @@
+---
+-- @Liquipedia
+-- wiki=commons
+-- page=Module:SeriesMedalStats/Faction
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Arguments = require('Module:Arguments')
+local Array = require('Module:Array')
+local Class = require('Module:Class')
+local Faction = require('Module:Faction')
+local Lua = require('Module:Lua')
+local Table = require('Module:Table')
+
+local OpponentLibraries = require('Module:OpponentLibraries')
+local Opponent = OpponentLibraries.Opponent
+
+local MedalStatsBase = Lua.import('Module:SeriesMedalStats', {requireDevIfEnabled = true})
+
+---@class SeriesMedalStatsFaction: SeriesMedalStats
+local MedalStats = Class.new(MedalStatsBase)
+
+function MedalStats.run(frame)
+	local args = Arguments.getArgs(frame)
+
+	--only query for solo opponents (only for them faction stats make sense)
+	args.opponentTypes = Opponent.solo
+
+	return MedalStats(args):query():create()
+end
+
+---@return Html?
+function MedalStats:create()
+	if Table.isEmpty(self.rawData) then return end
+
+	self:_processData()
+
+	local nameDisplay = function(identifier)
+		return Faction.Icon{faction = identifier, showLink = false} .. ' ' .. Faction.toName(identifier)
+	end
+
+	return self:defaultBuild(nameDisplay, self.args.title or 'Race', self.args.cutAfterPartial or 'Races')
+end
+
+function MedalStats:_processData()
+	---@param placement SeriesMedalStatsPlacementObject
+	---@return string?
+	local getIdentifier = function(placement)
+		return (placement.opponentplayers or {}).p1faction
+	end
+
+	self.data = {}
+
+	Array.forEach(self.rawData, function(placement)
+		return self:processByIdentifier(getIdentifier, placement)
+	end)
+
+	self.rawData = nil
+
+	self:sort()
+end
+
+return MedalStats
