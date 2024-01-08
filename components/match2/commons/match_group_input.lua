737e62dde9f86d318ffe9930760ad2a606cd5a51
hjpalpha <andreasbuesch1@gmail.com>
2024-01-08 11:52:07 +0100

    add patch to `getCommonTournamentVars`

--- components/match2/commons/match_group_input.lua
+++ components/match2/commons/match_group_input.lua
@@ -408,6 +408,7 @@ function MatchGroupInput.getCommonTournamentVars(obj, parent)
 	obj.tickername = Logic.emptyOr(obj.tickername, parent.tickername, Variables.varDefault('tournament_tickername'))
 	obj.tournament = Logic.emptyOr(obj.tournament, parent.tournament, Variables.varDefault('tournament_name'))
 	obj.type = Logic.emptyOr(obj.type, parent.type, Variables.varDefault('tournament_type'))
+	obj.patch = Logic.emptyOr(obj.patch, parent.patch, Variables.varDefault('tournament_patch'))
 
 	return obj
 end
