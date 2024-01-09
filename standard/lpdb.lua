1999d278514f4a0da03d673512e9f436a91981b7
Rikard Blixt <rikardblixt@gmail.com>
2024-01-09 18:46:52 +0100

    feat: create an lightweight ORM for saving

--- standard/lpdb.lua
+++ standard/lpdb.lua
@@ -6,9 +6,15 @@
 -- Please see https://github.com/Liquipedia/Lua-Modules to contribute
 --
 
+local Array = require('Module:Array')
+local Class = require('Module:Class')
+local FnUtil = require('Module:FnUtil')
+local Table = require('Module:Table')
+local TextSanitizer = require('Module:TextSanitizer')
+
 local Lpdb = {}
 
-local _MAXIMUM_QUERY_LIMIT = 5000
+local MAXIMUM_QUERY_LIMIT = 5000
 
 -- Executes a mass query.
 --[==[
@@ -19,6 +25,7 @@ Loops LPDB queries to e.g.
 	or additional limitations are reached
 
 example:
+```
 	local foundMatchIds = {}
 	local getMatchId = function(match)
 		if #foundMatchIds < args.matchLimit then
@@ -41,6 +48,7 @@ example:
 	Lpdb.executeMassQuery('match2', queryParameters, getMatchId)
 
 	return foundMatchIds
+```
 ]==]
 ---@param tableName string
 ---@param queryParameters table
@@ -48,7 +56,7 @@ example:
 ---@param limit number?
 function Lpdb.executeMassQuery(tableName, queryParameters, itemChecker, limit)
 	queryParameters.offset = queryParameters.offset or 0
-	queryParameters.limit = queryParameters.limit or _MAXIMUM_QUERY_LIMIT
+	queryParameters.limit = queryParameters.limit or MAXIMUM_QUERY_LIMIT
 	limit = limit or math.huge
 
 	while queryParameters.offset < limit do
@@ -68,5 +76,149 @@ function Lpdb.executeMassQuery(tableName, queryParameters, itemChecker, limit)
 		end
 	end
 end
+
+--- LPDB Object-Relational Mapping
+
+---@enum FieldTypes
+local FieldTypes = {
+	integer = 0,
+	string = 1,
+	page = 1,
+	boolean = 3,
+	date = 4,
+	datetime = 5,
+	array = 6,
+	table = 7,
+}
+
+---@alias ModelColumnData {name: string, fieldType: FieldTypes, default: any}
+
+---@class Model
+---@field tableName string
+---@field tableColumns ModelColumnData[]
+local Model = Class.new(function(self, name, columns)
+	self.tableName = name
+	self.tableColumns = columns
+end)
+
+---@class ModelRow
+---@field private tableName string
+---@field private tableColumns ModelColumnData[]
+---@field private fields table<string, any>
+---@field [any] any
+local ModelRow = Class.new(function(self, tableName, tableColumns)
+	rawset(self, 'tableName', tableName)
+	rawset(self, 'tableColumns', tableColumns)
+	rawset(self, 'fields', {})
+end)
+
+---@param initData table?
+---@return ModelRow
+function Model:new(initData)
+	local row = ModelRow(self.tableName, self.tableColumns)
+	if type(initData) == 'table' then
+		row:setMany(initData)
+	end
+	return row
+end
+
+---@private
+---@param columnData ModelColumnData
+function ModelRow:_validateField(columnData)
+	if not self.fields[columnData.name] then
+		error(self.tableName .. ' expects ' .. columnData.name .. ' to be set')
+	end
+	-- TODO: Verify types (at least when running tests)
+end
+
+---@private
+---@param columnData ModelColumnData
+function ModelRow:_prepareFieldForStorage(columnData)
+	-- Apply defaults
+	if not self.fields[columnData.name] then
+		if type(columnData.default) == 'function' then
+			self.fields[columnData.name] = columnData.default(self.fields)
+		else
+			self.fields[columnData.name] = columnData.default
+		end
+	end
+
+	-- Validate that all fields are correct
+	self:_validateField(columnData)
+end
+
+function ModelRow:save()
+	Array.forEach(self.tableColumns, FnUtil.curry(ModelRow._prepareFieldForStorage, self))
+	local objectName = Table.extract(self.fields, 'objectname')
+	mw.ext.LiquipediaDB['lpdb_' .. self.tableName](objectName, self.fields)
+end
+
+function ModelRow:__newindex(key, value)
+	-- Strip HTML from strings
+	local function stripHtml(str)
+		if type(str) == 'string' then
+			return TextSanitizer.stripHTML(str)
+		end
+		return str
+	end
+
+	if type(value) == 'table' then
+		value = Table.mapValues(value, stripHtml)
+	else
+		value = stripHtml(value)
+	end
+
+	self.fields[key] = value
+end
+
+function ModelRow:set(key, value)
+	self:__newindex(key, value)
+end
+
+function ModelRow:setMany(tbl)
+	Table.iter.forEachPair(tbl, FnUtil.curry(ModelRow.__newindex, self))
+end
+
+---@class Match2Model:Model
+Lpdb.Match2 = Model('match2', {
+	{
+		name = 'objectname',
+		fieldType = 'string',
+		default = function(fields)
+			return fields.match2id
+		end
+	},
+	{name = 'match2id', fieldType = 'string'},
+	{name = 'match2bracketid', fieldType = 'struct'},
+	{name = 'winner', fieldType = 'string', default = ''},
+	{name = 'walkover', fieldType = 'string', default = ''},
+	{name = 'resulttype', fieldType = 'string', default = ''},
+	{name = 'finished', fieldType = 'number', default = 0},
+	{name = 'mode', fieldType = 'string', default = ''},
+	{name = 'type', fieldType = 'string', default = ''},
+	{name = 'section', fieldType = 'string', default = ''},
+	{name = 'game', fieldType = 'string', default = ''},
+	{name = 'patch', fieldType = 'string', default = ''},
+	{name = 'date', fieldType = 'string', default = 0},
+	{name = 'dateexact', fieldType = 'number', default = 0},
+	{name = 'stream', fieldType = 'struct', default = {}},
+	{name = 'links', fieldType = 'struct', default = {}},
+	{name = 'bestof', fieldType = 'number', default = 0},
+	{name = 'vod', fieldType = 'string', default = ''},
+	{name = 'tournament', fieldType = 'string', default = ''},
+	{name = 'parent', fieldType = 'pagename', default = ''},
+	{name = 'tickername', fieldType = 'string', default = ''},
+	{name = 'shortname', fieldType = 'string', default = ''},
+	{name = 'series', fieldType = 'string', default = ''},
+	{name = 'icon', fieldType = 'string', default = ''},
+	{name = 'icondark', fieldType = 'string', default = ''},
+	{name = 'liquipediatier', fieldType = 'string|number', default = ''},
+	{name = 'liquipediatiertype', fieldType = 'string', default = ''},
+	{name = 'publishertier', fieldType = 'string', default = ''},
+	{name = 'extradata', fieldType = 'struct', default = {}},
+	{name = 'match2bracketdata', fieldType = 'struct', default = {}},
+	{name = 'match2opponents', fieldType = 'array', default = {}},
+	{name = 'match2games', fieldType = 'array', default = {}},
+})
 
 return Lpdb
