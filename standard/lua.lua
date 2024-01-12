c50e1a2c00b8b508f6acf24a7f2c82d9f8d1c051
Rikard Blixt <rikardblixt@gmail.com>
2024-01-12 11:27:47 +0100

    refactor: default `requireDevIfEnabled` to true in Lua.import (#3766)
    * refactor: enable `requireDevIfEnabled` by default for `lua.import`
    
    * typo

--- standard/lua.lua
+++ standard/lua.lua
@@ -56,16 +56,16 @@ function Lua.loadDataIfExists(name)
 end
 
 ---Imports a module by its name.
----
----Allows requireDevIfEnabled option (requires the development version of a module if it
----exists and the dev feature flag is enabled. Otherwise requires the non-development module).
+---By default it will include the /dev module if in dev mode activated. This can be turned off by setting
+--- the requireDevIfEnabled option to false.
+--- Optionally mw.loaddata can be used instead of require by passing the loadData option.
 ---@param name string
 ---@param options {requireDevIfEnabled: boolean?, loadData: boolean?}?
 ---@return unknown
 function Lua.import(name, options)
 	options = options or {}
 	local importFunction = options.loadData and mw.loadData or require
-	if options.requireDevIfEnabled then
+	if options.requireDevIfEnabled ~= false then
 		if StringUtils.endsWith(name, '/dev') then
 			error('Lua.import: Module name should not end in \'/dev\'')
 		end
