ed98287642e0de5853bddea168e4af561e10b164
hjpalpha <andreasbuesch1@gmail.com>
2024-01-12 15:16:52 +0100

    freefire

--- components/infobox/wikis/freefire/infobox_team_custom.lua
+++ components/infobox/wikis/freefire/infobox_team_custom.lua
@@ -12,8 +12,8 @@ local PlacementStats = require('Module:InfoboxPlacementStats')
 local Template = require('Module:Template')
 local Variables = require('Module:Variables')
 
-local Injector = Lua.import('Module:Infobox/Widget/Injector', {requireDevIfEnabled = true})
-local Team = Lua.import('Module:Infobox/Team', {requireDevIfEnabled = true})
+local Injector = Lua.import('Module:Infobox/Widget/Injector')
+local Team = Lua.import('Module:Infobox/Team')
 
 local Widgets = require('Module:Infobox/Widget/All')
 local Cell = Widgets.Cell
