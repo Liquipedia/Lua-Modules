0a15310183aa57cc8ac15597e0d03e486c90577a
hjpalpha <andreasbuesch1@gmail.com>
2024-01-12 14:54:28 +0100

    val

--- components/infobox/wikis/valorant/infobox_team_custom.lua
+++ components/infobox/wikis/valorant/infobox_team_custom.lua
@@ -11,8 +11,8 @@ local Lua = require('Module:Lua')
 local Template = require('Module:Template')
 local Variables = require('Module:Variables')
 
-local Injector = Lua.import('Module:Infobox/Widget/Injector', {requireDevIfEnabled = true})
-local Team = Lua.import('Module:Infobox/Team', {requireDevIfEnabled = true})
+local Injector = Lua.import('Module:Infobox/Widget/Injector')
+local Team = Lua.import('Module:Infobox/Team')
 
 local Widgets = require('Module:Infobox/Widget/All')
 local Cell = Widgets.Cell
