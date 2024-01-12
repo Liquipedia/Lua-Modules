af0647c57adda6586af0d401927e181fbf16d257
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2024-01-12 15:46:16 +0100

    requireDevIfEnabled

--- components/match2/wikis/clashofclans/match_group_input_custom.lua
+++ components/match2/wikis/clashofclans/match_group_input_custom.lua
@@ -16,7 +16,7 @@ local String = require('Module:StringUtils')
 local Table = require('Module:Table')
 local Variables = require('Module:Variables')
 
-local MatchGroupInput = Lua.import('Module:MatchGroup/Input', {requireDevIfEnabled = true})
+local MatchGroupInput = Lua.import('Module:MatchGroup/Input')
 local Opponent = Lua.import('Module:Opponent', {requireDevIfEnabled = true})
 
 local ALLOWED_STATUSES = {'W', 'FF', 'DQ', 'L', 'D'}
