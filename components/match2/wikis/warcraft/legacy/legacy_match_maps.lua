b05e49cdf93d345de7de92ed6da341e206cd14b8
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2024-01-14 13:40:52 +0100

    Fix: warcraft legacy match maps filterByKey condition

--- components/match2/wikis/warcraft/legacy/legacy_match_maps.lua
+++ components/match2/wikis/warcraft/legacy/legacy_match_maps.lua
@@ -121,7 +121,9 @@ function LegacyMatchMaps._readMaps(args)
 
 	for mapIndex = 1, MAX_NUM_MAPS do
 		local prefix = 'map' .. mapIndex
-		local map = Table.filterByKey(args, function(key) return String.startsWith(key, prefix) end)
+		local map = Table.filterByKey(args, function(key)
+			return key == prefix or string.find(key, '^' .. prefix .. '[^%d]')
+		end)
 		map = Table.map(map, function(key, value)
 			args[key] = nil
 
