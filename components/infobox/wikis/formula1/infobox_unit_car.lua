6b2acec8ccc8a2bee715229d8ad844b9ad249601
Cameron <58013431+FenrirWulf@users.noreply.github.com>
2024-01-15 12:52:35 -0800

    Damn whitespace

--- components/infobox/wikis/formula1/infobox_unit_car.lua
+++ components/infobox/wikis/formula1/infobox_unit_car.lua
@@ -39,7 +39,7 @@ end
 function CustomInjector:parse(id, widgets)
 	local args = self.caller.args
 	if id == 'custom' then
-		Array.appendWith(widgets, 
+		Array.appendWith(widgets,
 			Cell{name = 'Team', content = {args.team}},
 			Cell{name = 'Designer', content = {args.designer}},
 			Cell{name = 'Season(s)', content = {args.season}},
