fb055745e204e0815d6ba505bbff61aadc64d04a
Cameron <58013431+FenrirWulf@users.noreply.github.com>
2024-01-18 12:25:16 -0800

    Create Smite Info

---
+++ standard/info/wikis/smite/info.lua
@@ -0,0 +1,45 @@
+---
+-- @Liquipedia
+-- wiki=smite
+-- page=Module:Info
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+return {
+	startYear = 2013,
+	wikiName = 'smite',
+	name = 'SMITE',
+	games = {
+		smite = {
+			abbreviation = 'S1',
+			name = 'SMITE',
+			link = 'SMITE',
+			logo = {
+				darkMode = 'SMITE default darkmode.png',
+				lightMode = 'SMITE default lightmode.png',
+			},
+			defaultTeamLogo = {
+				darkMode = 'SMITE default darkmode.png',
+				lightMode = 'SMITE default lightmode.png',
+			},
+		},
+		['smite 2'] = {
+			abbreviation = 'S2',
+			name = 'SMITE 2',
+			link = 'SMITE 2',
+			logo = {
+				darkMode = 'SMITE 2 default allmode.png',
+				lightMode = 'SMITE 2 default allmode.png',
+			},
+			defaultTeamLogo = {
+				darkMode = 'SMITE default darkmode.png',
+				lightMode = 'SMITE default lightmode.png',
+			},
+		},
+	},
+	defaultGame = 'smite',
+	defaultTeamLogo = 'SMITE default lightmode.png', ---@deprecated
+	defaultTeamLogoDark = 'SMITE default darkmode.png', ---@deprecated
+	match2 = 2,
+}
