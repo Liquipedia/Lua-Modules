83793419272e726f4bffd361d26c26a929d01066
Cameron <58013431+FenrirWulf@users.noreply.github.com>
2024-01-18 13:02:56 -0800

    Removing unused variable

--- components/infobox/wikis/valorant/infobox_team_custom.lua
+++ components/infobox/wikis/valorant/infobox_team_custom.lua
@@ -8,7 +8,6 @@
 
 local Class = require('Module:Class')
 local Lua = require('Module:Lua')
-local TeamTemplate = require('Module:Team')
 local Template = require('Module:Template')
 
 local Injector = Lua.import('Module:Infobox/Widget/Injector', {requireDevIfEnabled = true})
