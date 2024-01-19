b4ec1126a5fe57f527587b0164cd1ffdc60aa6ad
hjpalpha <andreasbuesch1@gmail.com>
2024-01-19 12:59:28 +0100

    commons

--- components/infobox/commons/infobox_building.lua
+++ components/infobox/commons/infobox_building.lua
@@ -99,13 +99,11 @@ function Building:createInfobox()
 	infobox:categories('Buildings')
 	infobox:categories(unpack(self:getWikiCategories(args)))
 
-	local builtInfobox = infobox:widgetInjector(self:createWidgetInjector()):build(widgets)
-
 	if Namespace.isMain() then
 		self:setLpdbData(args)
 	end
 
-	return builtInfobox
+	return infobox:build(widgets)
 end
 
 ---@param args table
