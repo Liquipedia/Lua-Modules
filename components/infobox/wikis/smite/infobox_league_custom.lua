1b9762aa3cc000069338243a8398f7e38a72fd86
Cameron <58013431+FenrirWulf@users.noreply.github.com>
2024-01-19 10:33:15 -0800

    Anno cleanup

--- components/infobox/wikis/smite/infobox_league_custom.lua
+++ components/infobox/wikis/smite/infobox_league_custom.lua
@@ -17,7 +17,7 @@ local League = Lua.import('Module:Infobox/League')
 local Widgets = require('Module:Infobox/Widget/All')
 local Cell = Widgets.Cell
 
----@class ArenaofvalorLeagueInfobox: InfoboxLeagueTemp
+---@class SmiteLeagueInfobox: InfoboxLeague
 local CustomLeague = Class.new(League)
 local CustomInjector = Class.new(Injector)
 
