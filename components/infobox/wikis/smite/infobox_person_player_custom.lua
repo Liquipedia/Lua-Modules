3d4a84f165af2add536ea31a80d1ce9fdbd623a6
Hesketh2 <88981446+Hesketh2@users.noreply.github.com>
2024-01-24 22:55:25 +0700

    Rework for Role stuff
    My attempt is a port from LoL wiki for the needed stuff
    
    Notes
    - It looks like the "isPlayer" seems to be required, otherwise the Player Portal treat Coaches as a player (and not go into the Staff & Talents section)

--- components/infobox/wikis/smite/infobox_person_player_custom.lua
+++ components/infobox/wikis/smite/infobox_person_player_custom.lua
@@ -6,9 +6,12 @@
 -- Please see https://github.com/Liquipedia/Lua-Modules to contribute
 --
 
+local Array = require('Module:Array')
 local Class = require('Module:Class')
+local Logic = require('Module:Logic')
 local Lua = require('Module:Lua')
-local Role = require('Module:Role')
+local String = require('Module:StringUtils')
+local Page = require('Module:Page')
 local TeamHistoryAuto = require('Module:TeamHistoryAuto')
 
 local Injector = Lua.import('Module:Infobox/Widget/Injector')
@@ -16,6 +19,35 @@ local Player = Lua.import('Module:Infobox/Person')
 
 local Widgets = require('Module:Infobox/Widget/All')
 local Cell = Widgets.Cell
+
+local ROLES = {
+	-- Players
+	['solo'] = {category = 'Solo players', variable = 'Solo', isplayer = true},
+	['jungler'] = {category = 'Jungle players', variable = 'Jungler', isplayer = true},
+	['support'] = {category = 'Support players', variable = 'Support', isplayer = true},
+	['mid'] = {category = 'Mid Lane players', variable = 'Mid', isplayer = true},
+	['carry'] = {category = 'Carry players', variable = 'Carry', isplayer = true},
+
+	-- Staff and Talents
+	['analyst'] = {category = 'Analysts', variable = 'Analyst', isplayer = false},
+	['observer'] = {category = 'Observers', variable = 'Observer', isplayer = false},
+	['host'] = {category = 'Hosts', variable = 'Host', isplayer = false},
+	['journalist'] = {category = 'Journalists', variable = 'Journalist', isplayer = false},
+	['expert'] = {category = 'Experts', variable = 'Expert', isplayer = false},
+	['coach'] = {category = 'Coaches', variable = 'Coach', isplayer = false},
+	['caster'] = {category = 'Casters', variable = 'Caster', isplayer = false},
+	['talent'] = {category = 'Talents', variable = 'Talent', isplayer = false},
+	['manager'] = {category = 'Managers', variable = 'Manager', isplayer = false},
+	['producer'] = {category = 'Producers', variable = 'Producer', isplayer = false},
+	['admin'] = {category = 'Admins', variable = 'Admin', isplayer = false},
+	['streamer'] = {category = 'Streamers', variable = 'Streamer', isplayer = false},
+}
+ROLES['assistant coach'] = ROLES.coach
+ROLES['head coach'] = ROLES.coach
+ROLES['jgl'] = ROLES.jungler
+ROLES['adc'] = ROLES.carry
+ROLES['sup'] = ROLES.support
+ROLES['top'] = ROLES.solo
 
 ---@class SmiteInfoboxPlayer: Person
 ---@field role table
@@ -34,20 +66,72 @@ function CustomPlayer.run(frame)
 		iconModule = 'Module:PositionIcon/data',
 		player = player.pagename
 	}
-	player.role = Role.run{role = player.args.role}
-	player.role2 = Role.run{role = player.args.role2}
+	player.role = player:_getRoleData(player.args.role)
+	player.role2 = player:_getRoleData(player.args.role2)
 
 	return player:createInfobox()
 end
 
 function CustomInjector:parse(id, widgets)
-	if id == 'region' then return {}
-	elseif id == 'role' then
+	local caller = self.caller
+
+	if id == 'role' then
 		return {
-			Cell{name = 'Role(s)', content = {self.caller.role.display, self.caller.role2.display}}
+			Cell{name = 'Role', content = {
+				caller:_displayRole(caller.role),
+				caller:_displayRole(caller.role2),
+			}},
 		}
 	end
 	return widgets
+end
+
+---@param role string?
+---@return {category: string, variable: string, isplayer: boolean?}?
+function CustomPlayer:_getRoleData(role)
+	return ROLES[(role or ''):lower()]
+end
+
+---@param roleData {category: string, variable: string, isplayer: boolean?}?
+---@return string?
+function CustomPlayer:_displayRole(roleData)
+	if not roleData then return end
+
+	return Page.makeInternalLink(roleData.variable, ':Category:' .. roleData.category)
+end
+
+---@param categories string[]
+---@return string[]
+function CustomPlayer:getWikiCategories(categories)
+	return Array.append(categories,
+		(self.role or {}).category,
+		(self.role2 or {}).category
+	)
+end
+
+---@param lpdbData table
+---@param args table
+---@param personType string
+---@return table
+function CustomPlayer:adjustLPDB(lpdbData, args, personType)
+	lpdbData.extradata.role = (self.role or {}).variable
+	lpdbData.extradata.role2 = (self.role2 or {}).variable
+	lpdbData.type = self:_isPlayerOrStaff()
+	return lpdbData
+end
+
+---@return string
+function CustomPlayer:_isPlayerOrStaff()
+	local roleData
+	if String.isNotEmpty(self.args.role) then
+		roleData = ROLES[self.args.role:lower()]
+	end
+	-- If the role is missing, assume it is a player
+	if roleData and roleData.isplayer == false then
+		return 'staff'
+	else
+		return 'player'
+	end
 end
 
 return CustomPlayer
