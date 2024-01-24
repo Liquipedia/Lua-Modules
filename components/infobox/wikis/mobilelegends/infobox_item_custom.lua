a0731a72d783e634a465f62a6b53cd0f4e705418
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2024-01-24 10:13:17 +0100

    Refactor infobox item (#3843)
    * commons
    
    * wildrift
    
    * mobilelegends
    
    * fixes from testing
    
    * Update components/infobox/wikis/mobilelegends/infobox_item_custom.lua
    
    Co-authored-by: Rikard Blixt <rikardblixt@gmail.com>
    
    * Update components/infobox/wikis/mobilelegends/infobox_item_custom.lua
    
    Co-authored-by: Rikard Blixt <rikardblixt@gmail.com>
    
    * Update components/infobox/wikis/mobilelegends/infobox_item_custom.lua
    
    Co-authored-by: Rikard Blixt <rikardblixt@gmail.com>
    
    * Update components/infobox/wikis/wildrift/infobox_item_custom.lua
    
    Co-authored-by: Rikard Blixt <rikardblixt@gmail.com>
    
    * from review + fixes
    
    ---------
    
    Co-authored-by: Rikard Blixt <rikardblixt@gmail.com>

--- components/infobox/wikis/mobilelegends/infobox_item_custom.lua
+++ components/infobox/wikis/mobilelegends/infobox_item_custom.lua
@@ -6,6 +6,7 @@
 -- Please see https://github.com/Liquipedia/Lua-Modules to contribute
 --
 
+local Array = require('Module:Array')
 local Class = require('Module:Class')
 local Icon = require('Module:Icon')
 local ItemIcon = require('Module:ItemIcon')
@@ -24,14 +25,11 @@ local Title = Widgets.Title
 local Center = Widgets.Center
 local Breakdown = Widgets.Breakdown
 
-local CustomItem = Class.new()
-
+---@class MobilelegendsItemInfobox: ItemInfobox
+local CustomItem = Class.new(Item)
 local CustomInjector = Class.new(Injector)
 
-local _args
-local _categories = {}
-
-local _CATEGORY_DISPLAY = {
+local CATEGORY_DISPLAY = {
 	['tier 1'] = 'Tier 1 [[Category:Tier 1 Items]]',
 	['tier 2'] = 'Tier 2 [[Category:Tier 2 Items]]',
 	['tier 3'] = 'Tier 3 [[Category:Tier 3 Items]]',
@@ -40,17 +38,13 @@ local _CATEGORY_DISPLAY = {
 	enchantment = 'Enchantment [[Category:Enchantments]]',
 }
 
-local _DEFAULT_ATTRIBUTE_DISPLAY_FUNCTION = '_positiveConcatedArgsForBase'
+local DEFAULT_ATTRIBUTE_DISPLAY_FUNCTION = '_positiveConcatedArgsForBase'
 
 ---@param frame Frame
 ---@return Html
 function CustomItem.run(frame)
-	local item = Item(frame)
-	_args = item.args
-
-	item.nameDisplay = CustomItem.nameDisplay
-	item.getWikiCategories = CustomItem.getWikiCategories
-	item.createWidgetInjector = CustomItem.createWidgetInjector
+	local item = CustomItem(frame)
+	item:setWidgetInjector(CustomInjector(item))
 
 	return item:createInfobox()
 end
@@ -59,25 +53,25 @@ end
 ---@param widgets Widget[]
 ---@return Widget[]
 function CustomInjector:parse(id, widgets)
+	local caller = self.caller
+	local args = caller.args
+
 	if id == 'header' then
-		if String.isNotEmpty(_args.itemcost) then
-			table.insert(
-				widgets,
-				Breakdown{
-					content = CustomItem._getCostDisplay(),
-					classes = {
-						'infobox-header',
-						'wiki-backgroundcolor-light',
-						'infobox-header-2',
-						'infobox-gold'
-					}
+		if String.isNotEmpty(args.itemcost) then
+			table.insert(widgets, Breakdown{
+				content = caller:_getCostDisplay(),
+				classes = {
+					'infobox-header',
+					'wiki-backgroundcolor-light',
+					'infobox-header-2',
+					'infobox-gold'
 				}
-			)
+			})
 		end
-		if String.isNotEmpty(_args.itemname) then
-			local iconImage = ItemIcon.display({}, _args.itemname)
-			if String.isNotEmpty(_args.itemtext) then
-				iconImage = iconImage .. '<br><i>' .. _args.itemtext .. '</i>'
+		if String.isNotEmpty(args.itemname) then
+			local iconImage = ItemIcon.display({}, args.itemname)
+			if String.isNotEmpty(args.itemtext) then
+				iconImage = iconImage .. '<br><i>' .. args.itemtext .. '</i>'
 			end
 			table.insert(widgets, Center{content = {iconImage}})
 		end
@@ -108,106 +102,85 @@ function CustomInjector:parse(id, widgets)
 			{name = 'Critical Chance', parameter = 'critchance'},
 			{name = 'Movement Speed', funct = '_movementSpeedDisplay'},
 		}
-		widgets = CustomItem._getAttributeCells(attributeCells)
+		widgets = caller:_getAttributeCells(attributeCells)
 		if not Table.isEmpty(widgets) then
 			table.insert(widgets, 1, Title{name = 'Attributes'})
 		end
 		return widgets
 	elseif id == 'ability' then
-		if
-			String.isEmpty(_args.use) and
-			String.isEmpty(_args.active) and
-			String.isEmpty(_args.passive)
-		then
+		if String.isEmpty(args.use) and String.isEmpty(args.active) and String.isEmpty(args.passive) then
 			return {}
 		end
-		table.insert(widgets, Cell{name = 'Use', content = {
-			_args.use
-		}})
-		table.insert(widgets, Cell{name = 'Active', content = {
-			_args.active
-		}})
-		table.insert(widgets, Cell{name = 'Passive', content = {
-			_args.passive,
-			_args.passive2
-		}})
+		Array.appendWith(widgets,
+			Cell{name = 'Use', content = {args.use}},
+			Cell{name = 'Active', content = {args.active}},
+			Cell{name = 'Passive', content = {args.passive, args.passive2}}
+		)
 	elseif id == 'availability' then
-		if
-			String.isEmpty(_args.category) and
-			String.isEmpty(_args.drop)
-		then
-			return {}
-		end
+		if String.isEmpty(args.category) and String.isEmpty(args.drop) then return {} end
 		return {
 			Title{name = 'Item Tier'},
-			Cell{name = 'Category', content = {CustomItem._categoryDisplay()}},
-			Cell{name = 'Dropped From', content = {_args.drop}},
+			Cell{name = 'Category', content = {caller:_categoryDisplay()}},
+			Cell{name = 'Dropped From', content = {args.drop}},
 		}
 	elseif id == 'maps' then
-		if String.isEmpty(_args.sr) and String.isEmpty(_args.ha) then
+		if String.isEmpty(args.sr) and String.isEmpty(args.ha) then
 			return {}
-		else
-			table.insert(widgets, Cell{name = '[[Summoner\'s Rift]]', content = {_args.sr}})
-			table.insert(widgets, Cell{name = '[[Howling Abyss]]', content = {_args.ha}})
 		end
+		Array.appendWith(widgets,
+			Cell{name = '[[Summoner\'s Rift]]', content = {args.sr}},
+			Cell{name = '[[Howling Abyss]]', content = {args.ha}}
+		)
 	elseif id == 'recipe' then
-		if String.isEmpty(_args.recipe) then
-			return {}
-		else
-			table.insert(widgets, Center{content = {_args.recipe}})
-		end
+		if String.isEmpty(args.recipe) then return {} end
+		table.insert(widgets, Center{content = {args.recipe}})
 	elseif id == 'info' then return {}
 	end
 
 	return widgets
 end
 
----@return WidgetInjector
-function CustomItem:createWidgetInjector()
-	return CustomInjector()
-end
-
 ---@param args table
 ---@return string[]
 function CustomItem:getWikiCategories(args)
-	if Namespace.isMain() then
-		if
-			String.isNotEmpty(args.str) or
-			String.isNotEmpty(args.agi) or
-			String.isNotEmpty(args.int)
-		then
-			table.insert(_categories, 'Attribute Items')
-		end
+	if not Namespace.isMain() then return {} end
 
-		if String.isNotEmpty(args.movespeed) or String.isNotEmpty(args.movespeedmult) then
-			table.insert(_categories, 'Movement Speed Items')
-		end
-
-		local possibleCategories = {
-			['Strength Items'] = 'str',
-			['Agility Items'] = 'agi',
-			['Intelligence Items'] = 'int',
-			['Health Items'] = 'hp',
-			['Mana Pool Items'] = 'mana',
-			['Health Regeneration Items'] = 'hpregen',
-			['Mana Regeneration Items'] = 'manaregen',
-			['Armor Bonus Items'] = 'armor',
-			['Evasion Items'] = 'evasion',
-			['Magic Resistance Items'] = 'magicresist',
-			['Damage Items'] = 'damage',
-			['Items with Active Abilities'] = 'active',
-			['Items with Passive Abilities'] = 'passive',
-		}
-		for category, requiredArg in pairs(possibleCategories) do
-			if String.isNotEmpty(args[requiredArg]) then
-				table.insert(_categories, category)
-			end
-		end
-
-		return _categories
+	local categories = {}
+	if String.isNotEmpty(args.str) or String.isNotEmpty(args.agi) or String.isNotEmpty(args.int) then
+		table.insert(categories, 'Attribute Items')
 	end
 
-	return {}
+	if String.isNotEmpty(args.movespeed) or String.isNotEmpty(args.movespeedmult) then
+		table.insert(categories, 'Movement Speed Items')
+	end
+
+	local possibleCategories = {
+		['Strength Items'] = 'str',
+		['Agility Items'] = 'agi',
+		['Intelligence Items'] = 'int',
+		['Health Items'] = 'hp',
+		['Mana Pool Items'] = 'mana',
+		['Health Regeneration Items'] = 'hpregen',
+		['Mana Regeneration Items'] = 'manaregen',
+		['Armor Bonus Items'] = 'armor',
+		['Evasion Items'] = 'evasion',
+		['Magic Resistance Items'] = 'magicresist',
+		['Damage Items'] = 'damage',
+		['Items with Active Abilities'] = 'active',
+		['Items with Passive Abilities'] = 'passive',
+	}
+	for category, requiredArg in pairs(possibleCategories) do
+		if String.isNotEmpty(args[requiredArg]) then
+			table.insert(categories, category)
+		end
+	end
+
+	if not self:_categoryDisplay() then
+		table.insert(categories, 'Unknown Type')
+	end
+
+
+	return categories
 end
 
 ---@param args table
@@ -217,16 +190,16 @@ function CustomItem.nameDisplay(args)
 end
 
 ---@return string[]
-function CustomItem._getCostDisplay()
-	local costs = Item:getAllArgsForBase(_args, 'itemcost')
+function CustomItem:_getCostDisplay()
+	local costs = self:getAllArgsForBase(self.args, 'itemcost')
 
 	local innerDiv = CustomItem._costInnerDiv(table.concat(costs, '&nbsp;/&nbsp;'))
 	local outerDiv = mw.html.create('div')
 		:wikitext(Icon.display({}, 'gold', '21') .. ' ' .. tostring(innerDiv))
 	local display = tostring(outerDiv)
 
-	if String.isNotEmpty(_args.recipecost) then
-		innerDiv = CustomItem._costInnerDiv('(' .. _args.recipecost .. ')')
+	if String.isNotEmpty(self.args.recipecost) then
+		innerDiv = CustomItem._costInnerDiv('(' .. self.args.recipecost .. ')')
 		outerDiv = mw.html.create('div')
 			:css('padding-top', '3px')
 			:wikitext(Icon.display({}, 'recipe', '21') .. ' ' .. tostring(innerDiv))
@@ -247,67 +220,55 @@ function CustomItem._costInnerDiv(text)
 		:wikitext(text)
 end
 
+---@param caller MobilelegendsItemInfobox
 ---@param base string?
 ---@return string?
-function CustomItem._positiveConcatedArgsForBase(base)
-	if String.isNotEmpty(_args[base]) then
-		---@cast base -nil
-		local foundArgs = Item:getAllArgsForBase(_args, base)
-		return '+ ' .. table.concat(foundArgs, '&nbsp;/&nbsp;')
-	end
+function CustomItem._positiveConcatedArgsForBase(caller, base)
+	if String.isEmpty(caller.args[base]) then return end
+	---@cast base -nil
+	local foundArgs = caller:getAllArgsForBase(caller.args, base)
+	return '+ ' .. table.concat(foundArgs, '&nbsp;/&nbsp;')
 end
 
+---@param caller MobilelegendsItemInfobox
 ---@param base string?
 ---@return string?
-function CustomItem._positivePercentDisplay(base)
-	if String.isNotEmpty(_args[base]) then
-		local number = tonumber(_args[base])
-		if number == nil then
-			error('"' .. base .. '" has to be numerical')
-		end
-		number = number * 100
-		return '+ ' .. number .. '%'
+function CustomItem._positivePercentDisplay(caller, base)
+	if String.isEmpty(caller.args[base]) then
+		return
+	elseif not Logic.isNumeric(caller.args[base]) then
+		error('"' .. base .. '" has to be numerical')
 	end
+	---@cast base -nil
+	return '+ ' .. (tonumber(caller.args[base]) * 100) .. '%'
+end
+
+---@param caller MobilelegendsItemInfobox
+---@param base string?
+---@return string?
+function CustomItem._movementSpeedDisplay(caller, base)
+	local display = Array.append({},
+		String.nilIfEmpty(caller.args.movespeed),
+		Logic.isNumeric(caller.args.movespeedmult) and ((tonumber(caller.args.movespeedmult) + 100) .. '%') or nil
+	)
+	if Table.isEmpty(display) then return end
+	return '+ ' .. table.concat(display)
 end
 
 ---@return string?
-function CustomItem._movementSpeedDisplay()
-	local display
-	if String.isNotEmpty(_args.movespeed) then
-		display = _args.movespeed
-	elseif Logic.isNumeric(_args.movespeedmult) then
-		display = (tonumber(_args.movespeedmult) + 100) .. '%'
-	end
-	if String.isNotEmpty(display) then
-		return '+ ' .. display
-	end
-end
-
----@return string?
-function CustomItem._categoryDisplay()
-	local display = _CATEGORY_DISPLAY[string.lower(_args.category or '')]
-	if display then
-		return display
-	else
-		table.insert(_categories, 'Unknown Type')
-	end
+function CustomItem:_categoryDisplay()
+	return CATEGORY_DISPLAY[string.lower(self.args.category or '')]
 end
 
 ---@param attributeCells {name: string, parameter: string?, funct: string?}[]
 ---@return table
-function CustomItem._getAttributeCells(attributeCells)
-	local widgets = {}
-	for _, attribute in ipairs(attributeCells) do
-		local funct = attribute.funct or _DEFAULT_ATTRIBUTE_DISPLAY_FUNCTION
-		local content = CustomItem[funct](attribute.parameter)
-		if String.isNotEmpty(content) then
-			table.insert(widgets, Cell{name = attribute.name, content = {
-				CustomItem[funct](attribute.parameter)
-			}})
-		end
-	end
-
-	return widgets
+function CustomItem:_getAttributeCells(attributeCells)
+	return Array.map(attributeCells, function(attribute)
+		local funct = attribute.funct or DEFAULT_ATTRIBUTE_DISPLAY_FUNCTION
+		local content = CustomItem[funct](self, attribute.parameter)
+		if String.isEmpty(content) then return end
+		return Cell{name = attribute.name, content = {content}}
+	end)
 end
 
 return CustomItem
