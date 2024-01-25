6c9f012ff9be555ebc381f21f5884779847093c6
Rikard Blixt <rikardblixt@gmail.com>
2024-01-25 11:47:56 +0100

    line length for linter

--- spec/icon_spec.lua
+++ spec/icon_spec.lua
@@ -13,24 +13,30 @@ describe('Icon Maker', function()
 		end)
 
 		it('can apply color', function()
-			assert.are_equal('<i class="fas fa-check forest-green-text"></i>', Icon.makeIcon{iconName = 'check', color = 'forest-green-text'})
+			assert.are_equal('<i class="fas fa-check forest-green-text"></i>',
+				Icon.makeIcon{iconName = 'check', color = 'forest-green-text'})
 		end)
 
 		it('can hover text', function()
-			assert.are_equal('<i class="fas fa-check" title="someTitle"></i>', Icon.makeIcon{iconName = 'check', hover = 'someTitle'})
+			assert.are_equal('<i class="fas fa-check" title="someTitle"></i>',
+				Icon.makeIcon{iconName = 'check', hover = 'someTitle'})
 		end)
 
 		it('can set size pixels', function()
-			assert.are_equal('<i class="fas fa-check" style="font-size:14px"></i>', Icon.makeIcon{iconName = 'check', size = 14})
-			assert.are_equal('<i class="fas fa-check" style="font-size:14px"></i>', Icon.makeIcon{iconName = 'check', size = '14'})
+			assert.are_equal('<i class="fas fa-check" style="font-size:14px"></i>',
+				Icon.makeIcon{iconName = 'check', size = 14})
+			assert.are_equal('<i class="fas fa-check" style="font-size:14px"></i>',
+				Icon.makeIcon{iconName = 'check', size = '14'})
 		end)
 
 		it('can set size string', function()
-			assert.are_equal('<i class="fas fa-check" style="font-size:initial"></i>', Icon.makeIcon{iconName = 'check', size = 'initial'})
+			assert.are_equal('<i class="fas fa-check" style="font-size:initial"></i>',
+				Icon.makeIcon{iconName = 'check', size = 'initial'})
 		end)
 
 		it('can set hidden for screen readers', function()
-			assert.are_equal('<i class="fas fa-check" aria-hidden="true"></i>', Icon.makeIcon{iconName = 'check', screenReaderHidden = true})
+			assert.are_equal('<i class="fas fa-check" aria-hidden="true"></i>',
+				Icon.makeIcon{iconName = 'check', screenReaderHidden = true})
 		end)
 	end)
 end)
