464f349294e08e4e9ebcda84c7167c46a0c2eb12
salle <salle@users.noreply.github.com>
2024-01-25 14:01:19 +0100

    Update icon_data.lua
    Most of these are what is currently used,
    Some are more aspirational to change the current "icon" we have (unicode or ascii) to use a font-awesome one instead. 
    For example: infobox previous and next, 
    the transfer arrows, 
    up, right, down, left, 
    and rank up and down.

--- standard/icon_data.lua
+++ standard/icon_data.lua
@@ -6,5 +6,67 @@
 -- Please see https://github.com/Liquipedia/Lua-Modules to contribute
 
 return {
-	check = 'fas fa-check',
+    -- Usage: Match results
+    winner = 'fas fa-check',
+    draw = 'fas fa-minus',
+    loss = 'fas fa-times',
+
+    -- Usage: Other Match detail pop up icons
+    matchpopup = 'fas fa-info-circle',
+    timeout = 'far fa-clock',
+
+    -- Usage: Rumors, Predictions, etc.
+    correct = 'fas fa-check',
+    uncertain ='fas fa-question',
+    wrong = 'fas fa-times',
+
+    --Usage: A match or stream is live
+    live = 'fas fa-circle',
+
+    -- Usage: Previous and Next event in infoboxes
+	previous = 'fas fa-chevron-left',
+	next = 'fas fa-chevron-right',
+
+    -- Usage: Expanding and collapsing hidden sections
+	expand = 'fas fa-chevron-down',
+	collapse = 'fas fa-chevron-up',
+
+    -- Usage: to indicate a selection of something was either left, right, down or up
+    up ='fas fa-chevron-circle-up',
+    right = 'fas fa-chevron-circle-right',
+    down = 'fas fa-chevron-circle-down',
+    left = 'fas fa-chevron-circle-left',
+
+    -- Usage: Indicate if a team/player went up or down in placement in a league system or in a ranking system
+    rankup = 'fas fa-long-arrow-up',
+    rankdown ='fas fa-long-arrow-down',
+
+    -- Usage: Main Page buttons
+    featuredon = 'far fa-star',
+    featuredoff = 'fas fa-star',
+    upcoming = 'far fa-alarm-clock',
+    ongoing = 'far fa-stopwatch',
+    upcomingandongoing = 'far fa-clock',
+    concluded = 'fas fa-check',
+    transfers = 'fas fa-handshake-alt',
+
+    -- Usage: To indicate different transfer type with icon difference not just background colour
+    transferbetween = 'fas fa-arrow-alt-right',
+    transfertofreeagent = 'fas fa-arrow-alt-from-left',
+    fransferfromfreeagent = 'fas fa-arrow-alt-to-right',
+
+    -- Usage: Reference links in tables (ie transfers)
+    reference = 'fad fa-external-link-alt',
+
+    -- Usage: Section links
+    activestage = 'fas fa-bell-exclamation',
+    prizepool = 'fas fa-sack-dollar',
+    teams = 'fas fa-users',
+    results = 'fas fa-th-list',
+    headlines = 'fas fa-newspaper',
+    achievements = 'fas fa-medal',
+    activeroster = 'fas fa-users',
+    history = 'fas fa-books',
+    media = 'fas fa-newspaper',
+
 }
