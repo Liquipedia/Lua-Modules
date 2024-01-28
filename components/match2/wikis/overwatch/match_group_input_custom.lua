d4e9992013901c21e7df0c539ff96253ef2c7890
I am entry <145709809+Verus-Vales@users.noreply.github.com>
2024-01-28 13:34:42 -0600

    Epoch

--- components/match2/wikis/overwatch/match_group_input_custom.lua
+++ components/match2/wikis/overwatch/match_group_input_custom.lua
@@ -23,7 +23,7 @@ local _MAX_NUM_MAPS = 9
 local _DEFAULT_BESTOF = 3
 local _NO_SCORE = -99
 
-local EPOCH_TIME_EXTENDED = '1970-01-01T00:00:00+00:00'
+local EPOCH_TIME_EXTENDED = '0000-01-01T00:00:00+00:00'
 local TODAY = os.date('%Y-%m-%d', os.time())
 
 -- containers for process helper functions
@@ -73,7 +73,7 @@ function CustomMatchGroupInput.processOpponent(record, date)
 	local teamTemplateDate = date
 	-- If date is epoch, resolve using tournament dates instead
 	-- Epoch indicates that the match is missing a date
-	-- In order to get correct child team template, we will use an approximately date and not 1970-01-01
+	-- In order to get correct child team template, we will use an approximately date and not 0000-01-01
 	if teamTemplateDate == EPOCH_TIME_EXTENDED then
 		teamTemplateDate = Variables.varDefaultMulti(
 			'tournament_enddate',
