f1a8499ee985932daec66dace2a16e3cbfe82c82
I am entry <145709809+Verus-Vales@users.noreply.github.com>
2024-01-28 22:28:06 -0600

    Epoch

--- components/match2/wikis/zula/match_group_input_custom.lua
+++ components/match2/wikis/zula/match_group_input_custom.lua
@@ -46,7 +46,7 @@ local MAX_NUM_OPPONENTS = 2
 local DEFAULT_RESULT_TYPE = 'default'
 local DUMMY_MAP_NAME = 'null' -- Is set in Template:Map when |map= is empty.
 
-local EPOCH_TIME_EXTENDED = '1970-01-01T00:00:00+00:00'
+local EPOCH_TIME_EXTENDED = '0000-01-01T00:00:00+00:00'
 
 -- containers for process helper functions
 local matchFunctions = {}
@@ -96,7 +96,7 @@ function CustomMatchGroupInput.processOpponent(record, date)
 	local teamTemplateDate = date
 	-- If date is epoch, resolve using tournament dates instead
 	-- Epoch indicates that the match is missing a date
-	-- In order to get correct child team template, we will use an approximately date and not 1970-01-01
+	-- In order to get correct child team template, we will use an approximately date and not 0000-01-01
 	teamTemplateDate = teamTemplateDate ~= EPOCH_TIME_EXTENDED and teamTemplateDate or DateExt.getContextualDateOrNow()
 
 	Opponent.resolve(opponent, teamTemplateDate)
