75bba55f71cce57e667f57efa2112b59d87789d5
I am entry <145709809+Verus-Vales@users.noreply.github.com>
2024-01-28 22:39:19 -0600

    Epoch

--- components/match2/wikis/criticalops/match_group_input_custom.lua
+++ components/match2/wikis/criticalops/match_group_input_custom.lua
@@ -46,7 +46,7 @@ local MAX_NUM_PLAYERS = 10
 local DEFAULT_RESULT_TYPE = 'default'
 local DUMMY_MAP_NAME = 'null' -- Is set in Template:Map when |map= is empty.
 
-local EPOCH_TIME_EXTENDED = '1970-01-01T00:00:00+00:00'
+local EPOCH_TIME_EXTENDED = '0000-01-01T00:00:00+00:00'
 local TODAY = os.date('%Y-%m-%d')
 
 -- containers for process helper functions
@@ -97,7 +97,7 @@ function CustomMatchGroupInput.processOpponent(record, date)
 	local teamTemplateDate = date
 	-- If date is epoch, resolve using tournament dates instead
 	-- Epoch indicates that the match is missing a date
-	-- In order to get correct child team template, we will use an approximately date and not 1970-01-01
+	-- In order to get correct child team template, we will use an approximately date and not 0000-01-01
 	if teamTemplateDate == EPOCH_TIME_EXTENDED then
 		teamTemplateDate = Variables.varDefaultMulti(
 			'tournament_enddate',
