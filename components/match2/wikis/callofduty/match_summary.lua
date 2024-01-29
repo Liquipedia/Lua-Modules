cf7ba349c847a53d6a08ec320b6af2f2d8eca98e
I am entry <145709809+Verus-Vales@users.noreply.github.com>
2024-01-28 22:15:50 -0600

    Epoch

--- components/match2/wikis/callofduty/match_summary.lua
+++ components/match2/wikis/callofduty/match_summary.lua
@@ -15,8 +15,8 @@ local Table = require('Module:Table')
 local DisplayHelper = Lua.import('Module:MatchGroup/Display/Helper')
 local MatchSummary = Lua.import('Module:MatchSummary/Base')
 
-local EPOCH_TIME = '1970-01-01 00:00:00'
-local EPOCH_TIME_EXTENDED = '1970-01-01T00:00:00+00:00'
+local EPOCH_TIME = '0000-01-01 00:00:00'
+local EPOCH_TIME_EXTENDED = '0000-01-01T00:00:00+00:00'
 
 local GREEN_CHECK = '<i class="fa fa-check forest-green-text" style="width: 14px; text-align: center" ></i>'
 local NO_CHECK = '[[File:NoCheck.png|link=]]'
