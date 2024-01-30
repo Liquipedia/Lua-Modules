21106985a0e92a0a93dc9c27acd87e24e51fb63e
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2024-01-30 10:11:21 +0100

    fix custom hdb return annos (#3887)

--- components/hidden_data_box/commons/hidden_data_box_custom.lua
+++ components/hidden_data_box/commons/hidden_data_box_custom.lua
@@ -15,7 +15,7 @@ local CustomHiddenDataBox = {}
 
 ---Entry point
 ---@param args table
----@return string
+---@return Html
 function CustomHiddenDataBox.run(args)
 	BasicHiddenDataBox.addCustomVariables = CustomHiddenDataBox.addCustomVariables
 	return BasicHiddenDataBox.run(args)
