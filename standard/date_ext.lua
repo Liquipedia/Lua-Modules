d2cafc0d4cafa18a0afbbf760d3b1f4eb0179c57
hjpalpha <andreasbuesch1@gmail.com>
2024-01-30 16:04:55 +0100

    adjust DateExt - add defaultYear - add defaultDateTimeExtended - fix duplicate issue - rename defaultDateTimestamp to defaultTimestamp (as per usage)

--- standard/date_ext.lua
+++ standard/date_ext.lua
@@ -19,16 +19,18 @@ local DateExt = {}
 -- 0000-01-01 00:00:00
 DateExt.minTimestamp = -62167219200
 
--- default dateTime used in LPDB
-DateExt.defaultDateTimestamp = 0
-DateExt.defaultDateTime = '1970-01-01 00:00:00'
-DateExt.defaultDate = '1970-01-01'
-
 -- 9999-12-31 23:59:59
 DateExt.maxTimestamp = 253402300799
 
--- 1970-01-01 00:00:00
-DateExt.defaultDateTimestamp = 0
+-- default dateTime used in LPDB
+DateExt.defaultTimestamp = 0
+DateExt.defaultDateTime = '1970-01-01 00:00:00'
+DateExt.defaultDateTimeExtended = '1970-01-01T00:00:00+00:00'
+DateExt.defaultDate = '1970-01-01'
+DateExt.deaultYear = '1970'
+
+---@deprecated just here until any usage (outside of git) has been resolved
+DateExt.epochZero = 0
 
 --- Parses a date string into a timestamp, returning the number of seconds since UNIX epoch.
 --- The timezone offset is incorporated into the timestamp, and the timezone is discarded.
