8df63e3615b21dc81e7e1a74e3742237d96ec6c6
hjpalpha <andreasbuesch1@gmail.com>
2024-02-05 08:36:37 +0100

    commons

--- components/squad/commons/squad.lua
+++ components/squad/commons/squad.lua
@@ -11,13 +11,25 @@ local Arguments = require('Module:Arguments')
 local Logic = require('Module:Logic')
 local String = require('Module:StringUtils')
 
+---@class Squad
+---@field frame Frame
+---@field args table
+---@field root Html
+---@field content Html
+---@field type integer
 local Squad = Class.new()
 
-Squad.TYPE_ACTIVE = 0
-Squad.TYPE_INACTIVE = 1
-Squad.TYPE_FORMER = 2
-Squad.TYPE_FORMER_INACTIVE = 3
+---@enum SquadTypes
+local SquadTypes = {
+	ACTIVE = 0,
+	INACTIVE = 1,
+	FORMER = 2,
+	FORMER_INACTIVE = 3,
+}
+Squad.SquadTypes = SquadTypes
 
+---@param frame Frame
+---@return self
 function Squad:init(frame)
 	self.frame = frame
 	self.args = Arguments.getArgs(frame)
@@ -35,24 +47,19 @@ function Squad:init(frame)
 		self.args.isLoan = true
 	end
 
-	local status = (self.args.status or 'active'):lower()
+	local status = (self.args.status or 'active'):upper()
 
-	if status == 'inactive' then
-		self.type = Squad.TYPE_INACTIVE
-	elseif status == 'former' then
-		self.type = Squad.TYPE_FORMER
-	else
-		self.type = Squad.TYPE_ACTIVE
-	end
+	self.type = SquadTypes[status] or SquadTypes.ACTIVE
 
 	return self
 end
 
+---@return Squad
 function Squad:title()
 	local defaultTitle
-	if self.type == Squad.TYPE_FORMER then
+	if self.type == SquadTypes.FORMER then
 		defaultTitle = 'Former Squad'
-	elseif self.type == Squad.TYPE_INACTIVE then
+	elseif self.type == SquadTypes.INACTIVE then
 		defaultTitle = 'Inactive Players'
 	end
 
@@ -79,6 +86,7 @@ function Squad:title()
 	return self
 end
 
+---@return self
 function Squad:header()
 	local makeHeader = function(wikiText)
 		local headerCell = mw.html.create('th')
@@ -97,10 +105,10 @@ function Squad:header()
 		:node(makeHeader('Name'))
 		:node(makeHeader()) -- "Role"
 		:node(makeHeader('Join Date'))
-	if self.type == Squad.TYPE_FORMER then
+	if self.type == SquadTypes.FORMER then
 		headerRow:node(makeHeader('Leave Date'))
 			:node(makeHeader('New Team'))
-	elseif self.type == Squad.TYPE_INACTIVE then
+	elseif self.type == SquadTypes.INACTIVE then
 		headerRow:node(makeHeader('Inactive Date'))
 	end
 
@@ -109,15 +117,16 @@ function Squad:header()
 	return self
 end
 
+---@param row Html
+---@return self
 function Squad:row(row)
 	self.content:node(row)
 	return self
 end
 
+---@return Html
 function Squad:create()
-	self.root:node(self.content)
-
-	return self.root
+	return self.root:node(self.content)
 end
 
 return Squad
