bdcb5c6ce4837be8acf89a9708e3921f9397273b
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2024-02-05 09:04:34 +0100

    Add stormgate Squad Custom

---
+++ components/squad/wikis/squad_custom.lua
@@ -0,0 +1,133 @@
+---
+-- @Liquipedia
+-- wiki=commons
+-- page=Module:Squad/Custom
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Array = require('Module:Array')
+local Faction = require('Module:Faction')
+local Json = require('Module:Json')
+local Logic = require('Module:Logic')
+local Lua = require('Module:Lua')
+local ReferenceCleaner = require('Module:ReferenceCleaner')
+
+local Squad = Lua.import('Module:Squad')
+local SquadRow = Lua.import('Module:Squad/Row')
+local SquadAutoRefs = Lua.import('Module:SquadAuto/References')
+
+local CustomSquad = {}
+
+---@param frame Frame
+---@return Html
+function CustomSquad.run(frame)
+	local squad = Squad()
+
+	squad:init(frame):title():header()
+
+	local args = squad.args
+
+	local players = Array.mapIndexes(function(index)
+		return Json.parseIfString(args[index])
+	end)
+
+	Array.forEach(players, function(player)
+		squad:row(CustomSquad._playerRow(player, squad.type))
+	end)
+
+	return squad:create()
+end
+
+---@param playerList table[]
+---@param squadType integer
+---@return Html?
+function CustomSquad.runAuto(playerList, squadType)
+	if #playerList == 0 then
+		return
+	end
+
+	local squad = Squad()
+	squad:init(mw.getCurrentFrame())
+
+	squad.type = squadType
+
+	squad:title():header()
+
+	Array.forEach(playerList, function(player)
+		--Get Reference(s)
+		local joinReference = SquadAutoRefs.useReferences(player.joindateRef, player.joindate)
+		local leaveReference = SquadAutoRefs.useReferences(player.leavedateRef, player.leavedate)
+
+		-- Map between formats
+		player.joindate = (player.joindatedisplay or player.joindate) .. ' ' .. joinReference
+		player.leavedate = (player.leavedatedisplay or player.leavedate) .. ' ' .. leaveReference
+		player.inactivedate = player.leavedate
+
+		player.link = player.page
+		player.role = player.thisTeam.role
+		player.faction = Logic.emptyOr(player.thisTeam.position, player.newTeam.position)
+		player.team = player.thisTeam.role == 'Loan' and player.oldTeam.team
+
+		player.newteam = player.newTeam.team
+		player.newteamrole = player.newTeam.role
+		player.newteamdate = player.newTeam.date
+
+		squad:row(CustomSquad._playerRow(player, squad.type))
+	end)
+
+	return squad:create()
+end
+
+---@param player table
+---@param squadType integer
+---@return Html
+function CustomSquad._playerRow(player, squadType)
+	local row = SquadRow{useTemplatesForSpecialTeams = true}
+
+	local faction = Faction.read(player.faction)
+
+	row:status(squadType)
+	row:id{
+		player.id,
+		flag = player.flag,
+		faction = faction,
+		link = player.link,
+		captain = player.captain,
+		role = player.role,
+		team = player.team,
+		date = player.leavedate or player.inactivedate or player.joindate,
+	}
+	row:name{name = player.name}
+	row:role{role = player.role}
+	row:date(player.joindate, 'Join Date:&nbsp;', 'joindate')
+
+	if squadType == Squad.TYPE_FORMER then
+		if Logic.isEmpty(player.newteam) then
+			if Logic.readBool(player.retired) then
+				player.newteam = 'retired'
+			elseif Logic.readBool(player.military) then
+				player.newteam = 'military'
+			end
+		end
+
+		row:date(player.leavedate, 'Leave Date:&nbsp;', 'leavedate')
+		row:newteam{
+			newteam = player.newteam,
+			newteamrole = player.newteamrole,
+			newteamdate = player.newteamdate,
+			leavedate = player.leavedate,
+		}
+	elseif squadType == Squad.TYPE_INACTIVE then
+		row:date(player.inactivedate, 'Inactive Date:&nbsp;', 'inactivedate')
+	end
+
+	row:setExtradata{faction = faction}
+
+	return row:create(
+		mw.title.getCurrentTitle().prefixedText .. '_' .. player.id .. '_' .. ReferenceCleaner.clean(player.joindate)
+		.. (player.role and '_' .. player.role or '')
+	)
+end
+
+return CustomSquad
