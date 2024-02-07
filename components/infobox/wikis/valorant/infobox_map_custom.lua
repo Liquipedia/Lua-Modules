6c09559e01133c35df05b266da0bdd4e5c60ab83
Cameron <58013431+FenrirWulf@users.noreply.github.com>
2024-02-07 12:27:09 -0800

    Create Map Infobox for Valorant

---
+++ components/infobox/wikis/valorant/infobox_map_custom.lua
@@ -0,0 +1,72 @@
+---
+-- @Liquipedia
+-- wiki=valorant
+-- page=Module:Infobox/Map/Custom
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Array = require('Module:Array')
+local Class = require('Module:Class')
+local Lua = require('Module:Lua')
+local String = require('Module:StringUtils')
+
+local Injector = Lua.import('Module:Infobox/Widget/Injector')
+local Map = Lua.import('Module:Infobox/Map')
+local Flags = Lua.import('Module:Flags')
+
+local Widgets = require('Module:Infobox/Widget/All')
+local Cell = Widgets.Cell
+
+---@class ValorantMapInfobox: MapInfobox
+local CustomMap = Class.new(Map)
+local CustomInjector = Class.new(Injector)
+
+---@param frame Frame
+---@return Html
+function CustomMap.run(frame)
+	local map = CustomMap(frame)
+	map:setWidgetInjector(CustomInjector(map))
+
+	return map:createInfobox()
+end
+
+---@param id string
+---@param widgets Widget[]
+---@return Widget[]
+function CustomInjector:parse(id, widgets)
+	local args = self.caller.args
+
+	if id == 'location' then
+		return {
+			Cell{
+				name = 'Location',
+				content = {Flags.Icon{flag = args.country, shouldLink = false} .. '&nbsp;' .. args.country},
+			},
+		}
+	elseif id == 'custom' then
+		Array.appendWith(
+			widgets,
+			Cell{name = 'Bomb Sites', content = {args.bombsites}},
+			Cell{name = 'Teleporters', content = {args.teleporters}},
+			Cell{name = 'Game Mode', content = {args.mode}}
+		)
+	end
+	return widgets
+end
+
+---@param lpdbData table
+---@param args table
+---@return table
+function CustomMap:addToLpdb(lpdbData, args)
+	local resolveIfExists = function(value)
+		if not value then return end
+		return mw.ext.TeamLiquidIntegration.resolve_redirect(value)
+	end
+	lpdbData.extradata.creator = resolveIfExists(args.creator)
+	lpdbData.extradata.creator2 = resolveIfExists(args.creator2)
+
+	return lpdbData
+end
+
+return CustomMap
