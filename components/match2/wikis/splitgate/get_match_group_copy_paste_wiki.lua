d0d0dd20c7b0d5c19bb07157874ff17f6f677437
hjpalpha <andreasbuesch1@gmail.com>
2024-02-08 13:21:14 +0100

    splitgate

--- components/match2/wikis/splitgate/get_match_group_copy_paste_wiki.lua
+++ components/match2/wikis/splitgate/get_match_group_copy_paste_wiki.lua
@@ -8,66 +8,48 @@
 
 local Array = require('Module:Array')
 local Class = require('Module:Class')
+local Logic = require('Module:Logic')
 local Lua = require('Module:Lua')
 
 local BaseCopyPaste = Lua.import('Module:GetMatchGroupCopyPaste/wiki/Base')
---[[
 
-WikiSpecific Code for MatchList, Bracket, and SingleMatch Code Generators
-
-]]--
-
+---@class SplitgateMatch2CopyPaste: Match2CopyPasteBase
 local WikiCopyPaste = Class.new(BaseCopyPaste)
 
+local INDENT = WikiCopyPaste.Indent
+
 --returns the Code for a Match, depending on the input
+---@param bestof integer
+---@param mode string
+---@param index integer
+---@param opponents integer
+---@param args table
+---@return string
 function WikiCopyPaste.getMatchCode(bestof, mode, index, opponents, args)
-	local indent = '    '
+	local showScore = Logic.nilOr(Logic.readBoolOrNil(args.score), bestof == 0)
 
-	if bestof == 0 and args.score ~= 'false' then
-		args.score = 'true'
-	end
-
-	local score = args.score == 'true' and '|score=' or nil
 	local lines = Array.extend(
 		'{{Match',
-		index == 1 and (indent .. '|bestof=' .. (bestof ~= 0 and bestof or '')) or nil,
-		args.needsWinner == 'true' and indent .. '|winner=' or nil,
-		args.hasDate == 'true' and {indent .. '|date=', indent .. '|twitch='} or {}
+		index == 1 and (INDENT .. '|bestof=' .. (bestof ~= 0 and bestof or '')) or nil,
+		args.needsWinner == 'true' and INDENT .. '|winner=' or nil,
+		args.hasDate == 'true' and {INDENT .. '|date=', INDENT .. '|twitch='} or {},
+		Array.map(Array.range(1, opponents), function(opponentIndex)
+			return INDENT .. '|opponent' .. opponentIndex .. '=' .. WikiCopyPaste.getOpponent(mode, showScore)
+		end)
 	)
 
-	for i = 1, opponents do
-		table.insert(lines, indent .. '|opponent' .. i .. '=' .. WikiCopyPaste._getOpponent(mode, score))
-	end
-
-	if bestof ~= 0 then
-		for i = 1, bestof do
-			Array.appendWith(lines,
-				indent .. '|map' .. i .. '={{Map|map=|mode=',
-				indent .. indent .. '|score1=|score2=',
-				indent .. indent .. '|winner=',
-				indent .. '}}'
-			)
-		end
-	end
+	Array.forEach(Array.range(1, bestof), function(mapIndex)
+		Array.appendWith(lines,
+			INDENT .. '|map' .. mapIndex .. '={{Map|map=|mode=',
+			INDENT .. INDENT .. '|score1=|score2=',
+			INDENT .. INDENT .. '|winner=',
+			INDENT .. '}}'
+		)
+	end)
 
 	table.insert(lines, '}}')
 
 	return table.concat(lines, '\n')
-end
-
---subfunction used to generate the code for the Opponent template, depending on the type of opponent
-function WikiCopyPaste._getOpponent(mode, score)
-	local out
-
-	if mode == 'solo' then
-		out = '{{SoloOpponent||flag=' .. (score or '') .. '}}'
-	elseif mode == 'team' then
-		out = '{{TeamOpponent|' .. (score or '') .. '}}'
-	elseif mode == 'literal' then
-		out = '{{Literal|}}'
-	end
-
-	return out
 end
 
 return WikiCopyPaste
