a39598eba2dd7115f45d1c3d3d8caf939c536781
I am entry <145709809+Verus-Vales@users.noreply.github.com>
2024-02-13 07:26:22 -0600

    Update player_introduction.lua
    Co-authored-by: Rikard Blixt <rikardblixt@gmail.com>

--- components/infobox/extensions/commons/player_introduction.lua
+++ components/infobox/extensions/commons/player_introduction.lua
@@ -309,7 +309,7 @@ end
 function PlayerIntroduction._readTransferFromTransfers(player)
 	local queryData = mw.ext.LiquipediaDB.lpdb('transfer', {
 		conditions = '([[player::' .. player .. ']] OR [[player::' .. player:gsub('_', ' ') .. ']]) AND '
-			.. '[[date::>' .. DateExt.defaultDate .. ']]',
+			.. '[[date::!' .. DateExt.defaultDate .. ']]',
 		order = 'date desc',
 		limit = 1,
 		query = 'fromteam, toteam, role2, date, extradata'
