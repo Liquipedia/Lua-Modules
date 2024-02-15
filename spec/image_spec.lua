6b0357a07bd05c834dbee6b669c997b00789b0d5
hjpalpha <andreasbuesch1@gmail.com>
2024-02-15 19:30:29 +0100

    test cases

---
+++ spec/image_spec.lua
@@ -0,0 +1,19 @@
+--- Triple Comment to Enable our LLS Plugin
+describe('image', function()
+	local Image = require('Module:Image')
+
+	describe('display', function()
+		it('check', function()
+			assert.are_same('[[File:test]]', Image.display('test'))
+			assert.are_same('[[File:test|class=show-when-light-mode]][[File:test2|class=show-when-dark-mode]]',
+				Image.display('test', 'test2'))
+			assert.are_same('[[File:test|10px|link=test3|class=show-when-light-mode|capt]]'
+				.. '[[File:test2|10px|link=test3|class=show-when-dark-mode|capt]]',
+				Image.display('test', 'test2', {
+					link = 'test3',
+					size = 10,
+					caption = 'capt'
+				}))
+		end)
+	end)
+end)
