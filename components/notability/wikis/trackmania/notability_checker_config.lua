d3dbff12de808fb108c9429db704c6156fcfe521
SobakaPirat <40334641+SobakaPirat@users.noreply.github.com>
2024-02-15 15:39:19 +0300

    Remove unnecessary EXTRA_DROP_OFF_TYPES

--- components/notability/wikis/trackmania/notability_checker_config.lua
+++ components/notability/wikis/trackmania/notability_checker_config.lua
@@ -32,18 +32,6 @@ Config.PLACEMENT_QUERY =
 Config.NOTABILITY_THRESHOLD_MIN = 20
 Config.NOTABILITY_THRESHOLD_NOTABLE = 25
 
--- These are all the liquipediatiertypes which should be extra "penalised"
--- for a lower placement, see also the placementDropOffFunction below.
--- Generally these types will award the same points for first, but then
--- quickly decrease the point rewards as the placement gets lower
-Config.EXTRA_DROP_OFF_TYPES = {
-	Config.TIER_TYPE_GENERAL,
-	Config.TIER_TYPE_MONTHLY,
-	Config.TIER_TYPE_WEEKLY,
-	Config.TIER_TYPE_QUALIFIER,
-	Config.TIER_TYPE_CAMPAIGN,
-}
-
 -- Weights used for tournaments
 Config.weights = {
 	{
