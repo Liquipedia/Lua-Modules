4f8d14f4cf7f1023ca8419dc99b7b8896cc807b6
I am entry <145709809+Verus-Vales@users.noreply.github.com>
2024-02-15 09:21:40 -0600

    Remove typo

--- standard/date_ext.lua
+++ standard/date_ext.lua
@@ -28,7 +28,6 @@ DateExt.defaultDateTime = '0000-01-01 00:00:00'
 DateExt.defaultDateTimeExtended = '0000-01-01T00:00:00+00:00'
 DateExt.defaultDate = '0000-01-01'
 DateExt.defaultYear = '0000'
-DateExt.deaultYear = '0000'
 
 --- Parses a date string into a timestamp, returning the number of seconds since UNIX epoch.
 --- The timezone offset is incorporated into the timestamp, and the timezone is discarded.
