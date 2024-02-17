aff62bf60701b93c391139dfb77839c354dbdb28
hjpalpha <andreasbuesch1@gmail.com>
2024-02-17 14:56:11 +0100

    add it to unit infobox also

--- components/infobox/wikis/stormgate/infobox_unit_custom.lua
+++ components/infobox/wikis/stormgate/infobox_unit_custom.lua
@@ -101,7 +101,8 @@ function CustomInjector:parse(id, widgets)
 			Cell{name = 'Energy', content = {caller:_energyDisplay()}},
 			Cell{name = 'Sight', content = {args.sight}},
 			Cell{name = 'Speed', content = {args.speed}},
-			Cell{name = 'Passive', content = caller:_displayCommaSeparatedString(args.passive)}
+			Cell{name = 'Passive', content = caller:_displayCommaSeparatedString(args.passive)},
+			Cell{name = 'Upgrades To', content = caller:_displayCommaSeparatedString(args.upgrades_to)}
 		)
 		-- moved to the bottom due to having headers that would look ugly if in place where attack is set in commons
 		for _, attackArgs, attackIndex in Table.iter.pairsByPrefix(args, 'attack') do
@@ -208,6 +209,7 @@ function CustomUnit:setLpdbData(args)
 			energydesc = args.energy_desc,
 			passive = Array.parseCommaSeparatedString(args.passive),
 			armortypes = Array.parseCommaSeparatedString(args.armor_type),
+			upgradesto = Array.parseCommaSeparatedString(args.upgrades_to),
 		},
 	})
 end
