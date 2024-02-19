c503bdd8e00901b14a7a04e1f1dcfaea1d1fef5f
thangcoi02 <116045044+thangcoi02@users.noreply.github.com>
2024-02-20 02:38:15 +0700

    removing whitespace

--- components/notability/wikis/pubgmobile/notability_checker_config.lua
+++ components/notability/wikis/pubgmobile/notability_checker_config.lua
@@ -247,7 +247,7 @@ function Config.placementDropOffFunction(tier, tierType)
 				if ((tier == 1 or tier == 2 or tier == 3) and placement == 1) then
 					return score
 				end
-			
+
 			else
 				if (tier == 1 and placement <= 16)
 					or ((tier == 2 or tier == 3 or tier == 4 or tier == 5) and placement == 1)
