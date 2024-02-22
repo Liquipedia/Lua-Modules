4cfefdc55edd356f8c59acfb0c760dd8c670aae4
Rikard Blixt <rikardblixt@gmail.com>
2024-02-22 14:40:20 +0100

    untested match legacy

---
+++ components/match2/wikis/ageofempires/match_legacy.lua
@@ -0,0 +1,125 @@
+---
+-- @Liquipedia
+-- wiki=ageofempires
+-- page=Module:Match/Legacy
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local MatchLegacy = {}
+
+local Json = require('Module:Json')
+local Lua = require('Module:Lua')
+local String = require('Module:StringUtils')
+local Table = require('Module:Table')
+
+local DisplayHelper = Lua.import('Module:MatchGroup/Display/Helper')
+local OpponentLibraries = require('Module:OpponentLibraries')
+local Opponent = OpponentLibraries.Opponent
+
+
+function MatchLegacy.storeMatch(match2, options)
+	local match = MatchLegacy._convertParameters(match2)
+
+	if options.storeMatch1 then
+		match.games = MatchLegacy.storeGames(match, match2)
+		return mw.ext.LiquipediaDB.lpdb_match('legacymatch_' .. match2.match2id, Json.stringifySubTables(match))
+	end
+end
+
+function MatchLegacy.storeGames(match, match2)
+	local games = {}
+	for gameIndex, game2 in ipairs(match2.match2games or {}) do
+		local game = Table.deepCopy(game2)
+		-- Extradata
+		game.extradata = {}
+		game.extradata.gamenumber = gameIndex
+		game.extradata.mapmode = ''
+		game.extradata.tournament = match.tournament
+		game.extradata.vodmatch = match.vod
+		game.extradata.opponent1civ = game2.participants['1_1'].civ
+		game.extradata.opponent2civ = game2.participants['2_1'].civ
+		game.extradata.winnerciv =
+				(tonumber(game.winner) == 1 and game.extradata.opponent1civ) or
+				(tonumber(game.winner) == 2 and game.extradata.opponent2civ) or
+				''
+		game.extradata.loserciv =
+				(tonumber(game.winner) == 2 and game.extradata.opponent1civ) or
+				(tonumber(game.winner) == 1 and game.extradata.opponent2civ) or
+				''
+		game.extradata.opponent1name = game2.participants['1_1'].player
+		game.extradata.opponent2name = game2.participants['2_1'].player
+		-- Other stuff
+		game.opponent1 = match.opponent1
+		game.opponent2 = match.opponent2
+		game.opponent1flag = match.opponent1flag
+		game.opponent2flag = match.opponent2flag
+
+		local scores = Json.parseIfString(game2.scores) or {}
+		game.opponent1score = scores[1] or 0
+		game.opponent2score = scores[2] or 0
+		local res = mw.ext.LiquipediaDB.lpdb_game(
+			'legacygame_' .. match2.match2id .. gameIndex,
+			Json.stringifySubTables(game)
+		)
+		table.insert(games, res)
+	end
+	return table.concat(games)
+end
+
+function MatchLegacy._convertParameters(match2)
+	local match = Table.deepCopy(match2)
+	for key, _ in pairs(match) do
+		if String.startsWith(key, 'match2') then
+			match[key] = nil
+		end
+	end
+
+	if match.walkover == 'ff' or match.walkover == 'dq' then
+		match.walkover = match.winner
+	elseif match.walkover == 'l' then
+		match.walkover = nil
+	end
+
+	match.staticid = match2.match2id
+
+	-- Handle extradata fields
+	match.extradata = {}
+
+	match.extradata.bestofx = match2.bestof ~= 0 and tostring(match2.bestof) or ''
+	local bracketData = Json.parseIfString(match2.match2bracketdata)
+	if type(bracketData) == 'table' and bracketData.type == 'bracket' and bracketData.inheritedheader then
+		match.header = (DisplayHelper.expandHeader(bracketData.inheritedheader) or {})[1]
+	end
+	match.extradata.matchround = match.header
+
+	-- Handle Opponents
+	local handleOpponent = function(index)
+		local prefix = 'opponent' .. index
+		local opponent = match2.match2opponents[index] or {}
+		local opponentmatch2players = opponent.match2players or {}
+		if opponent.type == Opponent.solo then
+			local player = opponentmatch2players[1] or {}
+			match[prefix] = player.name
+			match[prefix .. 'score'] = (tonumber(opponent.score) or 0) > 0 and opponent.score or 0
+			match[prefix .. 'flag'] = player.flag
+			match.extradata[prefix .. 'name'] = player.displayname
+		elseif opponent.type == Opponent.literal then
+			match[prefix] = 'TBD'
+		end
+	end
+
+	handleOpponent(1)
+	handleOpponent(2)
+
+	local games = match2.match2games or {}
+	for key, game in ipairs(games) do
+		if String.isNotEmpty(game.vod) then
+			match.extradata['vodgame' .. key] = game.vod
+		end
+	end
+
+	return match
+end
+
+return MatchLegacy
