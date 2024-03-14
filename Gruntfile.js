be74019ec61e69c553fea06bd4a998dffa51b057
Rikard Blixt <rikardblixt@gmail.com>
2024-03-14 13:29:01 +0100

    ci: complie less to css pre-deploy

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,7 @@
 module.exports = function( grunt ) {
 	grunt.loadNpmTasks( 'grunt-eslint' );
 	grunt.loadNpmTasks( 'grunt-stylelint' );
+	grunt.loadNpmTasks( 'grunt-contrib-less' );
 
 	grunt.initConfig( {
 		eslint: {
@@ -19,9 +20,23 @@ module.exports = function( grunt ) {
 			all: [
 				'stylesheets/**/*.{css,scss,less}'
 			]
+		},
+		less: {
+			options: {
+				compress: false,
+				yuicompress: false,
+				optimization: 0
+			},
+			foobar: {
+				expand: true,
+				cwd: 'stylesheets/',
+				src: [ '**/*.less' ],
+				dest: 'stylesheets/complied/',
+				ext: '.css'
+			}
 		}
 	} );
 
 	grunt.registerTask( 'main', [ 'eslint', 'stylelint' ] );
-	grunt.registerTask( 'default', 'test' );
+	grunt.registerTask( 'default', [ 'test', 'less' ] );
 };
