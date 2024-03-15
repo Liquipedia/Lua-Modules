6360d9ca7d6d2511a1592d74d870cd30a8c12ce6
Robbie <42142350+robbai@users.noreply.github.com>
2024-03-15 16:18:24 +0000

    Make new tabs behaviour opt-in

--- standard/tabs.lua
+++ standard/tabs.lua
@@ -68,7 +68,8 @@ function Tabs.dynamic(args)
 		return Logic.isEmpty(tab.content) end)
 	assert(hasContent or allEmpty, 'Some of the tabs have contents while others do not')
 
-	if tabCount == 1 and hasContent then return Tabs._single(tabArgs[1], not Logic.readBool(args.suppressHeader)) end
+	local isSingular = tabCount == 1 and hasContent
+	if isSingular and Logic.readBool(args.reduceSingle) then return Tabs._single(tabArgs[1], not Logic.readBool(args.suppressHeader)) end
 
 	local tabs = mw.html.create('ul')
 		:addClass('nav nav-tabs tabs tabs' .. tabCount)
