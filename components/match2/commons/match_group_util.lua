a72613b7b293be6c32accdc8bd44dc5049bbdda3
hjpalpha <andreasbuesch1@gmail.com>
2024-03-19 08:40:45 +0100

    Bracket/Template

--- components/match2/commons/match_group_util.lua
+++ components/match2/commons/match_group_util.lua
@@ -67,6 +67,7 @@ MatchGroupUtil.types.AdvanceSpot = TypeUtil.struct({
 ---@field title string?
 ---@field type 'bracket'
 ---@field upperMatchId string?
+---@field matchId string?
 MatchGroupUtil.types.BracketBracketData = TypeUtil.struct({
 	advanceSpots = TypeUtil.array(MatchGroupUtil.types.AdvanceSpot),
 	bracketResetMatchId = 'string?',
@@ -114,6 +115,7 @@ MatchGroupUtil.types.MatchCoordinates = TypeUtil.struct({
 ---@field title string?
 ---@field dateHeader boolean?
 ---@field type 'matchlist'
+---@field matchId string?
 MatchGroupUtil.types.MatchlistBracketData = TypeUtil.struct({
 	header = 'string?',
 	title = 'string?',
