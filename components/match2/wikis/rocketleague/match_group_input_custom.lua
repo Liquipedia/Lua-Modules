fac7a2378d3adca3af2bd73c7e9118d211237406
hjpalpha <andreasbuesch1@gmail.com>
2024-03-26 14:37:15 +0100

    rl

--- components/match2/wikis/rocketleague/match_group_input_custom.lua
+++ components/match2/wikis/rocketleague/match_group_input_custom.lua
@@ -40,6 +40,9 @@ local matchFunctions = {}
 local mapFunctions = {}
 
 -- called from Module:MatchGroup
+---@param match table
+---@param options table?
+---@return table
 function CustomMatchGroupInput.processMatch(match, options)
 	Table.mergeInto(
 		match,
@@ -55,6 +58,8 @@ function CustomMatchGroupInput.processMatch(match, options)
 end
 
 -- called from Module:Match/Subobjects
+---@param map table
+---@return table
 function CustomMatchGroupInput.processMap(map)
 	map = mapFunctions.getExtraData(map)
 	map = mapFunctions.getScoresAndWinner(map)
@@ -63,6 +68,8 @@ function CustomMatchGroupInput.processMap(map)
 	return map
 end
 
+---@param record table
+---@param timestamp integer
 function CustomMatchGroupInput.processOpponent(record, timestamp)
 	local opponent = Opponent.readOpponentArgs(record)
 		or Opponent.blank()
@@ -100,13 +107,17 @@ function CustomMatchGroupInput.processOpponent(record, timestamp)
 end
 
 -- called from Module:Match/Subobjects
+---@param player table
+---@return table
 function CustomMatchGroupInput.processPlayer(player)
 	return player
 end
 
---
---
 -- function to sort out winner/placements
+---@param tbl table[]
+---@param key1 integer
+---@param key2 integer
+---@return boolean
 function CustomMatchGroupInput._placementSortFunction(tbl, key1, key2)
 	local op1 = tbl[key1]
 	local op2 = tbl[key2]
@@ -132,6 +143,8 @@ function CustomMatchGroupInput._placementSortFunction(tbl, key1, key2)
 	end
 end
 
+---@param opp table
+---@return integer
 function CustomMatchGroupInput._getSetWins(opp)
 	local extradata = opp.extradata or {}
 	local set1win = extradata.set1win and 1 or 0
@@ -144,16 +157,23 @@ end
 --
 -- match related functions
 --
+
+---@param matchArgs table
+---@return {date: string, dateexact: boolean, timestamp: integer, timezoneId: string?, timezoneOffset: string?}
 function matchFunctions.readDate(matchArgs)
 	return MatchGroupInput.readDate(matchArgs.date, {'tournament_enddate'})
 end
 
+---@param match table
+---@return table
 function matchFunctions.getTournamentVars(match)
 	match.mode = Logic.emptyOr(match.mode, Variables.varDefault('tournament_mode', '3v3'))
 	match.showh2h = Logic.emptyOr(match.showh2h, Variables.varDefault('showh2h'))
 	return MatchGroupInput.getCommonTournamentVars(match)
 end
 
+---@param match table
+---@return table
 function matchFunctions.getVodStuff(match)
 	match.stream = Streams.processStreams(match)
 	match.vod = Logic.emptyOr(match.vod, Variables.varDefault('vod'))
@@ -170,6 +190,8 @@ function matchFunctions.getVodStuff(match)
 	return match
 end
 
+---@param match table
+---@return table
 function matchFunctions.getExtraData(match)
 	local opponent1 = match.opponent1 or {}
 	local opponent2 = match.opponent2 or {}
@@ -193,6 +215,8 @@ function matchFunctions.getExtraData(match)
 	return match
 end
 
+---@param opponent table
+---@return boolean
 function matchFunctions._checkForNonEmptyOpponent(opponent)
 	if Opponent.typeIsParty(opponent.type) then
 		return Array.any(opponent.match2players, function(player) return Logic.isNotEmpty(player.name) end)
@@ -204,6 +228,8 @@ function matchFunctions._checkForNonEmptyOpponent(opponent)
 	return false
 end
 
+---@param match table
+---@return table
 function matchFunctions.getLinks(match)
 	match.links = {}
 
@@ -220,6 +246,8 @@ function matchFunctions.getLinks(match)
 	return match
 end
 
+---@param match table
+---@return boolean
 function matchFunctions.isFeatured(match)
 	local opponent1 = match.opponent1
 	local opponent2 = match.opponent2
@@ -244,6 +272,8 @@ function matchFunctions.isFeatured(match)
 	return false
 end
 
+---@param name string?
+---@return integer
 function matchFunctions.currentEarnings(name)
 	if String.isEmpty(name) then
 		return 0
@@ -261,6 +291,8 @@ function matchFunctions.currentEarnings(name)
 	return 0
 end
 
+---@param args table
+---@return table
 function matchFunctions.getOpponents(args)
 	-- read opponents and ignore empty ones
 	local opponents = {}
@@ -367,6 +399,9 @@ end
 --
 -- map related functions
 --
+
+---@param map table
+---@return table
 function mapFunctions.getExtraData(map)
 	local timeouts = Array.extractValues(Table.mapValues(mw.text.split(map.timeout or '', ','), tonumber))
 
@@ -383,6 +418,8 @@ function mapFunctions.getExtraData(map)
 	return map
 end
 
+---@param map table
+---@return table
 function mapFunctions.getScoresAndWinner(map)
 	map.scores = {}
 	local indexedScores = {}
@@ -420,11 +457,16 @@ function mapFunctions.getScoresAndWinner(map)
 	return map
 end
 
+---@param indexedScores table[]
+---@return integer?
 function mapFunctions.getWinner(indexedScores)
 	table.sort(indexedScores, mapFunctions.mapWinnerSortFunction)
 	return indexedScores[1].index
 end
 
+---@param op1 table
+---@param op2 table
+---@return boolean
 function mapFunctions.mapWinnerSortFunction(op1, op2)
 	local op1norm = op1.status == _STATUS_HAS_SCORE
 	local op2norm = op2.status == _STATUS_HAS_SCORE
@@ -442,6 +484,8 @@ function mapFunctions.mapWinnerSortFunction(op1, op2)
 	end
 end
 
+---@param map table
+---@return table
 function mapFunctions.getParticipantsData(map)
 	local participants = map.participants or {}
 
