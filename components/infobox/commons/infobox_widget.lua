9cfe51a35b1fc711a53c02e890291aef7ce86dfe
Rikard Blixt <rikardblixt@gmail.com>
2024-03-29 16:23:43 +0100

    refactor: clean up widget code

--- components/infobox/commons/infobox_widget.lua
+++ components/infobox/commons/infobox_widget.lua
@@ -6,14 +6,10 @@
 -- Please see https://github.com/Liquipedia/Lua-Modules to contribute
 --
 local Class = require('Module:Class')
-local Lua = require('Module:Lua')
 local String = require('Module:StringUtils')
-
-local Injector = Lua.import('Module:Infobox/Widget/Injector')
 
 ---@class Widget: BaseClass
 ---@operator call(): Widget
----@field public injector WidgetInjector?
 local Widget = Class.new()
 
 local _ERROR_TEXT = '<span style="color:#ff0000;font-weight:bold" class="show-when-logged-in">' ..
@@ -25,20 +21,18 @@ local _ERROR_TEXT = '<span style="color:#ff0000;font-weight:bold" class="show-wh
 ---@param value string
 ---@return string
 function Widget:assertExistsAndCopy(value)
-	if value == nil or value == '' then
-		error('Tried to set a nil value to a mandatory property')
-	end
-
-	return value
+	return assert(String.nilIfEmpty(value), 'Tried to set a nil value to a mandatory property')
 end
 
+---@param injector WidgetInjector?
 ---@return Widget[]|Html[]|nil
-function Widget:make()
+function Widget:make(injector)
 	error('A Widget must override the make() function!')
 end
 
+---@param injector WidgetInjector?
 ---@return Widget[]|Html[]|nil
-function Widget:tryMake()
+function Widget:tryMake(injector)
 	local _, output = xpcall(
 		function()
 			return self:make()
@@ -53,15 +47,6 @@ function Widget:tryMake()
 	)
 
 	return output
-end
-
----Sets the context of a widget
----@param context table
-function Widget:setContext(context)
-	self.context = context
-	if context.injector ~= nil and (context.injector['is_a'] == nil or context.injector:is_a(Injector) == false) then
-		error('Valid Injector from Infobox/Widget/Injector needs to be provided')
-	end
 end
 
 -- error widget for displaying errors of widgets
--- components/widget/widget_table.lua
+++ components/widget/widget_table.lua
@@ -49,8 +49,9 @@ function Table:addClass(class)
 	return self
 end
 
+---@param injector WidgetInjector?
 ---@return {[1]: Html}
-function Table:make()
+function Table:make(injector)
 	local displayTable = mw.html.create('div'):addClass('csstable-widget')
 	displayTable:css{
 		['grid-template-columns'] = 'repeat(' .. (self.columns or self:_getMaxCells()) .. ', auto)',
@@ -63,7 +64,7 @@ function Table:make()
 	displayTable:css(self.css)
 
 	for _, row in ipairs(self.rows) do
-		for _, node in ipairs(WidgetFactory.work(row, self.injector)) do
+		for _, node in ipairs(WidgetFactory.work(row, injector)) do
 			displayTable:node(node)
 		end
 	end
