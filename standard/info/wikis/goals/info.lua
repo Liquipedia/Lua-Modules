46d0c297a907b280bd37373a12ec4986d36e09d1
Hesketh2 <88981446+Hesketh2@users.noreply.github.com>
2024-04-03 00:54:09 +0700

    Create Info module for GOALS

---
+++ standard/info/wikis/goals/info.lua
@@ -0,0 +1,32 @@
+---
+-- @Liquipedia
+-- wiki=goals
+-- page=Module:Info
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+return {
+	startYear = 2024,
+	wikiName = 'goals',
+	name = 'GOALS',
+	games = {
+		goals = {
+			abbreviation = 'GOALS',
+			name = 'GOALS',
+			link = 'GOALS',
+			logo = {
+				darkMode = 'GOALS default allmode.png',
+				lightMode = 'GOALS default allmode.png',
+			},
+			defaultTeamLogo = {
+				darkMode = 'GOALS default allmode.png',
+				lightMode = 'GOALS default allmode.png',
+			},
+		},
+	},
+	defaultGame = 'goals',
+	defaultTeamLogo = 'GOALS default allmode.png', ---@deprecated
+	defaultTeamLogoDark = 'GOALS default allmode.png', ---@deprecated
+	match2 = 0,
+}
