ef59890fe1cf819b20fd58d92b89846a2818ff19
Rikard Blixt <rikardblixt@gmail.com>
2024-04-08 10:27:03 +0200

    unneeded

--- components/squad/wikis/smash/squad_custom.lua
+++ components/squad/wikis/smash/squad_custom.lua
@@ -65,8 +65,7 @@ function CustomSquad.run(frame)
 		local row = ExtendedSquadRow()
 
 		local game = player.game and mw.text.split(player.game:lower(), ',')[1] or tableGame
-		local mains = SquadPlayerData.get{link = player.link, player = player.id, game = game}
-			or player.mains
+		local mains = SquadPlayerData.get{link = player.link, player = player.id, game = game} or player.mains
 
 		row:status(squad.type)
 		row:id{
