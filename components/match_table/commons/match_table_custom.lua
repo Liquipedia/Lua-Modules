ca89047ea304b543f20c1c6841dcd5cb28ceb42c
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2024-04-19 17:36:45 +0200

    feat(matchtable): create standardized version (#3509)
    * init
    - read input (options, aliases, etc)
    - query and process data (for mini stats)
    todo: display & overall stats & custom
    
    * custom
    and move to correct dir
    
    * requireDevIfEnabled
    
    * requireDevIfEnabled
    
    * start building the display
    
    * more display
    build display except for participant, score and vs cell
    
    * typo
    
    * finish display
    
    * allow teamX, playerX, teams,m players support (for legacy reasons)
    
    * fixes
    
    * linter: "line is too long (121 > 120)"
    
    * fix vod icon display for `0`
    
    * round percentage
    
    * exclude not played matches in query conditions
    
    * Update components/match_table/commons/match_table.lua
    
    Co-authored-by: mbergen <mail@mbergen.de>
    
    * from review
    
    * from more reviews
    
    * change param name
    
    * rawdatetime = true
    
    * adjust date display for disabled timezone conversion
    
    * make martin happy: timezone id stuff
    
    * as per further reviews
    - allow to show type
    - 0-pad days
    - adjust tournament width
    
    * typo in function name
    
    * another typo
    
    * bold winners score as requested
    
    * fix date conditions if none are set
    
    * exclude default wins from calculation as per feedback
    
    * add option for the stupid fate headers
    
    * Update components/match_table/commons/match_table.lua
    
    * as per review
    
    * as per review
    
    * as per comments
    - left align vods
    - sort value for opponents
    
    * one underscore is enough
    
    * Update components/match_table/commons/match_table.lua
    
    ---------
    
    Co-authored-by: mbergen <mail@mbergen.de>

---
+++ components/match_table/commons/match_table_custom.lua
@@ -0,0 +1,22 @@
+---
+-- @Liquipedia
+-- wiki=commons
+-- page=Module:MatchTable/Custom
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Class = require('Module:Class')
+local Lua = require('Module:Lua')
+
+local MatchTable = Lua.import('Module:MatchTable')
+
+local CustomMatchTable = {}
+
+---@param args table
+---@return Html
+function CustomMatchTable.results(args)
+	return MatchTable(args):readConfig():query():build()
+end
+
+return Class.export(CustomMatchTable)
