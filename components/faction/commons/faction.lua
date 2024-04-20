b0cade3c5b420e483da0a5c76810249f640d088f
Rikard Blixt <rikardblixt@gmail.com>
2024-04-20 12:01:15 +0200

    from review

--- components/faction/commons/faction.lua
+++ components/faction/commons/faction.lua
@@ -14,7 +14,7 @@ local String = require('Module:StringUtils')
 local Table = require('Module:Table')
 local TypeUtil = require('Module:TypeUtil')
 
-local Data = mw.loadData('Module:Faction/Data')
+local Data = Lua.import('Module:Faction/Data', {loadData = true})
 local IconData = Lua.requireIfExists('Module:Faction/IconData', {loadData = true})
 	or {byFaction = {}}
 
