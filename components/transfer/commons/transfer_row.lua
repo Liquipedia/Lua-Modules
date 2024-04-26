6db02c6db9675b7d3b7586f8083865bd58424749
hjpalpha <andreasbuesch1@gmail.com>
2024-04-26 15:38:03 +0200

    more

--- components/transfer/commons/transfer_row.lua
+++ components/transfer/commons/transfer_row.lua
@@ -16,6 +16,7 @@ local Json = require('Module:Json')
 local Logic = require('Module:Logic')
 local Lua = require('Module:Lua')
 local Namespace = require('Module:Namespace')
+local String = require('Module:StringUtils')
 local Table = require('Module:Table')
 local Variables = require('Module:Variables')
 
@@ -69,12 +70,12 @@ end
 ---@return transfer[]
 function TransferRow:readInput()
 	local transferSortIndex = tonumber(Variables.varDefault('transfer_sort_index')) or 0
-	local args = self.args
 
 	self.baseData = self:_readBaseData()
-	self.references = self:_readReferences()
+	local players = self:readPlayers()
+	self.references = self:_readReferences(#players)
 
-	local transfers Array.map(self:readPlayers(), function(player, playerIndex)
+	local transfers Array.map(players, function(player, playerIndex)
 		playerIndex = playerIndex == 1 and '' or playerIndex
 
 		local transfer = self:_convertToTransferStructure{
@@ -94,27 +95,81 @@ end
 
 ---@return table
 function TransferRow:_readBaseData()
-	--TODO
+	local args = self.args
 
-	--[[
-			fromteam = fromTeam or '',
-			fromteamtemplate = fromTeamTemplate or '',
-			toteam = toTeam or '',
-			toteamtemplate = toTeamTemplate or '',
-			role1 = args.role1,
-			role2 = args.role2,
-			date = date,
-			wholeteam = Logic.readBool(args.wholeteam) and 1 or 0,
-			extradata = mw.ext.LiquipediaDB.lpdb_create_json({
-				displaydate = args.date or '',
-				fromteamsec = args.team1_2 and mw.ext.TeamTemplate.teampage(args.team1_2, date) or '',
-				toteamsec = args.team2_2 and mw.ext.TeamTemplate.teampage(args.team2_2, date) or '',
-				role1sec = args.role1_2 or '',
-				role2sec = args.role2_2 or '',
-				platform = args.platform or '',
-				notable = Logic.readBool(args.notableTransfer) and 1 or nil,
-			})
-	]]
+	---@param str string?
+	---@return string?
+	local ucFirst = function(str)
+		return str and String.upperCaseFirst(str) or nil
+	end
+
+	---@param data string[]
+	---@return string[]
+	local switchInactiveIfAppropriate = function(data)
+		if data[1] ~= 'Inactive' or Logic.isEmpty(data[2]) or data[2] == 'Inactive' then
+			return data
+		end
+		return {data[2], data[1]}
+	end
+
+	local fromRole = switchInactiveIfAppropriate(Array.map({args.role1, args.role1_2}, ucFirst))
+	local toRole = switchInactiveIfAppropriate(Array.map({args.role2, args.role2_2}, ucFirst))
+
+	local date = args.date_est or args.date
+	local fromDate = TransferRow._shiftDate(date)
+
+	---@param teamInput string?
+	---@param dateInput string?
+	---@return {name: string?, template: string?}
+	local checkTeam = function(teamInput, dateInput)
+		if not teamInput or not mw.ext.TeamTemplate.teamexists(teamInput) then
+			return {}
+		end
+		local teamData = mw.ext.TeamTemplate.raw(teamInput, dateInput)
+		return {name = teamData.page, template = teamData.templatename}
+	end
+
+	local toTeam = Array.map({args.team2, args.team2_2}, function(input) return checkTeam(input, date) end)
+	local fromTeam = Array.map({args.team1, args.team1_2}, function(input) return checkTeam(input, fromDate) end)
+
+	return {
+		date = date,
+		fromteam = toTeam[1].name or '',
+		fromteamtemplate = toTeam[1].template or '',
+		toteam = fromTeam[1].name or '',
+		toteamtemplate = fromTeam[1].template or '',
+		role1 = fromRole[1],
+		role2 = toRole[1],
+		wholeteam = Logic.readBool(args.wholeteam) and 1 or 0,
+		extradata = {
+			displaydate = args.date or '',
+			fromteamsec = fromTeam[1].name or '',
+			toteamsec = toTeam[2].name or '',
+			notable = Logic.readBool(args.notableTransfer) and 1 or nil,
+			role1sec = fromRole[2] or '',
+			role2sec = toRole[2] or '',
+			platform = self:readPlatform(),
+		},
+	}
+end
+
+---@return string
+function TransferRow:readPlatform()
+	if not self.config.platformIcons then return '' end
+	local getPlatform = require('Module:Platform')
+	self.args.platform = getPlatform._getName(self.args.platform) or ''
+	return self.args.platform
+end
+
+---@param dateInput string?
+---@return string?
+function TransferRow._shiftDate(dateInput)
+	if type(dateInput) ~= 'string' then return dateInput end
+
+	local year, month, day = dateInput:match('(%d+)-(%d+)-(%d+)')
+	local date = os.time{day=day, month=month, year=year}
+	date = date - 86400
+	return os.date( "%Y-%m-%d", date) --[[@as string]]
 end
 
 ---@return standardPlayer[]
@@ -163,7 +218,7 @@ function TransferRow:_convertToTransferStructure(data)
 		nationality = player.flag,
 		role1 = self.baseData.role1 or self.baseData.fromteam and subs[1] and 'Substitute' or nil,
 		role2 = self.baseData.role2 or self.baseData.toteam and subs[2] and 'Substitute' or nil,
-		reference = todo,
+		reference = self.references.applyAll and self.references.refs or self.references.refs['reference' .. playerIndex],
 		extradata = {
 			position = iconData.position,
 			icon = iconData.icon or '',
@@ -184,9 +239,27 @@ function TransferRow:readIconsAndPosition(player, playerIndex)
 	--todo
 end
 
----@return table<integer, string[]>
-function TransferRow:_readReferences()
-	--todo
+---@param numberOfPlayers integer
+---@return {refs: table, applyAll: boolean}
+function TransferRow:_readReferences(numberOfPlayers)
+	if Logic.isEmpty(self.args.ref) or not self.config.referencesAsTable then
+		return {refs = {reference1 = self.args.ref or ''}, applyAll = true}
+	end
+
+	local referencesArray = Array.parseCommaSeparatedString(self.args.ref, ';;;')
+
+	local references = Table.map(referencesArray, function(indedx, ref)
+		return 'reference' .. indedx, Logic.isNotEmpty(ref) and ref or nil
+	end)
+
+	local numberOfReferences = Table.size(references)
+
+	-- same amount of players and references? individually allocate them for LPDB storage
+	-- special case: 2 refs/players (often times this will be a reference from both teams)
+	local allRef = numberOfReferences == numberOfPlayers and
+		(numberOfReferences > 2 or not Logic.isEmpty(self.args.team1) or Logic.isEmpty(self.args.team2))
+
+	return {refs = references, applyAll = allRef}
 end
 
 ---@return self
