0871d2f14c979e093b3a703c66d9162aa0e109f8
hjpalpha <andreasbuesch1@gmail.com>
2024-04-29 10:47:32 +0200

    faction redirect customs

---
+++ components/transfer/wikis/starcraft/transfer_row_custom.lua
@@ -0,0 +1,11 @@
+---
+-- @Liquipedia
+-- wiki=starcraft
+-- page=Module:TransferRow/Custom
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Lua = require('Module:Lua')
+
+return Lua.import('Module:TransferRow/Faction')
---
+++ components/transfer/wikis/warcraft/transfer_row_custom.lua
@@ -0,0 +1,11 @@
+---
+-- @Liquipedia
+-- wiki=warcraft
+-- page=Module:TransferRow/Custom
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Lua = require('Module:Lua')
+
+return Lua.import('Module:TransferRow/Faction')
