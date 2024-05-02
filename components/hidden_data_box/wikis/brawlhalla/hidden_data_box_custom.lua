78a8cf21642f7fd8b2ecd3b523b959029a518e45
Rikard Blixt <rikardblixt@gmail.com>
2024-05-02 10:05:56 +0200

    remove unused vars

--- components/hidden_data_box/wikis/brawlhalla/hidden_data_box_custom.lua
+++ components/hidden_data_box/wikis/brawlhalla/hidden_data_box_custom.lua
@@ -7,9 +7,7 @@
 --
 
 local Class = require('Module:Class')
-local Logic = require('Module:Logic')
 local Lua = require('Module:Lua')
-local Table = require('Module:Table')
 local Tier = require('Module:Tier/Custom')
 local Variables = require('Module:Variables')
 
@@ -35,7 +33,6 @@ function CustomHiddenDataBox.addCustomVariables(args, queryResult)
 	Variables.varDefine('tournament_date', Variables.varDefault('tournament_enddate'))
 
 	Variables.varDefine('tournament_link', Variables.varDefault('tournament_parent'))
-	Variables.varDefine('series', Variables.varDefault('tournament_series'))
 	Variables.varDefine('tournament_region', queryResult.extradata.region)
 	Variables.varDefine('assumedpayout', queryResult.extradata.assumed_payout)
 	Variables.varDefine('tournament_entrants', queryResult.participantsnumber)
@@ -44,8 +41,6 @@ function CustomHiddenDataBox.addCustomVariables(args, queryResult)
 	Variables.varDefine('tournament_tier', Variables.varDefault('tournament_liquipediatier'))
 	Variables.varDefine('tournament_tiertype', Variables.varDefault('tournament_liquipediatiertype'))
 	Variables.varDefine('tournament_ticker_name', Variables.varDefault('tournament_tickername'))
-
-	Variables.varDefine('tournament_subpage', 'true')
 end
 
 return Class.export(CustomHiddenDataBox)
