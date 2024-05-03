65bbe3a34004a90409d8b1bf40b765ecf3e20ec8
Laura van Helvoort <lvanhelvoort@teamliquid.com>
2024-05-03 20:33:31 +0200

    Add an explanation of the function and remove some obsolete jsdoc.

--- javascript/commons/PanelBoxCollapsible.js
+++ javascript/commons/PanelBoxCollapsible.js
@@ -1,6 +1,12 @@
 /**
  * @file This module provides functionality for collapsible panel boxes.
  * @author Elysienna (Laura van Helvoort)
+ *
+ * A unique data attribute ID (data-panel-box-id) is set on the html of the panel box to help track if the panel is
+ * collapsed. If the panel is collapsed, the data-panel-box-id is added to the array of the local storage key.
+ * On init, it will check if the local storage contains panel box ids and applies the collapsed class to the element.
+ * It will then add a click event to the button to toggle the collapsed class and add or remove the panel box id in
+ * local storage.
  */
 
 /**
@@ -9,9 +15,9 @@
 liquipedia.panelBoxCollapsible = {
 
 	/**
-	 * LOCAL_STORAGE_KEY {string} - The key used to store the array of collapsed panel box IDs in local storage.
+	 * LOCAL_STORAGE_KEY {string} - This key used to store the array of collapsed panel box IDs in local storage.
 	 * CLASS_COLLAPSED {string} - The class added to a panel box when it is collapsed.
-	 * DATA_ATTR_PANEL_BOX_ID {string} - The data attribute used to store the ID of a panel box.
+	 * DATA_ATTR_PANEL_BOX_ID {string} - Data attribute used to store the ID of a panel box.
 	 */
 	LOCAL_STORAGE_KEY: 'panelBoxCollapsed',
 	CLASS_COLLAPSED: 'is--collapsed',
@@ -23,8 +29,6 @@ liquipedia.panelBoxCollapsible = {
 	},
 
 	/**
-	 * Handles a panel box element by adding an event listener to its collapsible button and checking if
-	 * it should be collapsed.
 	 * @param {Element} panelBox - The panel box element to handle.
 	 */
 	handlePanelBox: function( panelBox ) {
@@ -45,7 +49,6 @@ liquipedia.panelBoxCollapsible = {
 	},
 
 	/**
-	 * Retrieves the array of collapsed panel box IDs from local storage.
 	 * @return {Array} The array of collapsed panel box IDs.
 	 */
 	getFromLocalStorage: function() {
@@ -58,7 +61,6 @@ liquipedia.panelBoxCollapsible = {
 	},
 
 	/**
-	 * Adds a panel box ID to the array of collapsed panel box IDs in local storage.
 	 * @param {string} id - The ID of the panel box to add.
 	 */
 	setToLocalStorage: function( id ) {
@@ -70,7 +72,6 @@ liquipedia.panelBoxCollapsible = {
 	},
 
 	/**
-	 * Removes a panel box ID from the array of collapsed panel box IDs in local storage.
 	 * @param {string} id - The ID of the panel box to remove.
 	 */
 	removeFromLocalStorage: function( id ) {
@@ -83,7 +84,6 @@ liquipedia.panelBoxCollapsible = {
 	},
 
 	/**
-	 * Checks if a panel box ID is in the array of collapsed panel box IDs in local storage.
 	 * @param {string} id - The ID of the panel box to check.
 	 * @return {boolean} True if the ID is in the array, false otherwise.
 	 */
@@ -93,7 +93,6 @@ liquipedia.panelBoxCollapsible = {
 	},
 
 	/**
-	 * Adds or removes the collapsed class to a panel box element.
 	 * @param {Element} element - The panel box element to add or remove the class to.
 	 */
 	toggleCollapsedClass: function( element ) {
@@ -105,7 +104,6 @@ liquipedia.panelBoxCollapsible = {
 	},
 
 	/**
-	 * Handles a click event on a panel box, collapsing the panel box and adding its ID to local storage if necessary.
 	 * @param {Element} element - The panel box element that was clicked.
 	 * @param {string} id - The ID of the panel box that was clicked.
 	 */
