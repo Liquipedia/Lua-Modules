f799bcc50d4d6672eeeb2c024a3d66072db726c1
mbergen <mail@mbergen.de>
2024-05-04 22:06:46 +0200

    fix(opponent) Add custom opponent with special name resolving

---
+++ components/opponent/wikis/ageofempires/opponent_custom.lua
@@ -0,0 +1,32 @@
+---
+-- @Liquipedia
+-- wiki=ageofempires
+-- page=Module:Opponent/Custom
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Lua = require('Module:Lua')
+local Table = require('Module:Table')
+
+local Opponent = Lua.import('Module:Opponent')
+
+local CustomOpponent = Table.deepCopy(Opponent)
+
+--[[
+Converts a opponent to a name. The name is the same as the one used in the
+match2opponent.name field.
+
+Returns nil if the team template does not exist.
+]]
+---@param opponent standardOpponent
+---@return string
+function CustomOpponent.toName(opponent)
+	if opponent.type == Opponent.team then
+		return mw.ext.TeamTemplate.raw(opponent.template).page
+	else
+		return Opponent.toName(opponent)
+	end
+end
+
+return CustomOpponent
