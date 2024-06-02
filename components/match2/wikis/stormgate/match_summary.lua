b6a8c82a8fbae650da95c5f8378b8a2e06e8da57
Cameron <58013431+FenrirWulf@users.noreply.github.com>
2024-06-02 10:39:45 -0700

    Stormgate

--- components/match2/wikis/stormgate/match_summary.lua
+++ components/match2/wikis/stormgate/match_summary.lua
@@ -165,9 +165,9 @@ function CustomMatchSummary.createBody(match)
 	if match.casters then
 		local casters = Json.parseIfString(match.casters)
 		local casterRow = MatchSummary.Casters()
-		for _, caster in pairs(casters) do
-			casterRow:addCaster(caster)
-		end
+		Array.forEach(casters, function(caster)
+				casterRow:addCaster(caster)
+		end)
 
 		body:addRow(casterRow)
 	end
