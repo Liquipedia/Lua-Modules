3e5d242da96f619afd56bd349c5740a12cf615ce
Hesketh2 <88981446+Hesketh2@users.noreply.github.com>
2024-06-11 20:09:33 +0700

    feat (match2): Add Team Fortress support

---
+++ components/match2/wikis/teamfortress/match_summary.lua
@@ -0,0 +1,360 @@
+---
+-- @Liquipedia
+-- wiki=teamfortress
+-- page=Module:MatchSummary
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Abbreviation = require('Module:Abbreviation')
+local Array = require('Module:Array')
+local Class = require('Module:Class')
+local DateExt = require('Module:Date/Ext')
+local Icon = require('Module:Icon')
+local Json = require('Module:Json')
+local Logic = require('Module:Logic')
+local Lua = require('Module:Lua')
+local Page = require('Module:Page')
+local Table = require('Module:Table')
+local String = require('Module:StringUtils')
+
+local DisplayHelper = Lua.import('Module:MatchGroup/Display/Helper')
+local MatchSummary = Lua.import('Module:MatchSummary/Base')
+
+local MAP_VETO_START = '<b>Start Map Veto</b>'
+local ARROW_LEFT = '[[File:Arrow sans left.svg|15x15px|link=|Left team starts]]'
+local ARROW_RIGHT = '[[File:Arrow sans right.svg|15x15px|link=|Right team starts]]'
+local NONE = '-'
+local TBD = Abbreviation.make('TBD', 'To Be Determined') --[[@as string]]
+
+---@enum TFMatchIcons
+local Icons = {
+	CHECK = Icon.makeIcon{iconName = 'winner', color = 'forest-green-text', size = '110%'},
+	EMPTY = '[[File:NoCheck.png|link=]]',
+}
+
+local LINK_DATA = {
+	logstf = {icon = 'File:Logstf_icon.png', text = 'logs.tf Match Page '},
+	logstfgold = {icon = 'File:Logstf_gold_icon.png', text = 'logs.tf Match Page (Golden Cap) '},
+	esl = {
+		icon = 'File:ESL 2019 icon lightmode.png',
+		iconDark = 'File:ESL 2019 icon darkmode.png',
+		text = 'ESL matchpage'
+	},
+	esea = {icon = 'File:ESEA icon allmode.png', text = 'ESEA Match Page'},
+	etf2l = {icon = 'File:ETF2L.png', text = 'ETF2L Match Page'},
+	rgl = {icon = 'File:RGL_Logo.png', text = 'RGL Match Page'},
+	ozf = {icon = 'File:ozfortress-icon.png‎', text = 'ozfortress Match Page'},
+	tftv = {icon = 'File:Teamfortress.tv.png', text = 'TFTV Match Page'},
+}
+
+local VETO_TYPE_TO_TEXT = {
+	ban = 'BAN',
+	pick = 'PICK',
+	decider = 'DECIDER',
+	defaultban = 'DEFAULT BAN',
+}
+
+local CustomMatchSummary = {}
+
+-- Map Veto Class
+---@class TeamFortressMapVeto: MatchSummaryRowInterface
+---@operator call: self
+---@field root Html
+---@field table Html
+local MapVeto = Class.new(
+	function(self)
+		self.root = mw.html.create('div'):addClass('brkts-popup-mapveto')
+		self.table = self.root:tag('table')
+			:addClass('wikitable-striped'):addClass('collapsible'):addClass('collapsed')
+		self:createHeader()
+	end
+)
+
+---@return self
+function MapVeto:createHeader()
+	self.table:tag('tr')
+		:tag('th'):css('width','33%'):done()
+		:tag('th'):css('width','34%'):wikitext('Map Veto'):done()
+		:tag('th'):css('width','33%'):done()
+	return self
+end
+
+---@param firstVeto number?
+---@param format string?
+---@return self
+function MapVeto:vetoStart(firstVeto, format)
+	format = format and ('Veto format: ' .. format) or nil
+	local textLeft
+	local textCenter
+	local textRight
+	if firstVeto == 1 then
+		textLeft = MAP_VETO_START
+		textCenter = ARROW_LEFT
+		textRight = format
+	elseif firstVeto == 2 then
+		textLeft = format
+		textCenter = ARROW_RIGHT
+		textRight = MAP_VETO_START
+	else return self end
+
+	self.table:tag('tr'):addClass('brkts-popup-mapveto-vetostart')
+		:tag('th'):wikitext(textLeft):done()
+		:tag('th'):wikitext(textCenter):done()
+		:tag('th'):wikitext(textRight):done()
+
+	return self
+end
+
+---@param map1 string?
+---@param map2 string?
+---@return string, string
+function MapVeto._displayMaps(map1, map2)
+	if Logic.isEmpty(map1) and Logic.isEmpty(map2) then
+		return TBD, TBD
+	end
+
+	return Page.makeInternalLink(map1) or NONE,
+		Page.makeInternalLink(map2) or NONE
+end
+
+---@param map string?
+---@return self
+function MapVeto:addDecider(map)
+	map = Page.makeInternalLink(map) or TBD
+	local row = mw.html.create('tr'):addClass('brkts-popup-mapveto-vetoround')
+
+	self:addColumnVetoType(row, 'brkts-popup-mapveto-decider', 'DECIDER')
+	self:addColumnVetoMap(row, map)
+	self:addColumnVetoType(row, 'brkts-popup-mapveto-decider', 'DECIDER')
+
+	self.table:node(row)
+	return self
+end
+
+---@param vetoType string?
+---@param map1 string?
+---@param map2 string?
+---@return self
+function MapVeto:addRound(vetoType, map1, map2)
+	map1, map2 = MapVeto._displayMaps(map1, map2)
+
+	local vetoText = VETO_TYPE_TO_TEXT[vetoType]
+
+	if not vetoText then return self end
+
+	local class = 'brkts-popup-mapveto-' .. vetoType
+
+	local row = mw.html.create('tr'):addClass('brkts-popup-mapveto-vetoround')
+
+	self:addColumnVetoMap(row, map1)
+	self:addColumnVetoType(row, class, vetoText)
+	self:addColumnVetoMap(row, map2)
+
+	self.table:node(row)
+	return self
+end
+
+---@param row Html
+---@param styleClass string
+---@param vetoText string
+---@return self
+function MapVeto:addColumnVetoType(row, styleClass, vetoText)
+	row:tag('td')
+		:tag('span')
+			:addClass(styleClass)
+			:addClass('brkts-popup-mapveto-vetotype')
+			:wikitext(vetoText)
+	return self
+end
+
+---@param row Html
+---@param map string
+---@return self
+function MapVeto:addColumnVetoMap(row, map)
+	row:tag('td'):wikitext(map):done()
+	return self
+end
+
+---@return Html
+function MapVeto:create()
+	return self.root
+end
+
+---@param args table
+---@return Html
+function CustomMatchSummary.getByMatchId(args)
+	return MatchSummary.defaultGetByMatchId(CustomMatchSummary, args)
+end
+
+---@param match MatchGroupUtilMatch
+---@param footer MatchSummaryFooter
+---@return MatchSummaryFooter
+function CustomMatchSummary.addToFooter(match, footer)
+	footer = MatchSummary.addVodsToFooter(match, footer)
+		:addLinks(LINK_DATA, match.links)
+
+	---@param link string?
+	---@param linkType string 
+	---@param gameIndex integer
+	local addLink = function(link, linkType, gameIndex)
+		if Logic.isEmpty(link) then return end
+		local linkData = LINK_DATA[linkType]
+		footer:addLink(link, linkData.icon, linkData.iconDark, linkData.text .. 'for game ' .. gameIndex)
+	end
+
+	Array.forEach(match.games or {}, function(game, gameIndex)
+		local extradata = game.extradata or {}
+		addLink(extradata.logstf, 'logstf', gameIndex)
+		addLink(extradata.logstfgold, 'logstfgold', gameIndex)
+		--add further such game specific stuff here if there is ...
+	end)
+
+	return footer
+end
+
+---@param match MatchGroupUtilMatch
+---@return MatchSummaryBody
+function CustomMatchSummary.createBody(match)
+	local body = MatchSummary.Body()
+
+	if match.dateIsExact or match.timestamp ~= DateExt.defaultTimestamp then
+		-- dateIsExact means we have both date and time. Show countdown
+		-- if match is not epoch=0, we have a date, so display the date
+		body:addRow(MatchSummary.Row():addElement(
+			DisplayHelper.MatchCountdownBlock(match)
+		))
+	end
+
+	-- Iterate each map
+	for _, game in ipairs(match.games) do
+		if game.map then
+			body:addRow(CustomMatchSummary._createMapRow(game))
+		end
+	end
+
+	-- Add Match MVP(s)
+	if match.extradata.mvp then
+		local mvpData = match.extradata.mvp
+		if not Table.isEmpty(mvpData) and mvpData.players then
+			local mvp = MatchSummary.Mvp()
+			for _, player in ipairs(mvpData.players) do
+				mvp:addPlayer(player)
+			end
+			mvp:setPoints(mvpData.points)
+
+			body:addRow(mvp)
+		end
+
+	end
+
+	-- Add casters
+	if String.isNotEmpty(match.extradata.casters) then
+		local casters = Json.parseIfString(match.extradata.casters)
+		local casterRow = MatchSummary.Casters()
+		for _, caster in pairs(casters) do
+			casterRow:addCaster(caster)
+		end
+
+		body:addRow(casterRow)
+	end
+
+	-- Add the Map Vetoes
+	if match.extradata.mapveto then
+		local vetoData = match.extradata.mapveto
+		if vetoData then
+			local mapVeto = MapVeto()
+
+			for _,vetoRound in ipairs(vetoData) do
+				if vetoRound.vetostart then
+					mapVeto:vetoStart(tonumber(vetoRound.vetostart))
+				end
+				if vetoRound.type == 'decider' then
+					mapVeto:addDecider(vetoRound.decider)
+				else
+					mapVeto:addRound(vetoRound.type, vetoRound.team1, vetoRound.team2)
+				end
+			end
+
+			body:addRow(mapVeto)
+		end
+	end
+
+	return body
+end
+---@param game MatchGroupUtilGame
+---@param opponentIndex integer
+---@return Html
+function CustomMatchSummary._gameScore(game, opponentIndex)
+	return mw.html.create('div'):wikitext(game.scores[opponentIndex])
+end
+
+---@param game MatchGroupUtilGame
+---@return MatchSummaryRow
+function CustomMatchSummary._createMapRow(game)
+	local row = MatchSummary.Row()
+
+	-- Add Header
+	if Logic.isNotEmpty(game.header) then
+		local mapHeader = mw.html.create('div')
+			:wikitext(game.header)
+			:css('font-weight','bold')
+			:css('font-size','85%')
+			:css('margin','auto')
+		row:addElement(mapHeader)
+		row:addElement(MatchSummary.Break():create())
+	end
+
+	local centerNode = mw.html.create('div')
+		:addClass('brkts-popup-spaced')
+		:wikitext(Page.makeInternalLink(game.map))
+		:css('text-align', 'center')
+
+	if game.resultType == 'np' then
+		centerNode:addClass('brkts-popup-spaced-map-skip')
+	end
+
+	local leftNode = mw.html.create('div')
+		:addClass('brkts-popup-spaced')
+		:node(CustomMatchSummary._createCheckMarkOrCross(game.winner == 1, Icons.CHECK))
+		:node(CustomMatchSummary._gameScore(game, 1))
+		:css('width', '20%')
+
+	local rightNode = mw.html.create('div')
+		:addClass('brkts-popup-spaced')
+		:node(CustomMatchSummary._gameScore(game, 2))
+		:node(CustomMatchSummary._createCheckMarkOrCross(game.winner == 2, Icons.CHECK))
+		:css('width', '20%')
+
+	row:addElement(leftNode)
+		:addElement(centerNode)
+		:addElement(rightNode)
+
+	row:addClass('brkts-popup-body-game')
+		:css('overflow', 'hidden')
+
+	-- Add Comment
+	if Logic.isNotEmpty(game.comment) then
+		row:addElement(MatchSummary.Break():create())
+		local comment = mw.html.create('div')
+			:wikitext(game.comment)
+			:css('margin', 'auto')
+		row:addElement(comment)
+	end
+
+	return row
+end
+
+---@param showIcon boolean?
+---@param iconType TeamFortressMatchIcons?
+---@return Html
+function CustomMatchSummary._createCheckMarkOrCross(showIcon, iconType)
+	local container = mw.html.create('div'):addClass('brkts-popup-spaced'):css('line-height', '27px')
+
+	if showIcon then
+		return container:node(iconType)
+	end
+	return container:node(Icons.EMPTY)
+end
+
+return CustomMatchSummary
