364cc902977ab99efb3131a36546976366a7674d
hjpalpha <andreasbuesch1@gmail.com>
2024-06-26 18:06:48 +0200

    refactor `Module:Links/Stream` - get rid of some anno warnings - add display components (in prep for moving it from js to lua) - add filter function - allow 2nd stream for any platform

--- standard/links_stream.lua
+++ standard/links_stream.lua
@@ -11,18 +11,19 @@ Module containing utility functions for streaming platforms.
 ]]
 local StreamLinks = {}
 
+local Array = require('Module:Array')
 local Class = require('Module:Class')
 local Logic = require('Module:Logic')
+local Page = require('Module:Page')
 local String = require('Module:StringUtils')
+local Table = require('Module:Table')
 local Variables = require('Module:Variables')
 
---[[
-List of streaming platforms supported in Module:Countdown. This is a subset of
-the list in Module:Links
-]]
+local TL_STREAM = 'stream'
+
+--List of streaming platforms supported in Module:Countdown.
 StreamLinks.countdownPlatformNames = {
 	'afreeca',
-	'afreecatv',
 	'bilibili',
 	'cc163',
 	'dailymotion',
@@ -34,30 +35,32 @@ StreamLinks.countdownPlatformNames = {
 	'loco',
 	'mildom',
 	'nimo',
-	'stream',
+	TL_STREAM,
 	'tl',
 	'trovo',
 	'twitch',
-	'twitch2',
 	'youtube',
 }
-
---[[
-Lookup table of allowed inputs that use a plattform with a different name
-]]
-StreamLinks.streamPlatformLookupNames = {
-	twitch2 = 'twitch',
+local PLATFORM_TO_ICON = {
+	cc163 = 'cc',
+	huomao = 'huomaotv',
+	huya = 'huyatv',
+	himo = 'nimotv',
 }
+
+---@param key string
+---@return boolean
+function StreamLinks.isStream(key)
+	return Array.any(StreamLinks.countdownPlatformNames, function(platform)
+		return String.startsWith(key, platform)
+	end)
+end
 
 ---Extracts the streaming platform args from an argument table for use in Module:Countdown.
 ---@param args {[string]: string}
 ---@return table
 function StreamLinks.readCountdownStreams(args)
-	local stream = {}
-	for _, platformName in ipairs(StreamLinks.countdownPlatformNames) do
-		stream[platformName] = args[platformName]
-	end
-	return stream
+	return Table.filterByKey(args, StreamLinks.isStream)
 end
 
 ---Resolves the value of a stream given the platform
@@ -83,26 +86,113 @@ function StreamLinks.processStreams(forwardedInputArgs)
 		forwardedInputArgs.stream = nil
 	end
 
+	local processedStreams = {}
 	for _, platformName in pairs(StreamLinks.countdownPlatformNames) do
-		local streamValue = Logic.emptyOr(
-			streams[platformName],
-			forwardedInputArgs[platformName],
-			Variables.varDefault(platformName)
+		local streamValues = Table.merge(
+			Table.filterByKey(forwardedInputArgs, StreamLinks.isStream),
+			Table.filterByKey(streams, StreamLinks.isStream)
 		)
 
-		if String.isNotEmpty(streamValue) then
-			-- stream has no platform
-			if platformName ~= 'stream' then
-				local lookUpPlatform = StreamLinks.streamPlatformLookupNames[platformName] or platformName
+		if Table.isEmpty(streamValues) then
+			streamValues = {[platformName] = Variables.varDefault(platformName)}
+		end
 
-				streamValue = StreamLinks.resolve(lookUpPlatform, streamValue)
+		Table.mergeInto(processedStreams, StreamLinks._processStreamsOfPlatform(streamValues, platformName))
+	end
+
+	return processedStreams
+end
+
+---@param streamValues {[string]: string}
+---@param platformName string
+---@return {[string]: string}
+function StreamLinks._processStreamsOfPlatform(streamValues, platformName)
+	local platformStreams = {}
+	local legacyStreams = {}
+	local enCounter = 0
+
+	for key, streamValue in Table.iter.spairs(streamValues) do
+		if platformName ~= TL_STREAM then
+			streamValue = StreamLinks.resolve(platformName, streamValue)
+		end
+
+		-- legacy key
+		if key:match('^' .. platformName .. '%d*$') then
+			table.insert(legacyStreams, streamValue)
+		elseif key:match('^' .. platformName .. '_%a+_%d+') then
+			local streamKey = StreamLinks.StreamKey(key)
+			if streamKey.languageCode == 'en' then
+				enCounter = enCounter + 1
 			end
-
-			local key = StreamLinks.StreamKey(platformName):toString()
-			streams[key] = streamValue
-			streams[platformName] = streamValue -- Legacy
+			platformStreams[streamKey:toString()] = streamValue
 		end
 	end
+
+	for _, streamValue in ipairs(legacyStreams) do
+		if not Table.includes(platformStreams, streamValue) then
+			enCounter = enCounter + 1
+			local streamKey = StreamLinks.StreamKey(platformName, 'en', enCounter):toString()
+			platformStreams[streamKey] = streamValue
+			platformStreams[platformName] = streamValue -- Legacy
+		end
+	end
+
+	return platformStreams
+end
+
+---@param platform string
+---@param streamValue string
+---@return string?
+function StreamLinks.displaySingle(platform, streamValue)
+	local icon = '<i class="lp-icon lp-icon-21 lp-' .. (PLATFORM_TO_ICON[platform] or platform) .. '"></i>'
+	if platform == TL_STREAM then
+		return Page.makeExternalLink(icon, 'https://tl.net/video/streams/' .. streamValue)
+	end
+
+	local streamLink = StreamLinks.resolve(platform, streamValue)
+	if not streamLink then return nil end
+
+	return Page.makeInternalLink({}, icon, 'Special:Stream/' .. platform .. '/' .. streamValue)
+end
+
+---@param streams {string: string[]}
+---@return string?
+function StreamLinks.display(streams)
+	local displays = {}
+	Array.forEach(StreamLinks.countdownPlatformNames, function(platform)
+		Array.forEach(streams[platform] or {}, function(streamValue)
+			table.insert(displays, StreamLinks.displaySingle(platform, streamValue))
+		end)
+	end)
+	if Table.isEmpty(displays) then return nil end
+	return table.concat(displays, ' ')
+end
+
+---filters streams so that if english streams are present only those are returned
+---@param streamsInput {string: string}
+---@return {string: string[]}
+function StreamLinks.filterStreams(streamsInput)
+	local hasEnglishStream = Table.any(streamsInput, function(key)
+		return key:match('_en_') or Table.includes(StreamLinks.countdownPlatformNames, key)
+	end)
+
+	local streams = {}
+	for rawHost, stream in Table.iter.spairs(streamsInput) do
+		if #(mw.text.split(rawHost, '_', true)) == 3 then
+			local streamKey = StreamLinks.StreamKey(rawHost)
+			local platform = streamKey.platform
+			if not streams[platform] then
+				streams[platform] = {}
+			end
+			table.insert(streams[platform], (not hasEnglishStream or streamKey.languageCode == 'en') and stream or nil)
+		end
+	end
+
+	Array.forEach(StreamLinks.countdownPlatformNames, function(platformName)
+		local stream = streamsInput[platformName]
+		if type(streams[platformName]) == 'table' or String.isEmpty(stream) then return end
+		streams[platformName] = {stream, streamsInput[platformName .. 2]}
+	end)
 
 	return streams
 end
@@ -110,20 +200,19 @@ end
 --- StreamKey Class
 -- Contains the triplet that makes up a stream key
 -- [platform, languageCode, index]
----@class StreamKey
+---@class StreamKey: BaseClass
 ---@operator call(...): StreamKey
 ---@field platform string
 ---@field languageCode string
 ---@field index integer
----@field is_a function
-StreamLinks.StreamKey = Class.new(
+local StreamKey = Class.new(
 	function (self, ...)
 		self:new(...)
 	end
 )
-local StreamKey = StreamLinks.StreamKey
+StreamLinks.StreamKey = StreamKey
 
----@param tbl string
+---@param tbl string|StreamKey
 ---@param languageCode string
 ---@param index integer
 ---@overload fun(self, tbl: StreamKey): StreamKey
@@ -142,11 +231,15 @@ function StreamKey:new(tbl, languageCode, index)
 		local components = mw.text.split(tbl, '_', true)
 		-- Input is in legacy format (eg. twitch2)
 		if #components == 1 then
-			platform, index = self:_fromLegacy(tbl)
+			local tempIndex
+			platform, tempIndex = self:_fromLegacy(tbl)
+			index = tempIndex or index
 			languageCode = 'en'
 		-- Input is a StreamKey in string format
 		elseif #components == 3 then
-			platform, languageCode, index = unpack(components)
+			local stringIndex
+			platform, languageCode, stringIndex = unpack(components)
+			index = tonumber(stringIndex) --[[@as integer]]
 		end
 	end
 
@@ -161,27 +254,24 @@ end
 ---@return string?, integer?
 function StreamKey:_fromLegacy(input)
 	for _, platform in pairs(StreamLinks.countdownPlatformNames) do
-		-- The intersection of values in countdownPlatformNames and keys in streamPlatformLookupNames
-		-- are not valid platforms. E.g. "twitch2" is not a valid platform.
-		if not StreamLinks.streamPlatformLookupNames[platform] then
-			-- Check if this platform matches the input
-			if string.find(input, platform .. '%d-$') then
-				local index = 1
-				-- If the input is longer than the platform, there's an index at the end
-				-- Eg. In "twitch2", the 2 would the index.
-				if #input > #platform then
-					index = tonumber(input:sub(#platform + 1)) --[[@as integer]]
-				end
-				return platform, index
+		if string.find(input, platform .. '%d-$') then
+			local index = 1
+			-- If the input is longer than the platform, there's an index at the end
+			-- Eg. In "twitch2", the 2 would be the index.
+			if #input > #platform then
+				index = tonumber(input:sub(#platform + 1)) or index
 			end
+			return platform, index
 		end
 	end
 end
 
+---@return string
 function StreamKey:toString()
 	return self.platform .. '_' .. self.languageCode .. '_' .. self.index
 end
 
+---@return string
 function StreamKey:toLegacy()
 	-- Return twitch instead of twitch1
 	if self.index == 1 then
@@ -190,6 +280,7 @@ function StreamKey:toLegacy()
 	return self.platform .. self.index
 end
 
+---@return boolean
 function StreamKey:_isValid()
 	assert(Logic.isNotEmpty(self.platform), 'StreamKey: Platform is required')
 	assert(Logic.isNotEmpty(self.languageCode), 'StreamKey: Language Code is required')
