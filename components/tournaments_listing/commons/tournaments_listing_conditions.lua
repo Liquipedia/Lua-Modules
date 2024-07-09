a805419ff14f0588b8fa89f92212bf99397de82d
mbergen <mail@mbergen.de>
2024-07-09 11:10:11 +0200

    feat(TournamentsListing): Allow to query for multiple series (#4377)
    * feat(TournamentsListing) Allow to query for multiple series
    
    * Update tournaments_listing_conditions.lua
    
    * Fix issue without series input, review comment

--- components/tournaments_listing/commons/tournaments_listing_conditions.lua
+++ components/tournaments_listing/commons/tournaments_listing_conditions.lua
@@ -65,11 +65,15 @@ function TournamentsListingConditions.base(args)
 		conditions:add{ConditionNode(ColumnName('game'), Comparator.eq, args.game)}
 	end
 
-	if args.series then
-		conditions:add{ConditionTree(BooleanOperator.any):add{
-			ConditionNode(ColumnName('series'), Comparator.eq, args.series),
-			ConditionNode(ColumnName('extradata_series2'), Comparator.eq, args.series)
-		}}
+	if args.series1 or args.series then
+		local seriesConditions = ConditionTree(BooleanOperator.any)
+		for _, series in Table.iter.pairsByPrefix(args, 'series', {requireIndex = false}) do
+			seriesConditions:add{
+				ConditionNode(ColumnName('series'), Comparator.eq, series),
+				ConditionNode(ColumnName('extradata_series2'), Comparator.eq, series)
+			}
+		end
+		conditions:add{seriesConditions}
 	end
 
 	if args.location then
