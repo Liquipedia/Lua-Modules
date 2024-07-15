37746ed5bb50b1d219f544457b687765e36070e9
mbergen <mail@mbergen.de>
2024-07-15 16:39:12 +0200

    fix(squad): newteamdate is not working (#4388)
    * fix(squad) Use newteamdate from extradata
    
    * Add cleaning
    
    * Adjust to cleaned value
    
    * Update squad_row.lua
    
    * Filter empty string
    
    Co-authored-by: Rikard Blixt <rikardblixt@gmail.com>
    
    ---------
    
    Co-authored-by: Rikard Blixt <rikardblixt@gmail.com>

--- components/squad/commons/squad_utils.lua
+++ components/squad/commons/squad_utils.lua
@@ -146,7 +146,7 @@ function SquadUtils.readSquadPersonArgs(args)
 		extradata = {
 			loanedto = args.team,
 			loanedtorole = args.teamrole,
-			newteamdate = args.newteamdate,
+			newteamdate = String.nilIfEmpty(ReferenceCleaner.clean(args.newteamdate)),
 			faction = Faction.read(args.faction or args.race),
 		},
 	}
