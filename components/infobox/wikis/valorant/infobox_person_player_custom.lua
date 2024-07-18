baa69e419f8422f128acb04bbfd35d3ed5e07d6f
Verus <145709809+iamentry@users.noreply.github.com>
2024-07-18 00:48:52 -0500

    Update components/infobox/wikis/valorant/infobox_person_player_custom.lua
    Co-authored-by: hjpalpha <75081997+hjpalpha@users.noreply.github.com>

--- components/infobox/wikis/valorant/infobox_person_player_custom.lua
+++ components/infobox/wikis/valorant/infobox_person_player_custom.lua
@@ -91,11 +91,11 @@ function CustomInjector:parse(id, widgets)
 		}
 	elseif id == 'status' then
 		Array.appendWith(widgets,
-		Cell{name = 'Years Active (Player)', content = {args.years_active}},
-		Cell{name = 'Years Active (' .. Abbreviation.make('Org', 'Organisation') .. ')', content = {args.years_active_org}},
-		Cell{name = 'Years Active (Coach)', content = {args.years_active_coach}},
-		Cell{name = 'Years Active (Talent)', content = {args.years_active_talent}}
-	)
+			Cell{name = 'Years Active (Player)', content = {args.years_active}},
+			Cell{name = 'Years Active (' .. Abbreviation.make('Org', 'Organisation') .. ')', content = {args.years_active_org}},
+			Cell{name = 'Years Active (Coach)', content = {args.years_active_coach}},
+			Cell{name = 'Years Active (Talent)', content = {args.years_active_talent}}
+		)
 
 	elseif id == 'role' then
 		return {
