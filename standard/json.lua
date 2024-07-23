f87ff5ff3ba5618469bf54010cbb8abe367278b7
hjpalpha <andreasbuesch1@gmail.com>
2024-07-23 10:14:13 +0200

    module:json adjusts

--- standard/json.lua
+++ standard/json.lua
@@ -9,7 +9,11 @@
 local Json = {}
 
 local Arguments = require('Module:Arguments')
+local FnUtil = require('Module:FnUtil')
 local Table = require('Module:Table')
+
+local ERROR_PATTERN = '<span class="scribunto%-error" id="mw%-scribunto%-error%-%d">Lua error%s?i?n?%s?:? (.*)%.</span>'
+local JSON_ERROR_PATTERN = 'Module:Json/?d?e?v? at line %d+: Tried to parse Lua error &quot;(.*)&quot;'
 
 ---Json-stringifies all arguments from a supplied frame.
 ---@param frame Frame
@@ -59,28 +63,32 @@ end
 ---If the parse fails it returns an empty table.
 ---Second return value boolean indicates a failed parse.
 ---@param obj string
+---@param options {checkForError: boolean?}?
 ---@return table, boolean
----@overload fun(obj: any): {}, true
-function Json.parse(obj)
+---@overload fun(obj: any, options: {checkForError: boolean?}?): {}, true
+function Json.parse(obj, options)
+	options = options or {}
 	local parse = function(object) return mw.text.jsonDecode(object, mw.text.JSON_TRY_FIXING) end
 	local status, res = pcall(parse, obj);
 	if status then
 		return res, false
-	else
-		mw.log('Error: could not parse Json:')
-		mw.logObject(obj)
-		mw.log(debug.traceback())
-		return {}, true
+	elseif options.checkForError then
+		Json.checkForError(obj)
 	end
+	mw.log('Error: could not parse Json:')
+	mw.logObject(obj)
+	mw.log(debug.traceback())
+	return {}, true
 end
 
 ---Parses a given object if it is a string. Else it returns the given object.
 ---@param obj string
+---@param options {checkForError: boolean?}?
 ---@return table, boolean
----@overload fun(obj: any): any
-function Json.parseIfString(obj)
+---@overload fun(obj: any, options: {checkForError: boolean?}?): any
+function Json.parseIfString(obj, options)
 	if type(obj) == 'string' then
-		return Json.parse(obj)
+		return Json.parse(obj, options)
 	else
 		return obj
 	end
@@ -91,16 +99,20 @@ end
 ---
 ---Example: JsonExt.parseIfTable('{"a" = 3}') = {a = 3}
 ---@param any string
+---@param options {checkForError: boolean?}?
 ---@return table?
----@overload fun(any: any): nil
-function Json.parseIfTable(any)
+---@overload fun(any: any, options: {checkForError: boolean?}?): nil
+function Json.parseIfTable(any, options)
+	options = options or {}
 	if type(any) == 'string' then
 		local firstChar = any:sub(1, 1)
 		if firstChar == '{' or firstChar == '[' then
-			local result, hasError = Json.parse(any)
+			local result, hasError = Json.parse(any, options)
 			if not hasError then
 				return result
 			end
+		elseif options.checkForError then
+			Json.checkForError(any)
 		end
 	end
 	return nil
@@ -109,15 +121,35 @@ end
 ---Parses a given JSON input from a template call to `Json.stringify()`.
 ---If the parse fails it returns the original input.
 ---Second return value boolean indicates a failed parse.
+---@param options {checkForError: boolean?}?
 ---@param any string
 ---@return table, boolean
----@overload fun(any: any): any, true
-function Json.parseStringified(any)
-	local tbl = Json.parseIfTable(any)
+---@overload fun(options: {checkForError: boolean?}?, any: any): any, true
+---@overload fun(options: string): table, boolean
+---@overload fun(options: any): any, true
+function Json.parseStringified(options, any)
+	if not any and type(options) == 'string' then
+		any = options
+		options = nil
+	end
+	local tbl = Json.parseIfTable(any, options)
 	if not tbl then
 		return any, true
 	end
-	return Table.mapValues(tbl, Json.parseStringified), false
+	return Table.mapValues(tbl, FnUtil.curry(Json.parseStringified, options)), false
+end
+
+---throws if an lua script error is found in the provided string
+---@param str string?
+function Json.checkForError(str)
+	if type(str) ~= 'string' then return end
+
+	local errorMessage = string.match(str, ERROR_PATTERN)
+
+	if not errorMessage then return end
+
+	error('Tried to parse Lua error "' ..
+		errorMessage:gsub(JSON_ERROR_PATTERN, '%1') .. '"')
 end
 
 return Json
