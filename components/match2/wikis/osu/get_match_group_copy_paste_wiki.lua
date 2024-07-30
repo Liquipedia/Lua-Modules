bc6c258e6b196541a7f5c64c6da67a8fa28d7d2f
Hesketh2 <88981446+Hesketh2@users.noreply.github.com>
2024-07-30 16:13:31 +0700

    (WIP) still incomplete, working on it

--- components/match2/wikis/osu/get_match_group_copy_paste_wiki.lua
+++ components/match2/wikis/osu/get_match_group_copy_paste_wiki.lua
@@ -8,6 +8,7 @@
 
 local Array = require('Module:Array')
 local Class = require('Module:Class')
+local Logic = require('Module:Logic')
 local Lua = require('Module:Lua')
 
 local BaseCopyPaste = Lua.import('Module:GetMatchGroupCopyPaste/wiki/Base')
@@ -15,16 +16,16 @@ local BaseCopyPaste = Lua.import('Module:GetMatchGroupCopyPaste/wiki/Base')
 ---@class OsuMatch2CopyPaste: Match2CopyPasteBase
 local WikiCopyPaste = Class.new(BaseCopyPaste)
 
+local INDENT = BaseCopyPaste.Indent
 local VETOES = {
-	[0] = '',
-	[1] = 'ban,ban,ban,decider',
-	[2] = 'ban,ban,pick,ban',
-	[3] = 'ban,pick,ban,decider',
-	[5] = 'ban,pick,pick,decider',
-	[7] = 'ban,pick,pick,pick,decider',
-	[9] = 'ban,pick,pick,pick,pick,decider',
-	[11] = 'ban,pick,pick,pick,pick,pick,decider',
-	[13] = 'ban,pick,pick,pick,pick,pick,pick,decider',
+	[1] = {'ban', 'ban', 'ban', 'decider'},
+	[2] = {'ban', 'ban', 'pick', 'ban'},
+	[3] = {'ban', 'pick', 'ban', 'decider'},
+	[5] = {'ban', 'pick', 'pick', 'decider'},
+	[7] = {'ban', 'pick', 'pick', 'pick', 'decider'},
+	[9] = {'ban', 'pick', 'pick', 'pick', 'pick', 'decider'},
+	[11] = {'ban', 'pick', 'pick', 'pick', 'pick', 'pick', 'decider'},
+	[13] = {'ban', 'pick', 'pick', 'pick', 'pick', 'pick', 'pick', 'decider'},
 }
 
 --returns the Code for a Match, depending on the input
@@ -35,74 +36,46 @@ local VETOES = {
 ---@param args table
 ---@return string
 function WikiCopyPaste.getMatchCode(bestof, mode, index, opponents, args)
-	local indent = '  '
-	local showScore = args.score == 'true'
-	local mapVeto = args.mapVeto == 'true'
-	local protectVeto = args.protect == 'true'
-	local streams = args.streams == 'true'
-	local casters = args.casters == 'true'
-	local mplinks = args.mplinks == 'true'
-	local lines = {}
-	table.insert(lines, '{{Match|bestof=' .. bestof)
-	table.insert(lines, indent .. '|date=')
-
-	if casters then
-		table.insert(lines, indent .. '|caster1=|caster2=')
-	end
-
-	if streams then
-		table.insert(lines, indent .. '|twitch=|youtube=|vod=')
-	end
-
-	if mplinks then
-		table.insert(lines, indent .. '|mplink=|mplink2=|mplink3=')
-	end
-
-	for i = 1, opponents do
-		table.insert(lines, indent .. '|opponent' .. i .. '=' .. WikiCopyPaste._getOpponent(mode, showScore))
-	end
-
-	if mapVeto and VETOES[bestof] then
-		local types = VETOES[bestof]
-		if protectVeto then
-			types = 'protect,' .. types
-		end
-
-		local vetotypes = mw.text.split(types, ',', true)
-		table.insert(lines, indent .. '|mapveto={{MapVeto')
-		table.insert(lines, indent .. indent .. '|firstpick=')
-		table.insert(lines, indent .. indent .. '|types=' .. types)
-		Array.forEach(vetotypes,
-			function(vetotype, index)
-				if vetotype == 'protect' or vetotype == 'ban' or vetotype == 'pick' then
-					table.insert(lines, indent .. indent .. '|t1map' .. index .. '=|t2map' .. index .. '=')
-				elseif vetotype == 'decider' then
-					table.insert(lines, indent .. indent .. '|decider=')
-				end
-			end
-		)
-		table.insert(lines, indent .. '}}')
-	end
-	for i = 1, bestof do
-		table.insert(lines, indent .. '|map' .. i .. '={{Map|map=|mode=|score1=|score2=|winner=')
-		lines[#lines] = lines[#lines] .. '}}'
-	end
-	table.insert(lines,'}}')
+	local lines = Array.extend({},
+		'{{Match|bestof=' .. bestof,
+		INDENT .. '|date=',
+		Logic.readBool(args.casters) and (INDENT .. '|caster1=|caster2=') or nil,
+		Logic.readBool(args.streams) and (INDENT .. '|twitch=|youtube=|vod=') or nil,
+		Logic.readBool(args.mplinks) and (INDENT .. '|mplink=|mplink2=|mplink3=') or nil,
+		Array.map(Array.range(1, opponents), function(opponentIndex)
+			return INDENT .. '|opponent' .. opponentIndex .. '=' .. BaseCopyPaste.getOpponent(mode, Logic.readBool(args.score))
+		end),
+		WikiCopyPaste._getVetoes(args, bestof),
+		Array.map(Array.range(1, bestof), function(mapIndex)
+			return INDENT .. '|map' .. mapIndex .. '={{Map|map=|mode=|score1=|score2=|winner=}}'
+		end) or nil,
+		'}}'
+	)
 
 	return table.concat(lines, '\n')
 end
 
---subfunction used to generate the code for the Opponent template, depending on the type of opponent
-function WikiCopyPaste._getOpponent(mode, showScore)
-	local score = showScore and '|score=' or ''
-
-	if mode == 'team' then
-		return '{{TeamOpponent|' .. score .. '}}'
-	elseif mode == 'solo' then
-		return '{{SoloOpponent||flag=' .. (score or '') .. '}}'
-	elseif mode == 'literal' then
-		return '{{LiteralOpponent|}}'
+---@param args table
+---@param bestof integer
+---@return string[]?
+function WikiCopyPaste._getVetoes(args, bestof)
+	local vetoTypes = VETOES[bestof]
+	if not Logic.readBool(args.mapVeto) or not vetoTypes then return nil end
+	if Logic.readBool(args.protect) then
+		table.insert(vetoTypes, 1, 'protect')
 	end
+	return Array.append({},
+		INDENT .. '|mapveto={{MapVeto',
+		INDENT .. INDENT .. '|firstpick=',
+		INDENT .. INDENT .. '|types=' .. table.concat(vetoTypes, ','),
+		Array.map(vetoTypes, function(vetoType, vetoIndex)
+			if vetoType == 'decider' then
+				return INDENT .. INDENT .. '|decider='
+			end
+			return INDENT .. INDENT .. '|t1map' .. vetoIndex .. '=|t2map' .. vetoIndex .. '='
+		end),
+		INDENT .. '}}'
+	)
 end
 
 return WikiCopyPaste
