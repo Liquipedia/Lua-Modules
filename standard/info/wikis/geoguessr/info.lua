872d9442846a187dbafd5feece88eaae260ae9ff
Hesketh2 <88981446+Hesketh2@users.noreply.github.com>
2024-08-08 23:02:40 +0700

    feat(info): add GeoGuessr (#4483)
    * feat(info): Setup for GeoGuessr
    
    * Update info.lua
    
    * Adapt #4495
    
    * Update info.lua
    
    * Fix the abbreviation
    
    abbreviation should be just the full game name here.
    
    I think we can skip the manual width here, players name are not too long

---
+++ standard/info/wikis/geoguessr/info.lua
@@ -0,0 +1,40 @@
+---
+-- @Liquipedia
+-- wiki=geoguessr
+-- page=Module:Info
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+return {
+	startYear = 2013,
+	wikiName = 'geoguessr',
+	name = 'GeoGuessr',
+	defaultGame = 'geoguessr',
+	games = {
+		geoguessr = {
+			abbreviation = 'GeoGuessr',
+			name = 'GeoGuessr',
+			link = 'GeoGuessr',
+			logo = {
+				darkMode = 'GeoGuessr allmode.png',
+				lightMode = 'GeoGuessr allmode.png',
+			},
+			defaultTeamLogo = {
+				darkMode = 'GeoGuessr allmode.png',
+				lightMode = 'GeoGuessr allmode.png',
+			},
+		},
+	},
+	config = {
+		squads = {
+			hasPosition = false,
+			hasSpecialTeam = false,
+			allowManual = false,
+		},
+		match2 = {
+		},
+	},
+	defaultRoundPrecision = 0,
+	match2 = 0,
+}
