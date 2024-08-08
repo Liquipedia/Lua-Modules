97a2cab96af188111ee51fef336763a5795f9198
Rikard Blixt <rikardblixt@gmail.com>
2024-08-08 15:11:08 +0200

    feat(standard): build lightweight i18n library (#4493)
    * feat(standard): build lightweight i18n library
    
    * eof's

---
+++ standard/i18n.lua
@@ -0,0 +1,30 @@
+---
+-- @Liquipedia
+-- wiki=commons
+-- page=Module:I18n
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local I18nData = mw.loadData('Module:I18n/Data')
+local String = require('Module:StringUtils')
+
+local I18n = {}
+
+local LANGUAGE = mw.language.getContentLanguage():getCode()
+
+---Interpolaes an i18n string with data
+---TODO Add pluralization support (https://cldr.unicode.org/index/cldr-spec/plural-rules)
+---@param key string
+---@param data table<string, string|number>
+---@return string
+function I18n.translate(key, data)
+	local langMessages = I18nData[LANGUAGE] or I18nData.en
+	local message = langMessages[key] or I18nData.en[key]
+	if not message then
+		return '⧼' .. key .. '⧽'
+	end
+	return String.interpolate(message, data)
+end
+
+return I18n
---
+++ standard/i18n_data.lua
@@ -0,0 +1,13 @@
+---
+-- @Liquipedia
+-- wiki=commons
+-- page=Module:I18n/Data
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+return {
+	en = {
+		["brkts-header-rx"] = "Round ${round},R${round}",
+	}
+}
