46cc4b7a976897e45af0cb2d77add739aab5a5f5
hjpalpha <andreasbuesch1@gmail.com>
2024-08-09 11:07:37 +0200

    cleanup as per review | make use of #4503

--- components/match2/wikis/clashroyale/get_match_group_copy_paste_wiki.lua
+++ components/match2/wikis/clashroyale/get_match_group_copy_paste_wiki.lua
@@ -15,26 +15,10 @@ local BaseCopyPaste = Lua.import('Module:GetMatchGroupCopyPaste/wiki/Base')
 local OpponentLibrary = Lua.import('Module:OpponentLibraries')
 local Opponent = OpponentLibrary.Opponent
 
-local MODE_CONVERSION = {
-	['1v1'] = {Opponent.solo},
-	['2v2'] = {Opponent.duo},
-	['solo'] = {Opponent.solo},
-	['team'] = {Opponent.team},
-	['literal'] = {Opponent.literal},
-}
-MODE_CONVERSION.default = MODE_CONVERSION['1v1']
-
 ---@class ClashroyaleMatch2CopyPaste: Match2CopyPasteBase
 local WikiCopyPaste = Class.new(BaseCopyPaste)
 
 local INDENT = WikiCopyPaste.Indent
-
----Returns the cleaned opponent type
----@param mode string
----@return {[1]: OpponentType}
-function WikiCopyPaste.getMode(mode)
-	return MODE_CONVERSION[string.lower(mode or '')] or MODE_CONVERSION.default
-end
 
 --returns the Code for a Match, depending on the input
 ---@param bestof integer
@@ -58,27 +42,12 @@ function WikiCopyPaste.getMatchCode(bestof, mode, index, opponents, args)
 		streams and (INDENT .. '|twitch=|youtube=|vod=') or nil,
 		mvps and (INDENT .. '|mvp=') or nil,
 		Array.map(Array.range(1, opponents), function(opponentIndex)
-			return INDENT .. '|opponent' .. opponentIndex .. '=' .. WikiCopyPaste._getOpponent(mode, showScore)
+			return INDENT .. '|opponent' .. opponentIndex .. '=' .. WikiCopyPaste.getOpponent(mode, showScore)
 		end),
 		bans and '|t1bans={{Cards|}}|t2bans={{Cards|}}' or nil
 	)
 
 	return table.concat(lines, '\n')
-end
-
-function WikiCopyPaste._getOpponent(mode, showScore)
-	local score = showScore and '|score=' or ''
-	if mode == Opponent.solo then
-		return '{{SoloOpponent||flag=' .. score .. '}}'
-	elseif mode == Opponent.duo then
-		return '{{DuoOpponent|p1=|p1flag=|p2=|p2flag=' .. score .. '}}'
-	elseif mode == Opponent.team then
-		return '{{TeamOpponent|' .. score .. '}}'
-	elseif mode == Opponent.literal then
-		return '{{Literal|}}'
-	end
-
-	return ''
 end
 
 return WikiCopyPaste
