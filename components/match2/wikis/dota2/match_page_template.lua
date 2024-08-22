f294939f11353c69b00a9fc5a2ad794eaadef306
Rikard Blixt <rikardblixt@gmail.com>
2024-08-22 11:31:11 +0200

    feat(match2): use real api on dota2 (#4599)
    * feat(match2): use real api on dota2
    
    * most of the fixes
    
    * remove legacy bo1 thing from lol

--- components/match2/wikis/dota2/match_page_template.lua
+++ components/match2/wikis/dota2/match_page_template.lua
@@ -23,12 +23,7 @@ return {
 				<div class="match-bm-lol-match-header-tournament">[[{{parent}}|{{tournament}}]]</div>
 				<div class="match-bm-lol-match-header-date">{{&dateCountdown}}</div>
 			</div>
-			{{#isBestOfOne}}<div class="match-bm-lol-game-overview"><div class="match-bm-lol-game-summary">
-			<div class="match-bm-lol-game-summary-team">{{#games.1.teams.1.side}}[[File:Dota2 faction {{games.1.teams.1.side}}.png|link=|{{games.1.teams.1.side}} side]]{{/games.1.teams.1.side}}</div>
-			<div class="match-bm-lol-game-summary-center"><div class="match-bm-lol-game-summary-score-holder"><div class="match-bm-lol-game-summary-length">{{games.1.length}}</div></div></div>
-			<div class="match-bm-lol-game-summary-team">{{#games.1.teams.2.side}}[[File:Dota2 faction {{games.1.teams.2.side}}.png|link=|{{games.1.teams.1.side}} side]]{{/games.1.teams.2.side}}</div>
-			</div></div>{{/isBestOfOne}}
-			{{#extradata.mvp}}<div class="match-bm-lol-match-mvp"><b>MVP</b> {{#players}}[[{{name}}|{{displayname}}]]{{/players}}</div>{{/extradata.mvp}}
+			{{#extradata.mvp}}<div class="match-bm-lol-match-mvp"><b>MVP</b>{{#players}} [[{{name}}|{{displayname}}]]{{/players}}</div>{{/extradata.mvp}}
 		]=],
 	footer =
 		[=[
