2bc1769c5ba253ba9f34944331b0f7e2cea027ca
Hesketh2 <88981446+Hesketh2@users.noreply.github.com>
2024-08-23 22:45:36 +0700

    Remove this function

--- components/match2/wikis/teamfortress/match_legacy.lua
+++ components/match2/wikis/teamfortress/match_legacy.lua
@@ -13,17 +13,6 @@ local Opponent = require('Module:Opponent')
 local String = require('Module:StringUtils')
 local Table = require('Module:Table')
 
-function MatchLegacy.storeMatch(match2, options)
-	if not options.storeMatch1 then
-		return
-	end
-
-	return mw.ext.LiquipediaDB.lpdb_match(
-		'legacymatch_' .. match2.match2id,
-		MatchLegacy._convertParameters(match2)
-	)
-end
-
 function MatchLegacy._convertParameters(match2)
 	local match = Table.deepCopy(match2)
 	for key, _ in pairs(match) do
