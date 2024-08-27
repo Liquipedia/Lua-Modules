2546de09ea2c6a61b7fd419ec47cc62c593e9870
LuckeLucky <officialluckelucky@gmail.com>
2024-08-27 11:25:45 +0100

    m2 is winner not win

--- components/match2/wikis/rainbowsix/legacy/match_maps_legacy.lua
+++ components/match2/wikis/rainbowsix/legacy/match_maps_legacy.lua
@@ -92,7 +92,7 @@ function MatchMaps._main(args, frame)
 						t2otatk = details[prefix..'o1t2atk'],
 						t2otdef = details[prefix..'o1t2def'],
 						vod = details['vod'..i],
-						win = details[prefix .. 'win']
+						winner = details[prefix .. 'win']
 					}
 					details[prefix] = nil
 					details[prefix ..'win'] = nil
