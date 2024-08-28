dba72631a3a514a42005d0e10c3e1ce9d5fab0f4
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2024-08-28 13:05:17 +0200

    fix(opponent): wrong key for faction page var (#4663)

--- components/opponent/commons/starcraft_starcraft2/player_ext_starcraft.lua
+++ components/opponent/commons/starcraft_starcraft2/player_ext_starcraft.lua
@@ -212,7 +212,7 @@ function StarcraftPlayerExt.saveToPageVars(player, options)
 	local overwrite = options.overwritePageVars
 
 	if PlayerExt.shouldWritePageVar(displayName .. '_faction', player.faction, overwrite) then
-		globalVars:set(displayName .. '_faction', player.pageName)
+		globalVars:set(displayName .. '_faction', player.faction)
 	end
 
 	PlayerExt.saveToPageVars(player, options)
