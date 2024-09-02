7a3644d73daec14323c12936fd9651353bd74f9c
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2024-09-02 17:03:21 +0200

    split tests up a bit

--- spec/operator_spec.lua
+++ spec/operator_spec.lua
@@ -28,17 +28,20 @@ describe('operator', function()
 	end)
 
 	describe('property', function()
-		it('check', function()
-			local foo = {
-				{a = 3, b = 'abc'},
-				{a = 5, b = 'cedf'},
-				{a = 4, b = 'sfd'},
-			}
+		local foo = {
+			{a = 3, b = 'abc'},
+			{a = 5, b = 'cedf'},
+			{a = 4, b = 'sfd'},
+		}
+		it('check simple path', function()
+			assert.are_same({3, 5, 4}, Array.map(foo, Operator.property('a')))
+			assert.is_nil( Operator.property(5)(foo))
+		end)
+		it('check segmented path', function()
 			local bar = {
 				{a = {b = {c = 'd1'}, e = 'f1', 's1'}, g = 'h1', 't1', ['5'] = 'q1'},
 				{a = {b = {c = 'd2'}, e = 'f2', 's2'}, g = 'h2', 't2', ['5'] = 'q2'},
 			}
-			assert.are_same({3, 5, 4}, Array.map(foo, Operator.property('a')))
 			assert.are_same({{c = 'd1'}, {c = 'd2'}}, Array.map(bar, Operator.property('a.b')))
 			assert.are_same({'d1', 'd2'}, Array.map(bar, Operator.property('a.b.c')))
 			assert.are_same({'f1', 'f2'}, Array.map(bar, Operator.property('a.e')))
@@ -47,10 +50,21 @@ describe('operator', function()
 			assert.are_same({'t1', 't2'}, Array.map(bar, Operator.property('1')))
 			assert.are_same({'q1', 'q2'}, Array.map(bar, Operator.property('5')))
 			assert.are_same({'q1', 'q2'}, Array.map(bar, Operator.property(5)))
-			assert.is_nil( Operator.property(5)(foo))
-			assert.error(Operator.property)
-			assert.error(Operator.property('a'))
+		end)
+		it('check nil in path throws', function()
 			assert.error(function() return Operator.property('a.1')(foo) end)
+		end)
+		it('check wrong item type input throws', function()
+			assert.error(Operator.property)
+			-- intended type mismatch
+			---@diagnostic disable-next-line: param-type-mismatch
+			assert.error(function() Operator.property(foo) end)
+			-- intended type mismatch
+			---@diagnostic disable-next-line: param-type-mismatch
+			assert.error(function() Operator.property(nil) end)
+		end)
+		it('check wrong tbl input throws', function()
+			assert.error(Operator.property('a'))
 			-- intended type mismatch
 			---@diagnostic disable-next-line: param-type-mismatch
 			assert.error(function() return Operator.property('a')('string') end)
@@ -60,12 +74,6 @@ describe('operator', function()
 			-- intended type mismatch
 			---@diagnostic disable-next-line: param-type-mismatch
 			assert.error(function() return Operator.property('a')(nil) end)
-			-- intended type mismatch
-			---@diagnostic disable-next-line: param-type-mismatch
-			assert.error(Operator.property(foo))
-			-- intended type mismatch
-			---@diagnostic disable-next-line: param-type-mismatch
-			assert.error(Operator.property(nil))
 		end)
 	end)
 
