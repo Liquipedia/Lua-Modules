892491aab7ce9358a351e7bb5116f33fb4204b9d
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2024-09-02 16:48:28 +0200

    some test cases

--- spec/operator_spec.lua
+++ spec/operator_spec.lua
@@ -34,7 +34,38 @@ describe('operator', function()
 				{a = 5, b = 'cedf'},
 				{a = 4, b = 'sfd'},
 			}
+			local bar = {
+				{a = {b = {c = 'd1'}, e = 'f1', 's1'}, g = 'h1', 't1', ['5'] = 'q1'},
+				{a = {b = {c = 'd2'}, e = 'f2', 's2'}, g = 'h2', 't2', ['5'] = 'q2'},
+			}
 			assert.are_same({3, 5, 4}, Array.map(foo, Operator.property('a')))
+			assert.are_same({{c = 'd1'}, {c = 'd2'}}, Array.map(foo, Operator.property('a.b')))
+			assert.are_same({'d1', 'd2'}, Array.map(foo, Operator.property('a.b.c')))
+			assert.are_same({'f1', 'f2'}, Array.map(foo, Operator.property('a.e')))
+			assert.are_same({'s1', 's2'}, Array.map(foo, Operator.property('a.1')))
+			assert.are_same({'t1', 't2'}, Array.map(foo, Operator.property(1)))
+			assert.are_same({'t1', 't2'}, Array.map(foo, Operator.property('1')))
+			assert.are_same({'q1', 'q2'}, Array.map(foo, Operator.property('5')))
+			assert.are_same({'q1', 'q2'}, Array.map(foo, Operator.property(5)))
+			assert.is_nil( Operator.property(5)(foo))
+			assert.error(Operator.property)
+			assert.error(Operator.property('a'))
+			assert.error(function() return Operator.property('a.1')(foo) end)
+			-- intended type mismatch
+			---@diagnostic disable-next-line: param-type-mismatch
+			assert.error(function() return Operator.property('a')('string') end)
+			-- intended type mismatch
+			---@diagnostic disable-next-line: param-type-mismatch
+			assert.error(function() return Operator.property('a')(1) end)
+			-- intended type mismatch
+			---@diagnostic disable-next-line: param-type-mismatch
+			assert.error(function() return Operator.property('a')(nil) end)
+			-- intended type mismatch
+			---@diagnostic disable-next-line: param-type-mismatch
+			assert.error(Operator.property(foo))
+			-- intended type mismatch
+			---@diagnostic disable-next-line: param-type-mismatch
+			assert.error(Operator.property(nil))
 		end)
 	end)
 
