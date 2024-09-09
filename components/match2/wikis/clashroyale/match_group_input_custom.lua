dc6aa5089016a12f6343e5941c99676ff25bd15b
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2024-09-09 13:59:25 +0200

    Update components/match2/wikis/clashroyale/match_group_input_custom.lua
    Co-authored-by: Rikard Blixt <rikardblixt@gmail.com>

--- components/match2/wikis/clashroyale/match_group_input_custom.lua
+++ components/match2/wikis/clashroyale/match_group_input_custom.lua
@@ -222,8 +222,8 @@ function MapFunctions.calculateMapScore(mapInput, finished)
 	local winner = tonumber(mapInput.winner)
 	return function(opponentIndex)
 		local manualScore = tonumber(mapInput['score' .. opponentIndex])
-		if manualScore then return
-			manualScore
+		if manualScore then
+			return manualScore
 		end
 
 		-- TODO Better to check if map has started, rather than finished, for a more correct handling
