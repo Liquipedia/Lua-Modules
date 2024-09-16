929c30eb23bf670f6e5f1c0a581dbe81782ac2b6
Rikard Blixt <rikardblixt@gmail.com>
2024-09-16 10:59:44 +0200

    adjust to part 4

--- components/widget/widget_customizable.lua
+++ components/widget/widget_customizable.lua
@@ -14,20 +14,18 @@ local Widget = Lua.import('Module:Widget')
 ---@class CustomizableWidget: Widget
 ---@operator call({id: string, children: Widget[]}): CustomizableWidget
 ---@field id string
----@field children Widget[]
 local Customizable = Class.new(
 	Widget,
 	function(self, input)
 		self.id = self:assertExistsAndCopy(input.id)
-		self.children = input.children
 	end
 )
 
 ---@param injector WidgetInjector?
 ---@param children string[]
----@return string[]?
+---@return string
 function Customizable:make(injector, children)
-	return children
+	return table.concat(children)
 end
 
 ---@param injector WidgetInjector?
