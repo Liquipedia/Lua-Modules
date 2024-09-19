b9939b207d42c801de90b8e64a3276fab93955e0
Rikard Blixt <rikardblixt@gmail.com>
2024-09-19 17:02:56 +0200

    add folder to busted

--- spec/test_helper.lua
+++ spec/test_helper.lua
@@ -80,6 +80,7 @@ return function(busted, helper, options)
 		table.insert(paths, 'standard/links/commons/?.lua')
 		table.insert(paths, 'standard/tier/commons/?.lua')
 		table.insert(paths, 'components/widget/?.lua')
+		table.insert(paths, 'components/widget/infobox/?.lua')
 
 		package.path = table.concat(paths, ';')
 	end
