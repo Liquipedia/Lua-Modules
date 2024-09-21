bb61e0f4387fe0f6dd7476a776a8b9242b155b2d
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2024-09-21 05:30:15 +0200

    stormgate

--- components/match2/wikis/stormgate/match_group_input_custom.lua
+++ components/match2/wikis/stormgate/match_group_input_custom.lua
@@ -115,14 +115,12 @@ end
 ---@return {date: string, dateexact: boolean, timestamp: integer, timezoneId: string?, timezoneOffset: string?}
 function MatchFunctions.readDate(matchArgs)
 	local dateProps = MatchGroupInputUtil.readDate(matchArgs.date, {
-		'matchDate',
 		'match_date',
 		'tournament_startdate',
 		'tournament_enddate'
 	})
 
 	if dateProps.dateexact then
-		Variables.varDefine('matchDate', dateProps.date)
 		Variables.varDefine('match_date', dateProps.date)
 	end
 
