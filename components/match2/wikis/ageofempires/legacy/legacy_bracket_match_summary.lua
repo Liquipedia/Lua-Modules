73d199a7a7db50d4ad20e6e60f46303c0bdc239d
LuckeLucky <officialluckelucky@gmail.com>
2024-09-28 11:19:27 +0100

    Add legacy default to aoe
    Co-authored-by: mbergen <mail@mbergen.de>

---
+++ components/match2/wikis/ageofempires/legacy/legacy_bracket_match_summary.lua
@@ -0,0 +1,55 @@
+---
+-- @Liquipedia
+-- wiki=ageofempires
+-- page=Module:LegacyBracketMatchSummary
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Arguments = require('Module:Arguments')
+local Array = require('Module:Array')
+local Json = require('Module:Json')
+local Table = require('Module:Table')
+
+local LegacyBracketMatchSummary = {}
+
+---@param args table
+---@return table
+function LegacyBracketMatchSummary._handleMaps(args)
+	Array.mapIndexes(function(index)
+		local prefix = 'map' .. index
+
+		if args['match' .. index] then
+			local match = Json.parse(Table.extract(args, 'match' .. index)) --[[@as table]]
+			Table.mergeInto(args,
+					Table.map(match, function(subKey, value)
+						return prefix .. subKey, value
+					end)
+			)
+			args[prefix] = Table.extract(args, prefix .. 'map')
+			args[prefix .. 'mode'] = Table.extract(args, prefix .. 'mapmode')
+		end
+
+		if args[prefix] then
+			local mapInfo = Array.parseCommaSeparatedString(args[prefix], '|')
+			args[prefix] = mapInfo[1]
+		end
+
+		return args[prefix] or args[prefix .. 'win']
+	end)
+
+	return args
+end
+
+-- invoked by BracketMatchSummary
+---@param frame Frame
+---@return string
+function LegacyBracketMatchSummary.convert(frame)
+	local args = Arguments.getArgs(frame)
+	args = LegacyBracketMatchSummary._handleMaps(args)
+	args['civdraft'] = Table.extract(args, 'draft')
+
+	return Json.stringify(args)
+end
+
+return LegacyBracketMatchSummary
