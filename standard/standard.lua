447d55a64dbd40ffb2ad526a47e01e537519fa64
Rikard Blixt <rikardblixt@gmail.com>
2024-10-07 11:23:53 +0200

    remove some based on feedback

--- standard/standard.lua
+++ standard/standard.lua
@@ -7,28 +7,21 @@
 --
 
 local Lua = require('Module:Lua')
-local OpponentLibrary = Lua.import('Module:OpponentLibraries')
 
 return {
 	Array = Lua.import('Module:Array'),
 	Class = Lua.import('Module:Class'),
 	Condition = Lua.import('Module:Condition'),
 	DateExt = Lua.import('Module:Date/Ext'),
-	Game = Lua.import('Module:Game'),
 	I18n = Lua.import('Module:I18n'),
-	Icon = Lua.import('Module:Icon'),
-	Image = Lua.import('Module:Image'),
 	Info = Lua.import('Module:Info'),
 	Json = Lua.import('Module:Json'),
 	Logic = Lua.import('Module:Logic'),
 	Lpdb = Lua.import('Module:Lpdb'),
 	Lua = Lua,
 	Operator = Lua.import('Module:Operator'),
-	Opponent = OpponentLibrary.Opponent,
-	OpponentDisplay = OpponentLibrary.OpponentDisplay,
 	Page = Lua.import('Module:Page'),
 	String = Lua.import('Module:StringUtils'),
 	Table = Lua.import('Module:Table'),
-	Tier = Lua.import('Module:Tier'),
 	Variables = Lua.import('Module:Variables'),
 }
