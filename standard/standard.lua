45eed41e0ff3b24c4f92eb8efbdd7caf054ad573
Rikard Blixt <rikardblixt@gmail.com>
2024-10-07 11:25:39 +0200

    made global on request

--- standard/standard.lua
+++ standard/standard.lua
@@ -8,20 +8,18 @@
 
 local Lua = require('Module:Lua')
 
-return {
-	Array = Lua.import('Module:Array'),
-	Class = Lua.import('Module:Class'),
-	Condition = Lua.import('Module:Condition'),
-	DateExt = Lua.import('Module:Date/Ext'),
-	I18n = Lua.import('Module:I18n'),
-	Info = Lua.import('Module:Info'),
-	Json = Lua.import('Module:Json'),
-	Logic = Lua.import('Module:Logic'),
-	Lpdb = Lua.import('Module:Lpdb'),
-	Lua = Lua,
-	Operator = Lua.import('Module:Operator'),
-	Page = Lua.import('Module:Page'),
-	String = Lua.import('Module:StringUtils'),
-	Table = Lua.import('Module:Table'),
-	Variables = Lua.import('Module:Variables'),
-}
+Array = Lua.import('Module:Array')
+Class = Lua.import('Module:Class')
+Condition = Lua.import('Module:Condition')
+DateExt = Lua.import('Module:Date/Ext')
+I18n = Lua.import('Module:I18n')
+Info = Lua.import('Module:Info')
+Json = Lua.import('Module:Json')
+Logic = Lua.import('Module:Logic')
+Lpdb = Lua.import('Module:Lpdb')
+Lua = Lua
+Operator = Lua.import('Module:Operator')
+Page = Lua.import('Module:Page')
+String = Lua.import('Module:StringUtils')
+Table = Lua.import('Module:Table')
+Variables = Lua.import('Module:Variables')
