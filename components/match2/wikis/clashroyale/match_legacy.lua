e51192b1dd998f4aa59b038770231844aba3539a
Rikard Blixt <rikardblixt@gmail.com>
2024-10-11 12:33:01 +0200

    improve

--- components/match2/wikis/clashroyale/match_legacy.lua
+++ components/match2/wikis/clashroyale/match_legacy.lua
@@ -9,6 +9,7 @@
 local MatchLegacy = {}
 
 local Json = require('Module:Json')
+local Opponent = require('Module:Opponent')
 local String = require('Module:StringUtils')
 local Table = require('Module:Table')
 
@@ -27,41 +28,43 @@ function MatchLegacy.store(match2)
 	match.staticid = match2.match2id
 	match.extradata = Json.parseIfString(match.extradata) or {}
 	local opponent1 = match2.match2opponents[1] or {}
-	local opponent1match2players = opponent1.match2players or {}
 	local opponent2 = match2.match2opponents[2] or {}
-	local opponent2match2players = opponent2.match2players or {}
 
-	if opponent1.type == opponent2.type then
-		match.mode = opponent1.type
+	if opponent1.type ~= opponent2.type then
+		return
+	end
+	match.mode = opponent1.type
 
-		if opponent1.type == 'solo' then
-			local player = opponent1match2players[1] or {}
-			match.opponent1 = player.name and player.name:gsub('_', ' ') or nil
-			match.opponent1score = (tonumber(opponent1.score) or 0) >= 0 and opponent1.score or 0
-			match.opponent1flag = player.flag
-			match.extradata.opponent1name = player.displayname
+	local getScore = function(score)
+		return (tonumber(score) or 0) >= 0 and score or 0
+	end
+
+	if opponent1.type == Opponent.solo then
+		local function handlePlayer(index)
+			local opponent = match2.match2opponents[index] or {}
+			local player = opponent.match2players or {}
+			local prefix = 'opponent' .. index
+
+			match[prefix] = player.name and player.name:gsub('_', ' ') or nil
+			match[prefix .. 'score'] = getScore(opponent.score)
+			match[prefix .. 'flag'] = player.flag
+			match.extradata[prefix .. 'name'] = player.displayname
 			player.extradata = Json.parseIfString(player.extradata) or {}
-			player = opponent2match2players[1] or {}
-			match.opponent2 = player.name and player.name:gsub('_', ' ') or nil
-			match.opponent2score = (tonumber(opponent2.score) or 0) >= 0 and opponent2.score or 0
-			match.opponent2flag = player.flag
-			match.extradata.opponent2name = player.displayname
-			player.extradata = Json.parseIfString(player.extradata) or {}
-		elseif opponent1.type == 'team' then
-			match.opponent1 = String.isNotEmpty(opponent1.name) and opponent1.name:gsub('_', ' ') or 'TBD'
-			match.opponent1score = (tonumber(opponent1.score) or 0) >= 0 and opponent1.score or 0
-			match.opponent2 = String.isNotEmpty(opponent2.name) and opponent2.name:gsub('_', ' ') or 'TBD'
-			match.opponent2score = (tonumber(opponent2.score) or 0) >= 0 and opponent2.score or 0
-			match.mode = 'team'
-		else
-			return
 		end
+		handlePlayer(1)
+		handlePlayer(2)
+	elseif opponent1.type == Opponent.team then
+		match.opponent1 = String.isNotEmpty(opponent1.name) and opponent1.name:gsub('_', ' ') or 'TBD'
+		match.opponent1score = getScore(opponent1.score)
 
-		match.extradata.bestof = match2.bestof ~= 0 and tostring(match2.bestof) or ''
-		match.extradata = Json.stringify(match.extradata)
+		match.opponent2 = String.isNotEmpty(opponent2.name) and opponent2.name:gsub('_', ' ') or 'TBD'
+		match.opponent2score = getScore(opponent2.score)
 	else
 		return
 	end
+
+	match.extradata.bestof = match2.bestof ~= 0 and tostring(match2.bestof) or ''
+	match.extradata = Json.stringify(match.extradata)
 
 	return mw.ext.LiquipediaDB.lpdb_match(
 		'legacymatch_' .. match2.match2id,
