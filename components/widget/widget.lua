ac9923db333a71dfe07012d74d4aba94bf1bc41f
Rikard Blixt <rikardblixt@gmail.com>
2024-10-16 16:43:43 +0200

    feat(widget): allow for non-arrays as children (#4872)
    feat(widget): allow for non-table inputs to children

--- components/widget/widget.lua
+++ components/widget/widget.lua
@@ -5,10 +5,11 @@
 --
 -- Please see https://github.com/Liquipedia/Lua-Modules to contribute
 --
+
 local Array = require('Module:Array')
 local Class = require('Module:Class')
-local FnUtil = require('Module:FnUtil')
 local ErrorDisplay = require('Module:Error/Display')
+local FnUtil = require('Module:FnUtil')
 local Logic = require('Module:Logic')
 local String = require('Module:StringUtils')
 local Table = require('Module:Table')
@@ -20,7 +21,11 @@ local Table = require('Module:Table')
 ---@field injector WidgetInjector?
 local Widget = Class.new(function(self, props)
 	self.props = Table.copy(props) or {}
-	self.props.children = self.props.children or {}
+
+	if not Array.isArray(self.props.children) then
+		self.props.children = {self.props.children}
+	end
+
 	self.context = {} -- Populated by the parent
 end)
 
