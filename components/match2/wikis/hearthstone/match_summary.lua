93f1a41d61d266ab91d3b099c9c375f728d9cb05
Rikard Blixt <rikardblixt@gmail.com>
2024-10-31 10:04:49 +0100

    feat(match2): standardize hearthstone display

--- components/match2/wikis/hearthstone/match_summary.lua
+++ components/match2/wikis/hearthstone/match_summary.lua
@@ -8,10 +8,8 @@
 
 local Array = require('Module:Array')
 local DateExt = require('Module:Date/Ext')
-local CharacterIcon = require('Module:CharacterIcon')
-local Icon = require('Module:Icon')
-local Logic = require('Module:Logic')
 local Lua = require('Module:Lua')
+local Operator = require('Module:Operator')
 
 local DisplayHelper = Lua.import('Module:MatchGroup/Display/Helper')
 local MatchSummary = Lua.import('Module:MatchSummary/Base')
@@ -21,28 +19,12 @@ local WidgetUtil = Lua.import('Module:Widget/Util')
 local OpponentLibraries = require('Module:OpponentLibraries')
 local Opponent = OpponentLibraries.Opponent
 
-local ICONS = {
-	winner = Icon.makeIcon{iconName = 'winner', color = 'forest-green-text', size = 'initial'},
-	draw = Icon.makeIcon{iconName = 'draw', color = 'bright-sun-text', size = 'initial'},
-	loss = Icon.makeIcon{iconName = 'loss', color = 'cinnabar-text', size = 'initial'},
-	empty = '[[File:NoCheck.png|link=|16px]]',
-}
-
 local CustomMatchSummary = {}
 
 ---@param args table
 ---@return Html
 function CustomMatchSummary.getByMatchId(args)
-	return MatchSummary.defaultGetByMatchId(CustomMatchSummary, args, {
-		width = CustomMatchSummary._determineWidth,
-		teamStyle = 'bracket',
-	})
-end
-
----@param match MatchGroupUtilMatch
----@return string
-function CustomMatchSummary._determineWidth(match)
-	return '350px'
+	return MatchSummary.defaultGetByMatchId(CustomMatchSummary, args, {width = '350px', teamStyle = 'bracket'})
 end
 
 ---@param match MatchGroupUtilMatch
@@ -66,76 +48,24 @@ function CustomMatchSummary._isSolo(match)
 end
 
 ---@param game MatchGroupUtilGame
----@param paricipantId string
----@return {displayName: string?, pageName: string?, flag: string?, character: string?}
-function CustomMatchSummary._getPlayerData(game, paricipantId)
-	if not game or not game.participants then
-		return {}
-	end
-	return game.participants[paricipantId] or {}
-end
-
----@param game MatchGroupUtilGame
 ---@return Html?
 function CustomMatchSummary._createGame(game)
-	if not game.map and not game.winner then return end
-	local row = MatchSummary.Row()
-			:addClass('brkts-popup-body-game')
-			:css('font-size', '0.75rem')
-			:css('padding', '4px')
-			:css('min-height', '24px')
+	if not game.winner then return end
 	game.extradata = game.extradata or {}
 
-	local char1 =
-		CustomMatchSummary._createCharacterDisplay(CustomMatchSummary._getPlayerData(game, '1_1').character, false)
-	local char2 =
-		CustomMatchSummary._createCharacterDisplay(CustomMatchSummary._getPlayerData(game, '2_1').character, true)
+	local team1Characters = Array.map((game.opponents[1] or {}).players or {}, Operator.property('character'))
+	local team2Characters = Array.map((game.opponents[2] or {}).players or {}, Operator.property('character'))
 
-	row:addElement(char1:css('flex', '1 1 35%'):css('text-align', 'right'))
-	row:addElement(CustomMatchSummary._createCheckMark(game.winner, 1))
-	row:addElement(CustomMatchSummary._createCheckMark(game.winner, 2))
-	row:addElement(char2:css('flex', '1 1 35%'))
-
-	return row:create()
-end
-
----@param character string?
----@param reverse boolean?
----@return Html
-function CustomMatchSummary._createCharacterDisplay(character, reverse)
-	local characterDisplay = mw.html.create('span'):addClass('draft faction')
-
-	if not character then
-		return characterDisplay
-	end
-
-	local charIcon = CharacterIcon.Icon{
-		character = character,
-		size = '64px',
+	return MatchSummaryWidgets.Row{
+		classes = {'brkts-popup-body-game'},
+		css = {['font-size'] = '80%', padding = '4px', ['min-height'] = '24px'},
+		children = WidgetUtil.collect(
+			MatchSummaryWidgets.Characters{characters = team1Characters, flipped = false},
+			MatchSummaryWidgets.GameWinLossIndicator{winner = game.winner, opponentIndex = 1},
+			MatchSummaryWidgets.GameWinLossIndicator{winner = game.winner, opponentIndex = 2},
+			MatchSummaryWidgets.Characters{characters = team2Characters, flipped = true}
+		)
 	}
-	if reverse then
-		characterDisplay:wikitext(charIcon):wikitext('&nbsp;'):wikitext(character)
-	else
-		characterDisplay:wikitext(character):wikitext('&nbsp;'):wikitext(charIcon)
-	end
-	return characterDisplay
-end
-
----@param winner integer|string
----@param opponentIndex integer
----@return Html
-function CustomMatchSummary._createCheckMark(winner, opponentIndex)
-	return mw.html.create('div')
-			:addClass('brkts-popup-spaced')
-			:css('line-height', '17px')
-			:css('margin-left', '2%')
-			:css('margin-right', '2%')
-			:wikitext(
-				winner == opponentIndex and ICONS.winner
-				or winner == 0 and ICONS.draw
-				or Logic.isNotEmpty(winner) and ICONS.loss
-				or ICONS.empty
-			)
 end
 
 return CustomMatchSummary
