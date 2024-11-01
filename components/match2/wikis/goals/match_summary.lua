efeeb2a472c7d9ba009a3d88be3fc8afc4d9b806
Hesketh2 <88981446+Hesketh2@users.noreply.github.com>
2024-11-01 18:49:08 +0700

    Remove logic

--- components/match2/wikis/goals/match_summary.lua
+++ components/match2/wikis/goals/match_summary.lua
@@ -10,7 +10,6 @@ local CustomMatchSummary = {}
 
 local Array = require('Module:Array')
 local DateExt = require('Module:Date/Ext')
-local Logic = require('Module:Logic')
 local Lua = require('Module:Lua')
 
 local DisplayHelper = Lua.import('Module:MatchGroup/Display/Helper')
