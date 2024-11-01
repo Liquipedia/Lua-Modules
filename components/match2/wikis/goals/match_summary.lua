05232741f9e9a6972d1f5bf5dcd76562a514598c
Hesketh2 <88981446+Hesketh2@users.noreply.github.com>
2024-11-01 18:35:50 +0700

    Update match_summary.lua

--- components/match2/wikis/goals/match_summary.lua
+++ components/match2/wikis/goals/match_summary.lua
@@ -6,147 +6,49 @@
 -- Please see https://github.com/Liquipedia/Lua-Modules to contribute
 --
 
+local CustomMatchSummary = {}
+
 local Array = require('Module:Array')
 local DateExt = require('Module:Date/Ext')
-local Icon = require('Module:Icon')
 local Logic = require('Module:Logic')
 local Lua = require('Module:Lua')
-local Table = require('Module:Table')
 
 local DisplayHelper = Lua.import('Module:MatchGroup/Display/Helper')
 local MatchSummary = Lua.import('Module:MatchSummary/Base')
 local MatchSummaryWidgets = Lua.import('Module:Widget/Match/Summary/All')
 local WidgetUtil = Lua.import('Module:Widget/Util')
 
-local OpponentLibrary = require('Module:OpponentLibraries')
-local OpponentDisplay = OpponentLibrary.OpponentDisplay
-
-local GREEN_CHECK = Icon.makeIcon{iconName = 'winner', color = 'forest-green-text', size = '110%'}
-local DRAW_LINE = '<i class="fas fa-minus bright-sun-text" style="width: 14px; text-align: center" ></i>'
-local NO_CHECK = '[[File:NoCheck.png|link=]]'
-
-local CustomMatchSummary = {}
-
 ---@param args table
 ---@return Html
 function CustomMatchSummary.getByMatchId(args)
-	return MatchSummary.defaultGetByMatchId(CustomMatchSummary, args)
+	return MatchSummary.defaultGetByMatchId(CustomMatchSummary, args, {width = '400px', teamStyle = 'bracket'})
 end
 
 ---@param match MatchGroupUtilMatch
----@return MatchSummaryBody
+---@return Widget
 function CustomMatchSummary.createBody(match)
 	local showCountdown = match.timestamp ~= DateExt.defaultTimestamp
-	local games = Array.map(match.games, function(game)
-		return CustomMatchSummary._createGame(game):create()
-	end)
 
 	return MatchSummaryWidgets.Body{children = WidgetUtil.collect(
 		showCountdown and MatchSummaryWidgets.Row{children = DisplayHelper.MatchCountdownBlock(match)} or nil,
-		games
+		Array.map(match.games, CustomMatchSummary._createGame)
 	)}
 end
 
 ---@param game MatchGroupUtilGame
+---@param gameIndex integer
 ---@return MatchSummaryRow
-function CustomMatchSummary._createGame(game)
-	local row = MatchSummary.Row()
-			:addClass('brkts-popup-body-game')
-			:css('font-size', '84%')
-			:css('padding', '4px')
-			:css('min-height', '32px')
-	row
-		:addElement(CustomMatchSummary._createCheckMark(game.winner, 1))
-		:addElement(CustomMatchSummary._score(game.scores[1] or 0))
-		:addElement(mw.html.create('div')
-			:addClass('brkts-popup-body-element-vertical-centered')
-			:wikitext(game.map)
+function CustomMatchSummary._createGame(game, gameIndex)
+	return MatchSummaryWidgets.Row{
+		classes = {'brkts-popup-body-game'},
+		css = {['font-size'] = '80%', padding = '4px'},
+		children = WidgetUtil.collect(
+			MatchSummaryWidgets.GameWinLossIndicator{winner = game.winner, opponentIndex = 1},
+			MatchSummaryWidgets.GameCenter{children = Logic.nilIfEmpty(game.length) or ('Game ' .. gameIndex)},
+			MatchSummaryWidgets.GameWinLossIndicator{winner = game.winner, opponentIndex = 2},
+			MatchSummaryWidgets.GameComment{children = game.comment}
 		)
-		:addElement(CustomMatchSummary._score(game.scores[2] or 0))
-		:addElement(CustomMatchSummary._createCheckMark(game.winner, 2))
-
-	-- Add Comment
-	if not Logic.isEmpty(game.comment) then
-		row
-			:addElement(MatchSummaryWidgets.Break{})
-			:addElement(mw.html.create('div')
-				:wikitext(game.comment)
-				:css('margin', 'auto')
-			)
-	end
-	return row
-end
-
----@param game MatchGroupUtilGame
----@param match MatchGroupUtilMatch
----@return table[][]
-function CustomMatchSummary._extractPlayersFromGame(game, match)
-	local players = {{}, {}}
-
-	for participantKey, participant in Table.iter.spairs(game.participants or {}) do
-		participantKey = mw.text.split(participantKey, '_')
-		local opponentIndex = tonumber(participantKey[1])
-		local match2playerIndex = tonumber(participantKey[2])
-
-		local player = match.opponents[opponentIndex].players[match2playerIndex]
-
-		if not player then
-			player = {
-				displayName = participant.displayname,
-				pageName = participant.name,
-			}
-		end
-
-		table.insert(players[opponentIndex], player)
-	end
-
-	return players
-end
-
----@param winner integer
----@param opponentIndex integer
----@return Html
-function CustomMatchSummary._createCheckMark(winner, opponentIndex)
-	return mw.html.create('div')
-		:addClass('brkts-popup-body-element-vertical-centered')
-		:css('line-height', '17px')
-		:css('margin-left', '1%')
-		:css('margin-right', '1%')
-		:wikitext(
-			winner == opponentIndex and GREEN_CHECK
-			or winner == 0 and DRAW_LINE or NO_CHECK
-		)
-end
-
----@param score number|string|nil
----@return Html?
-function CustomMatchSummary._score(score)
-	if not score then return end
-
-	return mw.html.create('div')
-		:addClass('brkts-popup-body-element-vertical-centered')
-		:wikitext(score)
-end
-
----@param players table[]
----@param opponentIndex integer
----@param winner integer
----@return Html
-function CustomMatchSummary._players(players, opponentIndex, winner)
-	local flip = opponentIndex == 1
-
-	return mw.html.create('div')
-		:addClass(winner == opponentIndex and 'bg-win' or winner == 0 and 'bg-draw' or nil)
-		:css('align-items', 'center')
-		:css('border-radius', flip and '0 12px 12px 0' or '12px 0 0 12px')
-		:css('padding', '2px 8px')
-		:css('text-align', flip and 'right' or 'left')
-		:css('width', '35%')
-		:node(OpponentDisplay.BlockPlayers{
-			opponent = {players = players},
-			overflow = 'ellipsis',
-			showLink = true,
-		})
+	}
 end
 
 return CustomMatchSummary
