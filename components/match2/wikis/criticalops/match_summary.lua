2c1f251c27abd1f59a24f8eeee6bf7e08bca4cbe
Rikard Blixt <rikardblixt@gmail.com>
2024-11-04 18:08:38 +0100

    feat(match2): standardize cops and zula's display (#5020)

--- components/match2/wikis/criticalops/match_summary.lua
+++ components/match2/wikis/criticalops/match_summary.lua
@@ -6,12 +6,12 @@
 -- Please see https://github.com/Liquipedia/Lua-Modules to contribute
 --
 
-local Logic = require('Module:Logic')
 local Lua = require('Module:Lua')
 
 local DisplayHelper = Lua.import('Module:MatchGroup/Display/Helper')
 local MatchSummary = Lua.import('Module:MatchSummary/Base')
 local MatchSummaryWidgets = Lua.import('Module:Widget/Match/Summary/All')
+local WidgetUtil = Lua.import('Module:Widget/Util')
 
 local CustomMatchSummary = {}
 
@@ -24,19 +24,18 @@ end
 ---@param date string
 ---@param game MatchGroupUtilGame
 ---@param gameIndex integer
----@return Html?
+---@return Widget?
 function CustomMatchSummary.createGame(date, game, gameIndex)
 	if not game.map then
 		return
 	end
-	local row = MatchSummary.Row()
-	local extradata = game.extradata or {}
 
 	local function score(oppIdx)
 		return DisplayHelper.MapScore(game.scores[oppIdx], oppIdx, game.resultType, game.walkover, game.winner)
 	end
 
 	-- Teams scores
+	local extradata = game.extradata or {}
 	local t1sides = extradata.t1sides or {}
 	local t2sides = extradata.t2sides or {}
 	local t1halfs = extradata.t1halfs or {}
@@ -51,41 +50,18 @@ function CustomMatchSummary.createGame(date, game, gameIndex)
 		table.insert(team2Scores, {style = side2 and ('brkts-cs-score-color-'.. side2) or nil, score = score2})
 	end
 
-	-- Map Info
-	local mapInfo = mw.html.create('div')
-	mapInfo	:addClass('brkts-popup-spaced')
-			:wikitext('[[' .. game.map .. ']]')
-			:css('text-align', 'center')
-			:css('padding','5px 2px')
-			:css('flex-grow','1')
-
-	if game.resultType == 'np' then
-		mapInfo:addClass('brkts-popup-spaced-map-skip')
-	elseif game.resultType == 'draw' then
-		mapInfo:wikitext('<i>(Draw)</i>')
-	end
-
-	-- Build the HTML
-	row:addElement(MatchSummaryWidgets.GameWinLossIndicator{winner = game.winner, opponentIndex = 1})
-	row:addElement(MatchSummaryWidgets.DetailedScore{score = score(1), partialScores = team1Scores, flipped = false})
-
-	row:addElement(mapInfo)
-
-	row:addElement(MatchSummaryWidgets.DetailedScore{score = score(2), partialScores = team2Scores, flipped = true})
-	row:addElement(MatchSummaryWidgets.GameWinLossIndicator{winner = game.winner, opponentIndex = 2})
-
-	-- Add Comment
-	if not Logic.isEmpty(game.comment) then
-		row:addElement(MatchSummaryWidgets.Break{})
-		local comment = mw.html.create('div')
-		comment :wikitext(game.comment)
-				:css('margin', 'auto')
-		row:addElement(comment)
-	end
-
-	row:addClass('brkts-popup-body-game'):css('font-size', '85%'):css('overflow', 'hidden')
-
-	return row:create()
+	return MatchSummaryWidgets.Row{
+		classes = {'brkts-popup-body-game'},
+		css = {['font-size'] = '85%'},
+		children = WidgetUtil.collect(
+			MatchSummaryWidgets.GameWinLossIndicator{winner = game.winner, opponentIndex = 1},
+			MatchSummaryWidgets.DetailedScore{score = score(1), partialScores = team1Scores, flipped = false},
+			MatchSummaryWidgets.GameCenter{children = DisplayHelper.Map(game), css = {['flex-grow'] = '1'}},
+			MatchSummaryWidgets.DetailedScore{score = score(2), partialScores = team2Scores, flipped = true},
+			MatchSummaryWidgets.GameWinLossIndicator{winner = game.winner, opponentIndex = 2},
+			MatchSummaryWidgets.GameComment{children = game.comment}
+		)
+	}
 end
 
 return CustomMatchSummary
