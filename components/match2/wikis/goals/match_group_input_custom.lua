b1465f6f0dc2826432221ec6a7c75fa913355560
Hesketh2 <88981446+Hesketh2@users.noreply.github.com>
2024-11-06 15:15:49 +0700

    Update match_group_input_custom.lua

--- components/match2/wikis/goals/match_group_input_custom.lua
+++ components/match2/wikis/goals/match_group_input_custom.lua
@@ -7,14 +7,11 @@
 --
 
 local Array = require('Module:Array')
-local Logic = require('Module:Logic')
 local Lua = require('Module:Lua')
 local Operator = require('Module:Operator')
 local Table = require('Module:Table')
-local Variables = require('Module:Variables')
 
 local MatchGroupInputUtil = Lua.import('Module:MatchGroup/Input/Util')
-local Streams = Lua.import('Module:Links/Stream')
 
 local CustomMatchGroupInput = {}
 CustomMatchGroupInput.DEFAULT_MODE = 'solo'
