7fbfff58b240d52b0427e456c7bf7af1258dddfd
Rikard Blixt <rikardblixt@gmail.com>
2024-11-06 12:44:30 +0100

    fix(match2): icon class not being set properly

--- components/widget/match/summary/widget_match_summary_detailed_score.lua
+++ components/widget/match/summary/widget_match_summary_detailed_score.lua
@@ -23,9 +23,11 @@ function MatchSummaryDetailedScore:render()
 	local partialScores = Array.map(self.props.partialScores or {}, function(partialScore)
 		local children = {partialScore.score or '', partialScore.icon}
 
-		local styles = {'brkts-popup-body-match-sidewins'}
-		table.insert(styles, partialScore.style)
-		table.insert(styles, partialScore.icon and 'brkts-popup-body-match-sidewins-icon' or nil)
+		local styles = Array.append({},
+			'brkts-popup-body-match-sidewins',
+			partialScore.style,
+			partialScore.icon and 'brkts-popup-body-match-sidewins-icon' or nil
+		)
 
 		return HtmlWidgets.Td{
 			classes = styles,
