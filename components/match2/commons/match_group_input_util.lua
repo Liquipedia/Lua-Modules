f3d2a4d731045fdbb39094721582f0910137b6e0
Rikard Blixt <rikardblixt@gmail.com>
2024-11-07 17:03:38 +0100

    feat(match2): replace `walkover` and `resulttype` with `status` and `opponents` on map level on standardized wikis (#5058)
    * feat(match2): replace `walkover` and `resulttype` with `status` and `opponents` on map level on standardized wikis
    
    * clamp
    
    * save as arrays instead

--- components/match2/commons/match_group_input_util.lua
+++ components/match2/commons/match_group_input_util.lua
@@ -1228,7 +1228,7 @@ function MatchGroupInputUtil.standardProcessMaps(match, opponents, Parser)
 		end
 		map.finished = MatchGroupInputUtil.mapIsFinished(map)
 
-		local opponentInfo = Array.map(opponents, function(_, opponentIndex)
+		map.opponents = Array.map(opponents, function(_, opponentIndex)
 			local score, status = MatchGroupInputUtil.computeOpponentScore({
 				walkover = map.walkover,
 				winner = map.winner,
@@ -1238,11 +1238,10 @@ function MatchGroupInputUtil.standardProcessMaps(match, opponents, Parser)
 			return {score = score, status = status}
 		end)
 
-		map.scores = Array.map(opponentInfo, Operator.property('score'))
+		map.scores = Array.map(map.opponents, Operator.property('score'))
 		if map.finished then
-			map.resulttype = MatchGroupInputUtil.getResultType(winnerInput, finishedInput, opponentInfo)
-			map.walkover = MatchGroupInputUtil.getWalkover(map.resulttype, opponentInfo)
-			map.winner = MatchGroupInputUtil.getWinner(map.resulttype, winnerInput, opponentInfo)
+			map.status = MatchGroupInputUtil.getMatchStatus(winnerInput, finishedInput)
+			map.winner = MatchGroupInputUtil.getWinner(map.status, winnerInput, map.opponents)
 		end
 
 		map.extradata = Parser.getExtraData and Parser.getExtraData(match, map, opponents) or nil
