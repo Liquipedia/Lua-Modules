18a891d21992aa7ab7fb5acc870a03590cda67b6
Hesketh2 <88981446+Hesketh2@users.noreply.github.com>
2024-11-10 15:37:06 +0700

    add MLBB

---
+++ components/match2/wikis/mobilelegends/get_match_group_copy_paste_wiki.lua
@@ -0,0 +1,67 @@
+---
+-- @Liquipedia
+-- wiki=leagueoflegends
+-- page=Module:GetMatchGroupCopyPaste/wiki
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Array = require('Module:Array')
+local Class = require('Module:Class')
+local Logic = require('Module:Logic')
+local Lua = require('Module:Lua')
+
+local BaseCopyPaste = Lua.import('Module:GetMatchGroupCopyPaste/wiki/Base')
+
+---@class MobileLegendsMatch2CopyPaste: Match2CopyPasteBase
+local WikiCopyPaste = Class.new(BaseCopyPaste)
+
+local INDENT = WikiCopyPaste.Indent
+
+--returns the Code for a Match, depending on the input
+---@param bestof integer
+---@param mode string
+---@param index integer
+---@param opponents integer
+---@param args table
+---@return string
+function WikiCopyPaste.getMatchCode(bestof, mode, index, opponents, args)
+	local showScore = Logic.nilOr(Logic.readBoolOrNil(args.score), bestof == 0)
+	local bans = Logic.readBool(args.bans)
+
+	local lines = Array.extend(
+		'{{Match',
+		Logic.readBool(args.needsWinner) and INDENT .. '|winner=' or nil,
+		Array.map(Array.range(1, opponents), function(opponentIndex)
+			return INDENT .. '|opponent' .. opponentIndex .. '=' .. WikiCopyPaste.getOpponent(mode, showScore)
+		end),
+		Logic.readBool(args.hasDate) and {
+			INDENT .. '|date= |youtube=',
+		} or nil,
+		Array.map(Array.range(1, bestof), function(mapIndex)
+			return WikiCopyPaste._getMapCode(mapIndex, bans)
+		end),
+		'}}'
+	)
+
+	return table.concat(lines, '\n')
+end
+
+---@param mapIndex integer
+---@param bans boolean
+---@return string
+function WikiCopyPaste._getMapCode(mapIndex, bans)
+	return table.concat(Array.extend(
+		INDENT .. '|map' .. mapIndex .. '={{Map' ..  (vod == 'maps' and '|vod=' or ''),
+		INDENT .. INDENT .. '|team1side= |team2side= |length= |winner=',
+		INDENT .. INDENT .. '<!-- Hero picks -->',
+		INDENT .. INDENT .. '|t1h1= |t1h2= |t1h3= |t1h4= |t1h5=',
+		INDENT .. INDENT .. '|t2h1= |t2h2= |t2h3= |t2h4= |t2h5=',
+		INDENT .. INDENT .. '<!-- Hero bans -->',
+		INDENT .. INDENT .. '|t1b1= |t1b2= |t1b3= |t1b4= |t1b5=',
+		INDENT .. INDENT .. '|t2b1= |t2b2= |t2b3= |t2b4= |t2b5=',
+		INDENT .. '}}'
+	), '\n')
+end
+
+return WikiCopyPaste
