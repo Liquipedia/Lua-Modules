d824c9aa118f02a3a0cf7eadf08f74b5a737d753
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2024-11-11 20:54:04 +0100

    lol

--- components/infobox/wikis/leagueoflegends/infobox_league_custom.lua
+++ components/infobox/wikis/leagueoflegends/infobox_league_custom.lua
@@ -56,7 +56,7 @@ end
 ---@param args table
 ---@return string
 function CustomLeague:appendLiquipediatierDisplay(args)
-	return String.isNotEmpty(self.data.publishertier) and (' ' .. RIOT_ICON) or ''
+	return self.data.publishertier and (' ' .. RIOT_ICON) or ''
 end
 
 ---@param lpdbData table
@@ -66,12 +66,6 @@ function CustomLeague:addToLpdb(lpdbData, args)
 	lpdbData.extradata.individual = String.isNotEmpty(args.participants_number) or
 			String.isNotEmpty(args.individual) and 'true' or ''
 	return lpdbData
-end
-
----@param args table
-function CustomLeague:customParseArguments(args)
-	-- line below can be kicked after conversion bot runs
-	self.data.publishertier = Logic.readBool(args.highlighted or args.riotpremier)
 end
 
 ---@param args table
