ad318e99a30d8af9a704b687733b30b88162bd11
mbergen <mail@mbergen.de>
2024-11-19 13:11:32 +0100

    feat(match2): sort links in MatchSummary (#5074)
    * Add match link priority groups
    
    * Fix typo
    
    * add missing return
    
    * Use single priority list
    
    * Fix param order, do not require index
    
    * Remove unused import
    
    * fix ??
    
    * Apply suggestions
    
    * Iterate in alphabetical order for unspecified links
    
    * Iterate over copy
    
    * Remove import
    
    * Update components/match2/commons/match_summary_base.lua
    
    * Update components/match2/commons/match_summary_base.lua
    
    Co-authored-by: hjpalpha <75081997+hjpalpha@users.noreply.github.com>
    
    * Rename vars
    
    Co-authored-by: Rikard Blixt <rikardblixt@gmail.com>
    
    ---------
    
    Co-authored-by: hjpalpha <75081997+hjpalpha@users.noreply.github.com>
    Co-authored-by: Rikard Blixt <rikardblixt@gmail.com>

---
+++ standard/links/commons/links_match_priority_groups.lua
@@ -0,0 +1,22 @@
+---
+-- @Liquipedia
+-- wiki=commons
+-- page=Module:Links/MatchPriorityGroups
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+return {
+	-- common
+	'vod',
+	'preview',
+	'interview',
+	'review',
+	'lrthread',
+	'recap',
+	'headtohead',
+
+	-- ageofempires
+	'mapdraft',
+	'civdraft'
+}
