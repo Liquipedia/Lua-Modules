5000ea52dca135d6e8de2642cf0f9881c4fcd737
Rikard Blixt <rikardblixt@gmail.com>
2024-11-21 15:35:06 +0100

    feat(match2): create support for world of warcraft (#5116)
    * feat(match2): add support for world of warcraft
    
    * uses different template

---
+++ components/match2/wikis/worldofwarcraft/match_summary.lua
@@ -0,0 +1,57 @@
+---
+-- @Liquipedia
+-- wiki=worldofwarcraft
+-- page=Module:MatchSummary
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Array = require('Module:Array')
+local Logic = require('Module:Logic')
+local Lua = require('Module:Lua')
+local Operator = require('Module:Operator')
+
+local DisplayHelper = Lua.import('Module:MatchGroup/Display/Helper')
+local MatchSummary = Lua.import('Module:MatchSummary/Base')
+local MatchSummaryWidgets = Lua.import('Module:Widget/Match/Summary/All')
+local WidgetUtil = Lua.import('Module:Widget/Util')
+
+local CustomMatchSummary = {}
+
+---@param args table
+---@return Html
+function CustomMatchSummary.getByMatchId(args)
+	return MatchSummary.defaultGetByMatchId(CustomMatchSummary, args, {width = '400px', teamStyle = 'bracket'})
+end
+
+---@param date string
+---@param game MatchGroupUtilGame
+---@param gameIndex integer
+---@return Widget?
+function CustomMatchSummary.createGame(date, game, gameIndex)
+	local function makeTeamSection(opponentIndex)
+		local flipped = opponentIndex == 2
+		local characters = Array.map((game.opponents[opponentIndex] or {}).players or {}, Operator.property('character'))
+		return {
+			MatchSummaryWidgets.GameWinLossIndicator{winner = game.winner, opponentIndex = opponentIndex},
+			MatchSummaryWidgets.Characters{characters = characters, flipped = flipped},
+		}
+	end
+
+	return MatchSummaryWidgets.Row{
+		classes = {'brkts-popup-body-game'},
+		css = {['font-size'] = '85%'},
+		children = WidgetUtil.collect(
+			MatchSummaryWidgets.GameTeamWrapper{children = makeTeamSection(1)},
+			MatchSummaryWidgets.GameCenter{children = WidgetUtil.collect(
+				Logic.nilIfEmpty(game.length),
+				DisplayHelper.Map(game),
+				game.extradata.damp and '('.. game.extradata.damp ..')' or nil
+			)},
+			MatchSummaryWidgets.GameTeamWrapper{children = makeTeamSection(2), flipped = true},
+			MatchSummaryWidgets.GameComment{children = game.comment}
+		)
+	}
+end
+
+return CustomMatchSummary
