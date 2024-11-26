86bd45781ae3459b0fb25d975b2dbd5fbb72a80a
Rikard Blixt <rikardblixt@gmail.com>
2024-11-26 11:35:58 +0100

    fix naming

--- components/widget/match/summary/ffa/widget_match_summary_ffa_all.lua
+++ components/widget/match/summary/ffa/widget_match_summary_ffa_all.lua
@@ -15,12 +15,12 @@ Widgets.CountdownIcon = Lua.import('Module:Widget/Match/Summary/Ffa/CountdownIco
 Widgets.Header = Lua.import('Module:Widget/Match/Summary/Ffa/Header')
 Widgets.PointsDistribution = Lua.import('Module:Widget/Match/Summary/Ffa/PointsDistribution')
 Widgets.RankRange = Lua.import('Module:Widget/Match/Summary/Ffa/RankRange')
-Widgets.Row = Lua.import('Module:Widget/Match/Summary/Ffa/Row')
-Widgets.RowCell = Lua.import('Module:Widget/Match/Summary/Ffa/RowCell')
 Widgets.Tab = Lua.import('Module:Widget/Match/Summary/Ffa/Tab')
 Widgets.Table = Lua.import('Module:Widget/Match/Summary/Ffa/Table')
 Widgets.TableHeader = Lua.import('Module:Widget/Match/Summary/Ffa/TableHeader')
 Widgets.TableHeaderCell = Lua.import('Module:Widget/Match/Summary/Ffa/TableHeaderCell')
+Widgets.TableRow = Lua.import('Module:Widget/Match/Summary/Ffa/TableRow')
+Widgets.TableRowCell = Lua.import('Module:Widget/Match/Summary/Ffa/TableRowCell')
 Widgets.Trophy = Lua.import('Module:Widget/Match/Summary/Ffa/Trophy')
 
 return Widgets
