e4433541a8b49a784306978d55e5aaa2e65f4e24
Rikard Blixt <rikardblixt@gmail.com>
2024-11-28 18:02:16 +0100

    refactor(match2): standardize BR display further

--- components/match2/commons/match_summary_ffa.lua
+++ components/match2/commons/match_summary_ffa.lua
@@ -9,11 +9,287 @@
 local Array = require('Module:Array')
 local Date = require('Module:Date/Ext')
 local FnUtil = require('Module:FnUtil')
+local Lua = require('Module:Lua')
 local Table = require('Module:Table')
 local Timezone = require('Module:Timezone')
 local VodLink = require('Module:VodLink')
 
+local OpponentLibraries = require('Module:OpponentLibraries')
+local OpponentDisplay = OpponentLibraries.OpponentDisplay
+
+local MatchSummaryWidgets = Lua.import('Module:Widget/Match/Summary/Ffa/All')
+local HtmlWidgets = Lua.import('Module:Widget/Html/All')
+local IconWidget = Lua.import('Module:Widget/Image/Icon/Fontawesome')
+
 local MatchSummaryFfa = {}
+
+local PLACEMENT_BG = {
+	'cell--gold',
+	'cell--silver',
+	'cell--bronze',
+	'cell--copper',
+}
+
+local STATUS_ICONS = {
+	-- Normal Status
+	up = 'standings_up',
+	stayup = 'standings_stayup',
+	stay = 'standings_stay',
+	staydown = 'standings_staydown',
+	down = 'standings_down',
+}
+
+local MATCH_OVERVIEW_COLUMNS = {
+	{
+		class = 'cell--status',
+		show = function(match)
+			return Table.isNotEmpty(match.extradata.status)
+		end,
+		header = {
+			value = '',
+		},
+		row = {
+			class = function (opponent)
+				return 'bg-' .. (opponent.advanceBg or '')
+			end,
+			value = function (opponent, idx)
+				if not STATUS_ICONS[opponent.placementStatus] then
+					return
+				end
+				return IconWidget{
+					iconName = STATUS_ICONS[opponent.placementStatus],
+				}
+			end,
+		},
+	},
+	{
+		sortable = true,
+		sortType = 'rank',
+		class = 'cell--rank',
+		icon = 'rank',
+		header = {
+			value = 'Rank',
+		},
+		sortVal = {
+			value = function (opponent, idx)
+				return opponent.placement ~= -1 and opponent.placement or idx
+			end,
+		},
+		row = {
+			value = function (opponent, idx)
+				local place = opponent.placement ~= -1 and opponent.placement or idx
+				local placementDisplay = tostring(MatchSummaryWidgets.RankRange{rankStart = place})
+				return HtmlWidgets.Fragment{children = {
+					MatchSummaryWidgets.Trophy{place = place, additionalClasses = {'panel-table__cell-icon'}},
+					HtmlWidgets.Span{children = placementDisplay},
+				}}
+			end,
+		},
+	},
+	{
+		sortable = true,
+		sortType = 'team',
+		class = 'cell--team',
+		icon = 'team',
+		header = {
+			value = 'Team',
+		},
+		sortVal = {
+			value = function (opponent, idx)
+				return opponent.name
+			end,
+		},
+		row = {
+			value = function (opponent, idx)
+				return OpponentDisplay.BlockOpponent{
+					opponent = opponent,
+					showLink = true,
+					overflow = 'ellipsis',
+					teamStyle = 'hybrid',
+				}
+			end,
+		},
+	},
+	{
+		sortable = true,
+		sortType = 'total-points',
+		class = 'cell--total-points',
+		icon = 'points',
+		header = {
+			value = 'Total Points',
+			mobileValue = 'Pts.',
+		},
+		sortVal = {
+			value = function (opponent, idx)
+				return OpponentDisplay.InlineScore(opponent)
+			end,
+		},
+		row = {
+			value = function (opponent, idx)
+				return OpponentDisplay.InlineScore(opponent)
+			end,
+		},
+	},
+}
+local GAME_OVERVIEW_COLUMNS = {
+	{
+		class = 'panel-table__cell__game-placement',
+		icon = 'placement',
+		header = {
+			value = 'P',
+		},
+		row = {
+			class = function (opponent)
+				return PLACEMENT_BG[opponent.placement]
+			end,
+			value = function (opponent)
+				local placementDisplay
+				if opponent.status and opponent.status ~= 'S' then
+					placementDisplay = '-'
+				else
+					placementDisplay = tostring(MatchSummaryWidgets.RankRange{rankStart = opponent.placement})
+				end
+				return HtmlWidgets.Fragment{children = {
+					MatchSummaryWidgets.Trophy{place = opponent.placement, additionalClasses = {'panel-table__cell-icon'}},
+					HtmlWidgets.Span{
+						classes = {'panel-table__cell-game__text'},
+						children = placementDisplay,
+					}
+				}}
+			end,
+		},
+	},
+	{
+		class = 'panel-table__cell__game-kills',
+		icon = 'kills',
+		header = {
+			value = 'K',
+		},
+		row = {
+			value = function (opponent)
+				return opponent.scoreBreakdown.kills
+			end,
+		},
+	},
+}
+
+local GAME_STANDINGS_COLUMNS = {
+	{
+		sortable = true,
+		sortType = 'rank',
+		class = 'cell--rank',
+		icon = 'rank',
+		header = {
+			value = 'Rank',
+		},
+		sortVal = {
+			value = function (opponent, idx)
+				if opponent.placement == -1 or opponent.status ~= 'S' then
+					return idx
+				end
+				return opponent.placement
+			end,
+		},
+		row = {
+			value = function (opponent, idx)
+				local place = opponent.placement ~= -1 and opponent.placement or idx
+				local placementDisplay
+				if opponent.status and opponent.status ~= 'S' then
+					placementDisplay = '-'
+				else
+					placementDisplay = tostring(MatchSummaryWidgets.RankRange{rankStart = place})
+				end
+				return HtmlWidgets.Fragment{children = {
+					MatchSummaryWidgets.Trophy{place = place, additionalClasses = {'panel-table__cell-icon'}},
+					HtmlWidgets.Span{children = placementDisplay},
+				}}
+			end,
+		},
+	},
+	{
+		sortable = true,
+		sortType = 'team',
+		class = 'cell--team',
+		icon = 'team',
+		header = {
+			value = 'Team',
+		},
+		sortVal = {
+			value = function (opponent, idx)
+				return opponent.name
+			end,
+		},
+		row = {
+			value = function (opponent, idx)
+				return OpponentDisplay.BlockOpponent{
+					opponent = opponent,
+					showLink = true,
+					overflow = 'ellipsis',
+					teamStyle = 'hybrid',
+				}
+			end,
+		},
+	},
+	{
+		sortable = true,
+		sortType = 'total-points',
+		class = 'cell--total-points',
+		icon = 'points',
+		header = {
+			value = 'Total Points',
+			mobileValue = 'Pts.',
+		},
+		sortVal = {
+			value = function (opponent, idx)
+				return opponent.score
+			end,
+		},
+		row = {
+			value = function (opponent, idx)
+				return opponent.score
+			end,
+		},
+	},
+	{
+		sortable = true,
+		sortType = 'placements',
+		class = 'cell--placements',
+		icon = 'placement',
+		header = {
+			value = 'Placement Points',
+		},
+		sortVal = {
+			value = function (opponent, idx)
+				return opponent.scoreBreakdown.placePoints
+			end,
+		},
+		row = {
+			value = function (opponent, idx)
+				return opponent.scoreBreakdown.placePoints
+			end,
+		},
+	},
+	{
+		sortable = true,
+		sortType = 'kills',
+		class = 'cell--kills',
+		icon = 'kills',
+		header = {
+			value = 'Kill Points',
+		},
+		sortVal = {
+			value = function (opponent, idx)
+				return opponent.scoreBreakdown.killPoints
+			end,
+		},
+		row = {
+			value = function (opponent, idx)
+				return opponent.scoreBreakdown.killPoints
+			end,
+		},
+	},
+}
+
 
 ---Creates a countdown block for a given game
 ---Attaches any VODs of the game as well
@@ -83,5 +359,156 @@ function MatchSummaryFfa.createScoringData(match)
 		placement = scorePlacement,
 	}
 end
+
+---@param match table
+---@return MatchSummaryFfaTable
+function MatchSummaryFfa.standardMatch(match)
+	local rows = Array.map(match.opponents, function (opponent, index)
+		local children = Array.map(MATCH_OVERVIEW_COLUMNS, function(column)
+			if column.show and not column.show(match) then
+				return
+			end
+			return MatchSummaryWidgets.TableRowCell{
+				class = (column.class or '') .. ' ' .. (column.row.class and column.row.class(opponent) or ''),
+				sortable = column.sortable,
+				sortType = column.sortType,
+				sortValue = column.sortVal and column.sortVal.value(opponent, index) or nil,
+				value = column.row.value(opponent, index),
+			}
+		end)
+
+		local gameRowContainer = HtmlWidgets.Div{
+			classes = {'panel-table__cell', 'cell--game-container'},
+			attributes = {
+				['data-js-battle-royale'] = 'game-container'
+			},
+			children = Array.map(opponent.games, function(gameOpponent)
+				local gameRow = HtmlWidgets.Div{
+					classes = {'panel-table__cell', 'cell--game'},
+					children = Array.map(GAME_OVERVIEW_COLUMNS, function(column)
+						if column.show and not column.show(match) then
+							return
+						end
+						return MatchSummaryWidgets.TableRowCell{
+							class = (column.class or '') .. ' ' .. (column.row.class and column.row.class(gameOpponent) or ''),
+							value = column.row.value(gameOpponent),
+						}
+					end)
+				}
+				return gameRow
+			end)
+		}
+		table.insert(children, gameRowContainer)
+		return MatchSummaryWidgets.TableRow{children = children}
+	end)
+
+	local cells = Array.map(MATCH_OVERVIEW_COLUMNS, function(column)
+		if column.show and not column.show(match) then
+			return
+		end
+		return MatchSummaryWidgets.TableHeaderCell{
+			class = column.class,
+			icon = column.icon,
+			mobileValue = column.header.mobileValue,
+			show = column.show,
+			sortable = column.sortable,
+			sortType = column.sortType,
+			value = column.header.value,
+		}
+	end)
+
+	table.insert(cells, HtmlWidgets.Div{
+		classes = {'panel-table__cell', 'cell--game-container-nav-holder'},
+		attributes = {
+			['data-js-battle-royale'] = 'game-nav-holder'
+		},
+		children = {
+			HtmlWidgets.Div{
+				classes = {'panel-table__cell', 'cell--game-container'},
+				attributes = {
+					['data-js-battle-royale'] = 'game-container'
+				},
+				children = Array.map(match.games, function(game, idx)
+					return HtmlWidgets.Div{
+						classes = {'panel-table__cell', 'cell--game'},
+						children = {
+							HtmlWidgets.Div{
+								classes = {'panel-table__cell__game-head'},
+								children = {
+									HtmlWidgets.Div{
+										classes = {'panel-table__cell__game-title'},
+										children = {
+											MatchSummaryWidgets.CountdownIcon{game = game, additionalClasses = {'panel-table__cell-icon'}},
+											HtmlWidgets.Span{
+												classes = {'panel-table__cell-text'},
+												children = 'Game ' .. idx
+											}
+										}
+									},
+									MatchSummaryFfa.gameCountdown(game),
+								}
+							},
+							HtmlWidgets.Div{
+								classes = {'panel-table__cell__game-details'},
+								children = Array.map(GAME_OVERVIEW_COLUMNS, function(column)
+									return MatchSummaryWidgets.TableHeaderCell{
+										class = column.class,
+										icon = column.icon,
+										mobileValue = column.header.mobileValue,
+										show = column.show,
+										value = column.header.value,
+									}
+								end)
+							}
+						}
+					}
+				end)
+			}
+		}
+	})
+
+	return MatchSummaryWidgets.Table{children = {
+		MatchSummaryWidgets.TableHeader{children = cells},
+		unpack(rows)
+	}}
+end
+
+---@param game table
+---@return MatchSummaryFfaTable
+function MatchSummaryFfa.standardGame(game)
+	local rows = Array.map(game.opponents, function (opponent, index)
+		local children = Array.map(GAME_STANDINGS_COLUMNS, function(column)
+			if column.show and not column.show(game) then
+				return
+			end
+			return MatchSummaryWidgets.TableRowCell{
+				class = column.class,
+				sortable = column.sortable,
+				sortType = column.sortType,
+				sortValue = column.sortVal and column.sortVal.value(opponent, index) or nil,
+				value = column.row.value(opponent, index),
+			}
+		end)
+		return MatchSummaryWidgets.TableRow{children = children}
+	end)
+
+	return MatchSummaryWidgets.Table{children = {
+		MatchSummaryWidgets.TableHeader{children = Array.map(GAME_STANDINGS_COLUMNS, function(column)
+			if column.show and not column.show(game) then
+				return
+			end
+			return MatchSummaryWidgets.TableHeaderCell{
+				class = column.class,
+				icon = column.icon,
+				mobileValue = column.header.mobileValue,
+				sortable = column.sortable,
+				sortType = column.sortType,
+				value = column.header.value,
+			}
+		end)},
+		unpack(rows)
+	}}
+end
+
 
 return MatchSummaryFfa
