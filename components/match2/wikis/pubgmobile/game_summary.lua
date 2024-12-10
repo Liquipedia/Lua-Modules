19caa4eddde3c992e4296bffdb780e6ab94b0a78
Rikard Blixt <rikardblixt@gmail.com>
2024-12-10 13:50:14 +0100

    feat(match2): standardize BR game details

--- components/match2/wikis/pubgmobile/game_summary.lua
+++ components/match2/wikis/pubgmobile/game_summary.lua
@@ -9,14 +9,11 @@
 local CustomGameSummary = {}
 
 local Lua = require('Module:Lua')
-local Page = require('Module:Page')
 
 local MatchGroupUtil = Lua.import('Module:MatchGroup/Util')
 
 local SummaryHelper = Lua.import('Module:MatchSummary/Ffa')
 local MatchSummaryWidgets = Lua.import('Module:Widget/Match/Summary/Ffa/All')
-local HtmlWidgets = Lua.import('Module:Widget/Html/All')
-local IconWidget = Lua.import('Module:Widget/Image/Icon/Fontawesome')
 
 ---@class PubgmMatchGroupUtilGame: MatchGroupUtilGame
 ---@field stream table
@@ -39,26 +36,9 @@ function CustomGameSummary.getGameByMatchId(props)
 		matchId = match.matchId,
 		idx = props.gameIdx,
 		children = {
-			CustomGameSummary._createGameDetails(game),
+			MatchSummaryWidgets.GameDetails{game = game},
 			MatchSummaryWidgets.PointsDistribution{scores = scoringData},
 			SummaryHelper.standardGame(game)
-		}
-	}
-end
-
----@param game table
----@return Widget
-function CustomGameSummary._createGameDetails(game)
-	return MatchSummaryWidgets.ContentItemContainer{contentClass = 'panel-content__game-schedule',
-		items = {
-			{
-				icon = MatchSummaryWidgets.CountdownIcon{game = game},
-				content = MatchSummaryWidgets.GameCountdown{game = game},
-			},
-			game.map and {
-				icon = IconWidget{iconName = 'map'},
-				content = HtmlWidgets.Span{children = Page.makeInternalLink(game.map)},
-			} or nil,
 		}
 	}
 end
