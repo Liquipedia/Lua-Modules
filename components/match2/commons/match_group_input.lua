2d012f88ec7a1d07d9a02b727a17fd03b0f84139
Rikard Blixt <rikardblixt@gmail.com>
2024-12-12 17:29:04 +0100

    fix(match2): error when using ShowBracket (#5202)
    * fix(match2): error when using ShowBracket
    
    * add comment

--- components/match2/commons/match_group_input.lua
+++ components/match2/commons/match_group_input.lua
@@ -348,11 +348,13 @@ function MatchGroupInput.applyOverrideArgs(matches, args)
 	end
 end
 
----@param matches {match2opponents: table[]}[]
+---@param matches {match2opponents: table[]?, opponents: table[]?}[]
 ---@return integer
 function MatchGroupInput.getMaxOpponentCount(matches)
 	return Array.reduce(matches, function(cur, match)
-		return math.max(#match.match2opponents, cur)
+		-- If the match comes from ShowBracket then it's opponents, otherwise it's match2opponents
+		-- Which is stupid, and needs to be fixed
+		return math.max(#(match.match2opponents or match.opponents or {}), cur)
 	end, 0)
 end
 
