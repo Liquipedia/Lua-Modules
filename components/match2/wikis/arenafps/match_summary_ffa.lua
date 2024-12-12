46259858216db3fdaf4360a854dd50617c1b2ec8
Rikard Blixt <rikardblixt@gmail.com>
2024-12-12 15:18:54 +0100

    feat(match2): support arenafps FFA

---
+++ components/match2/wikis/arenafps/match_summary_ffa.lua
@@ -0,0 +1,44 @@
+---
+-- @Liquipedia
+-- wiki=tft
+-- page=Module:MatchSummary/Ffa
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local CustomMatchSummary = {}
+
+local Lua = require('Module:Lua')
+
+local MatchGroupUtil = Lua.import('Module:MatchGroup/Util')
+local SummaryHelper = Lua.import('Module:MatchSummary/Base/Ffa')
+
+local MatchSummaryWidgets = Lua.import('Module:Widget/Match/Summary/Ffa/All')
+local HtmlWidgets = Lua.import('Module:Widget/Html/All')
+
+---@class ArenaFpsFfaMatchGroupUtilMatch: MatchGroupUtilMatch
+---@field games ArenaFpsFfaMatchGroupUtilGame[]
+
+---@param props {bracketId: string, matchId: string}
+---@return Widget
+function CustomMatchSummary.getByMatchId(props)
+	---@class ArenaFpsFfaMatchGroupUtilMatch
+	local match = MatchGroupUtil.fetchMatchForBracketDisplay(props.bracketId, props.matchId)
+	SummaryHelper.updateMatchOpponents(match)
+	local scoringData = SummaryHelper.createScoringData(match)
+
+	return HtmlWidgets.Fragment{children = {
+		MatchSummaryWidgets.Header{matchId = match.matchId, games = match.games},
+		MatchSummaryWidgets.Tab{
+			matchId = match.matchId,
+			idx = 0,
+			children = {
+				MatchSummaryWidgets.GamesSchedule{games = match.games},
+				MatchSummaryWidgets.PointsDistribution{scores = scoringData},
+				SummaryHelper.standardMatch(match),
+			}
+		}
+	}}
+end
+
+return CustomMatchSummary
