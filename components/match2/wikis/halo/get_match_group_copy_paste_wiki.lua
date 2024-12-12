3d98b70fa8f11d074f65dfd749bbab8bbac492d7
Rikard Blixt <rikardblixt@gmail.com>
2024-12-12 15:27:44 +0100

    feat(match2): support halo FFA

--- components/match2/wikis/halo/get_match_group_copy_paste_wiki.lua
+++ components/match2/wikis/halo/get_match_group_copy_paste_wiki.lua
@@ -13,6 +13,9 @@ local Lua = require('Module:Lua')
 
 local BaseCopyPaste = Lua.import('Module:GetMatchGroupCopyPaste/wiki/Base')
 
+local OpponentLibraries = require('Module:OpponentLibraries')
+local Opponent = OpponentLibraries.Opponent
+
 ---@class HaloMatch2CopyPaste: Match2CopyPasteBase
 local WikiCopyPaste = Class.new(BaseCopyPaste)
 
@@ -26,6 +29,20 @@ local INDENT = WikiCopyPaste.Indent
 ---@param args table
 ---@return string
 function WikiCopyPaste.getMatchCode(bestof, mode, index, opponents, args)
+	if opponents > 2 then
+		return WikiCopyPaste.getFfaMatchCode(bestof, mode, index, opponents, args)
+	else
+		return WikiCopyPaste.getStandardMatchCode(bestof, mode, index, opponents, args)
+	end
+end
+
+---@param bestof integer
+---@param mode string
+---@param index integer
+---@param opponents integer
+---@param args table
+---@return string
+function WikiCopyPaste.getStandardMatchCode(bestof, mode, index, opponents, args)
 	local showScore = Logic.nilOr(Logic.readBoolOrNil(args.score), bestof == 0)
 
 	local lines = Array.extend(
@@ -43,6 +60,48 @@ function WikiCopyPaste.getMatchCode(bestof, mode, index, opponents, args)
 	)
 
 	return table.concat(lines, '\n')
+end
+
+---@param bestof integer
+---@param mode string
+---@param index integer
+---@param opponents integer
+---@param args table
+---@return string
+function WikiCopyPaste.getFfaMatchCode(bestof, mode, index, opponents, args)
+	local lines = Array.extend(
+		'{{Match|finished=',
+		INDENT .. '|p1=5|p2=4|p3=3|p4=2|p5=1',
+		INDENT .. '|twitch=|youtube=',
+		Array.map(Array.range(1, bestof), function(mapIndex)
+			return INDENT .. '|map' .. mapIndex .. '={{Map|map=|date=|finished=|vod=}}'
+		end),
+		Array.map(Array.range(1, opponents), function(opponentIndex)
+			return INDENT .. '|opponent' .. opponentIndex .. '=' .. WikiCopyPaste._getFfaOpponent(mode, bestof)
+		end),
+		'}}'
+	)
+
+	return table.concat(lines, '\n')
+end
+
+---@param mode string
+---@param mapCount integer
+---@return string
+function WikiCopyPaste._getFfaOpponent(mode, mapCount)
+	local mapScores = table.concat(Array.map(Array.range(1, mapCount), function(idx)
+		return '|m' .. idx .. '={{MS||}}'
+	end))
+
+	if mode == Opponent.solo then
+		return '{{SoloOpponent||flag=' .. mapScores .. '}}'
+	elseif mode == Opponent.team then
+		return '{{TeamOpponent|' .. mapScores .. '}}'
+	elseif mode == Opponent.literal then
+		return '{{Literal|}}'
+	end
+
+	return ''
 end
 
 return WikiCopyPaste
