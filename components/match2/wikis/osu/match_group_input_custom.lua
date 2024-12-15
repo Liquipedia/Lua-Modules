f28766c5f3b79d87ad0527b553cac5a60c7be1bd
hjpalpha <andreasbuesch1@gmail.com>
2024-12-15 14:53:30 +0100

    linter: unused import

--- components/match2/wikis/osu/match_group_input_custom.lua
+++ components/match2/wikis/osu/match_group_input_custom.lua
@@ -8,8 +8,6 @@
 
 local Array = require('Module:Array')
 local Lua = require('Module:Lua')
-local Operator = require('Module:Operator')
-local Table = require('Module:Table')
 local Variables = require('Module:Variables')
 
 local MatchGroupInputUtil = Lua.import('Module:MatchGroup/Input/Util')
