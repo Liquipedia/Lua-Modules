6e30e4aba7483f283a5f0692c753975e98a4d24a
mbergen <mail@mbergen.de>
2024-12-31 11:52:11 +0100

    fix(match2) Correct iteration for game links

--- components/match2/commons/match_summary_base.lua
+++ components/match2/commons/match_summary_base.lua
@@ -176,7 +176,7 @@ function Footer:addLinks(links)
 		if not currentLinkData then
 			mw.log('Unknown link: ' .. linkType)
 		elseif type(link) == 'table' then
-			Array.forEach(link, function(gameLink, gameIdx)
+			Table.iter.forEachPair(link, function(gameIdx, gameLink)
 				local newText = currentLinkData.text .. ' on Game ' .. gameIdx
 				self:addLink(gameLink, currentLinkData.icon, currentLinkData.iconDark, newText)
 			end)
