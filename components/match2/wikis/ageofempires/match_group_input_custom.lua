9f2e10d970429142ed014a4e7bf0710d6634bb53
Rikard Blixt <rikardblixt@gmail.com>
2025-01-03 15:21:49 +0100

    feat(match2): initial support of FFA on AoE (#5274)

--- components/match2/wikis/ageofempires/match_group_input_custom.lua
+++ components/match2/wikis/ageofempires/match_group_input_custom.lua
@@ -34,16 +34,22 @@ local MapFunctions = {
 	BREAK_ON_EMPTY = true,
 }
 
+local FffMatchFunctions = {
+	OPPONENT_CONFIG = OPPONENT_CONFIG,
+}
+local FfaMapFunctions = {}
+
 ---@param match table
 ---@param options table?
 ---@return table
 function CustomMatchGroupInput.processMatch(match, options)
-	assert(not Logic.readBool(match.ffa), 'FFA is not yet supported in AoE match2.')
 	Table.mergeInto(match, MatchGroupInputUtil.getTournamentContext(match))
 	match.game, match.mapsInfo = MatchFunctions.getGameAndMapsFromTournament(match)
 
-	return MatchGroupInputUtil.standardProcessMatch(match, MatchFunctions)
+	return MatchGroupInputUtil.standardProcessMatch(match, MatchFunctions, FffMatchFunctions)
 end
+
+--- Normal 2-opponent Match
 
 ---@param match table
 ---@param opponentIndex integer
@@ -290,6 +296,50 @@ end
 ---@return string?
 function MapFunctions.getGame(match, map)
 	return Logic.emptyOr(map.game, match.game, Variables.varDefault('tournament_game'))
+end
+
+--- FFA Match
+
+---@param match table
+---@param opponents table[]
+---@param scoreSettings table
+---@return table[]
+function FffMatchFunctions.extractMaps(match, opponents, scoreSettings)
+	return MatchGroupInputUtil.standardProcessFfaMaps(match, opponents, scoreSettings, FfaMapFunctions)
+end
+
+---@param opponents table[]
+---@param maps table[]
+---@return fun(opponentIndex: integer): integer?
+function FffMatchFunctions.calculateMatchScore(opponents, maps)
+	return function(opponentIndex)
+		return Array.reduce(Array.map(maps, function(map)
+			return map.opponents[opponentIndex].score or 0
+		end), Operator.add, 0) + (opponents[opponentIndex].extradata.startingpoints or 0)
+	end
+end
+
+---@param match table
+---@param games table[]
+---@param opponents table[]
+---@param settings table
+---@return table
+function FffMatchFunctions.getExtraData(match, games, opponents, settings)
+	return {
+		placementinfo = settings.placementInfo,
+		settings = settings.settings,
+	}
+end
+
+---@param match table
+---@param map table
+---@param opponents table[]
+---@return table
+function FfaMapFunctions.getExtraData(match, map, opponents)
+	return {
+		dateexact = map.dateexact,
+		comment = map.comment,
+	}
 end
 
 return CustomMatchGroupInput
