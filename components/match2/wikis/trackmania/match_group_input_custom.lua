2354325773db1f68175e307c49bd16ba9425ad53
Rikard Blixt <rikardblixt@gmail.com>
2025-01-03 14:11:40 +0100

    feat(match2): add support for trackmania FFA

--- components/match2/wikis/trackmania/match_group_input_custom.lua
+++ components/match2/wikis/trackmania/match_group_input_custom.lua
@@ -10,24 +10,33 @@
 local Array = require('Module:Array')
 local Logic = require('Module:Logic')
 local Lua = require('Module:Lua')
+local Operator = require('Module:Operator')
 
 local MatchGroupInputUtil = Lua.import('Module:MatchGroup/Input/Util')
 local Opponent = Lua.import('Module:Opponent')
 
 local CustomMatchGroupInput = {}
-local MatchFunctions = {}
+local MatchFunctions = {
+	DEFAULT_MODE = '2v2',
+	DATE_FALLBACKS = {'tournament_enddate'},
+	getBestOf = MatchGroupInputUtil.getBestOf,
+}
 local MapFunctions = {}
 
-MatchFunctions.DEFAULT_MODE = '2v2'
-MatchFunctions.DATE_FALLBACKS = {'tournament_enddate'}
-MatchFunctions.getBestOf = MatchGroupInputUtil.getBestOf
+local FfaMatchFunctions = {
+	DEFAULT_MODE = 'solo',
+	DATE_FALLBACKS = {'tournament_enddate'},
+}
+local FfaMapFunctions = {}
 
 ---@param match table
 ---@param options table?
 ---@return table
 function CustomMatchGroupInput.processMatch(match, options)
-	return MatchGroupInputUtil.standardProcessMatch(match, MatchFunctions)
+	return MatchGroupInputUtil.standardProcessMatch(match, MatchFunctions, FfaMatchFunctions)
 end
+
+--- Normal 2-opponent Match
 
 ---@param match table
 ---@param opponents table[]
@@ -111,6 +120,50 @@ function MapFunctions.getExtraData(match, map, opponents)
 	return {
 		comment = map.comment,
 		overtime = Logic.readBool(map.overtime)
+	}
+end
+
+--- FFA Match
+
+---@param match table
+---@param opponents table[]
+---@param scoreSettings table
+---@return table[]
+function FfaMatchFunctions.extractMaps(match, opponents, scoreSettings)
+	return MatchGroupInputUtil.standardProcessFfaMaps(match, opponents, scoreSettings, FfaMapFunctions)
+end
+
+---@param opponents table[]
+---@param maps table[]
+---@return fun(opponentIndex: integer): integer?
+function FfaMatchFunctions.calculateMatchScore(opponents, maps)
+	return function(opponentIndex)
+		return Array.reduce(Array.map(maps, function(map)
+			return map.opponents[opponentIndex].score or 0
+		end), Operator.add, 0) + (opponents[opponentIndex].extradata.startingpoints or 0)
+	end
+end
+
+---@param match table
+---@param games table[]
+---@param opponents table[]
+---@param settings table
+---@return table
+function FfaMatchFunctions.getExtraData(match, games, opponents, settings)
+	return {
+		placementinfo = settings.placementInfo,
+		settings = settings.settings,
+	}
+end
+
+---@param match table
+---@param map table
+---@param opponents table[]
+---@return table
+function FfaMapFunctions.getExtraData(match, map, opponents)
+	return {
+		dateexact = map.dateexact,
+		comment = map.comment,
 	}
 end
 
