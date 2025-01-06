db96d34ceb4926d1efd828aa56fb0fae049cccbe
Rikard Blixt <rikardblixt@gmail.com>
2025-01-06 18:04:10 +0100

    feat(match2): support hearthstone ffa (#5282)
    * feat(match2): support hearthstone ffa
    
    * support the placement points thingimagig
    
    * fix
    
    * use round instead of .2f
    
    * 5207

--- components/match2/wikis/hearthstone/match_group_input_custom.lua
+++ components/match2/wikis/hearthstone/match_group_input_custom.lua
@@ -11,6 +11,7 @@ local CharacterStandardization = mw.loadData('Module:CharacterStandardization')
 local FnUtil = require('Module:FnUtil')
 local Logic = require('Module:Logic')
 local Lua = require('Module:Lua')
+local Operator = require('Module:Operator')
 local Table = require('Module:Table')
 
 local MatchGroupInputUtil = Lua.import('Module:MatchGroup/Input/Util')
@@ -18,22 +19,32 @@ local OpponentLibraries = require('Module:OpponentLibraries')
 local Opponent = OpponentLibraries.Opponent
 
 local CustomMatchGroupInput = {}
-local MatchFunctions = {}
-MatchFunctions.OPPONENT_CONFIG = {
+local OPPONENT_CONFIG = {
 	resolveRedirect = true,
 	pagifyTeamNames = true,
 	pagifyPlayerNames = true,
+}
+
+local MatchFunctions = {
+	OPPONENT_CONFIG = OPPONENT_CONFIG,
 }
 local MapFunctions = {
 	ADD_SUB_GROUP = true,
 }
 
+local FffMatchFunctions = {
+	OPPONENT_CONFIG = OPPONENT_CONFIG,
+}
+local FfaMapFunctions = {}
+
 ---@param match table
 ---@param options table?
 ---@return table
 function CustomMatchGroupInput.processMatch(match, options)
-	return MatchGroupInputUtil.standardProcessMatch(match, MatchFunctions)
+	return MatchGroupInputUtil.standardProcessMatch(match, MatchFunctions, FffMatchFunctions)
 end
+
+--- Normal 2 opponent match
 
 ---@param match table
 ---@param opponents table[]
@@ -158,7 +169,50 @@ function MapFunctions.getPartyParticipants(mapInput, opponent, opponentIndex)
 	end)
 end
 
-MapFunctions.readCharacter = FnUtil.curry(MatchGroupInputUtil.getCharacterName,
-	CharacterStandardization) --[[@as fun(class: string): string]]
+MapFunctions.readCharacter = FnUtil.curry(MatchGroupInputUtil.getCharacterName, CharacterStandardization)
+
+--- FFA Match
+
+---@param match table
+---@param opponents table[]
+---@param scoreSettings table
+---@return table[]
+function FffMatchFunctions.extractMaps(match, opponents, scoreSettings)
+	return MatchGroupInputUtil.standardProcessFfaMaps(match, opponents, scoreSettings, FfaMapFunctions)
+end
+
+---@param opponents table[]
+---@param maps table[]
+---@return fun(opponentIndex: integer): integer?
+function FffMatchFunctions.calculateMatchScore(opponents, maps)
+	return function(opponentIndex)
+		return Array.reduce(Array.map(maps, function(map)
+			return map.opponents[opponentIndex].score or 0
+		end), Operator.add, 0) + (opponents[opponentIndex].extradata.startingpoints or 0)
+	end
+end
+
+---@param match table
+---@param games table[]
+---@param opponents table[]
+---@param settings table
+---@return table
+function FffMatchFunctions.getExtraData(match, games, opponents, settings)
+	return {
+		placementinfo = settings.placementInfo,
+		settings = settings.settings,
+	}
+end
+
+---@param match table
+---@param map table
+---@param opponents table[]
+---@return table
+function FfaMapFunctions.getExtraData(match, map, opponents)
+	return {
+		dateexact = map.dateexact,
+		comment = map.comment,
+	}
+end
 
 return CustomMatchGroupInput
