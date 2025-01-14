f8110cafd4c5e9426d7e4b9fafdc78ff7895215e
hjpalpha <andreasbuesch1@gmail.com>
2025-01-14 10:00:35 +0100

    add customs (redirects)

---
+++ components/match2/wikis/starcraft2/match_group_util_custom.lua
@@ -0,0 +1,12 @@
+---
+-- @Liquipedia
+-- wiki=starcraft2
+-- page=Module:MatchGroup/Util/Custom
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Lua = require('Module:Lua')
+local StarcraftMatchGroupUtil = Lua.import('Module:MatchGroup/Util/Starcraft')
+
+return StarcraftMatchGroupUtil
