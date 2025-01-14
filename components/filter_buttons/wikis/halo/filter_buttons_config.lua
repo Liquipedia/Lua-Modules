af1d840682cb31ae9aa4403d8407fbeeee1cfde3
Jasper Jacobs <97167868+Kanopedia@users.noreply.github.com>
2025-01-14 12:51:56 +0100

    Update filter_buttons_config.lua

--- components/filter_buttons/wikis/halo/filter_buttons_config.lua
+++ components/filter_buttons/wikis/halo/filter_buttons_config.lua
@@ -23,21 +23,6 @@ Config.categories = {
 		transform = function(tier)
 			return Tier.toName(tier)
 		end,
-		expandKey = "liquipediatiertype",
-	},
-	{
-		name = 'liquipediatiertype',
-		property = 'liquipediaTierType',
-		expandable = true,
-		load = function(category)
-			category.items = {}
-			for _, tiertype in Tier.iterate('tierTypes') do
-				table.insert(category.items, Tier.toIdentifier(tiertype.value))
-			end
-		end,
-		transform = function(tiertype)
-			return select(2, Tier.toName(1, tiertype))
-		end,
 	},
 }
 
