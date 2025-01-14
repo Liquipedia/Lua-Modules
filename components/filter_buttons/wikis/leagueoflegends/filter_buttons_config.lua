28196d33ff5ccd3d642b1e04f48951d2521c0f30
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2025-01-14 22:07:19 +0900

    feat(mainpage): Standardize LoL main page (#5302)
    * Add standardized mainpage for LoL
    
    * Use standardized tournament ticker
    
    Ref: Liquipedia/Lua-Modules#5293
    
    * Use widget version of MainPageMatches
    
    * Update boxids
    
    * Add regional filters
    
    * formatting
    
    * Add header image for LoL mainpage
    
    * Update components/filter_buttons/wikis/leagueoflegends/filter_buttons_config.lua
    
    Change double quotes to single quotes
    
    * Just another double quotes
    
    ---------
    
    Co-authored-by: Rikard Blixt <rikardblixt@gmail.com>

---
+++ components/filter_buttons/wikis/leagueoflegends/filter_buttons_config.lua
@@ -0,0 +1,56 @@
+---
+-- @Liquipedia
+-- wiki=leagueoflegends
+-- page=Module:FilterButtons/Config
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Tier = require('Module:Tier/Utils')
+local Config = {}
+
+Config.categories = {
+	{
+		name = 'liquipediatier',
+		property = 'liquipediaTier',
+		load = function(category)
+			category.items = {}
+			for _, tier in Tier.iterate('tiers') do
+				table.insert(category.items, tier.value)
+			end
+		end,
+		defaultItems = { '1', '2', '3' },
+		transform = function(tier)
+			return Tier.toName(tier)
+		end,
+		expandKey = 'region',
+	},
+	{
+		name = 'region',
+		property = 'region',
+		expandable = true,
+		items = {
+			'Europe', 'North America', 'Korea', 'China', 'Japan', 'Latin America North',
+			'Latin America South', 'Taiwan', 'Oceania', 'Brazil', 'Other',
+		},
+		defaultItems = { 'Europe', 'North America', 'Korea', 'China', 'Brazil', 'Other' },
+		transform = function(region)
+			local regionToShortName = {
+				['Europe'] = 'eu',
+				['North America'] = 'na',
+				['Korea'] = 'kr',
+				['China'] = 'ch',
+				['Japan'] = 'jp',
+				['Latin America North'] = 'latam n',
+				['Latin America South'] = 'latam s',
+				['Taiwan'] = 'tw',
+				['Oceania'] = 'oce',
+				['Brazil'] = 'br',
+				['Other'] = 'other',
+			}
+			return regionToShortName[region]
+		end,
+	},
+}
+
+return Config
