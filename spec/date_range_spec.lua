673a6f282b78630e045ce299eeb59c92f7280bb0
Rikard Blixt <rikardblixt@gmail.com>
2025-01-14 13:48:58 +0200

    int

--- spec/date_range_spec.lua
+++ spec/date_range_spec.lua
@@ -24,12 +24,14 @@ describe('DateRange Widget', function()
 		}), widget:render())
 	end)
 
-	it('should return "date-range-different-months-unknown-days-and-end-month" if endDate and startDay is missing', function()
-		local widget = DateRange{startDate = {year = 2023, month = 10}}
-		assert.are.equal(I18n.translate('date-range-different-months-unknown-days-and-end-month', {
-			startMonth = 'Oct'
-		}), widget:render())
-	end)
+	it('should return "date-range-different-months-unknown-days-and-end-month" if endDate and startDay is missing',
+		function()
+			local widget = DateRange{startDate = {year = 2023, month = 10}}
+			assert.are.equal(I18n.translate('date-range-different-months-unknown-days-and-end-month', {
+				startMonth = 'Oct'
+			}), widget:render())
+		end
+	)
 
 	it('should return "date-range-same-month-unknown-days" if both days are unknown and in the same month', function()
 		local widget = DateRange{startDate = {year = 2023, month = 10}, endDate = {year = 2023, month = 10}}
@@ -38,12 +40,14 @@ describe('DateRange Widget', function()
 		}), widget:render())
 	end)
 
-	it('should return "date-range-different-months-unknown-days" if both days are unknown and in different months', function()
-		local widget = DateRange{startDate = {year = 2023, month = 10}, endDate = {year = 2023, month = 11}}
-		assert.are.equal(I18n.translate('date-range-different-months-unknown-days', {
-			startMonth = 'Oct', endMonth = 'Nov'
-		}), widget:render())
-	end)
+	it('should return "date-range-different-months-unknown-days" if both days are unknown and in different months',
+		function()
+			local widget = DateRange{startDate = {year = 2023, month = 10}, endDate = {year = 2023, month = 11}}
+			assert.are.equal(I18n.translate('date-range-different-months-unknown-days', {
+				startMonth = 'Oct', endMonth = 'Nov'
+			}), widget:render())
+		end
+	)
 
 	it('should return "date-range-different-months-unknown-end-day" if end day is unknown', function()
 		local widget = DateRange{startDate = {year = 2023, month = 10, day = 1}, endDate = {year = 2023, month = 11}}
@@ -60,16 +64,16 @@ describe('DateRange Widget', function()
 	end)
 
 	it('should return "date-range-same-month" if startDate and endDate are in the same month', function()
-		local widget = DateRange{startDate = {year = 2023, month = 10, day = 1}, endDate = {year = 2023, month = 10, day = 10}}
+		local w = DateRange{startDate = {year = 2023, month = 10, day = 1}, endDate = {year = 2023, month = 10, day = 10}}
 		assert.are.equal(I18n.translate('date-range-same-month', {
 			startMonth = 'Oct', startDate = '01', endMonth = 'Oct', endDate = '10'
-		}), widget:render())
+		}), w:render())
 	end)
 
 	it('should return "date-range-different-months" if startDate and endDate are in different months', function()
-		local widget = DateRange{startDate = {year = 2023, month = 10, day = 1}, endDate = {year = 2023, month = 11, day = 10}}
+		local w = DateRange{startDate = {year = 2023, month = 10, day = 1}, endDate = {year = 2023, month = 11, day = 10}}
 		assert.are.equal(I18n.translate('date-range-different-months', {
 			startMonth = 'Oct', startDate = '01', endMonth = 'Nov', endDate = '10'
-		}), widget:render())
+		}), w:render())
 	end)
 end)
