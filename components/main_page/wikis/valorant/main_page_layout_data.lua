f5d6581f077acd07c53bfe14bfcbf62162cee1b2
Verus <145709809+iamentry@users.noreply.github.com>
2025-01-15 14:59:37 -0600

    Update components/main_page/wikis/valorant/main_page_layout_data.lua

--- components/main_page/wikis/valorant/main_page_layout_data.lua
+++ components/main_page/wikis/valorant/main_page_layout_data.lua
@@ -19,7 +19,7 @@ local CONTENT = {
 			'<div style="display:inline; float:left; font-style:italic;">[[#Top|Back to top]]</div>' ..
 			'<div style="display:inline; float:right;" class="plainlinks smalledit">' ..
 			'&#91;[[Special:EditPage/Player Transfers/{{CURRENTYEAR}}/{{CURRENTMONTHNAME}}|edit]]&#93;</div>' ..
-			'<div style="display:inline; margin:0 10px;white-space:nowrap; font-size:15px; font-style:italic;">' ..
+			'<div style="white-space:nowrap; display:inline; margin:0 10px; font-size:15px; font-style:italic;">' ..
 			'[[Portal:Transfers|See more transfers]]<span style="font-style:normal; padding:0 5px;">&#8226;</span>' ..
 			'[[Transfer query]]</div><div style="display:inline; margin:0 10px;white-space:nowrap; font-size:15px; ' ..
 			'font-style:italic;">[[Special:RunQuery/Transfer|Input Form]]<span style="font-style:normal; ' ..
