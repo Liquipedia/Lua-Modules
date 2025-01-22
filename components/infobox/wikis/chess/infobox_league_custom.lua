9bedf5ef2d8d2b56668e0b47bf1980f12e5cdeb7
Hesketh2 <88981446+Hesketh2@users.noreply.github.com>
2025-01-22 14:57:29 +0700

    feat(Infobox): add Infobox League for Chess

---
+++ components/infobox/wikis/chess/infobox_league_custom.lua
@@ -0,0 +1,67 @@
+---
+-- @Liquipedia
+-- wiki=chess
+-- page=Module:Infobox/League/Custom
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Class = require('Module:Class')
+local Lua = require('Module:Lua')
+local String = require('Module:StringUtils')
+
+local Injector = Lua.import('Module:Widget/Injector')
+local League = Lua.import('Module:Infobox/League')
+
+local Widgets = require('Module:Widget/All')
+local Cell = Widgets.Cell
+local Title = Widgets.Title
+
+---@class ChessLeagueInfobox: InfoboxLeague
+local CustomLeague = Class.new(League)
+local CustomInjector = Class.new(Injector)
+
+local MODES = {
+	classical = 'Classical',
+	blitz = 'Blitz',
+	rapid = 'Rapid',
+	chess960 = 'Chess960',
+	puzzle = 'Puzzle Rush',
+	dice = 'Dice Chess',
+	various = 'Multiple',
+}
+
+---@param frame Frame
+---@return Html
+function CustomLeague.run(frame)
+	local league = CustomLeague(frame)
+	league:setWidgetInjector(CustomInjector(league))
+
+	return league:createInfobox()
+end
+
+---@param id string
+---@param widgets Widget[]
+---@return Widget[]
+function CustomInjector:parse(id, widgets)
+	local args = self.caller.args
+
+	if id == 'gamesettings' then
+		return {Cell{name = 'Variant', content = {self.caller:_getGameMode()}},}
+	end
+	
+	return widgets
+end
+
+---@param args table
+function CustomLeague:customParseArguments(args)
+	self.data.mode = self:_getGameMode()
+end
+
+
+---@return string?
+function CustomLeague:_getGameMode()
+	return MODES[string.lower(self.args.mode or '')] or MODES['classical']
+end
+
+return CustomLeague
