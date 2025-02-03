0dd99fb80c903375ec827d7a5a4f15c435174d79
hjpalpha <andreasbuesch1@gmail.com>
2025-02-03 11:17:46 +0100

    shut up anno warning

--- components/match2/wikis/warcraft/game_summary.lua
+++ components/match2/wikis/warcraft/game_summary.lua
@@ -22,7 +22,8 @@ local MatchSummaryWidgets = Lua.import('Module:Widget/Match/Summary/Ffa/All')
 ---@param props {bracketId: string, matchId: string, gameIdx: integer}
 ---@return Html
 function CustomGameSummary.getGameByMatchId(props)
-	local match = MatchGroupUtil.fetchMatchForBracketDisplay(props.bracketId, props.matchId)
+	local match = MatchGroupUtil.fetchMatchForBracketDisplay(props.bracketId, props.matchId) --[[
+		@as FFAMatchGroupUtilMatch]]
 
 	local game = match.games[props.gameIdx]
 	assert(game, 'Error Game ID ' .. tostring(props.gameIdx) .. ' not found')
