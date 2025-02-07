70733b0896551daeefe69e176baf94186dde590d
Rikard Blixt <rikardblixt@gmail.com>
2025-02-07 10:50:14 +0100

    normlize rating numbers

--- components/ratings/ratings_storage_extension.lua
+++ components/ratings/ratings_storage_extension.lua
@@ -6,11 +6,12 @@
 -- Please see https://github.com/Liquipedia/Lua-Modules to contribute
 --
 
+local Array = require('Module:Array')
 local Date = require('Module:Date/Ext')
 local OpponentLibraries = require('Module:OpponentLibraries')
 local Opponent = OpponentLibraries.Opponent
 
-local RatingsStorageLpdb = {}
+local RatingsStorageExtension = {}
 
 local PROGRESSION_STEP_DAYS = 7 -- How many days each progression step is
 
@@ -18,7 +19,7 @@ local PROGRESSION_STEP_DAYS = 7 -- How many days each progression step is
 ---@param teamLimit integer?
 ---@param progressionLimit integer?
 ---@return RatingsEntry[]
-function RatingsStorageLpdb.getRankings(date, teamLimit, progressionLimit)
+function RatingsStorageExtension.getRankings(date, teamLimit, progressionLimit)
 	if not date then
 		error('No date provided')
 	end
@@ -36,20 +37,40 @@ function RatingsStorageLpdb.getRankings(date, teamLimit, progressionLimit)
 	local teams = mw.ext.Dota2Ranking.get(startDate, date)
 
 	-- Add additional data to the teams
-	for _, team in ipairs(teams) do
-		local teamInfo = RatingsStorageLpdb._getTeamInfo(team.name)
-		team.opponent = Opponent.resolve(Opponent.readOpponentArgs({type = Opponent.team, team.name}), date)
-		team.region = teamInfo.region
-	end
+	return Array.map(teams, function(team)
+		local teamInfo = RatingsStorageExtension._getTeamInfo(team.name)
+		---@type RatingsEntry
+		local newTeam = {
+			name = team.name,
+			rank = team.rank,
+			rating = RatingsStorageExtension._normalizeRating(team.rating),
+			region = teamInfo.region,
+			opponent = Opponent.resolve(Opponent.readOpponentArgs({type = Opponent.team, team.name}), date),
+			streak = team.streak,
+			progression = Array.map(team.progression, function(progression)
+				return {
+					date = progression.date,
+					rating = RatingsStorageExtension._normalizeRating(progression.rating),
+					rank = progression.rank,
+				}
+			end),
+		}
+		return newTeam
+	end)
+end
 
-	return teams
+--- Normalize a rating
+---@param rating number
+---@return number
+function RatingsStorageExtension._normalizeRating(rating)
+	return math.floor((rating * 1000) + 0.5)
 end
 
 --- Get team information from Team Template and Team Page
 ---@param teamName string
 ---@return {region: string?}
-function RatingsStorageLpdb._getTeamInfo(teamName)
-	local teamInfo = RatingsStorageLpdb._fetchTeamInfo(teamName)
+function RatingsStorageExtension._getTeamInfo(teamName)
+	local teamInfo = RatingsStorageExtension._fetchTeamInfo(teamName)
 
 	return {
 		region = teamInfo.region,
@@ -59,7 +80,7 @@ end
 --- Fetch team information from Team Page
 ---@param name string
 ---@return {region: string?}
-function RatingsStorageLpdb._fetchTeamInfo(name)
+function RatingsStorageExtension._fetchTeamInfo(name)
 	local res = mw.ext.LiquipediaDB.lpdb(
 		'team',
 		{
@@ -74,4 +95,4 @@ function RatingsStorageLpdb._fetchTeamInfo(name)
 
 	return res[1] or {}
 end
-return RatingsStorageLpdb
+return RatingsStorageExtension
