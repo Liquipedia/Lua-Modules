d85fc435bb27c8ae481f80d631cb826e8eea4fdc
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2025-02-07 09:19:48 +0100

    fix(Statistics): game identifiers not found (#5407)
    * fix(count): game identifiers not found
    
    * some more
    
    * another one
    
    * another one
    
    * another one
    
    * fix typo

--- components/statistics/portal_statistics.lua
+++ components/statistics/portal_statistics.lua
@@ -71,11 +71,15 @@ Section: Chart Entry Functions
 function StatisticsPortal.gameEarningsChart(args)
 	args = args or {}
 
+	local games = Array.map(StatisticsPortal._isTableOrSplitOrDefault(args.customGames, GAMES), function(game)
+		return Game.toIdentifier{game = game, useDefault = false} or game
+	end)
+
 	local params = {
 		variable = 'game',
 		processFunction = StatisticsPortal._defaultProcessFunction,
 		catLabel = 'Year',
-		defaultInputs = GAMES,
+		defaultInputs = games,
 		axisRotate = tonumber(args.axisRotate),
 	}
 
@@ -470,7 +474,8 @@ function StatisticsPortal.prizepoolBreakdown(args)
 		local conditions = StatisticsPortal._returnBaseConditions()
 
 		if args.game then
-			conditions:add{ConditionNode(ColumnName('game'), Comparator.eq, args.game)}
+			local gameIdentifier = Game.toIdentifier{game = args.game, useDefault = false} or args.game
+			conditions:add{ConditionNode(ColumnName('game'), Comparator.eq, gameIdentifier)}
 		end
 
 		conditions:add{ConditionTree(BooleanOperator.all):add{
@@ -521,7 +526,8 @@ function StatisticsPortal.prizepoolBreakdown(args)
 	local conditions = StatisticsPortal._returnBaseConditions()
 
 	if args.game then
-		conditions:add{ConditionNode(ColumnName('game'), Comparator.eq, args.game)}
+		local gameIdentifier = Game.toIdentifier{game = args.game, useDefault = false} or args.game
+		conditions:add{ConditionNode(ColumnName('game'), Comparator.eq, gameIdentifier)}
 	end
 
 	conditions:add{ConditionTree(BooleanOperator.all):add{
@@ -582,6 +588,9 @@ function StatisticsPortal.pieChartBreakdown(args)
 	)
 
 	if args.multiGame then
+		local games = Array.map(StatisticsPortal._isTableOrSplitOrDefault(args.customGames, GAMES), function(game)
+			return Game.toIdentifier{game = game, useDefault = false} or game
+		end)
 		wrapper:node(mw.html.create('div')
 			:addClass('template-box')
 			:css('padding-right', '5em')
@@ -589,7 +598,7 @@ function StatisticsPortal.pieChartBreakdown(args)
 			:css('text-align', 'center')
 			:wikitext('Game Breakdown')
 			:node(StatisticsPortal._getPieChartData(
-				args, 'game', 'Other', StatisticsPortal._isTableOrSplitOrDefault(args.customGames, GAMES)
+				args, 'game', 'Other', games
 			))
 		)
 	end
@@ -634,7 +643,8 @@ function StatisticsPortal.pieChartBreakdown(args)
 	end
 
 	if args.game then
-		conditions:add{ConditionNode(ColumnName('game'), Comparator.eq, args.game)}
+		local gameIdentifier = Game.toIdentifier{game = args.game, useDefault = false} or args.game
+		conditions:add{ConditionNode(ColumnName('game'), Comparator.eq, gameIdentifier)}
 	end
 
 	local data = mw.ext.LiquipediaDB.lpdb('tournament', {
@@ -900,7 +910,8 @@ function StatisticsPortal._getPieChartData(args, groupBy, defaultValue, groupVal
 	end
 
 	if args.game then
-		LPDBConditions:add{ConditionNode(ColumnName('game'), Comparator.eq, args.game)}
+		local gameIdentifier = Game.toIdentifier{game = args.game, useDefault = false} or args.game
+		LPDBConditions:add{ConditionNode(ColumnName('game'), Comparator.eq, gameIdentifier)}
 	end
 
 	local function parseTournament(data)
@@ -935,7 +946,12 @@ function StatisticsPortal._getPieChartData(args, groupBy, defaultValue, groupVal
 
 	if groupBy == 'game' and Logic.readBool(args.abbreviateGame) then
 		chartData = Array.map(chartData, function(entry)
-			entry.name = Game.abbreviation{game = entry.name} or entry.name
+			entry.name = Game.abbreviation{game = entry.name, useDefault = false} or entry.name
+			return entry
+		end)
+	elseif groupBy == 'game' then
+		chartData = Array.map(chartData, function(entry)
+			entry.name = Game.name{game = entry.name, useDefault = false} or entry.name
 			return entry
 		end)
 	end
@@ -1473,6 +1489,5 @@ function StatisticsPortal._addArrays(arrays)
 		return Array.reduce(Array.map(arrays, Operator.property(index)), Operator.add)
 	end)
 end
-
 
 return Class.export(StatisticsPortal)
