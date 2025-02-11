947cd79c23262236ece738be16ea4bc350fac66b
Hesketh2 <88981446+Hesketh2@users.noreply.github.com>
2025-02-11 16:47:15 +0700

    War Thunder

---
+++ standard/info/wikis/warthunder/info.lua
@@ -0,0 +1,41 @@
+---
+-- @Liquipedia
+-- wiki=warthunder
+-- page=Module:Info
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+return {
+	startYear = 2012,
+	wikiName = 'warthunder',
+	name = 'War Thunder',
+	defaultGame = 'War Thunder',
+	games = {
+		warthunder = {
+			abbreviation = 'WT',
+			name = 'War Thunder',
+			link = 'War Thunder',
+			logo = {
+				darkMode = 'War Thunder default allmode.png',
+				lightMode = 'War Thunder default allmode.png',
+			},
+			defaultTeamLogo = {
+				darkMode = 'War Thunder default allmode.png',
+				lightMode = 'War Thunder default allmode.png',
+			},
+		},
+	},
+	config = {
+		squads = {
+			hasPosition = false,
+			hasSpecialTeam = false,
+			allowManual = false,
+		},
+		match2 = {
+			status = 2,
+			matchWidth = 180,
+		},
+	},
+	defaultRoundPrecision = 0,
+}
