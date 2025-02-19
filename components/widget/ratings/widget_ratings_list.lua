814db51c1f36b90750677f5bfb4c679ae5411d90
Rikard Blixt <rikardblixt@gmail.com>
2025-02-19 15:17:29 +0100

    feat: team rankings progression graph on rankings table (#5455)
    * feat: add graph support for dota2 rankings
    
    * sort instead of reverse

--- components/widget/ratings/widget_ratings_list.lua
+++ components/widget/ratings/widget_ratings_list.lua
@@ -8,10 +8,12 @@
 
 local Array = require('Module:Array')
 local Class = require('Module:Class')
+local Date = require('Module:Date/Ext')
 local Flags = require('Module:Flags')
 local Icon = require('Module:Icon')
 local Logic = require('Module:Logic')
 local Lua = require('Module:Lua')
+local Operator = require('Module:Operator')
 
 local OpponentLibraries = require('Module:OpponentLibraries')
 local OpponentDisplay = OpponentLibraries.OpponentDisplay
@@ -28,6 +30,59 @@ local RatingsStorageFactory = Lua.import('Module:Ratings/Storage/Factory')
 ---@operator call(table): RatingsList
 local RatingsList = Class.new(Widget)
 
+---@param teamData RatingsEntry
+---@param defaultMaxY integer
+---@return string
+local function makeTeamChart(teamData, defaultMaxY)
+	local progression = Array.sortBy(teamData.progression, Operator.property('date'))
+	local worstRankOfTeam = Array.max(Array.map(progression, Operator.property('rank')))
+
+	local dates = Array.map(Array.map(progression, Operator.property('date')), function(isoDate)
+		return os.date('%b %d', os.time(Date.parseIsoDate(isoDate)))
+	end)
+
+	return mw.ext.Charts.chart {
+		xAxis = {
+			name = 'Date',
+			nameLocation = 'middle',
+			type = 'category',
+			data = dates,
+		},
+		yAxis = {
+			name = 'Rank',
+			nameLocation = 'middle',
+			nameRotate = 90,
+			type = 'value',
+			inverse = true,
+			min = 1,
+			max = math.max(worstRankOfTeam, defaultMaxY),
+		},
+		tooltip = {
+			trigger = 'axis',
+		},
+		grid = {
+			show = true,
+		},
+		size = {
+			height = 250,
+			pwidth = 100,
+		},
+		series = {
+			{
+				data = Array.map(progression, function(progress)
+					return progress.rank and tostring(progress.rank) or ''
+				end),
+				type = 'line',
+				name = 'Rank',
+				symbolSize = 8,
+				itemStyle = {
+					color = '#EE6666',
+				},
+			}
+		}
+	}
+end
+
 ---@return Widget
 function RatingsList:render()
 	-- Simple check to verify that the input is a osdate
@@ -38,6 +93,7 @@ function RatingsList:render()
 
 	local teamLimit = tonumber(self.props.teamLimit) or self.defaultProps.teamLimit
 	local progressionLimit = tonumber(self.props.progressionLimit) or self.defaultProps.progressionLimit
+	local showGraph = Logic.readBool(self.props.showGraph)
 	local isSmallerVersion = Logic.readBool(self.props.isSmallerVersion)
 
 	local getRankings = RatingsStorageFactory.createGetRankings {
@@ -48,6 +104,7 @@ function RatingsList:render()
 	local teams = getRankings(teamLimit, progressionLimit)
 
 	local teamRows = Array.map(teams, function(team, rank)
+		local uniqueId = dateAsString .. '-' .. rank
 		local streakText = team.streak > 1 and team.streak .. 'W' or (team.streak < -1 and (-team.streak) .. 'L') or '-'
 		local streakClass = (team.streak > 1 and 'group-table-rank-change-up')
 			or (team.streak < -1 and 'group-table-rank-change-down')
@@ -71,8 +128,42 @@ function RatingsList:render()
 				attributes = { ['data-ranking-table-cell'] = 'streak' },
 				children = streakText,
 				classes = { streakClass }
-			}
+			},
+			showGraph and (HtmlWidgets.Td {
+				attributes = {
+					class = 'ranking-table__toggle-graph-cell',
+					['data-ranking-table-cell'] = 'graph'
+				},
+				children = HtmlWidgets.Span {
+					attributes = {
+						class = 'ranking-table__toggle-graph',
+						['data-ranking-table'] = 'toggle',
+						['data-ranking-table-id'] = uniqueId,
+						['aria-controls'] = uniqueId,
+						tabindex = '1'
+					},
+					children = Icon.makeIcon { iconName = 'expand' }
+			} }) or nil
 		)
+
+		local graphRow = showGraph and {
+			HtmlWidgets.Td {
+				attributes = {
+					colspan = '7',
+					['data-ranking-table-cell'] = 'graph'
+				},
+				children = HtmlWidgets.Div {
+					children = {
+						OpponentDisplay.InlineOpponent { opponent = team.opponent },
+						Logic.tryOrElseLog(
+							function() return makeTeamChart(team, teamLimit) end,
+							function() return 'Failed to make graph for team' end
+						)
+					},
+					classes = { 'ranking-table__graph-row-container' }
+				}
+			}
+		} or nil
 
 		local isEven = rank % 2 == 0
 		local rowClasses = { 'ranking-table__row' }
@@ -85,6 +176,15 @@ function RatingsList:render()
 
 		return {
 			HtmlWidgets.Tr { children = teamRow, classes = rowClasses },
+			showGraph and HtmlWidgets.Tr {
+				children = graphRow,
+				classes = { 'ranking-table__graph-row d-none' },
+				attributes = {
+					['data-ranking-table'] = 'graph-row',
+					['aria-expanded'] = 'false',
+					['data-ranking-table-id'] = uniqueId
+				},
+			} or nil
 		}
 	end)
 
@@ -132,7 +232,11 @@ function RatingsList:render()
 							HtmlWidgets.Th { attributes = { ['data-ranking-table-cell'] = 'team' }, children = 'Team' },
 							HtmlWidgets.Th { attributes = { ['data-ranking-table-cell'] = 'rating' }, children = 'Points' },
 							HtmlWidgets.Th { attributes = { ['data-ranking-table-cell'] = 'region' }, children = 'Region' },
-							HtmlWidgets.Th { attributes = { ['data-ranking-table-cell'] = 'streak' }, children = 'Streak' }
+							HtmlWidgets.Th { attributes = { ['data-ranking-table-cell'] = 'streak' }, children = 'Streak' },
+							showGraph and HtmlWidgets.Th {
+								attributes = { ['data-ranking-table-cell'] = 'graph' },
+								children = Icon.makeIcon { iconName = 'chart' }
+							} or nil
 						),
 						classes = { 'ranking-table__header-row' },
 					},
