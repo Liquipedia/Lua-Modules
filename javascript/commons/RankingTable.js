f805767b440e433c9593c05f56889a7b1e12392a
Rikard Blixt <rikardblixt@gmail.com>
2025-02-19 21:03:36 +0100

    feat: week selection menu for Dota 2 rankings table (#5456)
    * feat: add possibility to select historic weeks for dota2 ratings
    
    * eof
    
    * extract to func
    
    * renamed upstreams
    
    * finish todo
    
    * typo from split
    
    * fix charts when going back in time
    
    * fix cached charts
    
    * fix capitalization

--- javascript/commons/RankingTable.js
+++ javascript/commons/RankingTable.js
@@ -1,20 +1,101 @@
 liquipedia.rankingTable = {
 	identifierAttribute: 'data-ranking-table-id',
-	ContentSelector: '[data-ranking-table="content"]',
+	contentSelector: '[data-ranking-table="content"]',
 	toggleButtonSelector: '[data-ranking-table="toggle"]',
 	graphRowSelector: '[data-ranking-table="graph-row"]',
+	selectContainerSelector: '[data-ranking-table="select-container"]',
+	patchLabelSelector: '[data-ranking-table="patch-label"]',
+	optionDataSelector: '[data-ranking-table="select-data"]',
 	patchLabelElement: null,
 	activeSelectOption: null,
+	rankingTable: null,
 	rankingContent: null,
+	cache: {},
 	toggleButtonListeners: [],
+	options: [],
 
 	init: function () {
-		this.rankingContent = document.querySelector( this.ContentSelector );
-		if ( !this.rankingContent ) {
+		mw.loader.using( 'ext.Charts.scripts', () => {
+			this.rankingContent = document.querySelector( this.contentSelector );
+			if ( !this.rankingContent ) {
+				return;
+			}
+
+			this.populateOptions();
+			this.toggleGraphVisibility();
+			this.initSelectElement();
+
+			// Set active select option to the first option object on init
+			if ( this.activeSelectOption === null && this.options.length > 0 ) {
+				this.activeSelectOption = this.options[ 0 ];
+			}
+
+			// Set patch label to the active select option patch
+			this.patchLabelElement = document.querySelector( this.patchLabelSelector );
+			if ( this.patchLabelElement && this.activeSelectOption !== null ) {
+				this.updatePatchLabel( this.activeSelectOption.patch );
+			}
+
+			// Store initial HTML content in cache
+			this.cache[ this.activeSelectOption.value ] = this.rankingContent.innerHTML;
+		} );
+	},
+
+	populateOptions: function () {
+		document.querySelectorAll( this.optionDataSelector ).forEach( ( option ) => {
+			const date = new Date( option.getAttribute( 'data-date' ) );
+			const patch = option.getAttribute( 'data-name' );
+			const value = this.standardizeDateFormat( date );
+			const text = date.toLocaleDateString( 'en-US', { year: 'numeric', month: 'long', day: 'numeric' } );
+			this.options.push( {
+				value: value,
+				text: text,
+				patch: patch
+			} );
+		} );
+	},
+
+	fetchRatingsData: function( date ) {
+		const dateValue = this.standardizeDateFormat( new Date( date ) );
+
+		// Check if data is already fetched
+		if ( this.cache[ dateValue ] ) {
+			this.removeToggleButtonListeners();
+			this.rankingContent.innerHTML = this.cache[ dateValue ];
+			this.toggleGraphVisibility();
+			mw.ext.Charts.recreateCharts();
 			return;
 		}
 
-		this.toggleGraphVisibility();
+		const wikiText =
+			`{{#invoke:Lua|invoke|module=Widget/Factory|fn=fromTemplate|widget=Ratings|teamLimit=20|
+			progressionLimit=12|date=${ dateValue }|storageType=extension}}`;
+		const api = new mw.Api();
+		api.get( {
+			action: 'parse',
+			format: 'json',
+			contentmodel: 'wikitext',
+			maxage: 600,
+			smaxage: 600,
+			disablelimitreport: true,
+			uselang: 'content',
+			prop: 'text',
+			text: wikiText
+		} ).done( ( data ) => {
+			if ( data.parse?.text?.[ '*' ] ) {
+				this.removeToggleButtonListeners();
+				// Insert fetched HTML content
+				this.rankingContent.insertAdjacentHTML( 'beforeend', data.parse.text[ '*' ] );
+				// Remove the original list
+				this.rankingContent.firstElementChild.remove();
+				// Remove non-list elements
+				this.rankingContent.innerHTML = this.rankingContent.querySelector( this.contentSelector ).innerHTML;
+				// Store fetched HTML content in cache
+				this.cache[ dateValue ] = this.rankingContent.outerHTML;
+				this.toggleGraphVisibility();
+				mw.ext.Charts.recreateCharts();
+			}
+		} );
 	},
 
 	removeToggleButtonListeners: function () {
@@ -29,6 +110,52 @@ liquipedia.rankingTable = {
 			this.patchLabelElement = document.querySelector( this.patchLabelSelector );
 		}
 		this.patchLabelElement.innerText = patch;
+	},
+
+	initSelectElement: function () {
+		const selectContainer = document.querySelector( this.selectContainerSelector );
+
+		if ( !selectContainer ) {
+			return;
+		}
+
+		const selectElement = this.createSelectElement();
+		const optionElements = this.createOptions();
+
+		selectElement.append( ...optionElements );
+		selectContainer.insertBefore( selectElement, selectContainer.firstChild );
+
+		// Add change event listener to update activeSelectOption and patch label
+		selectElement.addEventListener( 'change', this.onSelectChange.bind( this ) );
+	},
+
+	onSelectChange: function ( event ) {
+		const selectedOption = this.options.find( ( option ) => option.value === event.target.value );
+		if ( selectedOption ) {
+			this.activeSelectOption = selectedOption;
+			this.updatePatchLabel( selectedOption.patch );
+			this.fetchRatingsData( selectedOption.value );
+		}
+	},
+
+	createSelectElement: function () {
+		const selectElement = document.createElement( 'select' );
+		selectElement.ariaLabel = 'Select a week';
+		selectElement.classList.add( 'ranking-table__select' );
+		return selectElement;
+	},
+
+	createOptions: function () {
+		const optionsElements = [];
+
+		this.options.forEach( ( option ) => {
+			const optionElement = document.createElement( 'option' );
+			optionElement.value = option.value;
+			optionElement.innerText = option.text;
+			optionsElements.push( optionElement );
+		} );
+
+		return optionsElements;
 	},
 
 	toggleGraphVisibility: function () {
@@ -69,6 +196,10 @@ liquipedia.rankingTable = {
 				chartInstance = echarts.init( chart );
 			}
 		} );
+	},
+
+	standardizeDateFormat: function ( date ) {
+		return date.toISOString().slice( 0, 10 );
 	}
 };
 
