cac2fc6f810afed1b404ddd2235a3375099e8dfb
Rikard Blixt <rikardblixt@gmail.com>
2025-02-21 14:03:10 +0100

    linter

--- spec/league_icon_spec.lua
+++ spec/league_icon_spec.lua
@@ -1,47 +1,58 @@
 --- Triple Comment to Enable our LLS Plugin
 local LeagueIcon = require('Module:LeagueIcon')
 
+local FILLER_EXPECT = '<span class="league-icon-small-image">[[File:Logo filler event.png|link=]]</span>'
+local ICON_DARK_EXPECT = '<span class="league-icon-small-image">[[File:DarkIcon.png|link=||50x50px]]</span>'
+
+local ICON_BOTH_EXPECT =
+	'<span class="league-icon-small-image lightmode">[[File:LightIcon.png|link=||50x50px]]</span>' ..
+	'<span class="league-icon-small-image darkmode">[[File:DarkIcon.png|link=||50x50px]]</span>'
+local ICON_WITH_LINK_EXPECT = '<span class="league-icon-small-image">[[File:Icon.png|link=link|name|50x50px]]</span>'
+local ICON_WITH_LINK_BOTH_EXPECT =
+	'<span class="league-icon-small-image lightmode">[[File:LightIcon.png|link=link|name|50x50px]]</span>' ..
+	'<span class="league-icon-small-image darkmode">[[File:DarkIcon.png|link=link|name|50x50px]]</span>'
+
 insulate('LeagueIcon.display', function()
 	it('should return filler icon when no icons are provided', function()
 		local args = {}
 		local result = LeagueIcon.display(args)
-		assert.are.equal('<span class="league-icon-small-image">[[File:Logo filler event.png|link=]]</span>', result)
+		assert.are.equal(FILLER_EXPECT, result)
 	end)
 
 	it('should return iconDark when only iconDark is provided', function()
 		local args = {iconDark = 'DarkIcon.png'}
 		local result = LeagueIcon.display(args)
-		assert.are.equal('<span class="league-icon-small-image">[[File:DarkIcon.png|link=||50x50px]]</span>[[Category:Pages with only icondark]]', result)
+		assert.are.equal(ICON_DARK_EXPECT .. '[[Category:Pages with only icondark]]', result)
 	end)
 
 	it('should return both icons when both icon and iconDark are provided', function()
 		local args = {icon = 'LightIcon.png', iconDark = 'DarkIcon.png'}
 		local result = LeagueIcon.display(args)
-		assert.are.equal('<span class="league-icon-small-image lightmode">[[File:LightIcon.png|link=||50x50px]]</span><span class="league-icon-small-image darkmode">[[File:DarkIcon.png|link=||50x50px]]</span>', result)
+		assert.are.equal(ICON_BOTH_EXPECT, result)
 	end)
 
 	it('should not use template when noTemplate option is true', function()
 		local args = {options = {noTemplate = true}}
 		local result = LeagueIcon.display(args)
-		assert.are.equal('<span class="league-icon-small-image">[[File:Logo filler event.png|link=]]</span>', result)
+		assert.are.equal(FILLER_EXPECT, result)
 	end)
 
 	it('should not include link when noLink option is true', function()
 		local args = {icon = 'LightIcon.png', iconDark = 'DarkIcon.png', options = {noLink = true}}
 		local result = LeagueIcon.display(args)
-		assert.are.equal('<span class="league-icon-small-image lightmode">[[File:LightIcon.png|link=||50x50px]]</span><span class="league-icon-small-image darkmode">[[File:DarkIcon.png|link=||50x50px]]</span>', result)
+		assert.are.equal(ICON_BOTH_EXPECT, result)
 	end)
 end)
 
 insulate('LeagueIcon._make', function()
 	it('should return single icon when icon and iconDark are identical', function()
 		local result = LeagueIcon._make('Icon.png', 'Icon.png', 'link', 'name', 50)
-		assert.are.equal('<span class="league-icon-small-image">[[File:Icon.png|link=link|name|50x50px]]</span>', result)
+		assert.are.equal(ICON_WITH_LINK_EXPECT, result)
 	end)
 
 	it('should return both icons when icon and iconDark are different', function()
 		local result = LeagueIcon._make('LightIcon.png', 'DarkIcon.png', 'link', 'name', 50)
-		assert.are.equal('<span class="league-icon-small-image lightmode">[[File:LightIcon.png|link=link|name|50x50px]]</span><span class="league-icon-small-image darkmode">[[File:DarkIcon.png|link=link|name|50x50px]]</span>', result)
+		assert.are.equal(ICON_WITH_LINK_BOTH_EXPECT, result)
 	end)
 end)
 
@@ -55,8 +66,10 @@ insulate('LeagueIcon.getIconFromTemplate', function()
 
 	it('should extract icons from template string', function()
 		local templateString = 'File:LightIcon.pngFile:DarkIcon.png'
-		local icon, iconDark, trackingCategory = LeagueIcon.getIconFromTemplate({stringOfExpandedTemplate = templateString})
-		--assert.are.equal('LightIcon.png', icon)
+		local icon, iconDark, trackingCategory = LeagueIcon.getIconFromTemplate({
+			stringOfExpandedTemplate = templateString
+		})
+		assert.are.equal('LightIcon.png', icon)
 		assert.are.equal('DarkIcon.png', iconDark)
 		assert.are.equal('', trackingCategory)
 	end)
@@ -71,7 +84,7 @@ insulate('LeagueIcon.getTemplate', function()
 	it('should expand template with series', function()
 		local frame = mw.getCurrentFrame()
 		spy.on(frame, 'expandTemplate')
-		---@diagnostic disable-next-line: duplicate-set-field
+		--- @diagnostic disable-next-line: duplicate-set-field
 		frame.expandTemplate = function() return 'ExpandedTemplate' end
 		local result = LeagueIcon.getTemplate({series = 'SomeSeries'})
 		assert.are.equal('ExpandedTemplate', result)
@@ -80,7 +93,7 @@ insulate('LeagueIcon.getTemplate', function()
 	it('should expand template with abbreviation when series is not found', function()
 		local frame = mw.getCurrentFrame()
 		spy.on(frame, 'expandTemplate')
-		---@diagnostic disable-next-line: duplicate-set-field
+		--- @diagnostic disable-next-line: duplicate-set-field
 		frame.expandTemplate = function(_, template)
 			if template.args[1] == 'Series' then
 				return 'false'
