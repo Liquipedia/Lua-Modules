8b4daac48fa0ab94f2b3e2e80cc7ad20741e5e7d
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2025-02-26 21:08:40 +0900

    kick unused/unnecessary injector

--- components/infobox/wikis/leagueoflegends/infobox_patch_custom.lua
+++ components/infobox/wikis/leagueoflegends/infobox_patch_custom.lua
@@ -11,33 +11,16 @@ local Logic = require('Module:Logic')
 local Lua = require('Module:Lua')
 
 local Patch = Lua.import('Module:Infobox/Patch')
-local Injector = Lua.import('Module:Widget/Injector')
-local Widgets = require('Module:Widget/All')
 
 ---@class LoLPatchInfobox: PatchInfobox
 local CustomPatch = Class.new(Patch)
-local CustomInjector = Class.new(Injector)
 
 ---@param frame Frame
 ---@return Html
 function CustomPatch.run(frame)
 	local patch = CustomPatch(frame)
-	patch:setWidgetInjector(CustomInjector(patch))
 
 	return patch:createInfobox()
-end
-
----@param id string
----@param widgets Widget[]
----@return Widget[]
-function CustomInjector:parse(id, widgets)
-	local args = self.caller.args
-	if id == 'release' then
-		return {
-			Widgets.Cell{name = 'Release Date', content = { args.release }},
-		}
-	end
-	return widgets
 end
 
 ---Adjust Lpdb data
@@ -46,7 +29,6 @@ end
 ---@return table
 function CustomPatch:addToLpdb(lpdbData, args)
 	lpdbData.information = Logic.emptyOr(args.patch, lpdbData.information)
-
 	return lpdbData
 end
 
