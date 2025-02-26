c5799b4e04e7336ce08f26a27e4c1eb21636dea2
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2025-02-26 13:55:57 +0900

    add LoL patch infobox

---
+++ components/infobox/wikis/leagueoflegends/infobox_patch_custom.lua
@@ -0,0 +1,59 @@
+---
+-- @Liquipedia
+-- wiki=leagueoflegends
+-- page=Module:Infobox/Patch/Custom
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Class = require('Module:Class')
+local Lua = require('Module:Lua')
+
+local Patch = Lua.import('Module:Infobox/Patch')
+local Injector = Lua.import('Module:Widget/Injector')
+local Widgets = require('Module:Widget/All')
+
+---@class LoLPatchInfobox: PatchInfobox
+local CustomPatch = Class.new(Patch)
+local CustomInjector = Class.new(Injector)
+
+---@param frame Frame
+---@return Html
+function CustomPatch.run(frame)
+	local patch = CustomPatch(frame)
+	patch.args.version = patch.args.version or patch.args.patch
+	patch.args.release = patch.args.release or patch.args.releasedate or patch.args['release date']
+	patch:setWidgetInjector(CustomInjector(patch))
+
+	patch.getChronologyData = CustomPatch.getChronologyData
+
+	return patch:createInfobox()
+end
+
+---@param id string
+---@param widgets Widget[]
+---@return Widget[]
+function CustomInjector:parse(id, widgets)
+	local args = self.caller.args
+	if id == 'release' then
+		return {
+			Widgets.Cell{name = 'Release Date', content = { args.release }},
+		}
+	end
+	return widgets
+end
+
+---@param args table
+---@return {previous: string?, next: string?}
+function CustomPatch:getChronologyData(args)
+	local data = {}
+	if args.previous then
+		data.previous = 'Patch ' .. args.previous .. '|' .. args.previous
+	end
+	if args.next then
+		data.next = 'Patch ' .. args.next .. '|' .. args.next
+	end
+	return data
+end
+
+return CustomPatch
