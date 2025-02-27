31115ca173082c9ad30d8acdb2be16fe96b6e8f8
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2025-02-27 16:46:14 +0900

    add valorant character infobox

---
+++ components/infobox/wikis/valorant/infobox_character_custom.lua
@@ -0,0 +1,69 @@
+---
+-- @Liquipedia
+-- wiki=valorant
+-- page=Module:Infobox/Character/Custom
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Array = require('Module:Array')
+local Class = require('Module:Class')
+local Lua = require('Module:Lua')
+local Namespace = require('Module:Namespace')
+
+local Injector = Lua.import('Module:Widget/Injector')
+local Character = Lua.import('Module:Infobox/Character')
+
+local Widgets = Lua.import('Module:Widget/All')
+local Builder = Widgets.Builder
+local Cell = Widgets.Cell
+local Title = Widgets.Title
+
+---@class ValorantHeroInfobox: CharacterInfobox
+local CustomCharacter = Class.new(Character)
+local CustomInjector = Class.new(Injector)
+
+---@param frame Frame
+---@return Html
+function CustomCharacter.run(frame)
+	local character = CustomCharacter(frame)
+	character:setWidgetInjector(CustomInjector(character))
+	character.args.informationType = 'Agent'
+	return character:createInfobox()
+end
+
+---@param id string
+---@param widgets Widget[]
+---@return Widget[]
+function CustomInjector:parse(id, widgets)
+	local args = self.caller.args
+	if id == 'custom' then
+		Array.appendWith(
+			widgets,
+			Title{children = 'Abilities'},
+			Builder{
+				builder = function()
+					local abilities = self:getAllArgsForBase(args, 'ability')
+					return {
+						Cell{
+							name = #abilities > 1 and 'Abilities' or 'Ability',
+							content = abilities,
+						}
+					}
+				end
+			},
+			Cell{name = 'Signature Ability', content = {args.signature}},
+			Cell{name = 'Ultimate', content = {args.ultimate}}
+		)
+	end
+
+	return widgets
+end
+
+---@param args table
+---@return string[]
+function CustomCharacter:getWikiCategories(args)
+	return Namespace.isMain() and { 'Agents' } or {}
+end
+
+return CustomCharacter
