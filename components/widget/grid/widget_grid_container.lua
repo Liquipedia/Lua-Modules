38f13786d94cee84423f007eb8a4a42b86c53a1e
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2025-03-01 16:00:52 +0900

    Add grid container

---
+++ components/widget/grid/widget_grid_container.lua
@@ -0,0 +1,35 @@
+---
+-- @Liquipedia
+-- wiki=commons
+-- page=Module:Widget/Grid/Container
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Class = require('Module:Class')
+local Logic = require('Module:Logic')
+local Lua = require('Module:Lua')
+
+local Widget = Lua.import('Module:Widget')
+local HtmlWidgets = Lua.import('Module:Widget/Html/All')
+
+---@class GridContainer: Widget
+---@operator call(table): Panel
+---@field props table<string, any>
+local GridContainer = Class.new(Widget)
+
+---@return Widget
+function GridContainer:render()
+	return HtmlWidgets.Div{
+		classes = { 'lp-container-fluid' },
+		children = {
+			HtmlWidgets.Div{
+				classes = { Logic.readBool(self.props.center) and 'lp-row-center' or 'lp-row' },
+				css = { ['row-gap'] = self.props.rowGap or '0px' },
+				children = self.props.gridCells
+			}
+		}
+	}
+end
+
+return GridContainer
