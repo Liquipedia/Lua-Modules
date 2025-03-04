c211fa5b073e7aa8e9e91b01891749af92fbac7b
Hesketh2 <88981446+Hesketh2@users.noreply.github.com>
2025-03-04 21:30:16 +0700

    feat(Infobox): add Wildcard Character

---
+++ components/infobox/wikis/wildcard/infobox_character_custom.lua
@@ -0,0 +1,87 @@
+---
+-- @Liquipedia
+-- wiki=wildcard
+-- page=Module:Infobox/Character/Custom
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Array = require('Module:Array')
+local Class = require('Module:Class')
+local Logic = require('Module:Logic')
+
+local Lua = require('Module:Lua')
+
+local Injector = Lua.import('Module:Widget/Injector')
+local Character = Lua.import('Module:Infobox/Character')
+
+local Widgets = require('Module:Widget/All')
+local Cell = Widgets.Cell
+local Title = Widgets.Title
+
+---@class WildcardHeroInfobox: CharacterInfobox
+local CustomHero = Class.new(Character)
+local CustomInjector = Class.new(Injector)
+
+---@param frame Frame
+---@return Html
+function CustomHero.run(frame)
+	local character = CustomHero(frame)
+	character:setWidgetInjector(CustomInjector(character))
+	character.args.informationType = 'Champion'
+
+	return character:createInfobox()
+end
+
+---@param id string
+---@param widgets Widget[]
+---@return Widget[]
+function CustomInjector:parse(id, widgets)
+	local args = self.caller.args
+	if id == 'custom' then
+		Array.appendWith(
+			widgets,
+			Title{children = 'Basic Attributes'},
+			Cell{name = 'Health', content = {args.health}},
+			Cell{name = 'Damage per second', content = {args.dps}},
+			Cell{name = 'Move Speed', content = {args.movespeed}},
+			Cell{name = 'House', content = {args.house}}
+		)
+
+		Array.appendWith(
+			widgets,
+			Title{children = 'Basic Stats'},
+			Cell{name = 'Healing', content = {args.healing}},
+			Cell{name = 'Mobility', content = {args.mobility}},
+			Cell{name = 'Offense', content = {args.offense}},
+			Cell{name = 'Defense', content = {args.defense}},
+			Cell{name = 'Utility', content = {args.utility}}
+		)
+		return widgets
+	end
+
+	return widgets
+end
+
+---@param lpdbData table
+---@param args table
+function CustomHero:addToLpdb(lpdbData, args)
+	lpdbData.information = args.name
+	lpdbData.image = args.image
+	lpdbData.date = args.release
+	lpdbData.extradata = {
+		name = args.name,
+		health = args.health,
+		damagepersecond = args.dps,
+		movespeed = args.movespeed,
+		healing = args.healing,
+		mobility = args.mobility,
+		offense = args.offense,
+		defense = args.defense,
+		utility = args.utility
+	}
+
+	return lpdbData
+end
+
+return CustomHero
