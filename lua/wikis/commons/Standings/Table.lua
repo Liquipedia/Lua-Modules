432354d730298988f43596ce99d9fa997c413198
Rikard Blixt <rikardblixt@gmail.com>
2025-03-04 14:31:12 +0100

    lint

--- lua/wikis/commons/Standings/Table.lua
+++ lua/wikis/commons/Standings/Table.lua
@@ -32,7 +32,8 @@ local StandingsTable = {}
 ---@field match {w: integer, d: integer, l: integer}
 
 ---@class StandingTableOpponentData
----@field rounds {tiebreakerPoints: number?, specialstatus: string, scoreboard: Scoreboard?, matchId: string?, matches: MatchGroupUtilMatch[]}[]?
+---@field rounds {tiebreakerPoints: number?, specialstatus: string, scoreboard: Scoreboard?,
+---matchId: string?, matches: MatchGroupUtilMatch[]}[]?
 ---@field opponent standardOpponent
 ---@field startingPoints number?
 
