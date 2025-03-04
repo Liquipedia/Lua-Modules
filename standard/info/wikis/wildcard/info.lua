c17443717328bb77c5e28267a49b682aed9dd6ba
Hesketh2 <88981446+Hesketh2@users.noreply.github.com>
2025-03-04 21:18:05 +0700

    feat: add Wildcard info (#5542)
    feat(info): add Wildcard

---
+++ standard/info/wikis/wildcard/info.lua
@@ -0,0 +1,41 @@
+---
+-- @Liquipedia
+-- wiki=wildcard
+-- page=Module:Info
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+return {
+	startYear = 2024,
+	wikiName = 'wildcard',
+	name = 'Wildcard',
+	defaultGame = 'Wildcard',
+	games = {
+		wildcard = {
+			abbreviation = 'Wildcard',
+			name = 'Wildcard',
+			link = 'Wildcard',
+			logo = {
+				darkMode = 'Wildcard full darkmode.svg',
+				lightMode = 'Wildcard full lightmode.svg',
+			},
+			defaultTeamLogo = {
+				darkMode = 'Wildcard full darkmode.svg',
+				lightMode = 'Wildcard full lightmode.svg',
+			},
+		},
+	},
+	config = {
+		squads = {
+			hasPosition = false,
+			hasSpecialTeam = false,
+			allowManual = false,
+		},
+		match2 = {
+			status = 0,
+			matchWidth = 180,
+		},
+	},
+	defaultRoundPrecision = 0,
+}
