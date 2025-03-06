2ddb26141e11748b7591c3018c21a6d2c47de2f1
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2025-03-06 11:42:54 +0900

    add birthday list
    needed by PUBG

--- components/widget/main_page/widget_main_page_this_day_content.lua
+++ components/widget/main_page/widget_main_page_this_day_content.lua
@@ -8,15 +8,18 @@
 
 local Class = require('Module:Class')
 local Lua = require('Module:Lua')
+local Page = require('Module:Page')
+local String = require('Module:StringUtils')
 
 local Widget = Lua.import('Module:Widget')
 local HtmlWidgets = Lua.import('Module:Widget/Html/All')
 local Div = HtmlWidgets.Div
 local Link = Lua.import('Module:Widget/Basic/Link')
 local Small = HtmlWidgets.Small
+local WidgetUtil = Lua.import('Module:Widget/Util')
 
 ---@class ThisDayContent: Widget
----@field props { month: integer?, day: integer? }
+---@field props { month: integer?, day: integer?, birthdayListPage: string? }
 ---@operator call(table): ThisDayContent
 local ThisDayContent = Class.new(Widget)
 ThisDayContent.defaultProps = {
@@ -28,13 +31,26 @@ function ThisDayContent:render()
 	local month = self.props.month
 	local day = self.props.day
 	local frame = mw.getCurrentFrame()
-	return {
+	local birthdayListPage = self.props.birthdayListPage
+	local showBirthdayList = String.isNotEmpty(birthdayListPage) and Page.exists(birthdayListPage)
+	return WidgetUtil.collect(
 		Div{
 			attributes = { id = 'this-day-facts' },
 			children = {
 				frame:expandTemplate{ title = 'Liquipedia:This day/' .. month .. '/' .. day }
 			}
 		},
+		showBirthdayList and HtmlWidgets.Fragment{
+			children = {
+				HtmlWidgets.Hr,
+				Small{
+					css = { ['font-style'] = 'italic' },
+					children = {
+						Link{ children = 'Click to see all birthdays', link = birthdayListPage }
+					}
+				}
+			}
+		} or nil,
 		Small{
 			attributes = { id = 'this-day-trivialink' },
 			children = {
@@ -45,7 +61,7 @@ function ThisDayContent:render()
 				}
 			}
 		}
-	}
+	)
 end
 
 return ThisDayContent
