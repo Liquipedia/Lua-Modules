2bb1906673b072333af0e46c22aa12800ba9ed82
SzakaliOrs <szakali_ors@yahoo.com>
2025-03-06 15:30:53 +0200

    suggested change

--- components/notability/wikis/osu/notability_checker_config.lua
+++ components/notability/wikis/osu/notability_checker_config.lua
@@ -298,15 +298,15 @@ end
 -- Adjusts the score to compensate for the mode, you might
 -- want to decrease the points given for a certain mode
 function Config.adjustScoreForMode(score, mode)
-	if string.lower(mode) == "taiko" then
+	if string.lower(mode or '') == "taiko" then
         return score * 1.2
     end
 
-	if string.lower(mode) == "catch" then
+	if string.lower(mode or '') == "catch" then
         return score * 1.2
     end
 
-	if string.lower(mode) == "mania 7k" then
+	if string.lower(mode or '') == "mania 7k" then
         return score * 1.2
     end
 
