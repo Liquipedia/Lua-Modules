f4887d0c4c6a9d0aa78528c8d2241e89c178006a
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2025-03-06 16:36:23 +0900

    add missing param

--- components/infobox/commons/infobox_weapon.lua
+++ components/infobox/commons/infobox_weapon.lua
@@ -76,7 +76,9 @@ function Weapon:createInfobox()
 		Cell{name = 'Magazine Size', content = {args.magsize}},
 		Cell{name = 'Ammo Capacity', content = {args.ammocap}},
 		Cell{name = 'Reload Speed', content = {
-			Logic.isNotEmpty and (args.reloadspeed .. (args.reloadspeedunit and (' ' .. args.reloadspeedunit) or '')) or nil
+			Logic.isNotEmpty(args.reloadspeed) and (
+				args.reloadspeed .. (args.reloadspeedunit and (' ' .. args.reloadspeedunit) or '')
+			) or nil
 		}},
 		Customizable{
 			id = 'rateoffire',
