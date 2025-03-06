181a52d848c6d840942671d233d94c1076ff2e52
SlothyMan <76825878+SlothyMan@users.noreply.github.com>
2025-03-06 10:14:02 +0100

    Update components/infobox/wikis/warthunder/infobox_map_custom.lua

--- components/infobox/wikis/warthunder/infobox_map_custom.lua
+++ components/infobox/wikis/warthunder/infobox_map_custom.lua
@@ -6,7 +6,6 @@
 --
 local Array = require('Module:Array')
 local Class = require('Module:Class')
-local Image = require('Module:Image')
 local Logic = require('Module:Logic')
 local Lua = require('Module:Lua')
 local String = require('Module:StringUtils')
