84b0db5d347024c0243a675a4e5db248d7e68a8b
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2025-03-07 10:47:04 +0100

    spaces --> tabs

--- components/infobox/wikis/fortnite/infobox_team_custom.lua
+++ components/infobox/wikis/fortnite/infobox_team_custom.lua
@@ -42,112 +42,112 @@ local CustomInjector = Class.new(Injector)
 ---@param frame Frame
 ---@return Html
 function CustomTeam.run(frame)
-    local team = CustomTeam(frame)
-    team:setWidgetInjector(CustomInjector(team))
+	local team = CustomTeam(frame)
+	team:setWidgetInjector(CustomInjector(team))
 
-    team.args.achievements = TeamAchievements.achievements(frame)
+	team.args.achievements = TeamAchievements.achievements(frame)
 
-    return team:createInfobox()
+	return team:createInfobox()
 end
 
 ---@param id string
 ---@param widgets Widget[]
 ---@return Widget[]
 function CustomInjector:parse(id, widgets)
-    if id == 'earnings' then
-        local playerEarnings = self.caller.totalPlayerEarnings
-        table.insert(widgets, Cell{
-            name = PLAYER_EARNINGS_ABBREVIATION,
-            content = {playerEarnings ~= 0 and ('$' .. mw.getContentLanguage():formatNum(Math.round(playerEarnings))) or nil}
-        })
-    end
+	if id == 'earnings' then
+		local playerEarnings = self.caller.totalPlayerEarnings
+		table.insert(widgets, Cell{
+			name = PLAYER_EARNINGS_ABBREVIATION,
+			content = {playerEarnings ~= 0 and ('$' .. mw.getContentLanguage():formatNum(Math.round(playerEarnings))) or nil}
+		})
+	end
 
-    return widgets
+	return widgets
 end
 
 ---@return number
 ---@return table<integer, number>
 function CustomTeam:calculateEarnings()
-    self.totalPlayerEarnings = 0
+	self.totalPlayerEarnings = 0
 
-    if not Namespace.isMain() then
-        return 0, {}
-    end
+	if not Namespace.isMain() then
+		return 0, {}
+	end
 
-    local team = self.pagename
-    local query = 'individualprizemoney, prizemoney, opponentplayers, opponenttype, date, mode'
+	local team = self.pagename
+	local query = 'individualprizemoney, prizemoney, opponentplayers, opponenttype, date, mode'
 
-    local playerTeamConditions = ConditionTree(BooleanOperator.any)
-    for playerIndex = 1, MAXIMUM_NUMBER_OF_PLAYERS_IN_PLACEMENTS do
-        playerTeamConditions:add{
-            ConditionNode(ColumnName('players_p' .. playerIndex .. 'team'), Comparator.eq, team),
-        }
-    end
+	local playerTeamConditions = ConditionTree(BooleanOperator.any)
+	for playerIndex = 1, MAXIMUM_NUMBER_OF_PLAYERS_IN_PLACEMENTS do
+		playerTeamConditions:add{
+			ConditionNode(ColumnName('players_p' .. playerIndex .. 'team'), Comparator.eq, team),
+		}
+	end
 
-    local conditions = ConditionTree(BooleanOperator.all):add{
-        ConditionNode(ColumnName('date'), Comparator.neq, DateExt.defaultDateTime),
-        ConditionNode(ColumnName('prizemoney'), Comparator.gt, '0'),
-        ConditionTree(BooleanOperator.any):add{
-            ConditionNode(ColumnName('participantlink'), Comparator.eq, team),
-            ConditionTree(BooleanOperator.all):add{
-                ConditionNode(ColumnName('mode'), Comparator.neq, 'team'),
-                playerTeamConditions
-            },
-        },
-    }
+	local conditions = ConditionTree(BooleanOperator.all):add{
+		ConditionNode(ColumnName('date'), Comparator.neq, DateExt.defaultDateTime),
+		ConditionNode(ColumnName('prizemoney'), Comparator.gt, '0'),
+		ConditionTree(BooleanOperator.any):add{
+			ConditionNode(ColumnName('participantlink'), Comparator.eq, team),
+			ConditionTree(BooleanOperator.all):add{
+				ConditionNode(ColumnName('mode'), Comparator.neq, 'team'),
+				playerTeamConditions
+			},
+		},
+	}
 
-    local queryParameters = {
-        conditions = conditions:toString(),
-        query = query,
-    }
+	local queryParameters = {
+		conditions = conditions:toString(),
+		query = query,
+	}
 
-    local earnings = {total = 0}
+	local earnings = {total = 0}
 
-    local processPlacement = function(placement)
-        self:_addPlacementToEarnings(earnings, placement)
-    end
+	local processPlacement = function(placement)
+		self:_addPlacementToEarnings(earnings, placement)
+	end
 
-    Lpdb.executeMassQuery('placement', queryParameters, processPlacement)
+	Lpdb.executeMassQuery('placement', queryParameters, processPlacement)
 
-    if Namespace.isMain() then
-        mw.ext.LiquipediaDB.lpdb_datapoint('total_earnings_players_while_on_team_' .. team, {
-                type = 'total_earnings_players_while_on_team',
-                name = self.pagename,
-                information = self.totalPlayerEarnings,
-        })
-    end
+	if Namespace.isMain() then
+		mw.ext.LiquipediaDB.lpdb_datapoint('total_earnings_players_while_on_team_' .. team, {
+				type = 'total_earnings_players_while_on_team',
+				name = self.pagename,
+				information = self.totalPlayerEarnings,
+		})
+	end
 
-    local totalEarnings = Math.round(Table.extract(earnings, 'total'))
+	local totalEarnings = Math.round(Table.extract(earnings, 'total'))
 
-    return totalEarnings, earnings
+	return totalEarnings, earnings
 end
 
 ---@param earnings table
 ---@param data placement
 function CustomTeam:_addPlacementToEarnings(earnings, data)
-    local prizeMoney = data.prizemoney
+	local prizeMoney = data.prizemoney
 
-    if data.opponenttype ~= Opponent.team then
-        prizeMoney = data.individualprizemoney * self:_amountOfTeamPlayersInPlacement(data.opponentplayers)
-        self.totalPlayerEarnings = self.totalPlayerEarnings + prizeMoney
-    end
+	if data.opponenttype ~= Opponent.team then
+		prizeMoney = data.individualprizemoney * self:_amountOfTeamPlayersInPlacement(data.opponentplayers)
+		self.totalPlayerEarnings = self.totalPlayerEarnings + prizeMoney
+	end
 
-    local date = tonumber(string.sub(data.date, 1, 4)) --[[@as integer]]
-    earnings[date] = (earnings[date] or 0) + prizeMoney
-    earnings.total = earnings.total + prizeMoney
+	local date = tonumber(string.sub(data.date, 1, 4)) --[[@as integer]]
+	earnings[date] = (earnings[date] or 0) + prizeMoney
+	earnings.total = earnings.total + prizeMoney
 end
 
 ---@param players table
 ---@return integer
 function CustomTeam:_amountOfTeamPlayersInPlacement(players)
-    local amount = 0
-    for playerKey in Table.iter.pairsByPrefix(players, 'p') do
-        if players[playerKey .. 'team'] == self.pagename then
-            amount = amount + 1
-        end
-    end
+	local amount = 0
+	for playerKey in Table.iter.pairsByPrefix(players, 'p') do
+		if players[playerKey .. 'team'] == self.pagename then
+			amount = amount + 1
+		end
+	end
 
-    return amount
+	return amount
 end
 
 return CustomTeam
