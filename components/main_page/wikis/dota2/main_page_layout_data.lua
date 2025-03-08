55f4ae3d640515c3266360934cc2d743bee96fc7
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2025-03-08 11:42:19 +0900

    formatting

--- components/main_page/wikis/dota2/main_page_layout_data.lua
+++ components/main_page/wikis/dota2/main_page_layout_data.lua
@@ -25,8 +25,9 @@ local WidgetUtil = Lua.import('Module:Widget/Util')
 
 local ABOUT_HEADING = 'About Liquipedia\'s Dota 2 Wiki'
 local ABOUT_BODY = 'We are the largest Dota 2 wiki that anyone can edit, maintained by fans just like you. ' ..
-			'This wiki currently covers esports and game content, containing over ' .. mw.site.stats.articles ..
-			' articles managed by ' .. mw.site.stats.activeUsers .. ' active users.'
+			'This wiki currently covers esports and game content, containing over ' ..
+			mw.getContentLanguage():formatNum(mw.site.stats.articles) .. ' articles managed by ' ..
+			mw.site.stats.activeUsers .. ' active users.'
 local MAIN_PAGE_BUTTON = Button{
 	link = 'Main Page',
 	title = 'Click here to get to the main page',
