1f221ad52a4da905ec79867f1ab658320c18c922
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2025-03-10 11:48:34 +0900

    add LPDB datapoint for RL unofficial world champion

---
+++ components/infobox/wikis/rocketleague/infobox_unofficial_world_champion_custom.lua
@@ -0,0 +1,39 @@
+---
+-- @Liquipedia
+-- wiki=rocketleague
+-- page=Module:Infobox/UnofficialWorldChampion/Custom
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Class = require('Module:Class')
+local Json = require('Module:Json')
+local Lua = require('Module:Lua')
+local Namespace = require('Module:Namespace')
+
+local UnofficialWorldChampion = Lua.import('Module:Infobox/UnofficialWorldChampion')
+
+---@class RocketLeagueUnofficialWorldChampionInfobox: UnofficialWorldChampionInfobox
+local CustomUnofficialWorldChampion = Class.new(UnofficialWorldChampion)
+
+---@param frame Frame
+---@return Html
+function CustomUnofficialWorldChampion.run(frame)
+	local unofficialWorldChampion = CustomUnofficialWorldChampion(frame)
+	unofficialWorldChampion:_setLpdbData()
+	return unofficialWorldChampion:createInfobox()
+end
+
+function CustomUnofficialWorldChampion:_setLpdbData()
+	if Namespace.isMain() then
+		mw.ext.LiquipediaDB.lpdb_datapoint(
+			'Unofficial World Champion',
+			Json.stringifySubTables({
+				type = 'Unofficial World Champion',
+				name = self.args['current champion']
+			})
+		)
+	end
+end
+
+return CustomUnofficialWorldChampion
