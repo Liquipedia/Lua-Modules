d8bc769db1471c3290a3e8e427e143c69fe3720b
mbergen <mail@mbergen.de>
2025-03-12 16:18:25 +0100

    feat: Add new mainpage on AoE

---
+++ lua/wikis/ageofempires/FilterButtons/Config.lua
@@ -0,0 +1,35 @@
+local Config = {}
+local Tier = require('Module:Tier/Utils')
+local Game = require('Module:Game')
+
+---@type FilterButtonCategory[]
+Config.categories = {
+	{
+		name = 'tier',
+		property = 'liquipediatier',
+		load = function(category)
+			category.items = {}
+			for _, tier in Tier.iterate('tiers') do
+				table.insert(category.items, tier.value)
+			end
+		end,
+		defaultItems = {'1', '2', '3'},
+		transform = function(tier)
+			return Tier.toName(tonumber(tier))
+		end,
+		expandKey = "game"
+	},
+	{
+		name = 'game',
+		property = 'game',
+		expandable = true,
+		load = function(category)
+			category.items = Game.listGames({ordered = true})
+		end,
+		transform = function(game)
+			return Game.icon({game = game, noSpan = true, noLink = true, size = '20x20px'})
+		end
+	}
+}
+
+return Config
---
+++ lua/wikis/ageofempires/MainPageLayout/data.lua
@@ -0,0 +1,218 @@
+---
+-- @Liquipedia
+-- wiki=rainbowsix
+-- page=Module:MainPageLayout/data
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Lua = require('Module:Lua')
+
+local FilterButtonsWidget = Lua.import('Module:Widget/FilterButtons')
+local MatchTickerContainer = Lua.import('Module:Widget/Match/Ticker/Container')
+local TournamentsTicker = Lua.import('Module:Widget/Tournaments/Ticker')
+
+local HtmlWidgets = Lua.import('Module:Widget/Html/All')
+local Div = HtmlWidgets.Div
+local Link = Lua.import('Module:Widget/Basic/Link')
+local ThisDayWidgets = Lua.import('Module:Widget/MainPage/ThisDay')
+local TransfersList = Lua.import('Module:Widget/MainPage/TransfersList')
+local WidgetUtil = Lua.import('Module:Widget/Util')
+
+local CONTENT = {
+	usefulArticles = {
+		heading = 'Useful Articles',
+		body = '{{Liquipedia:Useful Articles}}',
+		padding = true,
+		boxid = 1503,
+	},
+	wantToHelp = {
+		heading = 'Want To Help?',
+		body = '{{Liquipedia:Want_to_help}}',
+		padding = true,
+		boxid = 1504,
+	},
+	transfers = {
+		heading = 'Transfers',
+		body = TransfersList{limit = 10},
+		boxid = 1509,
+	},
+	thisDay = {
+		heading = ThisDayWidgets.Title(),
+		body = ThisDayWidgets.Content{
+			birthdayListPage = 'Birthday list'
+		},
+		padding = true,
+		boxid = 1510,
+	},
+	specialEvents = {
+		noPanel = true,
+		body = '{{Liquipedia:Special Event}}',
+	},
+	filterButtons = {
+		noPanel = true,
+		body = Div{
+			css = { width = '100%', ['margin-bottom'] = '8px' },
+			children = { FilterButtonsWidget() }
+		}
+	},
+	matches = {
+		heading = 'Matches',
+		body = WidgetUtil.collect(
+			MatchTickerContainer{},
+			Div{
+				css = {
+					['white-space'] = 'nowrap',
+					display = 'block',
+					margin = '0 10px',
+					['font-size'] = '15px',
+					['font-style'] = 'italic',
+					['text-align'] = 'center',
+				},
+				children = { Link{ children = 'See more matches', link = 'Liquipedia:Matches'} }
+			}
+		),
+		padding = true,
+		boxid = 1507,
+		panelAttributes = {
+			['data-switch-group-container'] = 'countdown',
+		},
+	},
+	tournaments = {
+		heading = 'Tournaments',
+		body = TournamentsTicker{
+			upcomingDays = 30,
+			completedDays = 20
+		},
+		padding = true,
+		boxid = 1508,
+	},
+}
+
+return {
+	banner = {
+		lightmode = 'Age of Empires default allmode.png',
+		darkmode = 'Age of Empires default allmode.png',
+	},
+	metadesc = 'The Age of Empires esports wiki covering everything from tournaments, maps,' ..
+        ' to competitive players, teams and transfers.',
+	title = 'Age of Empires',
+	navigation = {
+        {
+            file = 'Hera Dreamhack Hannover 2022.jpg',
+            title = 'Players',
+            link = 'Portal:Players',
+            count = {
+                method = 'LPDB',
+                table = 'player',
+            },
+        },
+		{
+			file = 'Aussie Drongo RBWL 2022.jpg',
+			title = 'Teams',
+			link = 'Portal:Teams',
+			count = {
+				method = 'LPDB',
+				table = 'team',
+			},
+		},
+		{
+			file = 'Artosis Nili Dave Dreamhack Hannover 2022.jpg',
+			title = 'Tournaments',
+			link = 'Portal:Tournaments',
+			count = {
+				method = 'LPDB',
+				table = 'tournament',
+			},
+		},
+		{
+			file = 'Villese RBWL.jpg',
+			title = 'Transfers',
+			link = 'Portal:Transfers',
+			count = {
+				method = 'LPDB',
+				table = 'transfer',
+			},
+		},
+		{
+			file = 'Aussie Drongo RBWL 2022.jpg',
+			title = 'Recent Results',
+			link = 'Recent Results',
+		},
+		{
+			file = 'Age of Empires 2 DE The Mountain Royals key art.jpg',
+			title = 'Statistics',
+			link = 'Portal:Statistics',
+		},
+	},
+	layouts = {
+		main = {
+			{ -- Left
+				size = 5,
+				children = {
+					{
+						mobileOrder = 1,
+						content = CONTENT.specialEvents,
+					},
+					{
+						mobileOrder = 3,
+						content = CONTENT.transfers,
+					},
+                    {
+						mobileOrder = 4,
+						content = CONTENT.thisDay,
+					},
+					{
+						mobileOrder = 5,
+						content = CONTENT.wantToHelp,
+					},
+				}
+			},
+			{ -- Right
+				size = 7,
+				children = {
+					{
+						mobileOrder = 2,
+						children = {
+							{
+								children = {
+									{
+										noPanel = true,
+										content = CONTENT.filterButtons,
+									},
+								},
+							},
+							{
+								size = 6,
+								children = {
+									{
+										noPanel = true,
+										content = CONTENT.matches,
+									},
+								},
+							},
+							{
+								size = 6,
+								children = {
+									{
+										noPanel = true,
+										content = CONTENT.tournaments,
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+            {
+                size = 12,
+                children = {
+                    {
+                        mobileOrder = 5,
+                        content = CONTENT.usefulArticles,
+                    }
+                },
+            }
+		},
+	},
+}
