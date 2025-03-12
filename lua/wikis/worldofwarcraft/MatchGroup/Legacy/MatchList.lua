259a2627125352e07d7cd42ae43bdae2677c3500
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2025-03-12 17:00:53 +0100

    Update lua/wikis/worldofwarcraft/MatchGroup/Legacy/MatchList.lua

--- lua/wikis/worldofwarcraft/MatchGroup/Legacy/MatchList.lua
+++ lua/wikis/worldofwarcraft/MatchGroup/Legacy/MatchList.lua
@@ -1,6 +1,6 @@
 ---
 -- @Liquipedia
--- wiki=squadrons
+-- wiki=worldofwarcraft
 -- page=Module:MatchGroup/Legacy/MatchList
 --
 -- Please see https://github.com/Liquipedia/Lua-Modules to contribute
