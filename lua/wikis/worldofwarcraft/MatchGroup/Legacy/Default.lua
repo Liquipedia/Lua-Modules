5f6da057b7f1d34783f7ede8d54ddca605c54192
hjpalpha <andreasbuesch1@gmail.com>
2025-03-12 14:59:04 +0100

    bracket wrapper

---
+++ lua/wikis/worldofwarcraft/MatchGroup/Legacy/Default.lua
@@ -0,0 +1,31 @@
+---
+-- @Liquipedia
+-- wiki=worldofwarcraft
+-- page=Module:MatchGroup/Legacy/Default
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Class = require('Module:Class')
+local Lua = require('Module:Lua')
+
+local MatchGroupLegacy = Lua.import('Module:MatchGroup/Legacy')
+
+---@class WorldofwarcraftMatchGroupLegacyDefault: MatchGroupLegacy
+local MatchGroupLegacyDefault = Class.new(MatchGroupLegacy)
+
+---@return table
+function MatchGroupLegacyDefault:getMap()
+	return {
+		['$notEmpty$'] = 'match$1$',
+		['$parse$'] = 'match$1$'
+	}
+end
+
+---@param frame Frame
+---@return string
+function MatchGroupLegacyDefault.run(frame)
+	return MatchGroupLegacyDefault(frame):build()
+end
+
+return MatchGroupLegacyDefault
