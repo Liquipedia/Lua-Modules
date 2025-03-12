056ef6a00e847817186673857f5aa1a9a4a6872c
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2025-03-12 13:51:56 +0900

    make linter happy

--- lua/wikis/wildcard/MainPageLayout/data.lua
+++ lua/wikis/wildcard/MainPageLayout/data.lua
@@ -38,8 +38,8 @@ return {
 		lightmode = 'Wildcard full lightmode.svg',
 		darkmode = 'Wildcard full darkmode.svg',
 	},
-	metadesc = 'Comprehensive Wildcard wiki with articles covering everything from champions and summons, to strategies, ' ..
-	'to tournaments, to competitive players, and teams.',
+	metadesc = 'Comprehensive Wildcard wiki with articles covering everything from champions and summons, ' ..
+		'to strategies, to tournaments, to competitive players, and teams.',
 	title = 'The Wildcard Wiki',
 	navigation = {
 		{
