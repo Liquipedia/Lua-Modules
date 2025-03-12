19d10c0196bee2b41d132b42df901b80a2faefd3
hjpalpha <andreasbuesch1@gmail.com>
2025-03-12 09:47:05 +0100

    feat(match2): Artifact Bracket Legacy Wrapper

---
+++ lua/wikis/artifact/MatchGroup/Legacy/Default.lua
@@ -0,0 +1,51 @@
+---
+-- @Liquipedia
+-- wiki=artifact
+-- page=Module:MatchGroup/Legacy/Default
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+--python pwb.py replace -lang:artifact -transcludes:"BracketMatchSummary" -regex "\|\s*g(\d)score\s*=\s*(\d+)\-(\d+)" "|g\1p1score=\2|g\1p2score=\3"
+
+local Array = require('Module:Array')
+local Class = require('Module:Class')
+local Lua = require('Module:Lua')
+
+local MatchGroupLegacy = Lua.import('Module:MatchGroup/Legacy')
+
+local MAX_NUMBER_OF_OPPONENTS = 2
+local MAX_NUMBER_OF_CHARACTERS = 5
+
+---@class ArtifactMatchGroupLegacyDefault: MatchGroupLegacy
+local MatchGroupLegacyDefault = Class.new(MatchGroupLegacy)
+
+---@return table
+function MatchGroupLegacyDefault:getMap()
+	local map = {
+		['$notEmpty$'] = 'g$1$p1score',
+		map = 'g$1$',
+		winner = 'g$1$win',
+		vod = 'vodgame$1$',
+		score1 = 'g$1$p1score',
+		score2 = 'g$1$p2score',
+	}
+
+	Array.forEach(Array.range(1, MAX_NUMBER_OF_OPPONENTS), function (oppIndex)
+		local prefix = 'p' .. oppIndex
+		Array.forEach(Array.range(1, MAX_NUMBER_OF_CHARACTERS), function (charIndex)
+			map[prefix .. 'h' .. charIndex] = 'g$1$' .. prefix .. 'h' .. charIndex
+		end)
+		map[prefix .. 'd'] = prefix .. 'd$1$'
+	end)
+
+	return map
+end
+
+---@param frame Frame
+---@return string
+function MatchGroupLegacyDefault.run(frame)
+	return MatchGroupLegacyDefault(frame):build()
+end
+
+return MatchGroupLegacyDefault
