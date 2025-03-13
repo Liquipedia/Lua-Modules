7c3ef20506bd1fa32092503deeffa4bc473eb205
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2025-03-13 11:23:13 +0900

    finalize impl
    hopefully

--- lua/wikis/commons/Icon/Data.lua
+++ lua/wikis/commons/Icon/Data.lua
@@ -116,6 +116,10 @@ return {
 	-- Usage: Dota2 Main Page
 	hub = 'fas fa-external-link-alt',
 
+	-- Usage: Rainbow6 Infobox operator
+	yes = 'fa fa-check',
+	no = 'fa fa-times',
+
 	-- Usage: Stormgate
 	coop = 'fas fa-dungeon',
 	mayhem = 'fab fa-fort-awesome',
--- lua/wikis/rainbowsix/Infobox/Character/Custom.lua
+++ lua/wikis/rainbowsix/Infobox/Character/Custom.lua
@@ -6,7 +6,10 @@
 -- Please see https://github.com/Liquipedia/Lua-Modules to contribute
 --
 
+local Array = require('Module:Array')
 local Class = require('Module:Class')
+local FnUtil = require('Module:FnUtil')
+local Image = require('Module:Image')
 local Logic = require('Module:Logic')
 local Lua = require('Module:Lua')
 
@@ -20,8 +23,10 @@ local Character = Lua.import('Module:Infobox/Character')
 local Widgets = Lua.import('Module:Widget/All')
 local HtmlWidgets = Lua.import('Module:Widget/Html/All')
 local Cell = Widgets.Cell
+local IconFa = Lua.import('Module:Widget/Image/Icon/Fontawesome')
 local IconImageWidget = Lua.import('Module:Widget/Image/Icon/Image')
 local Link = Lua.import('Module:Widget/Basic/Link')
+local Title = Widgets.Title
 local WidgetUtil = Lua.import('Module:Widget/Util')
 
 local TEAM_ATTACK = HtmlWidgets.Fragment{
@@ -44,6 +49,53 @@ local TEAM_DEFENSE = HtmlWidgets.Fragment{
 		' Defense'
 	}
 }
+
+---@param label string
+---@return Widget
+local function createPriceLabel(label)
+	return HtmlWidgets.Sup{
+		children = {
+			HtmlWidgets.Span{
+				classes = { 'elm-bg' },
+				css = {
+					['font-family'] = 'monospace',
+					['font-weight'] = 'bold',
+					padding = '0 1px'
+				},
+				children = { label }
+			}
+		}
+	}
+end
+local STANDARD_EDITION_LABEL = createPriceLabel('SE')
+local SEASON_PASS_LABEL = createPriceLabel('SP')
+
+local OPERATOR_PRICES = {
+	launch = {
+		renown = 0,
+		credit = 300
+	},
+	['36m'] = {
+		renown = 10000,
+		credit = 240
+	},
+	['24m'] = {
+		renown = 15000,
+		credit = 360
+	},
+	['12m'] = {
+		renown = 20000,
+		credit = 480
+	},
+	['0m'] = {
+		renown = 25000,
+		credit = 600
+	}
+}
+OPERATOR_PRICES['36 months'] = OPERATOR_PRICES['36m']
+OPERATOR_PRICES['24 months'] = OPERATOR_PRICES['24m']
+OPERATOR_PRICES['12 months'] = OPERATOR_PRICES['12m']
+OPERATOR_PRICES['0 months'] = OPERATOR_PRICES['0m']
 
 ---@class RainbowsixHeroInfobox: CharacterInfobox
 local CustomCharacter = Class.new(Character)
@@ -117,7 +169,9 @@ function CustomInjector:parse(id, widgets)
 			}
 		}
 	elseif id == 'custom' then
-		--TODO
+		return WidgetUtil.collect(
+			self.caller:_getAdditionalInfo(args)
+		)
 	end
 
 	return widgets
@@ -167,6 +221,242 @@ function CustomCharacter:_buildTeamRainbowWidgets(teamName, allmode)
 		},
 		teamName
 	}
+end
+
+---@param args table<string, any>
+---@return Widget[]
+function CustomCharacter:_getAdditionalInfo(args)
+	return WidgetUtil.collect(
+		Logic.isNotEmpty(args.height) and Cell{
+			name = 'Height',
+			content = { args.height, 'm' },
+			options = { separator = ' ' }
+		} or nil,
+		Logic.isNotEmpty(args.weight) and Cell{
+			name = 'Height',
+			content = { args.weight, 'kg' },
+			options = { separator = ' ' }
+		} or nil,
+		Logic.isNotEmpty(args.voice) and Cell{
+			name = 'Voiced by',
+			content = { args.voice }
+		} or nil,
+		self:_getPriceCells(args.renownprice or args.creditprice),
+		Cell{
+			name = 'Has Elite Skin',
+			content = WidgetUtil.collect(
+				HtmlWidgets.Fragment{
+					children = Logic.readBool(args.eliteskin) and {
+						IconFa{ iconName = 'yes', color = 'forest-green-text' },
+						' ',
+						HtmlWidgets.I{
+							css = {
+								['padding-left'] = '2px',
+								['vertical-align'] = '-1px'
+							},
+							children = { 'Yes' }
+						}
+					} or {
+						IconFa{ iconName = 'no', color = 'cinnabar-text' },
+						' ',
+						HtmlWidgets.I{
+							css = {
+								['padding-left'] = '2px',
+								['vertical-align'] = '-1px'
+							},
+							children = { 'No' }
+						}
+					}
+				},
+				Logic.isNotEmpty(args['eliteskin-date']) and HtmlWidgets.I{
+					children = {
+						HtmlWidgets.Small{
+							children = { args['eliteskin-date'] }
+						}
+					}
+				} or nil
+			)
+		},
+		Cell{
+			name = 'Availability',
+			content = { args.availability }
+		}
+	)
+end
+
+---@param input string
+---@return Widget[]|nil
+function CustomCharacter:_getPriceCells(input)
+	local lowerInput = input:lower()
+	if (lowerInput == 'launch') then
+		local lang = mw.getContentLanguage()
+		return {
+			Cell{
+				name = 'Renown price',
+				content = WidgetUtil.collect(
+					'Free',
+					Array.map({ 500, 1000, 1500, 2000 }, function (renownPrice)
+						return HtmlWidgets.Fragment{
+							children = {
+								lang:formatNum(renownPrice),
+								' ',
+								STANDARD_EDITION_LABEL
+							}
+						}
+					end),
+					HtmlWidgets.Fragment{
+						children = {
+							'10% off ',
+							SEASON_PASS_LABEL
+						}
+					}
+				),
+			},
+			self:_getPriceCell('Renown', OPERATOR_PRICES.launch.credit)
+		}
+	elseif Logic.isNotEmpty(OPERATOR_PRICES[lowerInput]) then
+		local priceData = OPERATOR_PRICES[lowerInput]
+		return {
+			self:_getPriceCell('Renown', priceData.renown),
+			self:_getPriceCell('R6 Credit', priceData.credit)
+		}
+	end
+	return nil
+end
+
+---@param currency string
+---@param price integer
+---@return Widget
+function CustomCharacter:_getPriceCell(currency, price)
+	local lang = mw.getContentLanguage()
+	return Cell{
+		name = currency .. ' price',
+		content = {
+			lang:formatNum(price),
+			HtmlWidgets.Fragment{
+				children = {
+					lang:formatNum(price * 0.9),
+					' ',
+					SEASON_PASS_LABEL
+				}
+			}
+		},
+	}
+end
+
+---@param args table<string, any>
+---@return Widget[]
+function CustomCharacter:_getBaseStats(args)
+	return WidgetUtil.collect(
+		Title{ children = 'Base Stats' },
+		Cell{
+			name = 'Armor/Health',
+			content = self:_getArmorContent((args.armor or ''):lower()),
+			options = { separator = ' ' }
+		},
+		Cell{
+			name = 'Speed',
+			content = self:_getSpeedContent((args.speed or ''):lower()),
+			options = { separator = ' ' }
+		},
+		Cell{
+			name = 'Difficulty',
+			content = self:_getArmorContent((args.difficulty or ''):lower()),
+			options = { separator = ' ' }
+		}
+	)
+end
+
+---@param armor string
+---@return (string|Widget)[]
+function CustomCharacter:_getArmorContent(armor)
+	local armorContent = FnUtil.curry(CustomCharacter._getStatContent, 'armor')
+	local armorEq = FnUtil.curry(FnUtil.eq, armor)
+	if Array.any({'1', 'light', 'low'}, armorEq) then
+		return armorContent(1, 'Light (100 HP)')
+	elseif Array.any({'2', 'medium'}, armorEq) then
+		return armorContent(2, 'Medium (110 HP)')
+	elseif Array.any({'3', 'slow', 'high'}, armorEq) then
+		return armorContent(3, 'Heavy (125 HP)')
+	end
+	return {}
+end
+
+---@param speed string
+---@return (string|Widget)[]
+function CustomCharacter:_getSpeedContent(speed)
+	local speedContent = FnUtil.curry(CustomCharacter._getStatContent, 'speed')
+	local speedEq = FnUtil.curry(FnUtil.eq, speed)
+	if Array.any({'1', 'slow', 'low'}, speedEq) then
+		return speedContent(1, 'Light (100 HP)')
+	elseif Array.any({'2', 'medium'}, speedEq) then
+		return speedContent(2, 'Medium (110 HP)')
+	elseif Array.any({'3', 'fast', 'high'}, speedEq) then
+		return speedContent(3, 'Heavy (125 HP)')
+	end
+	return {}
+end
+
+---@param difficulty string
+---@return (string|Widget)[]
+function CustomCharacter:_getDifficultyContent(difficulty)
+	local difficultyContent = FnUtil.curry(CustomCharacter._getStatContent, 'armor')
+	local difficultyEq = FnUtil.curry(FnUtil.eq, difficulty)
+	if Array.any({'1', 'easy', 'low'}, difficultyEq) then
+		return difficultyContent(1, 'Easy')
+	elseif Array.any({'2', 'normal', 'medium'}, difficultyEq) then
+		return difficultyContent(2, 'Medium')
+	elseif Array.any({'3', 'hard', 'difficult'}, difficultyEq) then
+		return difficultyContent(3, 'Hard')
+	end
+	return {}
+end
+
+---@param type string
+---@param value string|integer
+---@param display string
+---@return (string|Widget)[]
+function CustomCharacter._getStatContent(type, value, display)
+	return {
+		Image.display('R6S operator-rating-' .. type .. '-' ..  value .. '.png', nil, { size = '40x20px', link = '' }),
+		HtmlWidgets.I{
+			css = {
+				['padding-left'] = '2px',
+				['vertical-align'] = '-1px'
+			},
+			children = { display }
+		}
+	}
+end
+
+---@param args table
+---@return string[]
+function CustomCharacter:getWikiCategories(args)
+	local categories = {}
+	local speedEq = FnUtil.curry(FnUtil.eq, args.speed)
+	local difficultyEq = FnUtil.curry(FnUtil.eq, args.difficulty)
+
+	Array.extendWith(categories, Array.map(self:getAllArgsForBase(args, 'function'), function (element)
+		return element .. ' Operators'
+	end))
+
+	if Array.any({'1', 'slow', 'low'}, speedEq) then
+		Array.appendWith(categories, '1 Speed Operators')
+	elseif Array.any({'2', 'medium'}, speedEq) then
+		Array.appendWith(categories, '2 Speed Operators')
+	elseif Array.any({'3', 'fast', 'high'}, speedEq) then
+		Array.appendWith(categories, '3 Speed Operators')
+	end
+
+	if Array.any({'1', 'easy', 'low'}, difficultyEq) then
+		Array.appendWith(categories, '1 Difficulty Operators')
+	elseif Array.any({'2', 'normal', 'medium'}, difficultyEq) then
+		Array.appendWith(categories, '2 Difficulty Operators')
+	elseif Array.any({'3', 'hard', 'difficult'}, difficultyEq) then
+		Array.appendWith(categories, '3 Difficulty Operators')
+	end
+
+	return categories
 end
 
 ---@param args table
