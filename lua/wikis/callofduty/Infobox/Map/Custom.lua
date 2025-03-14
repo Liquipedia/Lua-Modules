bfd9204c6be10089b6b4fa89c908ecb03886e412
SlothyMan <76825878+SlothyMan@users.noreply.github.com>
2025-03-14 20:23:15 +0100

    Update Custom.lua

--- lua/wikis/callofduty/Infobox/Map/Custom.lua
+++ lua/wikis/callofduty/Infobox/Map/Custom.lua
@@ -9,6 +9,7 @@
 local Array = require('Module:Array')
 local Class = require('Module:Class')
 local Lua = require('Module:Lua')
+local String = require('Module:StringUtils')
 
 local Game = Lua.import('Module:Game')
 local Injector = Lua.import('Module:Infobox/Widget/Injector')
@@ -26,8 +27,18 @@ local CustomInjector = Class.new(Injector)
 function CustomMap.run(frame)
 	local map = CustomMap(frame)
 	map:setWidgetInjector(CustomInjector(map))
-
 	return map:createInfobox()
+end
+
+---@param args table
+---@return string[]
+function CustomMap:_getGames(args)
+	if String.isNotEmpty(args.game) and String.isEmpty(args.game1) then
+		return {Game.name{game = args.game}}
+	else
+		local games = self:getAllArgsForBase(args, 'game')
+		return Array.map(games, function(game) return Game.name{game = game} end)
+	end
 end
 
 ---@param widgetId string
@@ -35,18 +46,16 @@ end
 ---@return Widget[]
 function CustomInjector:parse(widgetId, widgets)
 	local args = self.caller.args
-
 	if widgetId == 'custom' then
 		return Array.append(
 			widgets,
 			Cell{name = 'Type', content = {args.type}},
 			Cell{name = 'Size', content = {args.size}},
-			Cell{name = 'Game Version', content = {Game.name{game = args.game}}, options = {makeLink = true}},
+			Cell{name = 'Game Versions', content = self.caller:_getGames(args), options = {makeLink = true}},
 			Cell{name = 'Day/Night Variant', content = {args.daynight}},
 			Cell{name = 'Playlists', content = {args.playlist}}
 		)
 	end
-
 	return widgets
 end
 
@@ -54,9 +63,13 @@ end
 ---@param args table
 ---@return table
 function CustomMap:addToLpdb(lpdbData, args)
+	local games = self:_getGames(args)
 	lpdbData.extradata.type = args.type
 	lpdbData.extradata.size = args.size
-	lpdbData.extradata.game = Game.name{game = args.game}
+	-- Save all games
+	for i, game in ipairs(games) do
+		lpdbData.extradata["game" .. i] = game
+	end
 	lpdbData.extradata.daynight = args.daynight
 	lpdbData.extradata.playlist = args.playlist
 	return lpdbData
