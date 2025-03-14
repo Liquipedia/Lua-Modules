4fdcdb15daf0a57be9ed1a3468b59005971b75ee
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2025-03-14 15:57:42 +0900

    restructure hierarchy

--- lua/wikis/commons/Widget/Opponent/Inline.lua
+++ lua/wikis/commons/Widget/Opponent/Inline.lua
@@ -1,7 +1,7 @@
 ---
 -- @Liquipedia
 -- wiki=commons
--- page=Module:Widget/Opponent/Inline/Team
+-- page=Module:Widget/Opponent/Inline
 --
 -- Please see https://github.com/Liquipedia/Lua-Modules to contribute
 --
@@ -9,13 +9,18 @@
 local Array = require('Module:Array')
 local Class = require('Module:Class')
 local Image = require('Module:Image')
+local Logic = require('Module:Logic')
 local Lua = require('Module:Lua')
+local String = require('Module:StringUtils')
 local TeamTemplate = require('Module:TeamTemplate')
 
 local Widget = Lua.import('Module:Widget')
 local HtmlWidgets = Lua.import('Module:Widget/Html/All')
 local Link = Lua.import('Module:Widget/Basic/Link')
 local Span = HtmlWidgets.Span
+local WidgetUtil = Lua.import('Module:Widget/Util')
+
+local ICON_SIZE = '100x50px'
 
 ---@class TeamInlineParameters
 ---@field name string?
@@ -23,22 +28,29 @@ local Span = HtmlWidgets.Span
 ---@field teamTemplate teamTemplateData?
 ---@field flip boolean?
 
----@class TeamInline: Widget
----@operator call(TeamInlineParameters): TeamInline
+---@class TeamInlineWidget: Widget
+---@operator call(TeamInlineParameters): TeamInlineWidget
 ---@field props TeamInlineParameters
-local TeamInline = Class.new(Widget)
+---@field teamTemplate teamTemplateData
+---@field flip boolean
+local TeamInlineWidget = Class.new(Widget,
+	function (self, input)
+		self.teamTemplate = input.teamTemplate or TeamTemplate.getRaw(input.name, input.date)
+		self.flip = Logic.readBool(input.flip)
+	end
+)
 
 ---@return Widget
-function TeamInline:render()
-	local teamTemplate = self.props.teamTemplate or TeamTemplate.getRaw(self.props.name, self.props.date)
-	local flip = self.props.flip
-	local children = {
+function TeamInlineWidget:render()
+	local teamTemplate = self.teamTemplate
+	local flip = self.flip
+	local children = Array.interleave(WidgetUtil.collect(
 		HtmlWidgets.Fragment{children = {
 			Span{
 				classes = { 'team-template-image-icon', 'team-template-lightmode' },
 				children = {
 					Image.display(teamTemplate.image, nil, {
-						size = '100x50px',
+						size = ICON_SIZE,
 						alignment = 'middle',
 						link = teamTemplate.page
 					})
@@ -48,27 +60,36 @@ function TeamInline:render()
 				classes = { 'team-template-image-icon', 'team-template-lightmode' },
 				children = {
 					Image.display(teamTemplate.imagedark, nil, {
-						size = '100x50px',
+						size = ICON_SIZE,
 						alignment = 'middle',
 						link = teamTemplate.page
 					})
 				}
 			}
 		}},
-		' ',
-		Span{
+		String.isNotEmpty(TeamInlineWidget:getDisplayName()) and Span{
 			classes = 'team-template-text',
 			children = {Link{
-				children = teamTemplate.name,
+				children = TeamInlineWidget:getDisplayName(),
 				link = teamTemplate.page
 			}}
-		}
-	}
+		} or nil
+	), ' ')
 	return Span{
 		attributes = { ['data-highlighting-class'] = self.props.teamTemplate.name },
-		classes = { 'team-template-team' .. (flip and '2' or '') .. '-standard' },
+		classes = { 'team-template-team' .. (flip and '2' or '') .. '-' .. self:getType() },
 		children = flip and Array.reverse(children) or children
 	}
 end
 
-return TeamInline
+---@return string
+function TeamInlineWidget:getType()
+	error('Widget/Opponent/Inline is an abstract implementation and should not be instantiated directly')
+end
+
+---@return string
+function TeamInlineWidget:getDisplayName()
+	error('Widget/Opponent/Inline is an abstract implementation and should not be instantiated directly')
+end
+
+return TeamInlineWidget
--- lua/wikis/commons/Widget/Opponent/Inline/Short.lua
+++ lua/wikis/commons/Widget/Opponent/Inline/Short.lua
@@ -1,68 +1,28 @@
 ---
 -- @Liquipedia
 -- wiki=commons
--- page=Module:Widget/Opponent/Inline/Team
+-- page=Module:Widget/Opponent/Inline/Short
 --
 -- Please see https://github.com/Liquipedia/Lua-Modules to contribute
 --
 
-local Array = require('Module:Array')
 local Class = require('Module:Class')
-local Image = require('Module:Image')
 local Lua = require('Module:Lua')
-local TeamTemplate = require('Module:TeamTemplate')
 
-local Widget = Lua.import('Module:Widget')
-local HtmlWidgets = Lua.import('Module:Widget/Html/All')
-local Link = Lua.import('Module:Widget/Basic/Link')
-local Span = HtmlWidgets.Span
+local TeamInlineWidget = Lua.import('Module:Widget/Opponent/Inline')
 
----@class TeamShort: Widget
+---@class TeamShort: TeamInlineWidget
 ---@operator call(TeamInlineParameters): TeamShort
----@field props TeamInlineParameters
-local TeamShort = Class.new(Widget)
+local TeamShort = Class.new(TeamInlineWidget)
 
----@return Widget
-function TeamShort:render()
-	local teamTemplate = self.props.teamTemplate or TeamTemplate.getRaw(self.props.name, self.props.date)
-	local flip = self.props.flip
-	local children = {
-		HtmlWidgets.Fragment{children = {
-			Span{
-				classes = { 'team-template-image-icon', 'team-template-lightmode' },
-				children = {
-					Image.display(teamTemplate.image, nil, {
-						size = '100x50px',
-						alignment = 'middle',
-						link = teamTemplate.page
-					})
-				}
-			},
-			Span{
-				classes = { 'team-template-image-icon', 'team-template-lightmode' },
-				children = {
-					Image.display(teamTemplate.imagedark, nil, {
-						size = '100x50px',
-						alignment = 'middle',
-						link = teamTemplate.page
-					})
-				}
-			}
-		}},
-		' ',
-		Span{
-			classes = 'team-template-text',
-			children = {Link{
-				children = teamTemplate.shortname,
-				link = teamTemplate.page
-			}}
-		}
-	}
-	return Span{
-		attributes = { ['data-highlighting-class'] = self.props.teamTemplate.name },
-		classes = { 'team-template-team' .. (flip and '2' or '') .. '-short' },
-		children = flip and Array.reverse(children) or children
-	}
+---@return string
+function TeamShort:getType()
+	return 'short'
+end
+
+---@return string
+function TeamShort:getDisplayName()
+	return self.teamTemplate.shortname
 end
 
 return TeamShort
