77fe8f2f710d4a2b0827ed7d780f5ee23b093b26
Hesketh2 <88981446+Hesketh2@users.noreply.github.com>
2025-03-15 14:59:42 +0700

    Filter Buttons which is AoE port

---
+++ lua/wikis/callofduty/FilterButtons/Config.lua
@@ -0,0 +1,34 @@
+local Config = {}
+local Tier = require('Module:Tier/Utils')
+local Game = require('Module:Game')
+
+Config.categories = {
+	{
+		name = 'liquipediatier',
+		property = 'liquipediaTier',
+		load = function(category)
+			category.items = {}
+			for _, tier in Tier.iterate('tiers') do
+				table.insert(category.items, tier.value)
+			end
+		end,
+		defaultItems = {'1', '2', '3'},
+		transform = function(tier)
+			return Tier.toName(tier)
+		end,
+		expandKey = "game",
+	},
+	{
+		name = 'game',
+		property = 'game',
+		expandable = true,
+		load = function(category)
+			category.items = Game.listGames({ordered = true})
+		end,
+		transform = function(game)
+			return Game.icon({game = game, noSpan = true, noLink = true, size = '20x20px'})
+		end
+	}
+}
+
+return Config
