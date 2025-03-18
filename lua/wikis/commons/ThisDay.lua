289ccd49e5e9e852b1c712e0f3b0c65fb5e2442b
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2025-03-18 12:07:21 +0900

    add missing import

--- lua/wikis/commons/ThisDay.lua
+++ lua/wikis/commons/ThisDay.lua
@@ -9,6 +9,7 @@
 local Array = require('Module:Array')
 local Class = require('Module:Class')
 local DateExt = require('Module:Date/Ext')
+local Flags = require('Module:Flags')
 local LeagueIcon = require('Module:LeagueIcon')
 local Logic = require('Module:Logic')
 local Lua = require('Module:Lua')
