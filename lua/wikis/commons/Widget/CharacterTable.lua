8b0d878bf08331ed0b84b0668c00c99545f691f2
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2025-03-19 11:50:43 +0900

    add support for extra conditons

--- lua/wikis/commons/Widget/CharacterTable.lua
+++ lua/wikis/commons/Widget/CharacterTable.lua
@@ -10,6 +10,7 @@ local Array = require('Module:Array')
 local Class = require('Module:Class')
 local Lua = require('Module:Lua')
 local Operator = require('Module:Operator')
+local String = require('Module:StringUtils')
 
 local Widget = Lua.import('Module:Widget')
 local HtmlWidgets = Lua.import('Module:Widget/Html/All')
@@ -18,13 +19,18 @@ local Entry = Lua.import('Module:Widget/CharacterTable/Entry')
 
 ---@class CharacterTable: Widget
 ---@operator call(table): CharacterTable
----@field props {alias: string?}
+---@field props {alias: string?, extraConditions: string?}
 local CharacterTable = Class.new(Widget)
 
 ---@return Widget
 function CharacterTable:render()
+	local conditions = '[[type::character]]'
+	local extraConditions = self.props.extraConditions
+	if String.isNotEmpty(extraConditions) then
+		conditions = conditions .. ' AND (' .. extraConditions .. ')'
+	end
 	local characterNames = Array.map(mw.ext.LiquipediaDB.lpdb('datapoint', {
-		conditions = '[[type::character]]',
+		conditions = conditions,
 		characterNames = 'name',
 		order = 'name asc',
 		limit = 1000,
