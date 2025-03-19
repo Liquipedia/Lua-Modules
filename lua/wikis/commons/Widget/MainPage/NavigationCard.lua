eb74030294f170fd9dbd26eacccb4e866b0590b8
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2025-03-19 22:37:46 +0900

    refactor(mainpage): introduce widgets for frequently used panel components (#5674)
    * refactor match ticker widget
    
    * refactor headlines widget
    
    * slice out navigation card
    
    * type annotation
    
    * add extra safeguard
    
    * use more generic return type

---
+++ lua/wikis/commons/Widget/MainPage/NavigationCard.lua
@@ -0,0 +1,51 @@
+---
+-- @Liquipedia
+-- wiki=commons
+-- page=Module:Widget/MainPage/NavigationCard
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Class = require('Module:Class')
+local Image = require('Module:Image')
+local Lua = require('Module:Lua')
+
+local Widget = Lua.import('Module:Widget')
+local HtmlWidgets = Lua.import('Module:Widget/Html/All')
+local Link = Lua.import('Module:Widget/Basic/Link')
+local WidgetUtil = Lua.import('Module:Widget/Util')
+
+---@class NavigationCardParameters
+---@field file string?
+---@field link string?
+---@field title string?
+---@field count integer?
+
+---@class NavigationCard: Widget
+---@field props NavigationCardParameters
+---@operator call(NavigationCardParameters): NavigationCard
+local NavigationCard = Class.new(Widget)
+
+---@return Widget
+function NavigationCard:render()
+	local count = self.props.count
+	return HtmlWidgets.Div{
+		classes = {'navigation-card'},
+		children = WidgetUtil.collect(
+			HtmlWidgets.Div{
+				classes = {'navigation-card__image'},
+				children = Image.display(self.props.file, nil, {size = 240, link = ''}),
+			},
+			HtmlWidgets.Span{
+				classes = {'navigation-card__title'},
+				children = Link{link = self.props.link, children = self.props.title}
+			},
+			count and HtmlWidgets.Span{
+				classes = {'navigation-card__subtitle'},
+				children = count,
+			} or nil
+		)
+	}
+end
+
+return NavigationCard
