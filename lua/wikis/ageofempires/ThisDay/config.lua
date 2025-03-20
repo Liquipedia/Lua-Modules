93c3daafa1f5fb92272ea3721b9ddd3674006616
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2025-03-20 13:37:17 +0900

    Add deploy header

--- lua/wikis/ageofempires/ThisDay/config.lua
+++ lua/wikis/ageofempires/ThisDay/config.lua
@@ -1,3 +1,11 @@
+---
+-- @Liquipedia
+-- wiki=ageofempires
+-- page=Module:ThisDay/config
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
 return {
 	soloMode = '1v1',
 	tiers = {1, 2},
--- lua/wikis/starcraft2/ThisDay/config.lua
+++ lua/wikis/starcraft2/ThisDay/config.lua
@@ -1,3 +1,11 @@
+---
+-- @Liquipedia
+-- wiki=starcraft2
+-- page=Module:ThisDay/config
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
 return {
 	tiers = {1, 2},
 	tierTypes = {'!Qualifier', '!Charity'},
