0b2bd3a2c203a717ac07619257d0b68021702786
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2025-03-21 19:52:49 +0900

    cleanup

--- lua/wikis/worldoftanks/ThisDay/config.lua
+++ lua/wikis/worldoftanks/ThisDay/config.lua
@@ -6,12 +6,8 @@
 -- Please see https://github.com/Liquipedia/Lua-Modules to contribute
 --
 
-local Condition = require('Module:Condition')
-local BooleanOperator = Condition.BooleanOperator
-
 return {
 	tiers = {1, 2},
 	tierTypes = {'!Qualifier', '!Points', '!Showmatch'},
-	tierTypeBooleanOperator = BooleanOperator.all,
-	soloMode = 'individual',
+	showEmptyBirthdayList = true,
 }
