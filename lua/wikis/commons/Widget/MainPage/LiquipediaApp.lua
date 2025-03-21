840c91af06d4499219e887bc8711377cb4260039
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2025-03-21 11:57:20 +0900

    make it work

--- lua/wikis/commons/Widget/MainPage/LiquipediaApp.lua
+++ lua/wikis/commons/Widget/MainPage/LiquipediaApp.lua
@@ -40,71 +40,73 @@ local function buildDescriptionList(entries)
 	return HtmlWidgets.Dl{
 		children = Array.map(entries, function (entry)
 			return HtmlWidgets.Dd{
-				children = WidgetUtil.collect(entry)
+				children = WidgetUtil.collect(unpack(entry))
 			}
 		end)
 	}
 end
 
----@return Widget
+---@return Widget[]
 function LiquipediaApp:render()
-	return Div{
-		css = {
-			display = 'Flex',
-			['justify-content'] = 'left',
-			padding = '0px',
-		},
-		children = {
-			Div{
-				classes = { 'mobile-hide' },
-				children = {Image.display('Qr-code-app.svg', nil, { size = '132px', link = '' })}
+	return {
+		Div{
+			css = {
+				display = 'Flex',
+				['justify-content'] = 'left',
+				padding = '0px',
 			},
-			Div{
-				css = { ['line-height'] = '2.2em' },
-				children = buildDescriptionList{
-					{
-						GREEN_CHECK_CIRCLE,
-						'Follow your favorite ',
-						Info.name,
-						' players and teams!'
-					},
-					{
-						GREEN_CHECK_CIRCLE,
-						'Get notifications and never miss a match again.'
-					},
-					{
-						GREEN_CHECK_CIRCLE,
-						'Available in ',
-						Array.interleave(Array.map({'ru', 'br', 'fr', 'es', 'cn', 'de', 'jp'}, function (country)
-							return Flags.Icon({shouldLink = false}, country)
-						end), ' '),
-						' and 12 more languages!'
-					},
-					{
-						GREEN_CHECK_CIRCLE,
-						'Spoiler-free version.'
+			children = {
+				Div{
+					classes = { 'mobile-hide' },
+					children = {Image.display('Qr-code-app.svg', nil, { size = '132px', link = '' })}
+				},
+				Div{
+					css = { ['line-height'] = '2.2em' },
+					children = buildDescriptionList{
+						{
+							GREEN_CHECK_CIRCLE,
+							'Follow your favorite ',
+							Info.name,
+							' players and teams!'
+						},
+						{
+							GREEN_CHECK_CIRCLE,
+							'Get notifications and never miss a match again.'
+						},
+						{
+							GREEN_CHECK_CIRCLE,
+							'Available in ',
+							Array.interleave(Array.map({'ru', 'br', 'fr', 'es', 'cn', 'de', 'jp'}, function (country)
+								return Flags.Icon({shouldLink = false}, country)
+							end), ' '),
+							' and 12 more languages!'
+						},
+						{
+							GREEN_CHECK_CIRCLE,
+							'Spoiler-free version.'
+						}
 					}
 				}
+			}
+		},
+		Div{
+			css = {
+				display = 'flex',
+				['flex-wrap'] = 'wrap',
+				gap = '12px',
+				['justify-content'] = 'center',
+				clear = 'both'
 			},
-			Div{
-				css = {
-					display = 'flex',
-					['flex-wrap'] = 'wrap',
-					gap = '12px',
-					['justify-content'] = 'center',
-					clear = 'both'
-				},
-				children = {
-					Image.display('App-store.svg', nil, {
-						link = 'https://apps.apple.com/us/app/liquipedia-esports-tracker/id1640722331',
-						size = '160px'
-					}),
-					' ',
-					Image.display('Google-play.svg', nil, {
-						link = 'https://play.google.com/store/apps/details?id=com.teamliquid.liquipedia.liquipedia_app',
-						size = '160px'
-					})
-				}
+			children = {
+				Image.display('App-store.svg', nil, {
+					link = 'https://apps.apple.com/us/app/liquipedia-esports-tracker/id1640722331',
+					size = '160px'
+				}),
+				' ',
+				Image.display('Google-play.svg', nil, {
+					link = 'https://play.google.com/store/apps/details?id=com.teamliquid.liquipedia.liquipedia_app',
+					size = '160px'
+				})
 			}
 		}
 	}
