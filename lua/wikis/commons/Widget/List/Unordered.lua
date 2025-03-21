68b2afb6c5a15ecde87737ee636bafdad91f0002
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2025-03-21 20:19:34 +0900

    Introduce list widgets

---
+++ lua/wikis/commons/Widget/List/Unordered.lua
@@ -0,0 +1,24 @@
+---
+-- @Liquipedia
+-- wiki=commons
+-- page=Module:Widget/List/Ordered
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Class = require('Module:Class')
+local Lua = require('Module:Lua')
+
+local ListWidget = Lua.import('Module:Widget/List')
+local HtmlWidgets = Lua.import('Module:Widget/Html/All')
+
+---@class UnorderedList: ListWidget
+---@operator call(table): UnorderedList
+local UnorderedList = Class.new(ListWidget)
+
+---@return WidgetHtml
+function UnorderedList:getType()
+	return HtmlWidgets.Ul
+end
+
+return UnorderedList
