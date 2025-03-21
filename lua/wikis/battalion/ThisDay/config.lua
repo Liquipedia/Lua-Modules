5e637a2504f85a96885e69d26ee4b067a66e3722
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2025-03-21 21:21:43 +0900

    add configs based on template configs

---
+++ lua/wikis/battalion/ThisDay/config.lua
@@ -0,0 +1,14 @@
+---
+-- @Liquipedia
+-- wiki=battalion
+-- page=Module:ThisDay/config
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+---@type ThisDayConfig
+return {
+	tiers = {1, 2},
+	tierTypes = {'!Qualifier'},
+	showPatches = true,
+}
---
+++ lua/wikis/rainbowsix/ThisDay/config.lua
@@ -0,0 +1,14 @@
+---
+-- @Liquipedia
+-- wiki=rainbowsix
+-- page=Module:ThisDay/config
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+---@type ThisDayConfig
+return {
+	tiers = {1, 2},
+	tierTypes = {'!Qualifier'},
+	showPatches = true,
+}
