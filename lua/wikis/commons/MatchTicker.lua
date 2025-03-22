139cb6abcf3726bec0085c8383d5ba5d89e17fa0
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2025-03-22 19:29:16 +0900

    nil catch

--- lua/wikis/commons/MatchTicker.lua
+++ lua/wikis/commons/MatchTicker.lua
@@ -153,7 +153,7 @@ function MatchTicker:init(args)
 		not (config.upcoming or config.ongoing)),
 		'Invalid recent, upcoming, ongoing combination')
 
-	local teamPages = TeamTemplate.queryHistoricalNames(args.team)
+	local teamPages = args.team and TeamTemplate.queryHistoricalNames(args.team) or nil
 	if Logic.isNotEmpty(teamPages) then
 		Array.extendWith(teamPages,
 		Array.map(teamPages, function(team) return (team:gsub(' ', '_')) end),
