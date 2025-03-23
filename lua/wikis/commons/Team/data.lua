08f8450e9a9360bc3bec6220243093dc9c4025b5
Martin B <theimarbot@gmail.com>
2025-03-23 12:58:14 +0900

    Add base team data from Commons

---
+++ lua/wikis/commons/Team/data.lua
@@ -0,0 +1,79 @@
+return {
+	['builderdata'] = {
+		['team'] = {class = 'team-template-team-standard'},
+		['team2'] = {class = 'team-template-team-standard', textFirst = true},
+		['teamshort'] = {class = 'team-template-team-short', shortName = true},
+		['team2short'] = {class = 'team-template-team-short', shortName = true, textFirst = true},
+		['teamhybrid'] = {class = 'team-template-team-standard', textHybrid = true},
+		['teambracket'] = {class = 'team-template-team-bracket', noLink = true},
+		['teambracketshort'] = {class = 'team-template-team-bracket', noLink = true, shortName = true},
+		['teamicon'] = {class = 'team-template-team-icon', iconOnly = true},
+		['teampart'] = {class = 'team-template-team-part', iconOnly = true},
+		['teamlink'] = {class = '', textOnly = true},
+	},
+	['game'] = {},
+	['games'] = {
+		['']	 = {light = 'Filler 600px.png', dark = 'Filler 600px.png'}
+	},
+	['imagereplace'] = {},
+	['overrides'] = {
+		['aliases'] = {
+			['definitions'] = 'tbd',
+			['noteam'] = '',
+			['filler'] = '',
+		},
+		['raw'] = {
+			['tbd'] = {
+				name = '<abbr title="To Be Determined">TBD</abbr>',
+				imagename = 'To Be Determined',
+				page = 'TBD',
+				shortname = '<abbr title="To Be Determined">TBD</abbr>',
+				bracketname = '<abbr title="To Be Determined">TBD</abbr>',
+				templatename = 'tbd',
+				image = 'Filler 600px.png',
+				imagedark = 'Filler 600px.png',
+				legacyimage = '',
+				legacyimagedark = '',
+				nolink = true,
+			},
+			['tba'] = {
+				name = '<abbr title="To Be Announced">TBA</abbr>',
+				imagename = 'To Be Announced',
+				page = 'TBA',
+				shortname = '<abbr title="To Be Announced">TBA</abbr>',
+				bracketname = '<abbr title="To Be Announced">TBA</abbr>',
+				templatename = 'tba',
+				image = 'Filler 600px.png',
+				imagedark = 'Filler 600px.png',
+				legacyimage = '',
+				legacyimagedark = '',
+				nolink = true,
+			},
+			['bye'] = {
+				name = '<i class ="color-dimgray">BYE</i>',
+				imagename = 'BYE',
+				page = 'BYE',
+				shortname = '<i class ="color-dimgray">BYE</i>',
+				bracketname = '<i class ="color-dimgray">BYE</i>',
+				templatename = 'bye',
+				image = 'Filler 600px.png',
+				imagedark = 'Filler 600px.png',
+				legacyimage = '',
+				legacyimagedark = '',
+				nolink = true,
+			},
+			[''] = {
+				name = '',
+				page = '',
+				shortname = '',
+				bracketname = '',
+				templatename = '',
+				image = 'Filler 600px.png',
+				imagedark = 'Filler 600px.png',
+				legacyimage = '',
+				legacyimagedark = '',
+				nolink = true,
+			}
+		}
+	}
+}
