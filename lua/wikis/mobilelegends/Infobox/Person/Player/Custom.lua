88cda209b17c9df64d43b793300aac4f1d6b0742
Eetu Rantanen <eeturantanen.dev@gmail.com>
2025-03-25 15:35:58 +0200

    modified mobilelegends

--- lua/wikis/mobilelegends/Infobox/Person/Player/Custom.lua
+++ lua/wikis/mobilelegends/Infobox/Person/Player/Custom.lua
@@ -26,9 +26,6 @@ local Center = Widgets.Center
 
 local SIZE_HERO = '25x25px'
 
----@class MobilelegendsInfoboxPlayer: Person
----@field role table
----@field role2 table
 local CustomPlayer = Class.new(Player)
 local CustomInjector = Class.new(Injector)
 
@@ -39,8 +36,6 @@ function CustomPlayer.run(frame)
 	player:setWidgetInjector(CustomInjector(player))
 
 	player.args.autoTeam = true
-	player.role = Role.run{role = player.args.role}
-	player.role2 = Role.run{role = player.args.role2}
 
 	return player:createInfobox()
 end
@@ -87,10 +82,6 @@ function CustomInjector:parse(id, widgets)
 			}
 		end
 	elseif id == 'region' then return {}
-	elseif id == 'role' then
-		return {
-			Cell{name = 'Role(s)', content = {caller.role.display, caller.role2.display}}
-		}
 	end
 	return widgets
 end
