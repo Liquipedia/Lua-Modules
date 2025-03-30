42f81119641596c5a68f0f1f678aa224ff48b0fe
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2025-03-30 11:28:10 +0900

    update documentation for ConditionUtil.multiValueCondition

--- lua/wikis/commons/Condition.lua
+++ lua/wikis/commons/Condition.lua
@@ -165,7 +165,7 @@ end
 
 local ConditionUtil = {}
 
----Builds conditions for a collection of values.
+---Builds "all of" or "any of" condition from the given collection of values.
 ---@param column ColumnName
 ---@param values (string|number)[]
 ---@param booleanOperator lpdbBooleanOperator
