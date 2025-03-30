5b073977f499980770eda92172665a44d5fd4fdc
Martin B <theimarbot@gmail.com>
2025-03-30 04:22:05 +0100

    missed item
    Co-authored-by: ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>

--- lua/wikis/counterstrike/Infobox/League/Custom.lua
+++ lua/wikis/counterstrike/Infobox/League/Custom.lua
@@ -33,6 +33,7 @@ local Center = Widgets.Center
 local HtmlWidgets = Lua.import('Module:Widget/Html/All')
 local IconFontawesome = Lua.import('Module:Widget/Image/Icon/Fontawesome')
 local Link = Lua.import('Module:Widget/Basic/Link')
+local WidgetUtil = Lua.import('Module:Widget/Util')
 
 ---@class CounterstrikeLeagueInfobox: InfoboxLeague
 ---@field gameData table
