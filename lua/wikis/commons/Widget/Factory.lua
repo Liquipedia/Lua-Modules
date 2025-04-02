063d2968496d5bd99eb009e432f01ffba907d3b2
Rikard Blixt <rikardblixt@gmail.com>
2025-04-02 14:45:14 +0200

    make Factory.fromTemplate support function widgets

--- lua/wikis/commons/Widget/Factory.lua
+++ lua/wikis/commons/Widget/Factory.lua
@@ -18,25 +18,34 @@ local WidgetFactory = {}
 ---@param args {widget: string, children: ((Widget|Html|string|number)[])|Widget|Html|string|number, [any]:any}
 ---@return Widget
 function WidgetFactory.fromTemplate(args)
+	local Widget = Lua.import('Module:Widget')
 	local widgetClass = args.widget
+	assert(args.widget, '|widget= is required')
 	args.widget = nil
 	args.children = type(args.children) == 'table' and args.children or {args.children}
 	local WidgetClass = Lua.import('Module:Widget/' .. widgetClass)
+	assert(WidgetClass, 'Widget not found: ' .. widgetClass)
+	if not Class.instanceOf(WidgetClass, Widget) then
+		WidgetClass = WidgetClass.default
+		assert(type(WidgetClass) == 'function', 'Widget not found: ' .. widgetClass)
+	end
 	return WidgetClass(args)
 end
 
----@param widget string|Widget
----@param props table
+---@param widget string|Widget|fun(props: table?):Widget
+---@param props table?
 ---@param ... (Widget|Html|string|number)
+---@return Widget
 function WidgetFactory.createElement(widget, props, ...)
 	local widgetClass = widget
 	if type(widget) == 'string' then
+		widget = (widget:gsub("^%l", string.upper))
 		widgetClass = HtmlWidgets[widget]
 	end
 
 	assert(widgetClass, 'Widget not found')
 
-	return widgetClass(Table.merge(props, {children = WidgetUtil.collect(...)}))
+	return widgetClass(Table.merge(props or {}, {children = WidgetUtil.collect(...)}))
 end
 
 return Class.export(WidgetFactory)
