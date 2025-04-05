3731a4e2b6f3525a5eb2f339fc97fb68f022c2bb
Hesketh2 <88981446+Hesketh2@users.noreply.github.com>
2025-04-05 20:58:10 +0700

    feat(Infobox): Tarkov Arena player

---
+++ lua/wikis/tarkovarena/Infobox/Person/Player/Custom.lua
@@ -0,0 +1,35 @@
+---
+-- @Liquipedia
+-- wiki=tarkovarena
+-- page=Module:Infobox/Person/Player/Custom
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Array = require('Module:Array')
+local Class = require('Module:Class')
+local Lua = require('Module:Lua')
+local TeamHistoryAuto = require('Module:TeamHistoryAuto')
+
+local Injector = Lua.import('Module:Widget/Injector')
+local Player = Lua.import('Module:Infobox/Person')
+
+local Widgets = require('Module:Widget/All')
+local Cell = Widgets.Cell
+
+---@class TarkovArenaInfoboxPlayer: Person
+local CustomPlayer = Class.new(Player)
+local CustomInjector = Class.new(Injector)
+
+---@param frame Frame
+---@return Html
+function CustomPlayer.run(frame)
+	local player = CustomPlayer(frame)
+	player:setWidgetInjector(CustomInjector(player))
+
+	player.args.history = TeamHistoryAuto.results{convertrole = true}
+
+	return player:createInfobox()
+end
+
+return CustomPlayer
