6f242d6ba8b90a2ea6eff74a758eac81f93fb05a
Hesketh2 <88981446+Hesketh2@users.noreply.github.com>
2025-04-07 02:53:18 +0700

    Update Custom.lua

--- lua/wikis/tarkovarena/Infobox/Person/Player/Custom.lua
+++ lua/wikis/tarkovarena/Infobox/Person/Player/Custom.lua
@@ -10,12 +10,10 @@ local Class = require('Module:Class')
 local Lua = require('Module:Lua')
 local TeamHistoryAuto = require('Module:TeamHistoryAuto')
 
-local Injector = Lua.import('Module:Widget/Injector')
 local Player = Lua.import('Module:Infobox/Person')
 
 ---@class TarkovArenaInfoboxPlayer: Person
 local CustomPlayer = Class.new(Player)
-local CustomInjector = Class.new(Injector)
 
 ---@param frame Frame
 ---@return Html
@@ -23,7 +21,7 @@ function CustomPlayer.run(frame)
 	local player = CustomPlayer(frame)
 
 	player.args.history = TeamHistoryAuto.results{convertrole = true}
-
+	player.args.autoTeam = true
 	return player:createInfobox()
 end
 
