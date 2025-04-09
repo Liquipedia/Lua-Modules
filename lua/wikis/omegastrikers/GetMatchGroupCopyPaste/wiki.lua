3e86c023b1ed28f59777450f2e7179771d947211
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2025-04-09 18:56:39 +0900

    refactor omegastrikers generator

--- lua/wikis/omegastrikers/GetMatchGroupCopyPaste/wiki.lua
+++ lua/wikis/omegastrikers/GetMatchGroupCopyPaste/wiki.lua
@@ -8,8 +8,8 @@
 
 local Array = require('Module:Array')
 local Class = require('Module:Class')
+local Logic = require('Module:Logic')
 local Lua = require('Module:Lua')
-local String = require('Module:StringUtils')
 
 local BaseCopyPaste = Lua.import('Module:GetMatchGroupCopyPaste/wiki/Base')
 
@@ -40,77 +40,67 @@ local WikiCopyPaste = Class.new(BaseCopyPaste)
 local INDENT = WikiCopyPaste.Indent
 
 --returns the Code for a Match, depending on the input
+---@param bestof integer
+---@param mode string
+---@param index integer
+---@param opponents integer
+---@param args table
+---@return string
 function WikiCopyPaste.getMatchCode(bestof, mode, index, opponents, args)
+
+	local showScore = Logic.nilOr(Logic.readBool(args.score), true)
+
 	local lines = Array.extend(
 		'{{Match',
-		index == 1 and (INDENT .. '|bestof=' .. (bestof ~= 0 and bestof or '')) or nil,
-		args.hasDate == 'true' and {INDENT .. '|date=', INDENT .. '|twitch='} or {}
+		bestof ~= 0 and (INDENT .. '|bestof=' .. bestof) or nil,
+		Logic.readBool(args.hasDate) and {
+			INDENT .. '|date=',
+			INDENT .. '|twitch='
+		} or nil,
+		Array.map(Array.range(1, opponents), function(opponentIndex)
+			return INDENT .. '|opponent' .. opponentIndex .. '=' .. WikiCopyPaste.getOpponent(mode, showScore)
+		end),
+		Array.map(Array.range(1, bestof), function (mapIndex)
+			return INDENT .. '|map' .. mapIndex .. WikiCopyPaste._getMap(opponents, args.pickBan, args.mapBestof)
+		end),
+		'}}'
 	)
-
-	local score = args.score == 'true' and '|score=' or nil
-	for i = 1, opponents do
-		table.insert(lines, INDENT .. '|opponent' .. i .. '=' .. WikiCopyPaste._getOpponent(mode, score or ''))
-	end
-
-	lines = WikiCopyPaste._getMaps(lines, bestof, args, index, opponents)
-
-	table.insert(lines, '}}')
 
 	return table.concat(lines, '\n')
 end
 
---subfunction used to generate the code for the Opponent template, depending on the type of opponent
-function WikiCopyPaste._getOpponent(mode, score)
-	local out
+---@private
+---@param opponents integer
+---@param pickBan string?
+---@param mapBestOf integer?
+---@return string
+function WikiCopyPaste._getMap(opponents, pickBan, mapBestOf)
+	local map = Array.extend(
+		'={{Map',
+		INDENT .. INDENT .. '|map=',
+		INDENT .. INDENT .. '|score1=|score2=',
+		Array.flatMap(_CONVERT_PICK_BAN_ENTRY[pickBan or ''] or {}, function (element)
+			return WikiCopyPaste._pickBanParams(element, opponents)
+		end),
+		mapBestOf and (INDENT .. INDENT .. '|bestof=' .. mapBestOf) or nil,
+		INDENT .. '}}'
+	)
 
-	if mode == 'solo' then
-		out = '{{SoloOpponent||flag=|team=' .. score .. '}}'
-	elseif mode == 'team' then
-		out = '{{TeamOpponent|' .. score .. '}}'
-	elseif mode == 'literal' then
-		out = '{{Literal|}}'
-	end
-
-	return out
+	return table.concat(map, '\n')
 end
 
---subfunction used to generate the code for the Map template
---sets up as many maps as specified via the bestoff param
-function WikiCopyPaste._getMaps(lines, bestof, args, matchIndex, numberOfOpponents)
-	if bestof > 0 then
-		local map = '{{Map'
-			.. '\n' .. INDENT .. INDENT .. '|map='
-			.. '\n' .. INDENT .. INDENT .. '|score1=|score2='
-
-		for _, item in ipairs(_CONVERT_PICK_BAN_ENTRY[args.pickBan or ''] or {}) do
-			map = map .. WikiCopyPaste._pickBanParams(item, numberOfOpponents)
-		end
-
-		for mapIndex = 1, bestof do
-			local currentMap = INDENT .. '|map' .. mapIndex .. '=' .. map
-			--first map has additional mapBestof if it is the first match
-			if matchIndex == 1 and mapIndex == 1 and String.isNotEmpty(args.mapBestof) then
-				currentMap = currentMap .. '\n' .. INDENT .. INDENT .. '|bestof=' .. args.mapBestof
-			end
-			currentMap = currentMap .. '\n' .. INDENT .. '}}'
-			table.insert(lines, currentMap)
-		end
-	end
-
-	return lines
-end
-
+---@private
+---@param key string
+---@param numberOfOpponents integer
+---@return string[]
 function WikiCopyPaste._pickBanParams(key, numberOfOpponents)
 	local shortKey = _PARAM_TO_SHORT[key]
 	local limit = _LIMIT_OF_PARAM[key]
-	local display = ''
-
-	for opponentIndex = 1, numberOfOpponents do
-		display = display .. '\n' .. INDENT .. INDENT
-		for keyIndex = 1, limit do
-			display = display .. '|t' .. opponentIndex .. shortKey .. keyIndex .. '='
-		end
-	end
+	local display = Array.map(Array.range(1, numberOfOpponents), function (opponentIndex)
+		return INDENT .. INDENT .. table.concat(Array.map(Array.range(1, limit), function (keyIndex)
+			return '|t' .. opponentIndex .. shortKey .. keyIndex .. '='
+		end))
+	end)
 
 	return display
 end
