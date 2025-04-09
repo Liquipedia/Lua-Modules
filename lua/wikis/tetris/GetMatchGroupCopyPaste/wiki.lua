3678df2d1a9acaa86f4cc3cc00db5e924ecaffdb
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2025-04-09 19:17:10 +0900

    refactor tetris generator

--- lua/wikis/tetris/GetMatchGroupCopyPaste/wiki.lua
+++ lua/wikis/tetris/GetMatchGroupCopyPaste/wiki.lua
@@ -8,6 +8,7 @@
 
 local Array = require('Module:Array')
 local Class = require('Module:Class')
+local Logic = require('Module:Logic')
 local Lua = require('Module:Lua')
 
 local BaseCopyPaste = Lua.import('Module:GetMatchGroupCopyPaste/wiki/Base')
@@ -16,20 +17,6 @@ local BaseCopyPaste = Lua.import('Module:GetMatchGroupCopyPaste/wiki/Base')
 local WikiCopyPaste = Class.new(BaseCopyPaste)
 
 local INDENT = WikiCopyPaste.Indent
-
---allowed opponent types on the wiki
-local MODES = {
-	['solo'] = 'solo',
-	['team'] = 'team',
-}
-
---default opponent type (used if the entered mode is not found in the above table)
-local DefaultMode = 'solo'
-
---returns the cleaned opponent type
-function WikiCopyPaste.getMode(mode)
-	return MODES[string.lower(mode or '')] or DefaultMode
-end
 
 --returns the Code for a Match, depending on the input
 ---@param bestof integer
@@ -40,46 +27,23 @@ end
 ---@return string
 function WikiCopyPaste.getMatchCode(bestof, mode, index, opponents, args)
 
-	if bestof == 0 and args.score ~= 'false' then
-		args.score = 'true'
-	end
+	local showScore = Logic.nilOr(Logic.readBool(args.score), true)
 
-	local score = args.score == 'true' and '|score=' or nil
 	local lines = Array.extend(
 		'{{Match',
-		index == 1 and (INDENT .. '|bestof=' .. (bestof ~= 0 and bestof or '')) or nil,
-		args.needsWinner == 'true' and INDENT .. '|winner=' or nil,
-		args.hasDate == 'true' and {INDENT .. '|date=', INDENT .. '|youtube=|twitch='} or {}
+		bestof ~= 0 and (INDENT .. '|bestof=' .. bestof) or nil,
+		Logic.readBool(args.needsWinner) and INDENT .. '|winner=' or nil,
+		Logic.readBool(args.hasDate) and {
+			INDENT .. '|date=',
+			INDENT .. '|youtube=|twitch='
+		} or nil,
+		Array.map(Array.range(1, opponents), function(opponentIndex)
+			return INDENT .. '|opponent' .. opponentIndex .. '=' .. WikiCopyPaste.getOpponent(mode, showScore)
+		end),
+		'}}'
 	)
 
-	for i = 1, opponents do
-		table.insert(lines, INDENT .. '|opponent' .. i .. '=' .. WikiCopyPaste._getOpponent(mode, score))
-	end
-
-	if bestof ~= 0 then
-		for i = 1, bestof do
-			Array.appendWith()
-		end
-	end
-
-	table.insert(lines, '}}')
-
 	return table.concat(lines, '\n')
-end
-
---subfunction used to generate the code for the Opponent template, depending on the type of opponent
-function WikiCopyPaste._getOpponent(mode, score)
-	local out
-
-	if mode == 'solo' then
-		out = '{{SoloOpponent||flag=' .. (score or '') .. '}}'
-	elseif mode == 'team' then
-		out = '{{TeamOpponent|' .. (score or '') .. '}}'
-	elseif mode == 'literal' then
-		out = '{{Literal|}}'
-	end
-
-	return out
 end
 
 return WikiCopyPaste
