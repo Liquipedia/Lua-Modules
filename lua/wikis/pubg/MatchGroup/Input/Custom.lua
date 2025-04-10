0b6eeb066dcb3cb1b73477a6377157f823b5958b
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2025-04-10 11:17:08 +0900

    kick local caster parsing from pubg

--- lua/wikis/pubg/MatchGroup/Input/Custom.lua
+++ lua/wikis/pubg/MatchGroup/Input/Custom.lua
@@ -67,7 +67,6 @@ end
 function MatchFunctions.getExtraData(match, games, opponents)
 	return {
 		mvp = MatchGroupInputUtil.readMvp(match, opponents),
-		casters = MatchGroupInputUtil.readCasters(match, {noSort = true}),
 	}
 end
 
