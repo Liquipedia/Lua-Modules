55e65c93ae572d635a4e7e86ce3857bae564bbea
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2025-04-10 09:40:17 +0900

    change icon for casters
    blame Nydra :P

--- lua/wikis/commons/Icon/Data.lua
+++ lua/wikis/commons/Icon/Data.lua
@@ -15,7 +15,7 @@ return {
 	matchpopup = 'fas fa-info-circle',
 	timeout = 'far fa-clock',
 	veto = 'fas fa-times',
-	casters = 'fas fa-microphone-stand',
+	casters = 'fas fa-microphone-alt',
 	comment = 'far fa-comment',
 	mvp = 'fas fa-medal',
 	startleft = 'fas fa-chevron-left',
