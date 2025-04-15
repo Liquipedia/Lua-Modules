88b55401e384aa4e83cce20384d0bd1ec7301ca0
Eetu Rantanen <eeturantanen.dev@gmail.com>
2025-04-15 13:42:45 +0300

    modified overwatch infobox logic

---
+++ lua/wikis/overwatch/InGameRoles.lua
@@ -0,0 +1,17 @@
+---
+-- @Liquipedia
+-- wiki=overwatch
+-- page=Module:InGameRoles
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local inGameRoles = {
+	['dps'] = {category = 'DPS Players', display = 'DPS'},
+	['flex'] = {category = 'Flex Players', display = 'Flex'},
+	['support'] = {category = 'Support Players', display = 'Support'},
+	['igl'] = {category = 'In-game leaders', display = 'In-game leader'},
+	['tank'] = {category = 'Tank Players', display = 'Tank'},
+}
+
+return inGameRoles
