23ee46b76ac79fb8a227ac9df14d8cf2d10c9273
Eetu Rantanen <eeturantanen.dev@gmail.com>
2025-04-15 15:17:28 +0300

    modified r6 infobox logic

--- lua/wikis/rainbowsix/Infobox/Person/Player/Custom.lua
+++ lua/wikis/rainbowsix/Infobox/Person/Player/Custom.lua
@@ -16,7 +16,6 @@ local String = require('Module:StringUtils')
 local Table = require('Module:Table')
 local Team = require('Module:Team')
 local TeamHistoryAuto = require('Module:TeamHistoryAuto')
-local Variables = require('Module:Variables')
 local Template = require('Module:Template')
 
 local Injector = Lua.import('Module:Widget/Injector')
@@ -35,28 +34,6 @@ local Widgets = require('Module:Widget/All')
 local Cell = Widgets.Cell
 
 local BANNED = mw.loadData('Module:Banned')
-local ROLES = {
-	-- Players
-	['entry'] = {category = 'Entry fraggers', variable = 'Entry fragger', isplayer = true},
-	['support'] = {category = 'Support players', variable = 'Support', isplayer = true},
-	['flex'] = {category = 'Flex players', variable = 'Flex', isplayer = true},
-	['igl'] = {category = 'In-game leaders', variable = 'In-game leader', isplayer = true},
-
-	-- Staff and Talents
-	['analyst'] = {category = 'Analysts', variable = 'Analyst', isplayer = false},
-	['observer'] = {category = 'Observers', variable = 'Observer', isplayer = false},
-	['host'] = {category = 'Hosts', variable = 'Host', isplayer = false},
-	['journalist'] = {category = 'Journalists', variable = 'Journalist', isplayer = false},
-	['expert'] = {category = 'Experts', variable = 'Expert', isplayer = false},
-	['coach'] = {category = 'Coaches', variable = 'Coach', isplayer = false},
-	['caster'] = {category = 'Casters', variable = 'Caster', isplayer = false},
-	['talent'] = {category = 'Talents', variable = 'Talent', isplayer = false},
-	['manager'] = {category = 'Managers', variable = 'Manager', isplayer = false},
-	['producer'] = {category = 'Producers', variable = 'Producer', isplayer = false},
-	['admin'] = {category = 'Admins', variable = 'Admin', isplayer = false},
-}
-ROLES.entryfragger = ROLES.entry
-ROLES['assistant coach'] = ROLES.coach
 
 local GAMES = {
 	r6s = '[[Rainbow Six Siege|Siege]]',
@@ -66,9 +43,6 @@ GAMES.siege = GAMES.r6s
 
 local SIZE_OPERATOR = '25x25px'
 
----@class RainbowsixInfoboxPlayer: Person
----@field role {category: string, variable: string, isplayer: boolean?}?
----@field role2 {category: string, variable: string, isplayer: boolean?}?
 local CustomPlayer = Class.new(Player)
 local CustomInjector = Class.new(Injector)
 
@@ -87,8 +61,6 @@ function CustomPlayer.run(frame)
 	player.args.banned = tostring(player.args.banned or '')
 
 	player.args.autoTeam = true
-	player.role = player:_getRoleData(player.args.role)
-	player.role2 = player:_getRoleData(player.args.role2)
 
 	return player:createInfobox()
 end
@@ -131,59 +103,19 @@ function CustomInjector:parse(id, widgets)
 			Cell{name = 'Years Active (Talent)', content = {args.years_active_talent}},
 			Cell{name = 'Time Banned', content = {args.time_banned}},
 		}
-	elseif id == 'role' then
-		return {
-			Cell{name = 'Role', content = {
-				caller:_displayRole(caller.role),
-				caller:_displayRole(caller.role2),
-			}},
-		}
 	elseif id == 'history' then
 		table.insert(widgets, Cell{name = 'Retired', content = {args.retired}})
 	end
 	return widgets
 end
 
----@param role string?
----@return {category: string, variable: string, isplayer: boolean?}?
-function CustomPlayer:_getRoleData(role)
-	return ROLES[(role or ''):lower()]
-end
-
----@param roleData {category: string, variable: string, isplayer: boolean?}?
----@return string?
-function CustomPlayer:_displayRole(roleData)
-	if not roleData then return end
-
-	return Page.makeInternalLink(roleData.variable, ':Category:' .. roleData.category)
-end
-
----@param args table
-function CustomPlayer:defineCustomPageVariables(args)
-	Variables.varDefine('role', (self.role or {}).variable)
-	Variables.varDefine('role2', (self.role2 or {}).variable)
-end
-
----@param categories string[]
----@return string[]
-function CustomPlayer:getWikiCategories(categories)
-	return Array.append(categories,
-		(self.role or {}).category,
-		(self.role2 or {}).category
-	)
-end
-
 ---@param lpdbData table
 ---@param args table
----@param personType string
 ---@return table
-function CustomPlayer:adjustLPDB(lpdbData, args, personType)
-	lpdbData.extradata.role = (self.role or {}).variable
-	lpdbData.extradata.role2 = (self.role2 or {}).variable
+function CustomPlayer:adjustLPDB(lpdbData, args)
 	for _, operator, operatorIndex in Table.iter.pairsByPrefix(args, 'operator', {requireIndex = false}) do
 		lpdbData.extradata['signatureOperator' .. operatorIndex] = CharacterNames[operator:lower()]
 	end
-	lpdbData.type = self:_isPlayerOrStaff()
 
 	lpdbData.region = Template.safeExpand(mw.getCurrentFrame(), 'Player region', {args.country})
 
@@ -226,16 +158,6 @@ function CustomPlayer:_getStatusContents()
 			end
 		)
 	)
-end
-
----@return string
-function CustomPlayer:_isPlayerOrStaff()
-	-- If the role is missing, assume it is a player
-	if self.role and self.role.isplayer == false then
-		return 'staff'
-	else
-		return 'player'
-	end
 end
 
 return CustomPlayer
