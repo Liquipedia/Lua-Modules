48b9077d5543c951884c7e93344fbe7c6f4a9e32
hjpalpha <andreasbuesch1@gmail.com>
2025-04-15 18:04:47 +0200

    one more unused import

--- lua/wikis/commons/YearsActive.lua
+++ lua/wikis/commons/YearsActive.lua
@@ -6,7 +6,6 @@
 -- Please see https://github.com/Liquipedia/Lua-Modules to contribute
 --
 
-local Class = require('Module:Class')
 local Lua = require('Module:Lua')
 
 local CustomActiveYears = Lua.import('Module:YearsActive/Base')
