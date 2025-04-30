5f86cc7beed3e31f91a4c895b109824f70f26823
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2025-04-30 22:55:20 +0900

    fix(match2): lol matchpage does not display game length for bo1 matches (#5878)
    * add length display
    
    * refactor
    
    * adjust stylesheet

--- lua/wikis/leagueoflegends/MatchPage.lua
+++ lua/wikis/leagueoflegends/MatchPage.lua
@@ -178,6 +178,47 @@ end
 
 ---@private
 ---@param game LoLMatchPageGame
+---@return Widget[]
+function MatchPage:_buildGameResultSummary(game)
+	return {
+		Div{
+			classes = {'match-bm-lol-game-summary-faction'},
+			children = game.teams[1].side and IconImage{
+				imageLight = 'Lol faction ' .. game.teams[1].side .. '.png',
+				link = '',
+				caption = game.teams[1].side .. ' side'
+			} or nil
+		},
+		Div{
+			classes = {'match-bm-lol-game-summary-score-holder'},
+			children = game.finished and {
+				Div{
+					classes = {'match-bm-lol-game-summary-score'},
+					children = {
+						game.teams[1].scoreDisplay,
+						'&ndash;',
+						game.teams[2].scoreDisplay
+					}
+				},
+				Div{
+					classes = {'match-bm-lol-game-summary-length'},
+					children = game.length
+				}
+			} or nil
+		},
+		Div{
+			classes = {'match-bm-lol-game-summary-faction'},
+			children = game.teams[2].side and IconImage{
+				imageLight = 'Lol faction ' .. game.teams[2].side .. '.png',
+				link = '',
+				caption = game.teams[2].side .. ' side'
+			} or nil
+		}
+	}
+end
+
+---@private
+---@param game LoLMatchPageGame
 ---@return Widget?
 function MatchPage:_renderGameOverview(game)
 	if self:isBestOfOne() then return end
@@ -193,41 +234,7 @@ function MatchPage:_renderGameOverview(game)
 					},
 					Div{
 						classes = {'match-bm-lol-game-summary-center'},
-						children = {
-							Div{
-								classes = {'match-bm-lol-game-summary-faction'},
-								children = game.teams[1].side and IconImage{
-									imageLight = 'Lol faction ' .. game.teams[1].side .. '.png',
-									link = '',
-									caption = game.teams[1].side .. ' side'
-								} or nil
-							},
-							Div{
-								classes = {'match-bm-lol-game-summary-score-holder'},
-								children = game.finished and {
-									Div{
-										classes = {'match-bm-lol-game-summary-score'},
-										children = {
-											game.teams[1].scoreDisplay,
-											'&ndash;',
-											game.teams[2].scoreDisplay
-										}
-									},
-									Div{
-										classes = {'match-bm-lol-game-summary-length'},
-										children = game.length
-									}
-								} or nil
-							},
-							Div{
-								classes = {'match-bm-lol-game-summary-faction'},
-								children = game.teams[2].side and IconImage{
-									imageLight = 'Lol faction ' .. game.teams[2].side .. '.png',
-									link = '',
-									caption = game.teams[2].side .. ' side'
-								} or nil
-							}
-						}
+						children = self:_buildGameResultSummary(game)
 					},
 					Div{
 						classes = {'match-bm-lol-game-summary-team'},
@@ -371,7 +378,10 @@ function MatchPage:_renderTeamStats(game)
 							classes = {'match-bm-lol-team-stats-header-team'},
 							children = self.opponents[1].iconDisplay
 						},
-						Div{classes = {'match-bm-team-stats-list-cell'}},
+						Div{
+							classes = {'match-bm-team-stats-list-cell'},
+							children = self:isBestOfOne() and self:_buildGameResultSummary(game) or nil
+						},
 						Div{
 							classes = {'match-bm-lol-team-stats-header-team'},
 							children = self.opponents[2].iconDisplay
