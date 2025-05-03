e453bb6d6eec69c2c7dc57652a397c0c282a4aba
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2025-05-03 15:07:54 +0900

    fix header

--- lua/wikis/commons/Widget/Infobox/UpcomingTournaments/Row/Custom.lua
+++ lua/wikis/commons/Widget/Infobox/UpcomingTournaments/Row/Custom.lua
@@ -1,7 +1,7 @@
 ---
 -- @Liquipedia
 -- wiki=commons
--- page=Module:Widget/Infobox/UpcomingTournaments/Row
+-- page=Module:Widget/Infobox/UpcomingTournaments/Row/Custom
 --
 -- Please see https://github.com/Liquipedia/Lua-Modules to contribute
 --
