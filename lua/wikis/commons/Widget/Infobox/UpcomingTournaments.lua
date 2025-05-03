259dfc5f5107389a63b6a3c6dfad5d371c970caf
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2025-05-03 15:07:04 +0900

    tie everything together

--- lua/wikis/commons/Widget/Infobox/UpcomingTournaments.lua
+++ lua/wikis/commons/Widget/Infobox/UpcomingTournaments.lua
@@ -6,17 +6,30 @@
 -- Please see https://github.com/Liquipedia/Lua-Modules to contribute
 --
 
-local Class = require('Module:Class')
 local Lua = require('Module:Lua')
+
+local Array = Lua.import('Module:Array')
+local Class = Lua.import('Module:Class')
+local DateExt = Lua.import('Module:Date/Ext')
+local Logic = Lua.import('Module:Logic')
+
+local Condition = Lua.import('Module:Condition')
+local ConditionTree = Condition.Tree
+local ConditionNode = Condition.Node
+local Comparator = Condition.Comparator
+local BooleanOperator = Condition.BooleanOperator
+local ColumnName = Condition.ColumnName
 
 local Widget = Lua.import('Module:Widget')
 local HtmlWidgets = Lua.import('Module:Widget/Html/All')
 local Div = HtmlWidgets.Div
 local Header = Lua.import('Module:Widget/Infobox/UpcomingTournaments/Header')
+local Row = Lua.import('Module:Widget/Infobox/UpcomingTournaments/Row')
 local WidgetUtil = Lua.import('Module:Widget/Util')
 
 ---@class UpcomingTournaments: Widget
 ---@operator call(table): UpcomingTournaments
+---@field props {name: string}
 local UpcomingTournaments = Class.new(Widget)
 
 ---@return Widget
@@ -25,9 +38,43 @@ function UpcomingTournaments:render()
 		classes = {'fo-nttax-infobox', 'wiki-bordercolor-light', 'noincludereddit'},
 		css = {['border-top'] = 'none'},
 		children = WidgetUtil.collect(
-			Header{}
+			Header{},
+			self:_getTournaments()
 		)
 	}
+end
+
+---@private
+---@return Widget|Widget[]
+function UpcomingTournaments:_getTournaments()
+	local conditions = ConditionTree(BooleanOperator.all)
+		:add(ConditionNode(ColumnName('opponentname'), Comparator.eq, self.props.name))
+		:add(ConditionNode(ColumnName('date'), Comparator.gt, DateExt.getCurrentTimestamp() - 86400))
+		:add(ConditionNode(ColumnName('placement'), Comparator.eq, ''))
+
+	local placements = mw.ext.LiquipediaDB.lpdb('placement', {
+		conditons = conditions:toString(),
+		limit = 1000,
+		order = 'startdate asc',
+		query = 'tournament, date, startdate, pagename, icon, icondark, publishertier, extradata'
+	})
+
+	if Logic.isEmpty(placements) then
+		return Div{
+			classes = {'text-center'},
+			css = {
+				display = 'flex',
+				height = '3.75rem',
+				['align-items'] = 'center',
+				['flex-direction'] = 'row',
+				['justify-content'] = 'center'
+			},
+			children = 'No Upcoming Tournaments'
+		}
+	end
+	return Array.map(placements, function (placement)
+		return Row{data = placement}
+	end)
 end
 
 return UpcomingTournaments
