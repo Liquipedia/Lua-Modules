695f55a6e00a5434cadea0ca2e60f8a68a50f6c8
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2025-05-08 12:40:44 +0900

    update remaining wikis

--- lua/wikis/apexlegends/MainPageLayout/data.lua
+++ lua/wikis/apexlegends/MainPageLayout/data.lua
@@ -19,6 +19,7 @@ local LiquipediaApp = Lua.import('Module:Widget/MainPage/LiquipediaApp')
 local MatchTicker = Lua.import('Module:Widget/MainPage/MatchTicker')
 local ThisDayWidgets = Lua.import('Module:Widget/MainPage/ThisDay')
 local TransfersList = Lua.import('Module:Widget/MainPage/TransfersList')
+local WantToHelp = Lua.import('Module:Widget/MainPage/WantToHelp')
 
 local CONTENT = {
 	usefulArticles = {
@@ -29,7 +30,7 @@ local CONTENT = {
 	},
 	wantToHelp = {
 		heading = 'Want To Help?',
-		body = '{{Liquipedia:Want_to_help}}',
+		body = WantToHelp{},
 		padding = true,
 		boxid = 1504,
 	},
