ee1e26990c9b345436f9971747d57ebffffb5501
Eetu Rantanen <eeturantanen.dev@gmail.com>
2025-05-13 15:09:47 +0300

    removed unused locals

--- lua/wikis/smite/Infobox/Person/Player/Custom.lua
+++ lua/wikis/smite/Infobox/Person/Player/Custom.lua
@@ -6,17 +6,12 @@
 -- Please see https://github.com/Liquipedia/Lua-Modules to contribute
 --
 
-local Array = require('Module:Array')
 local Class = require('Module:Class')
 local Lua = require('Module:Lua')
-local Page = require('Module:Page')
 local TeamHistoryAuto = require('Module:TeamHistoryAuto')
 
 local Injector = Lua.import('Module:Widget/Injector')
 local Player = Lua.import('Module:Infobox/Person')
-
-local Widgets = require('Module:Widget/All')
-local Cell = Widgets.Cell
 
 ---@class SmiteInfoboxPlayer: Person
 local CustomPlayer = Class.new(Player)
