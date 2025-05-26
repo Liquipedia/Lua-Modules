26d3aa218ffe2d82365e5cb7bb563beae829c47f
Rikard Blixt <rikardblixt@gmail.com>
2025-05-26 09:18:06 +0200

    fix typo

--- lua/wikis/commons/Widget/Basic/DataTable.lua
+++ lua/wikis/commons/Widget/Basic/DataTable.lua
@@ -7,7 +7,7 @@
 --
 
 local Class = require('Module:Class')
-local Logic = require('Modukle:Logic')
+local Logic = require('Module:Logic')
 local Lua = require('Module:Lua')
 
 local Widget = Lua.import('Module:Widget')
