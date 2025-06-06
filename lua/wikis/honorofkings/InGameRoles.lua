1810d4edea20d0e4c93e892787a1988081312956
Rikard Blixt <rikardblixt@gmail.com>
2025-06-06 15:24:34 +0200

    some hok data update

--- lua/wikis/honorofkings/InGameRoles.lua
+++ lua/wikis/honorofkings/InGameRoles.lua
@@ -8,27 +8,31 @@
 
 ---@type PersonRoleData
 local inGameRoles = {
-	['igl'] = {category = 'In-game leaders', display = 'In-game Leader'},
 	['top'] = {category = 'Top Laners', display = 'Top Lane'},
 	['bottom'] = {category = 'Bottom Laners', display = 'Bottom Lane'},
 	['mid'] = {category = 'Mid Laners', display = 'Mid Lane'},
 	['side lane'] = {category = 'Side Laners', display = 'Side Lane'},
 	['jungler'] = {category = 'Junglers', display = 'Jungler'},
-	['support'] = {category = 'Supporters', display = 'Roamer'},
+	['roamer'] = {category = 'Roamers', display = 'Roamer'},
 }
 
 inGameRoles['jgl'] = inGameRoles.jungler
 inGameRoles['jungle'] = inGameRoles.jungler
-inGameRoles['roam'] = inGameRoles.support
-inGameRoles['sup'] = inGameRoles.support
+inGameRoles['support'] = inGameRoles.roamer
+inGameRoles['roam'] = inGameRoles.roamer
+inGameRoles['sup'] = inGameRoles.roamer
 inGameRoles['ad lane'] = inGameRoles.bottom
 inGameRoles['ad carry'] = inGameRoles.bottom
+inGameRoles['abyssal dragon lane'] = inGameRoles.bottom
 inGameRoles['adl'] = inGameRoles.bottom
 inGameRoles['adc'] = inGameRoles.bottom
 inGameRoles['bot'] = inGameRoles.bottom
 inGameRoles['carry'] = inGameRoles.bottom
+inGameRoles['farm'] = inGameRoles.bottom
 inGameRoles['mid laner'] = inGameRoles.mid
 inGameRoles['ds lane'] = inGameRoles.top
 inGameRoles['dsl'] = inGameRoles.top
+inGameRoles['dark slayer lane'] = inGameRoles.top
+inGameRoles['confront'] = inGameRoles.top
 
 return inGameRoles
