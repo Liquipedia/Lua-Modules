cf166db70cdcb1e64d6ed365dd9eeb4fe8289450
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2025-06-11 17:06:17 +0900

    refactor(match2): clean up mapveto start widget (#6005)
    * use fontawesome for start arrow
    
    * refactor start veto text
    
    * use I18n for veto text

--- lua/wikis/commons/Widget/Match/Summary/MapVetoStart.lua
+++ lua/wikis/commons/Widget/Match/Summary/MapVetoStart.lua
@@ -8,13 +8,16 @@
 
 local Array = require('Module:Array')
 local Class = require('Module:Class')
+local I18n = require('Module:I18n')
 local Lua = require('Module:Lua')
 
 local Widget = Lua.import('Module:Widget')
 local HtmlWidgets = Lua.import('Module:Widget/Html/All')
+local IconFa = Lua.import('Module:Widget/Image/Icon/Fontawesome')
 
-local ARROW_LEFT = '[[File:Arrow sans left.svg|15x15px|link=|Left team starts]]'
-local ARROW_RIGHT = '[[File:Arrow sans right.svg|15x15px|link=|Right team starts]]'
+local ARROW_LEFT = IconFa{iconName = 'startleft', size = '110%'}
+local ARROW_RIGHT = IconFa{iconName = 'startright', size = '110%'}
+local START_MAP_VETO = HtmlWidgets.B{children = I18n.translate('matchsummary-mapveto-start')}
 
 ---@class MatchSummaryMapVetoStart: Widget
 ---@operator call(table): MatchSummaryMapVetoStart
@@ -30,7 +33,7 @@ function MatchSummaryMapVetoStart:render()
 	local children = {}
 	if self.props.firstVeto == 1 then
 		children = {
-			'<b>Start Map Veto</b>',
+			START_MAP_VETO,
 			ARROW_LEFT,
 			format,
 		}
@@ -38,7 +41,7 @@ function MatchSummaryMapVetoStart:render()
 		children = {
 			format,
 			ARROW_RIGHT,
-			'<b>Start Map Veto</b>',
+			START_MAP_VETO,
 		}
 	end
 
