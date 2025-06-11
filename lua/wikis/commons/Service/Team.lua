367e4d505f29171d2bc6c3259052a14da1dd17d6
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2025-06-11 11:18:32 +0200

    Apply suggestions from code review
    Co-authored-by: Rikard Blixt <rikardblixt@gmail.com>

--- lua/wikis/commons/Service/Team.lua
+++ lua/wikis/commons/Service/Team.lua
@@ -1,6 +1,5 @@
 ---
 -- @Liquipedia
--- wiki=commons
 -- page=Module:Service/Team
 --
 -- Please see https://github.com/Liquipedia/Lua-Modules to contribute
--- lua/wikis/commons/Widget/NavBox/AutoTeam/Player.lua
+++ lua/wikis/commons/Widget/NavBox/AutoTeam/Player.lua
@@ -1,6 +1,5 @@
 ---
 -- @Liquipedia
--- wiki=commons
 -- page=Module:Widget/NavBox/AutoTeam/Player
 --
 -- Please see https://github.com/Liquipedia/Lua-Modules to contribute
