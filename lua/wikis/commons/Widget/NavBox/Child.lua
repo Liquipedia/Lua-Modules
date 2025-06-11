468a72417b454e42186c96be89e7650e28bdfe6e
hjpalpha <andreasbuesch1@gmail.com>
2025-06-11 07:42:02 +0200

    feat((navBox): allow empty child overwrite

--- lua/wikis/commons/Widget/NavBox/Child.lua
+++ lua/wikis/commons/Widget/NavBox/Child.lua
@@ -45,7 +45,7 @@ NavBoxChild.defaultProps = {
 function NavBoxChild:render()
 	local props = self.props
 
-	assert(props[1] or props.child1, EMPTY_CHILD_ERROR)
+	assert(Logic.readBool(props.allowEmpty) or props[1] or props.child1, EMPTY_CHILD_ERROR)
 
 	local listElements = Array.mapIndexes(function(index)
 		return self.props[index]
