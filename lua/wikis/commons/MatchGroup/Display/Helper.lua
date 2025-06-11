0ce472dd34397fc396327b180222d6ca8401388b
Rikard Blixt <rikardblixt@gmail.com>
2025-06-11 11:06:09 +0200

    feat(match2): always enable match popups on valorant (#6007)
    * feat(match2): always enable match popups on valorant
    
    * Update lua/wikis/commons/MatchGroup/Display/Helper.lua
    
    Co-authored-by: SyntacticSalt <mail@mbergen.de>
    
    ---------
    
    Co-authored-by: SyntacticSalt <mail@mbergen.de>

--- lua/wikis/commons/MatchGroup/Display/Helper.lua
+++ lua/wikis/commons/MatchGroup/Display/Helper.lua
@@ -98,9 +98,10 @@ function DisplayHelper.defaultMatchHasDetails(match)
 	return match.dateIsExact
 		or (match.timestamp and match.timestamp ~= Date.defaultTimestamp)
 		or Logic.isNotEmpty(match.vod)
-		or not Table.isEmpty(match.links)
+		or Table.isNotEmpty(match.links)
 		or Logic.isNotEmpty(match.comment)
 		or 0 < #match.games
+		or Info.config.match2.matchPage
 end
 
 -- Display component showing the streams, date, and countdown of a match.
