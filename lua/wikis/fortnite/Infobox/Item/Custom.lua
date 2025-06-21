5bfe6588e9d07980dc626b105e5658f452609820
hjpalpha <andreasbuesch1@gmail.com>
2025-06-21 15:22:16 +0200

    slice version display into extension

--- lua/wikis/fortnite/Infobox/Item/Custom.lua
+++ lua/wikis/fortnite/Infobox/Item/Custom.lua
@@ -11,35 +11,13 @@ local Array = Lua.import('Module:Array')
 local Class = Lua.import('Module:Class')
 local Logic = Lua.import('Module:Logic')
 local Variables = Lua.import('Module:Variables')
+local VersionDisplay = Lua.import('Infobox/Extension/VersionDisplay')
 
 local Injector = Lua.import('Module:Infobox/Widget/Injector')
 local Item = Lua.import('Module:Infobox/Item')
 
-local HtmlWidgets = Lua.import('Module:Widget/Html/All')
-local Link = Lua.import('Module:Widget/Basic/Link')
 local Widgets = Lua.import('Module:Infobox/Widget/All')
 local Cell = Widgets.Cell
-
-local VERSION_DATA = {
-	['4.5'] = {version = '4.5', date = '2018-07-03'},
-	['5.0'] = {version = '5.0', date = '2018-07-17'},
-	['5.10'] = {version = '5.10', date = '2018-07-24'},
-	['5.10 (Content Update)'] = {version = '5.10 (Content Update)', date = '2018-07-31'},
-	['5.20'] = {version = '5.20', date = '2018-08-07'},
-	['5.21'] = {version = '5.21', date = '2018-08-15'},
-	['5.30'] = {version = '5.30', date = '2018-08-23'},
-	['5.30 (Content Update)'] = {version = '5.30 (Content Update)', date = '2018-08-28'},
-	['5.40'] = {version = '5.40', date = '2018-09-06'},
-}
-VERSION_DATA['2018-07-03'] = VERSION_DATA['4.5']
-VERSION_DATA['2018-07-17'] = VERSION_DATA['5.0']
-VERSION_DATA['2018-07-24'] = VERSION_DATA['5.10']
-VERSION_DATA['2018-07-31'] = VERSION_DATA['5.10 (Content Update)']
-VERSION_DATA['2018-08-07'] = VERSION_DATA['5.20']
-VERSION_DATA['2018-08-15'] = VERSION_DATA['5.21']
-VERSION_DATA['2018-08-23'] = VERSION_DATA['5.30']
-VERSION_DATA['2018-08-28'] = VERSION_DATA['5.30 (Content Update)']
-VERSION_DATA['2018-09-06'] = VERSION_DATA['5.40']
 
 ---@class FortniteItemInfobox: ItemInfobox
 local CustomItem = Class.new(Item)
@@ -68,39 +46,12 @@ function CustomInjector:parse(id, widgets)
 	local args = self.caller.args
 	if id == 'released' then
 		return {
-			Cell{name = 'Released', content = {CustomItem._getVersionDisplay(args.release)}},
-			Cell{name = 'Removed', children = {CustomItem._getVersionDisplay(args.removed)}},
+			Cell{name = 'Released', content = {VersionDisplay.run(args.release)}},
+			Cell{name = 'Removed', children = {VersionDisplay.run(args.removed)}},
 		}
 	end
 
 	return widgets
-end
-
----@private
----@param input string?
----@return Widget|string?
-function CustomItem._getVersionDisplay(input)
-	local versionData = VERSION_DATA[input]
-	if not versionData then return input end
-
-	return HtmlWidgets.Fragment{
-		children = {
-			Link{
-				link = 'Version ' .. versionData.version,
-				children = {versionData.version},
-			},
-			'&nbsp;',
-			HtmlWidgets.I{
-				children = {HtmlWidgets.Small{
-					children = {
-						'(',
-						versionData.date,
-						')',
-					}
-				}}
-			}
-		}
-	}
 end
 
 ---@private
