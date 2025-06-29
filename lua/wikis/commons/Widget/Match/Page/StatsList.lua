bd849893bc7fa513ad5bd0529a3f9fe2a448c1f3
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2025-06-29 10:01:29 +0900

    suppress display of empty stats

--- lua/wikis/commons/Widget/Match/Page/StatsList.lua
+++ lua/wikis/commons/Widget/Match/Page/StatsList.lua
@@ -36,9 +36,14 @@ function MatchPageStatsList:render()
 	if Logic.isEmpty(self.props.data) then return end
 	return Div{
 		classes = {'match-bm-team-stats-list'},
-		children = Array.map(self.props.data, function (dataElement)
-			return self:_renderStat(dataElement)
-		end)
+		children = Array.map(
+			Array.filter(self.props.data, function (element)
+				return Logic.isNotEmpty(element.team1Value) and Logic.isNotEmpty(element.team2Value)
+			end),
+			function (dataElement)
+				return self:_renderStat(dataElement)
+			end
+		)
 	}
 end
 
