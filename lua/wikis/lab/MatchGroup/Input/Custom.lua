c88495f636ef095b26f3b5d4e161dc167bbbf14a
Jasper Jacobs <97167868+Kanopedia@users.noreply.github.com>
2025-07-03 15:45:38 +0200

    Update Custom.lua

--- lua/wikis/lab/MatchGroup/Input/Custom.lua
+++ lua/wikis/lab/MatchGroup/Input/Custom.lua
@@ -6,10 +6,8 @@
 --
 
 local Array = require('Module:Array')
-local Logic = require('Module:Logic')
 local Lua = require('Module:Lua')
 local Operator = require('Module:Operator')
-local Variables = require('Module:Variables')
 
 local MatchGroupInputUtil = Lua.import('Module:MatchGroup/Input/Util')
 
