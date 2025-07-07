d9b941b95c32528fe09bc2badaa55d03cd7f769a
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2025-07-07 13:23:37 +0200

    fix hasChronology

--- lua/wikis/commons/Infobox/Expansion.lua
+++ lua/wikis/commons/Infobox/Expansion.lua
@@ -168,7 +168,7 @@ end
 ---@param args table
 ---@return boolean
 function Expansion:hasChronology(args)
-	return Logic.isNotEmpty(args.previous) and Logic.isNotEmpty(args.next)
+	return Logic.isNotEmpty(args.previous) or Logic.isNotEmpty(args.next)
 end
 
 ---@return string
