688b40d67a5206607369be83fae668bbc58374d9
hjpalpha <andreasbuesch1@gmail.com>
2025-07-08 14:38:18 +0200

    add basic empty customs for the 4 remaining template infoboxes

---
+++ lua/wikis/hearthstone/Infobox/Unit/Custom.lua
@@ -0,0 +1,48 @@
+---
+-- @Liquipedia
+-- page=Module:Infobox/Unit/Custom
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Lua = require('Module:Lua')
+
+local Class = Lua.import('Module:Class')
+
+local Injector = Lua.import('Module:Widget/Injector')
+local Unit = Lua.import('Module:Infobox/Unit')
+
+local Widgets = Lua.import('Module:Widget/All')
+local Cell = Widgets.Cell
+
+---@class HearthstoneUnitInfobox: UnitInfobox
+local CustomUnit = Class.new(Unit)
+---@class HearthstoneUnitInfoboxWidgetInjector: WidgetInjector
+---@field caller HearthstoneUnitInfobox
+local CustomInjector = Class.new(Injector)
+
+---@param frame Frame
+---@return Html
+function CustomUnit.run(frame)
+	local unit = CustomUnit(frame)
+	unit:setWidgetInjector(CustomInjector(unit))
+
+	return unit:createInfobox()
+end
+
+---@param id string
+---@param widgets Widget[]
+---@return Widget[]
+function CustomInjector:parse(id, widgets)
+	local args = self.caller.args
+
+	if id == 'custom' then
+		return {
+			Cell{name = 'label', children = {}},
+		}
+	end
+
+	return widgets
+end
+
+return CustomUnit
