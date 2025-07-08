9f19a5fbb7084029da7c5ff28042427e0938d12a
Jasper Jacobs <97167868+Kanopedia@users.noreply.github.com>
2025-07-08 11:39:09 +0200

    Set Up Info for Lab wiki

---
+++ lua/wikis/lab/Info.lua
@@ -0,0 +1,39 @@
+---
+-- @Liquipedia
+-- page=Module:Info
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+return {
+	startYear = 1800,
+	wikiName = 'lab',
+	name = 'Lab',
+	defaultGame = 'lab',
+	games = {
+		rematch = {
+			abbreviation = 'Lab',
+			name = 'Lab',
+			link = 'Lab',
+			logo = {
+				darkMode = 'Halved Shield default darkmode.png',
+				lightMode = 'Halved Shield default lightmode.png',
+			},
+			defaultTeamLogo = {
+				darkMode = 'Halved Shield default darkmode.png',
+				lightMode = 'Halved Shield default lightmode.png',
+			},
+		},
+	},
+	config = {
+		squads = {
+			hasPosition = false,
+			hasSpecialTeam = false,
+			allowManual = false,
+		},
+		match2 = {
+			status = 0,
+			matchWidth = 180,
+		},
+	},
+}
