d920d19e2f94047936d64212baf4dd4ba4b6100c
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2025-07-09 08:13:31 +0200

    Update Custom.lua

--- lua/wikis/valorant/Infobox/Series/Custom.lua
+++ lua/wikis/valorant/Infobox/Series/Custom.lua
@@ -5,15 +5,16 @@
 -- Please see https://github.com/Liquipedia/Lua-Modules to contribute
 --
 
-local Class = require('Module:Class')
-local Flags = require('Module:Flags')
 local Lua = require('Module:Lua')
-local Table = require('Module:Table')
+
+local Class = Lua.import('Module:Class')
+local Flags = Lua.import('Module:Flags')
+local Table = Lua.import('Module:Table')
 
 local Injector = Lua.import('Module:Widget/Injector')
 local Series = Lua.import('Module:Infobox/Series')
 
-local Widgets = require('Module:Widget/All')
+local Widgets = Lua.import('Module:Widget/All')
 local Cell = Widgets.Cell
 local Chronology = Widgets.Chronology
 
