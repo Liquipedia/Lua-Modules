9f56c121a706af8ffed5526b89dd9a0d2ff149c7
hjpalpha <andreasbuesch1@gmail.com>
2025-07-10 18:05:56 +0200

    as per adjust in #6109

--- lua/wikis/commons/Infobox/Event.lua
+++ lua/wikis/commons/Infobox/Event.lua
@@ -129,11 +129,7 @@ function Event:createInfobox()
 		Customizable{id = 'customcontent', children = {}},
 		Center{children = {args.footnotes}},
 		Customizable{id = 'chronology', children = {
-			Chronology{
-				links = Table.filterByKey(args, function(key)
-					return type(key) == 'string' and (key:match('^previous%d?$') ~= nil or key:match('^next%d?$') ~= nil)
-				end)
-			}
+			Chronology{args = args}
 		}},
 		Accommodation{
 			args = args,
