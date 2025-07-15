f9616167d0dc3d2bdbacac879bd0b4cfe5134522
Hesketh2 <88981446+Hesketh2@users.noreply.github.com>
2025-07-15 21:23:45 +0700

    including changes from #6118
    6118 was a separate PR  of the same module, I didnt aware it was made. It had differences that doesnt include my platform display so this is my attempt at merging both

--- lua/wikis/rematch/Infobox/League/Custom.lua
+++ lua/wikis/rematch/Infobox/League/Custom.lua
@@ -5,18 +5,22 @@
 -- Please see https://github.com/Liquipedia/Lua-Modules to contribute
 --
 
-local Array = require('Module:Array')
-local Class = require('Module:Class')
 local Lua = require('Module:Lua')
+local Array = Lua.import('Module:Array')
+local Class = Lua.import('Module:Class')
+local String = Lua.import('Module:StringUtils')
 
 local Injector = Lua.import('Module:Widget/Injector')
 local League = Lua.import('Module:Infobox/League')
 
-local Widgets = require('Module:Widget/All')
+local Widgets = Lua.import('Module:Widget/All')
 local Cell = Widgets.Cell
+local Title = Widgets.Title
 
 ---@class RematchLeagueInfobox: InfoboxLeague
 local CustomLeague = Class.new(League)
+---@class RematchLeagueInfoboxWidgetInjector: WidgetInjector
+---@field caller RematchLeagueInfobox
 local CustomInjector = Class.new(Injector)
 
 -- Platform: Platform that the tournament is on
@@ -45,6 +49,7 @@ end
 ---@param widgets Widget[]
 ---@return Widget[]
 function CustomInjector:parse(id, widgets)
+	local args = self.caller.args
 	local caller = self.caller
 
 	if id == 'custom' then
@@ -52,8 +57,19 @@ function CustomInjector:parse(id, widgets)
 			widgets,
 			Cell{name = 'Platform', content = {caller.data.platform}}
 		)
+		elseif id == 'customcontent' then
+			if String.isNotEmpty(args.team_number) then
+			Array.appendWith(widgets,
+				Title{children = 'Teams'},
+				Cell{name = 'Number of teams', content = {args.team_number}}
+			)
+			elseif String.isNotEmpty(args.player_number) then
+			Array.appendWith(widgets,
+				Title{children = 'Players'},
+				Cell{name = 'Number of players', content = {args.player_number}}
+			)
+		end
 	end
-
 	return widgets
 end
 
