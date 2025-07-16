62aba7415816d330f41770fc2a942460b7cdb126
Rikard Blixt <rikardblixt@gmail.com>
2025-07-16 16:10:10 +0200

    feat: remove countdown from matchticker completed (#6135)
    * feat: remove countdown from matchticker completed
    
    * cleanup
    
    * tweak hidden target

--- lua/wikis/apexlegends/MainPageLayout/data.lua
+++ lua/wikis/apexlegends/MainPageLayout/data.lua
@@ -71,9 +71,6 @@ local CONTENT = {
 		body = MatchTicker{},
 		padding = true,
 		boxid = 1507,
-		panelAttributes = {
-			['data-switch-group-container'] = 'countdown',
-		},
 	},
 	tournaments = {
 		heading = 'Tournaments',
--- lua/wikis/commons/Widget/Match/Ticker/Container.lua
+++ lua/wikis/commons/Widget/Match/Ticker/Container.lua
@@ -116,29 +116,32 @@ function MatchTickerContainer:render()
 				},
 			},
 			HtmlWidgets.Div{
-				classes = {'switch-toggle-container'},
-				css = {margin = '1rem 0'},
-				children = {
-					HtmlWidgets.Div{
-						classes = {'switch-toggle'},
-						attributes = {
-							['data-switch-group'] = 'countdown',
-							['data-store-value'] = 'true',
-						},
-						children = {
-							HtmlWidgets.Div{classes = {'switch-toggle-slider'}},
-						},
-					},
-					HtmlWidgets.Div{children = 'Show Countdown'},
-				},
-			},
-			HtmlWidgets.Div{
 				attributes = {
+					['data-switch-group-container'] = 'countdown',
 					['data-toggle-area-content'] = '1',
 					['data-filter-expansion-template'] = buildTemplateExpansionString('upcoming'),
 					['data-filter-groups'] = filterText,
 				},
-				children = callTemplate('upcoming'),
+				children = {
+					HtmlWidgets.Div{
+						classes = {'switch-toggle-container'},
+						css = {margin = '1rem 0'},
+						children = {
+							HtmlWidgets.Div{
+								classes = {'switch-toggle'},
+								attributes = {
+									['data-switch-group'] = 'countdown',
+									['data-store-value'] = 'true',
+								},
+								children = {
+									HtmlWidgets.Div{classes = {'switch-toggle-slider'}},
+								},
+							},
+							HtmlWidgets.Div{children = 'Show Countdown'},
+						},
+					},
+					callTemplate('upcoming'),
+				}
 			},
 			HtmlWidgets.Div{
 				attributes = {
