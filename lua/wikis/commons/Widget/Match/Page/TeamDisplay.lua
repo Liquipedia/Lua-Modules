c35bee52ec0ef6934de12407afb2b80c486e07cc
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2025-07-22 15:53:12 +0900

    fix(match2): TBD opponent points to a link on match pages (#6174)
    * suppress link for TBD opponent
    
    * update type annotation

--- lua/wikis/commons/Widget/Match/Page/TeamDisplay.lua
+++ lua/wikis/commons/Widget/Match/Page/TeamDisplay.lua
@@ -54,7 +54,7 @@ function MatchPageTeamDisplay:render()
 end
 
 ---@private
----@return Widget|(string|Widget)[]?
+---@return Widget|Widget[]?
 function MatchPageTeamDisplay:_buildChildren()
 	local opponent = self.props.opponent
 	if Opponent.isEmpty(opponent) then return
@@ -66,6 +66,7 @@ function MatchPageTeamDisplay:_buildChildren()
 	end
 	local data = self.props.opponent.teamTemplateData
 	assert(data, TeamTemplate.noTeamMessage(opponent.template))
+	local hideLink = Opponent.isTbd(opponent)
 	return {
 		opponent.iconDisplay,
 		Div{
@@ -73,11 +74,11 @@ function MatchPageTeamDisplay:_buildChildren()
 			children = {
 				Div{
 					classes = { 'match-bm-match-header-team-long' },
-					children = { Link{ link = data.page, children = data.name } }
+					children = { hideLink and data.name or Link{ link = data.page, children = data.name } }
 				},
 				Div{
 					classes = { 'match-bm-match-header-team-short' },
-					children = { Link{ link = data.page, children = data.shortname } }
+					children = { hideLink and data.shortname or Link{ link = data.page, children = data.shortname } }
 				},
 				Div{
 					classes = { 'match-bm-match-header-round-results' },
