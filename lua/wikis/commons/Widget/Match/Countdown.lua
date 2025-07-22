01e72d729f2ae852ebc8e2961c91166e933efe90
Rikard Blixt <rikardblixt@gmail.com>
2025-07-22 15:29:11 +0200

    refactor countdow nargs

--- lua/wikis/commons/Widget/Match/Countdown.lua
+++ lua/wikis/commons/Widget/Match/Countdown.lua
@@ -10,12 +10,9 @@ local Lua = require('Module:Lua')
 local Class = Lua.import('Module:Class')
 local Countdown = Lua.import('Module:Countdown')
 local DateExt = Lua.import('Module:Date/Ext')
-local Timezone = Lua.import('Module:Timezone')
 
 local Widget = Lua.import('Module:Widget')
 local HtmlWidgets = Lua.import('Module:Widget/Html/All')
-
-local UTC = Timezone.getTimezoneString{timezone = 'UTC'}
 
 ---@class MatchCountdownProps
 ---@field match MatchGroupUtilMatch
@@ -32,26 +29,15 @@ function MatchCountdown:render()
 		return nil
 	end
 
-	if match.timestamp == DateExt.defaultTimestamp then
+	if not match.timestamp or match.timestamp == DateExt.defaultTimestamp then
 		return nil
-	end
-
-	local function dateTimeDisplay()
-		local baseTimestamp = match.timestamp or DateExt.readTimestamp(match.date)
-		if match.dateIsExact then
-			local timestamp = baseTimestamp + (Timezone.getOffset{timezone = match.extradata.timezoneid} or 0)
-			local timezoneString = Timezone.getTimezoneString{timezone = match.extradata.timezoneid} or UTC
-			return DateExt.formatTimestamp('F j, Y - H:i', timestamp) .. ' ' .. timezoneString
-		else
-			return DateExt.formatTimestamp('F j, Y', baseTimestamp) .. ' ' .. UTC
-		end
 	end
 
 	return HtmlWidgets.Span{
 		classes = {'match-info-countdown'},
 		children = Countdown._create{
 			rawdatetime = match.finished,
-			date = dateTimeDisplay(),
+			date = DateExt.toCountdownArg(match.timestamp, match.extradata.timezoneid, match.dateIsExact),
 			finished = match.finished,
 		},
 	}
