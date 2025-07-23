4ab5f01cfb5d04f7a427772442b00723beae51c6
mbergen <mail@mbergen.de>
2025-07-23 17:07:15 +0200

    Create new table

--- lua/wikis/commons/MatchTicker.lua
+++ lua/wikis/commons/MatchTicker.lua
@@ -453,7 +453,7 @@ function MatchTicker:expandGamesOfMatch(match)
 		gameMatch.opponents = Array.map(match.opponents, function(opponent, opponentIndex)
 			return MatchUtil.enrichGameOpponentFromMatchOpponent(opponent, game.opponents[opponentIndex])
 		end)
-		Table.mergeInto(gameMatch.extradata, game.extradata)
+		game.extradata = Table.merge(gameMatch.extradata, game.extradata)
 		return gameMatch
 	end)
 
