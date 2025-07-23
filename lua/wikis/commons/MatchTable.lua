8ba9980a60261dab8ae8661929f18550513c1a4b
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2025-07-23 20:21:05 +0200

    fix(matchTable): pass along match instead of just id

--- lua/wikis/commons/MatchTable.lua
+++ lua/wikis/commons/MatchTable.lua
@@ -871,7 +871,7 @@ end
 function MatchTable:_displayMatchPage(match)
 	if not self.config.showMatchPage then return end
 
-	return mw.html.create('td'):node(MatchPageButton{match = match.matchId})
+	return mw.html.create('td'):node(MatchPageButton{match = match})
 end
 
 ---@param winner any
