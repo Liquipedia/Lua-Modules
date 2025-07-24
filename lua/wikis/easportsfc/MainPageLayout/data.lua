63a75b337e09c9b94c43a2adb26ec2eba69bb6a6
Jasper Jacobs <97167868+Kanopedia@users.noreply.github.com>
2025-07-24 14:01:56 +0200

    Update data.lua

--- lua/wikis/easportsfc/MainPageLayout/data.lua
+++ lua/wikis/easportsfc/MainPageLayout/data.lua
@@ -4,6 +4,7 @@
 -- Please see https://github.com/Liquipedia/Lua-Modules to contribute
 --
 
+local DateExt = require('Module:Date/Ext')
 local Lua = require('Module:Lua')
 
 local FilterButtonsWidget = Lua.import('Module:Widget/FilterButtons')
