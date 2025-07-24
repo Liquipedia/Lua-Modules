bbaae9861949db037d038158c070200e92b75dbe
Hesketh2 <88981446+Hesketh2@users.noreply.github.com>
2025-07-24 16:04:57 +0700

    feat(infobox): Free Fire Infobox team cleanup (#6159)
    * feat(infobox): Free Fire Infobox league cleanup
    
    * 1st revision
    
    * Update Custom.lua
    
    * Added the suggested changes
    
    * Another set of changes
    
    * Update Custom.lua

--- lua/wikis/freefire/Infobox/Team/Custom.lua
+++ lua/wikis/freefire/Infobox/Team/Custom.lua
@@ -5,53 +5,37 @@
 -- Please see https://github.com/Liquipedia/Lua-Modules to contribute
 --
 
-local Class = require('Module:Class')
 local Lua = require('Module:Lua')
-local PlacementStats = require('Module:InfoboxPlacementStats')
-local Template = require('Module:Template')
 
-local Injector = Lua.import('Module:Widget/Injector')
+local Class = Lua.import('Module:Class')
+local PlacementStats = Lua.import('Module:InfoboxPlacementStats')
+
 local Team = Lua.import('Module:Infobox/Team')
 
-local Widgets = require('Module:Widget/All')
-local Cell = Widgets.Cell
+local HtmlWidgets = Lua.import('Module:Widget/Html/All')
+local UpcomingTournaments = Lua.import('Module:Widget/Infobox/UpcomingTournaments')
+local WidgetUtil = Lua.import('Module:Widget/Util')
 
----@class FreefireInfoboxTeam: InfoboxTeam
+---@class FreeFireInfoboxTeam: InfoboxTeam
 local CustomTeam = Class.new(Team)
-local CustomInjector = Class.new(Injector)
 
 ---@param frame Frame
 ---@return Html
 function CustomTeam.run(frame)
 	local team = CustomTeam(frame)
-	team:setWidgetInjector(CustomInjector(team))
 
 	return team:createInfobox()
 end
 
----@param id string
----@param widgets Widget[]
----@return Widget[]
-function CustomInjector:parse(id, widgets)
-	local args = self.caller.args
-	if id == 'staff' then
-		table.insert(widgets, 1, Cell{name = 'Founders', content = {args.founders}})
-		table.insert(widgets, 2, Cell{name = 'CEO', content = {args.ceo}})
-		table.insert(widgets, Cell{name = 'Analysts', content = {args.analysts}})
-	end
-	return widgets
-end
-
----@return string
+---@return Widget
 function CustomTeam:createBottomContent()
-	return tostring(PlacementStats.run{
-		tiers = {'1', '2', '3', '4'},
-		participant = self.name,
-	}) .. Template.expandTemplate(
-		mw.getCurrentFrame(),
-		'Upcoming and ongoing tournaments of',
-		{team = self.name}
-	)
+	return HtmlWidgets.Fragment{children = WidgetUtil.collect(
+		PlacementStats.run{
+			tiers = {'1', '2', '3', '4'},
+			participant = self.name,
+		},
+		not self.args.disbanded and UpcomingTournaments{name = self.pagename} or nil
+	)}
 end
 
 return CustomTeam
