3137baf1539d17a7eccc486574a69eb68471b99c
hjpalpha <andreasbuesch1@gmail.com>
2025-07-26 18:28:12 +0200

    simplify `Module:Opponent/Starcraft`

--- lua/wikis/commons/Opponent/Starcraft.lua
+++ lua/wikis/commons/Opponent/Starcraft.lua
@@ -9,18 +9,14 @@ local Lua = require('Module:Lua')
 
 local Faction = Lua.import('Module:Faction')
 local Logic = Lua.import('Module:Logic')
-local String = Lua.import('Module:StringUtils')
 local Table = Lua.import('Module:Table')
-local TeamTemplate = Lua.import('Module:TeamTemplate')
 
 local Opponent = Lua.import('Module:Opponent')
-local PlayerExt = Lua.import('Module:Player/Ext/Custom')
 
 local StarcraftOpponent = Table.deepCopy(Opponent)
 
 ---@class StarcraftStandardPlayer:standardPlayer
 ---@field position integer?
----@field faction string?
 
 ---@class StarcraftStandardOpponent:standardOpponent
 ---@field players StarcraftStandardPlayer[]
@@ -35,24 +31,17 @@ function StarcraftOpponent.readOpponentArgs(args)
 	local partySize = Opponent.partySize((opponent or {}).type)
 
 	if not opponent then
-		return nil
+		return opponent
 	end
 
-	if partySize == 1 then
-		opponent.players[1].faction = Faction.read(args.faction or args.race)
+	opponent.isArchon = partySize and Logic.readBool(args.isarchon) or false
+	if not opponent.isArchon then
+		return opponent
+	end
 
-	elseif partySize then
-		opponent.isArchon = Logic.readBool(args.isarchon)
-		if opponent.isArchon then
-			local archonFaction = Faction.read(args.faction or args.race)
-			for _, player in ipairs(opponent.players) do
-				player.faction = archonFaction
-			end
-		else
-			for playerIx, player in ipairs(opponent.players) do
-				player.faction = Faction.read(args['p' .. playerIx .. 'faction'] or args['p' .. playerIx .. 'race'])
-			end
-		end
+	local archonFaction = Faction.read(args.faction or args.race)
+	for _, player in ipairs(opponent.players) do
+		player.faction = archonFaction
 	end
 
 	return opponent
@@ -62,8 +51,6 @@ end
 ---@return StarcraftStandardOpponent
 function StarcraftOpponent.blank(type)
 	local opponent = Opponent.blank(type) --[[@as StarcraftStandardOpponent]]
-
-	opponent.extradata = {}
 	opponent.isArchon = false
 
 	return opponent
@@ -73,8 +60,6 @@ end
 ---@return StarcraftStandardOpponent
 function StarcraftOpponent.tbd(type)
 	local opponent = Opponent.tbd(type) --[[@as StarcraftStandardOpponent]]
-
-	opponent.extradata = {}
 	opponent.isArchon = false
 
 	return opponent
@@ -89,13 +74,7 @@ function StarcraftOpponent.fromMatch2Record(record)
 		return nil
 	end
 
-	if Opponent.typeIsParty(opponent.type) then
-		for playerIx, player in ipairs(opponent.players) do
-			local playerRecord = record.match2players[playerIx]
-			player.faction = Faction.read(playerRecord.extradata.faction) or Faction.defaultFaction
-		end
-		opponent.isArchon = Logic.readBool((record.extradata or {}).isarchon)
-	end
+	opponent.isArchon = Logic.readBool((record.extradata or {}).isarchon)
 
 	return opponent
 end
@@ -105,15 +84,9 @@ end
 function StarcraftOpponent.toLpdbStruct(opponent)
 	local storageStruct = Opponent.toLpdbStruct(opponent)
 
-	if Opponent.typeIsParty(opponent.type) then
-		if opponent.isArchon then
-			storageStruct.opponentplayers.isArchon = true
-			storageStruct.opponentplayers.faction = opponent.players[1].faction
-		else
-			for playerIndex, player in pairs(opponent.players) do
-				storageStruct.opponentplayers['p' .. playerIndex .. 'faction'] = player.faction
-			end
-		end
+	if Opponent.typeIsParty(opponent.type) and opponent.isArchon then
+		storageStruct.opponentplayers.isArchon = true
+		storageStruct.opponentplayers.faction = opponent.players[1].faction
 	end
 
 	return storageStruct
@@ -128,56 +101,8 @@ function StarcraftOpponent.fromLpdbStruct(storageStruct)
 		return nil
 	end
 
-	if Opponent.partySize(storageStruct.opponenttype) then
-		opponent.isArchon = storageStruct.opponentplayers.isArchon
-		for playerIndex, player in pairs(opponent.players) do
-			player.faction = storageStruct.opponentplayers['p' .. playerIndex .. 'faction']
-				or storageStruct.opponentplayers.faction
-		end
-	end
+	opponent.isArchon = storageStruct.opponentplayers.isArchon
 
-	return opponent
-end
-
---[[
-Resolves the identifiers of an opponent.
-For team opponents, this resolves the team template to a particular date. For
-party opponents, this fills in players' pageNames using their displayNames,
-using data stored in page variables if present.
-options.syncPlayer: Whether to fetch player information from variables or LPDB. Disabled by default.
-]]
----@param opponent StarcraftStandardOpponent
----@param date string|number|nil
----@param options {syncPlayer: boolean?, overwritePageVars: boolean?}
----@return StarcraftStandardOpponent
-function StarcraftOpponent.resolve(opponent, date, options)
-	options = options or {}
-	if opponent.type == Opponent.team then
-		opponent.template = TeamTemplate.resolve(opponent.template, date) or opponent.template or 'tbd'
-	elseif Opponent.typeIsParty(opponent.type) then
-		for _, player in ipairs(opponent.players) do
-			if options.syncPlayer then
-				local hasFaction = String.isNotEmpty(player.faction)
-				local savePageVar = not Opponent.playerIsTbd(player --[[@as standardPlayer]])
-				PlayerExt.syncPlayer(player, {
-					savePageVar = savePageVar,
-					date = date,
-					overwritePageVars = options.overwritePageVars,
-				})
-				player.team = PlayerExt.syncTeam(
-					player.pageName:gsub(' ', '_'),
-					player.team,
-					{date = date, savePageVar = savePageVar}
-				)
-				player.faction = (hasFaction or player.faction ~= Faction.defaultFaction) and player.faction or nil
-			else
-				PlayerExt.populatePageName(player)
-			end
-			if player.team then
-				player.team = TeamTemplate.resolve(player.team, date)
-			end
-		end
-	end
 	return opponent
 end
 
