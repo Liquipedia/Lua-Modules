73e37740c3c7f8ff79ceb41421c3572499e893ac
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2025-08-01 15:55:28 +0900

    add elo standings widget

---
+++ lua/wikis/warcraft/Widget/W3EloStandings.lua
@@ -0,0 +1,97 @@
+---
+-- @Liquipedia
+-- page=Module:Widget/W3EloStandings
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Lua = require('Module:Lua')
+
+local Array = Lua.import('Module:Array')
+local Class = Lua.import('Module:Class')
+local Flags = Lua.import('Module:Flags')
+local Logic = Lua.import('Module:Logic')
+local Template = Lua.import('Module:Template')
+
+local Widget = Lua.import('Module:Widget')
+local HtmlWidgets = Lua.import('Module:Widget/Html/All')
+local Link = Lua.import('Module:Widget/Basic/Link')
+local WidgetUtil = Lua.import('Module:Widget/Util')
+
+---@class W3EloStandings: Widget
+---@operator call({count: integer?}): W3EloStandings
+---@field props {count: integer?}
+local W3EloStandings = Class.new(Widget)
+W3EloStandings.defaultProps = {
+	count = 10,
+}
+
+---@return Widget?
+function W3EloStandings:render()
+	local rawData = mw.ext.TeamLiquidIntegration.w3elo(self.props.count)
+
+	if type(rawData) ~= 'table' then
+		return rawData
+	end
+
+	return HtmlWidgets.Div{
+		classes = {'table-responsive'},
+		children = HtmlWidgets.Table{
+			classes = {'wikitable', 'wikitable-striped', 'rankingtable'},
+			css = {width = '100%'},
+			children = WidgetUtil.collect(
+				W3EloStandings._buildHeader(),
+				Array.map(rawData, W3EloStandings._buildStandingRow)
+			)
+		}
+	}
+end
+
+---@private
+---@return Widget
+function W3EloStandings._buildHeader()
+	return HtmlWidgets.Tr{
+		children = Array.map({'Rank', 'Player', 'Rating'}, function (header)
+			return HtmlWidgets.Th{
+				css = {['text-align'] = 'left'},
+				children = header
+			}
+		end)
+	}
+end
+
+---@private
+---@param data table
+---@param placement integer
+---@return Widget
+function W3EloStandings._buildStandingRow(data, placement)
+	local country = data.country
+		if country == "zz" then
+			country = "xx"
+		end
+
+	local race = string.lower( data.main_race or '' )
+
+	if race == '' then mw.logObject(data) end
+
+	local raceshort = string.sub(race, 0, 1)
+
+	return HtmlWidgets.Tr{
+		children = {
+			HtmlWidgets.Td{children = placement},
+			HtmlWidgets.Td{
+				children = Array.interleave({
+					Flags.Icon{flag = country or ''},
+					Template.safeExpand(mw.getCurrentFrame(), 'RaceIconSmall', {raceshort}),
+					Link{
+						link = data.name,
+						children = Logic.emptyOr(data.liquipedia, data.name)
+					}
+				}, ' ')
+			},
+			HtmlWidgets.Td{children = data.elo}
+		}
+	}
+end
+
+return W3EloStandings
