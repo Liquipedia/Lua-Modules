d96ab3adde96c64c25c1ce1366343eb507efefca
Rikard Blixt <rikardblixt@gmail.com>
2025-08-04 10:06:37 +0200

    lint

--- lua/wikis/commons/Widget/Basic/Slider.lua
+++ lua/wikis/commons/Widget/Basic/Slider.lua
@@ -9,7 +9,6 @@ local Lua = require('Module:Lua')
 
 local Array = Lua.import('Module:Array')
 local Class = Lua.import('Module:Class')
-local Table = Lua.import('Module:Table')
 
 local Widget = Lua.import('Module:Widget')
 local HtmlWidgets = Lua.import('Module:Widget/Html/All')
