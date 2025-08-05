8df22f5f89d2b464f300114f2669caa3b88781a0
hjpalpha <andreasbuesch1@gmail.com>
2025-08-05 20:10:09 +0200

    brawlstars

--- lua/wikis/brawlstars/Infobox/Person/Player/Custom.lua
+++ lua/wikis/brawlstars/Infobox/Person/Player/Custom.lua
@@ -5,19 +5,20 @@
 -- Please see https://github.com/Liquipedia/Lua-Modules to contribute
 --
 
-local Class = require('Module:Class')
-local Logic = require('Module:Logic')
 local Lua = require('Module:Lua')
-local Page = require('Module:Page')
-local PlayerIntroduction = require('Module:PlayerIntroduction/Custom')
-local String = require('Module:StringUtils')
-local Team = require('Module:Team')
-local Template = require('Module:Template')
+
+local Class = Lua.import('Module:Class')
+local Logic = Lua.import('Module:Logic')
+local Page = Lua.import('Module:Page')
+local PlayerIntroduction = Lua.import('Module:PlayerIntroduction/Custom')
+local String = Lua.import('Module:StringUtils')
+local Team = Lua.import('Module:Team')
+local Template = Lua.import('Module:Template')
 
 local Injector = Lua.import('Module:Widget/Injector')
 local Player = Lua.import('Module:Infobox/Person')
 
-local Widgets = require('Module:Widget/All')
+local Widgets = Lua.import('Module:Widget/All')
 local Cell = Widgets.Cell
 
 ---@class BrawlstarsInfoboxPlayer: Person
