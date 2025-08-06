04feb8a13624359329e6d8acb20bb941114d3d3c
hjpalpha <andreasbuesch1@gmail.com>
2025-08-06 08:46:21 +0200

    trackmania

--- lua/wikis/trackmania/Infobox/Person/Player/Custom.lua
+++ lua/wikis/trackmania/Infobox/Person/Player/Custom.lua
@@ -5,14 +5,15 @@
 -- Please see https://github.com/Liquipedia/Lua-Modules to contribute
 --
 
-local Class = require('Module:Class')
 local Lua = require('Module:Lua')
+
+local Class = Lua.import('Module:Class')
 
 local Achievements = Lua.import('Module:Infobox/Extension/Achievements')
 local Injector = Lua.import('Module:Widget/Injector')
 local Player = Lua.import('Module:Infobox/Person')
 
-local Widgets = require('Module:Widget/All')
+local Widgets = Lua.import('Module:Widget/All')
 local Cell = Widgets.Cell
 
 ---@class TrackmaniaInfoboxPlayer: Person
