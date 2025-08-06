c4c06253bdb7003aa118b760f0b78cc07504e96c
hjpalpha <andreasbuesch1@gmail.com>
2025-08-06 11:02:19 +0200

    adjust role data as needed

--- lua/wikis/commons/ContractRoles.lua
+++ lua/wikis/commons/ContractRoles.lua
@@ -7,7 +7,7 @@
 
 ---@type table<string, PersonRoleData>
 local contractRoles = {
-	['loan'] = {category = 'Players On Loan', display = 'On loan'},
+	['loan'] = {category = 'Players On Loan', display = 'On loan', abbreviation = 'L.'},
 	['standard'] = {category = 'Standard Contracts', display = 'Standard'},
 	['standin'] = {category = 'Stand-in Players', display = 'Stand-in'},
 	['twoway'] = {category = 'Two-way Contracts', display = 'Two-way'},
