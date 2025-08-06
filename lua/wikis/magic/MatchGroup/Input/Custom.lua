e46c86afcf59ca2d9f0ac5d2d13ede71f74bee4e
hjpalpha <andreasbuesch1@gmail.com>
2025-08-06 09:09:41 +0200

    magic

--- lua/wikis/magic/MatchGroup/Input/Custom.lua
+++ lua/wikis/magic/MatchGroup/Input/Custom.lua
@@ -5,10 +5,11 @@
 -- Please see https://github.com/Liquipedia/Lua-Modules to contribute
 --
 
-local Array = require('Module:Array')
-local Logic = require('Module:Logic')
 local Lua = require('Module:Lua')
-local Variables = require('Module:Variables')
+
+local Array = Lua.import('Module:Array')
+local Logic = Lua.import('Module:Logic')
+local Variables = Lua.import('Module:Variables')
 
 local MatchGroupInputUtil = Lua.import('Module:MatchGroup/Input/Util')
 
