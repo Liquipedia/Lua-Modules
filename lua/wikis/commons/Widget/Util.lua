e0347804b68e2ed5921ffcbffb03dd1fb0be5cc4
hjpalpha <andreasbuesch1@gmail.com>
2025-08-08 11:13:26 +0200

    add isEmpty function to WidgetUtil

--- lua/wikis/commons/Widget/Util.lua
+++ lua/wikis/commons/Widget/Util.lua
@@ -8,10 +8,15 @@
 local Lua = require('Module:Lua')
 
 local Array = Lua.import('Module:Array')
+local Class = Lua.import('Module:Class')
+local Logic = Lua.import('Module:Logic')
 local Table = Lua.import('Module:Table')
+local Widget = Lua.import('Module:Widget')
 
 local Util = {}
 
+---@param ... Widget|string|number|Html?
+---@return (Widget|string|number|Html)[]
 function Util.collect(...)
 	local elements = Table.pack(...)
 	local array = {}
@@ -26,6 +31,21 @@ function Util.collect(...)
 		end
 	end
 	return array
+end
+
+---@param widget Widget|string|number|Html?
+---@return boolean
+function Util.isEmpty(widget)
+	if Logic.isEmpty(widget) then
+		return true
+	end
+
+	if not Class.instanceOf(widget, Widget) then
+		return false
+	end
+	---@cast widget Widget
+
+	return Array.all(widget.children or {}, Util.isEmpty)
 end
 
 return Util
