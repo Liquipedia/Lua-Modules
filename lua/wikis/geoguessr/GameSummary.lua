f743cc9ad9a503624a471292aa4b6e11180fb09e
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2025-08-11 12:31:11 +0200

    Update lua/wikis/geoguessr/GameSummary.lua
    Co-authored-by: SyntacticSalt <mail@mbergen.de>

--- lua/wikis/geoguessr/GameSummary.lua
+++ lua/wikis/geoguessr/GameSummary.lua
@@ -19,7 +19,7 @@ local MatchSummaryWidgets = Lua.import('Module:Widget/Match/Summary/Ffa/All')
 ---@param props {bracketId: string, matchId: string, gameIdx: integer}
 ---@return Html
 function CustomGameSummary.getGameByMatchId(props)
-	---@class FFAMatchGroupUtilMatch
+	---@type FFAMatchGroupUtilMatch
 	local match = MatchGroupUtil.fetchMatchForBracketDisplay(props.bracketId, props.matchId)
 
 	local game = match.games[props.gameIdx]
