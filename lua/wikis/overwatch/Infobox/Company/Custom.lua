0e0a6ce55c42ee32cf6b48ab74b2f3047541c77b
GeneralHuber <61513963+GeneralHuber@users.noreply.github.com>
2025-08-18 19:22:41 +0200

    add infobox company custom to overwatch and marvelrivals

---
+++ lua/wikis/overwatch/Infobox/Company/Custom.lua
@@ -0,0 +1,45 @@
+---
+-- @Liquipedia
+-- wiki=overwatch
+-- page=Module:Infobox/Company/Custom
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Class = require('Module:Class')
+local Lua = require('Module:Lua')
+
+local Company = Lua.import('Module:Infobox/Company')
+local Injector = Lua.import('Module:Widget/Injector')
+
+local Widgets = require('Module:Widget/All')
+local Cell = Widgets.Cell
+
+---@class OverwatchCompanyInfobox: CompanyInfobox
+local CustomCompany = Class.new(Company)
+local CustomInjector = Class.new(Injector)
+
+---@param frame Frame
+---@return Html
+function CustomCompany.run(frame)
+	local company = CustomCompany(frame)
+	company:setWidgetInjector(CustomInjector(company))
+
+	return company:createInfobox()
+end
+
+---@param id string
+---@param widgets Widget[]
+---@return Widget[]
+function CustomInjector:parse(id, widgets)
+	local args = self.caller.args
+	if id == 'parent' then
+		table.insert(widgets, Cell{name = 'Focus', content = {args.focus}})
+	elseif id == 'employees' then
+		table.insert(widgets, Cell{name = 'Key People', content = {args.people}})
+	end
+
+	return widgets
+end
+
+return CustomCompany
