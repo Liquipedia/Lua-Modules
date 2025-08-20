78aff252f40b030b74da126970654e5a75c91bb9
Eetu Rantanen <eeturantanen.dev@gmail.com>
2025-08-20 16:43:20 +0300

    completed state

---
+++ lua/wikis/commons/Widget/Match/HeaderBR.lua
@@ -0,0 +1,133 @@
+---
+-- @Liquipedia
+-- page=Module:Widget/Match/HeaderBR
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Lua = require('Module:Lua')
+
+local Array = Lua.import('Module:Array')
+local Class = Lua.import('Module:Class')
+
+local Widget = Lua.import('Module:Widget')
+local HtmlWidgets = Lua.import('Module:Widget/Html/All')
+local Icon = Lua.import('Module:Widget/Image/Icon/Fontawesome')
+local Div = HtmlWidgets.Div
+
+local OpponentDisplay = Lua.import('Module:OpponentDisplay/Custom')
+
+---@class MatchHeaderBRProps
+---@field match MatchGroupUtilMatch
+---@field teamStyle? teamStyle
+
+---@class MatchHeaderBR: Widget
+---@operator call(MatchHeaderBRProps): MatchHeaderBR
+---@field props MatchHeaderBRProps
+local MatchHeaderBR = Class.new(Widget)
+MatchHeaderBR.defaultProps = {
+	teamStyle = 'short',
+}
+
+---@return Widget?
+function MatchHeaderBR:render()
+	local match = self.props.match
+	if not match then
+		return nil
+	end
+
+	if #match.opponents < 3 then
+		return
+	end
+
+	local sortedOpponents = Array.sortBy(
+		match.opponents,
+		function(opponent)
+			return opponent.placement
+		end
+	)
+
+	local first = sortedOpponents[1]
+	local second = sortedOpponents[2]
+	local third = sortedOpponents[3]
+
+	return Div{
+		classes = {'match-info-headerbr'},
+		children = {
+			Div{
+				classes = {'match-info-headerbr-positionrow'},
+				children = {
+					Div{
+						classes = {'match-info-headerbr-positionholder'},
+						children = {
+							Icon{iconName = 'trophy', color = '#CCAB00'},
+							'1st'
+						}
+
+					},
+					Div{
+						classes = {'match-info-header-opponent'},
+						children = {
+							OpponentDisplay.BlockOpponent{
+								opponent = first,
+								teamStyle = self.props.teamStyle,
+								overflow = 'ellipsis',
+								flip = true,
+							}
+						}
+					}
+				}
+			},
+			Div{
+				classes = {'match-info-header-positionrow'},
+				children = {
+					Div{
+						classes = {'match-info-header-positionholder'},
+						children = {
+							Icon{iconName = 'trophy', color = '#557177'},
+							'2nd'
+						}
+
+					},
+					Div{
+						classes = {'match-info-header-opponent'},
+						children = {
+							OpponentDisplay.BlockOpponent{
+								opponent = second,
+								teamStyle = self.props.teamStyle,
+								overflow = 'ellipsis',
+								flip = true,
+							}
+						}
+					}
+				}
+			},
+			Div{
+				classes = {'match-info-header-positionrow'},
+				children = {
+					Div{
+						classes = {'match-info-header-positionholder'},
+						children = {
+							Icon{iconName = 'trophy', color = '#80704C'},
+							'3rd'
+						}
+
+					},
+					Div{
+						classes = {'match-info-header-opponent'},
+						children = {
+							OpponentDisplay.BlockOpponent{
+								opponent = third,
+								teamStyle = self.props.teamStyle,
+								overflow = 'ellipsis',
+								flip = true,
+							}
+						}
+					}
+				}
+			},
+		}
+	}
+end
+
+return MatchHeaderBR
