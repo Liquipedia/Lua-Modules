4df839423f27ae7251ff3265cde71040250eeb7b
Libavi <165576651+Libavi@users.noreply.github.com>
2025-08-22 23:10:48 +0200

    Added lab Faction/IconData.lua.

---
+++ lua/wikis/lab/Faction/IconData.lua
@@ -0,0 +1,176 @@
+---
+-- @Liquipedia
+-- page=Module:Faction/IconData
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+-- Heroes of Might and Magic
+
+local byFactionH1 = {
+	farm = {
+		icon = 'File:Farm H1 icon.webp',
+	},
+	forest = {
+		icon = 'File:Forest H1 icon.webp',
+	},
+	mountain = {
+		icon = 'File:Mountain H1 icon.webp',
+	},
+	plains = {
+		icon = 'File:Plains H1 icon.webp',
+	},
+
+	unknown = {
+		icon = 'File:Char head filler.png',
+	},
+}
+
+local byFactionH2 = {
+	barbarian = {
+		icon = 'File:Barbarian H2 icon.webp',
+	},
+	knight = {
+		icon = 'File:Knight H2 icon.webp',
+	},
+	necromancer = {
+		icon = 'File:Necromancer H2 icon.webp',
+	},
+	sorceress = {
+		icon = 'File:Sorceress H2 icon.webp',
+	},
+	warlock = {
+		icon = 'File:Warlock H2 icon.webp',
+	},
+	wizard = {
+		icon = 'File:Wizard H2 icon.webp',
+	},
+
+	unknown = {
+		icon = 'File:Char head filler.png',
+	},
+}
+
+local byFactionH3 = {
+	castle = {
+		icon = 'File:Castle H3 icon.png',
+	},
+	rampart = {
+		icon = 'File:Rampart H3 icon.png',
+	},
+	tower = {
+		icon = 'File:Tower H3 icon.png',
+	},
+	inferno = {
+		icon = 'File:Inferno H3 icon.png',
+	},
+	necropolis = {
+		icon = 'File:Necropolis H3 icon.png',
+	},
+	dungeon = {
+		icon = 'File:Dungeon H3 icon.png',
+	},
+	stronghold = {
+		icon = 'File:Stronghold H3 icon.png',
+	},
+	fortress = {
+		icon = 'File:Fortress H3 icon.png',
+	},
+	conflux = {
+		icon = 'File:Conflux H3 icon.png',
+	},
+	cove = {
+		icon = 'File:Cove H3 icon.png',
+	},
+	factory = {
+		icon = 'File:Factory H3 icon.png',
+	},
+	bulwark = {
+		icon = 'File:Bulwark H3 icon.png',
+	},
+
+	unknown = {
+		icon = 'File:Char head filler.png',
+	},
+}
+
+local byFactionH4 = {
+	haven = {
+		icon = 'File:Haven H4 icon.webp',
+	},
+	stronghold = {
+		icon = 'File:Stronghold H4 icon.webp',
+	},
+	academy = {
+		icon = 'File:Academy H4 icon.webp',
+	},
+	preserve = {
+		icon = 'File:Preserve H4 icon.webp',
+	},
+	necropolis = {
+		icon = 'File:Necropolis H4 icon.webp',
+	},
+	asylum = {
+		icon = 'File:Asylum H4 icon.webp',
+	},
+
+	unknown = {
+		icon = 'File:Char head filler.png',
+	},
+}
+
+local byFactionH5 = {
+	academy = {
+		icon = 'File:Academy H5 icon.webp',
+	},
+	dungeon = {
+		icon = 'File:Dungeon H5 icon.webp',
+	},
+	haven = {
+		icon = 'File:Haven H5 icon.webp',
+	},
+	inferno = {
+		icon = 'File:Inferno H5 icon.webp',
+	},
+	necropolis = {
+		icon = 'File:Necropolis H5 icon.webp',
+	},
+	sylvan = {
+		icon = 'File:Sylvan H5 icon.webp',
+	},
+	fortress = {
+		icon = 'File:Fortress H5 icon.webp',
+	},
+	stronghold = {
+		icon = 'File:Stronghold H5 icon.webp',
+	},
+
+	unknown = {
+		icon = 'File:Char head filler.png',
+	},
+}
+
+local byFactionHOE = {
+	temple = {
+		icon = 'File:Temple HOE icon.png',
+	},
+	necropolis = {
+		icon = 'File:Necropolis HOE icon.png',
+	},
+	sylvan = {
+		icon = 'File:Sylvan HOE icon.png',
+	},
+	dungeon = {
+		icon = 'File:Dungeon HOE icon.png',
+	},
+	hive = {
+		icon = 'File:Hive HOE icon.png',
+	},
+	schism = {
+		icon = 'File:Schism HOE icon.png',
+	},
+
+	unknown = {
+		icon = 'File:Char head filler.png',
+	},
+}
