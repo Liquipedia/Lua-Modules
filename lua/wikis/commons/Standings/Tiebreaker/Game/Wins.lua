0db6c3b4c8de23737f24930a3bd8c774702135f2
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2025-08-26 12:15:22 +0900

    refactor game wins tiebreaker

--- lua/wikis/commons/Standings/Tiebreaker/Game/Wins.lua
+++ lua/wikis/commons/Standings/Tiebreaker/Game/Wins.lua
@@ -7,11 +7,9 @@
 
 local Lua = require('Module:Lua')
 
-local Array = Lua.import('Module:Array')
 local Class = Lua.import('Module:Class')
-local Operator = Lua.import('Module:Operator')
-local Opponent = Lua.import('Module:Opponent/Custom')
 
+local TiebreakerGameUtil = Lua.import('Module:Standings/Tiebreaker/Game/Util')
 local TiebreakerInterface = Lua.import('Module:Standings/Tiebreaker/Interface')
 
 ---@class TiebreakerGameWins : StandingsTiebreaker
@@ -21,19 +19,8 @@ local TiebreakerGameWins = Class.new(TiebreakerInterface)
 ---@param opponent TiebreakerOpponent
 ---@return integer
 function TiebreakerGameWins:valueOf(state, opponent)
-	return Array.reduce(
-		Array.map(opponent.matches, function (match)
-			local gamesWon = Array.filter(match.games, function (game)
-				if not game.winner or game.winner == 0 then
-					return false
-				end
-				return Opponent.same(opponent.opponent, match.opponents[game.winner])
-			end)
-			return #gamesWon
-		end),
-		Operator.add,
-		0
-	)
+	local games = TiebreakerGameUtil.getGames(opponent)
+	return games.w
 end
 
 ---@return string
