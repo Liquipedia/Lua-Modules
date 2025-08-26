27084d91bc419929f74e18b07491393cf209411f
SlothyMan <76825878+SlothyMan@users.noreply.github.com>
2025-08-26 17:19:32 +0200

    add class

--- lua/wikis/chess/PlayerIntroduction/Custom.lua
+++ lua/wikis/chess/PlayerIntroduction/Custom.lua
@@ -7,21 +7,39 @@
 
 local Lua = require('Module:Lua')
 local String = require('Module:StringUtils')
+local Class = require('Module:Class')
+local Arguments = require('Module:Arguments')
+
 local PlayerIntroduction = Lua.import('Module:PlayerIntroduction')
+
+---@class CustomPlayerIntroduction: PlayerIntroduction
+local CustomPlayerIntroduction = Class.new(PlayerIntroduction)
+
+--- Module entry point for PlayerIntroduction
+---@param args playerIntroArgsValues?
+---@return string
+function CustomPlayerIntroduction.run(args)
+	return CustomPlayerIntroduction(args):queryPlayerInfo():queryTransferData(true):adjustData():create()
+end
+
+--- Template entry point for PlayerIntroduction
+---@param frame Frame
+---@return string
+function CustomPlayerIntroduction.templatePlayerIntroduction(frame)
+	return CustomPlayerIntroduction.run(Arguments.getArgs(frame))
+end
 
 --- Overrides the name display.
 ---@return string
-function PlayerIntroduction:nameDisplay()
+function CustomPlayerIntroduction:nameDisplay()
 	return '<b>' .. self.playerInfo.id .. '</b>'
 end
-
-local originalParsePlayerInfo = PlayerIntroduction._parsePlayerInfo
 
 --- Extends the original _parsePlayerInfo to include chess-specific data.
 ---@param args table
 ---@param playerInfo table
-function PlayerIntroduction:_parsePlayerInfo(args, playerInfo)
-	originalParsePlayerInfo(self, args, playerInfo)
+function CustomPlayerIntroduction:_parsePlayerInfo(args, playerInfo)
+	PlayerIntroduction._parsePlayerInfo(self, args, playerInfo)
 
 	if playerInfo and playerInfo.extradata then
 		self.playerInfo.chessTitle = playerInfo.extradata.chesstitle
@@ -29,26 +47,22 @@ function PlayerIntroduction:_parsePlayerInfo(args, playerInfo)
 	end
 end
 
-local originalGameDisplay = PlayerIntroduction._gameDisplay
-
 --- Customizes how the game is displayed.
 ---@return string
-function PlayerIntroduction:_gameDisplay()
+function CustomPlayerIntroduction:_gameDisplay()
 	local title = self.playerInfo.chessTitle
 	local game = self.playerInfo.game
 
 	if String.isNotEmpty(title) and String.isNotEmpty(game) then
 		return self._addConcatText(game)
 	else
-		return originalGameDisplay(self)
+		return PlayerIntroduction._gameDisplay(self)
 	end
 end
 
-local originalTypeDisplay = PlayerIntroduction.typeDisplay
-
 --- Customizes the player type display.
 ---@return string
-function PlayerIntroduction:typeDisplay()
+function CustomPlayerIntroduction:typeDisplay()
 	local title = self.playerInfo.chessTitle
 
 	if self.playerInfo.banned then
@@ -56,8 +70,8 @@ function PlayerIntroduction:typeDisplay()
 	elseif String.isNotEmpty(title) then
 		return self._addConcatText('chess ' .. title)
 	else
-		return originalTypeDisplay(self)
+		return PlayerIntroduction.typeDisplay(self)
 	end
 end
 
-return PlayerIntroduction
+return CustomPlayerIntroduction
