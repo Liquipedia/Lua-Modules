c140567dfa9e233968751dec4e909bfdaf3a9993
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2025-08-29 21:57:08 +0900

    add alt text to team icon widget

--- lua/wikis/commons/Widget/Image/Icon/TeamIcon.lua
+++ lua/wikis/commons/Widget/Image/Icon/TeamIcon.lua
@@ -16,24 +16,17 @@ local WidgetIcon = Lua.import('Module:Widget/Image/Icon')
 local WidgetIconImage = Lua.import('Module:Widget/Image/Icon/Image')
 local WidgetIconFontawesome = Lua.import('Module:Widget/Image/Icon/Fontawesome')
 
----@class TeamIconWidgetParameters
----@field imageLight string?
----@field imageDark string?
----@field page string?
----@field size string?
----@field noLink boolean?
----@field legacy boolean?
-
 ---@class TeamIconWidgetProps
 ---@field imageLight string?
 ---@field imageDark string?
+---@field name string?
 ---@field page string?
 ---@field size string
 ---@field noLink boolean?
 ---@field legacy boolean?
 
 ---@class TeamIconWidget: IconWidget
----@operator call(TeamIconWidgetParameters): TeamIconWidget
+---@operator call(TeamIconWidgetProps): TeamIconWidget
 ---@field props TeamIconWidgetProps
 local TeamIcon = Class.new(WidgetIcon)
 TeamIcon.defaultProps = {
@@ -63,6 +56,7 @@ function TeamIcon:_getIcon(image, size, link)
 		imageLight = image,
 		size = size,
 		alignment = 'middle',
+		alt = self.props.name,
 		link = link,
 	}
 end
