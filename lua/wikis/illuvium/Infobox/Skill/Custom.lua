ff980214a4bac7b55cdaab8010797c0310a41bc2
Eetu Rantanen <eeturantanen.dev@gmail.com>
2025-09-02 12:27:32 +0300

    inject the infobox

--- lua/wikis/illuvium/Infobox/Skill/Custom.lua
+++ lua/wikis/illuvium/Infobox/Skill/Custom.lua
@@ -13,39 +13,42 @@ local Injector = Lua.import('Module:Widget/Injector')
 local Skill = Lua.import('Module:Infobox/Skill')
 
 local Widgets = Lua.import('Module:Widget/All')
+local Array = Lua.import('Module:Array')
 local Cell = Widgets.Cell
 
 ---@class IlluviumSkillInfobox: SkillInfobox
 local CustomSkill = Class.new(Skill)
-
 local CustomInjector = Class.new(Injector)
 
 ---@param frame Frame
 ---@return Html
 function CustomSkill.run(frame)
-	local skill = CustomSkill(frame)
+  local skill = CustomSkill(frame)
 
-	assert(skill.args.informationType, 'Missing "informationType"')
+  assert(skill.args.informationType, 'Missing "informationType"')
 
-	return skill:createInfobox()
+  skill:setWidgetInjector(CustomInjector(skill))
+
+  return skill:createInfobox()
 end
 
 ---@param id string
 ---@param widgets Widget[]
 ---@return Widget[]
 function CustomInjector:parse(id, widgets)
-	local args = self.caller.args
+  local args = self.caller.args
 
-	if id == 'custom' then
-		return {
-			Cell{name = 'Class', children = {args.class}},
-			Cell{name = 'Type', children = {args.type}},
-			Cell{name = 'Release Date:', children = {args.releasedate}},
-			Cell{name = 'Synergy Levels:', children = {args.synergylevels}},
-		}
-	end
+  if id == 'custom' then
+    Array.appendWith(
+      widgets,
+      Cell{name = 'Class', children = {args.class}},
+      Cell{name = 'Type', children = {args.type}},
+      Cell{name = 'Release Date:', children = {args.releasedate}},
+      Cell{name = 'Synergy Levels:', children = {args.synergylevels}}
+    )
+  end
 
-	return widgets
+  return widgets
 end
 
 return CustomSkill
