e04e98afa4ec90eea35cfff6458363b659fefe36
Eetu Rantanen <eeturantanen.dev@gmail.com>
2025-09-02 11:51:28 +0300

    add custom skill for illuvium

---
+++ lua/wikis/illuvium/Infobox/Skill/Custom.lua
@@ -0,0 +1,51 @@
+---
+-- @Liquipedia
+-- page=Module:Infobox/Skill/Custom
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Lua = require('Module:Lua')
+
+local Class = Lua.import('Module:Class')
+
+local Injector = Lua.import('Module:Widget/Injector')
+local Skill = Lua.import('Module:Infobox/Skill')
+
+local Widgets = Lua.import('Module:Widget/All')
+local Cell = Widgets.Cell
+
+---@class IlluviumSkillInfobox: SkillInfobox
+local CustomSkill = Class.new(Skill)
+
+local CustomInjector = Class.new(Injector)
+
+---@param frame Frame
+---@return Html
+function CustomSkill.run(frame)
+	local skill = CustomSkill(frame)
+
+	assert(skill.args.informationType, 'Missing "informationType"')
+
+	return skill:createInfobox()
+end
+
+---@param id string
+---@param widgets Widget[]
+---@return Widget[]
+function CustomInjector:parse(id, widgets)
+	local args = self.caller.args
+
+	if id == 'custom' then
+		return {
+			Cell{name = 'Class', children = {args.class}},
+			Cell{name = 'Type', children = {args.type}},
+			Cell{name = 'Release Date:', children = {args.releasedate}},
+			Cell{name = 'Synergy Levels:', children = {args.synergylevels}},
+		}
+	end
+
+	return widgets
+end
+
+return CustomSkill
