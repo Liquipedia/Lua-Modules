1053f514b28195a3883451ae8248052c53de733d
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2025-09-02 09:33:51 +0900

    make separate widget

--- lua/wikis/commons/WarningBox.lua
+++ lua/wikis/commons/WarningBox.lua
@@ -11,39 +11,13 @@ local Lua = require('Module:Lua')
 
 local Array = Lua.import('Module:Array')
 
-local IconImage = Lua.import('Module:Widget/Image/Icon/Image')
 local HtmlWidgets = Lua.import('Module:Widget/Html/All')
+local WarningBoxWidget = Lua.import('Module:Widget/WarningBox')
 
 ---@param text string|number
 ---@return Widget
 function WarningBox.display(text)
-	local tbl = HtmlWidgets.Table{
-		children = HtmlWidgets.Tr{
-			children = {
-				HtmlWidgets.Td{
-					classes = {'ambox-image'},
-					children = IconImage{imageLight = 'Emblem-important.svg', size = '40px', link = ''}
-				},
-				HtmlWidgets.Td{
-					classes = {'ambox-text'},
-					children = text
-				},
-			}
-		}
-	}
-
-	return HtmlWidgets.Div{
-		classes = {
-			'show-when-logged-in',
-			'navigation-not-searchable',
-			'ambox-wrapper',
-			'ambox',
-			'wiki-bordercolor-dark',
-			'wiki-backgroundcolor-light',
-			'ambox-red'
-		},
-		children = tbl
-	}
+	return WarningBoxWidget{text = text}
 end
 
 ---@param arr (string|number)[]
---
+++ lua/wikis/commons/Widget/WarningBox.lua
@@ -0,0 +1,52 @@
+---
+-- @Liquipedia
+-- page=Module:Widget/WarningBox
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Lua = require('Module:Lua')
+
+local Class = Lua.import('Module:Class')
+
+local Widget = Lua.import('Module:Widget')
+local IconImage = Lua.import('Module:Widget/Image/Icon/Image')
+local HtmlWidgets = Lua.import('Module:Widget/Html/All')
+
+---@class WarningBoxWidget: Widget
+---@operator call(table): WarningBoxWidget
+---@field props {text: string|number}
+local WarningBoxWidget = Class.new(Widget)
+
+---@return Widget
+function WarningBoxWidget:render()
+	local tbl = HtmlWidgets.Table{
+		children = HtmlWidgets.Tr{
+			children = {
+				HtmlWidgets.Td{
+					classes = {'ambox-image'},
+					children = IconImage{imageLight = 'Emblem-important.svg', size = '40px', link = ''}
+				},
+				HtmlWidgets.Td{
+					classes = {'ambox-text'},
+					children = self.props.text
+				},
+			}
+		}
+	}
+
+	return HtmlWidgets.Div{
+		classes = {
+			'show-when-logged-in',
+			'navigation-not-searchable',
+			'ambox-wrapper',
+			'ambox',
+			'wiki-bordercolor-dark',
+			'wiki-backgroundcolor-light',
+			'ambox-red'
+		},
+		children = tbl
+	}
+end
+
+return WarningBoxWidget
