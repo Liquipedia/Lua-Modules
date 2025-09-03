5551297d475426623eb3baa0486d72ad04142247
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2025-09-03 22:59:56 +0900

    adjust existing tests

--- lua/spec/warning_box_spec.lua
+++ lua/spec/warning_box_spec.lua
@@ -1,10 +1,10 @@
 --- Triple Comment to Enable our LLS Plugin
-local WarningBox = require('Module:WarningBox')
+local WarningBox = require('Module:Widget/WarningBox')
 
 describe('WarningBox.display', function()
 	it('should return correct HTML for string input', function()
 		local text = "Warning: This is a test"
-		local result = tostring(WarningBox.display(text))
+		local result = tostring(WarningBox{text = text})
 		local expected = '<div class="show-when-logged-in navigation-not-searchable ambox-wrapper ambox ' ..
 			'wiki-bordercolor-dark wiki-backgroundcolor-light ambox-red"><table><tr><td class="ambox-image">' ..
 			'[[File:Emblem-important.svg|40px|link=]]</td><td class="ambox-text">Warning: This is a test</td>' ..
@@ -14,48 +14,10 @@ describe('WarningBox.display', function()
 
 	it('should return correct HTML for number input', function()
 		local text = 12345
-		local result = tostring(WarningBox.display(text))
+		local result = tostring(WarningBox{text = text})
 		local expected = '<div class="show-when-logged-in navigation-not-searchable ambox-wrapper ambox ' ..
 			'wiki-bordercolor-dark wiki-backgroundcolor-light ambox-red"><table><tr><td class="ambox-image">' ..
 			'[[File:Emblem-important.svg|40px|link=]]</td><td class="ambox-text">12345</td></tr></table></div>'
-		assert.are.equal(expected, result)
-	end)
-end)
-
-describe('WarningBox.displayAll', function()
-	it('should return correct HTML for array of strings', function()
-		local arr = {"Warning 1", "Warning 2"}
-		local result = tostring(WarningBox.displayAll(arr))
-		local expected = '<div class="show-when-logged-in navigation-not-searchable ambox-wrapper ambox ' ..
-			'wiki-bordercolor-dark wiki-backgroundcolor-light ambox-red"><table><tr><td class="ambox-image">' ..
-			'[[File:Emblem-important.svg|40px|link=]]</td><td class="ambox-text">Warning 1</td></tr></table></div>' ..
-			'<div class="show-when-logged-in navigation-not-searchable ambox-wrapper ambox ' ..
-			'wiki-bordercolor-dark wiki-backgroundcolor-light ambox-red"><table><tr><td class="ambox-image">' ..
-			'[[File:Emblem-important.svg|40px|link=]]</td><td class="ambox-text">Warning 2</td></tr></table></div>'
-		assert.are.equal(expected, result)
-	end)
-
-	it('should return correct HTML for array of numbers', function()
-		local arr = {123, 456}
-		local result = tostring(WarningBox.displayAll(arr))
-		local expected = '<div class="show-when-logged-in navigation-not-searchable ambox-wrapper ambox ' ..
-			'wiki-bordercolor-dark wiki-backgroundcolor-light ambox-red"><table><tr><td class="ambox-image">' ..
-			'[[File:Emblem-important.svg|40px|link=]]</td><td class="ambox-text">123</td></tr></table></div>' ..
-			'<div class="show-when-logged-in navigation-not-searchable ambox-wrapper ambox ' ..
-			'wiki-bordercolor-dark wiki-backgroundcolor-light ambox-red"><table><tr><td class="ambox-image">' ..
-			'[[File:Emblem-important.svg|40px|link=]]</td><td class="ambox-text">456</td></tr></table></div>'
-		assert.are.equal(expected, result)
-	end)
-
-	it('should return correct HTML for mixed array of strings and numbers', function()
-		local arr = {"Warning 1", 456}
-		local result = tostring(WarningBox.displayAll(arr))
-		local expected = '<div class="show-when-logged-in navigation-not-searchable ambox-wrapper ambox ' ..
-			'wiki-bordercolor-dark wiki-backgroundcolor-light ambox-red"><table><tr><td class="ambox-image">' ..
-			'[[File:Emblem-important.svg|40px|link=]]</td><td class="ambox-text">Warning 1</td></tr></table></div>' ..
-			'<div class="show-when-logged-in navigation-not-searchable ambox-wrapper ambox ' ..
-			'wiki-bordercolor-dark wiki-backgroundcolor-light ambox-red"><table><tr><td class="ambox-image">' ..
-			'[[File:Emblem-important.svg|40px|link=]]</td><td class="ambox-text">456</td></tr></table></div>'
 		assert.are.equal(expected, result)
 	end)
 end)
