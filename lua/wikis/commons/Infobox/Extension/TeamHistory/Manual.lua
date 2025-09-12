68a8fc8cf49bb4c158b157a95e71c8e9ffbe3c79
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2025-09-12 16:38:01 +0200

    shrug

--- lua/wikis/commons/Infobox/Extension/TeamHistory/Manual.lua
+++ lua/wikis/commons/Infobox/Extension/TeamHistory/Manual.lua
@@ -87,7 +87,7 @@ function TeamHistoryManual._readDateInput(dateInput)
 	local joinInput = String.trim(dates[1])
 	TeamHistoryManual._checkDate(joinInput)
 
-	local leaveInput = String.trim(dates[2])
+	local leaveInput = String.trim(dates[2] or 'present')
 	local leaveDate
 	if not leaveInput:find('[pP]resent') then
 		leaveDate = leaveInput
