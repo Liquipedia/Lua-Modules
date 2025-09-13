962a048ce494d011a8a50832db7a6bb5accadea8
hjpalpha <andreasbuesch1@gmail.com>
2025-09-13 15:19:10 +0200

    aoe

--- lua/wikis/ageofempires/MatchMaps/Legacy.lua
+++ lua/wikis/ageofempires/MatchMaps/Legacy.lua
@@ -12,6 +12,7 @@ local Array = Lua.import('Module:Array')
 local Json = Lua.import('Module:Json')
 local Logic = Lua.import('Module:Logic')
 local MatchGroup = Lua.import('Module:MatchGroup')
+local MatchGroupLegacy = Lua.import('Module:MatchGroup/Legacy')
 local PageVariableNamespace = Lua.import('Module:PageVariableNamespace')
 local Table = Lua.import('Module:Table')
 local Template = Lua.import('Module:Template')
@@ -87,10 +88,16 @@ end
 ---@param frame Frame
 ---@return Html
 function MatchMapsLegacy.convertMatch(frame)
-	local matchArgs = MatchMapsLegacy._mergeDetailsIntoArgs(Arguments.getArgs(frame))
+	local args = Arguments.getArgs(frame)
+	local generate = Logic.readBool(Table.extract(args, 'generate'))
+	local matchArgs = MatchMapsLegacy._mergeDetailsIntoArgs(args)
 	MatchMapsLegacy._readOpponents(matchArgs)
 	MatchMapsLegacy._readMaps(matchArgs)
 	matchArgs.winner = matchArgs.winner or Table.extract(matchArgs, 'win')
+
+	if generate then
+		return Json.stringify(matchArgs)
+	end
 
 	Template.stashReturnValue(matchArgs, 'LegacyMatchlist')
 	return mw.html.create('div'):css('display', 'none')
@@ -200,6 +207,53 @@ function MatchMapsLegacy.matchlistEnd()
 	matchlistVars:delete('gsl')
 
 	return MatchGroup.MatchList(matchListArgs)
+end
+
+--- for bot conversion to proper match2 matchlists
+---@param frame Frame
+---@return string
+function MatchMapsLegacy.generate(frame)
+	local args = Arguments.getArgs(frame)
+
+	local store = Logic.readBoolOrNil(args.store)
+
+	local offset = 0
+	local title = args.title
+	if not title and not Json.parseIfTable(args[1]) then
+		title = args[1]
+		offset = 1
+	end
+
+	local parsedArgs = {
+		id = args.id,
+		title = title,
+		width = args.width or '320px',
+		collapsed = Logic.nilOr(Logic.readBoolOrNil(args.hide), true),
+		attached = Logic.nilOr(Logic.readBoolOrNil(args.hide), true),
+		store = store,
+		noDuplicateCheck = store == false or nil,
+		matchsection = args.matchsection,
+		patch = args.patch,
+	}
+
+	local gsl = args.gsl
+	if Logic.isNotEmpty(gsl) then
+		if gsl == GSL_WINNERS or gsl == GSL_LOSERS then
+			gsl = gsl .. 'first'
+			parsedArgs['gsl'] = gsl
+		end
+	end
+
+	---@type table[]
+	local matches = Array.mapIndexes(function(index)
+		return parsedArgs[index + offset]
+	end)
+
+	Array.forEach(matches, function(match, matchIndex)
+		parsedArgs['M' .. matchIndex] = match
+	end)
+
+	return MatchGroupLegacy.generateWikiCodeForMatchList(parsedArgs)
 end
 
 return MatchMapsLegacy
