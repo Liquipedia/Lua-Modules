95186b49a4420479ee2de1990e1166a05db9ff24
hjpalpha <andreasbuesch1@gmail.com>
2025-09-13 13:46:08 +0200

    warcraft

--- lua/wikis/warcraft/LegacyMatchMaps.lua
+++ lua/wikis/warcraft/LegacyMatchMaps.lua
@@ -15,6 +15,7 @@ local Json = Lua.import('Module:Json')
 local Logic = Lua.import('Module:Logic')
 local Match = Lua.import('Module:Match')
 local MatchGroup = Lua.import('Module:MatchGroup')
+local MatchGroupLegacy = Lua.import('Module:MatchGroup/Legacy')
 local Opponent = Lua.import('Module:Opponent/Custom')
 local PageVariableNamespace = Lua.import('Module:PageVariableNamespace')
 local Table = Lua.import('Module:Table')
@@ -29,6 +30,67 @@ local TBD = 'TBD'
 local BYE = 'BYE'
 
 local LegacyMatchMaps = {}
+
+--- for bot conversion to proper match2 matchlists
+---@param frame Frame
+---@return string
+function LegacyMatchMaps.generateSolo(frame)
+	local args = Arguments.getArgs(frame)
+
+	local store = Logic.readBoolOrNil(args.store)
+
+	local parsedArgs = {
+		id = args.id,
+		title = args.title,
+		width = args.width,
+		collapsed = Logic.nilOr(Logic.readBoolOrNil(args.hide), true),
+		attached = Logic.nilOr(Logic.readBoolOrNil(args.hide), true),
+		store = store,
+		noDuplicateCheck = store == false or nil,
+	}
+
+	for _, matchInput, matchIndex in Table.iter.pairsByPrefix(args, 'match') do
+		parsedArgs['M' .. matchIndex] = LegacyMatchMaps._readSoloMatch(matchInput)
+	end
+
+	return MatchGroupLegacy.generateWikiCodeForMatchList(parsedArgs)
+end
+
+--- for bot conversion to proper match2 matchlists
+---@param frame Frame
+---@return string
+function LegacyMatchMaps.generateTeam(frame)
+	local args = Arguments.getArgs(frame)
+
+	local store = Logic.readBoolOrNil(args.store)
+
+	local offset = 0
+	local title = args.title
+	if not title and not Json.parseIfTable(args[1]) then
+		title = args[1]
+		offset = 1
+	end
+
+	local parsedArgs = {
+		id = args.id,
+		title = title,
+		width = args.width or '350px',
+		collapsed = Logic.nilOr(Logic.readBoolOrNil(args.hide), true),
+		attached = Logic.nilOr(Logic.readBoolOrNil(args.hide), true),
+		store = store,
+		noDuplicateCheck = store == false or nil,
+	}
+
+	local matches = Array.mapIndexes(function(index)
+		return args[index + offset]
+	end)
+
+	Array.forEach(matches, function(match, matchIndex)
+		args['M' .. matchIndex] = Match.makeEncodedJson(match)
+	end)
+
+	return MatchGroupLegacy.generateWikiCodeForMatchList(parsedArgs)
+end
 
 -- invoked by Template:MatchList
 ---@param frame Frame
@@ -171,8 +233,11 @@ end
 
 -- invoked by Template:MatchMapsTeams
 ---@param frame Frame
+---@return string?
 function LegacyMatchMaps.teamMatch(frame)
 	local args = Arguments.getArgs(frame)
+
+	local generate = Logic.readBool(Table.extract(args, 'generate'))
 
 	args = Table.merge(Json.parseIfString(args.details) or {}, args)
 	args.details = nil
@@ -180,6 +245,10 @@ function LegacyMatchMaps.teamMatch(frame)
 	LegacyMatchMaps._readTeamOpponents(args)
 	--map data gets preprocessed already due to using the same template as in brackets
 	LegacyMatchMaps._readMaps(args)
+
+	if generate then
+		return Json.stringify(args)
+	end
 
 	Template.stashReturnValue(args, 'LegacyMatchlist')
 end
