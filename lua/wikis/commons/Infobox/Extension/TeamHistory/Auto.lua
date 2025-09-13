56f26cb441312e23ec44c54f43fe5c349ee404ae
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2025-09-13 21:04:58 +0200

    Update Auto.lua

--- lua/wikis/commons/Infobox/Extension/TeamHistory/Auto.lua
+++ lua/wikis/commons/Infobox/Extension/TeamHistory/Auto.lua
@@ -24,7 +24,7 @@ local TeamHistoryAuto = {}
 ---@return Widget?
 function TeamHistoryAuto.run(frame)
 	local args = Arguments.getArgs(frame)
-	local player = args.player or Page.applyUnderScoresIfEnforced(String.upperCaseFirst(mw.title.getCurrentTitle().subpageText))
+	local player = Page.applyUnderScoresIfEnforced(args.player or String.upperCaseFirst(mw.title.getCurrentTitle().subpageText))
 
 	local transferList = TransferModel.getTeamHistoryForPerson{player = player, specialRoles = SPECIAL_ROLES}
 
