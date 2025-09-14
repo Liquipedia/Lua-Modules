413088b846cf3e79576e63bb9d0d283e306e716b
hjpalpha <andreasbuesch1@gmail.com>
2025-09-14 17:57:15 +0200

    aoe singlematch

--- lua/wikis/commons/MatchGroup/Legacy.lua
+++ lua/wikis/commons/MatchGroup/Legacy.lua
@@ -515,7 +515,7 @@ end
 ---@param args table
 ---@return string
 function MatchGroupLegacy.generateWikiCodeForSingleMatch(args)
-	local matchJson = Table.extract(args, match)
+	local matchJson = Table.extract(args, 'match')
 	local match = matchJson
 	if type(matchJson) == 'string' then
 		match = Json.parseIfTable(matchJson)
