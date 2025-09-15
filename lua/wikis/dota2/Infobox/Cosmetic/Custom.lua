27c63d44e3756559e0d9aab537609fbad7f9bdb7
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2025-09-15 18:15:10 +0200

    linter: line too long

--- lua/wikis/dota2/Infobox/Cosmetic/Custom.lua
+++ lua/wikis/dota2/Infobox/Cosmetic/Custom.lua
@@ -160,7 +160,8 @@ function CustomCosmetic._buyNow(marketable, defindex)
 	if marketable and defindex then
 		link = 'https://steamcommunity.com/market/search/?q=appid:570+prop_def_index:'.. defindex
 	elseif marketable then
-		link = 'https://steamcommunity.com/market/search/?q=appid:570+' .. (mw.title.getCurrentTitle().fullText:gsub(' ', '_'))
+		link = 'https://steamcommunity.com/market/search/?q=appid:570+'
+			.. (mw.title.getCurrentTitle().fullText:gsub(' ', '_'))
 	else
 		return
 	end
