8a939720f77805f87157eb3f0b192f8e73dc5c38
GeneralHuber <61513963+GeneralHuber@users.noreply.github.com>
2025-09-19 20:37:07 +0200

    Remove bestof, winner and finished from generator

--- lua/wikis/marvelrivals/GetMatchGroupCopyPaste/wiki.lua
+++ lua/wikis/marvelrivals/GetMatchGroupCopyPaste/wiki.lua
@@ -33,9 +33,6 @@ function WikiCopyPaste.getMatchCode(bestof, mode, index, opponents, args)
 
 	local lines = Array.extendWith({},
 		'{{Match',
-		index == 1 and (INDENT .. '|bestof' .. (bestof ~= 0 and bestof or '')) or nil,
-		Logic.readBool(args.needsWinner) and (INDENT .. '|winner=') or nil,
-		Logic.readBool(args.isFinished) and (INDENT .. '|finished=') or nil,
 		INDENT .. '|date=',
 		streams and (INDENT .. '|twitch=|youtube=|vod=') or nil,
 		casters and (INDENT .. '|caster1=|caster2=') or nil,
