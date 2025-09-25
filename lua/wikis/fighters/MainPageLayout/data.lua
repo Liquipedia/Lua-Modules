516dadf98448092565367c3c13311d60bf778151
Jasper Jacobs <97167868+Kanopedia@users.noreply.github.com>
2025-09-25 13:05:13 +0200

    feat(mainpage): Add Fighters Main Page (#6595)
    * feat(mainpage): Add Fighters Main Page
    
    * Update Banner.scss
    
    * Update data.lua
    
    * Update data.lua
    
    * Update lua/wikis/fighters/MainPageLayout/data.lua
    
    Co-authored-by: SlothyMan <76825878+SlothyMan@users.noreply.github.com>
    
    * Update lua/wikis/fighters/MainPageLayout/data.lua
    
    Co-authored-by: SlothyMan <76825878+SlothyMan@users.noreply.github.com>
    
    * Update lua/wikis/fighters/MainPageLayout/data.lua
    
    Co-authored-by: SlothyMan <76825878+SlothyMan@users.noreply.github.com>
    
    * Update lua/wikis/fighters/MainPageLayout/data.lua
    
    Co-authored-by: ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
    
    * Update data.lua
    
    * Update data.lua
    
    * Update lua/wikis/fighters/MainPageLayout/data.lua
    
    Co-authored-by: ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: SlothyMan <76825878+SlothyMan@users.noreply.github.com>
    Co-authored-by: ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>

---
+++ lua/wikis/fighters/MainPageLayout/data.lua
@@ -0,0 +1,178 @@
+-- @Liquipedia
+-- page=Module:MainPageLayout/data
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Lua = require('Module:Lua')
+
+local HtmlWidgets = Lua.import('Module:Widget/Html/All')
+local Div = HtmlWidgets.Div
+local Link = Lua.import('Module:Widget/Basic/Link')
+local ThisDayWidgets = Lua.import('Module:Widget/MainPage/ThisDay')
+local TransfersList = Lua.import('Module:Widget/MainPage/TransfersList')
+local WantToHelp = Lua.import('Module:Widget/MainPage/WantToHelp')
+
+local CONTENT = {
+	usefulArticles = {
+		heading = 'The Fighting Game Community',
+		body = '{{Liquipedia:Useful Articles}}',
+		padding = true,
+		boxid = 1503,
+	},
+	wantToHelp = {
+		heading = 'Want To Help?',
+		body = WantToHelp{},
+		padding = true,
+		boxid = 1504,
+	},
+	transfers = {
+		heading = 'Transfers',
+		body = TransfersList{
+			limit = 10,
+			transferPage = 'Player Transfers/' .. os.date('%Y')
+		},
+		boxid = 1509,
+	},
+	thisDay = {
+		heading = ThisDayWidgets.Title(),
+		body = ThisDayWidgets.Content(),
+		padding = true,
+		boxid = 1510,
+	},
+	specialEvents = {
+		noPanel = true,
+		body = '{{Liquipedia:Special_Event}}',
+		boxid = 1516,
+	},
+	tournaments = {
+		heading = 'Tournaments',
+		body = Div{
+			children = {
+				mw.getCurrentFrame():preprocess('<tournaments />'),
+				Div{
+					classes = {'mainpage-editlink'},
+					children = {
+						mw.text.nowiki('['),
+						Link{
+							link = 'Special:EditPage/Liquipedia:Tournaments',
+							children = mw.text.nowiki('edit')
+						},
+						mw.text.nowiki(']'),
+					}
+				}
+			}
+		},
+		padding = true,
+		boxid = 1508,
+	},
+}
+
+return {
+	banner = {
+		lightmode = 'Fighters-lightmode.svg',
+		darkmode = 'Fighters-darkmode.svg',
+	},
+	metadesc = 'The Fighting Games wiki covering everything from players, teams and transfers, to ' ..
+		'tournaments, strategies, games, and characters.',
+	title = 'Fighting Games',
+	navigation = {
+		{
+			file = 'Arslan Ash at EVO 2024.jpg',
+			title = 'Players',
+			link = 'Portal:Players',
+			count = {
+				method = 'LPDB',
+				table = 'player',
+			},
+		},
+		{
+			file = 'Bandits Gaming winning the 2023 SFL World Championship.jpg',
+			title = 'Teams',
+			link = 'Portal:Teams',
+			count = {
+				method = 'LPDB',
+				table = 'team',
+			},
+		},
+		{
+			file = 'EVO 2023 Crowd Shot.jpg',
+			title = 'Tournaments',
+			link = 'Portal:Tournaments',
+			count = {
+				method = 'LPDB',
+				table = 'tournament',
+			},
+		},
+			{
+			file = 'SonicFox and GO1 at EVO 2019.jpg',
+			title = 'Recent Results',
+			link = 'Recent_Tournament_Results',
+		},
+		{
+			file = 'Mago_Daigo_Tokido_Team_Madcatz.jpg',
+			title = 'Transfers',
+			link = 'Portal:Transfers',
+			count = {
+				method = 'LPDB',
+				table = 'transfer',
+			},
+		},
+		{
+			file = 'Daigo_Umehara_LG_Cup_2012.jpg',
+			title = 'Fighting Games',
+			link = 'Portal:Fighting_Games',
+		},
+	},
+	layouts = {
+		main = {
+			{ -- Left
+				size = 7,
+				children = {
+					{
+						mobileOrder = 1,
+						content = CONTENT.specialEvents,
+					},
+					{
+						mobileOrder = 3,
+						content = CONTENT.transfers,
+					},
+					{
+						mobileOrder = 4,
+						content = CONTENT.thisDay,
+					},
+					{
+						mobileOrder = 5,
+						content = CONTENT.wantToHelp,
+					},
+				}
+			},
+			{ -- Right
+				size = 5,
+				children = {
+					{
+						mobileOrder = 2,
+						children = {
+							{
+								children = {
+									{
+										noPanel = true,
+										content = CONTENT.tournaments,
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			{ -- Bottom
+				children = {
+					{
+						mobileOrder = 6,
+						content = CONTENT.usefulArticles,
+					},
+				},
+			},
+		},
+	},
+}
