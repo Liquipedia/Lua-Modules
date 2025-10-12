6d44f6b97c5b02df1c83c1f73079e6dedb8176bb
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2025-10-12 12:46:31 +0900

    use hybrid teamStyle in headtohead

--- lua/wikis/commons/MatchPage/Base.lua
+++ lua/wikis/commons/MatchPage/Base.lua
@@ -492,6 +492,7 @@ function BaseMatchPage:_buildHeadToHeadMatchTable()
 		team = self.opponents[1].name,
 		vsteam = self.opponents[2].name,
 		showOpponent = true,
+		teamStyle = 'hybrid',
 		useTickerName = true,
 	}
 end
