5871642068d4cead524759acd641391648d8c1ad
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2025-10-13 13:29:20 +0900

    add streampage impl

---
+++ lua/wikis/commons/StreamPage/Team.lua
@@ -0,0 +1,258 @@
+---
+-- @Liquipedia
+-- page=Module:StreamPage/Team
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Lua = require('Module:Lua')
+
+local Array = Lua.import('Module:Array')
+local Class = Lua.import('Module:Class')
+local Countdown = Lua.import('Module:Countdown')
+local DateExt = Lua.import('Module:Date/Ext')
+local HighlightConditions = Lua.import('Module:HighlightConditions')
+local Logic = Lua.import('Module:Logic')
+local MatchGroupUtil = Lua.import('Module:MatchGroup/Util/Custom')
+local MatchTable = Lua.import('Module:MatchTable/Custom')
+local MatchTicker = Lua.import('Module:MatchTicker')
+local Opponent = Lua.import('Module:Opponent/Custom')
+local OpponentDisplay = Lua.import('Module:OpponentDisplay/Custom')
+local Page = Lua.import('Module:Page')
+local PlayerDisplay = Lua.import('Module:Player/Display/Custom')
+local String = Lua.import('Module:StringUtils')
+local TeamTemplate = Lua.import('Module:TeamTemplate')
+
+local HtmlWidgets = Lua.import('Module:Widget/Html/All')
+local MatchPageHeader = Lua.import('Module:Widget/Match/Page/Header')
+local WidgetUtil = Lua.import('Module:Widget/Util')
+
+local StreamPage = {}
+
+---@param args {channel: string, provider: string}
+---@return Widget?
+function StreamPage.create(args)
+	if String.isEmpty(args.channel) or String.isEmpty(args.provider) then
+		return
+	end
+
+	local matches = StreamPage._getMatches(args)
+	if #matches == 0 then
+		return
+	end
+	local match = matches[1]
+
+	return HtmlWidgets.Fragment{children = {
+		'__NOTOC__',
+		StreamPage._panel(match),
+		StreamPage._opponentPlayerTable(match.opponents[1], 'left'),
+		StreamPage._opponentPlayerTable(match.opponents[2], 'right'),
+		HtmlWidgets.Div{
+			css = {
+				display = 'inline-block',
+				padding = '0.5rem',
+				width = 'calc( 100% - 454px )',
+				['min-width'] = 'min(100%, max(500px, calc( 100% - 454px)))',
+			},
+			children = WidgetUtil.collect(
+				StreamPage.displayUpcomingMatches(args, match),
+				HtmlWidgets.H3{children = 'Head to Head'},
+				MatchTable.results{
+					limit = 5,
+					edate = match.timestamp - 86400,
+					showOpponent = true,
+					tableMode = Opponent.team,
+					team = match.opponents[1].name,
+					vsteam = match.opponents[2].name,
+				}
+			)
+		},
+	}}
+end
+
+---@param args table
+---@param match MatchGroupUtilMatch
+---@return (Widget|Html)[]
+function StreamPage.displayUpcomingMatches(args, match)
+	local ticker = MatchTicker{
+		additionalConditions = 'AND ([[stream_' .. args.provider .. '::' .. args.channel .. ']]' ..
+								' OR [[stream_' .. args.provider .. '_en_1::' .. args.channel .. ']])' ..
+								' AND ([[date::' .. match.timestamp .. ']] OR [[date::>' .. match.timestamp .. ']])',
+		limit = 5,
+		ongoing = true,
+		upcoming = true,
+	}
+	return {
+		HtmlWidgets.H3{children = 'Channel Schedule'},
+		ticker:query():create()
+	}
+end
+
+---@param opponent standardOpponent
+---@param float string
+---@return Html
+function StreamPage._opponentPlayerTable(opponent, float)
+	local display = mw.html.create('table')
+		:addClass('table')
+		:addClass('wikitable')
+		:css('float', float)
+		:css('margin', '0')
+		:css('width', '227px')
+	local header = mw.html.create('tr')
+		:addClass('wiki-backgroundcolor-light')
+		:css('font-size', '150%')
+		:tag('th')
+			--:css('background-color', '#c1dfdf !important') -- todo replace with css (make it wiki color)
+			:css('padding', '5px')
+			:wikitext('Players'):done():done()
+	display:node(header)
+
+	for _, player in ipairs(opponent.players) do
+		StreamPage._playerDisplay(player, display)
+	end
+	-- fill up with TBD players
+	if #opponent.players < 5 then
+		for index = #opponent.players, 5 do
+			StreamPage._playerDisplay({displayName = 'TBD'}, display)
+		end
+	end
+
+	return display
+end
+
+---@param player standardPlayer
+---@param display Html
+function StreamPage._playerDisplay(player, display)
+	local lpdbData = mw.ext.LiquipediaDB.lpdb('player', {
+		conditions = '[[pagename::' .. (Page.pageifyLink(player.pageName) or '') .. ']]',
+		limit = 1
+	})[1]
+
+	local playerData = {}
+	local image
+	if lpdbData then
+		playerData = lpdbData
+		image = playerData.image
+		if String.isEmpty(image) then
+			image = (playerData.extradata or {}).image
+		end
+	end
+	if String.isEmpty(image) then
+		image = 'Blank Player Image.png'
+	end
+	local imageRow = mw.html.create('tr')
+		:tag('td')
+			:css('padding','0')
+			:css('text-align','center')
+			:wikitext('[[File:' .. image .. '|225x150px|link=]]'):done():done()
+	display:node(imageRow)
+
+	local nameDisplay = PlayerDisplay.InlinePlayer{
+		player = player
+	}
+	local nameRow = mw.html.create('tr')
+		:tag('th')
+			:css('position','absolute')
+			:css('margin-top','-32px')
+			:css('width','225px')
+			:css('opacity','95%')
+			:node(nameDisplay):done():done()
+	display:node(nameRow)
+end
+
+---@param match MatchGroupUtilMatch
+---@return Html
+function StreamPage._panel(match)
+	local countdownBlock = not DateExt.isDefaultTimestamp(match.timestamp) and HtmlWidgets.Div{
+		css = {
+			display = 'block',
+			['text-align'] = 'center'
+		},
+		children = Countdown.create{
+			date = DateExt.toCountdownArg(match.timestamp, match.timezoneId, match.dateIsExact),
+			finished = match.finished,
+			rawdatetime = Logic.readBool(match.finished),
+		}
+	} or nil
+	local tournament = StreamPage._getTournamentData(match.parent) or {}
+	Array.forEach(match.opponents, function (opponent, opponentIndex)
+		---@cast opponent MatchPageOpponent
+		opponent.iconDisplay = OpponentDisplay.InlineTeamContainer{
+			style = 'icon',
+			template = opponent.template,
+		}
+		opponent.teamTemplateData = TeamTemplate.getRaw(opponent.template)
+		opponent.seriesDots = {}
+	end)
+	return MatchPageHeader{
+		countdownBlock = countdownBlock,
+		isBestOfOne = match.bestof == 1,
+		mvp = match.extradata.mvp,
+		opponent1 = match.opponents[1],
+		opponent2 = match.opponents[2],
+		parent = match.parent,
+		phase = MatchGroupUtil.computeMatchPhase(match),
+		stream = match.stream,
+		tournamentName = match.tournament,
+		highlighted = HighlightConditions.tournament(tournament)
+	}
+end
+
+---@param parent string
+---@return tournament?
+function StreamPage._getTournamentData(parent)
+	return mw.ext.LiquipediaDB.lpdb('tournament', {
+		conditions = '[[pagename::' .. parent .. ']]',
+		limit = 1,
+	})[1]
+end
+
+---@param args table
+---@return MatchGroupUtilMatch[]
+function StreamPage._getMatches(args)
+	local conditions = {
+		'([[stream_' .. args.provider .. '::' .. args.channel .. ']] OR [[stream_' .. args.provider .. '_en_1::' .. args.channel .. ']])',
+		'[[finished::0]]',
+		'[[date::!' .. DateExt.defaultDateTime .. ']]',
+		'[[dateexact::1]]',
+		--'[[status::!cancelled]]',
+		--'[[status::!postponed]]',
+		-- possibly a condition to make it only retrieve team matches?
+	}
+
+	local data = Array.map(mw.ext.LiquipediaDB.lpdb('match2', {
+		conditions = table.concat (conditions, ' AND '),
+		order = 'date asc',
+		limit = 25, --have to kick bye matches later on, hence more matches queried here
+	}), MatchGroupUtil.matchFromRecord)
+
+	if type(data) == 'table' and type(data[1]) == 'table' then
+		return StreamPage._removeByeMatches(data)
+	end
+
+	return {}
+end
+
+---@param data MatchGroupUtilMatch[]
+---@return MatchGroupUtilMatch[]
+function StreamPage._removeByeMatches(data)
+	local matches = {}
+	for _, match in ipairs(data) do
+		if StreamPage._matchHasNoBye(match) then
+			table.insert(matches, match)
+		end
+		if #matches == 5 then
+			break
+		end
+	end
+
+	return matches
+end
+
+---@param match MatchGroupUtilMatch
+---@return boolean
+function StreamPage._matchHasNoBye(match)
+	return not Array.any(match.opponents, Opponent.isBye)
+end
+
+return Class.export(StreamPage, {exports = {'create'}})
