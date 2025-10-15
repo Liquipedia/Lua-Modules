6efdf4e9bf5e93963c364cda95699cf875b907f7
Jasper Jacobs <97167868+Kanopedia@users.noreply.github.com>
2025-10-15 12:32:44 +0200

    feat(mainpage): Add Commons Main Page

---
+++ lua/wikis/commons/MainPageLayout/data.lua
@@ -0,0 +1,82 @@
+---
+-- @Liquipedia
+-- page=Module:MainPageLayout/data
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Lua = require('Module:Lua')
+
+local Link = Lua.import('Module:Widget/Basic/Link')
+local HtmlWidgets = Lua.import('Module:Widget/Html/All')
+
+local CONTENT = {
+	usefulArticles = {
+		heading = 'About This Wiki',
+		body = '{{Liquipedia:Useful Articles}}',
+		padding = true,
+		boxid = 1503,
+	},
+	latestUploads = {
+		heading = 'Latest Uploads',
+		body = '{{Special:NewFiles|limit=18}}',
+		padding = true,
+		boxid = 1556,
+	},
+}
+return {
+	banner = {
+		lightmode = 'Commons-light-theme.svg',
+		darkmode = 'Commons-dark-theme.svg',
+	},
+	metadesc = 'The Commons wiki is the file repository for Liquipedia',
+	title = 'Commons',
+	navigation = {
+		{
+			iconName = 'upload',
+			title = 'Upload',
+			link = 'Special:Upload',
+			count = {},
+		},
+		{
+			iconName = 'file_standards_guide',
+			title = 'File Standards Guide',
+			link = 'File_Standards_Guide',
+			count = {},
+		},
+		{
+			iconName = 'clipboard',
+			title = 'Copyright Repository',
+			link = 'Copyrights_Repository',
+			count = {},
+		},
+		{
+			iconName = 'copyright',
+			title = 'Copyright Guide',
+			link = 'hub:A_Liquipedia_Guide_to_Copyright',
+			count = {},
+		},
+		{
+			iconName = 'find_images',
+			title = 'Find Files',
+			link = 'Special:RunQuery/Find_images',
+			count = {},
+		},
+	},
+	layouts = {
+		main = {
+			{
+				children = {
+					{
+						mobileOrder = 1,
+						content = CONTENT.usefulArticles,
+					},
+					{
+						mobileOrder = 2,
+						content = CONTENT.latestUploads,
+					},
+				},
+			},
+		},
+	},
+}
