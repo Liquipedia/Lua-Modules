11d42eb4a9895ea90f1bb976af25cc8699ed2acc
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2025-10-15 10:47:48 +0900

    do age calculation the long way

--- lua/wikis/commons/AgeCalculation.lua
+++ lua/wikis/commons/AgeCalculation.lua
@@ -166,7 +166,7 @@ function Age:calculate()
 
 	if self.birthDate.isExact and (self.deathDate.isExact or self.deathDate.isEmpty) then
 		---@cast endDate -nil
-		return self:_secondsToAge(os.difftime(endDate, self.birthDate.time))
+		return self:_calculateAge(endDate, self.birthDate.time)
 	elseif self.birthDate.year and (self.deathDate.year or self.deathDate.isEmpty) then
 		local minEndDate
 		local maxEndDate
@@ -178,8 +178,8 @@ function Age:calculate()
 			maxEndDate = self.deathDate:getLatestPossible()
 		end
 
-		local minAge = self:_secondsToAge(os.difftime(minEndDate, self.birthDate:getLatestPossible()))
-		local maxAge = self:_secondsToAge(os.difftime(maxEndDate, self.birthDate:getEarliestPossible()))
+		local minAge = self:_calculateAge(minEndDate, self.birthDate:getLatestPossible())
+		local maxAge = self:_calculateAge(maxEndDate, self.birthDate:getEarliestPossible())
 
 		-- If our min and max age values are identical, then we can just display that value. Else,
 		-- we have to display a range of ages
@@ -216,10 +216,23 @@ function Age:makeDisplay()
 	return result
 end
 
----@param seconds integer
+---@param toTimestamp integer
+---@param fromTimestamp integer
 ---@return integer
-function Age:_secondsToAge(seconds)
-	return math.floor(seconds / 60 / 60 / 24 / 365.2425)
+function Age:_calculateAge(toTimestamp, fromTimestamp)
+	local toDate = os.date('*t', toTimestamp) --[[@as osdate]]
+	local fromDate = os.date('*t', fromTimestamp) --[[@as osdate]]
+
+	local age = toDate - fromDate
+
+	local monthDiff = toDate.month - fromDate.month
+	local dayDiff = toDate.day - fromDate.month
+
+	if monthDiff > 0 or (monthDiff == 0 and dayDiff >= 0) then
+		--- birthday passed
+		return age
+	end
+	return age - 1
 end
 
 ---@param args table
