db06c08382b43fc94e80dcfa416420b80a1e2ad4
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2025-10-17 19:00:48 +0900

    use enum in analytics mapping too

--- lua/wikis/commons/MainPageLayout/AnalyticsMapping.lua
+++ lua/wikis/commons/MainPageLayout/AnalyticsMapping.lua
@@ -5,20 +5,24 @@
 -- Please see https://github.com/Liquipedia/Lua-Modules to contribute
 --
 
+local Lua = require('Module:Lua')
+
+local MainPageLayoutUtil = Lua.import('Module:MainPageLayout/Util')
+local BoxId = MainPageLayoutUtil.BoxId
+
 ---@type table<integer, string>
 local AnalyticsMapping = {
 	[1501] = 'Heroes panel',
 	[1502] = 'Updates panel',
-	[1503] = 'Useful articles panel',
-	[1504] = 'Want to help panel',
+	[BoxId.USEFUL_ARTICLES] = 'Useful articles panel',
+	[BoxId.WANT_TO_HELP] = 'Want to help panel',
 	[1505] = 'Mobile app panel',
-	[1507] = 'Matchticker',
-	[1508] = 'Tournament ticker',
-	[1509] = 'Transfers panel',
-	[1510] = 'This day panel',
-	[1516] = 'Featured panel',
+	[BoxId.MATCH_TICKER] = 'Matchticker',
+	[BoxId.TOURNAMENTS_TICKER] = 'Tournament ticker',
+	[BoxId.TRANSFERS] = 'Transfers panel',
+	[BoxId.THIS_DAY] = 'This day panel',
+	[BoxId.SPECIAL_EVENTS] = 'Featured panel',
 	[1520] = 'Rankings panel',
 }
 
 return AnalyticsMapping
-
