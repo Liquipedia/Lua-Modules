6d9a19c8423e455fde93a1db710f652a0e867167
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2025-10-19 11:48:26 +0900

    add lol entrypoint

--- lua/wikis/leagueoflegends/CharacterStats/Custom.lua
+++ lua/wikis/leagueoflegends/CharacterStats/Custom.lua
@@ -7,12 +7,16 @@
 
 local Lua = require('Module:Lua')
 
+local Arguments = Lua.import('Module:Arguments')
 local Array = Lua.import('Module:Array')
 local BaseCharacterStats = Lua.import('Module:CharacterStats')
 local Class = Lua.import('Module:Class')
 local String = Lua.import('Module:StringUtils')
 
+local CharacterStatsWidget = Lua.import('Module:Widget/CharacterStats')
+
 ---@class LoLCharacterStats: CharacterStats
+---@operator call(table): LoLCharacterStats
 local LoLCharacterStats = Class.new(BaseCharacterStats)
 
 ---@param game CharacterStatsGame
@@ -43,6 +47,25 @@ end
 ---@return string[]
 function LoLCharacterStats:getSides()
 	return {'blue', 'red'}
+end
+
+---@param frame Frame
+---@return Widget
+function LoLCharacterStats.run(frame)
+	local stats = LoLCharacterStats(Arguments.getArgs(frame))
+
+	local conditions = stats:buildConditions()
+	local matchIds = stats:getMatchIds(conditions)
+	local games = stats:queryGames(matchIds)
+	local processedData = stats:processGames(games)
+	return CharacterStatsWidget{
+		characterType = 'Champion',
+		data = processedData.characterData,
+		includeBans = true,
+		numGames = #games,
+		sides = stats:getSides(),
+		sideWins = processedData.overall.wins,
+	}
 end
 
 return LoLCharacterStats
