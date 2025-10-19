73463289da361146e234e01dc6ae5b577a25f710
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2025-10-19 14:49:19 +0900

    add popup

--- lua/wikis/commons/Widget/CharacterStats/Table.lua
+++ lua/wikis/commons/Widget/CharacterStats/Table.lua
@@ -10,12 +10,17 @@ local Lua = require('Module:Lua')
 local Array = Lua.import('Module:Array')
 local CharacterIcon = Lua.import('Module:CharacterIcon')
 local Class = Lua.import('Module:Class')
+local DateExt = Lua.import('Module:Date/Ext')
 local Logic = Lua.import('Module:Logic')
 local MathUtil = Lua.import('Module:MathUtil')
+local OpponentDisplay = Lua.import('Module:OpponentDisplay/Custom')
 local String = Lua.import('Module:StringUtils')
+local Table = Lua.import('Module:Table')
 
 local Widget = Lua.import('Module:Widget')
 local HtmlWidgets = Lua.import('Module:Widget/Html/All')
+local DetailsPopup = Lua.import('Module:Widget/CharacterStats/DetailsPopup')
+local DetailsPopupContainer = Lua.import('Module:Widget/CharacterStats/DetailsPopup/Container')
 local WidgetUtil = Lua.import('Module:Widget/Util')
 
 ---@class CharacterStatsTable: Widget
@@ -45,7 +50,7 @@ function CharacterStatsTable:render()
 				self:_buildFooterRow()
 			)
 		}
-    }
+	}
 end
 
 ---@private
@@ -146,8 +151,115 @@ function CharacterStatsTable:_buildCharacterRow(characterData, characterIndex)
 					characterData.total.pick + characterData.bans, self.props.numGames
 				)}
 			} or nil,
-			HtmlWidgets.Td{children = '-'}
+			HtmlWidgets.Td{children = DetailsPopupContainer{
+				children = DetailsPopup{
+					header = CharacterIcon.Icon{
+						character = characterData.name,
+						size = self.props.characterSize,
+						addTextLink = true
+					},
+					children = HtmlWidgets.Div{
+						classes = {'dota-stat-popup-info'},
+						css = {
+							display = 'flex',
+							gap = '10px',
+							['justify-content'] = 'center',
+							['flex-wrap'] = 'wrap',
+							margin = '10px',
+						},
+						children = {
+							CharacterStatsTable._buildPlayedByTeamTable(characterData.playedBy),
+							self:_buildPlayedTable('with', characterData.playedWith),
+							self:_buildPlayedTable('against', characterData.playedVs)
+						}
+					}
+				}
+			}}
 		)
+	}
+end
+
+---@param data table<string, CharacterAppearanceStats>
+function CharacterStatsTable._buildPlayedByTeamTable(data)
+	local sortedTeamData = Array.sortBy(Table.entries(data), function (element)
+		return element[2].pick
+	end)
+	return CharacterStatsTable._buildDetailsTable{
+		title = 'Played by Teams',
+		entryType = 'Team',
+		entries = Array.map(Array.sub(Array.reverse(sortedTeamData), 1, 5), function (teamData, index)
+			return {
+				index,
+				OpponentDisplay.InlineTeamContainer{
+					template = teamData[1],
+					date = DateExt.getContextualDateOrNow(),
+					style = 'short'
+				},
+				teamData[2].pick,
+				teamData[2].win,
+				teamData[2].loss,
+				CharacterStatsTable._calculatePercentage(teamData[2].win, teamData[2].pick)
+			}
+		end)
+	}
+end
+
+---@param data table<string, CharacterAppearanceStats>
+function CharacterStatsTable:_buildPlayedTable(playedType, data)
+	local sortedCharacterData = Array.sortBy(Table.entries(data), function (element)
+		return element[2].pick
+	end)
+	return CharacterStatsTable._buildDetailsTable{
+		title = 'Played ' .. playedType,
+		entryType = String.upperCaseFirst(self.props.characterType),
+		entries = Array.map(Array.sub(Array.reverse(sortedCharacterData), 1, 5), function (characterData, index)
+			return {
+				index,
+				CharacterIcon.Icon{
+					character = characterData[1],
+					size = self.props.characterSize,
+					addTextLink = true
+				},
+				characterData[2].pick,
+				characterData[2].win,
+				characterData[2].loss,
+				CharacterStatsTable._calculatePercentage(characterData[2].win, characterData[2].pick)
+			}
+		end)
+	}
+end
+
+---@private
+---@param props table
+---@return Widget
+function CharacterStatsTable._buildDetailsTable(props)
+	return HtmlWidgets.Div{
+		css = {flex = 'auto'},
+		children = HtmlWidgets.Table{
+			classes = {'wikitable', 'wikitable-striped', 'sortable'},
+			css = {width = '100%'},
+			children = WidgetUtil.collect(
+				Logic.isNotEmpty(props.title) and HtmlWidgets.Tr{
+					children = HtmlWidgets.Th{
+						attributes = {colspan = 6},
+						children = props.title
+					}
+				} or nil,
+				HtmlWidgets.Tr{children = {
+					HtmlWidgets.Th{},
+					HtmlWidgets.Th{children = props.entryType},
+					HtmlWidgets.Th{children = '∑'},
+					HtmlWidgets.Th{children = 'W'},
+					HtmlWidgets.Th{children = 'L'},
+					HtmlWidgets.Th{children = 'WR'}
+				}},
+				Array.map(props.entries, function (entry)
+					return HtmlWidgets.Tr{children = Array.map(entry, function (data)
+						return HtmlWidgets.Td{children = data}
+					end)}
+				end)
+			)
+		}
 	}
 end
 
