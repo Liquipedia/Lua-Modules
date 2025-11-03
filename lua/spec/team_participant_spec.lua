9eda36c7371aeedda437bfba3c408eec9b64dd5c
Rikard Blixt <rikardblixt@gmail.com>
2025-11-03 14:56:11 +0100

    visual test

---
+++ lua/spec/team_participant_spec.lua
@@ -0,0 +1,16 @@
+--- Triple Comment to Enable our LLS Plugin
+insulate('Team Participant', function()
+	it('integration tests', function()
+		local LpdbQuery = stub(mw.ext.LiquipediaDB, 'lpdb', function(tbl)
+			if tbl == 'placement' then
+				return require('test_assets/lpdb_placement')
+			end
+			return {}
+		end)
+		local TeamParticipantCardGroup = require('Module:Widget/Participants/Team/CardsGroup')
+
+		GoldenTest('team_participant', tostring(TeamParticipantCardGroup{pageName = 'Six_Lounge_Series/4/Online_Stage'}))
+
+		LpdbQuery:revert()
+	end)
+end)
