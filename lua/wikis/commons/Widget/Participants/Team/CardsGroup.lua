065c364861a9fb87c0b736baa089bc2fbddff6f2
Eetu Rantanen <eeturantanen.dev@gmail.com>
2025-11-05 16:32:34 +0200

    expanded variant & qualifier info

--- lua/wikis/commons/Widget/Participants/Team/CardsGroup.lua
+++ lua/wikis/commons/Widget/Participants/Team/CardsGroup.lua
@@ -12,17 +12,28 @@ local Class = Lua.import('Module:Class')
 local String = Lua.import('Module:StringUtils')
 local OpponentDisplay = Lua.import('Module:OpponentDisplay/Custom')
 local TeamParticipantsRepository = Lua.import('Module:TeamParticipants/Repository')
+local LeagueIcon = Lua.import('Module:LeagueIcon')
+local Tournament = Lua.import('Module:Tournament')
 
 local Widget = Lua.import('Module:Widget')
 local WidgetUtil = Lua.import('Module:Widget/Util')
 local AnalyticsWidget = Lua.import('Module:Widget/Analytics')
 local HtmlWidgets = Lua.import('Module:Widget/Html/All')
+local Link = Lua.import('Module:Widget/Basic/Link')
 local Div = HtmlWidgets.Div
 local IconFa = Lua.import('Module:Widget/Image/Icon/Fontawesome')
 
+---@class ParticipantsTeamCardParameters
+---@field pageName string
+---@field variant 'compact'|'expanded'|nil
+
 ---@class ParticipantsTeamCard: Widget
----@operator call(table): ParticipantsTeamCard
+---@operator call(ParticipantsTeamCardParameters): ParticipantsTeamCard
+---@field props ParticipantsTeamCardParameters
 local ParticipantsTeamCard = Class.new(Widget)
+ParticipantsTeamCard.defaultProps = {
+	variant = 'compact',
+}
 
 ---@return Widget?
 function ParticipantsTeamCard:render()
@@ -37,7 +48,17 @@ function ParticipantsTeamCard:render()
 			classes = { 'team-participant-cards' },
 			children = Array.map(participants, function(participant, index)
 				local header = self:_renderHeader(participant)
-				local content = self:_renderContent(participant)
+				local qualifierBox = self:_renderQualifierBox(participant)
+
+				local content = self:_renderContent(participant, qualifierBox)
+
+				local cardChildren = { header }
+
+				if self.props.variant == 'expanded' then
+					table.insert(cardChildren, qualifierBox)
+				end
+
+				table.insert(cardChildren, content)
 
 				return Div{
 					classes = { 'team-participant-card', 'is--collapsed' }, -- Hardcoded collapsed state until we implement the js
@@ -45,7 +66,7 @@ function ParticipantsTeamCard:render()
 						['data-component'] = 'team-participant-card',
 						['data-team-participant-card-id'] = index
 					},
-					children = { header, content }
+					children = WidgetUtil.collect(cardChildren)
 				}
 			end),
 		}
@@ -57,10 +78,16 @@ end
 ---@return Widget
 function ParticipantsTeamCard:_renderHeader(participant)
 	local labelDiv = self:_renderLabel(participant)
+	local variant = self.props.variant
+	local headerClasses = { 'team-participant-card-header' }
+
+	if variant == 'expanded' then
+		table.insert(headerClasses, 'team-participant-card-header--expanded')
+	end
 
 	-- TODO: Implement the non-compact version
 	return Div{
-		classes = { 'team-participant-card-header' },
+		classes = headerClasses,
 			attributes = {
 				tabindex = '0',
 				['data-component'] = 'team-participant-card-collapsible-button'
@@ -107,15 +134,55 @@ end
 
 ---@private
 ---@param participant TeamParticipantsEntity
+---@return Widget?
+function ParticipantsTeamCard:_renderQualifierBox(participant)
+	local qualifierPage = participant.qualifierPage
+
+	if not String.isNotEmpty(qualifierPage) then
+		return
+	end
+
+	return Div{
+		classes = { 'team-participant-card-qualifier' },
+		children = {
+			LeagueIcon.display{
+				icon = Tournament.icon(qualifierPage),
+				iconDark = Tournament.darkIcon(qualifierPage),
+				series = Tournament.series(qualifierPage),
+				link = qualifierPage,
+				options = {noTemplate = true},
+			},
+			Div{
+				classes = { 'team-participant-card-qualifier-details' },
+				children = Link{
+					link = qualifierPage,
+					children = participant.qualifierText
+				}
+			}
+		}
+	}
+end
+
+---@private
+---@param participant TeamParticipantsEntity
+---@param qualifierBox Widget?
 ---@return Widget
-function ParticipantsTeamCard:_renderContent(participant)
-	-- TODO: Implement qualifier box, roster functionality & notes
+function ParticipantsTeamCard:_renderContent(participant, qualifierBox)
+	local contentChildren = {}
+
+	if self.props.variant == 'compact' then
+		table.insert(contentChildren, qualifierBox)
+	end
+
+	-- TODO: Roster functionality & notes
+	table.insert(contentChildren, participant.opponent.name) -- Placeholder until proper content is added
+
 	return Div{
 		classes = { 'team-participant-card-collapsible-content' },
 		attributes = {
 			['data-component'] = 'team-participant-card-content'
 		},
-		children = { participant.opponent.name } -- Team details & roster here
+		children = WidgetUtil.collect(contentChildren)
 	}
 end
 
