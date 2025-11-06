8539258536639803997daaf93fbf40ab57ac2506
Eetu Rantanen <eeturantanen.dev@gmail.com>
2025-11-06 13:53:05 +0200

    extract single card to own component

---
+++ lua/wikis/commons/Widget/Participants/Team/Card.lua
@@ -0,0 +1,109 @@
+---
+-- @Liquipedia
+-- page=Module:Widget/Participants/Team/Card
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Lua = require('Module:Lua')
+
+local Class = Lua.import('Module:Class')
+local String = Lua.import('Module:StringUtils')
+local OpponentDisplay = Lua.import('Module:OpponentDisplay/Custom')
+
+local Widget = Lua.import('Module:Widget')
+local WidgetUtil = Lua.import('Module:Widget/Util')
+local HtmlWidgets = Lua.import('Module:Widget/Html/All')
+local Div = HtmlWidgets.Div
+local IconFa = Lua.import('Module:Widget/Image/Icon/Fontawesome')
+
+---@class ParticipantsTeamCard: Widget
+---@operator call(table): ParticipantsTeamCard
+local ParticipantsTeamCard = Class.new(Widget)
+
+---@return Widget
+function ParticipantsTeamCard:render()
+	local participant = self.props.participant
+
+	local header = self:_renderHeader(participant)
+	local content = self:_renderContent(participant)
+
+	return Div{
+		classes = { 'team-participant-card', 'is--collapsed' }, -- Hardcoded collapsed state until we implement the js
+		attributes = {
+			['data-component'] = 'team-participant-card'
+		},
+		children = { header, content }
+	}
+end
+
+---@private
+---@param participant TeamParticipantsEntity
+---@return Widget
+function ParticipantsTeamCard:_renderHeader(participant)
+	local labelDiv = self:_renderLabel(participant)
+
+	-- TODO: Implement the non-compact version
+	return Div{
+		classes = { 'team-participant-card-header' },
+			attributes = {
+				tabindex = '0',
+				['data-component'] = 'team-participant-card-collapsible-button'
+		},
+		children = WidgetUtil.collect(
+			OpponentDisplay.BlockOpponent{
+				opponent = participant.opponent,
+				overflow = 'ellipsis',
+				teamStyle = 'standard',
+				additionalClasses = {'team-participant-card-header-opponent', 'team-participant-square-icon'},
+			},
+			labelDiv,
+			-- TODO: Implement toggle functionality
+			Div{
+				classes = { 'team-participant-card-header-icon' },
+				children = IconFa{iconName = 'collapse'},
+			}
+		)
+	}
+end
+
+---@private
+---@param participant TeamParticipantsEntity
+---@return Widget?
+function ParticipantsTeamCard:_renderLabel(participant)
+	local labelText
+	if String.isNotEmpty(participant.qualifierPage) or String.isNotEmpty(participant.qualifierUrl) then
+		labelText = 'Qualifier'
+	elseif String.isNotEmpty(participant.qualifierText) then
+		labelText = 'Invited'
+	end
+
+	if not labelText then
+		return
+	end
+
+	return Div{
+		classes = { 'team-participant-card-header-label' },
+		children = {
+			HtmlWidgets.Span{
+				children = { labelText }
+			}
+		}
+	}
+end
+
+---@private
+---@param participant TeamParticipantsEntity
+---@return Widget
+function ParticipantsTeamCard:_renderContent(participant)
+	-- TODO: Implement qualifier box, roster functionality & notes
+	return Div{
+		classes = { 'team-participant-card-collapsible-content' },
+		attributes = {
+			['data-component'] = 'team-participant-card-content'
+		},
+		children = { participant.opponent.name } -- Team details & roster here
+	}
+end
+
+return ParticipantsTeamCard
--- lua/wikis/commons/Widget/Participants/Team/CardsGroup.lua
+++ lua/wikis/commons/Widget/Participants/Team/CardsGroup.lua
@@ -9,23 +9,20 @@ local Lua = require('Module:Lua')
 
 local Array = Lua.import('Module:Array')
 local Class = Lua.import('Module:Class')
-local String = Lua.import('Module:StringUtils')
-local OpponentDisplay = Lua.import('Module:OpponentDisplay/Custom')
 local TeamParticipantsRepository = Lua.import('Module:TeamParticipants/Repository')
 
 local Widget = Lua.import('Module:Widget')
-local WidgetUtil = Lua.import('Module:Widget/Util')
 local AnalyticsWidget = Lua.import('Module:Widget/Analytics')
 local HtmlWidgets = Lua.import('Module:Widget/Html/All')
 local Div = HtmlWidgets.Div
-local IconFa = Lua.import('Module:Widget/Image/Icon/Fontawesome')
+local ParticipantsTeamCard = Lua.import('Module:Widget/Participants/Team/Card')
 
----@class ParticipantsTeamCard: Widget
----@operator call(table): ParticipantsTeamCard
-local ParticipantsTeamCard = Class.new(Widget)
+---@class ParticipantsTeamCardsGroup: Widget
+---@operator call(table): ParticipantsTeamCardsGroup
+local ParticipantsTeamCardsGroup = Class.new(Widget)
 
 ---@return Widget?
-function ParticipantsTeamCard:render()
+function ParticipantsTeamCardsGroup:render()
 	local participants = TeamParticipantsRepository.getAllByPageName(self.props.pageName)
 	if not participants then
 		return
@@ -35,88 +32,13 @@ function ParticipantsTeamCard:render()
 		analyticsName = 'Team participants card',
 		children = Div{
 			classes = { 'team-participant-cards' },
-			children = Array.map(participants, function(participant, index)
-				local header = self:_renderHeader(participant)
-				local content = self:_renderContent(participant)
-
-				return Div{
-					classes = { 'team-participant-card', 'is--collapsed' }, -- Hardcoded collapsed state until we implement the js
-					attributes = {
-						['data-component'] = 'team-participant-card',
-						['data-team-participant-card-id'] = index
-					},
-					children = { header, content }
+			children = Array.map(participants, function(participant)
+				return ParticipantsTeamCard{
+					participant = participant,
 				}
 			end),
 		}
 	}
 end
 
----@private
----@param participant TeamParticipantsEntity
----@return Widget
-function ParticipantsTeamCard:_renderHeader(participant)
-	local labelDiv = self:_renderLabel(participant)
-
-	-- TODO: Implement the non-compact version
-	return Div{
-		classes = { 'team-participant-card-header' },
-			attributes = {
-				tabindex = '0',
-				['data-component'] = 'team-participant-card-collapsible-button'
-		},
-		children = WidgetUtil.collect(
-			OpponentDisplay.BlockOpponent{
-				opponent = participant.opponent,
-				overflow = 'ellipsis',
-				teamStyle = 'standard',
-				additionalClasses = {'team-participant-card-header-opponent', 'team-participant-square-icon'},
-			},
-			labelDiv,
-			-- TODO: Implement toggle functionality
-			Div{
-				classes = { 'team-participant-card-header-icon' },
-				children = IconFa{iconName = 'collapse'},
-			}
-		)
-	}
-end
-
----@private
----@param participant TeamParticipantsEntity
----@return Widget?
-function ParticipantsTeamCard:_renderLabel(participant)
-	local labelText
-	if String.isNotEmpty(participant.qualifierPage) or String.isNotEmpty(participant.qualifierUrl) then
-		labelText = 'Qualifier'
-	elseif String.isNotEmpty(participant.qualifierText) then
-		labelText = 'Invited'
-	end
-
-	if labelText then
-		return Div{
-			classes = { 'team-participant-card-header-label' },
-			children = {
-				HtmlWidgets.Span{
-					children = { labelText }
-				}
-			}
-		}
-	end
-end
-
----@private
----@param participant TeamParticipantsEntity
----@return Widget
-function ParticipantsTeamCard:_renderContent(participant)
-	-- TODO: Implement qualifier box, roster functionality & notes
-	return Div{
-		classes = { 'team-participant-card-collapsible-content' },
-		attributes = {
-			['data-component'] = 'team-participant-card-content'
-		},
-		children = { participant.opponent.name } -- Team details & roster here
-	}
-end
-
-return ParticipantsTeamCard
+return ParticipantsTeamCardsGroup
