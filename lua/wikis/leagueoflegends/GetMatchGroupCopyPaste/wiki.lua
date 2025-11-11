b217146036854c5b8fa8490af5d18d8362f7d683
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2025-11-11 20:24:25 +0900

    change default order of args

--- lua/wikis/leagueoflegends/GetMatchGroupCopyPaste/wiki.lua
+++ lua/wikis/leagueoflegends/GetMatchGroupCopyPaste/wiki.lua
@@ -80,12 +80,12 @@ function WikiCopyPaste._getMapCode(mapIndex, args)
 		INDENT .. INDENT .. '|vod=',
 		INDENT .. INDENT .. '|team1side=',
 		detailedPlayerInput and Array.map(Array.range(1, 5), function (index)
-			return INDENT .. INDENT .. '|t1p' .. index .. '={{Json|player= |kills= |deaths= |assists= |character=}}'
+			return INDENT .. INDENT .. '|t1p' .. index .. '={{Json|player= |character= |kills= |deaths= |assists=}}'
 		end) or (INDENT .. INDENT .. '|t1c1= |t1c2= |t1c3= |t1c4= |t1c5='),
 		bans and (INDENT .. INDENT .. '|t1b1= |t1b2= |t1b3= |t1b4= |t1b5=') or nil,
 		INDENT .. INDENT .. '|team2side=',
 		detailedPlayerInput and Array.map(Array.range(1, 5), function (index)
-			return INDENT .. INDENT .. '|t2p' .. index .. '={{Json|player= |kills= |deaths= |assists= |character=}}'
+			return INDENT .. INDENT .. '|t2p' .. index .. '={{Json|player= |character= |kills= |deaths= |assists=}}'
 		end) or (INDENT .. INDENT .. '|t2c1= |t2c2= |t2c3= |t2c4= |t2c5='),
 		bans and (INDENT .. INDENT .. '|t2b1= |t2b2= |t2b3= |t2b4= |t2b5=') or nil,
 		INDENT .. INDENT .. '|length= |winner=',
