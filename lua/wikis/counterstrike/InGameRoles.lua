765d1df863fb6e36cce3737d7480e2d51720bcdd
n3rc-Nate <180866733+n3rc-Nate@users.noreply.github.com>
2025-11-13 03:33:38 -0500

    fix: Incorrect display and category for CS roles (#6788)
    * Update InGameRoles.lua
    
    Editing display roles for different riflers.
    
    For further info see:
    https://discord.com/channels/93055209017729024/268719633366777856/1437857984595361964
    
    * Update lua/wikis/counterstrike/InGameRoles.lua
    
    Co-authored-by: GeneralHuber <61513963+GeneralHuber@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: GeneralHuber <61513963+GeneralHuber@users.noreply.github.com>

--- lua/wikis/counterstrike/InGameRoles.lua
+++ lua/wikis/counterstrike/InGameRoles.lua
@@ -9,9 +9,9 @@
 local inGameRoles = {
 	['awper'] = {category = 'AWPers', display = 'AWPer'},
 	['igl'] = {category = 'In-game leaders', display = 'In-game leader'},
-	['lurker'] = {category = 'Riflers', display = 'Rifler'},
-	['support'] = {category = 'Riflers', display = 'Rifler'},
-	['entry'] = {category = 'Riflers', display = 'Rifler'},
+	['lurker'] = {category = 'Lurkers', display = 'Lurker'},
+	['support'] = {category = 'Support players', display = 'Support'},
+	['entry'] = {category = 'Entry fraggers', display = 'Entry'},
 	['rifler'] = {category = 'Riflers', display = 'Rifler'},
 }
 
