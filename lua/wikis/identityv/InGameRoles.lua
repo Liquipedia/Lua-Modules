7cba0dc096c142a62ca0ef3393d53a5fb9dad6dd
Hesketh2 <88981446+Hesketh2@users.noreply.github.com>
2025-11-13 15:49:04 +0700

    feat(roles): Add Identity V Roles

---
+++ lua/wikis/identityv/InGameRoles.lua
@@ -0,0 +1,14 @@
+---
+-- @Liquipedia
+-- page=Module:InGameRoles
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+---@type table<string, RoleBaseData>
+local inGameRoles = {
+	['hunter'] = {category = 'Hunters', display = 'Hunter'},
+	['survivor'] = {category = 'Survivors', display = 'Survivor'},
+}
+
+return inGameRoles
