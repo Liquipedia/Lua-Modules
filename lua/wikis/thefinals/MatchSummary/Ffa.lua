c636436ac76cfd92b03382181bc4d8596786fefe
Hesketh2 <88981446+Hesketh2@users.noreply.github.com>
2025-11-14 00:00:15 +0700

    Update Ffa.lua

--- lua/wikis/thefinals/MatchSummary/Ffa.lua
+++ lua/wikis/thefinals/MatchSummary/Ffa.lua
@@ -1,46 +1,42 @@
 ---
 -- @Liquipedia
--- page=Module:MatchSummary
+-- page=Module:MatchSummary/Ffa
 --
 -- Please see https://github.com/Liquipedia/Lua-Modules to contribute
 --
 
-local Lua = require('Module:Lua')
-
-local DisplayHelper = Lua.import('Module:MatchGroup/Display/Helper')
-local MatchSummary = Lua.import('Module:MatchSummary/Base')
-local MatchSummaryWidgets = Lua.import('Module:Widget/Match/Summary/All')
-local WidgetUtil = Lua.import('Module:Widget/Util')
-
 local CustomMatchSummary = {}
 
----@param args table
----@return Html
-function CustomMatchSummary.getByMatchId(args)
-	return MatchSummary.defaultGetByMatchId(CustomMatchSummary, args)
-end
+local Lua = require('Module:Lua')
 
----@param date string
----@param game MatchGroupUtilGame
----@param gameIndex integer
----@return Widget?
-function CustomMatchSummary.createGame(date, game, gameIndex)
-	local function makeTeamSection(opponentIndex)
-		return {
-			MatchSummaryWidgets.GameWinLossIndicator{winner = game.winner, opponentIndex = opponentIndex},
-			DisplayHelper.MapScore(game.opponents[opponentIndex], game.status)
+local MatchGroupUtil = Lua.import('Module:MatchGroup/Util/Custom')
+local SummaryHelper = Lua.import('Module:MatchSummary/Base/Ffa')
+
+local MatchSummaryWidgets = Lua.import('Module:Widget/Match/Summary/Ffa/All')
+local HtmlWidgets = Lua.import('Module:Widget/Html/All')
+local WidgetUtil = Lua.import('Module:Widget/Util')
+
+---@param props {bracketId: string, matchId: string}
+---@return Widget
+function CustomMatchSummary.getByMatchId(props)
+	---@class FFAMatchGroupUtilMatch
+	local match = MatchGroupUtil.fetchMatchForBracketDisplay(props.bracketId, props.matchId)
+	SummaryHelper.updateMatchOpponents(match)
+	local scoringData = SummaryHelper.createScoringData(match)
+
+	return HtmlWidgets.Fragment{children = {
+		MatchSummaryWidgets.Header{matchId = match.matchId, games = match.games},
+		MatchSummaryWidgets.Tab{
+			matchId = match.matchId,
+			idx = 0,
+			children = WidgetUtil.collect(
+				MatchSummaryWidgets.GamesSchedule{match = match},
+				MatchSummaryWidgets.PointsDistribution{scores = scoringData},
+				MatchSummaryWidgets.MatchInformation(match),
+				SummaryHelper.standardMatch(match)
+			)
 		}
-	end
-
-	return MatchSummaryWidgets.Row{
-		classes = {'brkts-popup-body-game'},
-		children = WidgetUtil.collect(
-			MatchSummaryWidgets.GameTeamWrapper{children = makeTeamSection(1)},
-			MatchSummaryWidgets.GameCenter{children = DisplayHelper.Map(game)},
-			MatchSummaryWidgets.GameTeamWrapper{children = makeTeamSection(2), flipped = true},
-			MatchSummaryWidgets.GameComment{children = game.comment}
-		)
-	}
+	}}
 end
 
 return CustomMatchSummary
