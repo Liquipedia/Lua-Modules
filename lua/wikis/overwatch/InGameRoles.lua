ced9204a1b46137a86f9cc919d04faa54d824e0d
GeneralHuber <61513963+GeneralHuber@users.noreply.github.com>
2025-11-27 22:26:25 +0100

    kick duplicate arg

--- lua/wikis/overwatch/InGameRoles.lua
+++ lua/wikis/overwatch/InGameRoles.lua
@@ -10,8 +10,9 @@ local inGameRoles = {
 	['dps'] = {category = 'DPS Players', display = 'DPS'},
 	['flex'] = {category = 'Flex Players', display = 'Flex'},
 	['support'] = {category = 'Support Players', display = 'Support'},
-	['sup'] = {category = 'Support Players', display = 'Support'},
 	['tank'] = {category = 'Tank Players', display = 'Tank'},
 }
+
+inGameRoles.sup = inGameRoles.support
 
 return inGameRoles
