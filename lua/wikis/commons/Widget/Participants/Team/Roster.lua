2f2dc4fa13d549513c9ca8bae4fd5e4dc9007122
Rikard Blixt <rikardblixt@gmail.com>
2025-11-27 13:58:07 +0100

    feat: add dnp support to team participants

--- lua/wikis/commons/Widget/Participants/Team/Roster.lua
+++ lua/wikis/commons/Widget/Participants/Team/Roster.lua
@@ -49,13 +49,18 @@ local PERSON_TYPE_TO_TAB = {
 -- The biz logic behind the role display is somewhat complicated.
 -- There's 2 areas we show the role, left-role and right-role
 -- * Right-role:
---   * is only shown if there's a non-ingame role assigned. It will use the first one provided
+--   * If there's a non-ingame role assigned. It will use the first one provided
+--   * Otherwise, If the person has a specific other data (former/did not play), we will use that
 -- * Left-role:
 --   * If the first role has an icon, we use that to render the left-role
 --   * If not then we instead display the icon or text of the first ingame role
----@param roles RoleData[]
+---@param player table
 ---@return string?, string?
-local function getRoleDisplays(roles)
+local function getRoleDisplays(player)
+	local roles = player.extradata.roles or {}
+	local playerType = player.extradata.type
+	local played = player.extradata.played
+
 	local function roleLeftDisplay()
 		local firstRole = roles[1]
 		if firstRole and firstRole.icon then
@@ -72,6 +77,11 @@ local function getRoleDisplays(roles)
 			if role.type ~= RoleUtil.ROLE_TYPE.INGAME then
 				return role.display
 			end
+		end
+		if playerType == 'former' then
+			return 'Left'
+		elseif not played then
+			return 'DNP'
 		end
 	end
 	return roleLeftDisplay(), roleRightDisplay()
@@ -93,7 +103,7 @@ function ParticipantsTeamRoster:render()
 					type = Opponent.team,
 					template = playerTeam,
 				} or nil
-				local roleLeft, roleRight = getRoleDisplays(player.extradata.roles or {})
+				local roleLeft, roleRight = getRoleDisplays(player)
 				return ParticipantsTeamMember{
 					player = player,
 					team = playerTeamAsOpponent,
@@ -101,6 +111,7 @@ function ParticipantsTeamRoster:render()
 					roleLeft = roleLeft,
 					roleRight = roleRight,
 					trophies = player.extradata.trophies or 0,
+					strikethrough = not player.extradata.played,
 				}
 			end)
 		}
