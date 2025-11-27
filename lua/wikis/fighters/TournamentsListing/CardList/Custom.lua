597ff6c794cd491bea89f1f75c83279dca77dced
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2025-11-27 11:07:43 +0900

    copy over byYear entrypoint

--- lua/wikis/fighters/TournamentsListing/CardList/Custom.lua
+++ lua/wikis/fighters/TournamentsListing/CardList/Custom.lua
@@ -8,8 +8,12 @@
 local Lua = require('Module:Lua')
 
 local Arguments = Lua.import('Module:Arguments')
+local Array = Lua.import('Module:Array')
 local Class = Lua.import('Module:Class')
+local DateExt = Lua.import('Module:Date/Ext')
+local Info = Lua.import('Module:Info', {loadData = true})
 local Logic = Lua.import('Module:Logic')
+local Table = Lua.import('Module:Table')
 
 local Condition = Lua.import('Module:Condition')
 local ConditionNode = Condition.Node
@@ -18,6 +22,11 @@ local ColumnName = Condition.ColumnName
 
 local ListingConditions = Lua.import('Module:TournamentsListing/Conditions')
 local TournamentsListing = Lua.import('Module:TournamentsListing/CardList')
+
+local HtmlWidgets = Lua.import('Module:Widget/Html/All')
+
+local DEFAULT_START_YEAR = Info.startYear
+local DEFAULT_END_YEAR = DateExt.getYearOf()
 
 --- @class FightersTournamentsListing: BaseTournamentsListing
 --- @operator call(...): FightersTournamentsListing
@@ -40,6 +49,35 @@ function CustomTournamentsListing:buildConditions()
 	return tostring(conditions)
 end
 
+---@param args table
+---@return Widget?
+function CustomTournamentsListing.byYear(args)
+	args = args or {}
+
+	args.order = 'enddate desc'
+
+	local subPageName = mw.title.getCurrentTitle().subpageText
+	local fallbackYearData = {}
+	if subPageName:find('%d%-%d') then
+		fallbackYearData = mw.text.split(subPageName, '-')
+	end
+
+	local startYear = tonumber(args.startYear) or tonumber(fallbackYearData[1]) or DEFAULT_START_YEAR
+	local endYear = tonumber(args.endYear) or tonumber(fallbackYearData[2]) or DEFAULT_END_YEAR
+
+	local children = {}
+	Array.forEach(Array.reverse(Array.range(startYear, endYear)), function(year)
+		local tournaments = CustomTournamentsListing(Table.merge(args, {year = year})):create():build()
+		if not tournaments then return end
+		Array.appendWith(children,
+			HtmlWidgets.H3{children = year},
+			tournaments
+		)
+	end)
+
+	return HtmlWidgets.Fragment{children = children}
+end
+
 ---@param frame Frame
 ---@return Html|Widget?
 function CustomTournamentsListing.run(frame)
@@ -49,7 +87,7 @@ function CustomTournamentsListing.run(frame)
 		return CustomTournamentsListing.byYear(args)
 	end
 
-	return TournamentsListing(args):create():build()
+	return CustomTournamentsListing(args):create():build()
 end
 
 return CustomTournamentsListing
