9736644eedb96bb52e9032ec7f7a1243b49e2cfe
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2025-11-28 17:33:19 +0900

    refactor fighters custom

---
+++ lua/wikis/fighters/Widget/PlayerDisplay/Block/Custom.lua
@@ -0,0 +1,74 @@
+---
+-- @Liquipedia
+-- page=Module:Widget/PlayerDisplay/Block/Custom
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Lua = require('Module:Lua')
+
+local Array = Lua.import('Module:Array')
+local Characters = Lua.import('Module:Characters')
+local Class = Lua.import('Module:Class')
+local Logic = Lua.import('Module:Logic')
+
+local BlockPlayerWidget = Lua.import('Module:Widget/PlayerDisplay/Block')
+local HtmlWidgets = Lua.import('Module:Widget/Html/All')
+local Div = HtmlWidgets.Div
+local Span = HtmlWidgets.Span
+local WidgetUtil = Lua.import('Module:Widget/Util')
+
+---@class FightersBlockPlayerProps: BlockPlayerProps
+---@field player FightersStandardPlayer
+---@field oneLine boolean?
+
+---@class FightersBlockPlayerWidget: BlockPlayerWidget
+---@operator call(FightersBlockPlayerProps): FightersBlockPlayerWidget
+---@field protected player FightersStandardPlayer
+---@field props FightersBlockPlayerProps
+local CustomBlockPlayerWidget = Class.new(BlockPlayerWidget)
+
+---@return Widget|(string|Widget)[]
+function CustomBlockPlayerWidget:render()
+	local props = self.props
+	local charactersDisplay = self:getCharacters()
+	local block = Div{
+		classes = Array.extend(
+			'block-player',
+			self.props.flip and 'flipped' or nil,
+			self.props.showPlayerTeam and 'has-team' or nil
+		),
+		css = {['white-space'] = 'pre'},
+		children = WidgetUtil.collect(
+			self:getFlag(),
+			props.oneLine and charactersDisplay or nil,
+			self:getName(),
+			Logic.isNotEmpty(props.note) and HtmlWidgets.Sup{children = props.note} or nil,
+			self:getTeam()
+		)
+	}
+	if props.oneLine then
+		return block
+	end
+	return WidgetUtil.collect(block, charactersDisplay)
+end
+
+---@protected
+---@return (string|Widget)[]?
+function CustomBlockPlayerWidget:getCharacters()
+	local player = self.player
+	if Logic.isEmpty(player.chars) then
+		return
+	end
+	return Array.interleave(
+		Array.map(player.chars, function (character)
+			return Span{
+				classes = {'race'},
+				children = Characters.GetIconAndName{character, game = player.game}
+			}
+		end),
+		' '
+	)
+end
+
+return CustomBlockPlayerWidget
