015f45039904307402cc35d8721c59d2b7af9a53
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2025-11-28 12:36:21 +0900

    add counterstrike custom

---
+++ lua/wikis/counterstrike/TournamentsListing/CardList/Custom.lua
@@ -0,0 +1,32 @@
+---
+-- @Liquipedia
+-- page=Module:TournamentsListing/CardList/Custom
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Lua = require('Module:Lua')
+
+local Arguments = Lua.import('Module:Arguments')
+local Class = Lua.import('Module:Class')
+local Logic = Lua.import('Module:Logic')
+
+local TournamentsListing = Lua.import('Module:TournamentsListing/CardList')
+
+local CustomTournamentsListing = Class.new(TournamentsListing)
+
+---@param frame Frame
+---@return Html|Widget?
+function CustomTournamentsListing.run(frame)
+	local args = Arguments.getArgs(frame)
+
+	args.showGameIcon = Logic.nilOr(Logic.readBoolOrNil(args.showGameIcon), true)
+
+	if Logic.readBool(args.byYear) then
+		return CustomTournamentsListing.byYear(args)
+	end
+
+	return CustomTournamentsListing(args):create():build()
+end
+
+return CustomTournamentsListing
