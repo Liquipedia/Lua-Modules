b3c58342308313e69fed62bafa84a80e7d9c2908
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2025-11-28 12:44:18 +0900

    add ageofempires custom

---
+++ lua/wikis/ageofempires/TournamentsListing/CardList/Custom.lua
@@ -0,0 +1,36 @@
+---
+-- @Liquipedia
+-- page=Module:TournamentsListing/CardList/Custom
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Lua = require('Module:Lua')
+
+local Arguments = Lua.import('Module:Arguments')
+local Class = Lua.import('Module:Class')
+local Logic = Lua.import('Module:Logic')
+
+local TournamentsListing = Lua.import('Module:TournamentsListing/CardList')
+
+local CustomTournamentsListing = Class.new(TournamentsListing)
+
+---@param frame Frame
+---@return Html|Widget?
+function CustomTournamentsListing.run(frame)
+	local args = Arguments.getArgs(frame)
+
+	args.showTier = true
+	args.showGameIcon = true
+	args.useParent = false
+	args.order = Logic.emptyOr(args.order, 'enddate desc, pagename desc')
+	args.onlyTierTypeIfBoth = false
+
+	if Logic.readBool(args.byYear) then
+		return CustomTournamentsListing.byYear(args)
+	end
+
+	return CustomTournamentsListing(args):create():build()
+end
+
+return CustomTournamentsListing
