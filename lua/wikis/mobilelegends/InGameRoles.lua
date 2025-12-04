f91e9b303ee266f40e4b98e97ef95668fe73751d
Hesketh2 <88981446+Hesketh2@users.noreply.github.com>
2025-12-04 16:30:53 +0700

    Switch aliases and role entries

--- lua/wikis/mobilelegends/InGameRoles.lua
+++ lua/wikis/mobilelegends/InGameRoles.lua
@@ -7,9 +7,9 @@
 
 ---@type table<string, RoleBaseData>
 local inGameRoles = {
-	['exp'] = {category = 'EXP Laner', display = 'EXP Laner', sortOrder = 1},
-	['gold'] = {category = 'Gold Laner', display = 'Gold Laner', sortOrder = 4},
-	['mid'] = {category = 'Mid Laner', display = 'Mid Laner', sortOrder = 3},
+	['exp lane'] = {category = 'EXP Laner', display = 'EXP Laner', sortOrder = 1},
+	['gold lane'] = {category = 'Gold Laner', display = 'Gold Laner', sortOrder = 4},
+	['middle'] = {category = 'Mid Laner', display = 'Mid Laner', sortOrder = 3},
 	['jungler'] = {category = 'Jungler', display = 'Jungler', sortOrder = 2},
 	['roamer'] = {category = 'Roamer', display = 'Roamer', sortOrder = 5},
 	['flex'] = {category = 'Flex', display = 'Flex', sortOrder = 6},
@@ -19,11 +19,11 @@ inGameRoles['jgl'] = inGameRoles.jungler
 inGameRoles['jungle'] = inGameRoles.jungler
 inGameRoles['roam'] = inGameRoles.roamer
 inGameRoles['support'] = inGameRoles.roamer
-inGameRoles['top'] = inGameRoles.exp
-inGameRoles['exp lane'] = inGameRoles.exp
-inGameRoles['mid laner'] = inGameRoles.mid
-inGameRoles['middle'] = inGameRoles.mid
-inGameRoles['bottom'] = inGameRoles.gold
-inGameRoles['gold lane'] = inGameRoles.gold
+inGameRoles['top'] = inGameRoles['exp lane']
+inGameRoles['exp'] = inGameRoles['exp lane']
+inGameRoles['mid laner'] = inGameRoles.middle
+inGameRoles['mid'] = inGameRoles.middle
+inGameRoles['bottom'] = inGameRoles['gold lane']
+inGameRoles['gold'] = inGameRoles['gold lane']
 
 return inGameRoles
