d3ade14feb952c0db7ba962c0d0b8aa20eaeb01a
Hesketh2 <88981446+Hesketh2@users.noreply.github.com>
2025-12-04 16:45:59 +0700

    feat: fix Honor of Kings role

--- lua/wikis/honorofkings/InGameRoles.lua
+++ lua/wikis/honorofkings/InGameRoles.lua
@@ -7,12 +7,13 @@
 
 ---@type table<string, RoleBaseData>
 local inGameRoles = {
-	['top'] = {category = 'Top Laners', display = 'Top Lane', sortOrder = 1},
-	['bottom'] = {category = 'Bottom Laners', display = 'Bottom Laner', sortOrder = 5},
-	['mid'] = {category = 'Mid Laners', display = 'Mid Laner', sortOrder = 4},
+	['top lane'] = {category = 'Top Laners', display = 'Top Lane', sortOrder = 1},
+	['bottom lane'] = {category = 'Bottom Laners', display = 'Bottom Laner', sortOrder = 5},
+	['middle'] = {category = 'Mid Laners', display = 'Mid Laner', sortOrder = 4},
 	['side lane'] = {category = 'Side Laners', display = 'Side Laner', sortOrder = 2},
 	['jungler'] = {category = 'Junglers', display = 'Jungler', sortOrder = 3},
 	['roamer'] = {category = 'Roamers', display = 'Roamer', sortOrder = 6},
+	['flex'] = {category = 'Flex', display = 'Flex', sortOrder = 7},
 }
 
 inGameRoles['jgl'] = inGameRoles.jungler
@@ -20,18 +21,21 @@ inGameRoles['jungle'] = inGameRoles.jungler
 inGameRoles['support'] = inGameRoles.roamer
 inGameRoles['roam'] = inGameRoles.roamer
 inGameRoles['sup'] = inGameRoles.roamer
-inGameRoles['ad lane'] = inGameRoles.bottom
-inGameRoles['ad carry'] = inGameRoles.bottom
-inGameRoles['abyssal dragon lane'] = inGameRoles.bottom
-inGameRoles['adl'] = inGameRoles.bottom
-inGameRoles['adc'] = inGameRoles.bottom
-inGameRoles['bot'] = inGameRoles.bottom
-inGameRoles['carry'] = inGameRoles.bottom
-inGameRoles['farm'] = inGameRoles.bottom
-inGameRoles['mid laner'] = inGameRoles.mid
-inGameRoles['ds lane'] = inGameRoles.top
-inGameRoles['dsl'] = inGameRoles.top
-inGameRoles['dark slayer lane'] = inGameRoles.top
-inGameRoles['confront'] = inGameRoles.top
+inGameRoles['ad lane'] = inGameRoles['bottom lane']
+inGameRoles['ad carry'] = inGameRoles['bottom lane']
+inGameRoles['abyssal dragon lane'] = inGameRoles['bottom lane']
+inGameRoles['adl'] = inGameRoles['bottom lane']
+inGameRoles['adc'] = inGameRoles['bottom lane']
+inGameRoles['bot'] = inGameRoles['bottom lane']
+inGameRoles['bottom'] = inGameRoles['bottom lane']
+inGameRoles['carry'] = inGameRoles['bottom lane']
+inGameRoles['farm'] = inGameRoles['bottom lane']
+inGameRoles['mid laner'] = inGameRoles.middle
+inGameRoles['mid'] = inGameRoles.middle
+inGameRoles['top'] = inGameRoles['top lane']
+inGameRoles['ds lane'] = inGameRoles['top lane']
+inGameRoles['dsl'] = inGameRoles['top lane']
+inGameRoles['dark slayer lane'] = inGameRoles['top lane']
+inGameRoles['confront'] = inGameRoles['top lane']
 
 return inGameRoles
