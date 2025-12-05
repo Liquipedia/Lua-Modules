9c67164644f8dd0ee72ec19f80a3e661d68a0516
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2025-12-05 12:20:12 +0900

    make seriesdots a separate widget

--- lua/wikis/commons/Widget/Match/Page/PartyDisplay.lua
+++ lua/wikis/commons/Widget/Match/Page/PartyDisplay.lua
@@ -9,39 +9,21 @@ local Lua = require('Module:Lua')
 
 local Array = Lua.import('Module:Array')
 local Class = Lua.import('Module:Class')
-local Logic = Lua.import('Module:Logic')
 local Opponent = Lua.import('Module:Opponent/Custom')
 local PlayerDisplay = Lua.import('Module:Player/Display/Custom')
 
 local Widget = Lua.import('Module:Widget')
 local HtmlWidgets = Lua.import('Module:Widget/Html/All')
 local Div = HtmlWidgets.Div
+local SeriesDots = Lua.import('Module:Widget/Match/Page/SeriesDots')
 
 ---@class MatchPagePartyDisplayParameters
 ---@field opponent MatchPageOpponent
-
-local RESULT_DISPLAY_TYPES = {
-	['w'] = 'winner',
-	['l'] = 'loser',
-	['winner'] = 'winner',
-	['loser'] = 'loser',
-	['-'] = 'notplayed'
-}
 
 ---@class MatchPagePartyDisplay: Widget
 ---@operator call(MatchPagePartyDisplayParameters): MatchPagePartyDisplay
 ---@field props MatchPagePartyDisplayParameters
 local MatchPagePartyDisplay = Class.new(Widget)
-
-
----@private
----@param result string
----@return Widget
-function MatchPagePartyDisplay._makeGameResultIcon(result)
-	return Div{
-		classes = { 'match-bm-match-header-round-result', 'result--' .. RESULT_DISPLAY_TYPES[result:lower()] }
-	}
-end
 
 ---@return Widget?
 function MatchPagePartyDisplay:render()
@@ -67,10 +49,7 @@ function MatchPagePartyDisplay:_buildChildren()
 					return PlayerDisplay.InlinePlayer{player = player}
 				end)
 			},
-			Logic.isNotEmpty(opponent.seriesDots) and Div{
-				classes = { 'match-bm-match-header-round-results' },
-				children = Array.map(opponent.seriesDots, MatchPagePartyDisplay._makeGameResultIcon)
-			} or nil,
+			SeriesDots{seriesDots = opponent.seriesDots},
 		}
 	}
 end
