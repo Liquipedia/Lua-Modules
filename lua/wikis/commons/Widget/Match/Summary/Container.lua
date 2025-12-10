8d217c4f5d83eae4a407b039b48409ddba31e018
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2025-12-10 14:02:04 +0900

    restore gaps

--- lua/wikis/commons/Widget/Match/Summary/Container.lua
+++ lua/wikis/commons/Widget/Match/Summary/Container.lua
@@ -13,6 +13,7 @@ local DisplayHelper = Lua.import('Module:MatchGroup/Display/Helper')
 local Logic = Lua.import('Module:Logic')
 
 local Widget = Lua.import('Module:Widget')
+local HtmlWidgets = Lua.import('Module:Widget/Html/All')
 local AnalyticsWidget = Lua.import('Module:Widget/Analytics')
 local ContentSwitch = Lua.import('Module:Widget/ContentSwitch')
 
@@ -42,27 +43,50 @@ end
 
 ---@return Widget
 function MatchSummaryContainer:render()
-	local resetMatch = self.props.resetMatch
 	return AnalyticsWidget{
 		analyticsName = 'Match popup',
 		classes = Array.extend('brkts-popup', self.props.classes),
 		css = {width = self.props.width},
-		children = ContentSwitch{
-			css = {['margin-bottom'] = '0.5rem'},
-			tabs = {
-				{
-					label = MatchSummaryContainer._getExpandedHeader(self.props.match),
-					content = self.props.createMatch(self.props.match):create()
-				},
-				resetMatch and {
-					label = MatchSummaryContainer._getExpandedHeader(resetMatch) .. ' Reset',
-					content = self.props.createMatch(resetMatch):create()
-				} or nil,
+		children = self:_buildChildren(),
+	}
+end
+
+---@private
+---@return Widget|Html
+function MatchSummaryContainer:_buildChildren()
+	local resetMatch = self.props.resetMatch
+	if not resetMatch then
+		return self.props.createMatch(self.props.match):create()
+	end
+
+	---@param matchData MatchGroupUtilMatch
+	---@return Widget
+	local function createMatchContainer(matchData)
+		return HtmlWidgets.Div{
+			css = {
+				display = 'flex',
+				['flex-direction'] = 'column',
+				gap = '0.5rem',
 			},
-			size = 'small',
-			storeValue = false,
-			switchGroup = self.props.match.matchId .. '_resetSelector',
+			children = self.props.createMatch(matchData):create()
 		}
+	end
+
+	return ContentSwitch{
+		css = {['margin-bottom'] = '0.5rem'},
+		tabs = {
+			{
+				label = MatchSummaryContainer._getExpandedHeader(self.props.match),
+				content = createMatchContainer(self.props.match),
+			},
+			{
+				label = MatchSummaryContainer._getExpandedHeader(resetMatch) .. ' Reset',
+				content = createMatchContainer(resetMatch),
+			},
+		},
+		size = 'small',
+		storeValue = false,
+		switchGroup = self.props.match.matchId .. '_resetSelector',
 	}
 end
 
