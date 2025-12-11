8d0d242b122d33270dd24fa15fd3afbe628766e0
hjpalpha <andreasbuesch1@gmail.com>
2025-12-11 16:11:14 +0100

    missing import from resolving merge conflict

--- lua/wikis/commons/TeamParticipants/Controller.lua
+++ lua/wikis/commons/TeamParticipants/Controller.lua
@@ -17,6 +17,7 @@ local Lpdb = Lua.import('Module:Lpdb')
 local Operator = Lua.import('Module:Operator')
 local Opponent = Lua.import('Module:Opponent/Custom')
 local Table = Lua.import('Module:Table')
+local Variables = Lua.import('Module:Variables')
 
 local Condition = Lua.import('Module:Condition')
 local BooleanOperator = Condition.BooleanOperator
