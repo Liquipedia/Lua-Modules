e7b05b6b5be6232887f192c24fdc395d2063e3e8
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2026-01-10 14:31:46 +0900

    kick unused stuff

--- lua/wikis/commons/Widget/Basic/Dialog.lua
+++ lua/wikis/commons/Widget/Basic/Dialog.lua
@@ -7,7 +7,6 @@
 
 local Lua = require('Module:Lua')
 
-local Array = Lua.import('Module:Array')
 local Class = Lua.import('Module:Class')
 local Logic = Lua.import('Module:Logic')
 
@@ -16,8 +15,6 @@ local HtmlWidgets = Lua.import('Module:Widget/Html/All')
 local Div = HtmlWidgets.Div
 
 ---@class DialogWidgetProps
----@field wrapperClasses string[]?
----@field addDefaultClass boolean
 ---@field title? string|number|Widget|Html|(string|number|Widget|Html)[]
 ---@field trigger? string|number|Widget|Html|(string|number|Widget|Html)[]
 ---@field children? string|number|Widget|Html|(string|number|Widget|Html)[]
@@ -33,18 +30,8 @@ function DialogWidget:render()
 	if Logic.isEmpty(props.title) or Logic.isEmpty(props.trigger) or Logic.isEmpty(props.children) then
 		return
 	end
-	local addDefaultClass = Logic.nilOr(Logic.readBoolOrNil(props.addDefaultClass), true)
 	return Div{
 		classes = {'general-dialog'},
-		attributes = {
-			['dialog-classes'] = table.concat(
-				Array.extend(
-					addDefaultClass and 'general-dialog-container' or nil,
-					props.wrapperClasses
-				),
-				' '
-			)
-		},
 		children = {
 			Div{
 				classes = {'general-dialog-trigger'},
