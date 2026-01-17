547eeb60f77147de953b58655814ea1acb771100
SyntacticSalt <syntactic@mbergen.de>
2026-01-17 18:59:00 +0100

    Extend display tests

--- lua/spec/date_range_display_spec.lua
+++ lua/spec/date_range_display_spec.lua
@@ -5,11 +5,11 @@ insulate('DateRange', function()
 		local HtmlWidgets = require('Module:Widget/Html/All')
 
         local start_y = {year = 2023}
-        local start_ym
-        local start_ymd
+        local start_ym = {year = 2023, month = 10}
+        local start_ymd = {year = 2023, month = 10, day = 3}
 
         local end_y = {year = 2024}
-        local end_ym = {year = 2024}
+        local end_ym = {year = 2024, month = 11}
         local end_ymd = {year = 2024, month = 11, day = 4}
 
         local widgets = {
@@ -24,8 +24,19 @@ insulate('DateRange', function()
             -- Month display
             DateRange{startDate = start_ym},
             DateRange{startDate = start_ym, endDate = end_ymd},
+            DateRange{startDate = start_ym, endDate = start_ym},
+
+            -- Day display
+            DateRange{startDate = start_ymd},
+            DateRange{startDate = start_ymd, endDate = end_ym},
+            DateRange{startDate = start_ymd, endDate = {year = 2023, month = 10}},
+            DateRange{startDate = start_ymd, endDate = {year = 2023, month = 10, day = 04}},
+            DateRange{startDate = start_ymd, endDate = end_ymd},
+            DateRange{startDate = start_ymd, endDate = start_ymd},
+
+            -- showYear
         }
 
-		GoldenTest('date range display', tostring(HtmlWidgets.Fragment{children=widgets}))
+		GoldenTest('date range display', tostring(HtmlWidgets.Div{children=widgets}))
 	end)
 end)
