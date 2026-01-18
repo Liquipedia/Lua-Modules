198e6aa032a6bf8380d8abf055694bddf5faa2bf
SyntacticSalt <syntactic@mbergen.de>
2026-01-18 13:50:13 +0100

    Add table classes, header

--- lua/spec/date_range_display_spec.lua
+++ lua/spec/date_range_display_spec.lua
@@ -40,13 +40,25 @@ insulate('DateRange', function()
 
 		GoldenTest(
             'date range display',
-            tostring(HtmlWidgets.Table{children=Array.map(data, function (entry)
-                return HtmlWidgets.Tr{children={
-                    HtmlWidgets.Td{children=mw.dumpObject(entry[1] or {})},
-                    HtmlWidgets.Td{children=mw.dumpObject(entry[2] or {})},
-                    HtmlWidgets.Td{children=DateRange{startDate = entry[1], endDate = entry[2]}},
-                }}
-            end)})
+            tostring(HtmlWidgets.Table{
+                classes={'wikitable wikitable-striped'},
+                children=Array.extend(
+                    {
+                        HtmlWidgets.Tr{children={
+                            HtmlWidgets.Th{children="startDate"},
+                            HtmlWidgets.Th{children="endDate"},
+                            HtmlWidgets.Th{children="DateRange display"},
+                        }}
+                    },
+                    Array.map(data, function (entry)
+                        return HtmlWidgets.Tr{children={
+                            HtmlWidgets.Td{children=mw.dumpObject(entry[1] or {})},
+                            HtmlWidgets.Td{children=mw.dumpObject(entry[2] or {})},
+                            HtmlWidgets.Td{children=DateRange{startDate = entry[1], endDate = entry[2]}},
+                        }}
+                    end)
+                )
+            })
         )
 	end)
 end)
