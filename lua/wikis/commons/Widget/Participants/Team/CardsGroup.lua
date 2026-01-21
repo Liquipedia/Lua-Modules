7a4c89b3c00f2c9eeb7213fdcc8c797752f5a121
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2026-01-21 11:38:06 +0900

    add groups support

--- lua/wikis/commons/Widget/Participants/Team/CardsGroup.lua
+++ lua/wikis/commons/Widget/Participants/Team/CardsGroup.lua
@@ -9,6 +9,9 @@ local Lua = require('Module:Lua')
 
 local Array = Lua.import('Module:Array')
 local Class = Lua.import('Module:Class')
+local Logic = Lua.import('Module:Logic')
+local Operator = Lua.import('Module:Operator')
+local Tabs = Lua.import('Module:Tabs')
 
 local Widget = Lua.import('Module:Widget')
 local AnalyticsWidget = Lua.import('Module:Widget/Analytics')
@@ -28,6 +31,15 @@ function ParticipantsTeamCardsGroup:render()
 	if not participants then
 		return
 	end
+
+	local participantGroups = Array.groupBy(participants, Operator.property('participantGroup'))
+
+	local tabArgs = {}
+
+	Array.forEach(participantGroups, function (group, groupIndex)
+		tabArgs['name' .. groupIndex] = Logic.emptyOr(group[1].participantGroup, 'Unnamed')
+		tabArgs['content' .. groupIndex] = ParticipantsTeamCardsGroup._createParticipantGroup(group)
+	end)
 
 	return Div{
 		classes = { 'team-participant' },
@@ -60,17 +72,23 @@ function ParticipantsTeamCardsGroup:render()
 					}
 				}
 			},
-			AnalyticsWidget{
-				analyticsName = 'Team participants card',
-				children = Div{
-					classes = { 'team-participant__grid' },
-					children = Array.map(participants, function(participant)
-						return ParticipantsTeamCard{
-							participant = participant,
-						}
-					end),
+			Tabs.dynamic(tabArgs)
+		}
+	}
+end
+
+---@private
+---@param participantGroup TeamParticipant[]
+function ParticipantsTeamCardsGroup._createParticipantGroup(participantGroup)
+	AnalyticsWidget{
+		analyticsName = 'Team participants card',
+		children = Div{
+			classes = { 'team-participant__grid' },
+			children = Array.map(participantGroup, function(participant)
+				return ParticipantsTeamCard{
+					participant = participant,
 				}
-			}
+			end),
 		}
 	}
 end
