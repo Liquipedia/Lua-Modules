40b7bff741383beaaf031e40c80033bf268f27a1
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2026-02-04 16:22:40 +0900

    protect_page script to python

---
+++ scripts/protect_templates.py
@@ -0,0 +1,16 @@
+import os
+
+from protect_page import *
+
+WIKI_TO_PROTECT = os.getenv("WIKI_TO_PROTECT")
+
+with open("./templates/templatesToProtect", "r") as templates_to_protect:
+    for template_name in templates_to_protect.readlines():
+        template = "Template:" + template_name
+        print(f"::group::Checking {WIKI_TO_PROTECT}:{template}")
+        if WIKI_TO_PROTECT == "commons":
+            protect_existing_page(template, WIKI_TO_PROTECT)
+        else:
+            protect_non_existing_page(template, WIKI_TO_PROTECT)
+        print("::endgroup::")
+handle_protect_errors()
