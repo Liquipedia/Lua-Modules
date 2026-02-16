71efdea9c4a23eb296b9a03833a4bb0c6d35f65f
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2026-02-16 18:48:14 +0900

    refactor(eml): clean up eml list implementation (#6999)
    * type annotations
    
    * parseCommaSeparatedString
    
    * use condition nodes
    
    * extract display to separate widget
    
    * extract link to eml form
    
    * kick redundant tostring
    
    * type annotation
    
    * clean up code duplication
    
    * kick direct use of mw.html
    
    * merge form link back to eml module
    
    * replace assert with warn
    
    * remove form existence check
    
    * reorder imports
    
    * eof
    
    * update comment
    
    Co-authored-by: hjpalpha <75081997+hjpalpha@users.noreply.github.com>
    
    * curry
    
    ---------
    
    Co-authored-by: hjpalpha <75081997+hjpalpha@users.noreply.github.com>

---
+++ lua/wikis/commons/Widget/ExternalMedia/List.lua
@@ -0,0 +1,71 @@
+---
+-- @Liquipedia
+-- page=Module:Widget/ExternalMedia/List
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Lua = require('Module:Lua')
+
+local Array = Lua.import('Module:Array')
+local Class = Lua.import('Module:Class')
+local FnUtil = Lua.import('Module:FnUtil')
+local Logic = Lua.import('Module:Logic')
+local OpponentDisplay = Lua.import('Module:OpponentDisplay/Custom')
+local PlayerExt = Lua.import('Module:Player/Ext/Custom')
+
+local Widget = Lua.import('Module:Widget')
+local ExternalMediaLinkDisplay = Lua.import('Module:Widget/ExternalMedia/Link')
+local Link = Lua.import('Module:Widget/Basic/Link')
+local UnorderedList = Lua.import('Module:Widget/List/Unordered')
+local WidgetUtil = Lua.import('Module:Widget/Util')
+
+local NON_BREAKING_SPACE = '&nbsp;'
+
+---@class ExternalMediaListDisplay: Widget
+---@operator call(table): ExternalMediaListDisplay
+---@field props {data: externalmedialink[], showSubjectTeam: boolean?, showUsUk: boolean?, subject: string?}
+local ExternalMediaListDisplay = Class.new(Widget)
+
+---@return Widget?
+function ExternalMediaListDisplay:render()
+	local data = self.props.data
+	if Logic.isEmpty(data) then
+		return
+	end
+	return UnorderedList{children = Array.map(data, FnUtil.curry(ExternalMediaListDisplay._createListElement, self))}
+end
+
+---@private
+---@param item externalmedialink
+---@return (string|Widget)[]
+function ExternalMediaListDisplay:_createListElement(item)
+	return WidgetUtil.collect(
+		{
+			mw.text.nowiki('['),
+			Link{link = 'Data:' .. item.pagename, children = 'e'},
+			mw.text.nowiki(']'),
+			NON_BREAKING_SPACE
+		},
+		self.props.showSubjectTeam and self:_displayTeam(item.date) or nil,
+		ExternalMediaLinkDisplay{data = item, showUsUk = self.props.showUsUk}
+	)
+end
+
+---Displays the subject's team for a given External Media Link
+---@param date string
+---@return Widget?
+function ExternalMediaListDisplay:_displayTeam(date)
+	local subject = self.props.subject
+	if Logic.isEmpty(subject) then
+		return
+	end
+	---@cast subject -nil
+	local _, team = PlayerExt.syncTeam(subject, nil, {date = date})
+	if not team then
+		return
+	end
+	return OpponentDisplay.InlineTeamContainer{template = team, date = date, style = 'icon'}
+end
+
+return ExternalMediaListDisplay
