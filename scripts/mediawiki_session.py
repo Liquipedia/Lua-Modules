24a0834179e50e20c57b82a2d8d1683061e03ed4
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2026-03-05 15:31:09 +0900

    fix indexing

--- scripts/mediawiki_session.py
+++ scripts/mediawiki_session.py
@@ -63,7 +63,7 @@ class MediaWikiSession(contextlib.AbstractContextManager):
             data={
                 "lgname": WIKI_USER,
                 "lgpassword": WIKI_PASSWORD,
-                "lgtoken": token_response["query"]["tokens"]["logintoken"],
+                "lgtoken": token_response["tokens"]["logintoken"],
             },
         )
         self.__cookie_jar.save(ignore_discard=True)
