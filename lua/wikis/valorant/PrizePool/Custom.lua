1d981a8fc8e9456ae51a0767b8f42164b6c05fe0
Idris <78701338+Idris1401@users.noreply.github.com>
2026-03-17 17:28:17 +0100

    feat(prizepool): update weight calculation in valorant

--- lua/wikis/valorant/PrizePool/Custom.lua
+++ lua/wikis/valorant/PrizePool/Custom.lua
@@ -11,6 +11,7 @@ local Arguments = Lua.import('Module:Arguments')
 local Class = Lua.import('Module:Class')
 local Logic = Lua.import('Module:Logic')
 local Variables = Lua.import('Module:Variables')
+local HighlightConditions = Lua.import('Module:HighlightConditions')
 
 local PrizePool = Lua.import('Module:PrizePool')
 local Opponent = Lua.import('Module:Opponent')
@@ -22,6 +23,7 @@ local CustomPrizePool = {}
 
 local TIER_VALUE = {10, 6, 4, 2}
 local TYPE_MODIFIER = {Online = 0.65}
+local TIER_TYPE_MODIFIER = {Showmatch = 0,  Misc = 0.25, Qualifier = 0.25, Monthly = 0.4, Weekly = 0.1}
 
 -- Template entry point
 ---@param frame Frame
@@ -40,11 +42,14 @@ end
 ---@param opponent BasePlacementOpponent
 ---@return placement
 function CustomLpdbInjector:adjust(lpdbData, placement, opponent)
+	lpdbData.publishertier = Variables.varDefault('tournament_publishertier', '')
 	lpdbData.weight = CustomPrizePool.calculateWeight(
 		lpdbData.prizemoney,
 		Variables.varDefault('tournament_liquipediatier'),
 		placement.placeStart,
-		Variables.varDefault('tournament_type')
+		Variables.varDefault('tournament_liquipediatiertype'),
+		Variables.varDefault('tournament_type'),
+		HighlightConditions.tournament(lpdbData)
 	)
 
 	local participantLower = mw.ustring.lower(lpdbData.participant)
@@ -65,15 +70,21 @@ end
 ---@param tier string?
 ---@param place integer
 ---@param type string?
----@return integer
-function CustomPrizePool.calculateWeight(prizeMoney, tier, place, type)
+---@param tierType string?
+---@param isHighlighted boolean
+---@return number
+function CustomPrizePool.calculateWeight(prizeMoney, tier, place, type, tierType, isHighlighted)
 	if Logic.isEmpty(tier) then
 		return 0
 	end
 
 	local tierValue = TIER_VALUE[tier] or TIER_VALUE[tonumber(tier) or ''] or 1
+	local prizeFactor = (1000000 + prizeMoney) / 1000000
+	local tierTypeFactor = TIER_TYPE_MODIFIER[tierType] or 1
+	local highlightedFactor = isHighlighted and 2 or 1
+	local typeFactor = TYPE_MODIFIER[type] or 1
 
-	return tierValue * math.max(prizeMoney, 1) * (TYPE_MODIFIER[type] or 1) / place
+	return prizeFactor * tierValue * tierTypeFactor * highlightedFactor * typeFactor / place
 end
 
 return CustomPrizePool
