80cc59bcabbb25a964ed21a11f4ed487cd1de1a4
Eetu Rantanen <eeturantanen.dev@gmail.com>
2026-03-20 10:52:12 +0200

    adjust layout and styles

--- lua/wikis/commons/Widget/Tournaments/Ticker/ListItem.lua
+++ lua/wikis/commons/Widget/Tournaments/Ticker/ListItem.lua
@@ -8,12 +8,13 @@
 local Lua = require('Module:Lua')
 
 local Class = Lua.import('Module:Class')
+local LeagueIcon = Lua.import('Module:LeagueIcon')
 
 local Widget = Lua.import('Module:Widget')
 local HtmlWidgets = Lua.import('Module:Widget/Html/All')
 local DateRange = Lua.import('Module:Widget/Misc/DateRange')
+local Link = Lua.import('Module:Widget/Basic/Link')
 local TierPill = Lua.import('Module:Widget/Tournament/TierPill')
-local Title = Lua.import('Module:Widget/Tournament/Title')
 
 ---@class TournamentsTickerListItemProps
 ---@field tournament StandardTournament
@@ -34,24 +35,42 @@ function TournamentsTickerListItemWidget:render()
 	return HtmlWidgets.Div{
 		classes = {'tournaments-list-item'},
 		children = {
-			HtmlWidgets.Div{
-				classes = {'tournaments-list-item__title'},
-				children = {
-					Title{
-						tournament = tournament,
-						displayGameIcon = self.props.displayGameIcon,
-					},
-					TierPill{
-						tournament = tournament,
-						variant = 'subtle',
-					},
+			HtmlWidgets.Span{
+				classes = {'tournament-icon'},
+				children = LeagueIcon.display{
+					icon = tournament.icon,
+					iconDark = tournament.iconDark,
+					series = tournament.series,
+					link = tournament.pageName,
+					options = {noTemplate = true},
 				},
 			},
 			HtmlWidgets.Div{
-				classes = {'tournaments-list-item__date'},
-				children = DateRange{
-					startDate = tournament.startDate,
-					endDate = tournament.endDate,
+				classes = {'tournaments-list-item__content'},
+				children = {
+					HtmlWidgets.Div{
+						classes = {'tournaments-list-item__name'},
+						children = Link{
+							link = tournament.pageName,
+							children = tournament.displayName,
+						},
+					},
+					HtmlWidgets.Div{
+						classes = {'tournaments-list-item__meta'},
+						children = {
+							TierPill{
+								tournament = tournament,
+								variant = 'subtle',
+							},
+							HtmlWidgets.Div{
+								classes = {'tournaments-list-item__date'},
+								children = DateRange{
+									startDate = tournament.startDate,
+									endDate = tournament.endDate,
+								},
+							},
+						},
+					},
 				},
 			},
 		},
