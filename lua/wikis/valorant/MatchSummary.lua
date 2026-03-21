80ec2432d89148a72be5ab7d8c1840a31aa1639c
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2026-03-21 13:16:24 +0900

    update valorant match summary

--- lua/wikis/valorant/MatchSummary.lua
+++ lua/wikis/valorant/MatchSummary.lua
@@ -8,16 +8,23 @@
 local Lua = require('Module:Lua')
 
 local Array = Lua.import('Module:Array')
-local FnUtil = Lua.import('Module:FnUtil')
+local Class = Lua.import('Module:Class')
+local Logic = Lua.import('Module:Logic')
 local Operator = Lua.import('Module:Operator')
 
 local DisplayHelper = Lua.import('Module:MatchGroup/Display/Helper')
 local MatchSummary = Lua.import('Module:MatchSummary/Base')
+
+local HtmlWidgets = Lua.import('Module:Widget/Html/All')
 local MatchSummaryWidgets = Lua.import('Module:Widget/Match/Summary/All')
 local WidgetUtil = Lua.import('Module:Widget/Util')
 
 ---@class ValorantMatchSummary: CustomMatchSummaryInterface
 local CustomMatchSummary = {}
+
+---@class ValorantMatchSummaryGameRow: MatchSummaryGameRow
+---@operator call(MatchSummaryGameRowProps): ValorantMatchSummaryGameRow
+local ValorantMatchSummaryGameRow = Class.new(MatchSummaryWidgets.GameRow)
 
 ---@param args table
 ---@return Widget
@@ -30,22 +37,23 @@ end
 function CustomMatchSummary.createBody(match)
 
 	return WidgetUtil.collect(
-		Array.map(match.games, FnUtil.curry(CustomMatchSummary.createGame, match.date)),
+		MatchSummaryWidgets.GameContainer{
+			gridLayout = 'standard',
+			children = Array.map(match.games, function (game, gameIndex)
+				if Logic.isEmpty(game.map) then
+					return
+				end
+				return ValorantMatchSummaryGameRow{game = game, gameIndex = gameIndex}
+			end)
+		},
 		MatchSummaryWidgets.Mvp(match.extradata.mvp),
 		MatchSummaryWidgets.MapVeto(MatchSummary.preProcessMapVeto(match.extradata.mapveto, {game = match.game}))
 	)
 end
 
-
-
----@param date string
----@param game MatchGroupUtilGame
----@param gameIndex integer
----@return Widget?
-function CustomMatchSummary.createGame(date, game, gameIndex)
-	if not game.map then
-		return
-	end
+---@return Widget
+function ValorantMatchSummaryGameRow:createGameDetail()
+	local game = self.props.game
 
 	local function scoreDisplay(oppIdx)
 		return DisplayHelper.MapScore(game.opponents[oppIdx], game.status)
@@ -62,12 +70,14 @@ function CustomMatchSummary.createGame(date, game, gameIndex)
 	end
 
 	local extradata = game.extradata or {}
+
+	---@param opponentIndex integer
+	---@return Widget[]
 	local function makeTeamSection(opponentIndex)
 		local flipped = opponentIndex == 2
 		local firstSide = flipped and CustomMatchSummary._getOppositeSide(extradata.t1firstside) or extradata.t1firstside
 		local characters = Array.map((game.opponents[opponentIndex] or {}).players or {}, Operator.property('agent'))
 		return {
-			MatchSummaryWidgets.GameWinLossIndicator{winner = game.winner, opponentIndex = opponentIndex},
 			MatchSummaryWidgets.Characters{characters = characters, flipped = flipped, hideOnMobile = true},
 			MatchSummaryWidgets.DetailedScore{
 				score = scoreDisplay(opponentIndex),
@@ -80,14 +90,12 @@ function CustomMatchSummary.createGame(date, game, gameIndex)
 		}
 	end
 
-	return MatchSummaryWidgets.Row{
-		classes = {'brkts-popup-body-game'},
-		children = WidgetUtil.collect(
+	return HtmlWidgets.Div{
+		children = {
 			MatchSummaryWidgets.GameTeamWrapper{children = makeTeamSection(1)},
 			MatchSummaryWidgets.GameCenter{children = DisplayHelper.Map(game)},
-			MatchSummaryWidgets.GameTeamWrapper{children = makeTeamSection(2), flipped = true},
-			MatchSummaryWidgets.GameComment{children = game.comment}
-		)
+			MatchSummaryWidgets.GameTeamWrapper{children = makeTeamSection(2), flipped = true}
+		}
 	}
 end
 
