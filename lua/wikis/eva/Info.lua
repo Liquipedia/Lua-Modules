ba9985d4ebbbec8daf523e2d3e43d92c8d83d553
Hesketh2 <88981446+Hesketh2@users.noreply.github.com>
2026-03-24 15:34:41 +0900

    Add eva:Module:Info

---
+++ lua/wikis/eva/Info.lua
@@ -0,0 +1,46 @@
+---
+-- @Liquipedia
+-- page=Module:Info
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+return {
+	startYear = 2017,
+	wikiName = 'eva',
+	name = 'EVA',
+	defaultGame = 'eva',
+	games = {
+		eva= {
+			abbreviation = 'EVA',
+			name = 'EVA',
+			link = 'Esports Virtual Arena',
+			logo = {
+				darkMode = 'EVA lightmode.png',
+				lightMode = 'EVA darkmode.png',
+			},
+			defaultTeamLogo = {
+				darkMode = 'EVA lightmode.png',
+				lightMode = 'EVA darkmode.png',
+			},
+		},
+	},
+	config = {
+		squads = {
+			hasPosition = false,
+			hasSpecialTeam = false,
+			allowManual = false,
+		},
+		match2 = {
+			status = 2,
+			matchWidth = 180,
+		},
+		infoboxPlayer = {
+			autoTeam = true,
+			automatedHistory = {
+				mode = 'automatic',
+				storeFromWikiCode = true,
+			},
+		},
+	},
+}
