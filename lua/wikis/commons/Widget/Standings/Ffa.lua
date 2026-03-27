af3e13df23e0f14d182150c3b1d4143063c56c13
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2026-03-27 15:15:11 +0900

    move ffa standings to match2

--- lua/wikis/commons/Widget/Standings/Ffa.lua
+++ lua/wikis/commons/Widget/Standings/Ffa.lua
@@ -9,15 +9,17 @@ local Lua = require('Module:Lua')
 
 local Array = Lua.import('Module:Array')
 local Class = Lua.import('Module:Class')
-local Table = Lua.import('Module:Table')
+local String = Lua.import('Module:StringUtils')
 
 local WidgetUtil = Lua.import('Module:Widget/Util')
 local Widget = Lua.import('Module:Widget')
 local HtmlWidgets = Lua.import('Module:Widget/Html/All')
-local DataTable = Lua.import('Module:Widget/Basic/DataTable')
+local Label = Lua.import('Module:Widget/Basic/Label')
 local RoundSelector = Lua.import('Module:Widget/Standings/RoundSelector')
 local PlacementChange = Lua.import('Module:Widget/Standings/PlacementChange')
+local TableWidgets = Lua.import('Module:Widget/Table2/All')
 
+local Opponent = Lua.import('Module:Opponent/Custom')
 local OpponentDisplay = Lua.import('Module:OpponentDisplay/Custom')
 
 local STATUS_TO_DISPLAY = {
@@ -45,117 +47,131 @@ function StandingsFfaWidget:render()
 		function (round) return round.round end
 	) or {round = 0}).round
 	local hasFutureRounds = not standings.rounds[#standings.rounds].started
-	local showRoundColumns = #standings.rounds > 1
 
-	return DataTable{
-		wrapperClasses = {'standings-ffa', 'toggle-area', 'toggle-area-' .. activeRounds},
-		classes = {'wikitable-bordered', 'wikitable-striped'},
+	return HtmlWidgets.Div{
+		classes = {'toggle-area', 'toggle-area-' .. activeRounds},
+		css = {display = 'contents'},
 		attributes = {['data-toggle-area'] = activeRounds},
 		children = WidgetUtil.collect(
-			-- Outer header
-			HtmlWidgets.Tr{children = HtmlWidgets.Th{
-				attributes = {
-					colspan = 100,
-				},
+			activeRounds > 0 and RoundSelector{
+				rounds = activeRounds,
+				hasEnded = not hasFutureRounds,
+			} or nil,
+			TableWidgets.Table{
+				classes = {'standings-ffa'},
+				title = String.nilIfEmpty(standings.title),
 				children = {
-					HtmlWidgets.Div{
-						children = {
-							HtmlWidgets.Span{
-								children = RoundSelector{
-									rounds = activeRounds,
-									hasEnded = not hasFutureRounds,
-								}
-							},
-							HtmlWidgets.Span{
-								children = standings.title
-							},
-						},
-						classes = {'tableHeaderRow'}
-					},
-				},
-			}},
-			-- Column Header
-			HtmlWidgets.Tr{children = WidgetUtil.collect(
-				HtmlWidgets.Th{children = '#'},
-				HtmlWidgets.Th{children = 'Participant'},
-				showRoundColumns and HtmlWidgets.Th{children = ''} or nil,
-				Array.map(standings.tiebreakers, function(tiebreaker)
+					self:_headerRow(),
+					Array.map(standings.rounds, function (round)
+						if round.round > activeRounds then
+							return
+						end
+						return self:_createRoundBody(round)
+					end)
+				}
+			}
+		)
+	}
+end
+
+---@private
+---@return boolean
+function StandingsFfaWidget:_hasFutureRounds()
+	local standings = self.props.standings
+	return not standings.rounds[#standings.rounds].started
+end
+
+---@private
+---@return boolean
+function StandingsFfaWidget:_showRoundColumns()
+	local standings = self.props.standings
+	return #standings.rounds > 1
+end
+
+---@private
+---@return Widget
+function StandingsFfaWidget:_headerRow()
+	local standings = self.props.standings
+
+	---@param text string?
+	---@return Widget
+	local makeHeaderCell = function(text)
+		return TableWidgets.CellHeader{children = text}
+	end
+
+	return TableWidgets.TableHeader{children = {
+		TableWidgets.Row{children = WidgetUtil.collect(
+			makeHeaderCell('#'),
+			makeHeaderCell('Participant'),
+			self:_showRoundColumns() and makeHeaderCell() or nil,
+			Array.map(standings.tiebreakers, function(tiebreaker)
+				if not tiebreaker.title then
+					return
+				end
+				return makeHeaderCell(tiebreaker.title)
+			end),
+			self:_showRoundColumns() and Array.map(standings.rounds, function(round)
+				return makeHeaderCell(round.title)
+			end) or nil
+		)}
+	}}
+end
+
+---@private
+---@param round StandingsRound
+---@return Widget
+function StandingsFfaWidget:_createRoundBody(round)
+	local standings = self.props.standings
+	return TableWidgets.TableBody{children = Array.map(round.opponents, function (slot)
+		return TableWidgets.Row{
+			attributes = {
+				['data-position-status'] = slot.positionStatus,
+				['data-toggle-area-content'] = round.round,
+			},
+			children = WidgetUtil.collect(
+				TableWidgets.Cell{children = Label{
+					children = slot.placement,
+					attributes = {['data-placement-type'] = slot.definitiveStatus},
+					labelScheme = 'placement',
+				}},
+				TableWidgets.Cell{children = OpponentDisplay.BlockOpponent{
+					opponent = slot.opponent,
+					overflow = 'ellipsis',
+					teamStyle = 'hybrid',
+					showPlayerTeam = true,
+				}},
+				self:_showRoundColumns() and TableWidgets.Cell{
+					children = PlacementChange{change = slot.positionChangeFromPreviousRound}
+				} or nil,
+				Array.map(standings.tiebreakers, function(tiebreaker, tiebreakerIndex)
 					if not tiebreaker.title then
 						return
 					end
-					return HtmlWidgets.Th{children = tiebreaker.title}
-				end),
-				showRoundColumns and Array.map(standings.rounds, function(round)
-					return HtmlWidgets.Th{children = round.title}
-				end) or nil
-			)},
-			-- Rows
-			Array.flatMap(standings.rounds, function(round)
-				if round.round > activeRounds then
-					return {}
-				end
-				return Array.map(round.opponents, function(slot)
-					local positionBackground = slot.positionStatus and ('bg-' .. slot.positionStatus) or nil
-					local teamBackground
-					if not hasFutureRounds and slot.definitiveStatus then
-						teamBackground = 'bg-' .. slot.definitiveStatus
-					end
-					return HtmlWidgets.Tr{
-						children = WidgetUtil.collect(
-							HtmlWidgets.Td{
-								children = {slot.placement, '.'},
-								css = {['font-weight'] = 'bold'},
-								classes = {positionBackground},
-							},
-							HtmlWidgets.Td{
-								classes = {teamBackground},
-								children = OpponentDisplay.BlockOpponent{
-									opponent = slot.opponent,
-									overflow = 'ellipsis',
-									teamStyle = 'hybrid',
-									showPlayerTeam = true,
-								}
-							},
-							showRoundColumns and HtmlWidgets.Td{
-								classes = {teamBackground},
-								children = PlacementChange{change = slot.positionChangeFromPreviousRound}
-							} or nil,
-							Array.map(standings.tiebreakers, function(tiebreaker, tiebreakerIndex)
-								if not tiebreaker.title then
-									return
-								end
-								return HtmlWidgets.Td{
-									classes = {teamBackground},
-								css = {['font-weight'] = tiebreakerIndex == 1 and 'bold' or nil, ['text-align'] = 'center'},
-									children = slot.tiebreakerValues[tiebreaker.id] and slot.tiebreakerValues[tiebreaker.id].display or ''
-								}
-							end),
-							showRoundColumns and Array.map(standings.rounds, function(columnRound)
-								local text
-								if columnRound.round <= round.round then
-									local opponent = Array.find(columnRound.opponents, function(columnSlot)
-										return Table.deepEquals(columnSlot.opponent, slot.opponent)
-									end)
-									if opponent then
-										local roundStatus = opponent.specialStatus
-										if roundStatus == '' then
-											text = opponent.pointsChangeFromPreviousRound
-										else
-											text = STATUS_TO_DISPLAY[roundStatus]
-										end
-									end
-								end
-								return HtmlWidgets.Td{children = text, css = {['text-align'] = 'center'}}
-							end) or nil
-						),
-						attributes = {
-							['data-toggle-area-content'] = round.round,
-						}
+					return TableWidgets.Cell{
+						css = {['font-weight'] = tiebreakerIndex == 1 and 'bold' or nil, ['text-align'] = 'center'},
+						children = slot.tiebreakerValues[tiebreaker.id] and slot.tiebreakerValues[tiebreaker.id].display or ''
 					}
-				end)
-			end)
-		)
-	}
+				end),
+				self:_showRoundColumns() and Array.map(standings.rounds, function (columnRound)
+					local text
+					if columnRound.round <= round.round then
+						local opponent = Array.find(columnRound.opponents, function(columnSlot)
+							return Opponent.same(columnSlot.opponent, slot.opponent)
+						end)
+						if opponent then
+							local roundStatus = opponent.specialStatus
+							if roundStatus == '' then
+								text = opponent.pointsChangeFromPreviousRound
+							else
+								text = STATUS_TO_DISPLAY[roundStatus]
+							end
+						end
+					end
+					return TableWidgets.Cell{children = text, css = {['text-align'] = 'center'}}
+				end) or nil
+			)
+		}
+	end)}
 end
 
 return StandingsFfaWidget
