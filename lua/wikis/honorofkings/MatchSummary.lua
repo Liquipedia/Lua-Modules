feecea7548f968520fae625abe865310c3ffb4e9
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2026-03-31 12:29:40 +0900

    use grid layout in HoK

--- lua/wikis/honorofkings/MatchSummary.lua
+++ lua/wikis/honorofkings/MatchSummary.lua
@@ -5,12 +5,10 @@
 -- Please see https://github.com/Liquipedia/Lua-Modules to contribute
 --
 
-local CustomMatchSummary = {}
-
 local Lua = require('Module:Lua')
 
 local Array = Lua.import('Module:Array')
-local FnUtil = Lua.import('Module:FnUtil')
+local Class = Lua.import('Module:Class')
 local Logic = Lua.import('Module:Logic')
 
 local MatchSummary = Lua.import('Module:MatchSummary/Base')
@@ -19,6 +17,13 @@ local WidgetUtil = Lua.import('Module:Widget/Util')
 
 local MAX_NUM_BANS = 5
 local NUM_CHAMPIONS_PICK = 5
+
+---@class HoKCustomMatchSummary: CustomMatchSummaryInterface
+local CustomMatchSummary = {}
+
+---@class HoKMatchSummaryGameRow: MatchSummaryGameRow
+---@operator call(MatchSummaryGameRowProps): HoKMatchSummaryGameRow
+local HoKMatchSummaryGameRow = Class.new(MatchSummaryWidgets.GameRow)
 
 ---@param args table
 ---@return Widget
@@ -31,50 +36,50 @@ end
 function CustomMatchSummary.createBody(match)
 	local characterBansData = MatchSummary.buildCharacterBanData(match.games, MAX_NUM_BANS)
 
+	---@param game MatchGroupUtilGame
+	---@return boolean
+	local function hasCharacterData(game)
+		local extradata = game.extradata or {}
+		return Array.any(Array.range(1, NUM_CHAMPIONS_PICK), function (index)
+			return Logic.isNotEmpty(extradata['team1champion' .. index])
+				or Logic.isNotEmpty(extradata['team2champion' .. index])
+		end)
+	end
+
 	return WidgetUtil.collect(
-		Array.map(match.games, FnUtil.curry(CustomMatchSummary._createGame, match.date)),
+		MatchSummaryWidgets.GamesContainer{
+			gridLayout = 'standard',
+			children = Array.map(match.games, function (game, gameIndex)
+				if Logic.isEmpty(game.length) and Logic.isEmpty(game.winner) and not hasCharacterData(game) then
+					return
+				end
+				return HoKMatchSummaryGameRow{game = game, gameIndex = gameIndex}
+			end)
+		},
 		MatchSummaryWidgets.Mvp(match.extradata.mvp),
 		MatchSummaryWidgets.CharacterBanTable{bans = characterBansData, date = match.date}
 	)
 end
 
----@param date string
----@param game MatchGroupUtilGame
----@param gameIndex integer
----@return MatchSummaryRow?
-function CustomMatchSummary._createGame(date, game, gameIndex)
+---@return Renderable?
+function HoKMatchSummaryGameRow:createGameOverview()
+	return self:lengthDisplay()
+end
+
+---@param opponentIndex integer
+---@return Widget
+function HoKMatchSummaryGameRow:createGameOpponentView(opponentIndex)
+	local props = self.props
+	local game = props.game
 	local extradata = game.extradata or {}
 
-	-- TODO: Change to use participant data
-	local characterData = {
-		MatchSummary.buildCharacterList(extradata, 'team1champion', NUM_CHAMPIONS_PICK),
-		MatchSummary.buildCharacterList(extradata, 'team2champion', NUM_CHAMPIONS_PICK),
-	}
-
-	if Logic.isEmpty(game.length) and Logic.isEmpty(game.winner) and Logic.isDeepEmpty(characterData) then
-		return nil
-	end
-
-	return MatchSummaryWidgets.Row{
-		classes = {'brkts-popup-body-game'},
-		children = WidgetUtil.collect(
-			MatchSummaryWidgets.Characters{
-				flipped = false,
-				characters = characterData[1],
-				bg = 'brkts-popup-side-color brkts-popup-side-color--' .. (extradata.team1side or ''),
-				date = date,
-			},
-			MatchSummaryWidgets.GameWinLossIndicator{winner = game.winner, opponentIndex = 1},
-			MatchSummaryWidgets.GameCenter{children = Logic.nilIfEmpty(game.length) or ('Game ' .. gameIndex)},
-			MatchSummaryWidgets.GameWinLossIndicator{winner = game.winner, opponentIndex = 2},
-			MatchSummaryWidgets.Characters{
-				flipped = true,
-				characters = characterData[2],
-				bg = 'brkts-popup-side-color brkts-popup-side-color--' .. (extradata.team2side or ''),
-				date = date,
-			},
-			MatchSummaryWidgets.GameComment{children = game.comment}
-		)
+	return MatchSummaryWidgets.Characters{
+		flipped = opponentIndex == 2,
+		characters = MatchSummary.buildCharacterList(
+			extradata, 'team' .. opponentIndex .. 'champion', NUM_CHAMPIONS_PICK
+		),
+		bg = 'brkts-popup-side-color brkts-popup-side-color--' .. (extradata['team' .. opponentIndex .. 'side'] or ''),
+		date = game.date,
 	}
 end
 
