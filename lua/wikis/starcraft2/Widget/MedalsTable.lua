fa902ae2ae4a6037edfa4612ae89e5a89f03f679
hjpalpha <andreasbuesch1@gmail.com>
2026-03-31 18:37:02 +0200

    stats on player and team pages

---
+++ lua/wikis/starcraft2/Widget/MedalsTable.lua
@@ -0,0 +1,82 @@
+local Lua = require('Module:Lua')
+
+local Array = Lua.import('Module:Array')
+local Class = Lua.import('Module:Class')
+local Medals = Lua.import('Module:Medals')
+local Table = Lua.import('Module:Table')
+local Tier = require('Module:Tier/Utils')
+
+local TableWidgets = Lua.import('Module:Widget/Table2/All')
+local Widget = Lua.import('Module:Widget')
+local WidgetUtil = Lua.import('Module:Widget/Util')
+
+local DATA_COLUMNS = {'1', '2', '3', '3-4', '4', 'total'}
+
+---@class MedalsTable: Widget
+---@operator call(table): MedalsTable
+---@field props {caption: string?, footer: Renderable?, data: table}
+local MedalsTable = Class.new(Widget)
+
+---@return Widget
+function MedalsTable:render()
+	return TableWidgets.Table{
+		caption = self.props.caption,
+		sortable = true,
+		columns = WidgetUtil.collect(
+			{align = 'left'}, -- tier
+			Array.map(DATA_COLUMNS, function() return {align = 'right'} end)
+		),
+		children = {
+			TableWidgets.TableHeader{
+				children = TableWidgets.Row{
+					children = WidgetUtil.collect(
+						TableWidgets.CellHeader{children = 'Tier'},
+						Array.map(DATA_COLUMNS, MedalsTable._headerCell)
+					)
+				}
+			},
+			TableWidgets.TableBody{children = self:_rows()}
+		},
+		footer = self.props.footer
+	}
+end
+
+---@private
+---@param dataColumn string
+---@return Widget
+function MedalsTable._headerCell(dataColumn)
+	return TableWidgets.CellHeader{
+		children = dataColumn == 'total' and 'Total' or Medals.display{medal = dataColumn}
+	}
+end
+
+---@private
+---@return Widget[]
+function MedalsTable:_rows()
+	local totalRowDataSet = Table.extract(self.props.data, 'total')
+	local rows = {}
+
+	for tier, dataSet in Table.iter.spairs(self.props.data) do
+		table.insert(rows, self:_row(Tier.display(tonumber(tier), nil, {link = true}), dataSet))
+	end
+	table.insert(rows, self:_row('Total', totalRowDataSet))
+
+	return rows
+end
+
+---@private
+---@param firstCellContent Renderable[]|Renderable
+---@param data table<string|integer, integer>
+---@return Widget[]
+function MedalsTable:_row(firstCellContent, data)
+	return TableWidgets.Row{
+		children = WidgetUtil.collect(
+			TableWidgets.Cell{children = firstCellContent},
+			Array.map(DATA_COLUMNS, function(column)
+				return TableWidgets.Cell{children = data[tonumber(column) or column]}
+			end)
+		)
+	}
+end
+
+return MedalsTable
---
+++ lua/wikis/starcraft2/Widget/PlayerPageStatistics.lua
@@ -0,0 +1,154 @@
+local Lua = require('Module:Lua')
+
+local Array = Lua.import('Module:Array')
+local Class = Lua.import('Module:Class')
+local Faction = Lua.import('Module:Faction')
+local Json = require('Module:Json')
+local Logic = require('Module:Logic')
+local Math = require('Module:MathUtil')
+local Operator = require('Module:Operator')
+local Table = require('Module:Table')
+local Variables = require('Module:Variables')
+
+local EarningsStatsChart = Lua.import('Module:Widget/EarningsStatsChart')
+local MedalsTable = Lua.import('Module:Widget/MedalsTable')
+local TableWidgets = Lua.import('Module:Widget/Table2/All')
+local Widget = Lua.import('Module:Widget')
+local WidgetUtil = Lua.import('Module:Widget/Util')
+
+---@class PlayerPageStatistics: Widget
+---@operator call(table): PlayerPageStatistics
+local PlayerPageStatistics = Class.new(Widget)
+
+---@return Widget
+function PlayerPageStatistics:render()
+	return WidgetUtil.collect(
+		self:_matchupStats(),
+		self:_earningsChart(),
+		MedalsTable{
+			caption = '1v1 Medal Statistics',
+			data = Json.parseIfString(Variables.varDefault('medals')),
+		}
+	)
+end
+
+---@return Widget?
+function PlayerPageStatistics:_matchupStats()
+	local data = Json.parseIfString(Variables.varDefault('matchUpStats'))
+	if Logic.isEmpty(data) then
+		return
+	end
+
+	local columns = Array.map(Faction.knownFactions, function(faction)
+		return data.total[faction] and faction or nil
+	end)
+	table.insert(columns, 'total')
+
+	local rows = {}
+	for row, rowData in Table.iter.spairs(data, function(tbl, key1, key2)
+		local total1 = (tbl[key1].total.w or 0) + (tbl[key1].total.l or 0)
+		local total2 = (tbl[key2].total.w or 0) + (tbl[key2].total.l or 0)
+		return total1 > total2
+	end) do
+		table.insert(rows, self:_matchupStatsRow(row, rowData, columns))
+	end
+
+	return TableWidgets.Table{
+		caption = 'Matchup Statistics',
+		columns = WidgetUtil.collect(
+			{align = 'center'}, -- faction
+			unpack(Array.map(columns, function()
+				return {
+					{align = 'center'}, -- Record
+					{align = 'center'}, -- Win%
+				}
+			end))
+		),
+		children = {
+			self:_matchupStatsHeader(columns),
+			TableWidgets.TableBody{children = rows}
+		},
+		footer = self.props.footer
+	}
+end
+
+---@param row string
+---@param rowData table<string, {w: integer?, l: integer?}>
+---@param columns string[]
+---@return unknown
+function PlayerPageStatistics:_matchupStatsRow(row, rowData, columns)
+	return TableWidgets.Row{
+		classes = {row ~= 'total' and Faction.bgClass(row) or nil},
+		children = WidgetUtil.collect(
+			TableWidgets.Cell{
+				children = row == 'total' and 'Σ' or {
+					'as ',
+					Faction.Icon{faction = row},
+				}
+			},
+			unpack(Array.map(columns, function(col)
+				local data = rowData[col] or {}
+				local sum = (data.w or 0) + (data.l or 0)
+				local percent = sum == 0 and '-' or (Math.round((data.w or 0) * 100 / sum, 1) .. ' %')
+				return {
+					TableWidgets.Cell{
+						children = {
+							data.w or 0,
+							' - ',
+							data.l or 0
+						}
+					},
+					TableWidgets.Cell{children = percent},
+				}
+			end))
+		)
+	}
+end
+
+---@param columns string[]
+---@return Widget
+function PlayerPageStatistics:_matchupStatsHeader(columns)
+	return TableWidgets.TableHeader{
+		children = {
+			TableWidgets.Row{
+				children = WidgetUtil.collect(
+					TableWidgets.CellHeader{children = '', rowspan = 2},
+					Array.map(columns, function(col)
+						local text = col == 'total' and 'Total' or ('vs ' .. Faction.Icon{faction = col})
+						return TableWidgets.CellHeader{children = text, colspan = 2}
+					end)
+				)
+			},
+			TableWidgets.Row{
+				children = WidgetUtil.collect(
+					unpack(Array.map(columns, function(col)
+						return {
+							TableWidgets.CellHeader{children = 'Record'},
+							TableWidgets.CellHeader{children = 'Win%'},
+						}
+					end))
+				)
+			},
+		}
+	}
+end
+
+---@return Widget
+function PlayerPageStatistics:_earningsChart()
+	local rawData = Json.parseIfString(Variables.varDefault('earningsStats')) or {}
+
+	return EarningsStatsChart{
+		data = {
+			solo = Table.mapValues(rawData, Operator.property('solo')),
+			team = Table.mapValues(rawData, Operator.property('team')),
+			other = Table.mapValues(rawData, Operator.property('other')),
+		},
+		dataPoints = {
+			{key = 'solo', legend = '1v1 Earnings'},
+			{key = 'team', legend = 'Team Event Earnings'},
+			{key = 'other', legend = 'Other Earnings'},
+		},
+	}
+end
+
+return PlayerPageStatistics
