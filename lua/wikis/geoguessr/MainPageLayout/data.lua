e563f70cf7f71a5c310e2c13dd085341bea37816
steve020607 <legolas0.savage@gmail.com>
2026-04-01 13:12:52 +0700

    Add New Ticker to GeoGuessr and Mobilelegends (#7327)
    * GeoGuessr New Ticker
    
    * Mobile Legends Bang Bang new ticker

--- lua/wikis/geoguessr/MainPageLayout/data.lua
+++ lua/wikis/geoguessr/MainPageLayout/data.lua
@@ -10,7 +10,7 @@ local Lua = require('Module:Lua')
 local MainPageLayoutUtil = Lua.import('Module:MainPageLayout/Util')
 
 local FilterButtonsWidget = Lua.import('Module:Widget/FilterButtons')
-local TournamentsTicker = Lua.import('Module:Widget/Tournaments/Ticker')
+local TournamentsTicker = Lua.import('Module:Widget/Tournaments/Ticker/List')
 
 local HtmlWidgets = Lua.import('Module:Widget/Html/All')
 local Div = HtmlWidgets.Div
@@ -67,8 +67,10 @@ local CONTENT = {
 		heading = 'Tournaments',
 		body = TournamentsTicker{
 			upcomingDays = 60,
-			completedDays = 30
+			completedDays = 30,
+			variant = 'collapsible',
 		},
+		padding = false,
 		boxid = MainPageLayoutUtil.BoxId.TOURNAMENTS_TICKER,
 	},
 }
