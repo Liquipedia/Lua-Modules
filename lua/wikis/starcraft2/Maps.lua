be2472dc414ed2f0df52694f12766e95f9916b56
hjpalpha <andreasbuesch1@gmail.com>
2026-04-01 16:46:43 +0200

    Maps

---
+++ lua/wikis/starcraft2/Maps.lua
@@ -0,0 +1,238 @@
+local Lua = require('Module:Lua')
+
+local Array = Lua.import('Module:Array')
+local Class = Lua.import('Module:Class')
+local Json = Lua.import('Module:Json')
+local Logic = Lua.import('Module:Logic')
+local Table = Lua.import('Module:Table')
+local Variables = Lua.import('Module:Variables')
+
+---@type table<string, SC2MapsMap>
+local MapData = Lua.import('Module:MapData', {loadData = true})
+
+local Image = Lua.import('Module:Widget/Image/Icon/Image')
+local Link = Lua.import('Module:Widget/Basic/Link')
+local TableWidgets = Lua.import('Module:Widget/Table2/All')
+local WidgetUtil = Lua.import('Module:Widget/Util')
+
+---@class SC2MapsMap
+---@field link string
+---@field displayname string?
+---@field author string?
+---@field authorLink string?
+---@field image string?
+---@field imageDark string?
+
+---@class SC2MapsConfig
+---@field note string?
+---@field showAuthor boolean
+---@field sort boolean
+---@field size string
+---@field title string?
+---@field tournament string?
+
+local DEFAULT_THUMB_SIZE = 'x120px'
+
+local Maps = {}
+
+---@private
+---@param key string?
+---@return SC2MapsMap?
+function Maps._getData(key)
+	if Logic.isEmpty(key) then
+		return
+	end
+	---@cast key -nil
+
+	key = key:gsub('%s*LE$', ''):gsub('%s*TE$', ''):gsub('%s*CE$', ''):gsub(' %([mM]ap%)$', ''):gsub('_', ' ')
+	return MapData[key:lower()]
+end
+
+-- EntryPoint Template:MapThumb
+---@param args {[1]: string?, size: string|number?}?
+---@return Widget?
+function Maps.thumb(args)
+	args = args or {}
+	local data = Maps._getData(args[1])
+	if not data then
+		return
+	end
+
+	return Maps._displayThumb(data, args.size)
+end
+
+---@private
+---@param data SC2MapsMap
+---@param size string|number?
+---@return Widget
+function Maps._displayThumb(data, size)
+	return Image{
+		imageLight = data.image,
+		imageDark = data.imageDark,
+		link = data.link,
+		size = size or DEFAULT_THUMB_SIZE,
+		alt = data.displayname or data.link,
+	}
+end
+
+-- EntryPoint Template:MapAuthor
+---@param args {[1]: string?}?
+---@return Widget|string?
+function Maps.author(args)
+	args = args or {}
+
+	local data = Maps._getData(args[1])
+	if not data then
+		return
+	end
+
+	return Maps._displayAuthor(data)
+end
+
+---@private
+---@param data SC2MapsMap
+---@return Widget|string?
+function Maps._displayAuthor(data)
+	if not data.authorLink then
+		return data.author
+	end
+
+	return Link{link = data.authorLink, children = data.author}
+end
+
+-- EntryPoint Template:Maps
+---@param args table?
+---@return Widget?
+function Maps.run(args)
+	args = args or {}
+
+	local config = Maps._readConfig(args)
+	local mapList = Maps._readMaps(args, config)
+
+	if Logic.isEmpty(mapList) then
+		return
+	end
+
+	mapList = Array.map(mapList, function(map)
+		if Logic.isNotEmpty(map.image) and (Logic.isNotEmpty(map.author) or not config.showAuthor) then
+			return map
+		end
+
+		local data = Maps._getData(map.link) or {}
+		return Table.merge(data, map)
+	end)
+
+	if config.sort then
+		Array.sortInPlaceBy(mapList, function(map)
+			return map.link .. (map.displayname or '')
+		end)
+	end
+
+	return Maps._display(mapList, config)
+end
+
+---@private
+---@param args table
+---@return SC2MapsConfig
+function Maps._readConfig(args)
+	return {
+		note = args.note,
+		showAuthor = Logic.readBool(args.author),
+		sort = Logic.readBool(args.sort),
+		size = args.size,
+		title = args.title,
+		tournament = args.tournament,
+	}
+end
+
+---@private
+---@param args table
+---@param config SC2MapsConfig
+---@return SC2MapsMap[]
+function Maps._readMaps(args, config)
+	---@type SC2MapsMap[]
+	local maps = Array.mapIndexes(function(mapIndex)
+		local prefix = 'map' .. mapIndex
+		local map = args[prefix]
+		if Logic.isEmpty(map) then
+			return
+		end
+
+		return {
+			displayname = args[prefix .. 'display'],
+			link = mw.ext.TeamLiquidIntegration.resolve_redirect(map),
+			image = args[prefix .. 'image'],
+			imageDark = args[prefix .. 'imageDark'],
+			author = args[prefix .. 'author'],
+			authorLink = args[prefix .. 'authorLink'],
+		}
+	end)
+
+	if maps[1] then
+		return maps
+	end
+
+	if not config.tournament then
+		maps = Json.parseIfTable(Variables.varDefault('tournament_maps')) --[[@as SC2MapsMap[] ]]
+		if maps then
+			return maps
+		end
+	end
+
+	local tournament = config.tournament or mw.title.getCurrentTitle().prefixedText
+	local data = mw.ext.LiquipediaDB.lpdb('tournament', {
+		conditions = '[[pagename::' .. tournament:gsub(' ', '_') .. ']]',
+		query = 'maps'
+	})
+
+	if Logic.isEmpty(data[1]) then
+		return {}
+	end
+
+	return Json.parseIfTable(data[1].maps) or {}
+end
+
+---@private
+---@param mapList SC2MapsMap[]
+---@param config SC2MapsConfig
+---@return Widget
+function Maps._display(mapList, config)
+	return TableWidgets.Table{
+		sortable = false,
+		caption = config.title,
+		columns = Array.map(mapList, function()
+			return {align = 'center'}
+		end),
+		children = {
+			-- name row
+			TableWidgets.Row{children = Array.map(mapList, function(map)
+				return TableWidgets.CellHeader{children = Link{link = map.link, children = map.displayname}}
+			end)},
+			TableWidgets.TableBody{children = WidgetUtil.collect(
+				-- image row
+				TableWidgets.Row{children = Array.map(mapList, function(map)
+					return TableWidgets.Cell{
+						children = Maps._displayThumb(map, config.size),
+					}
+				end)},
+				-- author row (if enabled)
+				config.showAuthor and TableWidgets.Row{children = Array.map(mapList, function(map)
+					return TableWidgets.Cell{
+						children = map.authorLink and {
+							'By ',
+							Maps._displayAuthor(map)
+						} or nil,
+						css = {
+							['font-size'] = '90%',
+							['padding-left'] = '4px',
+							['padding-right'] = '4px',
+						},
+					}
+				end)} or nil
+			)}
+		},
+		footer = config.note,
+	}
+end
+
+return Class.export(Maps, {exports = {'thumb', 'author', 'run'}})
