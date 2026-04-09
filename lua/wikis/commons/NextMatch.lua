30373335c0cfa490084ef2e31bd13d0d1b1fefd0
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2026-04-09 10:38:16 +0900

    memoize

--- lua/wikis/commons/NextMatch.lua
+++ lua/wikis/commons/NextMatch.lua
@@ -10,6 +10,7 @@ local Lua = require('Module:Lua')
 local Class = Lua.import('Module:Class')
 local Countdown = Lua.import('Module:Countdown')
 local DateExt = Lua.import('Module:Date/Ext')
+local FnUtil = Lua.import('Module:FnUtil')
 local Logic = Lua.import('Module:Logic')
 local MathUtil = Lua.import('Module:MathUtil')
 local String = Lua.import('Module:StringUtils')
@@ -32,13 +33,19 @@ local NextMatch = {}
 function NextMatch.run(args)
 	if not args[1] then return args.default or '' end
 
+	---@param key string
+	---@return boolean
+	local isMatchGroupsSpecArg = FnUtil.memoize(function (key)
+		return String.startsWith(key, 'tournament') or String.startsWith(key, 'matchGroupId')
+	end)
+
 	local matchGroupsSpec = TournamentStructure.readMatchGroupsSpec(
 		Table.map(
 			Table.filterByKey(args, function (key)
-				return MathUtil.isInteger(key) or String.startsWith(key, 'tournament') or String.startsWith(key, 'matchGroupId')
+				return MathUtil.isInteger(key) or isMatchGroupsSpecArg(key)
 			end),
 			function (key, value)
-				if String.startsWith(key, 'tournament') or String.startsWith(key, 'matchGroupId') then
+				if isMatchGroupsSpecArg(key) then
 					return key, value
 				end
 				return 'tournament' .. key, value
