8c447fbe7ae3a0c75cf24377ab78859f2f9c7484
hjpalpha <andreasbuesch1@gmail.com>
2026-04-13 11:25:14 +0200

    refactor Infobox/Extension/PlacementStats to use MedalsTable

--- lua/wikis/commons/Widget/MedalsTable.lua
+++ lua/wikis/commons/Widget/MedalsTable.lua
@@ -9,41 +9,50 @@ local Lua = require('Module:Lua')
 
 local Array = Lua.import('Module:Array')
 local Class = Lua.import('Module:Class')
+local FnUtil = Lua.import('Module:FnUtil')
 local Medals = Lua.import('Module:Medals')
 local Table = Lua.import('Module:Table')
 local Tier = Lua.import('Module:Tier/Utils')
 
+local HtmlWidgets = Lua.import('Module:Widget/Html/All')
 local TableWidgets = Lua.import('Module:Widget/Table2/All')
 local Widget = Lua.import('Module:Widget')
 local WidgetUtil = Lua.import('Module:Widget/Util')
 
-local DATA_COLUMNS = {'1', '2', '3', '3-4', '4', 'total'}
+local DATA_COLUMNS_VARIANT_1 = {'1', '2', '3', '3-4', '4', 'total'}
+local DATA_COLUMNS_VARIANT_2 = {1, 2, 3, 'top3', 'total'}
 
 ---@class MedalsTableProps
 ---@field caption string?
 ---@field footer Renderable?
----@field data table
+---@field data table<string, table<string, integer>> # table<tier, table<place, integer>>
+---@field hasAll boolean?
 
 ---@class MedalsTable: Widget
 ---@operator call(MedalsTableProps): MedalsTable
 ---@field props MedalsTableProps
+---@field dataColumns string[]
 local MedalsTable = Class.new(Widget)
 
 ---@return Widget
 function MedalsTable:render()
+	self.dataColumns = self.props.hasAll and DATA_COLUMNS_VARIANT_2 or DATA_COLUMNS_VARIANT_1
 	return TableWidgets.Table{
 		caption = self.props.caption,
 		sortable = true,
 		columns = WidgetUtil.collect(
 			{align = 'left'}, -- tier
-			Array.map(DATA_COLUMNS, function() return {align = 'right'} end)
+			Array.map(self.dataColumns, function() return {align = 'right'} end)
 		),
 		children = {
 			TableWidgets.TableHeader{
 				children = TableWidgets.Row{
 					children = WidgetUtil.collect(
-						TableWidgets.CellHeader{children = 'Tier'},
-						Array.map(DATA_COLUMNS, MedalsTable._headerCell)
+						TableWidgets.CellHeader{
+							children = 'Tier',
+							css = self.props.hasAll and {['padding-left'] = '0.3rem'} or nil,
+						},
+						Array.map(self.dataColumns, FnUtil.curry(MedalsTable._headerCell, self))
 					)
 				}
 			},
@@ -56,9 +65,20 @@ end
 ---@private
 ---@param dataColumn string
 ---@return Widget
-function MedalsTable._headerCell(dataColumn)
+function MedalsTable:_headerCell(dataColumn)
+	---@type Renderable?
+	local header
+	if dataColumn == 'total' then
+		header = 'Total'
+	elseif dataColumn == 'top3' then
+		header = HtmlWidgets.Abbr{title = 'Total of top 3', children = 'Top3'}
+	else
+		header = Medals.display{medal = dataColumn}
+	end
+
 	return TableWidgets.CellHeader{
-		children = dataColumn == 'total' and 'Total' or Medals.display{medal = dataColumn}
+		css = self.props.hasAll and {['padding-left'] = '0.3rem'} or nil,
+		children = header
 	}
 end
 
@@ -84,7 +104,7 @@ function MedalsTable:_row(firstCellContent, data)
 	return TableWidgets.Row{
 		children = WidgetUtil.collect(
 			TableWidgets.Cell{children = firstCellContent},
-			Array.map(DATA_COLUMNS, function(column)
+			Array.map(self.dataColumns, function(column)
 				return TableWidgets.Cell{children = data[tonumber(column) or column]}
 			end)
 		)
