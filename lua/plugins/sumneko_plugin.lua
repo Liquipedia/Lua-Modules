61b73140bd792550e7ac1b8620c1a5104cf8115e
Rikard Blixt <rikardblixt@gmail.com>
2026-04-18 19:38:36 +0200

    refactor: improve IDE require resolver

--- lua/plugins/sumneko_plugin.lua
+++ lua/plugins/sumneko_plugin.lua
@@ -3,10 +3,41 @@
 -- The setting "Lua.runtime.plugin" needs to be set to "plugins/sumneko_plugin.lua"
 -- See more at https://github.com/sumneko/lua-language-server/wiki/Plugin
 
-local liquipedia = {}
-
 local importFunctions = {}
-importFunctions.functions = {'require', 'mw%.loadData', 'Lua%.import', 'Lua%.requireIfExists'}
+
+---@param  uri  string
+---@param  name string # Argument of require()
+---@param  source string # The source file uri
+---@return string[]?
+-- luacheck: push ignore
+function ResolveRequire(uri, name, source)
+-- luacheck: pop ignore
+	local fileName = importFunctions.luaifyModuleName(name)
+
+	-- Extract the base path (up to and including /lua/) and the wiki name from the source URI
+	local basePath, wiki = source:match('^(file://.-/lua)/wikis/([^/]+)/')
+	if not basePath then
+		return nil
+	end
+
+	-- Check if the file exists in the same wiki
+	local wikiPath = basePath .. '/wikis/' .. wiki .. '/' .. fileName .. '.lua'
+	local wikiFile = io.open(wikiPath:gsub('^file://', ''), 'r')
+	if wikiFile then
+		wikiFile:close()
+		return {wikiPath}
+	end
+
+	-- Fall back to commons wiki
+	local commonsPath = basePath .. '/wikis/commons/' .. fileName .. '.lua'
+	local commonsFile = io.open(commonsPath:gsub('^file://', ''), 'r')
+	if commonsFile then
+		commonsFile:close()
+		return {commonsPath}
+	end
+
+	return nil
+end
 
 ---Transforms a MediaWiki module name, e.g. `Module:Array`, into a lua repository name, e.g. `Array`
 ---@param name string
@@ -16,54 +47,6 @@ function importFunctions.luaifyModuleName(name)
 		:gsub('Module:', '')-- Remove starting Module:
 
 	return normModuleName
-end
-
-function importFunctions._row(name)
-	local normModuleName = importFunctions.luaifyModuleName(name)
-
-	return ' ---@module \'' .. normModuleName .. '\''
-end
-
-function importFunctions.annotate(text, funcName, diffs)
-	for module, positionEndOfRow in text:gmatch(funcName .. '%s*%(?%s*[\'"](.-)[\'"]%s*%)?.-()\r?\n') do
-		table.insert(diffs,
-			{start = positionEndOfRow, finish = positionEndOfRow - 1, text = importFunctions._row(module)}
-		)
-	end
-end
-
-function liquipedia.annotate(text, diffs)
-	for _, funcName in pairs(importFunctions.functions) do
-		importFunctions.annotate(text, funcName, diffs)
-	end
-end
-
----@class diff
----@field start integer # The number of bytes at the beginning of the replacement
----@field finish integer # The number of bytes at the end of the replacement
----@field text string # What to replace
-
--- luacheck: push ignore
--- setting non-standard global variable 'OnSetText' (but it's mandatory)
----@param uri string # The uri of file
----@param text string # The content of file
----@return nil|diff[]
----@diagnostic disable-next-line: global-element
-function OnSetText(uri, text)
--- luacheck: pop ignore
-	if text:sub(1, 3) ~= '---' then
-		return nil
-	end
-
-	if text:sub(1, 8) == '---@meta' then
-		return nil
-	end
-
-	local diffs = {}
-
-	liquipedia.annotate(text, diffs)
-
-	return diffs
 end
 
 return importFunctions
