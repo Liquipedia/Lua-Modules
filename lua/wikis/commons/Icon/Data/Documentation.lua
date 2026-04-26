d6f3176ec761915b6f84ff54e590d1e904872576
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2026-04-26 09:28:10 +0200

    feat: Automated Documentation for icon data

---
+++ lua/wikis/commons/Icon/Data/Documentation.lua
@@ -0,0 +1,51 @@
+---
+-- @Liquipedia
+-- page=Module:Icon/Data/Documentation
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+
+local Lua = require('Module:Lua')
+
+local Array = Lua.import('Module:Array')
+local FnUtil = Lua.import('Module:FnUtil')
+local IconData = Lua.import('Module:Icon/Data', {loadData = true})
+local Table = Lua.import('Module:Table')
+
+local HtmlWidgets = Lua.import('Module:Widget/Html/All')
+local Icon = Lua.import('Module:Widget/Image/Icon/Fontawesome')
+local UnorderedList = Lua.import('Module:Widget/List/Unordered')
+
+local IconDataDoc = {}
+
+---@return Widget
+function IconDataDoc.generate()
+	local iconNames = Array.extractKeys(IconData)
+	local groupedIcons
+	_, groupedIconNames = Array.groupBy(iconNames, function(iconName)
+		return iconName:sub(1,1):upper()
+	end)
+
+	local children = {}
+	for letter, group in Table.iter.spairs(groupedIconNames) do
+		Array.sortInPlaceBy(group, FnUtil.identity)
+
+		Array.appendWith(children,
+			HtmlWidgets.H3{children = letter},
+			UnorderedList{children = Array.map(group, IconDataDoc._displayIcon)}
+		)
+	end
+
+	return HtmlWidgets.Fragment{children = children}
+end
+
+---@param iconName string
+---@return Renderable[]
+function IconDataDoc._displayIcon(iconName)
+	return {
+		iconName,
+		': ',
+		Icon{iconName = iconName, size = '1rem'},
+	}
+end
+
+return IconDataDoc
