3af2e82f3dfbd17655d2e3ec1564946565b58e18
Rikard Blixt <rikardblixt@gmail.com>
2026-04-30 15:40:59 +0200

    typo

--- lua/wikis/commons/TeamParticipants/Repository.lua
+++ lua/wikis/commons/TeamParticipants/Repository.lua
@@ -28,7 +28,7 @@ local TeamParticipantsRepository = {}
 function TeamParticipantsRepository.save(participant)
 	-- Since we merge data from prizepool and teamparticipants, we need to first fetch the existing record from prizepool
 	-- Records can come from multiple prizepools, such as from both normal prizepool and an award
-	local lpdbDatas = TeamParticipantsRepository.getPrizepoolRecordForTeam(participant.opponent) or {}
+	local lpdbData = TeamParticipantsRepository.getPrizepoolRecordForTeam(participant.opponent) or {}
 
 	local function generateObjectName()
 		local team = Opponent.toName(participant.opponent)
