65e44c4dac883c4ec8d9ff52b4ac117eada561a3
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2026-05-08 17:44:59 +0900

    add golden

---
+++ lua/spec/standings_legend_spec.lua
@@ -0,0 +1,20 @@
+--- Triple Comment to Enable our LLS Plugin
+insulate('Widget/Legend', function()
+	it('integration', function()
+		local LegendComponent = require('Module:Widget/Legend')
+
+		GoldenTest('standings_legend', tostring(LegendComponent{
+		color = {
+			byeup = 'Lorem ipsum',
+			seedup = 'Lorem ipsum',
+			up = 'Lorem ipsum',
+			stayup = 'Lorem ipsum',
+			stay = 'Lorem ipsum',
+			staydown = 'Lorem ipsum',
+			down = 'Lorem ipsum',
+		},
+		points = {'Lorem ipsum'},
+		showMinimum = true,
+	}))
+	end)
+end)
