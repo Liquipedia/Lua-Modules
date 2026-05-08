742eb9d54ed602e483dc5372271065cccb49669e
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2026-05-08 11:31:24 +0900

    add color section

--- lua/wikis/commons/Widget/Legend.lua
+++ lua/wikis/commons/Widget/Legend.lua
@@ -9,6 +9,7 @@ local Lua = require('Module:Lua')
 
 local Array = Lua.import('Module:Array')
 local Class = Lua.import('Module:Class')
+local Json = Lua.import('Module:Json')
 local Logic = Lua.import('Module:Logic')
 
 local Widget = Lua.import('Module:Widget')
@@ -16,6 +17,9 @@ local ChevronToggle = Lua.import('Module:Widget/Participants/Team/ChevronToggle'
 local GeneralCollapsible = Lua.import('Module:Widget/GeneralCollapsible/Default')
 local HtmlWidgets = Lua.import('Module:Widget/Html/All')
 local Icon = Lua.import('Module:Widget/Image/Icon/Fontawesome')
+local WidgetUtil = Lua.import('Module:Widget/Util')
+
+local LABEL_COLORS = {'byeup', 'seedup', 'up', 'stayup', 'stay', 'staydown', 'down'}
 
 ---@class LegendWidget: Widget
 ---@operator call(table): LegendWidget
@@ -28,16 +32,9 @@ function LegendWidget:render()
 		collapseAreaClasses = {},
 		classes = {'legend'},
 		titleWidget = self:_createHeader(),
-		children = Array.mapIndexes(function (index)
-			local sectionContent = self.props['section' .. index]
-			if Logic.isEmpty(sectionContent) then
-				return
-			end
-			return HtmlWidgets.Div{
-				classes = {'legend-section'},
-				children = self.props['section' .. index]
-			}
-		end)
+		children = WidgetUtil.collect(
+			self:_createColorSection()
+		)
 	}
 end
 
@@ -57,6 +54,34 @@ function LegendWidget:_createHeader()
 			},
 			ChevronToggle{}
 		}
+	}
+end
+
+---@private
+---@return Widget?
+function LegendWidget:_createColorSection()
+	local sectionData = Json.parseIfString(self.props.color)
+	if Logic.isEmpty(sectionData) then
+		return
+	end
+	return HtmlWidgets.Div{
+		classes = {'legend-section'},
+		children = Array.map(LABEL_COLORS, function (labelColor)
+			local labelText = sectionData[labelColor]
+			if Logic.isEmpty(labelText) then
+				return
+			end
+			return HtmlWidgets.Div{
+				classes = {'legend-item'},
+				children = {
+					HtmlWidgets.Span{
+						classes = {labelColor .. '-text'},
+						children = '&#9679;'
+					},
+					HtmlWidgets.Span{children = labelText}
+				}
+			}
+		end)
 	}
 end
 
