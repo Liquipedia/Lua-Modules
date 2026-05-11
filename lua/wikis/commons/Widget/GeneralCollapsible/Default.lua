45f3c3c965d9150a636985b2eb69c8f77c704c4b
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2026-05-11 11:54:32 +0900

    collapsible to widget3

--- lua/wikis/commons/Widget/GeneralCollapsible/Default.lua
+++ lua/wikis/commons/Widget/GeneralCollapsible/Default.lua
@@ -8,54 +8,45 @@
 local Lua = require('Module:Lua')
 
 local Array = Lua.import('Module:Array')
-local Class = Lua.import('Module:Class')
 
 local CollapsibleToggle = Lua.import('Module:Widget/GeneralCollapsible/Toggle')
-local HtmlWidgets = Lua.import('Module:Widget/Html/All')
-local B = HtmlWidgets.B
-local Div = HtmlWidgets.Div
-local Widget = Lua.import('Module:Widget')
+local Component = Lua.import('Module:Widget/Component')
+local Html = Lua.import('Module:Widget/Html')
+local B = Html.B
+local Div = Html.Div
 
----@class DefaultCollapsibleProps
----@field attributes table<string, string>?
----@field classes string[]?
----@field css table<string, string|number>?
+---@class DefaultCollapsibleProps: HtmlProps
 ---@field shouldCollapse boolean?
----@field title Renderable?
+---@field title Renderable|Renderable[]?
 ---@field titleClasses string[]?
 ---@field titleWidget Renderable?
 ---@field collapseAreaClasses string[]?
 ---@field collapseAreaCss table<string, string|number>?
 ---@field children Renderable|Renderable[]?
 
----@class DefaultCollapsible: Widget
----@operator call(DefaultCollapsibleProps?): DefaultCollapsible
----@field props DefaultCollapsibleProps
-local DefaultCollapsible = Class.new(Widget)
-
----@return Widget
-function DefaultCollapsible:render()
-	local props = self.props
+---@param props DefaultCollapsibleProps
+---@return HtmlNode
+local function DefaultCollapsible(props)
 	return Div{
 		attributes = props.attributes,
 		css = props.css,
-		classes = Array.extend({},
+		classes = Array.extend(
 			'general-collapsible',
 			props.shouldCollapse and 'collapsed' or nil,
 			props.classes
 		),
 		children = {
 			props.titleWidget or Div{
-				classes = Array.extend({'general-collapsible-default-header'}, props.titleClasses),
+				classes = Array.extend('general-collapsible-default-header', props.titleClasses),
 				children = {
-					B{children = {props.title}, classes = {'general-collapsible-default-title'}},
+					B{children = props.title, classes = {'general-collapsible-default-title'}},
 					CollapsibleToggle{css = {float = 'right'}},
 				}
 			},
 			Div{
 				children = props.children,
 				css = props.collapseAreaCss,
-				classes = Array.extend({},
+				classes = Array.extend(
 					'should-collapse',
 					props.collapseAreaClasses
 				),
@@ -64,4 +55,4 @@ function DefaultCollapsible:render()
 	}
 end
 
-return DefaultCollapsible
+return Component.component(DefaultCollapsible)
