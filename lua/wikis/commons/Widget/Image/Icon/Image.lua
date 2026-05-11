516ade0a074ec188a5db1a31681dfd47cd31e8b5
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2026-05-11 11:25:50 +0900

    image widget to widget3

--- lua/wikis/commons/Widget/Image/Icon/Image.lua
+++ lua/wikis/commons/Widget/Image/Icon/Image.lua
@@ -8,10 +8,9 @@
 local Lua = require('Module:Lua')
 
 local Array = Lua.import('Module:Array')
-local Class = Lua.import('Module:Class')
 local Logic = Lua.import('Module:Logic')
 
-local WidgetIcon = Lua.import('Module:Widget/Image/Icon')
+local Component = Lua.import('Module:Widget/Component')
 
 ---@class IconImageWidgetParameters
 ---@field imageLight string?
@@ -26,59 +25,58 @@ local WidgetIcon = Lua.import('Module:Widget/Image/Icon')
 ---@field verticalAlignment 'baseline'|'sub'|'super'|'top'|'text-top'|'middle'|'bottom'|'text-bottom'?
 ---@field caption string?
 
----@class IconImageWidget: IconWidget
----@operator call(IconImageWidgetParameters): IconImageWidget
----@field props IconImageWidgetParameters
-local Icon = Class.new(WidgetIcon)
+---@class IconImageWidget
+local Icon = {}
 Icon.defaultProps = {
 	link = '',
 	size = 'x20px',
-	classes = {},
 	verticalAlignment = 'middle', -- make the implicit mw default explicit
 }
 
+---@param props IconImageWidgetParameters
 ---@return string|string[]?
-function Icon:render()
-	local imageLight = self.props.imageLight
-	local imageDark = self.props.imageDark
+function Icon.render(props)
+	local imageLight = props.imageLight
+	local imageDark = props.imageDark
 	if Logic.isEmpty(imageLight) or Logic.isEmpty(imageDark) or imageLight == imageDark then
-		return self:_make(Logic.emptyOr(imageLight, imageDark))
+		return Icon._make(Logic.emptyOr(imageLight, imageDark))
 	end
 
 	return {
-		self:_make(imageLight, 'show-when-light-mode'),
-		self:_make(imageDark, 'show-when-dark-mode'),
+		Icon._make(props, imageLight, 'show-when-light-mode'),
+		Icon._make(props, imageDark, 'show-when-dark-mode'),
 	}
 end
 
+---@param props IconImageWidgetParameters
 ---@param image string?
 ---@param themeClass string?
 ---@return string?
 ---@overload fun(nil): nil
-function Icon:_make(image, themeClass)
+function Icon._make(props, image, themeClass)
 	if Logic.isEmpty(image) then
 		return
 	end
-	local classes = table.concat(Array.extend(self.props.classes, themeClass), ' ')
+	local classes = table.concat(Array.extend(props.classes, themeClass), ' ')
 
-	local border = Logic.nilIfEmpty(self.props.border)
-	assert((self.props.format == 'frameless' or not self.props.format) or not border,
+	local border = Logic.nilIfEmpty(props.border)
+	assert((props.format == 'frameless' or not props.format) or not border,
 		'border can only be used for frameless images')
 
 	local parts = Array.extend(
 		'File:' .. image,
 		border,
-		Logic.nilIfEmpty(self.props.format),
-		Logic.isNumeric(self.props.size) and (self.props.size .. 'px') or Logic.nilIfEmpty(self.props.size),
-		Logic.nilIfEmpty(self.props.horizontalAlignment),
-		self.props.verticalAlignment ~= 'middle' and self.props.verticalAlignment or nil,
-		'link=' .. self.props.link,
-		Logic.isNotEmpty(self.props.alt) and ('alt=' .. self.props.alt) or nil,
+		Logic.nilIfEmpty(props.format),
+		Logic.isNumeric(props.size) and (props.size .. 'px') or Logic.nilIfEmpty(props.size),
+		Logic.nilIfEmpty(props.horizontalAlignment),
+		props.verticalAlignment ~= 'middle' and props.verticalAlignment or nil,
+		'link=' .. props.link,
+		Logic.isNotEmpty(props.alt) and ('alt=' .. props.alt) or nil,
 		Logic.isNotEmpty(classes) and ('class=' .. classes) or nil,
-		Logic.nilIfEmpty(self.props.caption)
+		Logic.nilIfEmpty(props.caption)
 	)
 
 	return '[[' .. table.concat(parts, '|') .. ']]'
 end
 
-return Icon
+return Component.component(Icon.render, Icon.defaultProps)
