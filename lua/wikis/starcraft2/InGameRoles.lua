ab497fd116738c96f9447b389da4e26127b784b3
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2026-05-15 21:16:11 +0200

    Create InGameRoles.lua

---
+++ lua/wikis/starcraft2/InGameRoles.lua
@@ -0,0 +1,13 @@
+---
+-- @Liquipedia
+-- page=Module:InGameRoles
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+---@type table<string, RoleBaseData>
+local inGameRoles = {
+	['captain'] = {category = 'Captain', display = 'Captain', iconFa = 'captain'},
+}
+
+return inGameRoles
