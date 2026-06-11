cb220f705a1959ae013ce49c6857cf9f1528a272
Mehdi9120310391 <mehdielj@icloud.com>
2026-06-11 11:13:48 +0100

    Update PowerRankings.lua

--- lua/wikis/fortnite/PowerRankings.lua
+++ lua/wikis/fortnite/PowerRankings.lua
@@ -12,20 +12,46 @@ local Array = Lua.import('Module:Array')
 local DateExt = Lua.import('Module:Date/Ext')
 local Icon = Lua.import('Module:Icon')
 local Logic = Lua.import('Module:Logic')
+local Page = Lua.import('Module:Page')
 
 local Opponent = Lua.import('Module:Opponent/Custom')
 local OpponentDisplay = Lua.import('Module:OpponentDisplay/Custom')
-local PlayerDisplay = Lua.import('Module:Player/Display')
-local PlayerExt = Lua.import('Module:Player/Ext')
+local PlayerDisplay = Lua.import('Module:Player/Display/Custom')
+local PlayerExt = Lua.import('Module:Player/Ext/Custom')
 
-local TableWidgets = Lua.import('Module:Widget/Table2/All')
 local HtmlWidgets = Lua.import('Module:Widget/Html/All')
-local WidgetUtil = Lua.import('Module:Widget/Util')
 local Link = Lua.import('Module:Widget/Basic/Link')
+local TableWidgets = Lua.import('Module:Widget/Table2/All')
+local WidgetUtil = Lua.import('Module:Widget/Util')
+
+local Condition = Lua.import('Module:Condition')
+local ConditionTree = Condition.Tree
+local ConditionNode = Condition.Node
+local Comparator = Condition.Comparator
+local BooleanOperator = Condition.BooleanOperator
+local ColumnName = Condition.ColumnName
 
 local PowerRankingsData = Lua.import('Module:PowerRankings/Data', {loadData = true})
 
-local p = {}
+local PowerRankings = {}
+
+---@param name string
+---@return string?
+local function resolvePrimaryTeam(name)
+	if Logic.isEmpty(name) then
+		return nil
+	end
+	local conditions = ConditionTree(BooleanOperator.any):add{
+		ConditionNode(ColumnName('pagename'), Comparator.eq, Page.pageifyLink(name)),
+		ConditionNode(ColumnName('id'), Comparator.eq, name),
+	}
+	local row = mw.ext.LiquipediaDB.lpdb('player', {
+		query = 'team',
+		conditions = conditions:toString(),
+		limit = 1,
+	})[1] or {}
+	return Logic.nilIfEmpty(row.team) or Logic.nilIfEmpty(PlayerExt.syncTeam(Page.pageifyLink(name)))
+end
 
 ---@param updated string?
 ---@return Renderable
@@ -64,7 +90,7 @@ end
 
 ---@param frame Frame
 ---@return VNode
-function p.main(frame)
+function PowerRankings.main(frame)
 	local args = Arguments.getArgs(frame)
 	local limit = tonumber(args.limit)
 	local showMore = Logic.readBool(args.showMore)
@@ -85,7 +111,7 @@ function p.main(frame)
 			pageName = Logic.nilIfEmpty(entry.link) or entry.name,
 		}
 		PlayerExt.syncPlayer(player)
-		local teamTemplate = PlayerExt.syncTeam(player.pageName)
+		local teamTemplate = resolvePrimaryTeam(entry.link or entry.name)
 
 		return TableWidgets.Row{children = {
 			TableWidgets.Cell{children = HtmlWidgets.B{children = entry.rank}},
@@ -123,4 +149,4 @@ function p.main(frame)
 	}
 end
 
-return p
+return PowerRankings
