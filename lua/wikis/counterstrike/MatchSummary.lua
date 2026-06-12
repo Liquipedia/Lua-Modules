24b16b8535a0e8a869c10d8fc0e2fa4731b92969
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2026-06-12 11:07:48 +0900

    remove unused imports

--- lua/wikis/counterstrike/MatchSummary.lua
+++ lua/wikis/counterstrike/MatchSummary.lua
@@ -8,7 +8,6 @@
 local Lua = require('Module:Lua')
 
 local Array = Lua.import('Module:Array')
-local Class = Lua.import('Module:Class')
 local Logic = Lua.import('Module:Logic')
 local String = Lua.import('Module:StringUtils')
 local Table = Lua.import('Module:Table')
