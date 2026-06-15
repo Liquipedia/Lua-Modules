b143d70a0900dc0ebe3720da2a898ccf3bd4a437
PolandBot <133791350+PolandBot@users.noreply.github.com>
2026-06-15 19:28:55 +0500

    feat(match2): fix MapVeto in brawlstars

--- lua/wikis/brawlstars/MatchSummary.lua
+++ lua/wikis/brawlstars/MatchSummary.lua
@@ -39,7 +39,7 @@ function CustomMatchSummary.createBody(match)
 	return WidgetUtil.collect(
 		Array.map(match.games, CustomMatchSummary._createMapRow),
 		MatchSummaryWidgets.Mvp(match.extradata.mvp),
-		MatchSummaryWidgets.MapVeto(MatchSummary.preProcessMapVeto(match.extradata.mapveto, {game = match.game})),
+		MatchSummaryWidgets.MapVeto(MatchSummary.preProcessMapVeto(match.extradata.mapveto, {emptyMapDisplay = NONE})),
 		MatchSummaryWidgets.CharacterBanTable{bans = characterBansData, date = match.date}
 	)
 end
