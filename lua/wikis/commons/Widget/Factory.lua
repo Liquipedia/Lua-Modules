11176d18833b0124b02517eb0bf6ed64c11af385
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2026-06-17 10:04:05 +0900

    coerce numeric types

--- lua/wikis/commons/Widget/Factory.lua
+++ lua/wikis/commons/Widget/Factory.lua
@@ -19,8 +19,12 @@ function WidgetFactory.fromTemplate(args)
 	local widgetClass = Table.extract(args, 'widget')
 	assert(String.isNotEmpty(widgetClass), 'WidgetFactory: widget must be specified')
 	local WidgetClass = Lua.import('Module:Widget/' .. widgetClass)
-	local copiedArgs = Table.copy(args)
-	copiedArgs.children = type(copiedArgs.children) == 'table' and copiedArgs.children or {copiedArgs.children}
+	local copiedArgs = Table.mapValues(args, function (arg)
+		if type(arg) == 'table' then
+			return arg
+		end
+		return tonumber(arg) or arg
+	end)
 	return WidgetClass(copiedArgs)
 end
 
