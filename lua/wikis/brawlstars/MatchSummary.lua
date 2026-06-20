830e26a9ec7d25ea0683eb9e6128ddc062f3f731
Knoxider <knoxi9192@gmail.com>
2026-06-20 13:42:03 +0500

    Apply suggestion from @hjpalpha
    Co-authored-by: hjpalpha <75081997+hjpalpha@users.noreply.github.com>

--- lua/wikis/brawlstars/MatchSummary.lua
+++ lua/wikis/brawlstars/MatchSummary.lua
@@ -21,7 +21,7 @@ local WidgetUtil = Lua.import('Module:Widget/Util')
 local CustomMatchSummary = {}
 
 ---@param args table
----@return Html
+---@return Renderable
 function CustomMatchSummary.getByMatchId(args)
 	return MatchSummary.defaultGetByMatchId(CustomMatchSummary, args, {width = '400px', teamStyle = 'bracket'})
 end
