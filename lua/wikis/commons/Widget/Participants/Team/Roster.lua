4140e1247210086743aea38d98dd9c903d18f9ef
hjpalpha <andreasbuesch1@gmail.com>
2026-06-22 11:42:51 +0200

    as per review

--- lua/wikis/commons/Widget/Participants/Team/Roster.lua
+++ lua/wikis/commons/Widget/Participants/Team/Roster.lua
@@ -54,8 +54,7 @@ end
 --   * If there's a non-ingame role assigned, display it after the status
 --   * Returns an array of labels to display (can be multiple)
 -- * Left-role:
---   * If the first role has an icon, we use that to render the left-role
---   * If not then we instead display the icon of the first ingame role with icon
+--   * Display the icon of the first ingame role with icon
 ---@param player table
 ---@return string?, string[]?
 local function getRoleDisplays(player)
