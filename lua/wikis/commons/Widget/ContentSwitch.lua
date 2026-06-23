7c7c0751d56b04a5816a0675b113598064e04e1a
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2026-06-23 12:45:33 +0900

    slice content switch pill

--- lua/wikis/commons/Widget/ContentSwitch.lua
+++ lua/wikis/commons/Widget/ContentSwitch.lua
@@ -8,10 +8,10 @@
 local Lua = require('Module:Lua')
 
 local Array = Lua.import('Module:Array')
-local Logic = Lua.import('Module:Logic')
 
 local Component = Lua.import('Module:Widget/Component')
 local Html = Lua.import('Module:Widget/Html')
+local SwitchPill = Lua.import('Module:Widget/ContentSwitch/Pill')
 local Div = Html.Div
 
 ---@class ContentSwitchTab
@@ -19,15 +19,8 @@ local Div = Html.Div
 ---@field value? string
 ---@field content? Renderable|Renderable[]
 
----@class ContentSwitchParameters
----@field tabs ContentSwitchTab[]
----@field variant? 'themed'|'generic'
----@field defaultActive? integer
----@field switchGroup string
+---@class ContentSwitchProps: ContentSwitchPillProps
 ---@field classes? string[]
----@field size? 'extrasmall'|'small'|'medium'
----@field storeValue? boolean
----@field css? table<string, string|number?>
 
 local defaultProps = {
 	variant = 'generic',
@@ -36,34 +29,15 @@ local defaultProps = {
 	storeValue = true,
 }
 
----@param props ContentSwitchParameters
+---@param props ContentSwitchProps
 ---@return Renderable|Renderable[]
 local function ContentSwitch(props)
 	local tabs = assert(props.tabs, 'ContentSwitch requires at least the tabs property to be set')
-	local variant = props.variant
 	local defaultActive = props.defaultActive
-	local switchGroup = assert(Logic.nilIfEmpty(props.switchGroup), 'ContentSwitch: missing \'switchGroup\' property')
 
 	if #tabs < 2 then
 		return (tabs[1] or {}).content
 	end
-
-	local tabOptions = Array.map(tabs, function(tab, index)
-		local isActive = index == defaultActive
-		local classes = {'switch-pill-option', 'toggle-area-button'}
-		if isActive then
-			table.insert(classes, 'switch-pill-option-active')
-		end
-
-		return Div{
-			classes = classes,
-			attributes = {
-				['data-toggle-area-btn'] = tostring(index),
-				['data-switch-value'] = tab.value or tostring(index),
-			},
-			children = Logic.emptyOr(tab.label, tostring(index)),
-		}
-	end)
 
 	local contentAreas = Array.map(tabs, function(tab, index)
 		local isActive = index == defaultActive
@@ -76,35 +50,11 @@ local function ContentSwitch(props)
 		}
 	end)
 
-	local switchPillClasses = {'switch-pill'}
-	if variant == 'generic' then
-		table.insert(switchPillClasses, 'switch-pill-generic')
-	end
-	if props.size == 'small' then
-		table.insert(switchPillClasses, 'switch-pill-small')
-	elseif props.size == 'extrasmall' then
-		table.insert(switchPillClasses, 'switch-pill-extrasmall')
-	end
-
-
 	return Div{
 		classes = {'toggle-area', 'toggle-area-' .. tostring(defaultActive)},
 		attributes = {['data-toggle-area'] = tostring(defaultActive)},
 		children = {
-			Div{
-				classes = {'switch-pill-container'},
-				css = props.css,
-				children = {
-					Div{
-						classes = switchPillClasses,
-						attributes = {
-							['data-switch-group'] = switchGroup,
-							['data-store-value'] = Logic.readBool(props.storeValue) and 'true' or nil,
-						},
-						children = tabOptions,
-					},
-				},
-			},
+			SwitchPill(props),
 			Div{
 				classes = {'content-switch-content-container'},
 				children = contentAreas,
---
+++ lua/wikis/commons/Widget/ContentSwitch/Pill.lua
@@ -0,0 +1,77 @@
+---
+-- @Liquipedia
+-- page=Module:Widget/ContentSwitch/Pill
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Lua = require('Module:Lua')
+
+local Array = Lua.import('Module:Array')
+local Logic = Lua.import('Module:Logic')
+
+local Component = Lua.import('Module:Widget/Component')
+local Html = Lua.import('Module:Widget/Html')
+local Div = Html.Div
+
+---@class ContentSwitchPillProps
+---@field tabs ContentSwitchTab[]
+---@field variant? 'themed'|'generic'
+---@field defaultActive? integer
+---@field switchGroup string
+---@field size? 'extrasmall'|'small'|'medium'
+---@field storeValue? boolean
+---@field css? table<string, string|number?>
+
+---@param props ContentSwitchPillProps
+---@return Renderable|Renderable[]
+local function ContentSwitchPill(props)
+	local tabs = assert(props.tabs, 'ContentSwitch requires at least the tabs property to be set')
+	local variant = props.variant
+	local defaultActive = props.defaultActive
+	local switchGroup = assert(Logic.nilIfEmpty(props.switchGroup), 'ContentSwitch: missing \'switchGroup\' property')
+
+	local tabOptions = Array.map(tabs, function(tab, index)
+		local isActive = index == defaultActive
+		local classes = {'switch-pill-option', 'toggle-area-button'}
+		if isActive then
+			table.insert(classes, 'switch-pill-option-active')
+		end
+
+		return Div{
+			classes = classes,
+			attributes = {
+				['data-toggle-area-btn'] = tostring(index),
+				['data-switch-value'] = tab.value or tostring(index),
+			},
+			children = Logic.emptyOr(tab.label, tostring(index)),
+		}
+	end)
+
+	local switchPillClasses = {'switch-pill'}
+	if variant == 'generic' then
+		table.insert(switchPillClasses, 'switch-pill-generic')
+	end
+	if props.size == 'small' then
+		table.insert(switchPillClasses, 'switch-pill-small')
+	elseif props.size == 'extrasmall' then
+		table.insert(switchPillClasses, 'switch-pill-extrasmall')
+	end
+
+	return Div{
+		classes = {'switch-pill-container'},
+		css = props.css,
+		children = {
+			Div{
+				classes = switchPillClasses,
+				attributes = {
+					['data-switch-group'] = switchGroup,
+					['data-store-value'] = Logic.readBool(props.storeValue) and 'true' or nil,
+				},
+				children = tabOptions,
+			},
+		},
+	}
+end
+
+return Component.component(ContentSwitchPill)
