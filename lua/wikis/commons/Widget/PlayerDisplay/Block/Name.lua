6fd660bcf805362004a86e9bdbb7bb0e4d577f96
Eetu Rantanen <eeturantanen.dev@gmail.com>
2026-06-24 11:10:42 +0300

    mark player links for hover preview

--- lua/wikis/commons/Widget/PlayerDisplay/Block/Name.lua
+++ lua/wikis/commons/Widget/PlayerDisplay/Block/Name.lua
@@ -11,6 +11,8 @@ local DisplayUtil = Lua.import('Module:DisplayUtil')
 local Logic = Lua.import('Module:Logic')
 local Opponent = Lua.import('Module:Opponent/Custom')
 
+local PagePreview = Lua.import('Module:PagePreview')
+
 local Component = Lua.import('Module:Widget/Component')
 local Html = Lua.import('Module:Widget/Html')
 local Link = Lua.import('Module:Widget/Basic/Link')
@@ -22,10 +24,12 @@ local ZERO_WIDTH_SPACE = '&#8203;'
 ---@return VNode?
 local function BlockPlayerName(props)
 	local player = props.player
+	local isLinked = not Opponent.playerIsTbd(player) and Logic.readBool(props.showLink)
+		and Logic.isNotEmpty(player.pageName)
 
 	---@return Renderable
 	local function getChildren()
-		if not Opponent.playerIsTbd(player) and Logic.readBool(props.showLink) and Logic.isNotEmpty(player.pageName) then
+		if isLinked then
 			return Link{link = player.pageName, children = player.displayName}
 		elseif props.useDefault then
 			return Logic.emptyOr(player.displayName, 'TBD') --[[@as string]]
@@ -33,8 +37,13 @@ local function BlockPlayerName(props)
 		return ZERO_WIDTH_SPACE
 	end
 
+	if isLinked then
+		PagePreview.register(player.pageName)
+	end
+
 	return (props.dq and Html.S or Html.Span){
-		classes = {'name'},
+		classes = {'name', isLinked and 'link-preview' or nil},
+		attributes = isLinked and {['data-preview-page'] = PagePreview.key(player.pageName)} or nil,
 		css = DisplayUtil.getOverflowStyles(props.overflow or 'ellipsis'),
 		children = getChildren(),
 	}
--- lua/wikis/commons/Widget/PlayerDisplay/Inline/Name.lua
+++ lua/wikis/commons/Widget/PlayerDisplay/Inline/Name.lua
@@ -8,6 +8,7 @@
 local Lua = require('Module:Lua')
 
 local Logic = Lua.import('Module:Logic')
+local PagePreview = Lua.import('Module:PagePreview')
 
 local Component = Lua.import('Module:Widget/Component')
 local Html = Lua.import('Module:Widget/Html')
@@ -17,10 +18,18 @@ local Link = Lua.import('Module:Widget/Basic/Link')
 ---@return Renderable?
 local function InlinePlayerName(props)
 	local player = props.player
-	local showLink = Logic.readBool(props.showLink)
-	local name = (showLink and Logic.isNotEmpty(player.pageName)) and Link{
-		link = player.pageName, children = player.displayName
-	} or player.displayName
+	local isLinked = Logic.readBool(props.showLink) and Logic.isNotEmpty(player.pageName)
+	local name = player.displayName
+	if isLinked then
+		PagePreview.register(player.pageName)
+		-- the marker sits on a wrapper span rather than the link itself, since
+		-- Link emits raw `[[...]]` wikitext that carries no attributes
+		name = Html.Span{
+			classes = {'link-preview'},
+			attributes = {['data-preview-page'] = PagePreview.key(player.pageName)},
+			children = Link{link = player.pageName, children = player.displayName},
+		}
+	end
 	if props.dq then
 		return Html.S{children = name}
 	end
