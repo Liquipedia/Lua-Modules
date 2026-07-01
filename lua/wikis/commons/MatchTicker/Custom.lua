692a511a3e4fd4096df60c06627410c5ccc4a959
Rikard Blixt <rikardblixt@gmail.com>
2026-07-01 10:23:45 +0200

    refactor: cleanup match ticker step 1 (#7692)
    * refactor: cleanup match ticker step 1
    
    * adress comments
    
    * change to collect

--- lua/wikis/commons/MatchTicker/Custom.lua
+++ lua/wikis/commons/MatchTicker/Custom.lua
@@ -12,23 +12,15 @@ local Logic = Lua.import('Module:Logic')
 local Table = Lua.import('Module:Table')
 
 local MatchTicker = Lua.import('Module:MatchTicker')
+local InfoboxHeader = Lua.import('Module:Widget/Infobox/Header')
 
 local CustomMatchTicker = {}
 
 ---Entry point for display on the main page
 ---@param frame Frame|table|nil
----@return Html
+---@return Renderable?
 function CustomMatchTicker.mainPage(frame)
 	local args = Arguments.getArgs(frame)
-	return MatchTicker(args):query():create()
-end
-
----Entry point for display on the main page with the new style
----@param frame Frame|table|nil
----@return Html?
-function CustomMatchTicker.newMainPage(frame)
-	local args = Arguments.getArgs(frame)
-	args.newStyle = true
 
 	args.tiers = args['filterbuttons-liquipediatier']
 	if args.tiers == 'curated' then
@@ -41,16 +33,16 @@ function CustomMatchTicker.newMainPage(frame)
 	args.games = args['filterbuttons-game']
 
 	if args.type == 'upcoming' then
-		return MatchTicker(Table.merge(args, {ongoing = true, upcoming = true})):query():create():addClass('new-match-style')
+		return MatchTicker(Table.merge(args, {ongoing = true, upcoming = true})):query():create()
 	elseif args.type == 'recent' then
-		return MatchTicker(Table.merge(args, {recent = true})):query():create():addClass('new-match-style')
+		return MatchTicker(Table.merge(args, {recent = true})):query():create()
 	end
 end
 
 ---Entry point for display on player pages
 ---Upcoming and ongoing matches are now automatically displayed via the entity match ticker
 ---@param frame Frame|table|nil
----@return Html
+---@return Renderable?
 function CustomMatchTicker.player(frame)
 	local args = Arguments.getArgs(frame)
 	args.player = args.player or mw.title.getCurrentTitle().text
@@ -60,7 +52,7 @@ end
 ---Displays recent matches for a player or team.
 ---@param args table
 ---@param matches {recent: table?}?
----@return Html
+---@return Renderable?
 function CustomMatchTicker.recent(args, matches)
 	matches = matches or {}
 
@@ -70,16 +62,8 @@ function CustomMatchTicker.recent(args, matches)
 	args.limit = args.limit or args.recentLimit or 5
 
 	return MatchTicker(args):query(matches.recent):create(
-		MatchTicker.DisplayComponents.Header('Recent Matches')
+		InfoboxHeader{name = 'Recent Matches', displayButtons = false}
 	)
-end
-
----@deprecated Use CustomMatchTicker.recent() instead. This function only displays recent matches.
----@param args table
----@param matches {recent: table?}?
----@return Html
-function CustomMatchTicker.participant(args, matches)
-	return CustomMatchTicker.recent(args, matches)
 end
 
 return CustomMatchTicker
