796f6642e152fbe5259a32d8f874d4de7be60fc1
Rikard Blixt <rikardblixt@gmail.com>
2026-07-09 14:38:36 +0200

    refactor: cleanup match ticker step 3 (#7694)
    * refactor: cleanup match ticker step 3
    
    * fix adjustMatch
    
    * revert to how it was before, with anno improvement
    
    * adjust
    
    * fix

--- lua/wikis/commons/Widget/Match/Ticker/Wrapper.lua
+++ lua/wikis/commons/Widget/Match/Ticker/Wrapper.lua
@@ -9,13 +9,13 @@ local Lua = require('Module:Lua')
 
 local Array = Lua.import('Module:Array')
 local Component = Lua.import('Module:Widget/Component')
-local MatchGroupUtil = Lua.import('Module:MatchGroup/Util/Custom')
 
 local Html = Lua.import('Module:Widget/Html')
 local MatchCard = Lua.import('Module:Widget/Match/Card')
 local HorizontalContainer = Lua.import('Module:Widget/Match/Ticker/HorizontalContainer')
 
----@param props {matches: table[], header: Renderable?, showInfoForEmptyResults: boolean, wrapperClasses: string[],
+---@param props {matches: {match: MatchGroupUtilMatch, gameData: MatchTickerGameData?}[],
+---header: Renderable?, showInfoForEmptyResults: boolean, wrapperClasses: string[],
 ---hideTournament: boolean, displayGameIcons: boolean, onlyHighlightOnValue: string?, variant: 'vertical'|'horizontal'}
 ---@return Renderable?
 local function MatchTickerWrapper(props)
@@ -36,17 +36,12 @@ local function MatchTickerWrapper(props)
 
 	local matchCards = Array.map(matches, function(match)
 		return MatchCard{
-			match = MatchGroupUtil.matchFromRecord(match),
+			match = match.match,
 			hideTournament = props.hideTournament,
 			displayGameIcons = props.displayGameIcons,
 			onlyHighlightOnValue = props.onlyHighlightOnValue,
 			variant = props.variant == 'horizontal' and 'vertical' or nil,
-			gameData = {
-				asGame = match.asGame,
-				gameIds = match.asGameIndexes,
-				map = match.map,
-				mapDisplayName = match.extradata and match.extradata.displayname or nil
-			}
+			gameData = match.gameData
 		}
 	end)
 
