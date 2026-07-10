b3c0c8625ffd7bade92581df288ef383ba7fbbd3
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2026-07-10 11:29:33 +0900

    rename methods

--- scripts/proxy_lp.py
+++ scripts/proxy_lp.py
@@ -11,11 +11,11 @@ class LiquipediaMapper:
             "only=styles" in flow.request.pretty_url
             and "skin=lakesideview" not in flow.request.pretty_url
         ):
-            self.__build_css_resource(flow)
+            self.__serve_local_css_resource(flow)
         elif "only=scripts" in flow.request.pretty_url:
-            self.__build_js_resource(flow)
+            self.__serve_local_js_resource(flow)
 
-    def __build_css_resource(self, flow: http.HTTPFlow):
+    def __serve_local_css_resource(self, flow: http.HTTPFlow):
         with open("lua/output/css/main.css", "rb") as f:
             flow.response = http.Response.make(
                 HTTPStatus.OK,
@@ -23,7 +23,7 @@ class LiquipediaMapper:
                 {"Content-Type": "text/css; charset=utf-8", "Via": "LiquipediaMapper"},
             )
 
-    def __build_js_resource(self, flow: http.HTTPFlow):
+    def __serve_local_js_resource(self, flow: http.HTTPFlow):
         with open("lua/output/js/main.js", "rb") as f:
             flow.response = http.Response.make(
                 HTTPStatus.OK,
