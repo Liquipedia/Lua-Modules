93271feddf53fd2fece9812723b6693824fe728e
Hesketh2 <88981446+Hesketh2@users.noreply.github.com>
2026-07-12 00:01:26 +0700

    Rework to include character icon roles
    read comments for more info

--- lua/wikis/deltaforce/Infobox/Character/Custom.lua
+++ lua/wikis/deltaforce/Infobox/Character/Custom.lua
@@ -9,10 +9,12 @@ local Lua = require('Module:Lua')
 
 local Class = Lua.import('Module:Class')
 
-local Injector = Lua.import('Module:Widget/Injector')
 local Character = Lua.import('Module:Infobox/Character')
+local Injector = Lua.import('Module:Widget/Injector')
+local PositionIcon = Lua.import('Module:OperatorsPositionIcon', {loadData = true})
 
 local Widgets = Lua.import('Module:Widget/All')
+local Cell = Widgets.Cell
 
 ---@class DeltaforceCharacterInfobox: CharacterInfobox
 local CustomCharacter = Class.new(Character)
@@ -21,7 +23,7 @@ local CustomCharacter = Class.new(Character)
 local CustomInjector = Class.new(Injector)
 
 ---@param frame Frame
----@return VNode
+---@return Html
 function CustomCharacter.run(frame)
 	local character = CustomCharacter(frame)
 	character:setWidgetInjector(CustomInjector(character))
@@ -29,11 +31,37 @@ function CustomCharacter.run(frame)
 	return character:createInfobox()
 end
 
+---@param id string
+---@param widgets Widget[]
+---@return Widget[]
+function CustomInjector:parse(id, widgets)
+	if id == 'role' then
+		return {
+			Cell{
+				name = 'Position',
+				children = {self:_toCellContent('position')}
+			},
+		}
+	end
+	return widgets
+end
+
+---@param key string
+---@return string?
+function CustomInjector:_toCellContent(key)
+	local args = self.caller.args
+	if not args[key] or args[key] == '' then return end
+	local iconData = PositionIcon[args[key]:lower()]
+	if not iconData then return args[key] end
+	
+	return '[[File:' .. iconData.icon .. '|15px|link=' .. (iconData.link or '') .. ']] ' .. iconData.displayName
+end
+
 ---@param lpdbData table
 ---@param args table
 ---@return table
 function CustomCharacter:addToLpdb(lpdbData, args)
-	lpdbData.extradata.class = args.class
+	lpdbData.extradata.position = args.position
 	return lpdbData
 end
 
