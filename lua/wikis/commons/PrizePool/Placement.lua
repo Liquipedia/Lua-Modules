d79aafa5308204a3cedc95772f9da4d1ffbc89b6
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2026-07-16 13:18:54 +0200

    kick unused import

--- lua/wikis/commons/PrizePool/Placement.lua
+++ lua/wikis/commons/PrizePool/Placement.lua
@@ -7,7 +7,6 @@
 
 local Lua = require('Module:Lua')
 
-local Abbreviation = Lua.import('Module:Abbreviation')
 local Array = Lua.import('Module:Array')
 local Class = Lua.import('Module:Class')
 local Logic = Lua.import('Module:Logic')
