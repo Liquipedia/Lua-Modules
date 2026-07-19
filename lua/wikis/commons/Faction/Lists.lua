f3446a16734c1d8f5da4202596a5e24412596625
SyntacticSalt <syntactic@mbergen.de>
2026-07-19 13:54:35 +0200

    Update Lists.lua

--- lua/wikis/commons/Faction/Lists.lua
+++ lua/wikis/commons/Faction/Lists.lua
@@ -20,7 +20,7 @@ local Box = Lua.import('Module:Widget/Basic/Box')
 
 local FactionLists = Class.new(Widget)
 
----@return Box
+---@return Renderable
 function FactionLists:render()
 	return Box{
 		children = Array.map(Game.listGames{ordered=true}, FactionLists._getTable)
