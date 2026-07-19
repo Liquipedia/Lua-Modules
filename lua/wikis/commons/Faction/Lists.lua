15bb63057b7ae540e1cc0f6b87cd7de8f5bbbf59
SyntacticSalt <syntactic@mbergen.de>
2026-07-19 15:41:13 +0200

    Remove unused import

--- lua/wikis/commons/Faction/Lists.lua
+++ lua/wikis/commons/Faction/Lists.lua
@@ -8,7 +8,6 @@
 local Lua = require('Module:Lua')
 
 local Array = Lua.import('Module:Array')
-local Class = Lua.import('Module:Class')
 local Faction = Lua.import('Module:Faction')
 local FnUtil = Lua.import('Module:FnUtil')
 local Game = Lua.import('Module:Game')
