2f97507cbb3e8aa46641b4610e9852285caf30df
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2026-07-20 12:06:32 +0900

    add cmd line argparser

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,3 +1,4 @@
+import argparse
 import itertools
 import os
 import pathlib
@@ -5,7 +6,7 @@ import re
 import subprocess
 import sys
 
-from typing import Iterable
+from typing import Iterable, Optional
 
 from dotenv import load_dotenv
 
@@ -24,7 +25,10 @@ HEADER_PATTERN = re.compile(
 
 
 def deploy_all_files_for_wiki(
-    wiki: str, file_paths: Iterable[pathlib.Path], deploy_reason: str
+    wiki: str,
+    file_paths: Iterable[pathlib.Path],
+    deploy_reason: str,
+    dev_environment: Optional[str],
 ) -> bool:
     all_modules_deployed = True
     with MediaWikiSession(wiki) as session:
@@ -36,9 +40,7 @@ def deploy_all_files_for_wiki(
                 print("...skipping - no magic comment found")
                 write_to_github_summary_file(f"{str(file_path)} skipped")
             else:
-                page = header_match.groupdict()["pageName"] + (
-                    os.getenv("LUA_DEV_ENV_NAME") or ""
-                )
+                page = header_match.groupdict()["pageName"] + (dev_environment or "")
                 module_deployed, _ = session.deploy_file(
                     file_path, file_content, page, deploy_reason
                 )
@@ -47,19 +49,62 @@ def deploy_all_files_for_wiki(
     return all_modules_deployed
 
 
+def get_dev_environment(dev_environment: Optional[str]) -> Optional[str]:
+    if dev_environment is None:
+        return None
+    elif dev_environment == "/dev/" or not dev_environment.startswith("/dev/"):
+        raise ValueError(f"Invalid dev environment name: {dev_environment}")
+    return dev_environment
+
+
+def build_argument_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-A",
+        "--deploy-all",
+        action="store_true",
+        help="Whether to deploy all files to wiki",
+    )
+    parser.add_argument(
+        "lua_files", nargs="*", type=pathlib.Path, help="List of lua files to deploy"
+    )
+    dev_arg_group = parser.add_argument_group(
+        "Dev Deploy", description="Deploy options for dev environments"
+    )
+    dev_arg_group.add_argument(
+        "--base-ref",
+        type=str,
+        default=os.getenv("LUA_DEV_BASE_REF") or "main",
+        help='Base reference to check against for finding changed files (default: LUA_DEV_BASE_REF environment variable if defined, otherwise "main")',
+    )
+    dev_arg_group.add_argument(
+        "--dev-environment",
+        type=get_dev_environment,
+        default=os.getenv("LUA_DEV_ENV_NAME"),
+        help="Name of the dev environment to deploy to (default: LUA_DEV_ENV_NAME environment variable)",
+    )
+    return parser
+
+
 def main():
     all_modules_deployed = True
     lua_files: Iterable[pathlib.Path]
     git_deploy_reason: str
-    assert os.getenv("LUA_DEV_ENV_NAME") is None or (
-        os.getenv("LUA_DEV_ENV_NAME") != "/dev/"
-        and os.getenv("LUA_DEV_ENV_NAME").startswith("/dev/")
-    ), f"Invalid dev environment: {os.getenv('LUA_DEV_ENV_NAME')}"
-    if len(sys.argv[1:]) == 0 and os.getenv("LUA_DEV_ENV_NAME") is None:
+
+    parser = build_argument_parser()
+    parsed_args = parser.parse_args()
+
+    dev_environment: Optional[str] = parsed_args.dev_environment
+
+    if parsed_args.deploy_all:
+        if dev_environment is not None:
+            parser.error("Target dev environment must not be specified in re-sync mode")
         lua_files = pathlib.Path("./lua/wikis/").rglob("*.lua")
         git_deploy_reason = "Automated Weekly Re-Sync"
     else:
-        if len(sys.argv[1:]) == 0:
+        if len(parsed_args.lua_files) == 0:
+            if dev_environment is None:
+                parser.error("Target dev environment is not specified")
             lua_files = [
                 pathlib.Path(changed_file)
                 for changed_file in subprocess.check_output(
@@ -68,7 +113,7 @@ def main():
                         "diff",
                         "--name-only",
                         "--diff-filter=d",
-                        os.getenv("LUA_DEV_BASE_REF") or "main",
+                        parsed_args.base_ref,
                         "lua/wikis/*",
                     ]
                 )
@@ -77,12 +122,12 @@ def main():
                 .splitlines()
             ]
         else:
-            lua_files = [pathlib.Path(arg) for arg in sys.argv[1:]]
+            lua_files = parsed_args.lua_files
         git_deploy_reason = get_git_deploy_reason()
 
     for wiki, files in itertools.groupby(sorted(lua_files), lambda path: path.parts[2]):
         all_modules_deployed &= deploy_all_files_for_wiki(
-            wiki, list(files), git_deploy_reason
+            wiki, list(files), git_deploy_reason, dev_environment
         )
 
     if not all_modules_deployed:
