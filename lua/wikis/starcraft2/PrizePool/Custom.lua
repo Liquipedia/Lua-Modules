ce0a8f7661e03cc762b7948d4dfeba58db336214
hjpalpha <andreasbuesch1@gmail.com>
2026-08-02 13:30:30 +0200

    as per review

--- lua/wikis/starcraft2/PrizePool/Custom.lua
+++ lua/wikis/starcraft2/PrizePool/Custom.lua
@@ -7,4 +7,124 @@
 
 local Lua = require('Module:Lua')
 
-return Lua.import('Module:PrizePool/Starcraft')
+local Arguments = Lua.import('Module:Arguments')
+local Array = Lua.import('Module:Array')
+local Class = Lua.import('Module:Class')
+local Logic = Lua.import('Module:Logic')
+local LpdbInjector = Lua.import('Module:Lpdb/Injector')
+local Namespace = Lua.import('Module:Namespace')
+local Opponent = Lua.import('Module:Opponent/Custom')
+local PrizePool = Lua.import('Module:PrizePool')
+local Table = Lua.import('Module:Table')
+local Variables = Lua.import('Module:Variables')
+
+---@class Starcraft2PrizePoolLpdbInjector: LpdbInjector
+local CustomLpdbInjector = Class.new(LpdbInjector)
+
+local TIER_TO_FACTOR = {
+	8,
+	4,
+	2,
+}
+local DEFAULT_PRIZE_VALUE = 0.0001
+
+local CustomPrizePool = {}
+
+-- Template entry point
+---@param frame Frame
+---@return Widget
+function CustomPrizePool.run(frame)
+	local args = Arguments.getArgs(frame)
+
+	-- set some default values
+	args.prizesummary = Logic.emptyOr(args.prizesummary, false)
+	args.exchangeinfo = Logic.emptyOr(args.exchangeinfo, false)
+	args.syncPlayers = Logic.emptyOr(args.syncPlayers, true)
+	args.placementsExtendImportLimit = Logic.emptyOr(args.placementsExtendImportLimit, true)
+
+	-- adjust import settings params
+	args.importLimit = tonumber(args.importLimit) or CustomPrizePool._defaultImportLimit()
+	args.allGroupsUseWdl = Logic.emptyOr(args.allGroupsUseWdl, true)
+	args.import = Logic.emptyOr(args.import, true)
+
+	-- fixed setting
+	args.resolveRedirect = true
+	args.groupScoreDelimiter = '-'
+
+	return PrizePool(args)
+		:setConfigDefault('storeLpdb', Namespace.isMain())
+		:create()
+		:setLpdbInjector(CustomLpdbInjector())
+		:build()
+end
+
+---@param lpdbData placement
+---@param placement PrizePoolPlacement
+---@param opponent BasePlacementOpponent
+---@return placement
+function CustomLpdbInjector:adjust(lpdbData, placement, opponent)
+	lpdbData.weight = CustomPrizePool._weight(lpdbData)
+
+	lpdbData.extradata = Table.mergeInto(lpdbData.extradata, {
+		seriesnumber = CustomPrizePool._seriesNumber(),
+		mod = Variables.varDefault('tournament_mod'),
+	})
+
+	lpdbData.objectName = CustomPrizePool._overwriteObjectName(lpdbData)
+
+	return lpdbData
+end
+
+---@param lpdbData placement
+---@return string
+function CustomPrizePool._overwriteObjectName(lpdbData)
+	if lpdbData.opponenttype == Opponent.team then
+		local prizePoolIndex = tonumber(Variables.varDefault('prizepool_index')) or 0
+		return lpdbData.objectName .. '_' .. prizePoolIndex
+	end
+
+	return lpdbData.objectName
+end
+
+---@return integer?
+function CustomPrizePool._defaultImportLimit()
+	local tier = tonumber(Variables.varDefault('tournament_liquipediatier'))
+	assert(tier, 'Prize Pool Import: Unset/Invalid liquipediatier')
+
+	return tier >= 4 and 8
+		or tier == 3 and 16
+		or nil
+end
+
+---@return string
+function CustomPrizePool._seriesNumber()
+	local seriesNumber = tonumber(Variables.varDefault('tournament_series_number'))
+	return seriesNumber and string.format('%05d', seriesNumber) or ''
+end
+
+---@param lpdbData placement
+---@return number
+function CustomPrizePool._weight(lpdbData)
+	local place = string.lower(lpdbData.placement or '')
+	if Logic.isEmpty(place) or place == 'l' or place == 'dq' then
+		return 0
+	end
+
+	local tierFactor = TIER_TO_FACTOR[tonumber(lpdbData.liquipediatier)] or 1
+
+	local tierTypeFactor = lpdbData.liquipediatiertype == 'Qualifier' and 0.001 or 1
+
+	local prize = tonumber(lpdbData.individualprizemoney) or 0
+	prize = prize ~= 0 and prize or DEFAULT_PRIZE_VALUE
+
+	if place == 'w' or place == 'd' or place == 'q' then
+		prize = 2
+		place = '1'
+	end
+
+	local placementFactor = tonumber(Array.parseCommaSeparatedString(place, '-')[1]) --[[@as integer]]
+
+	return tierFactor * (prize / placementFactor) * tierTypeFactor
+end
+
+return CustomPrizePool
