db11af49149e8d7c9417e4e6734e79edfc708ca4
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2026-08-03 17:12:45 +0900

    fix(match2): bracket opponent entries not using dynamic teamstyle (#7893)
    switch back to using dynamic

--- lua/wikis/commons/Widget/Match/Bracket/Opponent/Starcraft.lua
+++ lua/wikis/commons/Widget/Match/Bracket/Opponent/Starcraft.lua
@@ -25,7 +25,7 @@ local function StarcraftBracketOpponent(props)
 		playerClass = 'starcraft-bracket-block-player',
 		showLink = false,
 		showTbd = props.showTbd,
-		teamStyle = props.forceShortName and 'short' or 'hybrid',
+		teamStyle = props.forceShortName and 'short' or 'dynamic',
 	}
 end
 
