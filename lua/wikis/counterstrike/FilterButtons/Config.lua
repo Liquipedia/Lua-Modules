ec47a9b93379bbfd27a1e83aea32b08d6bd9565f
SyntacticSalt <syntactic@mbergen.de>
2026-08-05 13:44:53 +0200

    fix online stage

--- lua/wikis/counterstrike/FilterButtons/Config.lua
+++ lua/wikis/counterstrike/FilterButtons/Config.lua
@@ -34,9 +34,9 @@ Config.categories = {
 		property = 'liquipediaTierType',
 		expandable = true,
 		load = function(category)
-			category.items = {'Onlinestage', 'Monthly', 'Weekly', 'Qualifier', 'Showmatch'}
+			category.items = {'Online Stage', 'Monthly', 'Weekly', 'Qualifier', 'Showmatch'}
 		end,
-		defaultItems = {'Onlinestage'},
+		defaultItems = {'Online Stage'},
 		transform = function(tiertype)
 			return select(2, Tier.toName(1, tiertype))
 		end,
