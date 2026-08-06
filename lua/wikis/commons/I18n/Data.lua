ee59b0738b94fb7641281983a1ab84680f9d6d3d
Vladimir <48841210+LemurVladimir@users.noreply.github.com>
2026-08-06 16:10:44 +0300

    Add Russian translations for various UI elements

--- lua/wikis/commons/I18n/Data.lua
+++ lua/wikis/commons/I18n/Data.lua
@@ -123,5 +123,126 @@ return {
 
 		-- Shop Merch
 		['shop-merch-support-text'] = 'Purchases through this link support Liquipedia.',
+	},
+	ru = {
+		-- Match Stream / Трансляции матча
+		['matchstream-watch-live'] = 'Смотреть',
+		['matchstream-watch-upcoming'] = 'Трансляция',
+
+		-- Match Details / Детали матча
+		['matchdetails-view-long'] = 'Детали матча',
+		['matchdetails-add-long'] = 'Дополнить',
+		['matchdetails-short'] = 'Детали',
+
+		-- Tournament Filter / Фильтр турниров
+		['tournament-ticker-no-tournaments'] = 'Не найдено турниров для выбранных фильтров!',
+
+		-- Filter Buttons / Кнопки фильтра
+		['filterbuttons-all'] = 'Все',
+		['filterbuttons-featured'] = 'Особые',
+
+		-- Dates / Даты
+		['date-unknown'] = 'Б/О',
+		['date-range-unknown'] = 'Б/О',
+
+		-- Dates: Only startYear known / Даты: Известен год начала
+		['date-range-year'] = '${startYear}',
+		['date-range-year--unknown'] = '${startYear} – Б/О',
+		['date-range-year--year'] = '${startYear}–${endYear}',
+
+		-- Dates: Only startYear, startMonth known / Даты: Известны месяц и год начала
+		['date-range-year-month'] = '${startMonth} ${startYear}',
+		['date-range-year-month--unknown'] = '${startMonth} ${startYear} – Б/О',
+		['date-range-year-month--year-unknown_month'] = '${startMonth} ${startYear} – Б/О, ${endYear}',
+		['date-range-year-month--month'] = '${startMonth} – ${endMonth} ${startYear}',
+		['date-range-year-month--year-month'] = '${startMonth} ${startYear} – ${endMonth} ${endYear}',
+
+		['date-range-year-month--unknown_month'] = '${startMonth} – Б/О, ${startYear}',
+
+		-- Dates: Full startdate known / Даты: Известна полная дата начала
+		['date-range-year-month-day'] = '${startDate} ${startMonth} ${startYear}',
+		['date-range-year-month-day--unknown'] = '${startDate} ${startMonth} ${startYear} – Б/О',
+		['date-range-year-month-day--year-unknown_month'] = '${startDate} ${startMonth} ${startYear} – Б/О, ${endYear}',
+		['date-range-year-month-day--year-month-unknown_day']
+				= '${startDate} ${startMonth} ${startYear} – Б/О, ${endMonth} ${endYear}',
+		['date-range-year-month-day--year-month-day']
+				= '${startDate} ${startMonth} ${startYear} – ${endDate} ${endMonth} ${endYear}',
+
+		['date-range-year-month-day--month-day'] = '${startDate} ${startMonth} – ${endDate} ${endMonth} ${startYear}',
+		['date-range-year-month-day--month-unknown_day'] = '${startDate} ${startMonth} –  Б/О, ${endMonth} ${startYear}',
+		['date-range-year-month-day--day'] = '${startDate}–${endDate} ${startMonth} ${startYear}',
+
+		-- Dates: ticker variant (hidden years) / Даты: Без отображения года
+		-- startMonth known / Известен месяц начала
+		['date-range-month'] = '${startMonth}',
+		['date-range-month--unknown'] = '${startMonth} – Б/О',
+		['date-range-month--unknown_month'] = '${startMonth} – Б/О',
+		['date-range-month--month'] = '${startMonth} – ${endMonth}',
+
+		-- startMonth and startDay known / Известны день и месяц начала
+		['date-range-month-day'] = '${startDate} ${startMonth}',
+		['date-range-month-day--day'] = '${startDate}–${endDate} ${startMonth}',
+
+		['date-range-month-day--unknown'] = '${startDate} ${startMonth} – Б/О',
+		['date-range-month-day--unknown_month'] = '${startDate} ${startMonth} – Б/О',
+		['date-range-month-day--month-unknown_day'] = '${startDate} ${startMonth} – ${endMonth} Б/О',
+		['date-range-month-day--month-day'] = '${startMonth} ${startDate} – ${endMonth} ${endDate}',
+
+		-- HiddenDataBox warnings / Ошибки HiddenDataBox
+		['hiddendatabox-invalid-parent-warning'] = '${parent} не является турниром Liquipedia',
+		['hiddendatabox-invalid-tier-warning'] = '${tierString} - не турнир Liquipedia ${tierMode}',
+
+		-- Bracket Headers / Заголовки брэкета
+		['brkts-header-r1'] = 'Гранд-финал,Финал,ГФ',
+		['brkts-header-r2'] = 'Полуфиналы,ПолуФ,ПФ',
+		['brkts-header-r3'] = 'Четвертьфиналы,ЧетвертьФ,ЧФ',
+		['brkts-header-r4'] = 'Раунд ${round},Р${round}',
+		['brkts-header-rx'] = 'Раунд ${round},Р${round}',
+
+		['brkts-header-u1'] = 'Гранд-финал,Финал,ГФ',
+		['brkts-header-u2'] = 'Финал верхней сетки,Финал ВС,ФВС',
+		['brkts-header-u3'] = 'Полуфинал верхней сетки,Полуфинал ВС,ПФВС',
+		['brkts-header-u4'] = 'Четвертьфинал верхней сетки,Четвертьфинал ВС,ЧФВС',
+		['brkts-header-ux'] = 'Верхняя сетка Раунд ${round},ВС Раунд ${round},ВСР${round}',
+
+		['brkts-header-m1'] = 'Финал средней сетки,Финал СС,ССФ',
+		['brkts-header-m2'] = 'Полуфинал средней сетки,Полуфинал СС,ПФСС',
+		['brkts-header-m3'] = 'Четвертьфинал средней сетки,Четвертьфинал СС,ЧФСС',
+		['brkts-header-m4'] = 'Средняя сетка Раунд ${round},СС Раунд ${round},ССР${round}',
+		['brkts-header-mx'] = 'Средняя сетка Раунд ${round},СС Раунд ${round},ССР${round}',
+
+		['brkts-header-l2'] = 'Финал нижней сетки,Финал НС,НСФ',
+		['brkts-header-l3'] = 'Полуфинал нижней сетки,Полуфинал НС,ПФНС',
+		['brkts-header-l4'] = 'Четвертьфинал нижней сетки,Четвертьфинал НС,ЧФНС',
+		['brkts-header-lx'] = 'Нижняя сетка Раунд ${round},НС Раунд ${round},НСР${round}',
+
+		['brkts-header-q'] = 'Qualified,Qual.,Q',
+		-- ['brkts-header-q'] = 'Квалифицированы,Квал.,К', === This one was intentionally left commented out. ===
+		['brkts-header-tp'] = 'Матч за 3-е место,3-е место,3-е',
+
+		-- MatchPage meta description / Мета-описание страницы MatchPage
+		['matchpage-meta-desc'] = 'Find detailed results about the ${ongoingTense}${game} match ' ..
+									'between ${opponent1} and ${opponent2} in ${tournamentName}${tense}.',
+		['matchpage-meta-desc'] = 'Ищите детализированные результаты о ${ongoingTense} матче ${game} ' ..
+									'между ${opponent1} и ${opponent2} в ${tournamentName}${tense}.',
+		['matchpage-meta-desc-no-opponent']
+				= 'Ищите детализированные результаты о ${ongoingTense} матче ${game} в ${tournamentName}${tense}.',
+
+		-- Team Participant Card / Карточка команды-участника
+		['participants-hover-roster-label'] = 'Состав игроков',
+		['participants-notes-label'] = 'Заметки (${count})',
+
+		-- MatchSummary Map Veto
+		['matchsummary-mapveto-start'] = 'Карты в вето',
+
+		-- MatchTable
+		['matchtable-no-match-results'] = 'Этот участник ещё не играл матчи.',
+		['matchtable-no-h2h-match-results'] = 'Эти ${mode} ещё не играли матчи друг против друга.',
+
+		-- MatchTicker
+		['matchticker-upcoming-matches'] = 'Будущие матчи',
+
+		-- Shop Merch / Мерч в магазине
+		['shop-merch-support-text'] = 'Покупки по этой ссылке поддерживают Liquipedia.',
 	}
 }
