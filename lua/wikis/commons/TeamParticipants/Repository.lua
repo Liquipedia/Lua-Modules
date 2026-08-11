0edff93d85e50fca5e1fc27224d6f25c0391997d
SyntacticSalt <syntactic@mbergen.de>
2026-08-11 09:32:10 +0200

    Store variables from "raw" alias list

--- lua/wikis/commons/TeamParticipants/Repository.lua
+++ lua/wikis/commons/TeamParticipants/Repository.lua
@@ -149,16 +149,9 @@ end
 ---@param participant TeamParticipant
 function TeamParticipantsRepository.setPageVars(participant)
 	Array.forEach(participant.aliases or {}, function(teamTemplate)
-		local teamName = TeamTemplate.getPageNameNoRedirect(teamTemplate)
-		if not teamName then
-			return
-		end
-		local teamNameRedirected = mw.ext.TeamLiquidIntegration.resolve_redirect(teamName)
 		local teamPrefixes = {
-			teamName:gsub('_', ' '),
-			teamName:gsub(' ', '_'),
-			teamNameRedirected:gsub('_', ' '),
-			teamNameRedirected:gsub(' ', '_'),
+			teamTemplate:gsub('_', ' '),
+			teamTemplate:gsub(' ', '_'),
 		}
 		local playerCount, staffCount = 0, 0
 		Array.forEach(participant.opponent.players or {}, function(player)
