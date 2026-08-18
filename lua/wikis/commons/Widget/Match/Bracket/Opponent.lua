54f33c9413a8a309d6edb87089e1ddbfad0b0494
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2026-08-18 21:13:26 +0200

    Update Opponent.lua

--- lua/wikis/commons/Widget/Match/Bracket/Opponent.lua
+++ lua/wikis/commons/Widget/Match/Bracket/Opponent.lua
@@ -7,7 +7,8 @@
 
 local Lua = require('Module:Lua')
 
-local OpponentDisplay = Lua.import('Module:OpponentDisplay')
+local Opponent = Lua.import('Module:Opponent/Custom')
+local OpponentDisplay = Lua.import('Module:OpponentDisplay/Custom')
 
 local Component = Lua.import('Module:Widget/Component')
 
