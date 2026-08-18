d0ff6a403d359d5bc2525eb142128ff7f13ba94b
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2026-08-18 15:44:24 +0900

    referencetag docs

--- lua/wikis/commons/Widget/ReferenceTag.lua
+++ lua/wikis/commons/Widget/ReferenceTag.lua
@@ -10,6 +10,27 @@ local Lua = require('Module:Lua')
 local Component = Lua.import('Module:Widget/Component')
 local Renderer = Lua.import('Module:Widget/Renderer')
 
+--[[
+Widget component that is roughly equivalent to <ref> tags in MediaWiki.
+
+For example, the following Lua code snippet:
+
+```lua
+ReferenceTag{
+	name = 'Hello World',
+	children = {
+		'Lorem ipsum ',
+		'dolor sit amet'
+	}
+}
+```
+
+is roughly equivalent to the following wikicode snippet:
+
+```html
+<ref name="Hello World">Lorem ipsum dolor sit amet</ref>
+```
+]]
 ---@param props {frame: Frame?, name: string?, group: string?, children: Renderable|Renderable[]?}
 ---@param context Context?
 ---@return string
