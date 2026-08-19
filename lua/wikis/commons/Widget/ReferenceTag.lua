0ffd9d471619713014e84e53f906d571e19c83ae
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2026-08-19 10:03:53 +0900

    grammar

--- lua/wikis/commons/Widget/ReferenceTag.lua
+++ lua/wikis/commons/Widget/ReferenceTag.lua
@@ -40,7 +40,7 @@ local function ReferenceTag(props, context)
 		'ref',
 		--[[
 		Because Frame:extensionTag expects string argument for content, we cannot directly
-		pass children in its raw form. Thus, we manually call Renderer.render here instead
+		pass children in their raw form. Thus, we manually call Renderer.render here instead
 		of letting it be called after processing the parent component.
 		]]
 		Renderer.render(props.children, context),
