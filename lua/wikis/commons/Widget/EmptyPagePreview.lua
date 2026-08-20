ec7c106da9faa70e01e4098a497c5c8ee578e533
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2026-08-20 11:51:53 +0200

    feat(EmptyPagePreview): Add ambox about it only being a preview (#7971)
    * feat:(EmptyPagePreview): Add ambox about it only being a preview
    
    * port to widget v3
    
    * thx linter
    
    * fix typo
    
    * adjust styling to be better
    
    * as per review
    
    * Update lua/wikis/commons/Widget/ArticleMessageBox.lua
    
    Co-authored-by: ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
    
    * Update lua/wikis/commons/Widget/ArticleMessageBox.lua
    
    Co-authored-by: ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
    
    * chore: update visual snapshots
    
    * chore: update visual snapshots
    
    ---------
    
    Co-authored-by: ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>

--- lua/wikis/commons/Widget/EmptyPagePreview.lua
+++ lua/wikis/commons/Widget/EmptyPagePreview.lua
@@ -7,29 +7,45 @@
 
 local Lua = require('Module:Lua')
 
-local Class = Lua.import('Module:Class')
 local Namespace = Lua.import('Module:Namespace')
 local TeamTemplate = Lua.import('Module:TeamTemplate')
 
+local AmBox = Lua.import('Module:Widget/ArticleMessageBox')
+local Component = Lua.import('Module:Widget/Component')
 local EmptyTeamPagePreview = Lua.import('Module:Widget/EmptyPagePreview/Team')
 local EmptyPersonPagePreview = Lua.import('Module:Widget/EmptyPagePreview/Person')
-local Widget = Lua.import('Module:Widget')
+local Link = Lua.import('Module:Widget/Basic/Link')
 
----@class EmptyPagePreview: Widget
----@operator call(table): EmptyPagePreview
-local EmptyPagePreview = Class.new(Widget)
 
----@return Widget?
-function EmptyPagePreview:render()
+---@param props EmptyTeamPagePreviewProps|{pageName: string}
+---@return Renderable[]?
+local EmptyPagePreview = function(props)
 	if not Namespace.isMain() then
 		return
 	end
 
-	if TeamTemplate.exists(self.props.pageName or mw.title.getCurrentTitle().prefixedText) then
-		return EmptyTeamPagePreview(self.props)
+	local previewWarning = AmBox{
+		image = 'Liquipedia logo.png',
+		imageSize = '60px',
+		text = {
+			'You are currently viewing an automatically generated preview page. ',
+			'In future, a page may be created for the topic if it meets the ',
+			Link{link = 'Liquipedia:Notability_Guidelines', children = 'notability requirements'},
+			'.',
+		}
+	}
+
+	if TeamTemplate.exists(props.pageName) then
+		return {
+			previewWarning,
+			EmptyTeamPagePreview(props)
+		}
 	end
 
-	return EmptyPersonPagePreview(self.props)
+	return {
+		previewWarning,
+		EmptyPersonPagePreview(props)
+	}
 end
 
-return EmptyPagePreview
+return Component.component(EmptyPagePreview, {pageName = mw.title.getCurrentTitle().prefixedText})
