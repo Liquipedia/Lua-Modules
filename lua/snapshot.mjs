66d37d5bfc0df04c5c8404da6df4a1245335c9a6
SyntacticSalt <syntactic@mbergen.de>
2026-08-26 10:12:25 +0200

    Update snapshot.mjs

--- lua/snapshot.mjs
+++ lua/snapshot.mjs
@@ -78,7 +78,8 @@ const PIXELMATCH_OPTIONS = { threshold: 0.1 };
 		create: {
 			width: maxWidth,
 			height: height,
-			channels: 4
+			channels: 4,
+    		background: { r: 255, g: 0, b: 0, alpha: 1 }
 		}
 	}).composite([
 		{
