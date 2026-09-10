eda81f41387323b269dd4185e5ba231c3f4f5e89
Steve23 <Steve23liquipedia@gmail.com>
2026-09-10 19:03:05 +0700

    feat: moved PlacementStats to module

---
+++ lua/wikis/mobilelegends/Infobox/Team/Custom.lua
@@ -0,0 +1,32 @@
+---
+-- @Liquipedia
+-- page=Module:Infobox/Team/Custom
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Lua = require('Module:Lua')
+
+local Class = Lua.import('Module:Class')
+
+local Team = Lua.import('Module:Infobox/Team')
+local PlacementStats = Lua.import('Module:Infobox/Extension/PlacementStats')
+
+---@class MobileLegendsInfoboxTeam: InfoboxTeam
+---@operator call(Frame): MobileLegendsInfoboxTeam
+local CustomTeam = Class.new(Team)
+
+---@param frame Frame
+---@return VNode
+function CustomTeam.run(frame)
+	local team = CustomTeam(frame)
+
+	return team:createInfobox()
+end
+
+---@return Html|string
+function CustomTeam:createBottomContent()
+	return PlacementStats.run{}
+end
+
+return CustomTeam
