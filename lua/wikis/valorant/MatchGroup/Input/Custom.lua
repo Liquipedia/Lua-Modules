36f202a0f03cbf6d9570aa3d91c2a0eaa3ba1509
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2026-09-10 10:48:05 +0900

    make php happy

--- lua/wikis/valorant/MatchGroup/Input/Custom.lua
+++ lua/wikis/valorant/MatchGroup/Input/Custom.lua
@@ -196,7 +196,7 @@ function MatchFunctions.parseSkirmish(match, opponents)
 		return
 	end
 	return {
-		players = {player1, player2},
+		players = Table.deepCopy{player1, player2},
 		scores = scores,
 		winner = tonumber(skirmishData.winner) or (match.finished and (scores[1] > scores[2] and 1 or 2) or nil)
 	}
