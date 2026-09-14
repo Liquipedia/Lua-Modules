72df6735f911213a384cd7f384cb9bfa82ef0b5e
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2026-09-14 11:42:02 +0900

    adjust import in test

--- lua/spec/metadata_generator_spec.lua
+++ lua/spec/metadata_generator_spec.lua
@@ -1,7 +1,7 @@
 --- Triple Comment to Enable our LLS Plugin
 describe('metadata generator', function()
 	SetActiveWiki('counterstrike')
-	local MetadataGenerator = require('wikis.commons.MetadataGenerator')
+	local MetadataGenerator = require('Module:MetadataGenerator')
 
 	teardown(function ()
 		SetActiveWiki('')
