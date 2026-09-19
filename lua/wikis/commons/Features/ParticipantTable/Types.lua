1fdaf61c4874f19fc7878eeedee4d72f62c7344f
hjpalpha <andreasbuesch1@gmail.com>
2026-09-19 12:38:07 +0000

    move anno class def out of entry into types

---
+++ lua/wikis/commons/Features/ParticipantTable/Types.lua
@@ -0,0 +1,18 @@
+---
+-- @Liquipedia
+-- page=Module:Features/ParticipantTable/Types
+--
+-- Please see https://github.com/Liquipedia/Lua-Modules to contribute
+--
+
+local Types = {}
+
+---@class ParticipantsTableEntryProps
+---@field opponent standardOpponent
+---@field note string?
+---@field dq boolean?
+---@field config ParticipantTableConfig
+---@field additionalProps table?
+---@field useDefaultWidth boolean?
+
+return Types
